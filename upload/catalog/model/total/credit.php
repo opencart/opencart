180e6e691eedd7d9e914bf58300fb0dd9d3333b5
blueyon@gmail.com <blueyon@gmail.com@a472ed72-f7c0-11dd-9d1a-b59b2e1864b6>
2011-04-29 13:45:10 +0000

    fixed Issue 225

--- upload/catalog/model/total/credit.php
+++ upload/catalog/model/total/credit.php
@@ -31,7 +31,7 @@ class ModelTotalCredit extends Model {
 	public function confirm($order_info, $order_total) {
 		$this->load->language('total/credit');
 		
-		if ($order['customer_id']) {
+		if ($order_info['customer_id']) {
 			$this->db->query("INSERT INTO " . DB_PREFIX . "customer_transaction SET customer_id = '" . (int)$order_info['customer_id'] . "', order_id = '" . (int)$order_info['order_id'] . "', description = '" . $this->db->escape(sprintf($this->language->get('text_order_id'), (int)$order_info['order_id'])) . "', amount = '" . (float)$order_total['value'] . "', date_added = NOW()");				
 		}
 	}	

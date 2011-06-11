34ac0bafe9398c998f00ea76085fc6487ef5cebf
blueyon@gmail.com <blueyon@gmail.com@a472ed72-f7c0-11dd-9d1a-b59b2e1864b6>
2011-06-11 10:31:54 +0000

    fixed checkout success message.

--- upload/catalog/controller/checkout/success.php
+++ upload/catalog/controller/checkout/success.php
@@ -48,8 +48,12 @@ class ControllerCheckoutSuccess extends Controller {
 		
     	$this->data['heading_title'] = $this->language->get('heading_title');
 
-    	$this->data['text_message'] = sprintf($this->language->get('text_message'), $this->url->link('account/account', '', 'SSL'), $this->url->link('account/order', '', 'SSL'), $this->url->link('account/download', '', 'SSL'), $this->url->link('information/contact'));
-
+		if ($this->customer->isLogged()) {
+    		$this->data['text_message'] = sprintf($this->language->get('text_customer'), $this->url->link('account/account', '', 'SSL'), $this->url->link('account/order', '', 'SSL'), $this->url->link('account/download', '', 'SSL'), $this->url->link('information/contact'));
+		} else {
+    		$this->data['text_message'] = sprintf($this->language->get('text_guest'), $this->url->link('information/contact'));
+		}
+		
     	$this->data['button_continue'] = $this->language->get('button_continue');
 
     	$this->data['continue'] = $this->url->link('common/home');

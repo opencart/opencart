355bc0a8c596e2812beaaaf35b0c584533078db1
blueyon@gmail.com <blueyon@gmail.com@a472ed72-f7c0-11dd-9d1a-b59b2e1864b6>
2011-06-14 15:21:42 +0000

    affiliate link fix

--- upload/catalog/controller/affiliate/tracking.php
+++ upload/catalog/controller/affiliate/tracking.php
@@ -79,7 +79,7 @@ class ControllerAffiliateTracking extends Controller {
 			foreach ($results as $result) {
 				$json[] = array(
 					'name' => html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'),
-					'link' => $this->url->link('product/product', 'product_id=' . $result['product_id'] . '&tracking=' . $this->affiliate->getCode())				
+					'link' => str_replace('&amp;', '&', $this->url->link('product/product', 'product_id=' . $result['product_id'] . '&tracking=' . $this->affiliate->getCode()))			
 				);	
 			}
 		}

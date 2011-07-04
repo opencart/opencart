aba59ff1438a11aea83f5776cc3d36fc1b84c122
blueyon@gmail.com <blueyon@gmail.com@a472ed72-f7c0-11dd-9d1a-b59b2e1864b6>
2011-07-04 14:20:38 +0000

    removed redundant data from the zone to geo zone table

--- upload/install/opencart.sql
+++ upload/install/opencart.sql
@@ -7024,5 +7024,4 @@ CREATE TABLE `oc_zone_to_geo_zone` (
 
 INSERT INTO `oc_zone_to_geo_zone` (`zone_to_geo_zone_id`, `country_id`, `zone_id`, `geo_zone_id`, `date_added`, `date_modified`) VALUES
 (57, 222, 0, 3, '2010-02-26 22:33:24', '0000-00-00 00:00:00'),
-(51, 81, 0, 5, '2010-02-09 16:27:20', '0000-00-00 00:00:00'),
 (65, 222, 0, 4, '2010-12-15 15:18:13', '0000-00-00 00:00:00');

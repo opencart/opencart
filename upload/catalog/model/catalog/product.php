c41dc41e73e395f491f1179e1d174f1c62c30365
blueyon@gmail.com <blueyon@gmail.com@a472ed72-f7c0-11dd-9d1a-b59b2e1864b6>
2011-07-29 19:11:38 +0000

    language list fix

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -65,7 +65,7 @@ class ModelCatalogProduct extends Model {
 		
 		$cache = md5(http_build_query($data));
 		
-		$product_data = $this->cache->get('product.' . $cache . '.' . $customer_group_id);
+		$product_data = $this->cache->get('product.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . (int)$customer_group_id . '.' . $cache);
 		
 		if (!$product_data) {
 			$sql = "SELECT p.product_id, (SELECT AVG(rating) AS total FROM " . DB_PREFIX . "review r1 WHERE r1.product_id = p.product_id AND r1.status = '1' GROUP BY r1.product_id) AS rating FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id)"; 
@@ -164,7 +164,7 @@ class ModelCatalogProduct extends Model {
 				$product_data[$result['product_id']] = $this->getProduct($result['product_id']);
 			}
 			
-			$this->cache->set('product.' . $cache . '.' . $customer_group_id, $product_data);
+			$this->cache->set('product.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . (int)$customer_group_id . '.' . $cache, $product_data);
 		}
 		
 		return $product_data;

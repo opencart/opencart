dadcb19d0946c7ed9e0dd5a4e68f14625c35be38
Daniel Kerr <webmaster@opencart.com>
2012-07-27 22:14:50 +0800

    little bit of optimization for the controller.

--- upload/system/engine/controller.php
+++ upload/system/engine/controller.php
@@ -32,16 +32,15 @@ abstract class Controller {
 	
 	protected function getChild($child, $args = array()) {
 		$action = new Action($child, $args);
-		$file = $action->getFile();
-		$class = $action->getClass();
-		$method = $action->getMethod();
 	
-		if (file_exists($file)) {
-			require_once($file);
+		if (file_exists($action->getFile())) {
+			require_once($action->getFile());
+
+			$class = $action->getClass();
 
 			$controller = new $class($this->registry);
-			
-			$controller->$method($args);
+
+			$controller->{$action->getMethod()}($action->getArgs());
 			
 			return $controller->output;
 		} else {

5f622c4edfd1e827b37d37eeeaf9c63c3f2c9463
Richard Kuhnt <r15ch13+git@gmail.com>
2012-08-01 17:24:30 +0200

    autoloading language before rendering. can be overwritten between $this->language->load() and $this->render()

--- upload/system/engine/controller.php
+++ upload/system/engine/controller.php
@@ -1,25 +1,25 @@
 <?php
 abstract class Controller {
-	protected $registry;	
+	protected $registry;
 	protected $id;
 	protected $layout;
 	protected $template;
 	protected $children = array();
 	protected $data = array();
 	protected $output;
-	
+
 	public function __construct($registry) {
 		$this->registry = $registry;
 	}
-	
+
 	public function __get($key) {
 		return $this->registry->get($key);
 	}
-	
+
 	public function __set($key, $value) {
 		$this->registry->set($key, $value);
 	}
-			
+
 	protected function forward($route, $args = array()) {
 		return new Action($route, $args);
 	}
@@ -27,12 +27,12 @@ abstract class Controller {
 	protected function redirect($url, $status = 302) {
 		header('Status: ' . $status);
 		header('Location: ' . str_replace(array('&amp;', "\n", "\r"), array('&', '', ''), $url));
-		exit();				
+		exit();
 	}
-	
+
 	protected function getChild($child, $args = array()) {
 		$action = new Action($child, $args);
-	
+
 		if (file_exists($action->getFile())) {
 			require_once($action->getFile());
 
@@ -41,35 +41,36 @@ abstract class Controller {
 			$controller = new $class($this->registry);
 
 			$controller->{$action->getMethod()}($action->getArgs());
-			
+
 			return $controller->output;
 		} else {
 			trigger_error('Error: Could not load controller ' . $child . '!');
-			exit();					
-		}		
+			exit();
+		}
 	}
-	
+
 	protected function render() {
+		$this->data = array_merge($this->language->getData(), $this->data);
+
 		foreach ($this->children as $child) {
 			$this->data[basename($child)] = $this->getChild($child);
 		}
-		
+
 		if (file_exists(DIR_TEMPLATE . $this->template)) {
 			extract($this->data);
-			
+
       		ob_start();
-      
+
 	  		require(DIR_TEMPLATE . $this->template);
-      
+
 	  		$this->output = ob_get_contents();
 
       		ob_end_clean();
-      		
+
 			return $this->output;
     	} else {
 			trigger_error('Error: Could not load template ' . DIR_TEMPLATE . $this->template . '!');
-			exit();				
+			exit();
     	}
 	}
 }
-?>

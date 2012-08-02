540ee8ad4924c8f2ae8060902ec4c9b5d2ff9aaf
Daniel Kerr <webmaster@opencart.com>
2012-08-02 22:20:05 +0800

    important fedex work restored

--- upload/admin/controller/shipping/fedex.php
+++ upload/admin/controller/shipping/fedex.php
@@ -5,16 +5,16 @@ class ControllerShippingFedex extends Controller {
 	public function index() {   
 		$this->load->language('shipping/fedex');
 
-		$this->document->title = $this->language->get('heading_title');
+		$this->document->setTitle($this->language->get('heading_title'));
 		
 		$this->load->model('setting/setting');
 				
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && ($this->validate())) {
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
 			$this->model_setting_setting->editSetting('fedex', $this->request->post);		
 					
 			$this->session->data['success'] = $this->language->get('text_success');
 						
-			$this->redirect($this->url->https('extension/shipping'));
+			$this->redirect($this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL'));
 		}
 				
 		$this->data['heading_title'] = $this->language->get('heading_title');
@@ -23,13 +23,18 @@ class ControllerShippingFedex extends Controller {
 		$this->data['text_disabled'] = $this->language->get('text_disabled');
 		$this->data['text_all_zones'] = $this->language->get('text_all_zones');
 		$this->data['text_none'] = $this->language->get('text_none');
-		$this->data['text_yes'] = $this->language->get('text_yes');
-		$this->data['text_no'] = $this->language->get('text_no');
 		
+		$this->data['entry_key'] = $this->language->get('entry_key');
+		$this->data['entry_password'] = $this->language->get('entry_password');
 		$this->data['entry_account'] = $this->language->get('entry_account');
 		$this->data['entry_meter'] = $this->language->get('entry_meter');
 		$this->data['entry_test'] = $this->language->get('entry_test');
-		$this->data['entry_tax'] = $this->language->get('entry_tax');
+		$this->data['entry_service'] = $this->language->get('entry_service');
+		$this->data['entry_delivery_type'] = $this->language->get('entry_delivery_type');
+		$this->data['entry_packaging_type'] = $this->language->get('entry_packaging_type');
+		$this->data['entry_rate_type'] = $this->language->get('entry_rate_type');
+		$this->data['entry_destination_type'] = $this->language->get('entry_destination_type');
+		$this->data['entry_tax_class'] = $this->language->get('entry_tax_class');
 		$this->data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
 		$this->data['entry_status'] = $this->language->get('entry_status');
 		$this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
@@ -37,14 +42,24 @@ class ControllerShippingFedex extends Controller {
 		$this->data['button_save'] = $this->language->get('button_save');
 		$this->data['button_cancel'] = $this->language->get('button_cancel');
 
-		$this->data['tab_general'] = $this->language->get('tab_general');
-
  		if (isset($this->error['warning'])) {
 			$this->data['error_warning'] = $this->error['warning'];
 		} else {
 			$this->data['error_warning'] = '';
 		}
-
+		
+ 		if (isset($this->error['key'])) {
+			$this->data['error_key'] = $this->error['key'];
+		} else {
+			$this->data['error_key'] = '';
+		}
+		
+ 		if (isset($this->error['password'])) {
+			$this->data['error_password'] = $this->error['password'];
+		} else {
+			$this->data['error_password'] = '';
+		}
+				
  		if (isset($this->error['account'])) {
 			$this->data['error_account'] = $this->error['account'];
 		} else {
@@ -57,30 +72,42 @@ class ControllerShippingFedex extends Controller {
 			$this->data['error_meter'] = '';
 		}
 		
-  		$this->document->breadcrumbs = array();
+  		$this->data['breadcrumbs'] = array();
 
-   		$this->document->breadcrumbs[] = array(
-       		'href'      => $this->url->https('common/home'),
+   		$this->data['breadcrumbs'][] = array(
        		'text'      => $this->language->get('text_home'),
-      		'separator' => FALSE
+			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+      		'separator' => false
    		);
 
-   		$this->document->breadcrumbs[] = array(
-       		'href'      => $this->url->https('extension/shipping'),
+   		$this->data['breadcrumbs'][] = array(
        		'text'      => $this->language->get('text_shipping'),
+			'href'      => $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL'),
       		'separator' => ' :: '
    		);
 		
-   		$this->document->breadcrumbs[] = array(
-       		'href'      => $this->url->https('shipping/fedex'),
+   		$this->data['breadcrumbs'][] = array(
        		'text'      => $this->language->get('heading_title'),
+			'href'      => $this->url->link('shipping/fedex', 'token=' . $this->session->data['token'], 'SSL'),
       		'separator' => ' :: '
    		);
 		
-		$this->data['action'] = $this->url->https('shipping/fedex');
+		$this->data['action'] = $this->url->link('shipping/fedex', 'token=' . $this->session->data['token'], 'SSL');
 		
-		$this->data['cancel'] = $this->url->https('extension/shipping');
-	
+		$this->data['cancel'] = $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL');
+		
+		if (isset($this->request->post['fedex_key'])) {
+			$this->data['fedex_key'] = $this->request->post['fedex_key'];
+		} else {
+			$this->data['fedex_key'] = $this->config->get('fedex_key');
+		}
+		
+		if (isset($this->request->post['fedex_password'])) {
+			$this->data['fedex_password'] = $this->request->post['fedex_password'];
+		} else {
+			$this->data['fedex_password'] = $this->config->get('fedex_password');
+		}
+					
 		if (isset($this->request->post['fedex_account'])) {
 			$this->data['fedex_account'] = $this->request->post['fedex_account'];
 		} else {
@@ -99,6 +126,103 @@ class ControllerShippingFedex extends Controller {
 			$this->data['fedex_test'] = $this->config->get('fedex_test');
 		}
 		
+		if (isset($this->request->post['fedex_priority_overnight'])) {
+			$this->data['fedex_priority_overnight'] = $this->request->post['fedex_priority_overnight'];
+		} else {
+			$this->data['fedex_priority_overnight'] = $this->config->get('fedex_priority_overnight');
+		}
+		
+		if (isset($this->request->post['fedex_standard_overnight'])) {
+			$this->data['fedex_standard_overnight'] = $this->request->post['fedex_standard_overnight'];
+		} else {
+			$this->data['fedex_standard_overnight'] = $this->config->get('fedex_standard_overnight');
+		}
+		
+		if (isset($this->request->post['fedex_first_overnight'])) {
+			$this->data['fedex_first_overnight'] = $this->request->post['fedex_first_overnight'];
+		} else {
+			$this->data['fedex_first_overnight'] = $this->config->get('fedex_first_overnight');
+		}	
+			
+		if (isset($this->request->post['fedex_2_day'])) {
+			$this->data['fedex_2_day'] = $this->request->post['fedex_2_day'];
+		} else {
+			$this->data['fedex_2_day'] = $this->config->get('fedex_2_day');
+		}	
+		
+		if (isset($this->request->post['fedex_express_saver'])) {
+			$this->data['fedex_express_saver'] = $this->request->post['fedex_express_saver'];
+		} else {
+			$this->data['fedex_express_saver'] = $this->config->get('fedex_express_saver');
+		}	
+		
+		if (isset($this->request->post['fedex_international_priority'])) {
+			$this->data['fedex_international_priority'] = $this->request->post['fedex_international_priority'];
+		} else {
+			$this->data['fedex_international_priority'] = $this->config->get('fedex_international_priority');
+		}
+		
+		if (isset($this->request->post['fedex_international_economy'])) {
+			$this->data['fedex_international_economy'] = $this->request->post['fedex_international_economy'];
+		} else {
+			$this->data['fedex_international_economy'] = $this->config->get('fedex_international_economy');
+		}
+		
+		if (isset($this->request->post['fedex_international_first'])) {
+			$this->data['fedex_international_first'] = $this->request->post['fedex_international_first'];
+		} else {
+			$this->data['fedex_international_first'] = $this->config->get('fedex_international_first');
+		}
+		
+		if (isset($this->request->post['fedex_1_day_freight'])) {
+			$this->data['fedex_1_day_freight'] = $this->request->post['fedex_1_day_freight'];
+		} else {
+			$this->data['fedex_1_day_freight'] = $this->config->get('fedex_1_day_freight');
+		}
+		
+		if (isset($this->request->post['fedex_2_day_freight'])) {
+			$this->data['fedex_2_day_freight'] = $this->request->post['fedex_2_day_freight'];
+		} else {
+			$this->data['fedex_2_day_freight'] = $this->config->get('fedex_2_day_freight');
+		}
+		
+		if (isset($this->request->post['fedex_3_day_freight'])) {
+			$this->data['fedex_3_day_freight'] = $this->request->post['fedex_3_day_freight'];
+		} else {
+			$this->data['fedex_3_day_freight'] = $this->config->get('fedex_3_day_freight');
+		}
+		
+		if (isset($this->request->post['fedex_ground'])) {
+			$this->data['fedex_ground'] = $this->request->post['fedex_ground'];
+		} else {
+
+			$this->data['fedex_ground'] = $this->config->get('fedex_ground');
+		}
+		
+		if (isset($this->request->post['fedex_ground_home'])) {
+			$this->data['fedex_ground_home'] = $this->request->post['fedex_ground_home'];
+		} else {
+			$this->data['fedex_ground_home'] = $this->config->get('fedex_ground_home');
+		}
+		
+		if (isset($this->request->post['fedex_international_priority_freight'])) {
+			$this->data['fedex_international_priority_freight'] = $this->request->post['fedex_international_priority_freight'];
+		} else {
+			$this->data['fedex_international_priority_freight'] = $this->config->get('fedex_international_priority_freight');
+		}
+		
+		if (isset($this->request->post['fedex_international_economy_freight'])) {
+			$this->data['fedex_international_economy_freight'] = $this->request->post['fedex_international_economy_freight'];
+		} else {
+			$this->data['fedex_international_economy_freight'] = $this->config->get('fedex_international_economy_freight');
+		}
+		
+		if (isset($this->request->post['fedex_europe_first_international_priority'])) {
+			$this->data['fedex_europe_first_international_priority'] = $this->request->post['fedex_europe_first_international_priority'];
+		} else {
+			$this->data['fedex_europe_first_international_priority'] = $this->config->get('fedex_europe_first_international_priority');
+		}
+																														
 		if (isset($this->request->post['fedex_tax_class_id'])) {
 			$this->data['fedex_tax_class_id'] = $this->request->post['fedex_tax_class_id'];
 		} else {
@@ -132,8 +256,12 @@ class ControllerShippingFedex extends Controller {
 		$this->data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
 								
 		$this->template = 'shipping/fedex.tpl';
-		
- 		$this->render();
+		$this->children = array(
+			'common/header',
+			'common/footer'
+		);
+				
+ 		$this->response->setOutput($this->render());
 	}
 	
 	private function validate() {
--- upload/admin/language/english/shipping/fedex.php
+++ upload/admin/language/english/shipping/fedex.php
@@ -1,17 +1,48 @@
 <?php
 // Heading
-$_['heading_title']    = 'Fedex';
+$_['heading_title']                            = 'Fedex';
 
 // Text
-$_['text_shipping']    = 'Shipping';
-$_['text_success']     = 'Success: You have modified Fedex shipping!';
+$_['text_shipping']                            = 'Shipping';
+$_['text_success']                             = 'Success: You have modified Fedex shipping!';
+$_['text_priority_overnight']                  = 'Priority Overnight';
+$_['text_standard_overnight']                  = 'Standard Overnight';
+$_['text_first_overnight']                     = 'First Overnight';
+$_['text_2_day']                               = 'Fedex 2 Day';
+$_['text_express_saver']                       = 'Fedex Express Saver';
+$_['text_international_priority']              = 'International Priority';
+$_['text_international_economy']               = 'International Economy';
+$_['text_international_first']                 = 'International First';
+$_['text_1_day_freight']                       = 'Fedex 1 Day Freight';
+$_['text_2_day_freight']                       = 'Fedex 2 Day Freight';
+$_['text_3_day_freight']                       = 'Fedex 3 Day Freight';
+$_['text_ground']                              = 'Ground';
+$_['text_ground_home']                         = 'Ground Home Delivery';
+$_['text_international_priority_freight']      = 'International Priority Freight';
+$_['text_international_economy_freight']       = 'International Economy Freight';
+$_['text_europe_first_international_priority'] = 'Europe First International Priority';
 
 // Entry
-$_['entry_tax_class']  = 'Tax Class:';
-$_['entry_geo_zone']   = 'Geo Zone:';
-$_['entry_status']     = 'Status:';
-$_['entry_sort_order'] = 'Sort Order:';
+$_['entry_key']                                = 'Key:';
+$_['entry_password']                           = 'Password:';
+$_['entry_account']                            = 'Account Number:';
+$_['entry_meter']                              = 'Meter Number:';
+$_['entry_test']                               = 'Test Mode:';
+$_['entry_service']                            = 'Services:';
+$_['entry_delivery_type']                      = 'Delivery Type:';
+$_['entry_packaging_type']                     = 'Packaging Type:';
+$_['entry_rate_type']                          = 'Rate Type:';
+$_['entry_destination_type']                   = 'Destination Type:';
+$_['entry_tax_class']                          = 'Tax Class:';
+$_['entry_geo_zone']                           = 'Geo Zone:';
+$_['entry_status']                             = 'Status:';
+$_['entry_sort_order']                         = 'Sort Order:';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify Fedex shipping!';
+$_['error_permission']                         = 'Warning: You do not have permission to modify Fedex shipping!';
+$_['error_key']                                = 'Key Required!';
+$_['error_password']                           = 'Password Required!';
+$_['error_account']                            = 'Account Required!';
+$_['error_meter']                              = 'Meter Required!';
+$_['error_service']                            = 'Service Required!';
 ?>

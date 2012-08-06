8ad1e29db155f5e617b9bc83e660f8f0960876a4
Ruslan Brest <rb@labtodo.com>
2012-08-06 08:22:48 +0300

    Product image on account/order/info page

--- upload/catalog/model/account/order.php
+++ upload/catalog/model/account/order.php
@@ -116,7 +116,7 @@ class ModelAccountOrder extends Model {
 	}
 	
 	public function getOrderProducts($order_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "order_product WHERE order_id = '" . (int)$order_id . "'");
+		$query = $this->db->query("SELECT op.*, p.image FROM `" . DB_PREFIX . "order_product` op LEFT JOIN `" . DB_PREFIX . "product` p ON (p.product_id = op.product_id) WHERE order_id = '" . (int)$order_id . "'");
 	
 		return $query->rows;
 	}

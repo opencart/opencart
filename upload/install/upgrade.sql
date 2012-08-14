aecf8e63e68c259f7ef7f3037b8fe8c952bbbd99
PortalXL Group <mysterio@xs4all.nl>
2012-08-14 02:41:53 -0700

    1. Database table oc_product_tag might be removed as it is not in default installation, update produces an error while called. 2. Database table oc_customer_group do not have a column named password, might be a typo, update produces an error while called.

--- upload/install/upgrade.sql
+++ upload/install/upgrade.sql
@@ -72,10 +72,6 @@ ALTER TABLE oc_tax_rate DROP priority;
 ALTER TABLE oc_tax_rate MODIFY rate decimal(15,4) NOT NULL DEFAULT '0.0000' COMMENT '';
 ALTER TABLE oc_tax_rate DROP description;
 
-ALTER TABLE oc_product_tag ADD INDEX product_id (product_id);
-ALTER TABLE oc_product_tag ADD INDEX language_id (language_id);
-ALTER TABLE oc_product_tag ADD INDEX tag (tag);
-
 
 #### START 1.5.2
 
@@ -242,8 +238,6 @@ ALTER TABLE `oc_affiliate` ADD `salt` varchar(9) COLLATE utf8_bin NOT NULL DEFAU
 ALTER TABLE `oc_customer`  MODIFY `ip` varchar(40) NOT NULL;
 ALTER TABLE `oc_customer_ip`  MODIFY `ip` varchar(40) NOT NULL;
 ALTER TABLE `oc_customer_ip_ip_blacklist`  MODIFY `ip` varchar(40) NOT NULL;
-
-ALTER TABLE oc_customer_group ADD `salt` varchar(9) COLLATE utf8_bin NOT NULL DEFAULT '' AFTER `password`;
 
 ALTER TABLE `oc_order` MODIFY `ip` varchar(40) NOT NULL;
 ALTER TABLE `oc_order` MODIFY `forwarded_ip` varchar(40) NOT NULL;

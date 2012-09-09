8520adc6053ef5893595c21575d155c50d5621e7
Daniel Kerr <webmaster@opencart.com>
2012-09-09 23:37:12 +0800

    more work done on the filter system.
    added the sql entries
    added the language translations when adding a new language

--- upload/admin/controller/catalog/filter.php
+++ upload/admin/controller/catalog/filter.php
@@ -381,10 +381,6 @@ class ControllerCatalogFilter extends Controller {
 			}
 		}
 
-		if (($this->request->post['type'] == 'select' || $this->request->post['type'] == 'radio' || $this->request->post['type'] == 'checkbox') && !isset($this->request->post['filter_value'])) {
-			$this->error['warning'] = $this->language->get('error_type');
-		}
-
 		if (isset($this->request->post['filter_value'])) {
 			foreach ($this->request->post['filter_value'] as $filter_value_id => $filter_value) {
 				foreach ($filter_value['filter_value_description'] as $language_id => $filter_value_description) {
--- upload/install/opencart.sql
+++ upload/install/opencart.sql
@@ -318,6 +318,7 @@ DROP TABLE IF EXISTS `oc_category`;
 CREATE TABLE `oc_category` (
   `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `image` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `path` text COLLATE utf8_bin NOT NULL,
   `parent_id` int(11) NOT NULL DEFAULT '0',
   `top` tinyint(1) NOT NULL,
   `column` int(3) NOT NULL,
@@ -1072,8 +1073,7 @@ CREATE TABLE `oc_download` (
 -- Dumping data for table `oc_download`
 --
 
-
--- --------------------------------------------------------
+----------------------------------------------------------
 
 --
 -- Table structure for table `oc_download_description`
@@ -1091,6 +1091,77 @@ CREATE TABLE `oc_download_description` (
 -- Dumping data for table `oc_download_description`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oc_filter`
+--
+
+DROP TABLE IF EXISTS `oc_filter`;
+CREATE TABLE `oc_filter` (
+  `filter_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sort_order` int(3) NOT NULL,
+  PRIMARY KEY (`filter_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `oc_filter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oc_filter_description`
+--
+
+DROP TABLE IF EXISTS `oc_filter_description`;
+CREATE TABLE `oc_filter_description` (
+  `filter_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL,
+  `name` varchar(64) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`filter_id`,`language_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `oc_filter_description`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oc_filter_value`
+--
+
+DROP TABLE IF EXISTS `oc_filter_value`;
+CREATE TABLE `oc_filter_value` (
+  `filter_value_id` int(11) NOT NULL AUTO_INCREMENT,
+  `filter_id` int(11) NOT NULL,
+  `sort_order` int(3) NOT NULL,
+  PRIMARY KEY (`filter_value_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `oc_filter_value`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oc_filter_value_description`
+--
+
+DROP TABLE IF EXISTS `oc_filter_value_description`;
+CREATE TABLE `oc_filter_value_description` (
+  `filter_value_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL,
+  `filter_id` int(11) NOT NULL,
+  `name` varchar(64) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`filter_value_id`,`language_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `oc_filter_value_description`
+--
 
 -- --------------------------------------------------------
 
@@ -2040,6 +2111,23 @@ INSERT INTO `oc_product_discount` (`product_discount_id`, `product_id`, `custome
 (440, 42, 1, 30, 1, '66.0000', '0000-00-00', '0000-00-00'),
 (439, 42, 1, 20, 1, '77.0000', '0000-00-00', '0000-00-00'),
 (438, 42, 1, 10, 1, '88.0000', '0000-00-00', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oc_product_filter`
+--
+
+DROP TABLE IF EXISTS `oc_product_filter`;
+CREATE TABLE `oc_product_filter` (
+  `product_id` int(11) NOT NULL,
+  `filter_value_id` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`,`filter_value_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `oc_product_filter`
+--
 
 -- --------------------------------------------------------
 

7b5257f03b17431d6f776e1b5e7e4bfc8f3f4a89
Ekrem KAYA <eka7a>
2012-10-26 22:50:48 +0300

    If free shipping is enable, disable others

--- upload/catalog/model/shipping/weight.php
+++ upload/catalog/model/shipping/weight.php
@@ -8,7 +8,7 @@ class ModelShippingWeight extends Model {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "geo_zone ORDER BY name");
 	
 		foreach ($query->rows as $result) {
-			if ($this->config->get('weight_' . $result['geo_zone_id'] . '_status')) {
+			if ($this->config->get('weight_' . $result['geo_zone_id'] . '_status') && $this->cart->getSubTotal() < $this->config->get('free_total')) {
 				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$result['geo_zone_id'] . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 			
 				if ($query->num_rows) {

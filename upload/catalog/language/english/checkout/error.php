c9d781916331f0e6032fe0ec1c2117f3b41a52df
Alberto Gonzalez de Dios <alberto.gdedios@ieee.org>
2012-10-28 18:21:00 +0100

    New checkout error controller (when payment fails)
    Now in Opencart, when a payment has failed (payment denied, payment
    canceled ...), there is o way of showing the customer that the order has
    not been processed. Opencart only have a success controller that shows a
    Success page with "Your Order Has Been Processed!" although the payment
    has failed.
    With these 3 new files, a payment gateway module can redirect the
    costumer to this error controller if the payment has failed.
    In the payment gateway, the return page of the bank should be managed by
    the payment gateway and determine if the order is processed or not. If
    yes, then redirect the costumer to checkout/success. If not, then
    redirect the costumer to checkout/error.
    You can see this behavior in my free payment gateway module called BBVA
    (http://www.opencart.com/index.php?route=extension/extension/info&extension_id=4309),
    although I redirect to checkout/checkout instead of error page.
    This controller is backward compatible. Payment gateways can implement
    it if developers want, but it´s not mandatory.
    The files are very similar to success.php (success controller), with
    some little modifications. In the controller, cart data is not clear to
    allow the costumer to retry the payment.

---
+++ upload/catalog/language/english/checkout/error.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title'] = 'Payment Processing Failed';
+$_['heading_title_customer'] = 'Payment Processing Failed';
+
+// Text
+$_['text_customer'] = '<p>Sorry, your order has not been processed. <p></p>Unfortunately there was an error while processing your payment.</p><p>Please, try again and verify the payment information.</p><p>We apologize for any inconvenience, please <a href="%s">contact us</a> if you are having difficulties making your payment.</p>';
+$_['text_basket']   = 'Basket';
+$_['text_checkout'] = 'Checkout';
+$_['text_error']  = 'Error';
+?>

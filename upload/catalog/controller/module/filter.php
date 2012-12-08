1c7068b70f80ce1fc59d17f1d855646301cbabfc
opencart <webmaster@opencart.com>
2012-12-09 01:21:57 +0800

    more filter stuff done

--- upload/catalog/controller/module/filter.php
+++ upload/catalog/controller/module/filter.php
@@ -1,10 +1,6 @@
 <?php  
 class ControllerModuleFilter extends Controller {
 	protected function index($setting) {
-		$this->language->load('module/filter');
-		
-    	$this->data['heading_title'] = $this->language->get('heading_title');
-		
 		if (isset($this->request->get['path'])) {
 			$parts = explode('_', (string)$this->request->get['path']);
 		} else {
@@ -18,42 +14,47 @@ class ControllerModuleFilter extends Controller {
 		$category_info = $this->model_catalog_category->getCategory($category_id);
 		
 		if ($category_info) {
+			$this->language->load('module/filter');
+		
+			$this->data['heading_title'] = $this->language->get('heading_title');
+			
 			$this->load->model('catalog/filter');
 
 			$this->load->model('catalog/product');
 
-			$this->data['filters'] = array();
+			$this->data['filter_groups'] = array();
 
-			$filters = $this->model_catalog_filter->getFilterGroupsByCategoryId($category_id);
+			$filter_groups = $this->model_catalog_filter->getFilterGroupsByCategoryId($category_id);
 
-			foreach ($filters as $filter) {
-				$filter_value_data = array();
+			foreach ($filter_groups as $filter_group) {
+				$filter_data = array();
 
-				$filter_values = $this->model_catalog_filter->getFilters($filter['filter_id']);
+				$filters = $this->model_catalog_filter->getFilters($filter_group['filter_id']);
 
-				foreach ($filter_values as $filter_value) {
+				foreach ($filters as $filter) {
 					$data = array(
-						'filter_category_id'     => $category_id,
-						'filter_sub_filter'      => true,
-						'filter_filter_value_id' => $filter_value['filter_value_id']
+						'filter_category_id' => $category_id,
+						'filter_sub_filter'  => true,
+						'filter_filter_id'   => $filter_value['filter_id']
 					);
 
 					$product_total = $this->model_catalog_product->getTotalProducts($data);
 
 					$total += $product_total;
 
-					$filter_value_data[] = array(
+					$filter_data[] = array(
 						'filter_id' => $child['filter_id'],
-						'name'        => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $product_total . ')' : ''),
-						'href'        => $this->url->link('product/filter', 'path=' . $filter['filter_id'] . '_' . $child['filter_id'])	
+						'name'      => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $product_total . ')' : ''),
+						'href'      => $this->url->link('product/filter', 'path=' . $filter['filter_id'] . '_' . $child['filter_id'])	
 					);		
 				}
-
-				$this->data['filters'][] = array(
-					'filter_id'     => $filter['filter_id'],
-					'name'          => $filter['name'],
-					'cfilter_value' => $filter_value_data
-				);	
+				
+				if ($filter_data) {
+					$this->data['filter_groups'][] = array(
+						'name'   => $filter_group['name'],
+						'filter' => $filter_data
+					);
+				}
 			}
 			
 			if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/filter.tpl')) {

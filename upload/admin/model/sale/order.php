55551381f7fa3e93ca1d2dbc8b14ae2ee9c5392d
Haydar Kulekci <haydar.kulekci@reformo.net>
2012-12-25 17:12:10 +0200

    order table error fixed when DB_PREFIX is null

--- upload/admin/model/sale/order.php
+++ upload/admin/model/sale/order.php
@@ -759,7 +759,7 @@ class ModelSaleOrder extends Model {
 			$implode[] = "op.product_id = '" . $product_id . "'";
 		}
 				
-		$query = $this->db->query("SELECT DISTINCT email FROM " . DB_PREFIX . "order o LEFT JOIN " . DB_PREFIX . "order_product op ON (o.order_id = op.order_id) WHERE (" . implode(" OR ", $implode) . ") AND o.order_status_id <> '0' LIMIT " . $start . "," . $end);	
+		$query = $this->db->query("SELECT DISTINCT email FROM `" . DB_PREFIX . "order` o LEFT JOIN " . DB_PREFIX . "order_product op ON (o.order_id = op.order_id) WHERE (" . implode(" OR ", $implode) . ") AND o.order_status_id <> '0' LIMIT " . $start . "," . $end);	
 		
 		return $query->row['total'];
 	}	

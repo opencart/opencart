9d7834996f118f69d0cd07911ccf800dae18bab8
Daniel <webmaster@opencart.com>
2013-01-04 00:53:23 +0800

    klarna sql fix

--- upload/install/opencart.sql
+++ upload/install/opencart.sql
@@ -2192,12 +2192,12 @@ CREATE TABLE `oc_order_total` (
 -- Dumping data for table `oc_order_total`
 --
 
-DROP TABLE IF EXISTS `oc_order_total_klarna`
+DROP TABLE IF EXISTS `oc_order_total_klarna`;
 CREATE TABLE `oc_order_total_klarna` (
-    `order_total_id` INT,
-    `tax` decimal(10, 4) NOT NULL,
+    `order_total_id` INT(11) NOT NULL,
+    `tax` decimal(10,4) NOT NULL,
     PRIMARY KEY (`order_total_id`)
-);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 

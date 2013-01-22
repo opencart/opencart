9e22b2787b4807b3257ad485f37221a4d5190cec
Daniel Kerr <webmaster@opencart.com>
2013-01-23 00:50:40 +0800

    Update upload/system/library/session.php
    session class revert to avoid session that might have already started

--- upload/system/library/session.php
+++ upload/system/library/session.php
@@ -2,20 +2,16 @@
 class Session {
 	public $data = array();
 			
-  	public function __construct($session_id = '') {		
-		ini_set('session.use_cookies', 'On');
-		ini_set('session.use_trans_sid', 'Off');
-		
-		if ($session_id) {
-			session_id($session_id);
-		}
-		
-		if (preg_match('/^[0-9a-z]*$/i', session_id())) {
+  	public function __construct() {		
+		if (!session_id()) {
+			ini_set('session.use_cookies', 'On');
+			ini_set('session.use_trans_sid', 'Off');
+			
 			session_set_cookie_params(0, '/');
 			session_start();
-			
-			$this->data =& $_SESSION;
 		}
+			
+		$this->data =& $_SESSION;
 	}
 	
 	function getId() {

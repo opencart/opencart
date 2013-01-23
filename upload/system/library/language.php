a5bbaaeb199415766c485b2e5aa9469b91a0e911
Daniel Kerr <webmaster@opencart.com>
2013-01-23 19:20:35 +0800

    updated the upgrade script and language class

--- upload/system/library/language.php
+++ upload/system/library/language.php
@@ -23,18 +23,6 @@ class Language {
 			$this->data = array_merge($this->data, $_);
 			
 			return $this->data;
-		}
-		
-		$file = DIR_LANGUAGE . $this->default . '/' . $filename . '.php';
-		
-		if (file_exists($file)) {
-			$_ = array();
-	  		
-			require($file);
-		
-			$this->data = array_merge($this->data, $_);
-			
-			return $this->data;
 		} else {
 			trigger_error('Error: Could not load language ' . $filename . '!');
 			exit();

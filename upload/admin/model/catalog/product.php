2310877e2c9cae87a60cbd40d2f122d3875f743e
shadyyx <krajcar.peter@gmail.com>
2013-01-28 13:20:35 +0100

    Added somehow missing code from previous versions.

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -353,6 +353,26 @@ class ModelCatalogProduct extends Model {
 			$sql .= " AND p.status = '" . (int)$data['filter_status'] . "'";
 		}
 		
+		if (!empty($data['filter_category_id'])) {
+			if (!empty($data['filter_sub_category'])) {
+				$implode_data = array();
+				
+				$implode_data[] = "category_id = '" . (int)$data['filter_category_id'] . "'";
+				
+				$this->load->model('catalog/category');
+				
+				$categories = $this->model_catalog_category->getCategories($data['filter_category_id']);
+				
+				foreach ($categories as $category) {
+					$implode_data[] = "p2c.category_id = '" . (int)$category['category_id'] . "'";
+				}
+				
+				$sql .= " AND (" . implode(' OR ', $implode_data) . ")";			
+			} else {
+				$sql .= " AND p2c.category_id = '" . (int)$data['filter_category_id'] . "'";
+			}
+		}
+		
 		$sql .= " GROUP BY p.product_id";
 					
 		$sort_data = array(

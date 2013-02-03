44cd7302736ec625b5707906490f991245795d86
Daniel Kerr <webmaster@opencart.com>
2013-02-03 23:01:45 +0800

    [opencart] Padigantion don't work correktly (#419) fix

--- upload/catalog/controller/product/special.php
+++ upload/catalog/controller/product/special.php
@@ -226,11 +226,11 @@ class ControllerProductSpecial extends Controller {
 		
 		sort($limits);
 
-		foreach($limits as $limit){
+		foreach($limits as $limits){
 			$this->data['limits'][] = array(
-				'text'  => $limit,
-				'value' => $limit,
-				'href'  => $this->url->link('product/special', $url . '&limit=' . $limit)
+				'text'  => $limits,
+				'value' => $limits,
+				'href'  => $this->url->link('product/special', $url . '&limit=' . $limits)
 			);
 		}
 			

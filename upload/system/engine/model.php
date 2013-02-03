85238e83392a76e12c0814dd5d1bdb49c6714691
Nikita <ushnik@yandex.ru>
2013-02-04 03:17:46 +0400

    Added some phpDoc comments to engine files wich describes objects that registry contains. Very useful for autocomplete feacher of IDE's like Netbeans and Eclipse PDT in child controllers, models etc.

--- upload/system/engine/model.php
+++ upload/system/engine/model.php
@@ -1,5 +1,26 @@
 <?php
+/**
+ * @property Loader $load Loads whatever you want
+ * @property Config $config Access to congiguration
+ * @property Url $url Helpes to generate links
+ * @property Log $log Log helper
+ * @property Request $request Access to request data (POST, GET, COOKIE, FILES, SERVER)
+ * @property Response $response Some functionality to work with response
+ * @property Cache $cache Access to cache
+ * @property Session $session Access to Session
+ * @property Language $language Language helper
+ * @property Document $document Helper to work with html DOM document
+ * @property Customer $customer Access to customers data
+ * @property Affiliate $affiliate 
+ * @property Currency $currency 
+ * @property Tax $tax Tax helper
+ * @property Weight $weight Weight helper
+ * @property Length $length Length helper
+ * @property Cart $cart Access to cart data
+ * @property Encryption $encryption Encription helper
+ */
 abstract class Model {
+    /** @var Registry */
 	protected $registry;
 
 	public function __construct($registry) {

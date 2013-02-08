ff35cd4678ecde7c93b69c778986769ce62105e7
Haydar Kulekci <haydar.kulekci@reformo.net>
2013-02-08 22:13:55 +0200

    fix for #433, because mysql "order by" default is ASC.

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -306,7 +306,7 @@ class ModelCatalogProduct extends Model {
 	public function getPopularProducts($limit) {
 		$product_data = array();
 		
-		$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY p.viewed, p.date_added DESC LIMIT " . (int)$limit);
+		$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY p.viewed DESC, p.date_added DESC LIMIT " . (int)$limit);
 		
 		foreach ($query->rows as $result) { 		
 			$product_data[$result['product_id']] = $this->getProduct($result['product_id']);

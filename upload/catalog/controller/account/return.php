b8a65ef459d7bd9bf0109620810ea1c92386c2fc
Daniel Kerr <webmaster@opencart.com>
2013-02-11 19:44:48 +0800

    fix for
    https://github.com/opencart/opencart/issues/448

--- upload/catalog/controller/account/return.php
+++ upload/catalog/controller/account/return.php
@@ -12,9 +12,7 @@ class ControllerAccountReturn extends Controller {
     	$this->language->load('account/return');
 
     	$this->document->setTitle($this->language->get('heading_title'));
-		$this->document->addScript('catalog/view/javascript/jquery/colorbox/jquery.colorbox-min.js');
-		$this->document->addStyle('catalog/view/javascript/jquery/colorbox/colorbox.css');
-						
+								
       	$this->data['breadcrumbs'] = array();
 
       	$this->data['breadcrumbs'][] = array(
@@ -308,6 +306,9 @@ class ControllerAccountReturn extends Controller {
     	} 
 							
 		$this->document->setTitle($this->language->get('heading_title'));
+		
+		$this->document->addScript('catalog/view/javascript/jquery/colorbox/jquery.colorbox-min.js');
+		$this->document->addStyle('catalog/view/javascript/jquery/colorbox/colorbox.css');
 		
       	$this->data['breadcrumbs'] = array();
 

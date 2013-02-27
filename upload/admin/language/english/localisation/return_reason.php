f9dc740debbb82c6cf85531ead4e52699e93c590
devtsit <development@ts-it-service.de>
2013-02-27 17:34:08 +0100

    Update upload/admin/language/english/localisation/return_reason.php

--- upload/admin/language/english/localisation/return_reason.php
+++ upload/admin/language/english/localisation/return_reason.php
@@ -14,6 +14,6 @@ $_['entry_name']       = 'Return Reason Name:';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify return reasons!';
-$_['error_name']       = 'Return Reason Name must be between 3 and 32 characters!';
+$_['error_name']       = 'Return Reason Name must be between 3 and 128 characters!';
 $_['error_return']     = 'Warning: This return reason cannot be deleted as it is currently assigned to %s returned products!';
 ?>

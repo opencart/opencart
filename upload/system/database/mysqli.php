c22130ecae06d1f9ee2cb04271a757577f55da44
Haydar Kulekci <haydar.kulekci@reformo.net>
2013-06-18 10:29:02 +0300

    mysqli is_resource editing and adding some object to handle older actions

--- upload/system/database/mysqli.php
+++ upload/system/database/mysqli.php
@@ -1,47 +1,63 @@
 <?php
 final class MySQLi {
 	private $mysqli;
+	private $statement = null;
 
 	public function __construct($hostname, $username, $password, $database) {
+
 		$this->mysqli = new mysqli($hostname, $username, $password, $database);
 
 		if ($this->mysqli->connect_error) {
 			trigger_error('Error: Could not make a database link (' . $this->mysqli->connect_errno . ') ' . $this->mysqli->connect_error);
 		}
-		
+
 		$this->mysqli->set_charset("utf-8");
+
 	}
 
 	public function query($sql) {
-		$result = $this->mysqli->query($sql);
+
+		$query = $this->mysqli->query($sql);
 
 		if ($this->mysqli->errno) {
 			trigger_error('Error: ' . $this->mysqli->error . '<br />Error No: ' . $this->mysqli->errno . '<br />' . $sql);
 			exit();
 		}
 
-		if (is_resource($result)) {
-			$data = array();
+		if ($query){
 
-			while ($row = $result->fetch_object()) {
-				$data[] = $row;
+			if (isset($query->num_rows)){
+				$result = new stdClass();
+				$data = array();
+
+				while ($row = $query->fetch_array()) {
+					$data[] = $row;
+				}
+
+				$result->num_rows = $query->num_rows;
+				$query->close();
+				$result->row = isset($data[0]) ? $data[0] : array();
+				$result->rows = $data;
+				unset($data);
+
+				return $result;
+
+			}else{
+
+				$result = new stdClass(); // to handle older opencart actions
+				$result->row = array(); // to handle older opencart actions
+				$result->rows = array(); // to handle older opencart actions
+				$result->num_rows = 0; // to handle older opencart actions
+				return $result; // should be false
+
 			}
 
-			$result->close();
-			
-			$query = new stdClass();
-			$query->row = isset($data[0]) ? $data[0] : array();
-			$query->rows = $data;
-			$query->num_rows = $result->num_rows;
-
-			unset($data);
-
-			return $query;	
-		} else {
-			return true;
+		}else {
+			trigger_error('Error: ' . $this->mysqli->error . '<br />Error No: ' . $this->mysqli->errno . '<br />' . $sql);
+			exit();
 		}
 	}
-	
+
 	public function escape($value) {
 		return $this->mysqli->real_escape_string($value);
 	}

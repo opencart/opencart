e0e3ad817703c989b77cfc00640805fe0f29d5bc
Ekrem KAYA <webmaster@e-piksel.com>
2013-06-21 19:49:54 +0300

    Updated ckeditor v4.1.2

--- upload/admin/view/javascript/ckeditor/plugins/codemirror/lang/de.js
+++ upload/admin/view/javascript/ckeditor/plugins/codemirror/lang/de.js
@@ -3,5 +3,10 @@ Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'codemirror', 'de', {
-	toolbar: 'Quellcode'
+    toolbar: 'Quellcode',
+    searchCode: 'Quellcode durchsuchen',
+	autoFormat: 'Auswahl formatieren',
+	commentSelectedRange: 'Auswahl auskommentieren',
+	uncommentSelectedRange: 'Auskommentierung entferen',
+	autoCompleteToggle: 'HTML Tag Autovervollständigen de-/aktivieren'
 });
--- upload/admin/view/javascript/ckeditor/plugins/codemirror/lang/nb.js
+++ upload/admin/view/javascript/ckeditor/plugins/codemirror/lang/nb.js
@@ -3,5 +3,8 @@ Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'codemirror', 'nb', {
-	toolbar: 'Kilde'
+	toolbar: 'Kilde',
+	autoFormat: 'Format Selection',
+	commentSelectedRange: 'Comment Selection',
+	uncommentSelectedRange: 'Uncomment Selection'
 });

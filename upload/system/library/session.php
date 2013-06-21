794d17b1467d40cba0c50e3ac0ccf40a01ce18b6
opencart <webmaster@opencart.com>
2013-06-21 21:02:13 +0800

    removed the session validation completely

--- upload/system/library/session.php
+++ upload/system/library/session.php
@@ -7,10 +7,8 @@ class Session {
 			ini_set('session.use_cookies', 'On');
 			ini_set('session.use_trans_sid', 'Off');
 
-			if (!isset($_COOKIE[session_name()]) || preg_match('/^[a-z0-9]{32}$/', $_COOKIE[session_name()])) {
-				session_set_cookie_params(0, '/');
-				session_start();
-			}
+			session_set_cookie_params(0, '/');
+			session_start();
 		}
 
 		$this->data =& $_SESSION;

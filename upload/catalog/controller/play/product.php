ae676cb8a2e60f3ad98567996a4a7e623494b3af
James Allsup <james@allsup.eu>
2013-10-29 13:25:27 +0000

    Modified all of the lib and front end to use tabs not space indent.

--- upload/catalog/controller/play/product.php
+++ upload/catalog/controller/play/product.php
@@ -1,144 +1,144 @@
 <?php
 class ControllerPlayProduct extends Controller{
-    public function getProductChanges() {
-        $this->openbay->play->log('getProductChanges()');
+	public function getProductChanges() {
+		$this->openbay->play->log('getProductChanges()');
 
-        if(!empty($this->request->post)) {
-            $auth = $this->validateAuth();
-            if($auth == true) {
-                
-                $this->load->model('play/product');
-                
-                //get the formatted array of products
-                $products = $this->openbay->play->getLinkedProducts(true);
-                $response = array(
-                    'error' => false,
-                    'msg'   => ''
-                );
+		if(!empty($this->request->post)) {
+			$auth = $this->validateAuth();
+			if($auth == true) {
 
-                if($products != 0) {
-                    foreach($products as $product) {
-                        if($product['product_id'] != 0) {
-                            $response['products'][] = array(
-                                'product-id'            => html_entity_decode($product['play_product_id']),
-                                'product-id-type'       => (int)$product['play_product_id_type'],
-                                'sku'                   => (int)$product['product_id'],
-                                'dispatch-to'           => (int)$product['dispatch_to'],
-                                'delivered-price-gbp'   => (double)$product['price_gb'],
-                                'delivered-price-euro'  => (double)$product['price_eu'],
-                                'quantity'              => (int)$product['quantity'],
-                                'item-condition'        => (int)$product['condition'],
-                                'comment'               => html_entity_decode($product['comment']),
-                                'dispatch-from'         => (int)$product['dispatch_from'],
-                                'add-delete'            => html_entity_decode($product['action']),
-                            );
+				$this->load->model('play/product');
 
-                            $this->db->query("UPDATE `" . DB_PREFIX . "play_product_insert` SET `status` = '2', `submitted` = now() WHERE `product_id` = '".(int)$product['product_id']."'");
-                        }
-                    }
-                }
-                
-                $remove = $this->openbay->play->getRemovedProducts();
+				//get the formatted array of products
+				$products = $this->openbay->play->getLinkedProducts(true);
+				$response = array(
+					'error' => false,
+					'msg'   => ''
+				);
 
-                if($remove != 0) {
-                    foreach($remove as $product) {
-                        if($product['product_id'] != 0) {
-                            $response['products'][] = array(
-                                'product-id'            => html_entity_decode($product['play_product_id']),
-                                'product-id-type'       => (int)$product['play_product_id_type'],
-                                'sku'                   => (int)$product['product_id'],
-                                'dispatch-to'           => (int)$product['dispatch_to'],
-                                'delivered-price-gbp'   => (double)$product['price_gb'],
-                                'delivered-price-euro'  => (double)$product['price_eu'],
-                                'quantity'              => 0,
-                                'item-condition'        => (int)$product['condition'],
-                                'comment'               => html_entity_decode($product['comment']),
-                                'dispatch-from'         => (int)$product['dispatch_from'],
-                                'add-delete'            => html_entity_decode($product['action']),
-                            );
+				if($products != 0) {
+					foreach($products as $product) {
+						if($product['product_id'] != 0) {
+							$response['products'][] = array(
+								'product-id'            => html_entity_decode($product['play_product_id']),
+								'product-id-type'       => (int)$product['play_product_id_type'],
+								'sku'                   => (int)$product['product_id'],
+								'dispatch-to'           => (int)$product['dispatch_to'],
+								'delivered-price-gbp'   => (double)$product['price_gb'],
+								'delivered-price-euro'  => (double)$product['price_eu'],
+								'quantity'              => (int)$product['quantity'],
+								'item-condition'        => (int)$product['condition'],
+								'comment'               => html_entity_decode($product['comment']),
+								'dispatch-from'         => (int)$product['dispatch_from'],
+								'add-delete'            => html_entity_decode($product['action']),
+							);
 
-                            $this->openbay->play->delete($product['product_id']);
-                        }
-                    }
-                }
+							$this->db->query("UPDATE `" . DB_PREFIX . "play_product_insert` SET `status` = '2', `submitted` = now() WHERE `product_id` = '".(int)$product['product_id']."'");
+						}
+					}
+				}
 
-                $this->openbay->play->log('getProductChanges() - '.json_encode($response));
-            }else{
-                $response = array(
-                    'error' => true,
-                    'msg'   => 'Invalid details'
-                );
-            }
+				$remove = $this->openbay->play->getRemovedProducts();
 
-        }else{
-            $response = array(
-                'error' => true,
-                'msg'   => 'Data error'
-            );
-        }
+				if($remove != 0) {
+					foreach($remove as $product) {
+						if($product['product_id'] != 0) {
+							$response['products'][] = array(
+								'product-id'            => html_entity_decode($product['play_product_id']),
+								'product-id-type'       => (int)$product['play_product_id_type'],
+								'sku'                   => (int)$product['product_id'],
+								'dispatch-to'           => (int)$product['dispatch_to'],
+								'delivered-price-gbp'   => (double)$product['price_gb'],
+								'delivered-price-euro'  => (double)$product['price_eu'],
+								'quantity'              => 0,
+								'item-condition'        => (int)$product['condition'],
+								'comment'               => html_entity_decode($product['comment']),
+								'dispatch-from'         => (int)$product['dispatch_from'],
+								'add-delete'            => html_entity_decode($product['action']),
+							);
 
-        $this->response->addHeader('Content-type: application/json');
-        $this->response->setOutput(json_encode($response));
-    }
+							$this->openbay->play->delete($product['product_id']);
+						}
+					}
+				}
 
-    public function updateProductChanges() {
-        $this->openbay->play->log('updateProductChanges()');
-        if(!empty($this->request->post)) {
+				$this->openbay->play->log('getProductChanges() - '.json_encode($response));
+			}else{
+				$response = array(
+					'error' => true,
+					'msg'   => 'Invalid details'
+				);
+			}
 
-            if($this->validateAuth() == true) {
-                
-                $this->load->model('play/product');
-                
-                $data = $this->openbay->play->decrypt($this->request->post['data'], true);
+		}else{
+			$response = array(
+				'error' => true,
+				'msg'   => 'Data error'
+			);
+		}
 
-                $pWarnings = array();
-                
-                if(isset($data['warnings']) && is_array($data['warnings'])) {
-                    foreach($data['warnings'] as $warning) {
-                        $this->model_play_product->removeAllWarnings($warning['product_id']);
-                        $this->model_play_product->updateProduct($warning['product_id'], 6);
-                        $this->model_play_product->addWarning($warning['msg'], $warning['product_id']);
-                        $pWarnings[] = $warning['product_id'];
-                    }
-                }
+		$this->response->addHeader('Content-type: application/json');
+		$this->response->setOutput(json_encode($response));
+	}
 
-                if(isset($data['errors']) && is_array($data['errors'])) {
-                    foreach($data['errors'] as $error) {
-                        $this->model_play_product->removeAllWarnings($error['product_id']);
-                        $this->model_play_product->updateProduct($error['product_id'], 4);
-                        $this->model_play_product->addWarning($error['msg'], $error['product_id']);
-                        $pWarnings[] = $warning['product_id'];
-                    }
-                }
+	public function updateProductChanges() {
+		$this->openbay->play->log('updateProductChanges()');
+		if(!empty($this->request->post)) {
 
-                if(isset($data['active']['products']) && is_array($data['active']['products'])) {
-                    foreach($data['active']['products'] as $active) {
-                        if(!in_array($active['sku'], $pWarnings)) {
-                            $this->model_play_product->removeAllWarnings($active['sku']);
-                            $this->model_play_product->updateProduct($active['sku'], 3);
-                        }
-                    }
-                }
-            }
+			if($this->validateAuth() == true) {
 
-            $this->response->addHeader('Content-type: application/json');
-            $this->response->setOutput(json_encode(array('ok')));
-        }else{
-            $this->response->addHeader('Content-type: application/json');
-            $this->response->setOutput(json_encode(array('fail')));
-        }
-    }
+				$this->load->model('play/product');
 
-    private function validateAuth() {
-        $this->openbay->play->log('validateAuth()');
+				$data = $this->openbay->play->decrypt($this->request->post['data'], true);
 
-        if($this->request->post['token'] == $this->config->get('obp_play_token') &&
-           $this->request->post['secret'] == $this->config->get('obp_play_secret'))
-        {
-            return true;
-        }else{
-            return false;
-        }
-    }
+				$pWarnings = array();
+
+				if(isset($data['warnings']) && is_array($data['warnings'])) {
+					foreach($data['warnings'] as $warning) {
+						$this->model_play_product->removeAllWarnings($warning['product_id']);
+						$this->model_play_product->updateProduct($warning['product_id'], 6);
+						$this->model_play_product->addWarning($warning['msg'], $warning['product_id']);
+						$pWarnings[] = $warning['product_id'];
+					}
+				}
+
+				if(isset($data['errors']) && is_array($data['errors'])) {
+					foreach($data['errors'] as $error) {
+						$this->model_play_product->removeAllWarnings($error['product_id']);
+						$this->model_play_product->updateProduct($error['product_id'], 4);
+						$this->model_play_product->addWarning($error['msg'], $error['product_id']);
+						$pWarnings[] = $warning['product_id'];
+					}
+				}
+
+				if(isset($data['active']['products']) && is_array($data['active']['products'])) {
+					foreach($data['active']['products'] as $active) {
+						if(!in_array($active['sku'], $pWarnings)) {
+							$this->model_play_product->removeAllWarnings($active['sku']);
+							$this->model_play_product->updateProduct($active['sku'], 3);
+						}
+					}
+				}
+			}
+
+			$this->response->addHeader('Content-type: application/json');
+			$this->response->setOutput(json_encode(array('ok')));
+		}else{
+			$this->response->addHeader('Content-type: application/json');
+			$this->response->setOutput(json_encode(array('fail')));
+		}
+	}
+
+	private function validateAuth() {
+		$this->openbay->play->log('validateAuth()');
+
+		if($this->request->post['token'] == $this->config->get('obp_play_token') &&
+		   $this->request->post['secret'] == $this->config->get('obp_play_secret'))
+		{
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
 ?>

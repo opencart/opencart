b746ca456612dfe6e0d7d98d08193ec467bc2353
Daniel Kerr <webmaster@opencart.com>
2013-11-16 02:48:29 +0800

    updated

--- upload/catalog/controller/payment/worldpay.php
+++ upload/catalog/controller/payment/worldpay.php
@@ -1,47 +1,52 @@
 <?php
 class ControllerPaymentWorldPay extends Controller {
 	protected function index() {
-    	$data['button_confirm'] = $this->language->get('button_confirm');
+		$data['button_confirm'] = $this->language->get('button_confirm');
 
 		$this->load->model('checkout/order');
-		
+
 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
-		
-		if (!$this->config->get('worldpay_test')){
+
+		if (!$this->config->get('worldpay_test')) {
 			$data['action'] = 'https://secure.worldpay.com/wcc/purchase';
 		}else{
 			$data['action'] = 'https://secure-test.worldpay.com/wcc/purchase';
 		}
-	  
+
 		$data['merchant'] = $this->config->get('worldpay_merchant');
 		$data['order_id'] = $order_info['order_id'];
 		$data['amount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);
 		$data['currency'] = $order_info['currency_code'];
 		$data['description'] = $this->config->get('config_name') . ' - #' . $order_info['order_id'];
 		$data['name'] = $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'];
-		
+
 		if (!$order_info['payment_address_2']) {
 			$data['address'] = $order_info['payment_address_1'] . ', ' . $order_info['payment_city'] . ', ' . $order_info['payment_zone'];
 		} else {
 			$data['address'] = $order_info['payment_address_1'] . ', ' . $order_info['payment_address_2'] . ', ' . $order_info['payment_city'] . ', ' . $order_info['payment_zone'];
 		}
-		
+
 		$data['postcode'] = $order_info['payment_postcode'];
 		$data['country'] = $order_info['payment_iso_code_2'];
 		$data['telephone'] = $order_info['telephone'];
 		$data['email'] = $order_info['email'];
 		$data['test'] = $this->config->get('worldpay_test');
-		
+
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/worldpay.tpl')) {
 			return $this->load->view($this->config->get('config_template') . '/template/payment/worldpay.tpl', $data);
 		} else {
 			return $this->load->view('default/template/payment/worldpay.tpl', $data);
 		}
 	}
-	
+
 	public function callback() {
+<<<<<<< HEAD
 		$this->load->language('payment/worldpay');
 	
+=======
+		$this->language->load('payment/worldpay');
+
+>>>>>>> 611eb64a3c370e67b173f0cda42ca1689f2b0d04
 		$data['title'] = sprintf($this->language->get('heading_title'), $this->config->get('config_name'));
 
 		if (!isset($this->request->server['HTTPS']) || ($this->request->server['HTTPS'] != 'on')) {
@@ -49,18 +54,18 @@ class ControllerPaymentWorldPay extends Controller {
 		} else {
 			$data['base'] = $this->config->get('config_ssl');
 		}
-	
+
 		$data['language'] = $this->language->get('code');
 		$data['direction'] = $this->language->get('direction');
-	
+
 		$data['heading_title'] = sprintf($this->language->get('heading_title'), $this->config->get('config_name'));
-		
+
 		$data['text_response'] = $this->language->get('text_response');
 		$data['text_success'] = $this->language->get('text_success');
 		$data['text_success_wait'] = sprintf($this->language->get('text_success_wait'), $this->url->link('checkout/success'));
 		$data['text_failure'] = $this->language->get('text_failure');
 		$data['text_failure_wait'] = sprintf($this->language->get('text_failure_wait'), $this->url->link('checkout/checkout', '', 'SSL'));
-	
+
 		if (isset($this->request->post['transStatus']) && $this->request->post['transStatus'] == 'Y') { 
 			$this->load->model('checkout/order');
 
@@ -70,21 +75,21 @@ class ControllerPaymentWorldPay extends Controller {
 			} else {
 				$this->model_checkout_order->confirm($this->request->post['cartId'], $this->config->get('config_order_status_id'), $this->language->get('text_pw_mismatch'));
 			}
-	
+
 			$message = '';
 
 			if (isset($this->request->post['transId'])) {
 				$message .= 'transId: ' . $this->request->post['transId'] . "\n";
 			}
-		
+
 			if (isset($this->request->post['transStatus'])) {
 				$message .= 'transStatus: ' . $this->request->post['transStatus'] . "\n";
 			}
-		
+
 			if (isset($this->request->post['countryMatch'])) {
 				$message .= 'countryMatch: ' . $this->request->post['countryMatch'] . "\n";
 			}
-		
+
 			if (isset($this->request->post['AVS'])) {
 				$message .= 'AVS: ' . $this->request->post['AVS'] . "\n";
 			}	
@@ -100,15 +105,15 @@ class ControllerPaymentWorldPay extends Controller {
 			if (isset($this->request->post['rawAuthMessage'])) {
 				$message .= 'rawAuthMessage: ' . $this->request->post['rawAuthMessage'] . "\n";
 			}	
-		
+
 			if (isset($this->request->post['wafMerchMessage'])) {
 				$message .= 'wafMerchMessage: ' . $this->request->post['wafMerchMessage'] . "\n";
 			}				
 
 			$this->model_checkout_order->update($this->request->post['cartId'], $this->config->get('worldpay_order_status_id'), $message, false);
-	
+
 			$data['continue'] = $this->url->link('checkout/success');
-			
+
 			if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/worldpay_success.tpl')) {
 				$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/payment/worldpay_success.tpl', $data));
 			} else {
@@ -116,7 +121,7 @@ class ControllerPaymentWorldPay extends Controller {
 			}				
 		} else {
 			$data['continue'] = $this->url->link('checkout/cart');
-				
+
 			if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/worldpay_failure.tpl')) {
 				$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/payment/worldpay_failure.tpl', $data));
 			} else {

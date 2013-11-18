74469b27c13d5328d8c6661ed13ab81568ab08ea
Daniel Kerr <webmaster@opencart.com>
2013-11-18 15:25:33 +0800

    fix for
    https://github.com/opencart/opencart/issues/966

--- upload/catalog/model/checkout/coupon.php
+++ upload/catalog/model/checkout/coupon.php
@@ -6,7 +6,7 @@ class ModelCheckoutCoupon extends Model {
 		$coupon_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "coupon` WHERE code = '" . $this->db->escape($code) . "' AND ((date_start = '0000-00-00' OR date_start < NOW()) AND (date_end = '0000-00-00' OR date_end > NOW())) AND status = '1'");
 			
 		if ($coupon_query->num_rows) {
-			if ($coupon_query->row['total'] >= $this->cart->getSubTotal()) {
+			if ($coupon_query->row['total'] > $this->cart->getSubTotal()) {
 				$status = false;
 			}
 		

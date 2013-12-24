93975bd25410e4a1915838ed13543586ae1c2d79
James Allsup <admin@ecommercehq.co.uk>
2013-12-24 18:09:28 +0000

    Re-added the play library file which is empty to allow for upgrades.non-beta

---
+++ upload/system/library/play.php
@@ -0,0 +1,12 @@
+<?php
+final class Play {
+	/**
+	* The play module has been removed.
+	* This file remains to stop loading errors on old installs
+	**/
+
+	public function __construct($registry) {
+
+	}
+}
+?>

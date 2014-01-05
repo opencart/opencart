2b4e90e43b3880cf459487d414895dc1b821408f
frymi <rob.fm@centrum.cz>
2014-01-05 23:13:57 +0100

    Create unit_class.php

---
+++ upload/admin/language/english/localisation/unit_class.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']    = 'Unit Quantity';
+
+// Text
+$_['text_success']     = 'Úspěch: Změnili jste množstevní jednotky!';
+
+// Column
+$_['column_title']     = 'Unit name';
+$_['column_unit']      = 'Označení jednotky';
+$_['column_value']     = 'Hodnota';
+$_['column_action']    = 'Action';
+
+// Entry
+$_['entry_title']      = 'Unit name:';
+$_['entry_unit']       = 'Označení jednotky:<br /><span class="help">CZ lokalizace: 1x|2-4|5+</span>';
+$_['entry_value']      = 'Value:<br /><span class="help">Nastavte na 1.00000 pokud se jedná o výchozí jednotku množství.</span>';
+
+// Error
+$_['error_permission'] = 'Varování: Nemáte oprávnění na změnu jednotek množství!';
+$_['error_title']      = 'Název jednotky musí být delší než 3 znaky a kratší než 32 znaků!';
+$_['error_unit']       = 'Označení jednotky musí být delší než 1 znak a kratší než 4 znaky!';
+$_['error_default']    = 'Varování: Tato jednotka množství nemůže být smazána je nastavena jako výchozí jednotka obchodu!';
+$_['error_product']    = 'Varování: Tato jednotka množství nemůže být smazána je aktuálně přiřazena k %s produktům!';
+?>

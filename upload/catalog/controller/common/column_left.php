3b2a2f696607524c50acc0f0a352c5e8cd643d0f
OpenCartAddons <webmaster@opencartaddons.com>
2014-01-28 15:36:36 -0500

    Module Multiple Layout Selection

--- upload/catalog/controller/common/column_left.php
+++ upload/catalog/controller/common/column_left.php
@@ -47,7 +47,7 @@ class ControllerCommonColumnLeft extends Controller {
 		
 			if ($modules) {
 				foreach ($modules as $module) {
-					if ($module['layout_id'] == $layout_id && $module['position'] == 'column_left' && $module['status']) {
+					if (((isset($module['layout_id']) && $module['layout_id'] == $layout_id) || (isset($module['layouts']) && in_array($layout_id, $module['layouts']))) && $module['position'] == 'column_left' && $module['status']) {
 						$module_data[] = array(
 							'code'       => $extension['code'],
 							'setting'    => $module,

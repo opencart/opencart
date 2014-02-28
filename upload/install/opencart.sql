6fed9f34b12afaf35c663c4ea7a18e61ee55a467
James Allsup <james@allsup.eu>
2014-02-28 16:41:17 +0000

    Added reference column for recurring payment transaction.

--- upload/install/opencart.sql
+++ upload/install/opencart.sql
@@ -88,6 +88,7 @@ CREATE TABLE `oc_order_recurring_transaction` (
   `created` datetime NOT NULL,
   `amount` decimal(10,4) NOT NULL,
   `type` varchar(255) NOT NULL,
+  `reference` varchar(255) NOT NULL,
   PRIMARY KEY (`order_recurring_transaction_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 

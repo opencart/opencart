6a1b6a33e93b2cb8afedd12327c54472a1a1b98d
James Allsup <jamesa@welfordmedia.co.uk>
2014-03-10 18:39:38 +0000

    Improved code for defining modification constant on installer.

--- upload/install/index.php
+++ upload/install/index.php
@@ -9,9 +9,9 @@ define('HTTP_OPENCART', 'http://' . $_SERVER['HTTP_HOST'] . rtrim(rtrim(dirname(
 // DIR
 define('DIR_APPLICATION', str_replace('\'', '/', realpath(dirname(__FILE__))) . '/');
 define('DIR_SYSTEM', str_replace('\'', '/', realpath(dirname(__FILE__) . '/../')) . '/system/');
-define('DIR_MODIFICATION', str_replace('\'', '/', realpath(dirname(__FILE__) . '/../')) . '/system/modification/');
 define('DIR_OPENCART', str_replace('\'', '/', realpath(DIR_APPLICATION . '../')) . '/');
 define('DIR_DATABASE', DIR_SYSTEM . 'database/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'modification/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');

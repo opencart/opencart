ccf0fce31176cb0df0c042b8878c42e4c2f0c4c7
Steven <steven@commentics.org>
2014-03-10 22:51:21 +0000

    Spelling and grammar fixes.

--- upload/admin/language/english/extension/modification.php
+++ upload/admin/language/english/extension/modification.php
@@ -4,7 +4,7 @@ $_['heading_title']     = 'Modifications';
 
 // Text
 $_['text_success']      = 'Success: You have modified modifications!';
-$_['text_refresh']      = 'Whenever you enable / disable or delete a modifcation you need to click the refresh button to rebuild you modification cache!';
+$_['text_refresh']      = 'Whenever you enable / disable or delete a modification you need to click the refresh button to rebuild your modification cache!';
 
 // Column
 $_['column_name']       = 'Modification Name';

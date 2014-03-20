9884e983dd3fe792c9e4bab156d9703c111846c6
Júlio César Gonçalves de Oliveira <julio@pinguineras.com.br>
2014-03-20 14:25:22 -0300

    Update order.php delete() method
    Problem with /admin/controller/sale/order.php with method delete(), using wrong method to delete and wrong request->get

--- upload/admin/controller/sale/order.php
+++ upload/admin/controller/sale/order.php
@@ -33,50 +33,61 @@ class ControllerSaleOrder extends Controller {
   	}
 		
   	public function delete() {
+
+
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateDelete()) {
-			$this->model_sale_order->editOrder($this->request->get['order_id']);
 
-			$this->session->data['success'] = $this->language->get('text_success');
 
-			$url = '';
+			$this->load->model('sale/order');
 
-			if (isset($this->request->get['filter_order_id'])) {
-				$url .= '&filter_order_id=' . $this->request->get['filter_order_id'];
+			if(isset($this->request->post['selected']) && ($this->validateDelete())){
+				foreach ($this->request->post['selected'] as $order_id) {
+					$this->model_sale_order->deleteOrder($order_id);
+				}
+
+				$this->session->data['success'] = $this->language->get('text_success');
+
+				$url = '';
+
+				if (isset($this->request->get['filter_order_id'])) {
+					$url .= '&filter_order_id=' . $this->request->get['filter_order_id'];
+				}
+
+				if (isset($this->request->get['filter_customer'])) {
+					$url .= '&filter_customer=' . urlencode(html_entity_decode($this->request->get['filter_customer'], ENT_QUOTES, 'UTF-8'));
+				}
+
+				if (isset($this->request->get['filter_order_status_id'])) {
+					$url .= '&filter_order_status_id=' . $this->request->get['filter_order_status_id'];
+				}
+
+				if (isset($this->request->get['filter_total'])) {
+					$url .= '&filter_total=' . $this->request->get['filter_total'];
+				}
+
+				if (isset($this->request->get['filter_date_added'])) {
+					$url .= '&filter_date_added=' . $this->request->get['filter_date_added'];
+				}
+
+				if (isset($this->request->get['filter_date_modified'])) {
+					$url .= '&filter_date_modified=' . $this->request->get['filter_date_modified'];
+				}
+
+				if (isset($this->request->get['sort'])) {
+					$url .= '&sort=' . $this->request->get['sort'];
+				}
+
+				if (isset($this->request->get['order'])) {
+					$url .= '&order=' . $this->request->get['order'];
+				}
+
+				if (isset($this->request->get['page'])) {
+					$url .= '&page=' . $this->request->get['page'];
+				}
+
+				$this->response->redirect($this->url->link('sale/order', 'token=' . $this->session->data['token'] . $url, 'SSL'));
+
 			}
-
-			if (isset($this->request->get['filter_customer'])) {
-				$url .= '&filter_customer=' . urlencode(html_entity_decode($this->request->get['filter_customer'], ENT_QUOTES, 'UTF-8'));
-			}
-
-			if (isset($this->request->get['filter_order_status_id'])) {
-				$url .= '&filter_order_status_id=' . $this->request->get['filter_order_status_id'];
-			}
-
-			if (isset($this->request->get['filter_total'])) {
-				$url .= '&filter_total=' . $this->request->get['filter_total'];
-			}
-
-			if (isset($this->request->get['filter_date_added'])) {
-				$url .= '&filter_date_added=' . $this->request->get['filter_date_added'];
-			}
-
-			if (isset($this->request->get['filter_date_modified'])) {
-				$url .= '&filter_date_modified=' . $this->request->get['filter_date_modified'];
-			}
-
-			if (isset($this->request->get['sort'])) {
-				$url .= '&sort=' . $this->request->get['sort'];
-			}
-
-			if (isset($this->request->get['order'])) {
-				$url .= '&order=' . $this->request->get['order'];
-			}
-
-			if (isset($this->request->get['page'])) {
-				$url .= '&page=' . $this->request->get['page'];
-			}
-
-			$this->response->redirect($this->url->link('sale/order', 'token=' . $this->session->data['token'] . $url, 'SSL'));
 		}
 
 		$this->getList();

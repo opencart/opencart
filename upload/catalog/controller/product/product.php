bd9bc806206c3b4f0fc3c93ece523abcca46e519
Daniel Kerr <webmaster@opencart.com>
2014-03-28 01:35:44 +0800

    more upload work done

--- upload/catalog/controller/product/product.php
+++ upload/catalog/controller/product/product.php
@@ -781,11 +781,10 @@ class ControllerProductProduct extends Controller {
 			
 			move_uploaded_file($this->request->files['file']['tmp_name'], DIR_DOWNLOAD . $file);
 			
-			$code = sha1(uniqid(mt_rand(), true));
-		
-
 			// Hide the uploaded file name so people can not link to it directly.
-			$json['file'] = addUpload();$file;
+			$this->load->model('tool/upload');
+			
+			$json['code'] = $this->model_tool_upload->addUpload($filename, $file);
 						
 			$json['success'] = $this->language->get('text_upload');
 		}	

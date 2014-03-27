15cbd4a7cddcba095169bba201ebcfeffa507a70
Bob Brown <bob@turboweb.co.nz>
2014-03-27 16:11:21 +1300

    Wait until the banner message from the mail server is displayed before continuing. In the event of a successful connection to the mail server but no welcome message yet - because the server is slow to respond - the mail library would simply continue with the SMTP conversation resulting in a "554 SMTP synchronization error". This change loops until $line (the response from the mail server) is not false. Ref: http://www.faqs.org/rfcs/rfc2821.html, section 3.1.

--- upload/system/library/mail.php
+++ upload/system/library/mail.php
@@ -193,7 +193,7 @@ class Mail {
 					
 					$reply = '';
 
-					while ($line = fgets($handle, 515)) {
+					while ($line = fgets($handle, 515) and $line === false) {
 						$reply .= $line;
 
 						if (substr($line, 3, 1) == ' ') {

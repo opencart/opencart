f7206fbeaa0772411f9b933283e5c86f5c2965c9
James Allsup <james@allsup.eu>
2014-04-14 11:03:09 +0100

    Fix for image manager issue caused by the new UTF8.php helper update.
    Added check for length.

--- upload/system/helper/utf8.php
+++ upload/system/helper/utf8.php
@@ -15,8 +15,12 @@ if (extension_loaded('mbstring')) {
 	}
 	
 	function utf8_substr($string, $offset, $length = null) {
-		return mb_substr($string, $offset, $length);
-	}	
+		if ($length == null) {
+			return mb_substr($string, $offset, utf8_strlen($string));
+		} else {
+			return mb_substr($string, $offset, $length);
+		}
+	}
 	
 	function utf8_strtoupper($string) {	
 		return mb_strtoupper($string);

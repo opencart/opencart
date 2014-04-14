cdf20be73d813c227a3b12d13e9d867e46b9e5eb
Ekrem KAYA <webmaster@e-piksel.com>
2014-04-14 13:05:17 +0300

    Removed unused strings in category controller and opencart.sql

--- upload/install/opencart.sql
+++ upload/install/opencart.sql
@@ -1416,7 +1416,6 @@ CREATE TABLE `oc_download` (
   `download_id` int(11) NOT NULL AUTO_INCREMENT,
   `filename` varchar(128) NOT NULL,
   `mask` varchar(128) NOT NULL,
-  `remaining` int(11) NOT NULL DEFAULT '0',
   `date_added` datetime NOT NULL,
   PRIMARY KEY (`download_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;

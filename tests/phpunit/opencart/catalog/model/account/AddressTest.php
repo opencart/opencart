ffd4153e650ab6fb290c99a5a1add7cc01b65aec
M <martynas@welfordmedia.co.uk>
2014-05-06 15:24:14 +0100

    Finished the account/address test.

--- tests/phpunit/opencart/catalog/model/account/AddressTest.php
+++ tests/phpunit/opencart/catalog/model/account/AddressTest.php
@@ -63,4 +63,146 @@ class CatalogModelAccountAddressTest extends OpenCartTest {
 		$customer = $this->db->query("SELECT * FROM " . DB_PREFIX . "customer WHERE customer_id = 1")->row;
 		$this->assertEquals($addressId, $customer['address_id'], 'Changed default address unnecessarily');
 	}
+	
+	public function testEditAddress() {
+		$address = array(
+			'firstname' => '',
+			'lastname' => '',
+			'company' => '',
+			'address_1' => '',
+			'address_2' => '',
+			'postcode' => '',
+			'city' => '',
+			'zone_id' => 0,
+			'country_id' => 0,
+			'custom_data' => array(),
+			'default' => true,
+		);
+
+		$addressId = $this->model_account_address->addAddress($address);
+		
+		$address = array(
+			'firstname' => 'firstname',
+			'lastname' => 'lastname',
+			'company' => 'company',
+			'address_1' => 'address_1',
+			'address_2' => 'address_2',
+			'postcode' => 'postcode',
+			'city' => 'city',
+			'zone_id' => 0,
+			'country_id' => 0,
+			'custom_data' => array(),
+			'default' => true,
+		);
+		
+		$this->model_account_address->editAddress($addressId, $address);
+		
+		$result = $this->db->query("SELECT * FROM " . DB_PREFIX . "address")->row;
+		
+		foreach ($address as $key => $value) {
+			$this->assertEquals($value, $address[$key]);
+		}
+		
+		$addressId = $this->model_account_address->addAddress($address);
+		$address['default'] = false;
+		$this->model_account_address->editAddress($addressId, $address);
+		$customer = $this->db->query("SELECT * FROM " . DB_PREFIX . "customer WHERE customer_id = 1")->row;
+		$this->assertEquals($addressId, $customer['address_id'], 'Changed default address unnecessarily');
+	}
+	
+	public function testDeleteAddress() {
+		$address = array(
+			'firstname' => '',
+			'lastname' => '',
+			'company' => '',
+			'address_1' => '',
+			'address_2' => '',
+			'postcode' => '',
+			'city' => '',
+			'zone_id' => 0,
+			'country_id' => 0,
+			'custom_data' => array(),
+			'default' => true,
+		);
+		
+		$addressId = $this->model_account_address->addAddress($address);
+		
+		$result = $this->db->query("SELECT * FROM " . DB_PREFIX . "address")->row;
+		$this->assertNotEmpty($result);
+		
+		$this->model_account_address->deleteAddress($addressId);
+		
+		$result = $this->db->query("SELECT * FROM " . DB_PREFIX . "address")->row;
+		$this->assertEmpty($result);
+	}
+	
+	public function testGetAddress() {
+		$address = array(
+			'firstname' => '',
+			'lastname' => '',
+			'company' => '',
+			'address_1' => '',
+			'address_2' => '',
+			'postcode' => '',
+			'city' => '',
+			'zone_id' => 0,
+			'country_id' => 0,
+			'custom_data' => array(),
+			'default' => true,
+		);
+		
+		$addressId = $this->model_account_address->addAddress($address);
+		
+		$address = $this->model_account_address->getAddress($addressId);
+		$this->assertNotFalse($address);
+		
+		$address = $this->model_account_address->getAddress(0);
+		$this->assertFalse($address);
+	}
+	
+	public function testGetAddresses() {
+		$address = array(
+			'firstname' => '',
+			'lastname' => '',
+			'company' => '',
+			'address_1' => '',
+			'address_2' => '',
+			'postcode' => '',
+			'city' => '',
+			'zone_id' => 0,
+			'country_id' => 0,
+			'custom_data' => array(),
+			'default' => true,
+		);
+		
+		for ($i = 0; $i < 5; $i++) {
+			$this->model_account_address->addAddress($address);
+		}
+		
+		$addresses = $this->model_account_address->getAddresses();
+		$this->assertCount(5, $addresses);
+	}
+	
+	public function testGetTotalAddresses() {
+		$address = array(
+			'firstname' => '',
+			'lastname' => '',
+			'company' => '',
+			'address_1' => '',
+			'address_2' => '',
+			'postcode' => '',
+			'city' => '',
+			'zone_id' => 0,
+			'country_id' => 0,
+			'custom_data' => array(),
+			'default' => true,
+		);
+		
+		for ($i = 0; $i < 5; $i++) {
+			$this->model_account_address->addAddress($address);
+		}
+		
+		$addressCount = $this->model_account_address->getTotalAddresses();
+		$this->assertEquals(5, $addressCount);
+	}
 }

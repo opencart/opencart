b51ec2bab495e381a226ca1a054cc8219410c5a7
M <martynas@welfordmedia.co.uk>
2014-05-28 11:37:05 +0100

    SQL Fix.

--- upload/admin/model/openbay/order.php
+++ upload/admin/model/openbay/order.php
@@ -1,22 +1,22 @@
 <?php
 class ModelOpenbayOrder extends Model {
 	public function getTotalOrders($data = array()) {
-		$sql = "SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "order`";
+		$sql = "SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "order` o";
 		
 		if ($this->config->get('openbay_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "ebay_order eo USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "ebay_order eo ON o.order_id = eo.order_id";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) eo ";
 		}
 		
 		if ($this->config->get('amazon_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "amazon_order ao USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "amazon_order ao ON o.order_id = ao.order_id ";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) ao ";
 		}
 		
 		if ($this->config->get('amazonus_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "amazonus_order auso USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "amazonus_order auso ON o.order_id = auso.order_id ";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) auso ";
 		}
@@ -52,19 +52,19 @@ class ModelOpenbayOrder extends Model {
 		$sql = "SELECT o.order_id, CONCAT(o.firstname, ' ', o.lastname) AS customer, (SELECT os.name FROM " . DB_PREFIX . "order_status os WHERE os.order_status_id = o.order_status_id AND os.language_id = '" . (int)$this->config->get('config_language_id') . "') AS status, o.currency_code, o.currency_value, o.date_added, IF(ao.order_id IS NULL, IF(auso.order_id IS NULL, IF(eo.order_id IS NULL, 'web', 'ebay'), 'amazonus'), 'amazon') AS channel FROM `" . DB_PREFIX . "order` o";
 
 		if ($this->config->get('openbay_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "ebay_order eo USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "ebay_order eo ON o.order_id = eo.order_id ";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) eo ";
 		}
 		
 		if ($this->config->get('amazon_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "amazon_order ao USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "amazon_order ao ON o.order_id = ao.order_id ";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) ao ";
 		}
 		
 		if ($this->config->get('amazonus_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "amazonus_order auso USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "amazonus_order auso ON o.order_id = auso.order_id ";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) auso ";
 		}
@@ -132,19 +132,19 @@ class ModelOpenbayOrder extends Model {
 		$sql = "SELECT o.order_id, o.order_status_id, o.shipping_method, CONCAT(o.firstname, ' ', o.lastname) AS customer, (SELECT os.name FROM " . DB_PREFIX . "order_status os WHERE os.order_status_id = o.order_status_id AND os.language_id = '" . (int)$this->config->get('config_language_id') . "') AS status, o.currency_code, o.currency_value, o.date_added, IF(ao.order_id IS NULL, IF(auso.order_id IS NULL, IF(eo.order_id IS NULL, 'web', 'ebay'), 'amazonus'), 'amazon') AS channel FROM `" . DB_PREFIX . "order` o";
 
 		if ($this->config->get('openbay_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "ebay_order eo USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "ebay_order eo ON o.order_id = eo.order_id ";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) eo ";
 		}
 		
 		if ($this->config->get('amazon_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "amazon_order ao USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "amazon_order ao ON o.order_id = ao.order_id ";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) ao ";
 		}
 		
 		if ($this->config->get('amazonus_status')) {
-			$sql .= " LEFT JOIN " . DB_PREFIX . "amazonus_order auso USING(order_id) ";
+			$sql .= " LEFT JOIN " . DB_PREFIX . "amazonus_order auso ON o.order_id = auso.order_id ";
 		} else {
 			$sql .= " JOIN (SELECT NULL AS order_id) auso ";
 		}

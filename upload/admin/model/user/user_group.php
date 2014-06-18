3df91bbf0588be25de966a25b037015b85d230e8
Daniel Kerr <webmaster@opencart.com>
2014-06-18 19:22:15 +0800

    more api work done

--- upload/admin/model/user/user_group.php
+++ upload/admin/model/user/user_group.php
@@ -1,45 +1,15 @@
 <?php
 class ModelUserUserGroup extends Model {
 	public function addUserGroup($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "user_group SET name = '" . $this->db->escape($data['name']) . "', permission = '" . (isset($data['permission']) ? serialize($data['permission']) : '') . "'");
+		$this->db->query("INSERT INTO " . DB_PREFIX . "user_group SET name = '" . $this->db->escape($data['name']) . "', permission = '" . (isset($data['permission']) ? $this->db->escape(serialize($data['permission'])) : '') . "'");
 	}
 
 	public function editUserGroup($user_group_id, $data) {
-		$this->db->query("UPDATE " . DB_PREFIX . "user_group SET name = '" . $this->db->escape($data['name']) . "', permission = '" . (isset($data['permission']) ? serialize($data['permission']) : '') . "' WHERE user_group_id = '" . (int)$user_group_id . "'");
+		$this->db->query("UPDATE " . DB_PREFIX . "user_group SET name = '" . $this->db->escape($data['name']) . "', permission = '" . (isset($data['permission']) ? $this->db->escape(serialize($data['permission'])) : '') . "' WHERE user_group_id = '" . (int)$user_group_id . "'");
 	}
 
 	public function deleteUserGroup($user_group_id) {
 		$this->db->query("DELETE FROM " . DB_PREFIX . "user_group WHERE user_group_id = '" . (int)$user_group_id . "'");
-	}
-
-	public function addPermission($user_id, $type, $page) {
-		$user_query = $this->db->query("SELECT DISTINCT user_group_id FROM " . DB_PREFIX . "user WHERE user_id = '" . (int)$user_id . "'");
-
-		if ($user_query->num_rows) {
-			$user_group_query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "user_group WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
-
-			if ($user_group_query->num_rows) {
-				$data = unserialize($user_group_query->row['permission']);
-
-				$data[$type][] = $page;
-
-				$this->db->query("UPDATE " . DB_PREFIX . "user_group SET permission = '" . serialize($data) . "' WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
-			}
-		}
-	}
-
-	public function removePermission($user_id, $type, $page) {
-		$user_query = $this->db->query("SELECT DISTINCT user_group_id FROM " . DB_PREFIX . "user WHERE user_id = '" . (int)$user_id . "'");
-
-		if ($user_query->num_rows) {
-			$user_group_query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "user_group WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
-
-			if ($user_group_query->num_rows) {
-				$data = unserialize($user_group_query->row['permission']);
-                                $data[$type] = array_diff($data[$type], array($page));
-				$this->db->query("UPDATE " . DB_PREFIX . "user_group SET permission = '" . serialize($data) . "' WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
-			}
-		}
 	}
 
 	public function getUserGroup($user_group_id) {
@@ -85,5 +55,37 @@ class ModelUserUserGroup extends Model {
       	$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "user_group");
 
 		return $query->row['total'];
+	}
+	
+	public function addPermission($user_id, $type, $route) {
+		$user_query = $this->db->query("SELECT DISTINCT user_group_id FROM " . DB_PREFIX . "user WHERE user_id = '" . (int)$user_id . "'");
+
+		if ($user_query->num_rows) {
+			$user_group_query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "user_group WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
+
+			if ($user_group_query->num_rows) {
+				$data = unserialize($user_group_query->row['permission']);
+
+				$data[$type][] = $route;
+
+				$this->db->query("UPDATE " . DB_PREFIX . "user_group SET permission = '" . $this->db->escape(serialize($data)) . "' WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
+			}
+		}
+	}
+
+	public function removePermission($user_id, $type, $route) {
+		$user_query = $this->db->query("SELECT DISTINCT user_group_id FROM " . DB_PREFIX . "user WHERE user_id = '" . (int)$user_id . "'");
+
+		if ($user_query->num_rows) {
+			$user_group_query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "user_group WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
+
+			if ($user_group_query->num_rows) {
+				$data = unserialize($user_group_query->row['permission']);
+                
+				$data[$type] = array_diff($data[$type], array($route));
+				
+				$this->db->query("UPDATE " . DB_PREFIX . "user_group SET permission = '" . $this->db->escape(serialize($data)) . "' WHERE user_group_id = '" . (int)$user_query->row['user_group_id'] . "'");
+			}
+		}
 	}
 }
--- upload/catalog/controller/api/order.php
+++ upload/catalog/controller/api/order.php
@@ -1,73 +1,5 @@
-  <?php
+<?php
 class ControllerApiOrder extends Controller {
-	public function setCustomer() {
-		$this->load->language('api/order');
-		
-		$json = array();
-		
-		// Customer
-		if ($this->request->post['customer_id']) {
-			$this->load->model('account/customer');
-
-			$customer_info = $this->model_account_customer->getCustomer($this->request->post['customer_id']);
-
-			if (!$customer_info) {
-				$json['error']['warning'] = $this->language->get('error_customer');
-			}
-		}
-		
-		if ((utf8_strlen(trim($this->request->post['firstname'])) < 1) || (utf8_strlen(trim($this->request->post['firstname'])) > 32)) {
-			$json['error']['firstname'] = $this->language->get('error_firstname');
-		}
-
-		if ((utf8_strlen(trim($this->request->post['lastname'])) < 1) || (utf8_strlen(trim($this->request->post['lastname'])) > 32)) {
-			$json['error']['lastname'] = $this->language->get('error_lastname');
-		}
-
-		if ((utf8_strlen($this->request->post['email']) > 96) || (!preg_match('/^[^\@]+@.*\.[a-z]{2,6}$/i', $this->request->post['email']))) {
-			$json['error']['email'] = $this->language->get('error_email');
-		}
-
-		if ((utf8_strlen($this->request->post['telephone']) < 3) || (utf8_strlen($this->request->post['telephone']) > 32)) {
-			$json['error']['telephone'] = $this->language->get('error_telephone');
-		}
-		
-		// Customer Group
-		if (isset($this->request->post['customer_group_id']) && is_array($this->config->get('config_customer_group_display')) && in_array($this->request->post['customer_group_id'], $this->config->get('config_customer_group_display'))) {
-			$customer_group_id = $this->request->post['customer_group_id'];
-		} else {
-			$customer_group_id = $this->config->get('config_customer_group_id');
-		}
-
-		// Custom field validation
-		$this->load->model('account/custom_field');
-
-		$custom_fields = $this->model_account_custom_field->getCustomFields(array('filter_customer_group_id' => $customer_group_id));
-
-		foreach ($custom_fields as $custom_field) {
-			if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']])) {
-				$json['error']['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
-			}
-		}
-
-		if (!$json) {
-			$this->session->data['customer'] = array(
-				'customer_id'       => $this->request->post['customer_id'],
-				'customer_group_id' => $customer_group_id,
-				'firstname'         => $this->request->post['firstname'],
-				'lastname'          => $this->request->post['lastname'],
-				'email'             => $this->request->post['email'],
-				'telephone'         => $this->request->post['lastname'],
-				'fax'               => $this->request->post['fax'],
-				'custom_field'      => unserialize($this->request->post['custom_field'])
-			);
-			
-			$json['success'] = $this->language->get('text_success');			
-		}
-		
-		$this->response->setOutput(json_encode($json));					
-	}
-			
 	public function addOrder() {
 		$this->load->language('api/order');
 		
@@ -311,7 +243,10 @@ class ControllerApiOrder extends Controller {
 			$order_data['comment'] = $this->session->data['comment'];
 			$order_data['total'] = $total;
 
-
+			$order_data['affiliate_id'] = $total;
+			
+			
+			
 			/*
 			if (isset($this->request->cookie['tracking'])) {
 				$order_data['tracking'] = $this->request->cookie['tracking'];
@@ -388,6 +323,87 @@ class ControllerApiOrder extends Controller {
 		$this->load->language('api/order');
 		
 		$json = array();
+		
+		// Customer
+		if (!isset($this->session->data['customer'])) {
+			$json['error']['warning'] = $this->language->get('error_customer');
+		}	
+				
+		// Payment Address
+		if (!isset($this->session->data['payment_address'])) {
+			$json['error']['warning'] = $this->language->get('error_payment_address');
+		}	
+		
+		// Payment Method
+		if (!isset($this->session->data['payment_method'])) {
+			$json['error']['warning'] = $this->language->get('error_payment_method');			
+		}
+
+		// Shipping
+		if ($this->cart->hasShipping()) {
+			// Shipping Address
+			if (!isset($this->session->data['shipping_address'])) {
+				$json['error']['warning'] = $this->language->get('error_shipping_address');
+			}
+		
+			// Shipping Method
+			if (!isset($this->request->post['shipping_method'])) {
+				$json['error']['warning'] = $this->language->get('error_shipping_method');
+			}
+		} else {
+			unset($this->session->data['shipping_address']);
+			unset($this->session->data['shipping_method']);
+			unset($this->session->data['shipping_methods']);
+		}
+						
+		// Cart
+		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
+			$json['error']['warning'] = $this->language->get('error_stock');
+		}
+
+
+
+		// Remove coupon, vouchers reward points history
+		if (isset($this->request->get['order_id'])) {
+			$this->load->model('account/order');
+
+			$order_totals = $this->model_account_order->getOrderTotals($this->request->get['order_id']);
+
+			foreach ($order_totals as $order_total) {
+				$this->load->model('total/' . $order_total['code']);
+
+				if (method_exists($this->{'model_total_' . $order_total['code']}, 'clear')) {
+					$this->{'model_total_' . $order_total['code']}->clear($this->request->get['order_id']);
+				}
+			}			
+		}
+			
+			
+			
+
+
+		// Add the coupon, vouchers and reward points back
+		if (isset($this->request->get['order_id'])) {
+			$this->load->model('account/order');
+			
+			$order_info = $this->model_account_order->getOrder($this->request->get['order_id']);
+			
+			if ($order_info) {
+				$order_totals = $this->model_account_order->getOrderTotals($this->request->get['order_id']);
+				
+				foreach ($order_totals as $order_total) {
+					$this->load->model('total/' . $order_total['code']);
+					
+					if (method_exists($this->{'model_total_' . $order_total['code']}, 'confirm')) {
+						$this->{'model_total_' . $order_total['code']}->confirm($order_info, $order_total);
+					}
+				}
+			}
+		}
+		
+		
+		
+		
 		
 		// Add the coupon, vouchers and reward points back
 		if (isset($this->request->get['order_id'])) {

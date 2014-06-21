29854e675080ed2e1f123e0210bd61df83d093b7
Jason <mithereal@gmail.com>
2014-06-20 19:57:42 -0700

    fix

--- upload/install/controller/step_3.php
+++ upload/install/controller/step_3.php
@@ -94,6 +94,7 @@ class ControllerStep3 extends Controller {
 		$data['text_mysqli'] = $this->language->get('text_mysqli');
 		$data['text_mpdo'] = $this->language->get('text_mpdo');
 		$data['text_pgsql'] = $this->language->get('text_pgsql');
+		$data['text_mmsql'] = $this->language->get('text_mmsql');
 
 		$data['entry_db_driver'] = $this->language->get('entry_db_driver');
 		$data['entry_db_hostname'] = $this->language->get('entry_db_hostname');

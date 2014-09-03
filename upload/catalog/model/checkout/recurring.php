9c5e3f0d9b491af1da96a3531a1d8ce5c91e5b2e
MostafaEs <mostafarobotic@gmail.com>
2014-09-03 11:47:06 +0430

    Update recurring.php
    just added spaces

--- upload/catalog/model/checkout/recurring.php
+++ upload/catalog/model/checkout/recurring.php
@@ -9,9 +9,9 @@ class ModelCheckoutRecurring extends Model {
 	public function addReference($recurring_id, $ref) {
 		$this->db->query("UPDATE " . DB_PREFIX . "order_recurring SET reference = '" . $this->db->escape($ref) . "' WHERE order_recurring_id = '" . (int)$recurring_id . "'");
 
-		if($this->db->countAffected() > 0) {
+		if ($this->db->countAffected() > 0) {
 			return true;
-		}else{
+		} else {
 			return false;
 
 		}

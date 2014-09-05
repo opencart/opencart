2cea9e328e55deb74657a6b4760b25a9c99d2e69
James Allsup <james@allsup.eu>
2014-09-05 10:32:49 +0100

    Footer version fix - do not show any part of the version string until logged in.

--- upload/admin/controller/common/footer.php
+++ upload/admin/controller/common/footer.php
@@ -8,7 +8,7 @@ class ControllerCommonFooter extends Controller {
 		if ($this->user->isLogged() && isset($this->request->get['token']) && ($this->request->get['token'] == $this->session->data['token'])) {
 			$data['text_version'] = sprintf($this->language->get('text_version'), VERSION);
 		} else {
-			$data['text_version'] = sprintf($this->language->get('text_version'), '');
+			$data['text_version'] = '';
 		}
 
 		return $this->load->view('common/footer.tpl', $data);

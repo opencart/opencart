fdd0767184931e4e84933c71916bfe1517ed2e58
Daniel kerr <webmaster@opnecart.com>
2014-09-07 13:59:33 +0800

    https://github.com/opencart/opencart/issues/1817

--- upload/admin/controller/error/not_found.php
+++ upload/admin/controller/error/not_found.php
@@ -9,6 +9,18 @@ class ControllerErrorNotFound extends Controller {
 
 		$data['text_not_found'] = $this->language->get('text_not_found');
 
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('error/not_found', 'token=' . $this->session->data['token'], 'SSL')
+		);
+		
 		$data['header'] = $this->load->controller('common/header');
 		$data['menu'] = $this->load->controller('common/menu');
 		$data['footer'] = $this->load->controller('common/footer');

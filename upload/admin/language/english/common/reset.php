4c1d803160c32601616c179981eb19fbc3b2cb36
James Allsup <james@allsup.eu>
2014-09-12 22:36:02 +0100

    Fixed admin password reset and changed style to match auth/login page.

--- upload/admin/language/english/common/reset.php
+++ upload/admin/language/english/common/reset.php
@@ -9,7 +9,7 @@ $_['text_success']   = 'Success: Your password has been successfully updated.';
 
 // Entry
 $_['entry_password'] = 'Password';
-$_['entry_confirm']  = 'Password Confirm';
+$_['entry_confirm']  = 'Confirm';
 
 // Error
 $_['error_password'] = 'Password must be between 5 and 20 characters!';

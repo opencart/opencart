f381dfa71f4aa8592a52087c4606ba939a53ded3
Shinrai <Shinrai@users.noreply.github.com>
2014-09-16 02:35:48 -0700

    Update cache.php
    revert back to earlier __construct to allow ocmod files to modify the drivers.

--- upload/system/library/cache.php
+++ upload/system/library/cache.php
@@ -2,11 +2,16 @@
 class Cache {
 	private $cache;
 
-	public function __construct($driver, $expire = 3600) {
-		$class = 'Cache\\' . $driver;
 
-		if (class_exists($class)) {
-			$this->cache = new $class($expire);
+	public function __construct($driver, $expire = 3600) {
+		$file = dirname(__FILE__) . '/driver/cache/' . $driver . '.php';
+		
+		if (file_exists($file)) {
+			require_once($file);        
+			
+			$class = 'Cache'. $driver;
+			
+			$this->cache = new $class($expire);		
 		} else {
 			exit('Error: Could not load cache driver ' . $driver . ' cache!');
 		}

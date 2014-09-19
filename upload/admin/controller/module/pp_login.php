ce70d23fb398176f2fc0efcbbf818984b3afda65
James Allsup <james@allsup.eu>
2014-09-19 09:44:05 +0100

    Pagination fix for uploaded files.
    Updated event trigger for paypal login/logout.

--- upload/admin/controller/module/pp_login.php
+++ upload/admin/controller/module/pp_login.php
@@ -344,7 +344,7 @@ class ControllerModulePPLogin extends Controller {
 	public function install() {
 		$this->load->model('tool/event');
 
-		$this->model_tool_event->addEvent('pp_login', 'customer_logout', 'module/pp_login/logout');
+		$this->model_tool_event->addEvent('pp_login', 'post.customer.logout', 'module/pp_login/logout');
 	}
 
 	public function uninstall() {

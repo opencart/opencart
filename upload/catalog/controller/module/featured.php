d4e4d02d812ad4a7afd8982f3819ce32d37663c6
James Allsup <james@allsup.eu>
2014-09-25 13:31:00 +0100

    Fix featured module image sizes.

--- upload/catalog/controller/module/featured.php
+++ upload/catalog/controller/module/featured.php
@@ -30,9 +30,9 @@ class ControllerModuleFeatured extends Controller {
 
 			if ($product_info) {
 				if ($product_info['image']) {
-					$image = $this->model_tool_image->resize($product_info['image'], $setting['image_width'], $setting['image_height']);
+					$image = $this->model_tool_image->resize($product_info['image'], $setting['width'], $setting['height']);
 				} else {
-					$image = $this->model_tool_image->resize('placeholder.png', $setting['image_width'], $setting['image_height']);
+					$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
 				}
 
 				if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {

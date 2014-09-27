47da580cdeed1dc0e6b2b04e338b93bbccc981a5
James Allsup <james@allsup.eu>
2014-09-27 19:32:38 +0100

    Updated incorrect language vars names.
    Updated pagination urls.
    
    #1776

--- upload/admin/language/english/report/customer_activity.php
+++ upload/admin/language/english/report/customer_activity.php
@@ -7,13 +7,13 @@ $_['text_address_add']    = '<a href="customer_id=%d">%s</a> added a new address
 $_['text_address_edit']   = '<a href="customer_id=%d">%s</a> updated their address.';
 $_['text_address_delete'] = '<a href="customer_id=%d">%s</a> deleted one of their address.';
 $_['text_edit']           = '<a href="customer_id=%d">%s</a> updated their account details.';
-$_['text_fogotten']       = '<a href="customer_id=%d">%s</a> requested a new password.';
+$_['text_forgotten']      = '<a href="customer_id=%d">%s</a> requested a new password.';
 $_['text_login']          = '<a href="customer_id=%d">%s</a> logged in.';
 $_['text_password']       = '<a href="customer_id=%d">%s</a> updated their account password.';
 $_['text_register']       = '<a href="customer_id=%d">%s</a> registered for an account.';
-$_['text_return_accout']  = '<a href="customer_id=%d">%s</a> submitted a product return.';
+$_['text_return_account'] = '<a href="customer_id=%d">%s</a> submitted a product return.';
 $_['text_return_guest']   = '%s submitted a product return.';
-$_['text_order_accout']   = '<a href="customer_id=%d">%s</a> created a <a href="order_id=%d">new order</a>.';
+$_['text_order_account']  = '<a href="customer_id=%d">%s</a> created a <a href="order_id=%d">new order</a>.';
 $_['text_order_guest']    = '%s created a <a href="order_id=%d">new order</a>.';
 
 // Column

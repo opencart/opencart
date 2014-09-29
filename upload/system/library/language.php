f5463c9f241808ce66bda38bd092ab0bcfa1e157
Daniel kerr <webmaster@opnecart.com>
2014-09-29 14:47:49 +0800

    https://github.com/opencart/opencart/issues/1865

--- upload/system/library/language.php
+++ upload/system/library/language.php
@@ -12,10 +12,6 @@ class Language {
 		return (isset($this->data[$key]) ? $this->data[$key] : $key);
 	}
 	
-	public function getAll() {
-		return $this->data;
-	}
-	
 	public function load($filename) {
 		$_ = array();
 

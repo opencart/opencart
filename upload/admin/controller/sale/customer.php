4dd876649df077341513d1391d26a34d3914d401
James Allsup <james@allsup.eu>
2014-10-01 12:17:37 +0100

    Fix for admin/controller/sale/customer.php build failure due to random slash.

--- upload/admin/controller/sale/customer.php
+++ upload/admin/controller/sale/customer.php
@@ -416,7 +416,7 @@ class ControllerSaleCustomer extends Controller {
 		}
 
 		$data['heading_title'] = $this->language->get('heading_title');
-		
+
 		$data['text_list'] = $this->language->get('text_list');
 		$data['text_enabled'] = $this->language->get('text_enabled');
 		$data['text_disabled'] = $this->language->get('text_disabled');
@@ -595,7 +595,7 @@ class ControllerSaleCustomer extends Controller {
 
 	protected function getForm() {
 		$data['heading_title'] = $this->language->get('heading_title');
-		\
+
 		$data['text_form'] = !isset($this->request->get['customer_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 		$data['text_enabled'] = $this->language->get('text_enabled');
 		$data['text_disabled'] = $this->language->get('text_disabled');

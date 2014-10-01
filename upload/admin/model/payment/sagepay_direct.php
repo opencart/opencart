4ee19fe6f50e4ab8f685e270fe2bdd96776e7d38
James Allsup <james@allsup.eu>
2014-10-01 11:51:52 +0100

    Small fixes for release action of payment. based on #1889

--- upload/admin/model/payment/sagepay_direct.php
+++ upload/admin/model/payment/sagepay_direct.php
@@ -110,7 +110,7 @@ class ModelPaymentSagepayDirect extends Model {
 		$sagepay_direct_order = $this->getOrder($order_id);
 		$total_released = $this->getTotalReleased($sagepay_direct_order['sagepay_direct_order_id']);
 
-		if (!empty($sagepay_direct_order) && $sagepay_direct_order['release_status'] == 0 && $total_released <= $amount) {
+		if (!empty($sagepay_direct_order) && $sagepay_direct_order['release_status'] == 0 && ($total_released + $amount <= $sagepay_direct_order['total'])) {
 			$release_data = array();
 
 			if ($this->config->get('sagepay_direct_test') == 'live') {

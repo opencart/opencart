7911c76c43e29ddc1e4168f36dff6a0be6179719
James Allsup <james@allsup.eu>
2014-10-01 12:41:47 +0100

    Changed remaining text vars from Product Feeds to Feeds.

--- upload/admin/language/english/feed/google_base.php
+++ upload/admin/language/english/feed/google_base.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Google Base';
 
 // Text
-$_['text_feed']        = 'Product Feeds';
+$_['text_feed']        = 'Feeds';
 $_['text_success']     = 'Success: You have modified Google Base feed!';
 $_['text_list']        = 'Layout List';
 

52f36617fb2fce95073c784998a360a911dbba1c
David Teal <david@welfordmedia.co.uk>
2014-10-22 15:17:50 +0100

    Removing category count for top level categories

--- upload/catalog/controller/module/category.php
+++ upload/catalog/controller/module/category.php
@@ -32,7 +32,7 @@ class ControllerModuleCategory extends Controller {
 		$categories = $this->model_catalog_category->getCategories(0);
 
 		foreach ($categories as $category) {
-			$total = $this->model_catalog_product->getTotalProducts(array('filter_category_id' => $category['category_id']));
+			$total = $this->config->get('config_product_count') ? $this->model_catalog_product->getTotalProducts(array('filter_category_id' => $category['category_id'])) : 0;
 
 			$children_data = array();
 

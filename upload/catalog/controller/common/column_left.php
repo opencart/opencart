944d7f81288dacecc2423cfd28794838afa67b58
TECHDEV EIRL <CoverUp@users.noreply.github.com>
2014-11-27 15:28:31 -0400

    Update column_left.php
    Prevent double call and fix notices about undefined index, also fix order according priority.

--- upload/catalog/controller/common/column_left.php
+++ upload/catalog/controller/common/column_left.php
@@ -48,16 +48,14 @@ class ControllerCommonColumnLeft extends Controller {
 		foreach ($modules as $module) {
 			$part = explode('.', $module['code']);
 			
-			if (isset($part[0]) && $this->config->get($part[0] . '_status')) {
-				$data['modules'][] = $this->load->controller('module/' . $part[0]);
-			}
-						
 			if (isset($part[1])) {
 				$setting_info = $this->model_extension_module->getModule($part[1]);
 				
 				if ($setting_info && $setting_info['status']) {
 					$data['modules'][] = $this->load->controller('module/' . $part[0], $setting_info);
 				}
+			}elseif (isset($part[0]) && $this->config->get($part[0] . '_status')) {
+				$data['modules'][] = $this->load->controller('module/' . $part[0]);
 			}
 		}
 

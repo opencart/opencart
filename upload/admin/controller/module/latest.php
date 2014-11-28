c7cb43ec8a734b1852ee077b900729d0ea873e91
TECHDEV EIRL <CoverUp@users.noreply.github.com>
2014-11-28 17:01:48 -0400

    Update latest.php
    breadcrumbs fix added

--- upload/admin/controller/module/latest.php
+++ upload/admin/controller/module/latest.php
@@ -77,7 +77,7 @@ class ControllerModuleLatest extends Controller {
 		if (!isset($this->request->get['module_id'])) {
 			$data['breadcrumbs'][] = array(
 				'text' => $this->language->get('heading_title'),
-				'href' => $this->url->link('module/html', 'token=' . $this->session->data['token'], 'SSL')
+				'href' => $this->url->link('module/latest', 'token=' . $this->session->data['token'], 'SSL')
 			);
 		} else {
 			$data['breadcrumbs'][] = array(

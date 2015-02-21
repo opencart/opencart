e24bafd9194d2cf1f9bee983fe54ab2873ae1abe
OC2PS-2 <vivek.pundir@gmail.com>
2015-02-22 00:50:25 +0800

    Multilingual reviews
    Sites that offer reviews across countries have dealt with and resolved
    the issue of multilingual reviews in a pretty good way - show reviews in
    current language on top before showing reviews in other languages.
    
    This patch adds the same function to OpenCart.

--- upload/catalog/model/catalog/review.php
+++ upload/catalog/model/catalog/review.php
@@ -3,7 +3,7 @@ class ModelCatalogReview extends Model {
 	public function addReview($product_id, $data) {
 		$this->event->trigger('pre.review.add', $data);
 
-		$this->db->query("INSERT INTO " . DB_PREFIX . "review SET author = '" . $this->db->escape($data['name']) . "', customer_id = '" . (int)$this->customer->getId() . "', product_id = '" . (int)$product_id . "', text = '" . $this->db->escape($data['text']) . "', rating = '" . (int)$data['rating'] . "', date_added = NOW()");
+		$this->db->query("INSERT INTO " . DB_PREFIX . "review SET author = '" . $this->db->escape($data['name']) . "', customer_id = '" . (int)$this->customer->getId() . "', product_id = '" . (int)$product_id . "', text = '" . $this->db->escape($data['text']) . "', rating = '" . (int)$data['rating'] . "', language_id = '" . (int)$this->config->get('config_language_id') . "', date_added = NOW()");
 
 		$review_id = $this->db->getLastId();
 
@@ -51,8 +51,8 @@ class ModelCatalogReview extends Model {
 		if ($limit < 1) {
 			$limit = 20;
 		}
-
-		$query = $this->db->query("SELECT r.review_id, r.author, r.rating, r.text, p.product_id, pd.name, p.price, p.image, r.date_added FROM " . DB_PREFIX . "review r LEFT JOIN " . DB_PREFIX . "product p ON (r.product_id = p.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) WHERE p.product_id = '" . (int)$product_id . "' AND p.date_available <= NOW() AND p.status = '1' AND r.status = '1' AND pd.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY r.date_added DESC LIMIT " . (int)$start . "," . (int)$limit);
+		
+		$query = $this->db->query("SELECT r.review_id, r.author, r.rating, r.text, p.product_id, pd.name, p.price, p.image, r.date_added FROM " . DB_PREFIX . "review r LEFT JOIN " . DB_PREFIX . "product p ON (r.product_id = p.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) WHERE p.product_id = '" . (int)$product_id . "' AND p.date_available <= NOW() AND p.status = '1' AND r.status = '1' AND pd.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY r.language_id = '" . (int)$this->config->get('config_language_id') . "' DESC, r.language_id ASC, r.date_added DESC LIMIT " . (int)$start . "," . (int)$limit);
 
 		return $query->rows;
 	}

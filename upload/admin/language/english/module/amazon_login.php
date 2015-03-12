1446f61b3f8ee8441393fd0186d4b0c09c939844
Chris <chris@welfordmedia.co.uk>
2015-03-12 13:31:01 +0000

    adding Amazon Login and Pay integration files

---
+++ upload/admin/language/english/module/amazon_login.php
@@ -0,0 +1,34 @@
+<?php
+
+// Heading
+$_['heading_title'] = 'Login with Amazon';
+
+//Text
+$_['text_module'] = 'Modules';
+$_['text_success'] = 'Success: You have modified module Log In with Amazon!';
+$_['text_content_top'] = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left'] = 'Column Left';
+$_['text_column_right'] = 'Column Right';
+$_['text_lwa_button'] = 'Login with Amazon';
+$_['text_login_button'] = 'Login';
+$_['text_a_button'] = 'A';
+$_['text_gold_button'] = 'Gold';
+$_['text_darkgrey_button'] = 'Dark Grey';
+$_['text_lightgrey_button'] = 'Light Grey';
+$_['text_small_button'] = 'Small';
+$_['text_medium_button'] = 'Medium';
+$_['text_large_button'] = 'Large';
+$_['text_x_large_button'] = 'X-Large';
+
+//Entry
+$_['entry_button_type'] = 'Button Type';
+$_['entry_button_colour'] = 'Button Colour';
+$_['entry_button_size'] = 'Button Size';
+$_['entry_layout'] = 'Layout';
+$_['entry_position'] = 'Position';
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+//Error
+$_['error_permission'] = 'Warning: You do not have permission to modify module Log In with Amazon!';

9d295d302d84d6002e2582e49d4b6014c922bcb0
James Allsup <james@allsup.eu>
2015-03-16 14:34:45 +0000

    Removed the config_ignore_token var as it no longer is used in the core code.

--- upload/admin/controller/common/login.php
+++ upload/admin/controller/common/login.php
@@ -135,14 +135,6 @@ class ControllerCommonLogin extends Controller {
 				'error/permission'
 			);
 
-			$config_ignore = array();
-
-			if ($this->config->get('config_token_ignore')) {
-				$config_ignore = unserialize($this->config->get('config_token_ignore'));
-			}
-
-			$ignore = array_merge($ignore, $config_ignore);
-
 			if (!in_array($route, $ignore) && (!isset($this->request->get['token']) || !isset($this->session->data['token']) || ($this->request->get['token'] != $this->session->data['token']))) {
 				return new Action('common/login');
 			}

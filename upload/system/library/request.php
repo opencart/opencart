2e30c6c9dbcf76f46d0e52b787d8cba371d232d3
maks feltrin <pine3ree@gmail.com>
2015-03-27 17:38:50 +0100

    utility methods
    I've been using these for a very long time, always forgot to PR.
    Caching results to static vars to avoid array access and already made comparisons

--- upload/system/library/request.php
+++ upload/system/library/request.php
@@ -28,4 +28,42 @@ class Request {
 
 		return $data;
 	}
+
+	public function isGet() {
+		static $is_get = null;
+		if ($is_get === null) {
+			$is_get = ($this->server['REQUEST_METHOD'] === 'GET');
+		}
+		return $is_get;
+	}
+
+	public function isPost() {
+		static $is_post = null;
+		if ($is_post === null) {
+			$is_post = ($this->server['REQUEST_METHOD'] === 'POST');
+		}
+		return $is_post;
+	}
+
+	public function isSecure() {
+		static $is_secure = null;
+		if ($is_secure === null) {
+			$https = $this->server['HTTPS'];
+			$is_secure = ($https === 'on' || $https === '1');
+		}
+		return $is_secure;
+	}
+
+	public function isSSL() {
+		return $this->isSecure();
+	}
+
+	public function isAjax() {
+		static $is_ajax = null;
+		if ($is_ajax === null) {
+			$hxrw = $this->server['HTTP_X_REQUESTED_WITH'];
+			$is_ajax = (strtolower($hxrw) === 'xmlhttprequest');
+		}
+		return $is_ajax;
+	}
 }

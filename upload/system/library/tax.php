31bde933b9b080772b5c3ba2c38414c965cc593a
maks feltrin <pine3ree@gmail.com>
2015-04-05 13:53:33 +0200

    performance improvement lazy loading rates
    Not every route needs tax calculations (ajax generated zone/country listing, product review creation, login, logout)
    By moving tax rates loading out of the constructors we can avoid (3) unused database query for those routes.
    
    also, refactored setters....they are basically one method with a parameter ($based)

--- upload/system/library/tax.php
+++ upload/system/library/tax.php
@@ -2,56 +2,51 @@
 final class Tax {
 	private $tax_rates = array();
 
+	private $ready = array();
+
 	public function __construct($registry) {
 		$this->config = $registry->get('config');
 		$this->db = $registry->get('db');
 		$this->session = $registry->get('session');
-
-		if (isset($this->session->data['shipping_address'])) {
-			$this->setShippingAddress($this->session->data['shipping_address']['country_id'], $this->session->data['shipping_address']['zone_id']);
-		} elseif ($this->config->get('config_tax_default') == 'shipping') {
-			$this->setShippingAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
-		}
-
-		if (isset($this->session->data['payment_address'])) {
-			$this->setPaymentAddress($this->session->data['payment_address']['country_id'], $this->session->data['payment_address']['zone_id']);
-		} elseif ($this->config->get('config_tax_default') == 'payment') {
-			$this->setPaymentAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
-		}
-
-		$this->setStoreAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
 	}
-
-	public function setShippingAddress($country_id, $zone_id) {
-		$tax_query = $this->db->query("SELECT tr1.tax_class_id, tr2.tax_rate_id, tr2.name, tr2.rate, tr2.type, tr1.priority FROM " . DB_PREFIX . "tax_rule tr1 LEFT JOIN " . DB_PREFIX . "tax_rate tr2 ON (tr1.tax_rate_id = tr2.tax_rate_id) INNER JOIN " . DB_PREFIX . "tax_rate_to_customer_group tr2cg ON (tr2.tax_rate_id = tr2cg.tax_rate_id) LEFT JOIN " . DB_PREFIX . "zone_to_geo_zone z2gz ON (tr2.geo_zone_id = z2gz.geo_zone_id) LEFT JOIN " . DB_PREFIX . "geo_zone gz ON (tr2.geo_zone_id = gz.geo_zone_id) WHERE tr1.based = 'shipping' AND tr2cg.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND z2gz.country_id = '" . (int)$country_id . "' AND (z2gz.zone_id = '0' OR z2gz.zone_id = '" . (int)$zone_id . "') ORDER BY tr1.priority ASC");
-
-		foreach ($tax_query->rows as $result) {
-			$this->tax_rates[$result['tax_class_id']][$result['tax_rate_id']] = array(
-				'tax_rate_id' => $result['tax_rate_id'],
-				'name'        => $result['name'],
-				'rate'        => $result['rate'],
-				'type'        => $result['type'],
-				'priority'    => $result['priority']
-			);
+	
+	private function prepare()
+	{
+		if (empty($this->ready['shipping'])) {
+			if (isset($this->session->data['shipping_address'])) {
+				$this->setShippingAddress($this->session->data['shipping_address']['country_id'], $this->session->data['shipping_address']['zone_id']);
+			} elseif ($this->config->get('config_tax_default') == 'shipping') {
+				$this->setShippingAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
+			}
 		}
+
+		if (empty($this->ready['payment'])) {
+			if (isset($this->session->data['payment_address'])) {
+				$this->setPaymentAddress($this->session->data['payment_address']['country_id'], $this->session->data['payment_address']['zone_id']);
+			} elseif ($this->config->get('config_tax_default') == 'payment') {
+				$this->setPaymentAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
+			}
+		}
+
+		if (empty($this->ready['store'])) {
+			$this->setStoreAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
+		}
+	}
+	
+	public function setShippingAddress($country_id, $zone_id) {
+		$this->setAddress('shipping', $country_id, $zone_id);;
 	}
 
 	public function setPaymentAddress($country_id, $zone_id) {
-		$tax_query = $this->db->query("SELECT tr1.tax_class_id, tr2.tax_rate_id, tr2.name, tr2.rate, tr2.type, tr1.priority FROM " . DB_PREFIX . "tax_rule tr1 LEFT JOIN " . DB_PREFIX . "tax_rate tr2 ON (tr1.tax_rate_id = tr2.tax_rate_id) INNER JOIN " . DB_PREFIX . "tax_rate_to_customer_group tr2cg ON (tr2.tax_rate_id = tr2cg.tax_rate_id) LEFT JOIN " . DB_PREFIX . "zone_to_geo_zone z2gz ON (tr2.geo_zone_id = z2gz.geo_zone_id) LEFT JOIN " . DB_PREFIX . "geo_zone gz ON (tr2.geo_zone_id = gz.geo_zone_id) WHERE tr1.based = 'payment' AND tr2cg.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND z2gz.country_id = '" . (int)$country_id . "' AND (z2gz.zone_id = '0' OR z2gz.zone_id = '" . (int)$zone_id . "') ORDER BY tr1.priority ASC");
-
-		foreach ($tax_query->rows as $result) {
-			$this->tax_rates[$result['tax_class_id']][$result['tax_rate_id']] = array(
-				'tax_rate_id' => $result['tax_rate_id'],
-				'name'        => $result['name'],
-				'rate'        => $result['rate'],
-				'type'        => $result['type'],
-				'priority'    => $result['priority']
-			);
-		}
+		$this->setAddress('payment', $country_id, $zone_id);
 	}
 
 	public function setStoreAddress($country_id, $zone_id) {
-		$tax_query = $this->db->query("SELECT tr1.tax_class_id, tr2.tax_rate_id, tr2.name, tr2.rate, tr2.type, tr1.priority FROM " . DB_PREFIX . "tax_rule tr1 LEFT JOIN " . DB_PREFIX . "tax_rate tr2 ON (tr1.tax_rate_id = tr2.tax_rate_id) INNER JOIN " . DB_PREFIX . "tax_rate_to_customer_group tr2cg ON (tr2.tax_rate_id = tr2cg.tax_rate_id) LEFT JOIN " . DB_PREFIX . "zone_to_geo_zone z2gz ON (tr2.geo_zone_id = z2gz.geo_zone_id) LEFT JOIN " . DB_PREFIX . "geo_zone gz ON (tr2.geo_zone_id = gz.geo_zone_id) WHERE tr1.based = 'store' AND tr2cg.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND z2gz.country_id = '" . (int)$country_id . "' AND (z2gz.zone_id = '0' OR z2gz.zone_id = '" . (int)$zone_id . "') ORDER BY tr1.priority ASC");
+		$this->setAddress('store', $country_id, $zone_id);
+	}
+
+	private function setAddress($based, $country_id, $zone_id) {
+		$tax_query = $this->db->query("SELECT tr1.tax_class_id, tr2.tax_rate_id, tr2.name, tr2.rate, tr2.type, tr1.priority FROM " . DB_PREFIX . "tax_rule tr1 LEFT JOIN " . DB_PREFIX . "tax_rate tr2 ON (tr1.tax_rate_id = tr2.tax_rate_id) INNER JOIN " . DB_PREFIX . "tax_rate_to_customer_group tr2cg ON (tr2.tax_rate_id = tr2cg.tax_rate_id) LEFT JOIN " . DB_PREFIX . "zone_to_geo_zone z2gz ON (tr2.geo_zone_id = z2gz.geo_zone_id) LEFT JOIN " . DB_PREFIX . "geo_zone gz ON (tr2.geo_zone_id = gz.geo_zone_id) WHERE tr1.based = '{$based}' AND tr2cg.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND z2gz.country_id = '" . (int)$country_id . "' AND (z2gz.zone_id = '0' OR z2gz.zone_id = '" . (int)$zone_id . "') ORDER BY tr1.priority ASC");
 
 		foreach ($tax_query->rows as $result) {
 			$this->tax_rates[$result['tax_class_id']][$result['tax_rate_id']] = array(
@@ -62,8 +57,10 @@ final class Tax {
 				'priority'    => $result['priority']
 			);
 		}
-	}
 
+		$this->ready[$based] = true;
+	}
+	
 	public function calculate($value, $tax_class_id, $calculate = true) {
 		if ($tax_class_id && $calculate) {
 			$amount = 0;
@@ -107,6 +104,8 @@ final class Tax {
 	}
 
 	public function getRates($value, $tax_class_id) {
+		$this->prepare();
+		
 		$tax_rate_data = array();
 
 		if (isset($this->tax_rates[$tax_class_id])) {
@@ -137,6 +136,7 @@ final class Tax {
 	}
 
 	public function has($tax_class_id) {
+		$this->prepare();
 		return isset($this->taxes[$tax_class_id]);
 	}
 }

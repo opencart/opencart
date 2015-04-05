2d9ababe0bef34285a410a4d923aa7a4a6034579
maks feltrin <pine3ree@gmail.com>
2015-04-05 17:03:32 +0200

    mutually excluding conditions

--- upload/catalog/controller/account/login.php
+++ upload/catalog/controller/account/login.php
@@ -34,9 +34,7 @@ class ControllerAccountLogin extends Controller {
 
 				if ($this->config->get('config_tax_customer') == 'payment') {
 					$this->session->data['payment_address'] = $this->model_account_address->getAddress($this->customer->getAddressId());
-				}
-
-				if ($this->config->get('config_tax_customer') == 'shipping') {
+				} elseif ($this->config->get('config_tax_customer') == 'shipping') {
 					$this->session->data['shipping_address'] = $this->model_account_address->getAddress($this->customer->getAddressId());
 				}
 
@@ -62,9 +60,7 @@ class ControllerAccountLogin extends Controller {
 
 			if ($this->config->get('config_tax_customer') == 'payment') {
 				$this->session->data['payment_address'] = $this->model_account_address->getAddress($this->customer->getAddressId());
-			}
-
-			if ($this->config->get('config_tax_customer') == 'shipping') {
+			} elseif ($this->config->get('config_tax_customer') == 'shipping') {
 				$this->session->data['shipping_address'] = $this->model_account_address->getAddress($this->customer->getAddressId());
 			}
 

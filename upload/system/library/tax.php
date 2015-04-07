92227997b0d39ad169ac434883d5ba30ff2fc36c
maks feltrin <pine3ree@gmail.com>
2015-04-07 16:01:54 +0200

    loop improvement

--- upload/system/library/tax.php
+++ upload/system/library/tax.php
@@ -45,11 +45,9 @@ final class Tax {
 	
 	private function initialize() {
 		if (!$this->initialized) {
-			$address_types = array('shipping', 'payment', 'store');
-
-			foreach ($address_types as $based) {
-				$country_id = $this->address[$based]['country_id'];
-				$zone_id    = $this->address[$based]['zone_id'];
+			foreach ($this->address as $based => $address) {
+				$country_id = $address['country_id'];
+				$zone_id    = $address['zone_id'];
 
 				$tax_query = $this->db->query("SELECT tr1.tax_class_id, tr2.tax_rate_id, tr2.name, tr2.rate, tr2.type, tr1.priority FROM " . DB_PREFIX . "tax_rule tr1 LEFT JOIN " . DB_PREFIX . "tax_rate tr2 ON (tr1.tax_rate_id = tr2.tax_rate_id) INNER JOIN " . DB_PREFIX . "tax_rate_to_customer_group tr2cg ON (tr2.tax_rate_id = tr2cg.tax_rate_id) LEFT JOIN " . DB_PREFIX . "zone_to_geo_zone z2gz ON (tr2.geo_zone_id = z2gz.geo_zone_id) LEFT JOIN " . DB_PREFIX . "geo_zone gz ON (tr2.geo_zone_id = gz.geo_zone_id) WHERE tr1.based = '{$based}' AND tr2cg.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND z2gz.country_id = '" . (int)$country_id . "' AND (z2gz.zone_id = '0' OR z2gz.zone_id = '" . (int)$zone_id . "') ORDER BY tr1.priority ASC");
 

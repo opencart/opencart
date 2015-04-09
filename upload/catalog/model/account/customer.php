7974e4674258bfe3688c7b54c5c984ddc98d6cf9
opencart-russia <forum@mail-opencart-russia.ru>
2015-04-09 14:04:57 +0400

    BUG when sending the e-mail to admin
    error when sending the e-mail to admin if to register the client

--- upload/catalog/model/account/customer.php
+++ upload/catalog/model/account/customer.php
@@ -75,6 +75,9 @@ class ModelAccountCustomer extends Model {
 			$mail->smtp_port = $this->config->get('config_mail_smtp_port');
 			$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
 			
+			$mail->setTo($this->config->get('config_email'));
+			$mail->setFrom($this->config->get('config_email'));
+			$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
 			$mail->setSubject(html_entity_decode($this->language->get('text_new_customer'), ENT_QUOTES, 'UTF-8'));
 			$mail->setText($message);
 			$mail->send();

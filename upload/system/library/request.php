705ae1f6d38dd42b553d09a4411c56ca109f935a
maks feltrin <pine3ree@gmail.com>
2015-04-13 15:10:58 +0200

    Update request.php
    moved method static vars to instance properties

--- upload/system/library/request.php
+++ upload/system/library/request.php
@@ -5,6 +5,11 @@ class Request {
 	public $cookie = array();
 	public $files = array();
 	public $server = array();
+	
+	protected $is_get;
+	protected $is_post;
+	protected $is_ssl;
+	protected $is_ajax;
 
 	public function __construct() {
 		$this->get = $this->clean($_GET);
@@ -30,40 +35,40 @@ class Request {
 	}
 
 	public function isGet() {
-		static $is_get = null;
-		if ($is_get === null) {
-			$is_get = ($this->server['REQUEST_METHOD'] === 'GET');
+		if ($this->is_get === null) {
+			$this->is_get = ($this->server['REQUEST_METHOD'] === 'GET');
 		}
-		return $is_get;
+
+		return $this->is_get;
 	}
 
 	public function isPost() {
-		static $is_post = null;
-		if ($is_post === null) {
-			$is_post = ($this->server['REQUEST_METHOD'] === 'POST');
+		if ($this->is_post === null) {
+			$this->is_post = ($this->server['REQUEST_METHOD'] === 'POST');
 		}
-		return $is_post;
-	}
 
-	public function isSecure() {
-		static $is_secure = null;
-		if ($is_secure === null) {
-			$https = $this->server['HTTPS'];
-			$is_secure = ($https === 'on' || $https === '1');
-		}
-		return $is_secure;
+		return $this->is_post;
 	}
 
 	public function isSSL() {
-		return $this->isSecure();
+		if ($this->is_ssl === null) {
+			$https = strtolower($this->server['HTTPS']);
+			$this->is_ssl = ($https === 'on' || $https === '1');
+		}
+
+		return $this->is_ssl;
+	}
+
+	public function isSecure() {
+		return $this->isSSL();
 	}
 
 	public function isAjax() {
-		static $is_ajax = null;
-		if ($is_ajax === null) {
-			$hxrw = $this->server['HTTP_X_REQUESTED_WITH'];
-			$is_ajax = (strtolower($hxrw) === 'xmlhttprequest');
+		if ($this->is_ajax === null) {
+			$hxrw = strtolower($this->server['HTTP_X_REQUESTED_WITH']);
+			$this->is_ajax = ($hxrw === 'xmlhttprequest');
 		}
-		return $is_ajax;
+
+		return $this->is_ajax;
 	}
 }

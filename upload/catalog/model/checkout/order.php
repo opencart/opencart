d5f66b0d75a5adf35815e333e45598bbc38750d0
Daniel Kerr <webmaster@opencart.com>
2015-04-21 21:18:42 +0800

    $this->load->model('fraud/' . $extension['code']);

--- upload/catalog/model/checkout/order.php
+++ upload/catalog/model/checkout/order.php
@@ -301,6 +301,8 @@ class ModelCheckoutOrder extends Model {
 	
 				foreach ($extensions as $extension) {
 					if ($this->config->get($extension['code'] . '_status')) {	
+						$this->load->model('fraud/' . $extension['code']);
+					
 						$fraud_status_id = $this->{'model_fraud_' . $extension['code']}->check($order_info);
 					
 						if ($fraud_status_id) {

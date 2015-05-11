414519f49e1f093c8b94ff0815899cab66ea9c8f
Valdeir Santana <wallace_gatinho_cat@hotmail.com>
2015-05-11 01:57:06 -0300

    Update product.php

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -303,7 +303,7 @@ class ModelCatalogProduct extends Model {
 			$data['product_download'] = $this->getProductDownloads($product_id);
 			$data['product_layout'] = $this->getProductLayouts($product_id);
 			$data['product_store'] = $this->getProductStores($product_id);
-			$data['product_recurrings'] => $this->getRecurrings($product_id);
+			$data['product_recurrings'] = $this->getRecurrings($product_id);
 
 			$this->addProduct($data);
 		}

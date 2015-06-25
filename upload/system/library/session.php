709f06c3bc09c4e26ab6dc09b99ac4edfa5483ae
Daniel Kerr <webmaster@opencart.com>
2015-06-25 14:44:02 +0800

    https://github.com/opencart/opencart/issues/3131

--- upload/system/library/session.php
+++ upload/system/library/session.php
@@ -3,17 +3,19 @@ class Session {
 	public $data = array();
 
 	public function __construct($session_id = '') {
-		ini_set('session.use_only_cookies', 'On');
-		ini_set('session.use_cookies', 'On');
-		ini_set('session.use_trans_sid', 'Off');
-		ini_set('session.cookie_httponly', 'On');
-
 		if ($session_id) {
 			session_id($session_id);
 		}
-
-		session_set_cookie_params(0, '/');
-		session_start();
+		
+		if (!session_id()) {
+			ini_set('session.use_only_cookies', 'On');
+			ini_set('session.use_cookies', 'On');
+			ini_set('session.use_trans_sid', 'Off');
+			ini_set('session.cookie_httponly', 'On');
+			
+			session_set_cookie_params(0, '/');
+			session_start();
+		}
 		
 		$this->data =& $_SESSION;
 	}

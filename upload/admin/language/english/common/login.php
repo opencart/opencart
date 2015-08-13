0b3d209b5f88fc2c046c68e04eb4dc73b5492a65
Mauro Dutra <mdutra@mdisys.net>
2015-08-13 08:59:00 -0700

    Enhanced OC Admin User login with PCI compliance.

--- upload/admin/language/english/common/login.php
+++ upload/admin/language/english/common/login.php
@@ -6,14 +6,30 @@ $_['heading_title']  = 'Administration';
 $_['text_heading']   = 'Administration';
 $_['text_login']     = 'Please enter your login details.';
 $_['text_forgotten'] = 'Forgotten Password';
+$_['text_change_password'] = 'Change Your Password';
+$_['text_please_change_password'] = 'Please change your password.';
+$_['text_answer_security_question'] = 'Answer security question';
+$_['text_old_answer_security_question'] = 'Old answer to security question';
+$_['text_new_answer_security_question'] = 'Enter new answer to security question';
 
 // Entry
 $_['entry_username'] = 'Username';
 $_['entry_password'] = 'Password';
+$_['entry_new_password'] = 'New Password:';
+$_['entry_old_password'] = 'Old Password:';
+$_['entry_security_question'] = 'Who is your zookeeper?';
 
 // Button
-$_['button_login']   = 'Login';
+$_['button_login']             = 'Login';
+$_['button_change_password']   = 'Change Password';
 
 // Error
-$_['error_login']    = 'No match for Username and/or Password.';
-$_['error_token']    = 'Invalid token session. Please login again.';
+$_['error_login']                    = 'No match for Username and/or Password.';
+$_['error_token']                    = 'Invalid token session. Please login again.';
+$_['password_expired_error']         = 'Your password has expired. Call Systems Administrator for assistance.';
+$_['password_expired_today_warning'] = 'Your password has expired today. You most change your password now.';
+$_['password_expires_warning']       = 'Your password will expire in %s day(s). You most change your password or you will be locked out.';
+$_['password_expiration_error']      = 'System failed to validate password expiration date. Call Systems Administrator for assistance.';
+$_['error_invalid_login_credentials_not_allow_password_change'] = 'Invalid old login credentials do not allow change of password.';
+$_['error_failed_to_update_login_credentials'] = 'Failed to update login credentials. Please call Systems Administrator for assistance.';
+$_['error_login_sesion_expired']    = 'Your login session has expired. Please login again!';

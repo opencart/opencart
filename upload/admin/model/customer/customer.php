f15b50ea4f46e774777de4b7cfcbdaa38e64942f
Ekrem KAYA <webmaster@e-piksel.com>
2015-08-21 18:53:13 +0300

    Fixed pagination on customer ips list

--- upload/admin/model/customer/customer.php
+++ upload/admin/model/customer/customer.php
@@ -506,8 +506,16 @@ class ModelCustomerCustomer extends Model {
 		return $query->row['total'];
 	}
 
-	public function getIps($customer_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "customer_ip WHERE customer_id = '" . (int)$customer_id . "'");
+	public function getIps($customer_id, $start = 0, $limit = 10) {
+		if ($start < 0) {
+			$start = 0;
+		}
+
+		if ($limit < 1) {
+			$limit = 10;
+		}
+		
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "customer_ip WHERE customer_id = '" . (int)$customer_id . "' ORDER BY date_added DESC LIMIT " . (int)$start . "," . (int)$limit);
 
 		return $query->rows;
 	}

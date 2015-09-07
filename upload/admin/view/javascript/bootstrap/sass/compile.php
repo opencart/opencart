39830e957b884db12448f2d54c280a76c9656ca1
Daniel Kerr <webmaster@opencart.com>
2015-09-07 18:20:18 +0800

    updated the admin to use sass compiler

--- upload/admin/view/javascript/bootstrap/sass/compile.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/css');
-
-require('scss.inc.php');
-
-$scss = new scssc();
-$scss->setImportPaths(__DIR__ . '/');
-echo $scss->compile('@import "_bootstrap.scss"');

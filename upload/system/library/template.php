ff541c430201274e54f1075c4d24692663d3719c
Sergey Smirnov <smilesrg@gmail.com>
2015-09-19 01:10:57 +0300

    Added template service

--- upload/system/library/template.php
+++ upload/system/library/template.php
@@ -1,5 +1,20 @@
 <?php
 class Template {
+	private $registry;
+
+	public function __construct($registry) {
+		$this->registry = $registry;
+	}
+
+	public function render($filename, $data) {
+		if (file_exists(DIR_TEMPLATE . $this->registry->config->get('config_template') . '/template/common/' . $filename)) {
+			$this->registry->response->setOutput($this->registry->load->view($this->registry->config->get('config_template') . '/template/common/' . $filename, $data));
+		} else {
+			$this->registry->response->setOutput($this->registry->load->view('default/template/common/' . $filename, $data));
+		}
+	}
+
+/*
 	private $data = array();
 
   public function __construct($driver) {
@@ -36,4 +51,5 @@ class Template {
 			exit();
 		}
 	}
+*/
 }

73d08b4ca0d92aafd3f85a90410c4bb80f3d83c4
André Madeira Pereira <andre_madeira_pereira@hotmail.com>
2015-10-04 23:54:36 +0100

    getIdSql function

--- upload/catalog/model/catalog/category.php
+++ upload/catalog/model/catalog/category.php
@@ -8,16 +8,8 @@ class ModelCatalogCategory extends Model {
 
 	public function getCategories($parent_id = 0) {
             
-                $sql_id = "";
-                
-                if (empty($parent_id)) {
-                    $sql_id = " IS NULL";
-                } else {
-                    $sql_id = " = '" . (int)$parent_id . "'";
-                }
-            
                 //AMP : $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id " . $sql_id . " AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id " . getIdSql($parent_id) . " AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");
 
 		return $query->rows;
 	}
@@ -72,17 +64,9 @@ class ModelCatalogCategory extends Model {
 	}
 
 	public function getTotalCategoriesByCategoryId($parent_id = 0) {
-            
-                $sql_id = "";
-                
-                if (empty($parent_id)) {
-                    $sql_id = " IS NULL";
-                } else {
-                    $sql_id = " = '" . (int)$parent_id . "'";
-                }
-                
+
 		//AMP : $query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "' AND c.status = '1'");
-                $query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id " . $sql_id . " AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "' AND c.status = '1'");
+                $query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id " . getIdSql($parent_id) . " AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "' AND c.status = '1'");
 
 		return $query->row['total'];
 	}

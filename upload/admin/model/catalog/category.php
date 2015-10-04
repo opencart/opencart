58fae0d1162e373c0a26590a07fb3ddf5cfc565e
André Madeira Pereira <andre_madeira_pereira@hotmail.com>
2015-10-04 21:00:34 +0100

    getIdSql function

--- upload/admin/model/catalog/category.php
+++ upload/admin/model/catalog/category.php
@@ -185,16 +185,8 @@ class ModelCatalogCategory extends Model {
 
 	public function repairCategories($parent_id = 0) {
             
-                $sql_id = "";
-                
-                if (empty($parent_id)) {
-                    $sql_id = " IS NULL";
-                } else {
-                    $sql_id = " = '" . (int)$parent_id . "'";
-                }
-                
                 //AMP : $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category WHERE parent_id = '" . (int)$parent_id . "'");
-                $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category WHERE parent_id " . $sql_id);
+                $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category WHERE parent_id " . getIdSql($parent_id));
 
 		foreach ($query->rows as $category) {
 			// Delete the path below the current one
@@ -203,7 +195,8 @@ class ModelCatalogCategory extends Model {
 			// Fix for records with no paths
 			$level = 0;
 
-			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "category_path` WHERE category_id = '" . (int)$parent_id . "' ORDER BY level ASC");
+                        //AMP : $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "category_path` WHERE category_id = '" . (int)$parent_id . "' ORDER BY level ASC");
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "category_path` WHERE category_id "  . getIdSql($parent_id) . " ORDER BY level ASC");
 
 			foreach ($query->rows as $result) {
 				$this->db->query("INSERT INTO `" . DB_PREFIX . "category_path` SET category_id = '" . (int)$category['category_id'] . "', `path_id` = '" . (int)$result['path_id'] . "', level = '" . (int)$level . "'");

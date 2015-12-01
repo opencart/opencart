e8a927c11036c5d37a8cc53d09984f3bdb445e43
Euan Callus <euancallus@gmail.com>
2015-12-01 11:47:56 +0000

    Undefined variable 'header' on line 1 of account.tpl when trying to view account/account. See image: https://dl.dropboxusercontent.com/u/31484274/Screen%20Shot%202015-12-01%20at%2011.40.50.png
    Added the $data['header'] declaration back into the account/account.php controller.

--- upload/catalog/controller/account/account.php
+++ upload/catalog/controller/account/account.php
@@ -70,6 +70,7 @@ class ControllerAccountAccount extends Controller {
 		$data['content_top'] = $this->load->controller('common/content_top');
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
 		
 		$this->response->setOutput($this->load->view('account/account', $data));
 	}

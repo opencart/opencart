c14bfa64d52f91a70979855af6ae10d86534a84e
Daniel Kerr <webmaster@opencart.com>
2015-12-23 21:25:26 +0800

    config update

--- upload/system/config/admin.php
+++ upload/system/config/admin.php
@@ -4,13 +4,13 @@ $_['site.base']         = substr(HTTP_SERVER, 7);
 $_['site.ssl']          = false;
 
 // Database
-$_['db.autostart']         = true;
-$_['db.type']              = DB_DRIVER; // mpdo, mssql, mysql, mysqli or postgre
-$_['db.hostname']          = DB_HOSTNAME;
-$_['db.username']          = DB_USERNAME;
-$_['db.password']          = DB_PASSWORD;
-$_['db.database']          = DB_DATABASE;
-$_['db.port']              = DB_PORT;
+$_['db.autostart']      = true;
+$_['db.type']           = DB_DRIVER; // mpdo, mssql, mysql, mysqli or postgre
+$_['db.hostname']       = DB_HOSTNAME;
+$_['db.username']       = DB_USERNAME;
+$_['db.password']       = DB_PASSWORD;
+$_['db.database']       = DB_DATABASE;
+$_['db.port']           = DB_PORT;
 
 // Autoload Libraries
 $_['library.autoload'] = array(
@@ -33,3 +33,7 @@ $_['action.pre_action'] = array(
 	'action/login',
 	'action/permission'
 );
+
+
+// Actions
+$_['action.default']       = 'common/dashboard';

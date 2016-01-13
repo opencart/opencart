1a243921ae3c22a7094d8b7b9f87f18c3220b14d
Daniel Kerr <webmaster@opencart.com>
2016-01-14 01:12:50 +0800

    events fix

--- upload/system/engine/loader.php
+++ upload/system/engine/loader.php
@@ -126,7 +126,7 @@ final class Loader {
 	protected function callback($registry, $route) {
 		return function($args) use($registry, &$route) {			
 			// Trigger the pre events
-			$result = $registry->get('event')->trigger('model/' . $route . '/before', array(&$route) + $args);
+			$result = $registry->get('event')->trigger('model/' . $route . '/before', array_merge(array(&$route), $args));
 			
 			if ($result) {
 				return $result;
@@ -151,7 +151,7 @@ final class Loader {
 			}
 													
 			// Trigger the post events
-			$result = $registry->get('event')->trigger('model/' . $route . '/after', array(&$route, &$output) + $args);
+			$result = $registry->get('event')->trigger('model/' . $route . '/after', array_merge(array(&$route, &$output), $args));
 			
 			if ($result) {
 				return $result;

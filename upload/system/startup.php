58d288bca4d5bdf3bed739f5e7103e40827cfcc9
James Allsup <james@allsup.eu>
2016-01-14 14:49:42 +0000

    Changed install instructions
    Updated vendor folder location.

--- upload/system/startup.php
+++ upload/system/startup.php
@@ -85,8 +85,8 @@ function modification($filename) {
 
 // Autoloader
 
-if (file_exists(DIR_SYSTEM .' vendor/autoload.php')) {
-	require_once(DIR_SYSTEM . 'vendor/autoload.php');
+if (file_exists(DIR_SYSTEM . '../../vendor/autoload.php')) {
+	require_once(DIR_SYSTEM . '../../vendor/autoload.php');
 }
 
 function library($class) {

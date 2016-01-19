34b54daaf6bdf4d2e01d693b12205cf6b1aa81eb
Danijel Gombač <info.gombac@gmail.com>
2016-01-19 14:24:19 +0100

    Update router.php

--- upload/catalog/controller/startup/router.php
+++ upload/catalog/controller/startup/router.php
@@ -2,7 +2,7 @@
 class ControllerStartupRouter extends Controller {
 	public function index() {
 		// Route
-		if (isset($this->request->get['route']) && $this->request->get['route'] != 'action/router') {
+		if (isset($this->request->get['route']) && $this->request->get['route'] != 'startup/router') {
 			$route = $this->request->get['route'];
 		} else {
 			$route = $this->config->get('action_default');

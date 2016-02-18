b8a79c422b5b4ad8fd17b60949d71301e65252d0
Ekrem KAYA <webmaster@e-piksel.com>
2016-02-18 14:20:46 +0200

    Added customer password reset

--- upload/catalog/controller/account/forgotten.php
+++ upload/catalog/controller/account/forgotten.php
@@ -16,15 +16,16 @@ class ControllerAccountForgotten extends Controller {
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
 			$this->load->language('mail/forgotten');
 
-			$password = substr(sha1(uniqid(mt_rand(), true)), 0, 10);
+			$code = token(40);
 
-			$this->model_account_customer->editPassword($this->request->post['email'], $password);
+			$this->model_account_customer->editCode($this->request->post['email'], $code);
 
 			$subject = sprintf($this->language->get('text_subject'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
 
 			$message  = sprintf($this->language->get('text_greeting'), html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8')) . "\n\n";
-			$message .= $this->language->get('text_password') . "\n\n";
-			$message .= $password;
+			$message .= $this->language->get('text_change') . "\n\n";
+			$message .= $this->url->link('account/reset', 'code=' . $code, true) . "\n\n";
+			$message .= sprintf($this->language->get('text_ip'), $this->request->server['REMOTE_ADDR']) . "\n\n";
 
 			$mail = new Mail();
 			$mail->protocol = $this->config->get('config_mail_protocol');
@@ -38,8 +39,8 @@ class ControllerAccountForgotten extends Controller {
 			$mail->setTo($this->request->post['email']);
 			$mail->setFrom($this->config->get('config_email'));
 			$mail->setSender(html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8'));
-			$mail->setSubject($subject);
-			$mail->setText($message);
+			$mail->setSubject(html_entity_decode($subject, ENT_QUOTES, 'UTF-8'));
+			$mail->setText(html_entity_decode($message, ENT_QUOTES, 'UTF-8'));
 			$mail->send();
 
 			$this->session->data['success'] = $this->language->get('text_success');
@@ -98,6 +99,12 @@ class ControllerAccountForgotten extends Controller {
 
 		$data['back'] = $this->url->link('account/login', '', true);
 
+		if (isset($this->request->post['email'])) {
+			$data['email'] = $this->request->post['email'];
+		} else {
+			$data['email'] = '';
+		}
+
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['column_right'] = $this->load->controller('common/column_right');
 		$data['content_top'] = $this->load->controller('common/content_top');
@@ -118,7 +125,7 @@ class ControllerAccountForgotten extends Controller {
 		$customer_info = $this->model_account_customer->getCustomerByEmail($this->request->post['email']);
 
 		if ($customer_info && !$customer_info['approved']) {
-		    $this->error['warning'] = $this->language->get('error_approved');
+			$this->error['warning'] = $this->language->get('error_approved');
 		}
 
 		return !$this->error;

e456d70a4c94297d9b1532ce122bb98510a1b82e
Ekrem KAYA <webmaster@e-piksel.com>
2016-02-18 14:32:14 +0200

    Fixed language

--- upload/catalog/language/en-gb/account/forgotten.php
+++ upload/catalog/language/en-gb/account/forgotten.php
@@ -7,7 +7,7 @@ $_['text_account']    = 'Account';
 $_['text_forgotten']  = 'Forgotten Password';
 $_['text_your_email'] = 'Your E-Mail Address';
 $_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have a password reset link e-mailed to you.';
-$_['text_success']    = 'An email with a confirmation link has been sent your admin email address.';
+$_['text_success']    = 'An email with a confirmation link has been sent your email address.';
 
 // Entry
 $_['entry_email']     = 'E-Mail Address';

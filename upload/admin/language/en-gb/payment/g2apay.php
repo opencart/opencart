5d5a113f4b4f75379d02c31367d8c3b3db4b6bbf
James Allsup <james@allsup.eu>
2016-02-22 22:26:05 +0000

    Remove duplicate language string close #3992

--- upload/admin/language/en-gb/payment/g2apay.php
+++ upload/admin/language/en-gb/payment/g2apay.php
@@ -7,7 +7,19 @@ $_['text_payment']				 		 = 'Payment';
 $_['text_success']				 		 = 'Success: You have modified the G2APay details.';
 $_['text_edit']					 		 = 'Edit G2APay';
 $_['text_g2apay']				 		 = '<a href="https://pay.g2a.com/" target="_blank"><img src="view/image/payment/g2apay.png" alt="G2APay" title="G2APay" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_payment_info']			 		 = 'Payment information';
+$_['text_refund_status']		 		 = 'Payment refund';
+$_['text_order_ref']			 		 = 'Order ref';
+$_['text_order_total']			 		 = 'Total authorised';
+$_['text_total_released']		 		 = 'Total released';
+$_['text_transactions']			 		 = 'Transactions';
+$_['text_column_amount']		 		 = 'Amount';
+$_['text_column_type']			 		 = 'Type';
+$_['text_column_date_added']	 		 = 'Added';
+$_['text_refund_ok']			 		 = 'Refund was successfully requested';
+$_['text_refund_ok_order']		 		 = 'Refund was successfully requested, amount fully refunded';
 
+// Entry
 $_['entry_username']			 		 = 'Username';
 $_['entry_secret']				 		 = 'Secret';
 $_['entry_api_hash']		     		 = 'API Hash';
@@ -15,12 +27,10 @@ $_['entry_environment']			 		 = 'Environment';
 $_['entry_secret_token']		 		 = 'Secret Token';
 $_['entry_ipn_url']				 		 = 'IPN URL:';
 $_['entry_total']				 		 = 'Total';
-
 $_['entry_geo_zone']			 		 = 'Geo Zone';
 $_['entry_status']				 		 = 'Status';
 $_['entry_sort_order']			 		 = 'Sort Order';
 $_['entry_debug']				 		 = 'Debug logging';
-
 $_['entry_order_status']				 = 'Order Status';
 $_['entry_complete_status']				 = 'Complete Status:';
 $_['entry_rejected_status']				 = 'Rejected Status:';
@@ -44,20 +54,6 @@ $_['error_email']				 		 = 'E-Mail Required!';
 $_['error_secret']				 		 = 'Secret Required!';
 $_['error_api_hash']			 		 = 'API Hash Required!';
 $_['entry_status']				 		 = 'Status';
-$_['entry_order_status']		 		 = 'Order Status';
-
-// Order page - payment tab
-$_['text_payment_info']			 		 = 'Payment information';
-$_['text_refund_status']		 		 = 'Payment refund';
-$_['text_order_ref']			 		 = 'Order ref';
-$_['text_order_total']			 		 = 'Total authorised';
-$_['text_total_released']		 		 = 'Total released';
-$_['text_transactions']			 		 = 'Transactions';
-$_['text_column_amount']		 		 = 'Amount';
-$_['text_column_type']			 		 = 'Type';
-$_['text_column_date_added']	 		 = 'Added';
-$_['text_refund_ok']			 		 = 'Refund was successfully requested';
-$_['text_refund_ok_order']		 		 = 'Refund was successfully requested, amount fully refunded';
 
 //Button
 $_['btn_refund']				 		 = 'refund';

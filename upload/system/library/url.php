bf30812a9b6f0041f64134c198782ceaabd252b5
yuriygr <a1d516ac5f5d290@gmail.com>
2016-03-02 17:00:23 +0500

    To create a link without a host.

--- upload/system/library/url.php
+++ upload/system/library/url.php
@@ -32,4 +32,22 @@ class Url {
 
 		return $url;
 	}
+	
+	public function link($route, $args = '', $secure = false) {
+		$url = 'index.php?route=' . $route;
+		
+		if ($args) {
+			if (is_array($args)) {
+				$url .= '&amp;' . http_build_query($args);
+			} else {
+				$url .= str_replace('&', '&amp;', '&' . ltrim($args, '&'));
+			}
+		}
+
+		foreach ($this->rewrite as $rewrite) {
+			$url = $rewrite->rewrite($url);
+		}
+
+		return $url;
+	}
 }

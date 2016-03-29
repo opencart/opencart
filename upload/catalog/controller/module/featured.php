ea6b8fa1d0160e95c11caa2e909bf18c359f0889
awmaito <andre.maito@terra.com.br>
2016-03-29 15:39:01 -0300

    Change heading_title variable according to setting
    Now that OC supports more than one instance of the module in the same layout, we can have different titles to each one. Just change
    
    $data['heading_title'] = $this->language->get('heading_title');
    
    to
    
    $data['heading_title'] = $setting['name'];
    
    This could be applied to other modules too..

--- upload/catalog/controller/module/featured.php
+++ upload/catalog/controller/module/featured.php
@@ -3,7 +3,7 @@ class ControllerModuleFeatured extends Controller {
 	public function index($setting) {
 		$this->load->language('module/featured');
 
-		$data['heading_title'] = $this->language->get('heading_title');
+		$data['heading_title'] = $setting['name'];
 
 		$data['text_tax'] = $this->language->get('text_tax');
 

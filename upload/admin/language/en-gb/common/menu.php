80b0e7609b52c8c42e544d782046a3a0cb91f5c5
Daniel Kerr <webmaster@opencart.com>
2016-04-17 17:55:43 +0800

    added missing language entry

--- upload/admin/language/en-gb/common/menu.php
+++ upload/admin/language/en-gb/common/menu.php
@@ -63,6 +63,7 @@ $_['text_tax']                         = 'Taxes';
 $_['text_tax_class']                   = 'Tax Classes';
 $_['text_tax_rate']                    = 'Tax Rates';
 $_['text_translation']                 = 'Translations';
+$_['text_theme']                       = 'Theme Editor';
 $_['text_tools']                       = 'Tools';
 $_['text_upload']                      = 'Uploads';
 $_['text_user']                        = 'Users';

a4ba067cbcecff6c7680e348a0cf93170cfbd20b
Daniel Kerr <webmaster@opencart.com>
2016-05-05 14:53:30 +0800

    more event work

--- upload/admin/controller/startup/event.php
+++ upload/admin/controller/startup/event.php
@@ -1,19 +1,15 @@
 <?php
 class ControllerStartupEvent extends Controller {
 	public function index() {
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "event` WHERE `trigger` LIKE 'admin/%' AND status = '1' ORDER BY `event_id` ASC");
-		
-		foreach ($query->rows as $result) {
-			$this->event->register(substr($result['trigger'], strpos($result['trigger'], '/') + 1), new Action($result['action']));
-		}
-		
 		// Add events from the DB
 		$this->load->model('extension/event');
 		
 		$results = $this->model_extension_event->getEvents();
 		
 		foreach ($results as $result) {
-			$this->event->register(substr($result['trigger'], strpos($result['trigger'], '/') + 1), new Action($result['action']));
+			if ((substr($result['trigger'], 0, 6) == 'admin/') && $result['status']) {
+				$this->event->register(substr($result['trigger'], 6), new Action($result['action']));
+			}
 		}		
 	}
 }

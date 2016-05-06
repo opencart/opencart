f62866a7710b5c7e8cc1155ffcc173a3ecd783bd
maks feltrin <pine3ree@gmail.com>
2016-05-06 13:48:03 +0200

    memcache entries deletion
    Deletion from memcached server should have the same effect of the file cache adapter.
    when i use `$this->cache->delete('product')`, this call will delete all cache files with basename starting with 'product'. The same should happen with memcache adapter, but in the current imlementation it only deletes the CACHE_PREFIX . 'product' key

--- upload/system/library/cache/mem.php
+++ upload/system/library/cache/mem.php
@@ -15,11 +15,28 @@ class Mem {
 		return $this->cache->get(CACHE_PREFIX . $key);
 	}
 
-	public function set($key,$value) {
+	public function set($key, $value) {
 		return $this->cache->set(CACHE_PREFIX . $key, $value, MEMCACHE_COMPRESSED, $this->expire);
 	}
 
 	public function delete($key) {
-		$this->cache->delete(CACHE_PREFIX . $key);
+		$all_slabs = $this->memcache->getExtendedStats('slabs');
+		foreach ($all_slabs as $server => $slabs) {
+			foreach ($slabs as $slab_id => $slab_meta) {
+				if (!is_int($slab_id)) {
+					continue;
+				}
+				$cachedump = $this->memcache->getExtendedStats('cachedump', $slab_id, 1000000);
+				foreach($cachedump as $server => $entries) {
+					if (!empty($entries) && is_array($entries)) {
+						foreach(array_keys($entries) as $entry_key) {
+							if (strpos($entry_key, CACHE_PREFIX . $key) === 0) {
+								$this->cache->delete($entry_key);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }

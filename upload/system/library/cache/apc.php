b5b1373b2d441be40722224207ca8c6d53d81f06
maks feltrin <pine3ree@gmail.com>
2016-05-07 19:09:04 +0200

    check if apc is enabled

--- upload/system/library/cache/apc.php
+++ upload/system/library/cache/apc.php
@@ -2,20 +2,28 @@
 namespace Cache;
 class APC {
 	private $expire;
+	private $active = false;
 
 	public function __construct($expire) {
 		$this->expire = $expire;
+		if (function_exists('apc_cache_info')) {
+			$this->active = true;
+		}
 	}
 
 	public function get($key) {
-		return apc_fetch(CACHE_PREFIX . $key);
+		return $this->active ? apc_fetch(CACHE_PREFIX . $key) : false;
 	}
 
 	public function set($key, $value) {
-		return apc_store(CACHE_PREFIX . $key, $value, $this->expire);
+		return $this->active ? apc_store(CACHE_PREFIX . $key, $value, $this->expire) : false;
 	}
 
 	public function delete($key) {
-		apc_delete(CACHE_PREFIX . $key);
+		if (!$this->active) {
+			return false;
+		}
+		
+		return apc_delete(CACHE_PREFIX . $key);
 	}
 }

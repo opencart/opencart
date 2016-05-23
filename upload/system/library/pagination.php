f0a53f36821839cf035e6e654e33adfb499c02d4
florinsith <flo@shop-carts.net>
2016-05-23 18:49:10 +0300

    Pagination page 1 issue
    It occurs when seo urls are on and there is no other url parameter aside from page (&page is ?page)

--- upload/system/library/pagination.php
+++ upload/system/library/pagination.php
@@ -33,9 +33,9 @@ class Pagination {
 		$output = '<ul class="pagination">';
 
 		if ($page > 1) {
-			$output .= '<li><a href="' . str_replace('&amp;page={page}', '', $this->url) . '">' . $this->text_first . '</a></li>';
+			$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '?page={page}'), '', $this->url) . '">' . $this->text_first . '</a></li>';
 			if($page - 1 === 1){
-				$output .= '<li><a href="' . str_replace('&amp;page={page}', '', $this->url) . '">' . $this->text_prev . '</a></li>';
+				$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '?page={page}'), '', $this->url) . '">' . $this->text_prev . '</a></li>';
 			} else {
 				$output .= '<li><a href="' . str_replace('{page}', $page - 1, $this->url) . '">' . $this->text_prev . '</a></li>';
 			}
@@ -64,7 +64,7 @@ class Pagination {
 				if ($page == $i) {
 					$output .= '<li class="active"><span>' . $i . '</span></li>';
 				} else {if($i === 1){
-					$output .= '<li><a href="' . str_replace('&amp;page={page}', '', $this->url) . '">' . $i . '</a></li>';
+					$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '?page={page}'), '', $this->url) . '">' . $i . '</a></li>';
 				} else {
 					$output .= '<li><a href="' . str_replace('{page}', $i, $this->url) . '">' . $i . '</a></li>';
 				}

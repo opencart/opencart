69b1352634194d49adf686e71354d33fda6a91b9
David Teal <david@welfordmedia.co.uk>
2016-06-06 11:03:23 +0100

    Adding method to tax class unset tax_rates

--- upload/system/library/cart/tax.php
+++ upload/system/library/cart/tax.php
@@ -9,6 +9,10 @@ final class Tax {
 		$this->session = $registry->get('session');
 	}
 
+    public function unsetRates() {
+        $this->tax_rates = array();
+    }
+
 	public function setShippingAddress($country_id, $zone_id) {
 		$tax_query = $this->db->query("SELECT tr1.tax_class_id, tr2.tax_rate_id, tr2.name, tr2.rate, tr2.type, tr1.priority FROM " . DB_PREFIX . "tax_rule tr1 LEFT JOIN " . DB_PREFIX . "tax_rate tr2 ON (tr1.tax_rate_id = tr2.tax_rate_id) INNER JOIN " . DB_PREFIX . "tax_rate_to_customer_group tr2cg ON (tr2.tax_rate_id = tr2cg.tax_rate_id) LEFT JOIN " . DB_PREFIX . "zone_to_geo_zone z2gz ON (tr2.geo_zone_id = z2gz.geo_zone_id) LEFT JOIN " . DB_PREFIX . "geo_zone gz ON (tr2.geo_zone_id = gz.geo_zone_id) WHERE tr1.based = 'shipping' AND tr2cg.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND z2gz.country_id = '" . (int)$country_id . "' AND (z2gz.zone_id = '0' OR z2gz.zone_id = '" . (int)$zone_id . "') ORDER BY tr1.priority ASC");
 

30b98c87b1964eaf1b80a61e1af5382af6563611
Chris <chris@welfordmedia.co.uk>
2016-06-10 16:12:23 +0100

    LPA - coding standards

--- upload/catalog/controller/extension/payment/amazon_login_pay.php
+++ upload/catalog/controller/extension/payment/amazon_login_pay.php
@@ -455,7 +455,7 @@ class ControllerPaymentAmazonLoginPay extends Controller {
 
 				$option_data[] = array(
 					'name' => $option['name'],
-					'value' => (utf8_strlen($value) > 20 ? utf8_substr($value, 0, 20) . '..' : $value)
+					'value' => ((utf8_strlen($value) > 20) ? utf8_substr($value, 0, 20) . '..' : $value)
 				);
 			}
 

45b413fffae82174e7070e3215dfd5231274f4ce
Daniel Kerr <webmaster@opencart.com>
2016-06-15 14:50:01 +0800

    https://github.com/opencart/opencart/issues/4151

--- upload/install/model/upgrade/1007.php
+++ upload/install/model/upgrade/1007.php
@@ -51,7 +51,7 @@ class ModelUpgrade1007 extends Model {
 		
 		$this->db->query("UPDATE `" . DB_PREFIX . "setting` SET `code` = 'theme_default', `key` = 'theme_default_image_compare_width' WHERE `code` = 'config' AND `key` = 'config_image_compare_width'");
 		$this->db->query("UPDATE `" . DB_PREFIX . "setting` SET `code` = 'theme_default', `key` = 'theme_default_image_compare_height' WHERE `code` = 'config' AND `key` = 'config_image_compare_height'");
-	
+			
 		// Paypal Express
 		$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "paypal_order' AND COLUMN_NAME = 'created'");
 
@@ -110,5 +110,20 @@ class ModelUpgrade1007 extends Model {
 			
 			$this->db->query("DROP TABLE `" . DB_PREFIX . "banner_image_description`");
 		}
+		
+		// Images
+        $this->db->query("UPDATE `" . DB_PREFIX . "banner_image` SET `image` =   REPLACE ( image , 'data/', 'catalog/' )");
+
+        $this->db->query("UPDATE `" . DB_PREFIX . "category` SET `image` =   REPLACE ( image , 'data/', 'catalog/' )");
+
+        $this->db->query("UPDATE `" . DB_PREFIX . "manufacturer` SET `image` =   REPLACE ( image , 'data/', 'catalog/' )");
+
+        $this->db->query("UPDATE `" . DB_PREFIX . "product` SET `image` =   REPLACE ( image , 'data/', 'catalog/' )");
+
+        $this->db->query("UPDATE `" . DB_PREFIX . "product_image` SET `image` =   REPLACE ( image , 'data/', 'catalog/' )");
+
+        $this->db->query("UPDATE `" . DB_PREFIX . "setting` SET `value` =  REPLACE ( value , 'data/', 'catalog/' ) WHERE `key` = 'config_logo'");
+
+        $this->db->query("UPDATE `" . DB_PREFIX . "setting` SET `value` =  REPLACE ( value , 'data/', 'catalog/' ) WHERE `key` = 'config_icon'");		
 	}
 }

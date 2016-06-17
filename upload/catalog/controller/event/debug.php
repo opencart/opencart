f7df2b2462cc8a42e1aaeca0736001f4960be395
Daniel Kerr <webmaster@opencart.com>
2016-06-17 14:50:43 +0800

    added some helpful debug code if required. will help detect which controllers are loading slow.

--- upload/catalog/controller/event/debug.php
+++ upload/catalog/controller/event/debug.php
@@ -1,14 +1,19 @@
 <?php
 class ControllerEventDebug extends Controller {
 	public function before(&$route, &$data) {
-		if ($route == '') {
-			$this->log->write(func_get_args());
-		}
+		$this->session->data['debug'][$route] = microtime();
 	}
 	
 	public function after(&$route, &$data, &$output) {
-		if ($route == '') {
-			$this->log->write(func_get_args());
-		}
+		//if ($route == '') {
+			if (isset($this->session->data['debug'][$route])) {
+				$data = array(
+					'route' => $route,
+					'time'  => microtime() - $this->session->data['debug'][$route]
+				);
+				
+				$this->log->write($data);
+			}
+		//}
 	}	
 }

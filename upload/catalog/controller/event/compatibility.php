089b1e6a95ec1ad34501bdc0be0c5acf861048fc
Daniel Kerr <webmaster@opencart.com>
2016-06-21 13:31:28 +0800

    more compatibility fixes

--- upload/catalog/controller/event/compatibility.php
+++ upload/catalog/controller/event/compatibility.php
@@ -1,6 +1,6 @@
 <?php
 class ControllerEventCompatibility extends Controller {
-	public function index(&$route) {
+	public function controller(&$route) {
 		$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
 		
 		$part = explode('/', $route);
@@ -21,4 +21,50 @@ class ControllerEventCompatibility extends Controller {
 			$route = $part[1] . '/' . $part[2];
 		}
 	}
+	
+	public function model(&$route) {
+		$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
+		
+		$part = explode('/', $route);
+		
+		$extension = array(
+			'extension/analytics',
+			'extension/captcha',
+			'extension/feed',
+			'extension/fraud',
+			'extension/module',
+			'extension/payment',
+			'extension/shipping',
+			'extension/theme',
+			'extension/total'
+		);
+		
+		if ((count($part) >= 3) && in_array($part[0] . '/' . $part[1], $extension) && !is_file(DIR_LANGUAGE . $this->config->get('config_language') . '/' . $route . '.php') && is_file(DIR_LANGUAGE . $this->config->get('config_language') . '/' . $part[1] . '/' . $part[2] . '.php')) {
+			$route = $part[1] . '/' . $part[2];
+		}
+	}	
+	
+	public function language(&$route) {
+		$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
+		
+		$part = explode('/', $route);
+		
+		$extension = array(
+			'extension/analytics',
+			'extension/captcha',
+			'extension/feed',
+			'extension/fraud',
+			'extension/module',
+			'extension/payment',
+			'extension/shipping',
+			'extension/theme',
+			'extension/total'
+		);
+		
+		if ((count($part) >= 3) && in_array($part[0] . '/' . $part[1], $extension) && !is_file(DIR_LANGUAGE . $this->config->get('config_language') . '/' . $route . '.php') && is_file(DIR_LANGUAGE . $this->config->get('config_language') . '/' . $part[1] . '/' . $part[2] . '.php')) {
+			$route = $part[1] . '/' . $part[2];
+		}
+	}
+	
+
 }

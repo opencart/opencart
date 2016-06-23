70aa7da933c9f7755c93fa9136099851fbf4b310
p0v1n0m <p0v1n0m@gmail.com>
2016-06-23 13:36:11 +0200

    Alert mail settings in one place

--- upload/catalog/model/affiliate/affiliate.php
+++ upload/catalog/model/affiliate/affiliate.php
@@ -39,7 +39,7 @@ class ModelAffiliateAffiliate extends Model {
 		$mail->send();
 
 		// Send to main admin email if new affiliate email is enabled
-		if ($this->config->get('config_affiliate_mail')) {
+		if (in_array('affiliate', (array)$this->config->get('config_mail_alert'))) {
 			$message  = $this->language->get('text_signup') . "\n\n";
 			$message .= $this->language->get('text_store') . ' ' . html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8') . "\n";
 			$message .= $this->language->get('text_firstname') . ' ' . $data['firstname'] . "\n";
@@ -62,7 +62,7 @@ class ModelAffiliateAffiliate extends Model {
 			$mail->send();
 
 			// Send to additional alert emails if new affiliate email is enabled
-			$emails = explode(',', $this->config->get('config_mail_alert'));
+			$emails = explode(',', $this->config->get('config_alert_email'));
 
 			foreach ($emails as $email) {
 				if (utf8_strlen($email) > 0 && filter_var($email, FILTER_VALIDATE_EMAIL)) {

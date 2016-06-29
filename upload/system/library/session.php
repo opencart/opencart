46fb638d0daaa11e341c7626b7293661b14a50f4
Daniel Kerr <webmaster@opencart.com>
2016-06-29 11:31:15 +0800

    more session changes

--- upload/system/library/session.php
+++ upload/system/library/session.php
@@ -7,6 +7,7 @@ class Session {
 	public function __construct($adaptor = 'native') {
 		$class = 'Session\\' . $adaptor;
 
+		// Pass-by-reference
 		$session_id = &$this->session_id;
 		
 		if (class_exists($class)) {
@@ -36,30 +37,39 @@ class Session {
 	}
 	
 	public function start($session_id = '') {
-		if (!session_id()) {
-			ini_set('session.use_only_cookies', 'Off');
-			ini_set('session.use_cookies', 'On');
-			ini_set('session.use_trans_sid', 'Off');
-			ini_set('session.cookie_httponly', 'On');
-							
-			if ($session_id) {
-				$this->session_id = $session_id;
-			} elseif (isset($_COOKIE[session_name()])) {
-				$this->session_id = $_COOKIE[session_name()];
-			} else {
-				$this->session_id = $this->createId();
-			}
-			
-			if (isset($_COOKIE[session_name()]) && !preg_match('/^[a-zA-Z0-9,\-]{22,52}$/', $_COOKIE[session_name()])) {
-				exit('Error: Invalid session ID!');
-			}	
-					
-			//echo $this->session_id;
-			
-			session_start();
-			
-			setcookie(session_name(), $this->session_id, 0);
+		ini_set('session.use_only_cookies', 'Off');
+		ini_set('session.use_cookies', 'On');
+		ini_set('session.use_trans_sid', 'Off');
+		ini_set('session.cookie_httponly', 'On');
+						
+		if ($session_id) {
+			$this->session_id = $session_id;
+		} elseif (isset($_COOKIE[session_name()])) {
+			$this->session_id = $_COOKIE[session_name()];
+		} else {
+			$this->session_id = $this->createId();
 		}
+		
+		if (isset($_COOKIE[session_name()]) && !preg_match('/^[a-zA-Z0-9,\-]{22,52}$/', $_COOKIE[session_name()])) {
+			exit('Error: Invalid session ID!');
+		}	
+				
+				INI_Get('session.cookie_lifetime')
+* INI_Get('session.cookie_path')
+* INI_Get('session.cookie_domain')
+* INI_Get('session.cookie_secure')
+* INI_Get('session.cookie_httponly')
+
+
+		session_set_cookie_params();
+		
+		//echo $this->session_id;
+		
+		//session_set_cookie_params(0, '/');
+		
+		session_start();
+		
+		//setcookie(session_name(), $this->session_id, 0);
 		
 		return true;
 	}	

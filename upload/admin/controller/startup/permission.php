f441ef49d562a610e0cf4b4343c1c5a9cdc52049
Daniel Kerr <webmaster@opencart.com>
2016-06-29 22:28:18 +0800

    https://github.com/opencart/opencart/issues/4582

--- upload/admin/controller/startup/permission.php
+++ upload/admin/controller/startup/permission.php
@@ -13,24 +13,27 @@ class ControllerStartupPermission extends Controller {
 			if (isset($part[1])) {
 				$route .= '/' . $part[1];
 			}
-
-			// If a 3rd part is found we need to check if its under one of the extension folders.
-			$routes = array(
-				'extension/analytics',
-				'extension/captcha',
-				'extension/feed',
-				'extension/fraud',
-				'extension/module',
-				'extension/payment',
-				'extension/shipping',
-				'extension/theme',
-				'extension/total'
-			);
 			
-			if (isset($part[2]) && (in_array(substr($this->request->get['route'], 0, 10), $routes))) {
-				$route .= '/' . $part[2];
+			if (isset($part[2])) {
+				// If a 3rd part is found we need to check if its under one of the extension folders.
+				$routes = array(
+					'extension/dashboard',
+					'extension/analytics',
+					'extension/captcha',
+					'extension/feed',
+					'extension/fraud',
+					'extension/module',
+					'extension/payment',
+					'extension/shipping',
+					'extension/theme',
+					'extension/total'
+				);
+			
+				if (in_array($part[0] . '/' . $part[1], $routes)) {
+					$route .= '/' . $part[2];
+				}
 			}
-			
+		
 			// We want to ingore some pages from having its permission checked. 
 			$ignore = array(
 				'common/dashboard',

6dafc6b60a285d3696d0b0b4d432f68f14c3aa09
Daniel Kerr <webmaster@opencart.com>
2016-07-01 15:17:20 +0800

    minimum php version 5.4 now. needed access to the native session handler.

--- upload/system/startup.php
+++ upload/system/startup.php
@@ -3,8 +3,8 @@
 error_reporting(E_ALL);
 
 // Check Version
-if (version_compare(phpversion(), '5.3.0', '<') == true) {
-	exit('PHP5.3+ Required');
+if (version_compare(phpversion(), '5.4.0', '<') == true) {
+	exit('PHP5.4+ Required');
 }
 
 // Magic Quotes Fix

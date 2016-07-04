f8f4f620888d4f1a64257250bb57c96437f2106e
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-07-04 16:46:47 +0300

    Update ebay.php

--- upload/catalog/controller/extension/openbay/ebay.php
+++ upload/catalog/controller/extension/openbay/ebay.php
@@ -1,14 +1,15 @@
 <?php
 class ControllerExtensionOpenbayEbay extends Controller {
 	public function eventAddOrder($route, $data) {
-
 	}
-
-	public function eventAddOrderHistory($route, $order_id, $order_status_id, $comment = '', $notify = false, $override = false) {
-		if (!empty($order_id)) {
+	public function eventAddOrderHistory($route, $data) {
+		$this->openbay->ebay->log('eventAddOrderHistory Event fired: ' . $route);
+		
+		if (isset($data[0]) && !empty($data[0])) {
 			$this->load->model('extension/openbay/ebay_order');
-
-			$this->model_extension_openbay_ebay_order->addOrderHistory($order_id);
+			
+			$this->openbay->ebay->log('Order ID: ' . (int)$data[0]);
+			$this->model_extension_openbay_ebay_order->addOrderHistory((int)$data[0]);
 		}
 	}
 }

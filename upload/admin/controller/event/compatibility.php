8c4100b34c3c88182fb84ebc01de220b9d40fd64
Daniel Kerr <webmaster@opencart.com>
2016-07-05 15:18:14 +0800

    more compatibility fixes

--- upload/admin/controller/event/compatibility.php
+++ upload/admin/controller/event/compatibility.php
@@ -23,49 +23,10 @@ class ControllerEventCompatibility extends Controller {
 	}
 	
 	public function view(&$route, &$data) {
-		$part = explode('/', $route);	
-		//echo  $route;
-		if (isset($data['back'])) {
+		$part = explode('/', $route);
 			
+		if (isset($part[0]) && isset($data['back'])) {
+			$data['back'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=' . $part[0], true);
 		}
-		
-		/*
-		// The below code will old extensions compatible with the extension page move 
-		if (isset($this->request->get['route']) && ($this->request->get['route'] != 'extension/extension')) {
-			$url_info = parse_url(str_replace('&amp;', '&', $link));
-			
-			$data = array();
-
-			parse_str($url_info['query'], $data);
-		
-			
-		
-			$routes = array(
-				'extension/analytics',
-				'extension/captcha',
-				'extension/feed',
-				'extension/fraud',
-				'extension/module',
-				'extension/payment',
-				'extension/shipping',
-				'extension/theme',
-				'extension/total'
-			);
-			
-			if (in_array($part[0] . '/' . $part[1], $routes)) {
-				$query  = '?route=extension/extension&type=' . $part[1];
-				
-				unset($data['route']);
-				
-				$query .= '&' . http_build_query($data);
-			
-				return $url_info['scheme'] . '://' . $url_info['host'] . (isset($url_info['port']) ? ':' . $url_info['port'] : '') . $url_info['path'] . $query;
-			} else {
-				return $link;
-			}
-		} else {
-			return $link;
-		}
-		*/
 	}
 }

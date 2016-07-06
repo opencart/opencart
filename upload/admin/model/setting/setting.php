dbd007dbb9083e961ee9026e7403aa1eda2d9167
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-07-06 12:12:44 +0300

    Update setting.php

--- upload/admin/model/setting/setting.php
+++ upload/admin/model/setting/setting.php
@@ -2,9 +2,7 @@
 class ModelSettingSetting extends Model {
 	public function getSetting($code, $store_id = 0) {
 		$setting_data = array();
-
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE store_id = '" . (int)$store_id . "' AND `code` = '" . $this->db->escape($code) . "'");
-
 		foreach ($query->rows as $result) {
 			if (!$result['serialized']) {
 				$setting_data[$result['key']] = $result['value'];
@@ -12,13 +10,10 @@ class ModelSettingSetting extends Model {
 				$setting_data[$result['key']] = json_decode($result['value'], true);
 			}
 		}
-
 		return $setting_data;
 	}
-
 	public function editSetting($code, $data, $store_id = 0) {
 		$this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE store_id = '" . (int)$store_id . "' AND `code` = '" . $this->db->escape($code) . "'");
-
 		foreach ($data as $key => $value) {
 			if (substr($key, 0, strlen($code)) == $code) {
 				if (!is_array($value)) {
@@ -29,14 +24,12 @@ class ModelSettingSetting extends Model {
 			}
 		}
 	}
-
 	public function deleteSetting($code, $store_id = 0) {
 		$this->db->query("DELETE FROM " . DB_PREFIX . "setting WHERE store_id = '" . (int)$store_id . "' AND `code` = '" . $this->db->escape($code) . "'");
 	}
 	
 	public function getSettingValue($key, $store_id = 0) {
 		$query = $this->db->query("SELECT value FROM " . DB_PREFIX . "setting WHERE store_id = '" . (int)$store_id . "' AND `key` = '" . $this->db->escape($key) . "'");
-
 		if ($query->num_rows) {
 			return $query->row['value'];
 		} else {
@@ -50,42 +43,5 @@ class ModelSettingSetting extends Model {
 		} else {
 			$this->db->query("UPDATE " . DB_PREFIX . "setting SET `value` = '" . $this->db->escape(json_encode($value)) . "', serialized = '1' WHERE `code` = '" . $this->db->escape($code) . "' AND `key` = '" . $this->db->escape($key) . "' AND store_id = '" . (int)$store_id . "'");
 		}
-	}
-
-	public function getTimezones() {
-		// The list of available timezone groups to use.
-		$use_zones = array('Africa', 'America', 'Antarctica', 'Arctic', 'Asia', 'Atlantic', 'Australia', 'Europe', 'Indian', 'Pacific');
-
-		// Get the list of time zones from the server.
-		$zones = DateTimeZone::listIdentifiers();
-
-		// Build the group lists.
-		foreach ($zones as $zone) {
-			// Time zones not in a group we will ignore.
-			if (strpos($zone, '/') === false) {
-				continue;
-			}
-
-			// Get the group/locale from the timezone.
-			list ($group, $locale) = explode('/', $zone, 2);
-
-			// Only use known groups.
-			if (in_array($group, $use_zones)) {
-				// Initialize the group if necessary.
-				if (!isset($groups[$group])) {
-					$groups[$group] = array();
-				}
-
-				// Only add options where a locale exists.
-				if (!empty($locale)) {
-					$groups[$group][$zone] = str_replace('_', ' ', $locale);
-				}
-			}
-		}
-
-		// Sort the group lists.
-		ksort($groups);
-
-		return $groups;
 	}
 }

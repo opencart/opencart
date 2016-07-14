1fd54798f7641f15941295dcf27e159ecc21cc4a
prhost <prhost@prhost.com.br>
2016-07-14 00:41:42 -0300

    Instantiating the full namespace of the template engine and passing the registry into the render

--- upload/system/library/template.php
+++ upload/system/library/template.php
@@ -2,21 +2,21 @@
 class Template {
 	private $adaptor;
 
-  	public function __construct($adaptor) {
-	    $class = 'Template\\' . $adaptor;
+  	public function __construct($class) {
 
 		if (class_exists($class)) {
 			$this->adaptor = new $class();
 		} else {
-			throw new \Exception('Error: Could not load template adaptor ' . $adaptor . '!');
+			throw new \Exception('Error: Could not load template class ' . $class . '!');
 		}
+
 	}
 
 	public function set($key, $value) {
 		$this->adaptor->set($key, $value);
 	}
 
-	public function render($template) {
-		return $this->adaptor->render($template);
+	public function render($template, $registry) {
+		return $this->adaptor->render($template, $registry);
 	}
 }

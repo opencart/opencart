9d19384b54a326af4968b7207fa1847210ac9fb7
prhost <prhost@prhost.com.br>
2016-07-14 00:40:02 -0300

    Modifications and improvements to the template engine of twig like syntax error handling, event trigger added, removed register because it was deprecated

--- upload/system/library/template/twig.php
+++ upload/system/library/template/twig.php
@@ -1,35 +1,43 @@
 <?php
 namespace Template;
-final class Twig {
-	private $twig;
-	private $data = array();
-	
-	public function __construct() {
-		// include and register Twig auto-loader
-		include_once DIR_SYSTEM . 'library/template/Twig/Autoloader.php';
-		
-		\Twig_Autoloader::register();	
-		
-		// specify where to look for templates
-		$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);	
-		
-		// initialize Twig environment
-		$this->twig = new \Twig_Environment($loader, array('autoescape' => false));			
-	}	
-	
-	public function set($key, $value) {
-		$this->data[$key] = $value;
-	}
-	
-	public function render($template) {
-		try {
-			// load template
-			$template = $this->twig->loadTemplate($template);
-			
-			return $template->render($this->data);
-		} catch (Exception $e) {
-			trigger_error('Error: Could not load template ' . $template . '!');
-			exit();	
-		}	
-	}	
+
+final class Twig
+{
+    private $data = array();
+
+    public function set($key, $value)
+    {
+        $this->data[$key] = $value;
+    }
+
+    public function render($template, $registry)
+    {
+        try {
+
+            $paths = array(
+                DIR_TEMPLATE,
+            );
+
+            $fileSystem = new \Twig_Loader_Filesystem($paths);
+
+            //Set configs
+            $twig = new \Twig_Environment($fileSystem, array(
+                'autoescape' => false
+            ));
+
+            $registry->get('event')->trigger('library/template/twig/before', array(&$twig));
+
+            if ($fileSystem->exists($template)) {
+                $output = $twig->render($template, $this->data);
+            } else {
+                $template = str_replace('.tpl', '', $template) . '.twig';
+                $output = $twig->render($template, $this->data);
+            }
+
+            return $output;
+
+        } catch (\Twig_Error_Syntax $e) {
+            trigger_error('Error: ' . $e->getMessage());
+        }
+    }
 }

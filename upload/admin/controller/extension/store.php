95b1ed3a4ed5694497842f4109e3f28ea2ce9589
Daniel Kerr <webmaster@opencart.com>
2016-07-21 23:51:44 +0800

    more extension store work

--- upload/admin/controller/extension/store.php
+++ upload/admin/controller/extension/store.php
@@ -14,25 +14,12 @@ class ControllerExtensionStore extends Controller {
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'], true)
+			'href' => $this->url->link('extension/store', 'token=' . $this->session->data['token'], true)
 		);
 		
 		$data['heading_title'] = $this->language->get('heading_title');
         
 		$data['text_list'] = $this->language->get('text_list');
-		$data['text_all'] = $this->language->get('text_all');
-		$data['text_license'] = $this->language->get('text_license');
-		$data['text_free'] = $this->language->get('text_free');
-		$data['text_paid'] = $this->language->get('text_paid');
-		$data['text_category'] = $this->language->get('text_category');
-		$data['text_theme'] = $this->language->get('text_theme');
-		$data['text_payment'] = $this->language->get('text_payment');
-		$data['text_shipping'] = $this->language->get('text_shipping');
-		$data['text_module'] = $this->language->get('text_module');
-		$data['text_total'] = $this->language->get('text_total');
-		$data['text_feed'] = $this->language->get('text_feed');
-		$data['text_report'] = $this->language->get('text_report');
-		$data['text_other'] = $this->language->get('text_other');
 
 		$data['token'] = $this->session->data['token'];
 
@@ -46,12 +33,22 @@ class ControllerExtensionStore extends Controller {
 	public function store() {
 		$this->load->language('extension/store');
 
-		$json = array();
-				
-		$url = '';
-		/*
-		$url  = '?api_key=' . $this->config->get('config_api_key'); 
+		$url = '?api_key=' . $this->config->get('config_api_key'); 
+		$url .= '&store=' . $this->request->server['HTTP_HOST'];
+		$url .= '&language=' . $this->config->get('config_language');
+		$url .= '&currency=' . $this->config->get('config_currency');
+		$url .= '&ssl=true';
 		
+		if (isset($this->request->get['call'])) {
+			$url .= '&route=' . $this->request->get['call'];
+		} else {
+			$url .= '&route=extension/extension';
+		}
+				
+		if (isset($this->request->get['extension_id'])) {
+			$url .= '&extension_id=' . $this->request->get['extension_id'];
+		}
+				
 		if (isset($this->request->get['sort'])) {
 			$url .= '&sort=' . $this->request->get['sort'];
 		}
@@ -71,9 +68,12 @@ class ControllerExtensionStore extends Controller {
 		if (isset($this->request->get['tags'])) {
 			$url .= '&tags=' . $this->request->get['tags'];
 		}		
-		*/
-		//echo HTTP_TEST;// . $url
-		$curl = curl_init(HTTP_TEST);
+		
+		//http://localhost/opencart-dev/upload/admin/index.php?route=extension/store&token=KGcMIrIZDOqsV1ux6g3qGbzEkEXenM8v&call=extension/extension/info&extension_id=26943
+		
+		echo $url;
+		
+		$curl = curl_init(HTTP_TEST . 'index.php' . $url);
 				
 		curl_setopt($curl, CURLOPT_PORT, 443);
 		//curl_setopt($curl, CURLOPT_HEADER, 0);
@@ -83,79 +83,16 @@ class ControllerExtensionStore extends Controller {
 		curl_setopt($curl, CURLOPT_FRESH_CONNECT, 1);
 		curl_setopt($curl, CURLOPT_POST, 1);
 		curl_setopt($curl, CURLOPT_PORT, 80);
-		//curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
 			
 		$response = curl_exec($curl);
 		
-		echo $response;
-		
-		/*
 		curl_close($curl);
-
-		if (!$response) {
-			$json['error'] = curl_error($curl) . '(' . curl_errno($curl) . ')';
-		} else {
-			$json = json_decode($response);
-		}		
 		
-		$this->response->addHeader('Content-Type: application/json');
-		$this->response->setOutput(json_encode($json));				
-		*/
+		$response = str_replace('{{ store }}', !$this->request->server['HTTPS'] ? HTTP_SERVER : HTTPS_SERVER, $response);
+		$response = str_replace('{{ token }}', $this->session->data['token'], $response);
+		
+		$this->response->setOutput($response);				
 	}
-	
-	public function info() {
-		$this->load->language('extension/store');
-
-		$json = array();
-				
-		$url = '';
-		
-		$curl = curl_init('https://extension.opencart.com');
-		
-		$request  = '?api_key=' . $this->config->get('config_api_key'); 
-		
-		if (isset($this->request->get['sort'])) {
-			$request .= '&sort=' . $this->request->get['sort'];
-		}
-
-		if (isset($this->request->get['order'])) {
-			$request .= '&order=' . $this->request->get['order'];
-		}
-
-		if (isset($this->request->get['page'])) {
-			$request .= '&page=' . $this->request->get['page'];
-		}
-
-		if (isset($this->request->get['search'])) {
-			$request .= '&search=' . $this->request->get['search'];
-		}		
-		
-		if (isset($this->request->get['tags'])) {
-			$request .= '&tags=' . $this->request->get['tags'];
-		}		
-				
-		curl_setopt($curl, CURLOPT_PORT, 443);
-		curl_setopt($curl, CURLOPT_HEADER, 0);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl, CURLOPT_FORBID_REUSE, 1);
-		curl_setopt($curl, CURLOPT_FRESH_CONNECT, 1);
-		curl_setopt($curl, CURLOPT_POST, 1);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
-				
-		$response = curl_exec($curl);
-
-		curl_close($curl);
-
-		if (!$response) {
-			$json['error'] = curl_error($curl) . '(' . curl_errno($curl) . ')';
-		} else {
-			$json = json_decode($response);
-		}
-		
-		$this->response->addHeader('Content-Type: application/json');
-		$this->response->setOutput(json_encode($json));		
-	}		
 	
 	public function download() {
 		$this->load->language('extension/store');

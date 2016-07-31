e95f8c7ff5c3b812fef40a7c03560e966ea42516
OpenCartAddons <joel@opencartaddons.com>
2016-07-31 10:35:04 -0400

    Update compatibility.php
    Added language directory fallback

--- upload/admin/controller/event/compatibility.php
+++ upload/admin/controller/event/compatibility.php
@@ -17,8 +17,13 @@ class ControllerEventCompatibility extends Controller {
 		// Compatibility code for old extension folders
 		$part = explode('/', $route);
 		
-		if (!is_file(DIR_LANGUAGE . $this->config->get('config_language') . '/' . $route . '.php') && is_file(DIR_LANGUAGE . $this->config->get('config_language') . '/' . $part[1] . '/' . $part[2] . '.php')) {
-			$route = $part[1] . '/' . $part[2];
+		$directories = array($this->config->get('config_language'), $this->language->default, 'english');
+		
+		foreach ($directories as $directory) {
+			if (!is_file(DIR_LANGUAGE . $directory . '/' . $route . '.php') && is_file(DIR_LANGUAGE . $directory . '/' . $part[1] . '/' . $part[2] . '.php')) {
+				$route = $part[1] . '/' . $part[2];
+				break;
+			}
 		}
 	}
 	

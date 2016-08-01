85e0f35ae96197e0aa92a4aedd3aad76eebd2961
James Allsup <james@allsup.eu>
2016-08-01 14:43:14 +0100

    2.3.0.2 tagged

--- tests/phpunit/bootstrap-dist.php
+++ tests/phpunit/bootstrap-dist.php
@@ -1,5 +1,5 @@
 <?php
-define('VERSION', '2.3.0.2_rc');
+define('VERSION', '2.3.0.2');
 
 define('ADMIN_USERNAME', '');
 define('ADMIN_PASSWORD', '');
--- upload/index.php
+++ upload/index.php
@@ -1,6 +1,6 @@
 <?php
 // Version
-define('VERSION', '2.3.0.3');
+define('VERSION', '2.3.0.2');
 
 // Configuration
 if (is_file('config.php')) {

631c92b9adeef50632d2f5d561e477ad0d8d21f5
Billy Noah <billynoah@gmail.com>
2016-08-25 16:46:04 -0400

    Update google_base.php
    Add feed products to $product_data array to prevent duplicates.

--- upload/catalog/controller/extension/feed/google_base.php
+++ upload/catalog/controller/extension/feed/google_base.php
@@ -29,6 +29,9 @@ class ControllerExtensionFeedGoogleBase extends Controller {
 
 				foreach ($products as $product) {
 					if (!in_array($product['product_id'], $product_data) && $product['description']) {
+						
+						$product_data[] = $product['product_id'];
+						
 						$output .= '<item>';
 						$output .= '<title><![CDATA[' . $product['name'] . ']]></title>';
 						$output .= '<link>' . $this->url->link('product/product', 'product_id=' . $product['product_id']) . '</link>';

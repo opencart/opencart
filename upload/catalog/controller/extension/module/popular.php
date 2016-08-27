6bf16041e8a7aa81ad910babf9dd96b54c688f41
ocshop <info@vilkov.info>
2016-08-27 23:24:52 +0300

    Популярные товары

---
+++ upload/catalog/controller/extension/module/popular.php
@@ -0,0 +1,96 @@
+<?php
+// *	@copyright	OPENCART.PRO 2011 - 2016.
+// *	@forum	http://forum.opencart.pro
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+class ControllerExtensionModulePopular extends Controller {
+	public function index($setting) {
+		$this->load->language('extension/module/popular');
+		
+		$data['heading_title'] = $this->language->get('heading_title');
+		
+		$data['text_tax'] = $this->language->get('text_tax');
+		
+		$data['button_cart'] = $this->language->get('button_cart');
+		$data['button_wishlist'] = $this->language->get('button_wishlist');
+		$data['button_compare'] = $this->language->get('button_compare');
+		
+		$this->load->model('catalog/product');
+		
+		$this->load->model('tool/image');
+		
+		$data['products'] = array();		
+
+		$product_data = array();
+		
+		if(isset($setting['limit']) && $setting['limit']!=''){
+		   $setting['limit'] = $setting['limit'];
+		}
+		else{
+  		   $setting['limit'] = 4;
+		}
+		
+		
+		$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY p.viewed DESC LIMIT " . (int)$setting['limit']);
+		
+		
+		
+		foreach ($query->rows as $result) { 		
+			$product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
+		}
+					 	 		
+		$results = $product_data;
+		
+		if ($results) {
+		foreach ($results as $result) {
+			if ($result['image']) {
+				$image = $this->model_tool_image->resize($result['image'], $setting['width'], $setting['height']);
+			} else {
+				$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
+			}
+			
+			
+			if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+					$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				} else {
+					$price = false;
+				}
+
+				if ((float)$result['special']) {
+					$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				} else {
+					$special = false;
+				}
+
+				if ($this->config->get('config_tax')) {
+					$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+				} else {
+					$tax = false;
+				}
+			
+			
+			if ($this->config->get('config_review_status')) {
+				$rating = $result['rating'];
+			} else {
+				$rating = false;
+			}
+							
+			$data['products'][] = array(
+				'product_id'   => $result['product_id'],
+				'thumb'   	   => $image,
+				'name'         => $result['name'],
+				'description'  => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
+				'price'   	   => $price,
+				'special' 	   => $special,
+				'tax'          => $tax,
+				'rating'       => $rating,
+				'href'         => $this->url->link('product/product', 'product_id=' . $result['product_id']),
+			);
+		}
+
+		return $this->load->view('extension/module/bestseller', $data);
+		
+	    }
+	}
+}
---
+++ upload/catalog/language/en-gb/extension/module/popular.php
@@ -0,0 +1,14 @@
+<?php
+// *	@copyright	OPENCART.PRO 2011 - 2016.
+// *	@forum	http://forum.opencart.pro
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Heading 
+$_['heading_title'] = 'Popular Products';
+
+// Text
+$_['text_reviews']  = 'Based on %s reviews.'; 
+
+// Tax
+$_['text_tax']      = 'Ex Tax:';

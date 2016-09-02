5476d34490919d1df4781b7bf3409fe3560deade
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-09-02 19:06:53 +0300

    Add files via upload

---
+++ upload/admin/language/en-gb/extension/module/slicebox.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']    = 'Slicebox';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified slicebox module!';
+$_['text_edit']        = 'Edit Slicebox Module';
+$_['text_type_default'] = 'Default settings';
+$_['text_horizontal_slice'] = 'Slideshow with play &amp; pause and horizontal slices';
+$_['text_random_orientation_slices'] = 'Random orientation and number of slices';
+$_['text_random_higher_disperse'] = 'Same like previous, but with a higher disperse factor';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_banner']     = 'Banner';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
+$_['entry_type']       = 'Type';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify slicebox module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';

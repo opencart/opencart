d01086f023b8231cb12bedc57a9e1077d0007274
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-09-04 23:36:23 +0300

    Create total.php

---
+++ upload/admin/language/en-gb/extension/extension/total.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']     = 'Order Totals';
+
+// Text
+$_['text_success']      = 'Success: You have modified totals!';
+$_['text_list']         = 'Order Total List';
+
+// Column
+$_['column_name']       = 'Order Totals';
+$_['column_status']     = 'Status';
+$_['column_sort_order'] = 'Sort Order';
+$_['column_action']     = 'Action';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify totals!';

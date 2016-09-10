6dd3f92e459c32df462e9c9b25f59250b9094190
ismailhkn <ismailcikla@gmail.com>
2016-09-10 07:29:12 +0300

    Update permission.php

--- upload/admin/controller/startup/permission.php
+++ upload/admin/controller/startup/permission.php
@@ -27,7 +27,7 @@ class ControllerStartupPermission extends Controller {
 				'extension/shipping',
 				'extension/theme',
 				'extension/total',
-				'extension/total'
+				'extension/report'
 			);
 
 			if (isset($part[2]) && in_array($route, $extension)) {

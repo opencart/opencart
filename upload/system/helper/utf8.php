720d4df9e5197b5fc9bd4d5d686c18cb09e6c185
ocshop <info@vilkov.info>
2016-09-24 00:31:52 +0300

    System @copyright	OPENCART.PRO

--- upload/system/helper/utf8.php
+++ upload/system/helper/utf8.php
@@ -1,4 +1,9 @@
 <?php
+// *	@copyright	OPENCART.PRO 2011 - 2016.
+// *	@forum	http://forum.opencart.pro
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
 if (extension_loaded('mbstring')) {
 	mb_internal_encoding('UTF-8');
 

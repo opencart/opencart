098aaacdbdb84b5200925a2918c11b6d3088879c
Qphoria <qphoria@gmail.com>
2016-10-03 10:28:58 -0500

    - Reverted Featured module changes - Remove Redundant delete for attributes

--- upload/catalog/controller/extension/module/featured.php
+++ upload/catalog/controller/extension/module/featured.php
@@ -25,49 +25,49 @@ class ControllerExtensionModuleFeatured extends Controller {
 			$products = array_slice($setting['product'], 0, (int)$setting['limit']);
 
 			foreach ($products as $product_id) {
-				$result = $this->model_catalog_product->getProduct($product_id);
+				$product_info = $this->model_catalog_product->getProduct($product_id);
 
-				if ($result) {
-					if ($result['image']) {
-						$image = $this->model_tool_image->resize($result['image'], $setting['width'], $setting['height']);
+				if ($product_info) {
+					if ($product_info['image']) {
+						$image = $this->model_tool_image->resize($product_info['image'], $setting['width'], $setting['height']);
 					} else {
 						$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
 					}
 
 					if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-						$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+						$price = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 					} else {
 						$price = false;
 					}
 
-					if ((float)$result['special']) {
-						$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+					if ((float)$product_info['special']) {
+						$special = $this->currency->format($this->tax->calculate($product_info['special'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 					} else {
 						$special = false;
 					}
 
 					if ($this->config->get('config_tax')) {
-						$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+						$tax = $this->currency->format((float)$product_info['special'] ? $product_info['special'] : $product_info['price'], $this->session->data['currency']);
 					} else {
 						$tax = false;
 					}
 
 					if ($this->config->get('config_review_status')) {
-						$rating = $result['rating'];
+						$rating = $product_info['rating'];
 					} else {
 						$rating = false;
 					}
 
 					$data['products'][] = array(
-						'product_id'  => $result['product_id'],
+						'product_id'  => $product_info['product_id'],
 						'thumb'       => $image,
-						'name'        => $result['name'],
-						'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
+						'name'        => $product_info['name'],
+						'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
 						'price'       => $price,
 						'special'     => $special,
 						'tax'         => $tax,
 						'rating'      => $rating,
-						'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'])
+						'href'        => $this->url->link('product/product', 'product_id=' . $product_info['product_id'])
 					);
 				}
 			}

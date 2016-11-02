d3203e99dff7ed9dd6c77fce8129ed0abf1d1f71
Daniel Kerr <webmaster@opencart.com>
2016-11-02 20:19:55 +0800

    compatibility fix

--- upload/system/config/admin.php
+++ upload/system/config/admin.php
@@ -30,35 +30,5 @@ $_['action_default'] = 'common/dashboard';
 
 // Action Events
 $_['action_event'] = array(
-    'view/*/before'                           => 'event/theme',
-	'controller/extension/analytics/*/before' => 'event/compatibility/controller',
-	'controller/extension/captcha/*/before'   => 'event/compatibility/controller',
-	'controller/extension/feed/*/before'      => 'event/compatibility/controller',
-	'controller/extension/fraud/*/before'     => 'event/compatibility/controller',
-	'controller/extension/module/*/before'    => 'event/compatibility/controller',
-	'controller/extension/payment/*/before'   => 'event/compatibility/controller',
-	'controller/extension/recurring/*/before' => 'event/compatibility/controller',
-	'controller/extension/shipping/*/before'  => 'event/compatibility/controller',
-	'controller/extension/theme/*/before'     => 'event/compatibility/controller',
-	'controller/extension/total/*/before'     => 'event/compatibility/controller',
-	'view/analytics/*/before'                 => 'event/compatibility/view',
-	'view/captcha/*/before'                   => 'event/compatibility/view',
-	'view/feed/*/before'                      => 'event/compatibility/view',
-	'view/fraud/*/before'                     => 'event/compatibility/view',
-	'view/module/*/before'                    => 'event/compatibility/view',
-	'view/payment/*/before'                   => 'event/compatibility/view',
-	'view/recurring/*/before'                 => 'event/compatibility/view',
-	'view/shipping/*/before'                  => 'event/compatibility/view',
-	'view/theme/*/before'                     => 'event/compatibility/view',
-	'view/total/*/before'                     => 'event/compatibility/view',
-	'language/extension/analytics/*/before'   => 'event/compatibility/language',
-	'language/extension/captcha/*/before'     => 'event/compatibility/language',
-	'language/extension/feed/*/before'        => 'event/compatibility/language',
-	'language/extension/fraud/*/before'       => 'event/compatibility/language',
-	'language/extension/module/*/before'      => 'event/compatibility/language',
-	'language/extension/payment/*/before'     => 'event/compatibility/language',
-	'language/extension/recurring/*/before'   => 'event/compatibility/language',
-	'language/extension/shipping/*/before'    => 'event/compatibility/language',
-	'language/extension/theme/*/before'       => 'event/compatibility/language',
-	'language/extension/total/*/before'       => 'event/compatibility/language'
+    'view/*/before' => 'event/theme'
 );

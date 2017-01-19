c38c275d742aac05faa5a76976fbe62950bf950b
James Allsup <james@allsup.eu>
2017-01-19 16:11:07 +0000

    Fix for script including

--- upload/catalog/controller/common/footer.php
+++ upload/catalog/controller/common/footer.php
@@ -72,7 +72,7 @@ class ControllerCommonFooter extends Controller {
 			$this->model_tool_online->addOnline($ip, $this->customer->getId(), $url, $referer);
 		}
 
-		$data['scripts'] = $this->document->getScripts();
+		$data['scripts'] = $this->document->getScripts('footer');
 		
 		return $this->load->view('common/footer', $data);
 	}

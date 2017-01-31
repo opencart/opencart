c2c54441a4e44ebf54fb08a3db13d370c21aa44b
ocshop <info@vilkov.info>
2017-01-31 04:13:32 +0200

    fix lang

--- upload/admin/language/ru-ru/report/customer_search.php
+++ upload/admin/language/ru-ru/report/customer_search.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']       = 'Customer Searches Report';
+$_['heading_title']       = 'Поисковые запросы покупателей';
 
 // Text
-$_['text_list']           = 'Customer Searches List';
-$_['text_guest']          = 'Guest';
+$_['text_list']           = 'Список поисковых запросов покупателей';
+$_['text_guest']          = 'Гость';
 $_['text_customer']       = '<a href="%s">%s</a>';
 
 // Column
-$_['column_keyword']      = 'Keyword';
-$_['column_products']     = 'Found Products';
-$_['column_category']     = 'Category';
-$_['column_customer']     = 'Customer';
+$_['column_keyword']      = 'Запрос';
+$_['column_products']     = 'Найденные товары';
+$_['column_category']     = 'Категория';
+$_['column_customer']     = 'Покупатель';
 $_['column_ip']           = 'IP';
-$_['column_date_added']   = 'Date Added';
+$_['column_date_added']   = 'Дата добовления';
 
 // Entry
-$_['entry_date_start']    = 'Date Start';
-$_['entry_date_end']      = 'Date End';
-$_['entry_keyword']       = 'Keyword';
-$_['entry_customer']      = 'Customer';
+$_['entry_date_start']    = 'Дата начала';
+$_['entry_date_end']      = 'Дата окончания';
+$_['entry_keyword']       = 'Запрос';
+$_['entry_customer']      = 'Покупатель';
 $_['entry_ip']            = 'IP';

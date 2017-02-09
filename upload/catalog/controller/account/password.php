2a148864d3fe572517592a9dea371016446cc5df
wazzzar <isakov.dmitriy@list.ru>
2017-02-10 00:28:26 +0300

    Update password.php

--- upload/catalog/controller/account/password.php
+++ upload/catalog/controller/account/password.php
@@ -113,16 +113,16 @@ class ControllerAccountPassword extends Controller {
 	}
 	
 	public function generate(){
-		if( isset($this->request->get['lenght']) ){
-			$lenght = (int)$this->request->get['lenght'];
+		if( isset($this->request->get['length']) ){
+			$length = (int)$this->request->get['length'];
 		}
-		echo $this->generator( $lenght );
+		echo $this->generator( $length );
 	}
 	
-	public function generator( $lenght = 12 ){
+	public function generator( $length = 12 ){
 		$arr = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','X','Y','Z','0','1','2','3','4','5','6','7','8','9'); // ,'.',',', '(',')','[',']','!','?', '&','^','%','@','*','$', '<','>','/','|','+','-', '{','}','`','~'
 		$pass = "";
-		for($i = 0; $i < $lenght; $i++){
+		for($i = 0; $i < $length; $i++){
 			$index = rand(0, count($arr) - 1);
 			$pass .= $arr[ $index ];
 		}

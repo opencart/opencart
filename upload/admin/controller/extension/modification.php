d527344e847d936c4dc4841b262a858a8255e477
Lispum <lispum@users.noreply.github.com>
2017-02-22 11:53:24 +0100

    Update modification.php - fix issue #5189
    Fixing issue #5189 where 
    > after refreshing modifications, the success message is not displayed.

--- upload/admin/controller/extension/modification.php
+++ upload/admin/controller/extension/modification.php
@@ -422,10 +422,7 @@ class ControllerExtensionModification extends Controller {
 			// Maintance mode back to original settings
 			$this->model_setting_setting->editSettingValue('config', 'config_maintenance', $maintenance);
 
-			// Do not return success message if refresh() was called with $data
-			if (!empty($data['redirect'])) {
-				$this->session->data['success'] = $this->language->get('text_success');
-			}
+			$this->session->data['success'] = $this->language->get('text_success');
 
 			$url = '';
 

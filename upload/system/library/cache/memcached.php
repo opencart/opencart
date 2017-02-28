a1932754434ce76225a60a4c2f533d8866a6cc4b
howdu <howdu@users.noreply.github.com>
2017-02-28 21:28:28 +0000

    Deletion from memcached server should have the same effect of the file cache adapter. More info: https://github.com/opencart/opencart/commit/f62866a7710b5c7e8cc1155ffcc173a3ecd783bd

--- upload/system/library/cache/memcached.php
+++ upload/system/library/cache/memcached.php
@@ -21,7 +21,26 @@ class Memcached {
 	}
 
 	public function delete($key) {
-		$this->memcached->delete(CACHE_PREFIX . $key);
+		// Deletion from memcached server should have the same effect of the file cache adapter
+		// More info https://github.com/opencart/opencart/commit/f62866a7710b5c7e8cc1155ffcc173a3ecd783bd
+		$all_slabs = $this->memcache->getExtendedStats('slabs');
+		foreach ($all_slabs as $server => $slabs) {
+			foreach ($slabs as $slab_id => $slab_meta) {
+				if (!is_int($slab_id)) {
+					continue;
+				}
+				$cachedump = $this->memcache->getExtendedStats('cachedump', $slab_id, self::CACHEDUMP_LIMIT);
+				foreach ($cachedump as $server => $entries) {
+					if (!empty($entries) && is_array($entries)) {
+						foreach (array_keys($entries) as $entry_key) {
+							if (strpos($entry_key, CACHE_PREFIX . $key) === 0) {
+								$this->memcache->delete($entry_key);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 	public function flush() {

9e6bf71367c74b21e79277870046821c8dde8cbd
Daniel Kerr <webmaster@opencart.com>
2017-03-27 14:11:30 +0800

    https://github.com/opencart/opencart/issues/5225

--- upload/system/engine/loader.php
+++ upload/system/engine/loader.php
@@ -10,25 +10,24 @@ final class Loader {
 		// Sanitize the call
 		$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
 		
-		$output = '';
+		$trigger = $route;
 		
 		// Trigger the pre events
-		$result = $this->registry->get('event')->trigger('controller/' . $route . '/before', array(&$route, &$data, &$output));
+		$result = $this->registry->get('event')->trigger('controller/' . $trigger . '/before', array($route, &$data));
+		
+		// Make sure its only the last event that returns an output if required.
+		if ($result) {
+			$output = $result;
+		} else {
+			$action = new Action($route);
+			$output = $action->execute($this->registry, array(&$data));			
+		}
+		
+		// Trigger the post events
+		$result = $this->registry->get('event')->trigger('controller/' . $trigger . '/after', array($route, &$data, &$output));
 		
 		if ($result) {
-			return $result;
-		}
-		
-		if (!$output) {
-			$action = new Action($route);
-			$output = $action->execute($this->registry, array(&$data));
-		}
-			
-		// Trigger the post events
-		$result = $this->registry->get('event')->trigger('controller/' . $route . '/after', array(&$route, &$data, &$output));
-		
-		if ($output instanceof Exception) {
-			return false;
+			$output = $result;
 		}
 
 		return $output;
@@ -47,6 +46,8 @@ final class Loader {
 	
 				$proxy = new Proxy();
 				
+				// Overriding models is a little harder so we have to use PHP's magic methods
+				// In future version we can use runkit
 				foreach (get_class_methods($class) as $method) {
 					$proxy->{$method} = $this->callback($this->registry, $route . '/' . $method);
 				}
@@ -59,33 +60,34 @@ final class Loader {
 	}
 
 	public function view($route, $data = array()) {
+		echo $route . '<br>';
+		
 		// Sanitize the call
 		$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
 		
-		$output = '';
+		$trigger = $route;
 		
 		// Trigger the pre events
-		$result = $this->registry->get('event')->trigger('view/' . $route . '/before', array(&$route, &$data, &$output));
+		$result = $this->registry->get('event')->trigger('view/' . $trigger . '/before', array(&$route, &$data));
 		
+		// Make sure its only the last event that returns an output if required.
 		if ($result) {
-			return $result;
-		}
-		
-		if (!$output) {
+			$output = $result;
+		} else {
 			$template = new Template($this->registry->get('config')->get('template_engine'));
-			
+				
 			foreach ($data as $key => $value) {
 				$template->set($key, $value);
 			}
-		
-			$output = $template->render($route);
+			
+			$output = $template->render($route);		
 		}
 		
 		// Trigger the post events
-		$result = $this->registry->get('event')->trigger('view/' . $route . '/after', array(&$route, &$data, &$output));
+		$result = $this->registry->get('event')->trigger('view/' . $trigger . '/after', array(&$route, &$data, &$output));
 		
 		if ($result) {
-			return $result;
+			$output = $result;
 		}
 		
 		return $output;
@@ -126,59 +128,62 @@ final class Loader {
 	}
 
 	public function language($route) {
-		$output = '';
+		$result = $this->registry->get('event')->trigger('language/' . $route . '/before', array($route, &$output));
 		
-		$this->registry->get('event')->trigger('language/' . $route . '/before', array(&$route, &$output));
+		if ($result) {
+			$output = $result;
+		} else {
+			$output = $this->registry->get('language')->load($route);
+		}
 		
-		$output = $this->registry->get('language')->load($route);
+		$result = $this->registry->get('event')->trigger('language/' . $route . '/after', array($route, &$output));
 		
-		$this->registry->get('event')->trigger('language/' . $route . '/after', array(&$route, &$output));
-		
+		if ($result) {
+			$output = $result;
+		}
+				
 		return $output;
 	}
 	
 	protected function callback($registry, $route) {
-		return function($args) use($registry, &$route) {
+		return function($args) use($registry, $route) {
 			static $model;
 			
-			$output = '';
+			$route = preg_replace('/[^a-zA-Z0-9_\/]/', '', (string)$route);
+			
+			$trigger = $route;
 			
 			// Trigger the pre events
-			$result = $registry->get('event')->trigger('model/' . $route . '/before', array(&$route, &$args, &$output));
+			$result = $registry->get('event')->trigger('model/' . $route . '/before', array($route, &$args));
 			
 			if ($result) {
-				return $result;
-			}
-			
-			// Store the model object
-			if (!isset($model[$route])) {
-				$file = DIR_APPLICATION . 'model/' .  substr($route, 0, strrpos($route, '/')) . '.php';
-				$class = 'Model' . preg_replace('/[^a-zA-Z0-9]/', '', substr($route, 0, strrpos($route, '/')));
-
-				if (is_file($file)) {
-					include_once($file);
-				
-					$model[$route] = new $class($registry);
-				} else {
-					throw new \Exception('Error: Could not load model ' . substr($route, 0, strrpos($route, '/')) . '!');
-				}
-			}
-
-			$method = substr($route, strrpos($route, '/') + 1);
-			
-			$callable = array($model[$route], $method);
-
-			if (is_callable($callable)) {
-				$output = call_user_func_array($callable, $args);
+				$output = $result;
 			} else {
-				throw new \Exception('Error: Could not call model/' . $route . '!');
+				$class = 'Model' . preg_replace('/[^a-zA-Z0-9]/', '', substr($route, 0, strrpos($route, '/')));
+				
+				// Store the model object
+				$key = substr($route, 0, strrpos($route, '/'));
+				
+				if (!isset($model[$key])) {
+					$model[$key] = new $class($registry);
+				}
+				
+				$method = substr($route, strrpos($route, '/') + 1);
+				
+				$callable = array($model[$route], $method);
+	
+				if (is_callable($callable)) {
+					$output = call_user_func_array($callable, $args);
+				} else {
+					throw new \Exception('Error: Could not call model/' . $route . '!');
+				}					
 			}
 			
 			// Trigger the post events
-			$result = $registry->get('event')->trigger('model/' . $route . '/after', array(&$route, &$args, &$output));
+			$result = $registry->get('event')->trigger('model/' . $route . '/after', array($route, &$args, &$output));
 			
 			if ($result) {
-				return $result;
+				$output = $result;
 			}
 						
 			return $output;

fb6a785980c1d91401bf2c1b4e990a7591063ef5
Daniel Kerr <webmaster@opencart.com>
2017-04-18 16:34:56 +0800

    https://github.com/opencart/opencart/issues/5378

--- upload/install/opencart.sql
+++ upload/install/opencart.sql
@@ -2150,25 +2150,6 @@ CREATE TABLE `oc_order` (
 -----------------------------------------------------------
 
 --
--- Table structure for table `oc_order_custom_field`
---
-
-DROP TABLE IF EXISTS `oc_order_custom_field`;
-CREATE TABLE `oc_order_custom_field` (
-  `order_custom_field_id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) NOT NULL,
-  `custom_field_id` int(11) NOT NULL,
-  `custom_field_value_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `value` text NOT NULL,
-  `type` varchar(32) NOT NULL,
-  `location` varchar(16) NOT NULL,
-  PRIMARY KEY (`order_custom_field_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
------------------------------------------------------------
-
---
 -- Table structure for table `oc_order_history`
 --
 

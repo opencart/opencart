2d6129373a7e0663d3d5ed8a86e3fe716377c85a
Daniel Kerr <webmaster@opencart.com>
2017-04-27 14:04:14 +0800

    customer approval system finished

--- upload/admin/controller/customer/customer_approval.php
+++ upload/admin/controller/customer/customer_approval.php
@@ -4,115 +4,7 @@ class ControllerCustomerCustomerApproval extends Controller {
 		$this->load->language('customer/customer_approval');
 
 		$this->document->setTitle($this->language->get('heading_title'));
-
-		$this->load->model('customer/customer_approval');	
 		
-		$this->getList();		
-	}
-	
-	public function approve() {
-		$this->load->language('customer/customer_approval');
-
-		$this->document->setTitle($this->language->get('heading_title'));
-
-		$this->load->model('customer/customer_approval');
-
-		if (isset($this->request->post['selected']) && $this->validate()) {
-			$this->load->model('customer/customer');
-			
-			foreach ($this->request->post['selected'] as $customer_id) {
-				if ($this->request->get['type'] == 'customer') {
-					$this->model_customer_customer_approval->approveCustomer($customer_id);
-				} elseif ($this->request->get['type'] == 'affiliate') {
-					$this->model_customer_customer_approval->approveAffiliate($customer_id);
-				}
-			}
-			
-			$this->session->data['success'] = $this->language->get('text_success');
-
-			$url = '';
-
-			if (isset($this->request->get['filter_name'])) {
-				$url .= '&filter_name=' . urlencode(html_entity_decode($this->request->get['filter_name'], ENT_QUOTES, 'UTF-8'));
-			}
-
-			if (isset($this->request->get['filter_email'])) {
-				$url .= '&filter_email=' . urlencode(html_entity_decode($this->request->get['filter_email'], ENT_QUOTES, 'UTF-8'));
-			}
-			
-			if (isset($this->request->get['filter_customer_group_id'])) {
-				$url .= '&filter_customer_group_id=' . $this->request->get['filter_customer_group_id'];
-			}
-			
-			if (isset($this->request->get['filter_type'])) {
-				$url .= '&filter_type=' . $this->request->get['filter_type'];
-			}
-			
-			if (isset($this->request->get['filter_date_added'])) {
-				$url .= '&filter_date_added=' . $this->request->get['filter_date_added'];
-			}
-						
-			if (isset($this->request->get['page'])) {
-				$url .= '&page=' . $this->request->get['page'];
-			}
-
-			$this->response->redirect($this->url->link('customer/customer_approval', 'user_token=' . $this->session->data['user_token'] . $url, true));
-		}
-
-		$this->getList();
-	}	
-	
-	public function deny() {
-		$this->load->language('customer/customer_approval');
-
-		$this->document->setTitle($this->language->get('heading_title'));
-
-		$this->load->model('customer/customer_approval');
-				
-		if (isset($this->request->post['selected']) && $this->validate()) {
-			foreach ($this->request->post['selected'] as $customer_id) {
-				if ($this->request->get['type'] == 'customer') {
-					$this->model_customer_customer_approval->denyCustomer($customer_id);
-				} elseif ($this->request->get['type'] == 'affiliate') {
-					$this->model_customer_customer_approval->denyAffiliate($customer_id);
-				}
-			}
-						
-			$url = '';
-			
-			if (isset($this->request->get['filter_name'])) {
-				$url .= '&filter_name=' . urlencode(html_entity_decode($this->request->get['filter_name'], ENT_QUOTES, 'UTF-8'));
-			}
-
-			if (isset($this->request->get['filter_email'])) {
-				$url .= '&filter_email=' . urlencode(html_entity_decode($this->request->get['filter_email'], ENT_QUOTES, 'UTF-8'));
-			}
-			
-			if (isset($this->request->get['filter_customer_group_id'])) {
-				$url .= '&filter_customer_group_id=' . $this->request->get['filter_customer_group_id'];
-			}
-			
-			if (isset($this->request->get['filter_type'])) {
-				$url .= '&filter_type=' . $this->request->get['filter_type'];
-			}
-			
-			if (isset($this->request->get['filter_date_added'])) {
-				$url .= '&filter_date_added=' . $this->request->get['filter_date_added'];
-			}
-			
-			if (isset($this->request->get['page'])) {
-				$url .= '&page=' . $this->request->get['page'];
-			}
-					
-			$this->session->data['success'] = $this->language->get('text_success');
-			
-			$this->response->redirect($this->url->link('customer/customer_approval', 'user_token=' . $this->session->data['user_token'], true));
-		}
-		
-		$this->getList();
-	}
-			
-	public function getList() {
 		if (isset($this->request->get['filter_name'])) {
 			$filter_name = $this->request->get['filter_name'];
 		} else {
@@ -142,13 +34,7 @@ class ControllerCustomerCustomerApproval extends Controller {
 		} else {
 			$filter_date_added = '';
 		}
-						
-		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
-		} else {
-			$page = 1;
-		}		
-		
+			
 		$url = '';
 		
 		if (isset($this->request->get['filter_name'])) {
@@ -186,9 +72,79 @@ class ControllerCustomerCustomerApproval extends Controller {
 			'text' => $this->language->get('heading_title'),
 			'href' => $this->url->link('customer/customer_approval', 'user_token=' . $this->session->data['user_token'], true)
 		);	
+		
+		$data['heading_title'] = $this->language->get('heading_title');
+		
+		$data['text_list'] = $this->language->get('text_list');
+		$data['text_default'] = $this->language->get('text_default');
+		$data['text_customer'] = $this->language->get('text_customer');
+		$data['text_affiliate'] = $this->language->get('text_affiliate');
+		
+		$data['entry_name'] = $this->language->get('entry_name');
+		$data['entry_email'] = $this->language->get('entry_email');
+		$data['entry_customer_group'] = $this->language->get('entry_customer_group');
+		$data['entry_type'] = $this->language->get('entry_type');
+		$data['entry_date_added'] = $this->language->get('entry_date_added');
+		
+		$data['button_filter'] = $this->language->get('button_filter');
+		
+		$data['filter_name'] = $filter_name;
+		$data['filter_email'] = $filter_email;
+		$data['filter_customer_group_id'] = $filter_customer_group_id;
+		$data['filter_type'] = $filter_type;
+		$data['filter_date_added'] = $filter_date_added;
 
-		$data['approve'] = $this->url->link('customer/customer_approval/approve', 'user_token=' . $this->session->data['user_token'] . $url, true);
-		$data['deny'] = $this->url->link('customer/customer_approval/deny', 'user_token=' . $this->session->data['user_token'] . $url, true);
+		$data['user_token'] = $this->session->data['user_token'];
+
+		$this->load->model('customer/customer_group');
+
+		$data['customer_groups'] = $this->model_customer_customer_group->getCustomerGroups();
+		
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('customer/customer_approval', $data));	
+	}
+				
+	public function list() {
+		$this->load->language('customer/customer_approval');
+		
+		if (isset($this->request->get['filter_name'])) {
+			$filter_name = $this->request->get['filter_name'];
+		} else {
+			$filter_name = '';
+		}
+
+		if (isset($this->request->get['filter_email'])) {
+			$filter_email = $this->request->get['filter_email'];
+		} else {
+			$filter_email = '';
+		}
+		
+		if (isset($this->request->get['filter_customer_group_id'])) {
+			$filter_customer_group_id = $this->request->get['filter_customer_group_id'];
+		} else {
+			$filter_customer_group_id = '';
+		}
+
+		if (isset($this->request->get['filter_type'])) {
+			$filter_type = $this->request->get['filter_type'];
+		} else {
+			$filter_type = '';
+		}
+		
+		if (isset($this->request->get['filter_date_added'])) {
+			$filter_date_added = $this->request->get['filter_date_added'];
+		} else {
+			$filter_date_added = '';
+		}
+						
+		if (isset($this->request->get['page'])) {
+			$page = $this->request->get['page'];
+		} else {
+			$page = 1;
+		}		
 
 		$data['customer_approvals'] = array();
 
@@ -202,6 +158,8 @@ class ControllerCustomerCustomerApproval extends Controller {
 			'limit'                    => $this->config->get('config_limit_admin')
 		);
 
+		$this->load->model('customer/customer_approval');	
+
 		$customer_approval_total = $this->model_customer_customer_approval->getTotalCustomerApprovals($filter_data);
 
 		$results = $this->model_customer_customer_approval->getCustomerApprovals($filter_data);
@@ -214,18 +172,13 @@ class ControllerCustomerCustomerApproval extends Controller {
 				'customer_group' => $result['customer_group'],
 				'type'           => $this->language->get('text_' . $result['type']),
 				'date_added'     => date($this->language->get('date_format_short'), strtotime($result['date_added'])),
-				'edit'           => $this->url->link('customer/customer/edit', 'user_token=' . $this->session->data['user_token'] . '&customer_id=' . $result['customer_id'] . $url, true)
+				'approve'        => $this->url->link('customer/customer_approval/approve', 'user_token=' . $this->session->data['user_token'] . '&customer_id=' . $result['customer_id'] . '&type=' . $result['type'], true),
+				'deny'           => $this->url->link('customer/customer_approval/deny', 'user_token=' . $this->session->data['user_token'] . '&customer_id=' . $result['customer_id'] . '&type=' . $result['type'], true),
+				'edit'           => $this->url->link('customer/customer/edit', 'user_token=' . $this->session->data['user_token'] . '&customer_id=' . $result['customer_id'], true)
 			);
 		}
-
-		$data['heading_title'] = $this->language->get('heading_title');
 		
-		$data['text_list'] = $this->language->get('text_list');
-		$data['text_default'] = $this->language->get('text_default');
 		$data['text_no_results'] = $this->language->get('text_no_results');
-		$data['text_confirm'] = $this->language->get('text_confirm');
-		$data['text_customer'] = $this->language->get('text_customer');
-		$data['text_affiliate'] = $this->language->get('text_affiliate');
 		
 		$data['column_name'] = $this->language->get('column_name');
 		$data['column_email'] = $this->language->get('column_email');
@@ -234,39 +187,9 @@ class ControllerCustomerCustomerApproval extends Controller {
 		$data['column_date_added'] = $this->language->get('column_date_added');
 		$data['column_action'] = $this->language->get('column_action');
 		
-		$data['entry_name'] = $this->language->get('entry_name');
-		$data['entry_email'] = $this->language->get('entry_email');
-		$data['entry_customer_group'] = $this->language->get('entry_customer_group');
-		$data['entry_type'] = $this->language->get('entry_type');
-		$data['entry_date_added'] = $this->language->get('entry_date_added');
-
 		$data['button_approve'] = $this->language->get('button_approve');
 		$data['button_deny'] = $this->language->get('button_deny');
-		$data['button_delete'] = $this->language->get('button_delete');
-		$data['button_filter'] = $this->language->get('button_filter');
-		
-		$data['user_token'] = $this->session->data['user_token'];
 
-		if (isset($this->error['warning'])) {
-			$data['error_warning'] = $this->error['warning'];
-		} else {
-			$data['error_warning'] = '';
-		}
-		
-		if (isset($this->session->data['success'])) {
-			$data['success'] = $this->session->data['success'];
-
-			unset($this->session->data['success']);
-		} else {
-			$data['success'] = '';
-		}
-
-		if (isset($this->request->post['selected'])) {
-			$data['selected'] = (array)$this->request->post['selected'];
-		} else {
-			$data['selected'] = array();
-		}
-		
 		$url = '';
 
 		if (isset($this->request->get['filter_name'])) {
@@ -299,28 +222,52 @@ class ControllerCustomerCustomerApproval extends Controller {
 
 		$data['results'] = sprintf($this->language->get('text_pagination'), ($customer_approval_total) ? (($page - 1) * $this->config->get('config_limit_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_limit_admin')) > ($customer_approval_total - $this->config->get('config_limit_admin'))) ? $customer_approval_total : ((($page - 1) * $this->config->get('config_limit_admin')) + $this->config->get('config_limit_admin')), $customer_approval_total, ceil($customer_approval_total / $this->config->get('config_limit_admin')));
 
-		$data['filter_name'] = $filter_name;
-		$data['filter_email'] = $filter_email;
-		$data['filter_customer_group_id'] = $filter_customer_group_id;
-		$data['filter_type'] = $filter_type;
-		$data['filter_date_added'] = $filter_date_added;
-
-		$this->load->model('customer/customer_group');
-
-		$data['customer_groups'] = $this->model_customer_customer_group->getCustomerGroups();
-
-		$data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['footer'] = $this->load->controller('common/footer');
-
-		$this->response->setOutput($this->load->view('customer/customer_approval', $data));
+		$this->response->setOutput($this->load->view('customer/customer_approval_list', $data));
 	}
-	
-	protected function validate() {
+
+	public function approve() {
+		$this->load->language('customer/customer_approval');
+
+		$json = array();
+
 		if (!$this->user->hasPermission('modify', 'customer/customer_approval')) {
-			$this->error['warning'] = $this->language->get('error_permission');
+			$json['error'] = $this->language->get('error_permission');
+		} else {
+			$this->load->model('customer/customer_approval');
+			
+			if ($this->request->get['type'] == 'customer') {
+				$this->model_customer_customer_approval->approveCustomer($this->request->get['customer_id']);
+			} elseif ($this->request->get['type'] == 'affiliate') {
+				$this->model_customer_customer_approval->approveAffiliate($this->request->get['customer_id']);
+			}
+			
+			$json['success'] = $this->language->get('text_success');
 		}
 
-		return !$this->error;
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}	
+	
+	public function deny() {
+		$this->load->language('customer/customer_approval');
+
+		$json = array();
+				
+		if (!$this->user->hasPermission('modify', 'customer/customer_approval')) {
+			$json['error'] = $this->language->get('error_permission');
+		} else {
+			$this->load->model('customer/customer_approval');
+			
+			if ($this->request->get['type'] == 'customer') {
+				$this->model_customer_customer_approval->denyCustomer($this->request->get['customer_id']);
+			} elseif ($this->request->get['type'] == 'affiliate') {
+				$this->model_customer_customer_approval->denyAffiliate($this->request->get['customer_id']);
+			}
+					
+			$json['success'] = $this->language->get('text_success');
+		}
+		
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
 	}
 }

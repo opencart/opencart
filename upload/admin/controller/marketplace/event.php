f14fd2dacfb44796136a83be09680c515a1cf2ce
Daniel Kerr <webmaster@opencart.com>
2017-05-09 16:38:44 +0800

    https://github.com/opencart/opencart/issues/5406

--- upload/admin/controller/marketplace/event.php
+++ upload/admin/controller/marketplace/event.php
@@ -190,6 +190,7 @@ class ControllerMarketplaceEvent extends Controller {
 		$data['text_no_results'] = $this->language->get('text_no_results');
 		$data['text_confirm'] = $this->language->get('text_confirm');
 		$data['text_event'] = $this->language->get('text_event');
+		$data['text_info'] = $this->language->get('text_info');
 		$data['text_trigger'] = $this->language->get('text_trigger');
 		$data['text_action'] = $this->language->get('text_action');
 

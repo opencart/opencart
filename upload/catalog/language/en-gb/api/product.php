ee4ad09e1c5d74eb02b90945007f7dda5efa3d0c
Richard Marchant <RichardMarchant@users.noreply.github.com>
2017-05-12 18:20:05 +0100

    Create language file for api/product

---
+++ upload/catalog/language/en-gb/api/product.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_success']           = 'Success';
+// Error
+$_['error_permission']       = 'Warning: You do not have permission to access the API!';
+$_['error_not_found']        = 'Warning: Product could not be found!';

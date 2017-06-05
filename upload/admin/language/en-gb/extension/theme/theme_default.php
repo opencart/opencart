bd118906e0840c067bd20a7ff87703807e36e34e
OpenCartTeam <info@systematic.com.ua>
2017-06-05 18:17:11 +0300

    Add translate in theme_default.php (admin panel)
    Tab "Home -> Extension -> Default Store Theme"

--- upload/admin/language/en-gb/extension/theme/theme_default.php
+++ upload/admin/language/en-gb/extension/theme/theme_default.php
@@ -3,6 +3,7 @@
 $_['heading_title']                    = 'Default Store Theme';
 
 // Text
+$_['text_extension']                   = 'Extensions';
 $_['text_theme']                       = 'Themes';
 $_['text_success']                     = 'Success: You have modified the default store theme!';
 $_['text_edit']                        = 'Edit Default Store Theme';

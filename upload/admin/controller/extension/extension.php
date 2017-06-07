4e323002042031fa8aee67b4de52c45d1dfb5344
ocshop <info@vilkov.info>
2017-06-07 22:40:31 +0300

    Модули по умолчанию

--- upload/admin/controller/extension/extension.php
+++ upload/admin/controller/extension/extension.php
@@ -37,7 +37,7 @@ class ControllerExtensionExtension extends Controller {
 		if (isset($this->request->get['type'])) {
 			$data['type'] = $this->request->get['type'];
 		} else {
-			$data['type'] = '';
+			$data['type'] = 'module';
 		}
 
 		$data['categories'] = array();

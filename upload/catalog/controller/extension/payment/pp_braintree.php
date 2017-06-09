11b565b22a8b28f8b62d2598bf26fd5976518110
James Allsup <james@allsup.eu>
2017-06-09 08:59:04 +0100

    Fix incorrect model class reference in mail/order.php
    Fix twig render error due to missing var.
    
    Added check for pp_braintree payment option when returning in express mode to ensure the option is still available now that the address and order total are set. Changed the pp_express.php code as well to make it more understandable.

--- upload/catalog/controller/extension/payment/pp_braintree.php
+++ upload/catalog/controller/extension/payment/pp_braintree.php
@@ -1118,8 +1118,13 @@ class ControllerExtensionPaymentPPBraintree extends Controller {
 
 		array_multisort($sort_order, SORT_ASC, $method_data);
 
+		if (!isset($method_data['pp_braintree'])) {
+			$this->session->data['error_warning'] = $this->language->get('error_unavailable');
+			$this->response->redirect($this->url->link('checkout/checkout', '', true));
+		}
+
 		$this->session->data['payment_methods'] = $method_data;
-		$this->session->data['payment_method'] = $this->session->data['payment_methods']['pp_braintree'];
+		$this->session->data['payment_method'] = $method_data['pp_braintree'];
 
 		$data['action_confirm'] = $this->url->link('extension/payment/pp_braintree/expressComplete', '', true);
 

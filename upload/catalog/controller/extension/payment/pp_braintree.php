44c92be661083281f2b740554ae9b9a8933a85d4
James Allsup <james@allsup.eu>
2017-06-19 18:05:41 +0100

    Fix problem with EC checkout for Braintree being unable to validate the status of the option due to missing prefix.

--- upload/catalog/controller/extension/payment/pp_braintree.php
+++ upload/catalog/controller/extension/payment/pp_braintree.php
@@ -943,7 +943,7 @@ class ControllerExtensionPaymentPPBraintree extends Controller {
 
 				if (!empty($results)) {
 					foreach ($results as $result) {
-						if ($this->config->get($result['code'] . '_status')) {
+						if ($this->config->get('shipping_' . $result['code'] . '_status')) {
 							$this->load->model('extension/shipping/' . $result['code']);
 
 							$quote = $this->{'model_extension_shipping_' . $result['code']}->getQuote($shipping_address);
@@ -1022,7 +1022,7 @@ class ControllerExtensionPaymentPPBraintree extends Controller {
 			array_multisort($sort_order, SORT_ASC, $results);
 
 			foreach ($results as $result) {
-				if ($this->config->get($result['code'] . '_status')) {
+				if ($this->config->get('total_' . $result['code'] . '_status')) {
 					$this->load->model('extension/total/' . $result['code']);
 
 					// We have to put the totals in an array so that they pass by reference.
@@ -1065,7 +1065,7 @@ class ControllerExtensionPaymentPPBraintree extends Controller {
 		$results = $this->model_setting_extension->getExtensions('payment');
 
 		foreach ($results as $result) {
-			if ($this->config->get($result['code'] . '_status')) {
+			if ($this->config->get('payment_' . $result['code'] . '_status')) {
 				$this->load->model('extension/payment/' . $result['code']);
 
 				$method = $this->{'model_extension_payment_' . $result['code']}->getMethod($payment_address, $total);
@@ -1222,7 +1222,7 @@ class ControllerExtensionPaymentPPBraintree extends Controller {
 			array_multisort($sort_order, SORT_ASC, $results);
 
 			foreach ($results as $result) {
-				if ($this->config->get($result['code'] . '_status')) {
+				if ($this->config->get('total_' . $result['code'] . '_status')) {
 					$this->load->model('extension/total/' . $result['code']);
 
 					// We have to put the totals in an array so that they pass by reference.

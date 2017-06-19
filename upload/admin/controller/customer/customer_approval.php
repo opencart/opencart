7e22fbafb0f32fb89d8f70f211c02fbf57342a20
Daniel Kerr <webmaster@opencart.com>
2017-06-20 01:05:38 +0800

    customer_approval fix

--- upload/admin/controller/customer/customer_approval.php
+++ upload/admin/controller/customer/customer_approval.php
@@ -92,7 +92,7 @@ class ControllerCustomerCustomerApproval extends Controller {
 		$this->response->setOutput($this->load->view('customer/customer_approval', $data));	
 	}
 				
-	public function list() {
+	public function customer_approval() {
 		$this->load->language('customer/customer_approval');
 		
 		if (isset($this->request->get['filter_name'])) {

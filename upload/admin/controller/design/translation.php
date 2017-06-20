1f71d4484f7487987eb22f9b9dda3eeacfe82cc8
OpenCartAddons <joel@opencartaddons.com>
2017-06-19 22:17:18 -0400

    Update translation.php

--- upload/admin/controller/design/translation.php
+++ upload/admin/controller/design/translation.php
@@ -222,7 +222,7 @@ class ControllerDesignTranslation extends Controller {
 		$data['sort_store'] = $this->url->link('design/translation', 'user_token=' . $this->session->data['user_token'] . '&sort=store' . $url, true);
 		$data['sort_language'] = $this->url->link('design/translation', 'user_token=' . $this->session->data['user_token'] . '&sort=language' . $url, true);
 		$data['sort_route'] = $this->url->link('design/translation', 'user_token=' . $this->session->data['user_token'] . '&sort=route' . $url, true);
-		$data['sort_key'] = $this->url->link('design/translation', 'user_token=' . $this->session->data['user_token'] . '&sort=t.key' . $url, true);
+		$data['sort_key'] = $this->url->link('design/translation', 'user_token=' . $this->session->data['user_token'] . '&sort=key' . $url, true);
 		$data['sort_value'] = $this->url->link('design/translation', 'user_token=' . $this->session->data['user_token'] . '&sort=value' . $url, true);
 		
 		$pagination = new Pagination();

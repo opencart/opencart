7682154e51294c2156540f561a8c7f51e2943088
Danijel Gombač <info.gombac@gmail.com>
2017-06-27 08:13:35 +0200

    Update authorizenet_aim.php

--- upload/catalog/model/extension/payment/authorizenet_aim.php
+++ upload/catalog/model/extension/payment/authorizenet_aim.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionPaymentAuthorizeNetAim extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('extension/payment/authorizenet_aim');
+		$this->load->language('extension/payment/authorizenet_aim', 'authorizenet_aim');
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_authorizenet_aim_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
@@ -20,7 +20,7 @@ class ModelExtensionPaymentAuthorizeNetAim extends Model {
 		if ($status) {
 			$method_data = array(
 				'code'       => 'authorizenet_aim',
-				'title'      => $this->language->get('text_title'),
+				'title'      => $this->language->get('authorizenet_aim')->get('text_title'),
 				'terms'      => '',
 				'sort_order' => $this->config->get('payment_authorizenet_aim_sort_order')
 			);

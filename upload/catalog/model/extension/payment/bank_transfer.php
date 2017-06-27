ff3896a26b49fe4012016e23ab56af27851a3204
Danijel Gombač <info.gombac@gmail.com>
2017-06-27 08:14:37 +0200

    Update bank_transfer.php

--- upload/catalog/model/extension/payment/bank_transfer.php
+++ upload/catalog/model/extension/payment/bank_transfer.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionPaymentBankTransfer extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('extension/payment/bank_transfer');
+		$this->load->language('extension/payment/bank_transfer', 'bank_transfer');
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('bank_transfer_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
@@ -20,7 +20,7 @@ class ModelExtensionPaymentBankTransfer extends Model {
 		if ($status) {
 			$method_data = array(
 				'code'       => 'bank_transfer',
-				'title'      => $this->language->get('text_title'),
+				'title'      => $this->language->get('bank_transfer')->get('text_title'),
 				'terms'      => '',
 				'sort_order' => $this->config->get('bank_transfer_sort_order')
 			);

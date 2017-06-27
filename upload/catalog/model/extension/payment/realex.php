0fa44f5d8d36d8d2de32ec221dae6cc32d1875aa
Danijel Gombač <info.gombac@gmail.com>
2017-06-27 08:36:40 +0200

    Update realex.php

--- upload/catalog/model/extension/payment/realex.php
+++ upload/catalog/model/extension/payment/realex.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionPaymentRealex extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('extension/payment/realex');
+		$this->load->language('extension/payment/realex', 'realex');
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_realex_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
@@ -20,7 +20,7 @@ class ModelExtensionPaymentRealex extends Model {
 		if ($status) {
 			$method_data = array(
 				'code'       => 'realex',
-				'title'      => $this->language->get('text_title'),
+				'title'      => $this->language->get('realex')->get('text_title'),
 				'terms'      => '',
 				'sort_order' => $this->config->get('payment_realex_sort_order')
 			);

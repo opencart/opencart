7732253c6e6e7902f3cb6808c5c2e81325617ad6
Danijel Gombač <info.gombac@gmail.com>
2017-06-27 07:29:43 +0200

    Update item.php

--- upload/catalog/model/extension/shipping/item.php
+++ upload/catalog/model/extension/shipping/item.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionShippingItem extends Model {
 	function getQuote($address) {
-		$this->load->language('extension/shipping/item');
+		$this->load->language('extension/shipping/item', 'item');
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('shipping_item_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
@@ -28,7 +28,7 @@ class ModelExtensionShippingItem extends Model {
 
 			$quote_data['item'] = array(
 				'code'         => 'item.item',
-				'title'        => $this->language->get('text_description'),
+				'title'        => $this->language->get('item')->get('text_description'),
 				'cost'         => $this->config->get('shipping_item_cost') * $items,
 				'tax_class_id' => $this->config->get('shipping_item_tax_class_id'),
 				'text'         => $this->currency->format($this->tax->calculate($this->config->get('shipping_item_cost') * $items, $this->config->get('shipping_item_tax_class_id'), $this->config->get('config_tax')), $this->session->data['currency'])
@@ -36,7 +36,7 @@ class ModelExtensionShippingItem extends Model {
 
 			$method_data = array(
 				'code'       => 'item',
-				'title'      => $this->language->get('text_title'),
+				'title'      => $this->language->get('item')->get('text_title'),
 				'quote'      => $quote_data,
 				'sort_order' => $this->config->get('shipping_item_sort_order'),
 				'error'      => false

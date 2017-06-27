32e12f24b5ba0099c6195b1fbd2bf38640672252
Danijel Gombač <info.gombac@gmail.com>
2017-06-27 08:16:59 +0200

    Update cardconnect.php

--- upload/catalog/model/extension/payment/cardconnect.php
+++ upload/catalog/model/extension/payment/cardconnect.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionPaymentCardConnect extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('extension/payment/cardconnect');
+		$this->load->language('extension/payment/cardconnect', 'cardconnect');
 
 		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('cardconnect_geo_zone') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')");
 
@@ -19,10 +19,10 @@ class ModelExtensionPaymentCardConnect extends Model {
 
 		if ($status) {
 			$method_data = array(
-				'code'			=> 'cardconnect',
-				'title'			=> $this->language->get('text_title'),
-				'terms'			=> '',
-				'sort_order'	=> $this->config->get('cardconnect_sort_order')
+				'code'		 => 'cardconnect',
+				'title'		 => $this->language->get('cardconnect')->get('text_title'),
+				'terms'		 => '',
+				'sort_order' => $this->config->get('cardconnect_sort_order')
 			);
 		}
 

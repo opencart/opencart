138ffb4765ae237efe158f85c32378dc50800046
Danijel Gombač <info.gombac@gmail.com>
2017-06-27 08:38:40 +0200

    Update sagepay_us.php

--- upload/catalog/model/extension/payment/sagepay_us.php
+++ upload/catalog/model/extension/payment/sagepay_us.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionPaymentSagePayUS extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('extension/payment/sagepay_us');
+		$this->load->language('extension/payment/sagepay_us', 'sagepay_us');
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_sagepay_us_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
@@ -20,7 +20,7 @@ class ModelExtensionPaymentSagePayUS extends Model {
 		if ($status) {
 			$method_data = array(
 				'code'       => 'sagepay_us',
-				'title'      => $this->language->get('text_title'),
+				'title'      => $this->language->get('sagepay_us')->get('text_title'),
 				'terms'      => '',
 				'sort_order' => $this->config->get('payment_sagepay_us_sort_order')
 			);

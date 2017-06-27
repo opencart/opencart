1dcdde4d6012821edefc50be8fea582f3320f85f
Danijel Gombač <info.gombac@gmail.com>
2017-06-27 08:18:50 +0200

    Update cod.php

--- upload/catalog/model/extension/payment/cod.php
+++ upload/catalog/model/extension/payment/cod.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionPaymentCOD extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('extension/payment/cod');
+		$this->load->language('extension/payment/cod', 'cod');
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_cod_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
@@ -22,7 +22,7 @@ class ModelExtensionPaymentCOD extends Model {
 		if ($status) {
 			$method_data = array(
 				'code'       => 'cod',
-				'title'      => $this->language->get('text_title'),
+				'title'      => $this->language->get('cod')->('text_title'),
 				'terms'      => '',
 				'sort_order' => $this->config->get('payment_cod_sort_order')
 			);

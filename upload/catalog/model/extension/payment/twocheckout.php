b2ab0ac18951e71eb1c8be8ea2a4f9710c5f3c22
Danijel Gombač <info.gombac@gmail.com>
2017-06-27 08:40:34 +0200

    Update twocheckout.php

--- upload/catalog/model/extension/payment/twocheckout.php
+++ upload/catalog/model/extension/payment/twocheckout.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionPaymentTwoCheckout extends Model {
 	public function getMethod($address, $total) {
-		$this->load->language('extension/payment/twocheckout');
+		$this->load->language('extension/payment/twocheckout', 'twocheckout');
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_twocheckout_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
@@ -20,7 +20,7 @@ class ModelExtensionPaymentTwoCheckout extends Model {
 		if ($status) {
 			$method_data = array(
 				'code'       => 'twocheckout',
-				'title'      => $this->language->get('text_title'),
+				'title'      => $this->language->get('twocheckout')->get('text_title'),
 				'terms'      => '',
 				'sort_order' => $this->config->get('payment_twocheckout_sort_order')
 			);

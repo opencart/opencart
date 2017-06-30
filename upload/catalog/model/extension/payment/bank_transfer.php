6a65f16bf2b7b54659be7cefef700a25badb75d3
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2017-06-30 09:45:58 -0300

    Fix model bank_transfer.php

--- upload/catalog/model/extension/payment/bank_transfer.php
+++ upload/catalog/model/extension/payment/bank_transfer.php
@@ -3,11 +3,11 @@ class ModelExtensionPaymentBankTransfer extends Model {
 	public function getMethod($address, $total) {
 		$this->load->language('extension/payment/bank_transfer');
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('bank_transfer_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_bank_transfer_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
-		if ($this->config->get('bank_transfer_total') > 0 && $this->config->get('bank_transfer_total') > $total) {
+		if ($this->config->get('payment_bank_transfer_total') > 0 && $this->config->get('payment_bank_transfer_total') > $total) {
 			$status = false;
-		} elseif (!$this->config->get('bank_transfer_geo_zone_id')) {
+		} elseif (!$this->config->get('payment_bank_transfer_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
 			$status = true;
@@ -22,7 +22,7 @@ class ModelExtensionPaymentBankTransfer extends Model {
 				'code'       => 'bank_transfer',
 				'title'      => $this->language->get('text_title'),
 				'terms'      => '',
-				'sort_order' => $this->config->get('bank_transfer_sort_order')
+				'sort_order' => $this->config->get('payment_bank_transfer_sort_order')
 			);
 		}
 

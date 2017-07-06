3870e222bbe7e2e053e1e93ff3395e355970d1f3
Daniel Kerr <webmaster@opencart.com>
2017-07-06 22:37:07 +0800

    some js was messed up also fixed an install issue after adding the storage folder

--- upload/install/index.php
+++ upload/install/index.php
@@ -18,6 +18,7 @@ define('HTTP_OPENCART', $protocol . $_SERVER['HTTP_HOST'] . rtrim(rtrim(dirname(
 define('DIR_OPENCART', str_replace('\\', '/', realpath(dirname(__FILE__) . '/../') . '/'));
 define('DIR_APPLICATION', str_replace('\\', '/', realpath(dirname(__FILE__))) . '/');
 define('DIR_SYSTEM', str_replace('\\', '/', realpath(dirname(__FILE__) . '/../')) . '/system/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
 define('DIR_IMAGE', str_replace('\\', '/', realpath(dirname(__FILE__) . '/../')) . '/image/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');

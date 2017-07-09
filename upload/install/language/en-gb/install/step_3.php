86aa090fac48dd303b683f449c1ddb46b2161995
valdeirpsr <valdeirpsr@hotmail.com.br>
2017-07-09 18:33:21 -0300

    Add text_pgsql

--- upload/install/language/en-gb/install/step_3.php
+++ upload/install/language/en-gb/install/step_3.php
@@ -9,6 +9,7 @@ $_['text_db_administration'] = '2. Please enter a username and password for the 
 $_['text_mysqli']            = 'MySQLi';
 $_['text_mysql']             = 'MySQL';
 $_['text_mpdo']              = 'mPDO';
+$_['text_pgsql']             = 'PostgreSQL';
 
 // Entry
 $_['entry_db_driver']        = 'DB Driver';

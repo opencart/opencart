eeba91bbb228c424773588286a617a788010bf30
Daniel Kerr <webmaster@opencart.com>
2017-07-11 21:00:04 +0800

    minor changes to the backup ui

--- upload/system/config/catalog.php
+++ upload/system/config/catalog.php
@@ -1,24 +1,24 @@
 <?php
 // Site
-$_['site_url']          = HTTP_SERVER;
-$_['site_ssl']          = HTTPS_SERVER;
+$_['site_url']           = HTTP_SERVER;
+$_['site_ssl']           = HTTPS_SERVER;
 
 // Url
-$_['url_autostart']     = false;
+$_['url_autostart']      = false;
 
 // Database
-$_['db_autostart']      = true;
-$_['db_engine']         = DB_DRIVER; // mpdo, mssql, mysql, mysqli or postgre
-$_['db_hostname']       = DB_HOSTNAME;
-$_['db_username']       = DB_USERNAME;
-$_['db_password']       = DB_PASSWORD;
-$_['db_database']       = DB_DATABASE;
-$_['db_port']           = DB_PORT;
+$_['db_autostart']       = true;
+$_['db_engine']          = DB_DRIVER; // mpdo, mssql, mysql, mysqli or postgre
+$_['db_hostname']        = DB_HOSTNAME;
+$_['db_username']        = DB_USERNAME;
+$_['db_password']        = DB_PASSWORD;
+$_['db_database']        = DB_DATABASE;
+$_['db_port']            = DB_PORT;
 
 // Session
-$_['session_autostart'] = true;
-$_['session_engine']    = 'db';
-$_['session_name']      = 'OCSESSID';
+$_['session_autostart']  = true;
+$_['session_engine']     = 'db';
+$_['session_name']       = 'OCSESSID';
 
 // Template
 $_['template_engine']    = 'twig';
@@ -49,7 +49,7 @@ $_['action_event'] = array(
 		'event/language/after'
 	),	
 	'view/*/before' => array(
-		0    => 'event/theme/override',
+		500  => 'event/theme/override',
 		998  => 'event/language',
 		1000 => 'event/theme'
 	),

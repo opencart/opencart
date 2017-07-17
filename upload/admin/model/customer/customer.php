0ce722e9e9328a5feef67edf2d81f43a2cf272e2
KOOPY <KOOPY@KOOPY-PC>
2017-07-17 17:11:45 +0800

    Added missing function ModelCustomerCustomer::getAffliateByTracking()

--- upload/admin/model/customer/customer.php
+++ upload/admin/model/customer/customer.php
@@ -270,6 +270,12 @@ class ModelCustomerCustomer extends Model {
 
 		return $query->row['total'];
 	}
+        
+        public function getAffliateByTracking($tracking) {
+                $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "customer_affiliate WHERE tracking = '" . $this->db->escape($tracking) . "'");
+                
+                return $query->row;
+        }
 	
 	public function getAffiliate($customer_id) {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "customer_affiliate WHERE customer_id = '" . (int)$customer_id . "'");

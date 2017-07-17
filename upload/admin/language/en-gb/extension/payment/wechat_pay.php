1e4b1cc271aaf037f9f7efeff17d2b71ac8a7819
mengwenbin <mengwb@opencart.cn>
2017-07-17 15:31:23 +0800

    wechat pay (QR code)

---
+++ upload/admin/language/en-gb/extension/payment/wechat_pay.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']                  = 'Wechat Pay';
+
+// Text
+$_['text_extension']                 = 'Extensions';
+$_['text_success']                   = 'Success: You have modified Wechat account details!';
+$_['text_edit']                      = 'Edit Wechat Pay';
+$_['text_wechat_pay']                = '<a target="_BLANK" href="https://pay.weixin.qq.com"><img src="view/image/payment/Wechat.png" alt="Wechat Pay Website" title="Wechat Pay Website" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Entry
+$_['entry_app_id']                   = 'App ID';
+$_['entry_app_secret']               = 'App Secret';
+$_['entry_mch_id']                   = 'Merchant ID';
+$_['entry_api_secret']               = 'API Secret';
+$_['entry_debug']                    = 'Debug Mode';
+$_['entry_total']                    = 'Total';
+$_['entry_currency']                 = 'Currency';
+$_['entry_completed_status']         = 'Completed Status';
+$_['entry_geo_zone']                 = 'Geo Zone';
+$_['entry_status']                   = 'Status';
+$_['entry_sort_order']               = 'Sort Order';
+
+// Help
+$_['help_total']                     = 'The checkout total the order must reach before this payment method becomes active';
+$_['help_currency']                  = 'The currency customer paid merchant!';
+
+// Error
+$_['error_permission']               = 'Warning: You do not have permission to modify payment Wechat!';
+$_['error_app_id']                   = 'App ID required!';
+$_['error_app_secret']               = 'App Secret required!';
+$_['error_mch_id']                   = 'Merchant ID required!';
+$_['error_api_secret']               = 'API Secret required!';

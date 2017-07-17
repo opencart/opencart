6551fb198053e6960f3ede0d38f0f3484410f663
mengwenbin <mengwb@opencart.cn>
2017-07-17 15:32:25 +0800

    wechat pay (QR code)

---
+++ upload/catalog/language/en-gb/extension/payment/wechat_pay.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title']              = 'Wechat QR Code Pay';
+
+// Text
+$_['text_title']                 = 'Wechat Pay';
+$_['text_checkout']              = 'Checkout';
+$_['text_qrcode']                = 'QrCode';
+$_['text_qrcode_description']    = 'Please scan the QR code in WeChat app to pay your order!';

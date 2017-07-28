d0816228cae0250da3974fe54d78cfc05633500d
Daniel Kerr <webmaster@opencart.com>
2017-07-28 23:51:41 +0800

    https://github.com/opencart/opencart/issues/5808#issuecomment-318673203

--- upload/install/model/upgrade/1003.php
+++ upload/install/model/upgrade/1003.php
@@ -77,7 +77,7 @@ class ModelUpgrade1003 extends Model {
 			}
 
 			if (preg_match('/^(a:)/', $result['payment_custom_field'])) {
-				$this->db->query("UPDATE `" . DB_PREFIX . "order` SET `payment_custom_field` = '" . $this->db->escape(json_encode(unserialize($result['shipping_custom_field']))) . "' WHERE `order_id` = '" . (int)$result['order_id'] . "'");
+				$this->db->query("UPDATE `" . DB_PREFIX . "order` SET `payment_custom_field` = '" . $this->db->escape(json_encode(unserialize($result['payment_custom_field']))) . "' WHERE `order_id` = '" . (int)$result['order_id'] . "'");
 			}
 
 			if (preg_match('/^(a:)/', $result['shipping_custom_field'])) {

a66e8b2667827f2cc3e127c52633b6ed9303031b
Anatoly <evpadallas@gmail.com>
2017-07-30 12:17:53 +0300

    Update db.php

--- upload/system/library/db.php
+++ upload/system/library/db.php
@@ -29,20 +29,6 @@ class DB {
 
 		if (class_exists($class)) {
 			$this->adaptor = new $class($hostname, $username, $password, $database, $port);
-			$query = $this->adaptor->query("SELECT value FROM " . DB_PREFIX . "setting WHERE `code` = 'config' AND `key` = 'config_timezone' ");
-            if (!empty($query->row)) {
-                $timezone = $query->row['value'];
-                date_default_timezone_set($timezone);
-
-                $now = new DateTime();
-                $mins = $now->getOffset() / 60;
-                $sgn = ($mins < 0 ? -1 : 1);
-                $mins = abs($mins);
-                $hrs = floor($mins / 60);
-                $mins -= $hrs * 60;
-                $offset = sprintf('%+d:%02d', $hrs * $sgn, $mins);
-                $this->adaptor->query("SET time_zone='" . $offset . "'");
-            }
 		} else {
 			throw new \Exception('Error: Could not load database adaptor ' . $adaptor . '!');
 		}

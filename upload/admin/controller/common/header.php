dd812357c01db8cecc491f4f561ee29e7f3c4e06
Daniel Kerr <webmaster@opencart.com>
2017-08-10 15:25:52 +0800

    removed the gravatar crap

--- upload/admin/controller/common/header.php
+++ upload/admin/controller/common/header.php
@@ -31,10 +31,15 @@ class ControllerCommonHeader extends Controller {
 			$data['home'] = $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true);
 			$data['logout'] = $this->url->link('common/logout', 'user_token=' . $this->session->data['user_token'], true);
 			$data['profile'] = $this->url->link('common/profile', 'user_token=' . $this->session->data['user_token'], true);
-		
-			$this->load->model('user/user');
-	
+
 			$this->load->model('tool/image');
+
+			$data['firstname'] = '';
+			$data['lastname'] = '';
+			$data['user_group'] = '';
+			$data['image'] = $this->model_tool_image->resize('profile.png', 45, 45);
+						
+			$this->load->model('user/user');
 	
 			$user_info = $this->model_user_user->getUser($this->user->getId());
 	
@@ -46,15 +51,8 @@ class ControllerCommonHeader extends Controller {
 	
 				if (is_file(DIR_IMAGE . $user_info['image'])) {
 					$data['image'] = $this->model_tool_image->resize($user_info['image'], 45, 45);
-				} else {
-					$data['image'] = 'https://www.gravatar.com/avatar/' . md5( strtolower( trim( $user_info['email'] ) ) ) . "?d=" . urlencode( $this->model_tool_image->resize('profile.png', 45, 45) ) . "&s=45";
 				}
-			} else {
-				$data['firstname'] = '';
-				$data['lastname'] = '';
-				$data['user_group'] = '';
-				$data['image'] = '';
-			}			
+			} 		
 			
 			// Online Stores
 			$data['stores'] = array();

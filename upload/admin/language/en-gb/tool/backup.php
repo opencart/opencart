1006daae2bbfe048e45783a709eee4edc0f62cd6
Daniel Kerr <webmaster@opencart.com>
2017-09-06 14:55:44 +0800

    more marketplace work

--- upload/admin/language/en-gb/tool/backup.php
+++ upload/admin/language/en-gb/tool/backup.php
@@ -3,17 +3,28 @@
 $_['heading_title']    = 'Backup &amp; Restore';
 
 // Text
-$_['text_success']     = 'Success: You have successfully imported your database!';
-$_['text_backup']      = 'Backup Options';
-$_['text_file']        = 'Modified Files';
-$_['text_database']    = 'Database';
+$_['text_success']      = 'Success: You have successfully imported your database!';
+$_['text_backup']       = 'Backup Options';
+$_['text_all']          = 'All Files';
+$_['text_modified']     = 'Modified Files';
+$_['text_none']         = 'None';
 $_['text_image']       = 'Images';
 $_['text_download']    = 'Downloads';
 $_['text_upload']      = 'Uploads';
 
 // Entry
-$_['entry_progress']   = 'Progress';
-$_['entry_export']     = 'Export';
+$_['entry_progress']    = 'Progress';
+$_['entry_export']      = 'Export';
+
+$_['entry_file']        = 'Files';
+$_['entry_php']         = 'PHP Files';
+$_['entry_database']    = 'Database';
+
+
+
+
+
+
 
 // Tab
 $_['tab_backup']       = 'Backup';

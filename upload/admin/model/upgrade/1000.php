2140210e8c671300e814b5d3777794607ab05711
Daniel Kerr <webmaster@opencart.com>
2017-09-20 00:39:44 +0800

    https://github.com/opencart/opencart/issues/5698

--- upload/admin/model/upgrade/1000.php
+++ upload/admin/model/upgrade/1000.php
@@ -4,7 +4,7 @@ class ModelUpgrade1000 extends Model {
 		// marketing_history
 		$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "marketing_history'");
 
-		if ($query->num_rows) {
+		if (!$query->num_rows) {
 			$this->db->query("
 			CREATE TABLE `" . DB_PREFIX . "marketing_history` (
 			`marketing_history_id` int(11) NOT NULL AUTO_INCREMENT,

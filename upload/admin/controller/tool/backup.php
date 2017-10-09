5d032fa139f48fd031bcca9552682fe74ebe2abe
Danijel Gombač <info.gombac@gmail.com>
2017-10-09 16:57:42 +0200

    Update backup.php

--- upload/admin/controller/tool/backup.php
+++ upload/admin/controller/tool/backup.php
@@ -238,7 +238,7 @@ class ControllerToolBackup extends Controller {
 					$start = true;
 				}
 
-				if ($i > 0 && (substr($line, 0, 24) == 'TRUNCATE TABLE `' . DB_PREFIX . 'user`' || substr($line, 0, 30) == 'TRUNCATE TABLE `' . DB_PREFIX . 'ser_group`')) {
+				if ($i > 0 && (substr($line, 0, 24) == 'TRUNCATE TABLE `' . DB_PREFIX . 'user`' || substr($line, 0, 30) == 'TRUNCATE TABLE `' . DB_PREFIX . 'user_group`')) {
 					fseek($handle, $position, SEEK_SET);
 
 					break;

73812f1c1e17572ae511ec8e212730e4a914b156
Daniel Kerr <webmaster@opencart.com>
2017-10-12 15:43:23 +0800

    https://github.com/opencart/opencart/pull/6080

--- upload/catalog/controller/extension/module/featured.php
+++ upload/catalog/controller/extension/module/featured.php
@@ -26,47 +26,47 @@ class ControllerExtensionModuleFeatured extends Controller {
 
 			$products = array_slice($product_data, 0, (int)$setting['limit']);
 
-			foreach ($products as $product_id) {
-				if ($product_info['image']) {
-					$image = $this->model_tool_image->resize($product_info['image'], $setting['width'], $setting['height']);
+			foreach ($products as $product) {
+				if ($product['image']) {
+					$image = $this->model_tool_image->resize($product['image'], $setting['width'], $setting['height']);
 				} else {
 					$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
 				}
 
 				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-					$price = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+					$price = $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 				} else {
 					$price = false;
 				}
 
-				if ((float)$product_info['special']) {
-					$special = $this->currency->format($this->tax->calculate($product_info['special'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				if ((float)$product['special']) {
+					$special = $this->currency->format($this->tax->calculate($product['special'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 				} else {
 					$special = false;
 				}
 
 				if ($this->config->get('config_tax')) {
-					$tax = $this->currency->format((float)$product_info['special'] ? $product_info['special'] : $product_info['price'], $this->session->data['currency']);
+					$tax = $this->currency->format((float)$product['special'] ? $product['special'] : $product['price'], $this->session->data['currency']);
 				} else {
 					$tax = false;
 				}
 
 				if ($this->config->get('config_review_status')) {
-					$rating = $product_info['rating'];
+					$rating = $product['rating'];
 				} else {
 					$rating = false;
 				}
 
 				$data['products'][] = array(
-					'product_id'  => $product_info['product_id'],
+					'product_id'  => $product['product_id'],
 					'thumb'       => $image,
-					'name'        => $product_info['name'],
-					'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+					'name'        => $product['name'],
+					'description' => utf8_substr(strip_tags(html_entity_decode($product['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
 					'tax'         => $tax,
 					'rating'      => $rating,
-					'href'        => $this->url->link('product/product', 'product_id=' . $product_info['product_id'])
+					'href'        => $this->url->link('product/product', 'product_id=' . $product['product_id'])
 				);
 			}
 		}

8399fedb3d56b765a3895988f2c9b3f51561dcdc
Thomas Lang <thomas.lang@tom-it.nl>
2017-10-23 09:11:17 +0000

    Hide the discounts if the user is not logged in and config_customer_price = 1

--- upload/catalog/controller/product/product.php
+++ upload/catalog/controller/product/product.php
@@ -296,12 +296,13 @@ class ControllerProductProduct extends Controller {
 			$discounts = $this->model_catalog_product->getProductDiscounts($this->request->get['product_id']);
 
 			$data['discounts'] = array();
-
+			if($discounts && ($this->customer->isLogged() || !$this->config->get('config_customer_price'))){
 			foreach ($discounts as $discount) {
 				$data['discounts'][] = array(
 					'quantity' => $discount['quantity'],
 					'price'    => $this->currency->format($this->tax->calculate($discount['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency'])
 				);
+			}
 			}
 
 			$data['options'] = array();

5fb7ffb7485886914b5b131c8b54e5423e1fb5e0
seanmacdomhnall <seanmacdomhnall@gmail.com>
2017-11-24 11:51:11 +0000

    Small amendments for Realex / Global Payments integrations for OpenCart 2.3.x

--- upload/catalog/model/extension/payment/realex_remote.php
+++ upload/catalog/model/extension/payment/realex_remote.php
@@ -64,7 +64,7 @@ class ModelExtensionPaymentRealexRemote extends Model {
 		curl_setopt($ch, CURLOPT_USERAGENT, "OpenCart " . VERSION);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		$response = curl_exec ($ch);
 		curl_close ($ch);
 
@@ -112,7 +112,7 @@ class ModelExtensionPaymentRealexRemote extends Model {
 		curl_setopt($ch, CURLOPT_USERAGENT, "OpenCart " . VERSION);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		$response = curl_exec ($ch);
 		curl_close ($ch);
 
@@ -151,8 +151,8 @@ class ModelExtensionPaymentRealexRemote extends Model {
 				$xml .= '<type>' . $type . '</type>';
 				$xml .= '<chname>' . $name . '</chname>';
 				$xml .= '<cvn>';
-					$xml .= '<number>' . (int)$cvv . '</number>';
-					$xml .= '<presind>2</presind>';
+					$xml .= '<number>' . $cvv . '</number>';
+					$xml .= '<presind>1</presind>';
 				$xml .= '</cvn>';
 				if (!empty($issue)) {
 					$xml .= '<issueno>' . (int)$issue . '</issueno>';
@@ -226,7 +226,7 @@ class ModelExtensionPaymentRealexRemote extends Model {
 		curl_setopt($ch, CURLOPT_USERAGENT, "OpenCart " . VERSION);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		$response = curl_exec ($ch);
 		curl_close ($ch);
 

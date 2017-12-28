3c230fd1ae21bc6482c28ee82e682233ef492683
Jesse <jesse@opencart.com>
2017-12-28 17:57:52 +0800

    getSeoUrlsByQuery fixed

--- upload/admin/model/design/seo_url.php
+++ upload/admin/model/design/seo_url.php
@@ -114,9 +114,9 @@ class ModelDesignSeoUrl extends Model {
 
 		return $query->rows;
 	}
-	
-	public function getSeoUrlsByQuery($keyword) {
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "seo_url` WHERE query = '" . $this->db->escape($keyword) . "'");
+
+	public function getSeoUrlsByQuery($query) {
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "seo_url` WHERE query = '" . $this->db->escape($query) . "'");
 
 		return $query->rows;
 	}

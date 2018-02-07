5de6cde8db2d0fe46b5e7c921e152dd07867d79d
Daniel Kerr <webmaster@opencart.com>
2018-02-07 16:00:41 +0800

    wishlist fix

--- upload/catalog/controller/account/wishlist.php
+++ upload/catalog/controller/account/wishlist.php
@@ -93,7 +93,7 @@ class ControllerAccountWishList extends Controller {
 					'stock'      => $stock,
 					'price'      => $price,
 					'special'    => $special,
-					'minimum'    => $result['minimum'] > 0 ? $result['minimum'] : 1,
+					'minimum'    => $product_info['minimum'] > 0 ? $product_info['minimum'] : 1,
 					'href'       => $this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&product_id=' . $product_info['product_id']),
 					'remove'     => $this->url->link('account/wishlist', 'language=' . $this->config->get('config_language') . '&remove=' . $product_info['product_id'])
 				);

a884575d82f4b7c64fd3ebc44ad84fed9824f0f2
Anton <stalker780@users.noreply.github.com>
2018-02-09 21:15:50 +0200

    2Checkout fixes
    1. fixed SSL links in admin
    2. fixed USD conversion. Cause 2Checkout works only with USD
    3. fixed digital products checkout. They don't require shipping address now on 2Checkout gateway page
    4. Fixed redirects
    5. Updated form field names according to latest 2Checkout documentation

--- upload/admin/controller/extension/payment/twocheckout.php
+++ upload/admin/controller/extension/payment/twocheckout.php
@@ -14,7 +14,7 @@ class ControllerExtensionPaymentTwoCheckout extends Controller {
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
-			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment'));
+			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true));
 		}
 
 		if (isset($this->error['warning'])) {
@@ -39,22 +39,22 @@ class ControllerExtensionPaymentTwoCheckout extends Controller {
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'])
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
 		);
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('text_extension'),
-			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment')
+			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true)
 		);
 
 		$data['breadcrumbs'][] = array(
 			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('extension/payment/twocheckout', 'user_token=' . $this->session->data['user_token'])
+			'href' => $this->url->link('extension/payment/twocheckout', 'user_token=' . $this->session->data['user_token'], true)
 		);
 
-		$data['action'] = $this->url->link('extension/payment/twocheckout', 'user_token=' . $this->session->data['user_token']);
+		$data['action'] = $this->url->link('extension/payment/twocheckout', 'user_token=' . $this->session->data['user_token'], true);
 
-		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment');
+		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true);
 
 		if (isset($this->request->post['payment_twocheckout_account'])) {
 			$data['payment_twocheckout_account'] = $this->request->post['payment_twocheckout_account'];

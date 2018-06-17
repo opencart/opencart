cd0d38870a2c451a5ab0a88794277d252a39efe4
OSWorX <info@osworx.net>
2018-06-17 17:51:07 +0200

    Update default.php
    - unused vars, + new vars

--- upload/admin/language/en-gb/extension/menu/default.php
+++ upload/admin/language/en-gb/extension/menu/default.php
@@ -1,25 +1,24 @@
 <?php
 // Heading
-$_['heading_title']          = 'Default Menu';
+$_['heading_title']			= 'Default Menu';
 
 // Text
-$_['text_extension']         = 'Extensions';
-$_['text_success']           = 'Success: You have modified Google Base feed!';
-$_['text_edit']              = 'Edit Google Base';
-$_['text_import']            = 'To download the latest Google category list by <a href="https://support.google.com/merchants/answer/160081?hl=en" target="_blank" class="alert-link">clicking here</a> and choose taxonomy with numeric IDs in Plain Text (.txt) file. Upload via the green import button.';
+$_['text_extension']		= 'Extensions';
+$_['text_success']			= 'Success: You have modified Default Menu!';
+$_['text_edit']				= 'Edit Default Menu';
+$_['text_enabled']			= 'Enabled';
+$_['text_disabled']			= 'Disabled';
+$_['text_no_results']		= 'No results';
+$_['text_layout']			= 'After you have installed and configured a layout, you can call it <a href="%s" class="alert-link">here</a>!';
 
 // Column
-$_['column_google_category'] = 'Google Category';
-$_['column_category']        = 'Category';
-$_['column_action']          = 'Action';
+$_['column_name']			= 'Name';
+$_['column_status']			= 'Status';
+$_['column_action']			= 'Action';
 
-// Entry
-$_['entry_google_category']  = 'Google Category';
-$_['entry_category']         = 'Category';
-$_['entry_data_feed']        = 'Data Feed Url';
-$_['entry_status']           = 'Status';
+// Buttons
+$_['button_install']		= 'Install';
+$_['button_uninstall']		= 'Uninstall';
 
 // Error
-$_['error_permission']       = 'Warning: You do not have permission to modify Google Base feed!';
-$_['error_upload']           = 'File could not be uploaded!';
-$_['error_filetype']         = 'Invalid file type!';
+$_['error_permission']		= = 'Warning: You do not have permission to edit Default Menu!';

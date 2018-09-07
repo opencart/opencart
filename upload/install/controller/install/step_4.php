f3688c4a40d80e45a5256ea46ea04de149202028
James Allsup <james@allsup.eu>
2018-09-07 02:04:30 +0100

    Updated promotional section on install stage 4 to Amazon Pay.

--- upload/install/controller/install/step_4.php
+++ upload/install/controller/install/step_4.php
@@ -2,12 +2,12 @@
 class ControllerInstallStep4 extends Controller {
 	public function index() {
 		$this->load->language('install/step_4');
-		
+
 		$this->document->setTitle($this->language->get('heading_title'));
 
 		$data['heading_title'] = $this->language->get('heading_title');
-		
-		$data['text_step_4'] = $this->language->get('text_step_4');		
+
+		$data['text_step_4'] = $this->language->get('text_step_4');
 		$data['text_catalog'] = $this->language->get('text_catalog');
 		$data['text_admin'] = $this->language->get('text_admin');
 		$data['text_loading'] = $this->language->get('text_loading');
@@ -15,7 +15,7 @@ class ControllerInstallStep4 extends Controller {
 		$data['text_mail'] = $this->language->get('text_mail');
 		$data['text_mail_description'] = $this->language->get('text_mail_description');
 		$data['text_openbay'] = $this->language->get('text_openbay');
-		$data['text_maxmind'] = $this->language->get('text_maxmind');
+		$data['text_amazon_pay'] = $this->language->get('text_amazon_pay');
 		$data['text_facebook'] = $this->language->get('text_facebook');
 		$data['text_facebook_description'] = $this->language->get('text_facebook_description');
 		$data['text_facebook_visit'] = $this->language->get('text_facebook_visit');
@@ -44,9 +44,9 @@ class ControllerInstallStep4 extends Controller {
 			$data['success'] = '';
 		}
 
-		$data['maxmind'] = $this->url->link('3rd_party/maxmind');
 		$data['openbay'] = $this->url->link('3rd_party/openbay');
 		$data['extension'] = $this->url->link('3rd_party/extension');
+		$data['link_amazon_pay'] = "https://pay.amazon.com/uk/signup";
 
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
--- upload/install/language/en-gb/common/footer.php
+++ upload/install/language/en-gb/common/footer.php
@@ -3,4 +3,3 @@
 $_['text_project']       = 'Project Homepage';
 $_['text_documentation'] = 'Documentation';
 $_['text_support']       = 'Support Forums';
-$_['text_footer']        = 'Copyright © 2014 OpenCart - All rights reserved';

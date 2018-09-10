7daf2624adf54b25857755521ab4c406e97aff5d
gerard <gerard@opencart.com>
2018-09-10 11:11:42 +0100

    cookie popup

---
+++ upload/admin/language/en-gb/extension/module/gdpr.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'GDPR';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified GDPR module!';
+$_['text_edit']        = 'Edit GDPR Module';
+
+// Entry
+$_['entry_policy_url']       = 'Cookie Policy URL';
+$_['entry_status']     = 'Status';
+
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify GDPR module!';

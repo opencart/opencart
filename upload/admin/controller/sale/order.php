ee9f580a572a6daff21b9e581495c19887f1fbe7
Daniel Kerr <webmaster@opencart.com>
2018-10-15 16:05:12 +0800

    https://github.com/opencart/opencart/issues/6794

--- upload/admin/controller/sale/order.php
+++ upload/admin/controller/sale/order.php
@@ -1790,7 +1790,7 @@ class ControllerSaleOrder extends Controller {
 
 							$product_option_value_info = $this->model_catalog_product_option->getProductOptionValue($product['product_id'], $option['product_option_value_id']);
 
-							if ($product_option_value_info) {
+							if (!empty($product_option_value_info['weight'])) {
 								if ($product_option_value_info['weight_prefix'] == '+') {
 									$option_weight += $product_option_value_info['weight'];
 								} elseif ($product_option_value_info['weight_prefix'] == '-') {

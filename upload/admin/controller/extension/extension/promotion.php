9460db932f56899c7cb01fe52bf0e619488301c1
Daniel Kerr <webmaster@opencart.com>
2018-10-29 14:11:37 +0800

    promotion fix css changes

--- upload/admin/controller/extension/extension/promotion.php
+++ upload/admin/controller/extension/extension/promotion.php
@@ -1,25 +1,23 @@
 <?php
 class ControllerExtensionExtensionPromotion extends Controller {
 	public function index() {
-		if (isset($this->request->get['type'])) {
-			$curl = curl_init();
+		$curl = curl_init();
 
-			curl_setopt($curl, CURLOPT_URL, OPENCART_SERVER . 'index.php?route=api/promotion');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($curl, CURLOPT_HEADER, false);
-			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
-			curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+		curl_setopt($curl, CURLOPT_URL, OPENCART_SERVER . 'index.php?route=api/promotion&type=' . substr($this->request->get['route'], strrpos($this->request->get['route'], '/') + 1));
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 30);
 
-			$response = curl_exec($curl);
+		$response = curl_exec($curl);
 
-			curl_close($curl);
+		curl_close($curl);
 
-			if ($response) {
-				return $response;
-			} else {
-				return '';
-			}
+		if ($response) {
+			return $response;
+		} else {
+			return '';
 		}
 	}
 }

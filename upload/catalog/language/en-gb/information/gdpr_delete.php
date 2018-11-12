45a55fb7f3b1c00df05d90c898d3460b41c722cc
OSWorX <info@osworx.net>
2018-11-12 23:27:02 +0100

    Update gdpr_delete.php
    1 line too much (combined text of both)

--- upload/catalog/language/en-gb/information/gdpr_delete.php
+++ upload/catalog/language/en-gb/information/gdpr_delete.php
@@ -6,10 +6,8 @@ $_['heading_title'] = 'GDPR Delete Account';
 $_['text_account']  = 'Account';
 $_['text_gdpr']     = '<strong>GDPR</strong> account deletion requests will process after 180 days so any fraud detection, chargebacks or refunds can be processed.';
 
-$_['text_access']  = 'You will no longer have access to your %s account.';
-$_['text_access']   = 'No longer have access to your order history, wishlists, downloads';
+$_['text_access']  = 'You will no longer have access to your %s account, order history, wishlists and downloads.';
 $_['text_delete']   = 'account deletion requests will process after 180 days so any fraud detection, chargebacks or refunds can be processed.';
-
 
 // Buttons
 $_['button_remove'] = 'Remove Personal Data';

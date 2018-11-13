c8b5d09f28f224084a162028b97259f26a4ff1a7
OSWorX <info@osworx.net>
2018-11-13 10:32:38 +0100

    Update gdpr.php
    Consistency, wording

--- upload/catalog/language/en-gb/information/gdpr.php
+++ upload/catalog/language/en-gb/information/gdpr.php
@@ -3,12 +3,12 @@ $_['heading_title']     = 'General Data Personal Request';
 
 // Text
 $_['text_account']      = 'Account';
-$_['text_email']        = 'Before we can carry out any GDPR requests we must validate you account. please enter your email below.';
+$_['text_email']        = 'Before we can carry out any GDPR requests we must validate you account. Please enter your E-Mail Address below.';
 $_['text_verification'] = 'Account Verification';
 $_['text_action']       = 'Choose an action';
 $_['text_access']       = 'You will no longer have access to your %s account.';
-$_['text_history']      = 'No longer have access to your order history, invoices, wishlists or downloads.';
-$_['text_limit']        = 'Account deletion requests will process after %s days so any fraud detection, chargebacks or refunds can be processed.';
+$_['text_history']      = 'You will no longer have access to your order history, invoices, wishlists or downloads.';
+$_['text_limit']        = 'Account deletion requests will be processed after %s days so any fraud detection, chargebacks or refunds can be processed.';
 $_['text_export']       = 'Export Personal Data';
 $_['text_delete']       = 'Remove Personal Data';
 

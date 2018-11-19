1b5c19d815ca943bd7b384c9f983c2a04b113c15
howdu <howdu@users.noreply.github.com>
2018-11-19 21:09:51 +0000

    Google recaptcha

--- upload/catalog/controller/extension/captcha/google.php
+++ upload/catalog/controller/extension/captcha/google.php
@@ -17,22 +17,18 @@ class ControllerExtensionCaptchaGoogle extends Controller {
     }
 
     public function validate() {
-		if (empty($this->session->data['gcapcha'])) {
-			$this->load->language('extension/captcha/google');
+		$this->load->language('extension/captcha/google');
 
-			if (!isset($this->request->post['g-recaptcha-response'])) {
-				return $this->language->get('error_captcha');
-			}
-
+		if (!empty($this->request->post['g-recaptcha-response'])) {
 			$recaptcha = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($this->config->get('captcha_google_secret')) . '&response=' . $this->request->post['g-recaptcha-response'] . '&remoteip=' . $this->request->server['REMOTE_ADDR']);
 
 			$recaptcha = json_decode($recaptcha, true);
 
 			if ($recaptcha['success']) {
-				$this->session->data['gcapcha']	= true;
-			} else {
-				return $this->language->get('error_captcha');
+				return true;
 			}
 		}
+
+		return $this->language->get('error_captcha');
     }
 }

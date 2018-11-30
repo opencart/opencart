e68500bf0d332215e914bcc85fbac32b71315973
OSWorX <info@osworx.net>
2018-11-30 04:17:47 +0100

    Update gdpr_export.php
    Either delete or export, but because we are here in the export file, I am sure you meant export ..

--- upload/catalog/language/en-gb/mail/gdpr_export.php
+++ upload/catalog/language/en-gb/mail/gdpr_export.php
@@ -6,7 +6,7 @@ $_['text_request']   = 'Export Data Request';
 $_['text_gdpr']      = 'A GDPR request from this email address, To confirm this action please click on the link below:';
 
 
-$_['text_greeting']  = 'A request to delete your account from <strong>%s</strong> has been received.';
+$_['text_greeting']  = 'A request to export my account data from <strong>%s</strong> has been received.';
 //$_['text_delete']    = 'GDPR account deletion requests will process after <strong>%s days</strong> so any chargebacks, refunds or fraud detection can be processed.';
 
 
@@ -15,5 +15,5 @@ $_['text_contact']   = 'If you did not make this request please contact the stor
 $_['text_thanks']    = 'Thanks,';
 
 // Button
-$_['button_confirm'] = 'I confirm export my data';
+$_['button_confirm'] = 'I confirm to export my data';
 $_['button_contact'] = 'Contact Us';

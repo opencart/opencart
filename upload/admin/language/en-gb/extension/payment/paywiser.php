51da22b28c0b67ab88ad7ab4c3d6ec19f2deb417
faca5 <11090866+faca5@users.noreply.github.com>
2018-12-08 20:10:33 +0100

    Create paywiser.php
    Adding payment method Paywiser

---
+++ upload/admin/language/en-gb/extension/payment/paywiser.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']					 = 'PayWiser';
+
+// Text
+$_['text_payment']					 = 'Payment';
+$_['text_success']					 = 'Success: You have modified PayWiser account details!';
+$_['text_paywiser']				 	  = '<a onclick="window.open(\'https://www.paywiser.eu/\');"><img src="view/image/payment/paywiser.png" alt="PayWiser" title="PayWiser" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Entry
+$_['entry_api_key']					 = 'API Key:';
+$_['entry_test']					 = 'Testing Mode:';
+$_['entry_total']                    = 'Total:';
+$_['entry_canceled_reversal_status'] = 'Canceled Reversal Status:';
+$_['entry_completed_status']         = 'Completed Status:';
+$_['entry_denied_status']			 = 'Denied Status:';
+$_['entry_expired_status']			 = 'Expired Status:';
+$_['entry_failed_status']			 = 'Failed Status:';
+$_['entry_pending_status']			 = 'Pending Status:';
+$_['entry_processed_status']		 = 'Processed Status:';
+$_['entry_refunded_status']			 = 'Refunded Status:';
+$_['entry_reversed_status']			 = 'Reversed Status:';
+$_['entry_voided_status']		     = 'Voided Status:';
+$_['entry_geo_zone']				 = 'Geo Zone:';
+$_['entry_status']					 = 'Status:';
+$_['entry_sort_order']				 = 'Sort Order:';
+
+// Error
+$_['error_permission']				 = 'Warning: You do not have permission to modify payment Paywiser!';
+$_['error_api_key']					 = 'API key required!';
+?>

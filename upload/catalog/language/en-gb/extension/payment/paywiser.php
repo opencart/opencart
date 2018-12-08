bcfdcb0019dc04a70098f327675cbfa2e9eadde7
faca5 <11090866+faca5@users.noreply.github.com>
2018-12-08 20:15:27 +0100

    Create paywiser.php
    Adding payment method Paywiser

---
+++ upload/catalog/language/en-gb/extension/payment/paywiser.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_title']    = 'Paywser';
+$_['text_reason'] 	= 'REASON';
+$_['text_testmode']	= 'Warning: The payment gateway is in \'Testing Mode\'. Your account will not be charged.';
+$_['text_total']	= 'Shipping, Handling, Discounts & Taxes';
+?>

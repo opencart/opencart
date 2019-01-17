bbd109fe5cff11d80b92f66ff06b650e1c6e92df
mhcwebdesign <github@mhccorp.com>
2019-01-17 16:21:56 +0000

    Update template.php

--- upload/system/library/template/template.php
+++ upload/system/library/template/template.php
@@ -1,68 +1,61 @@
 <?php
 namespace Template;
 final class Template {
-	protected $code;
-	protected $filters = array();
 	protected $data = array();
-
-	public function addFilter($key, $value) {
-		$this->filters[$key] = $value;
-	}
 
 	public function set($key, $value) {
 		$this->data[$key] = $value;
 	}
 
-	public function render($filename, $cache = true) {
-		$file = DIR_TEMPLATE . $filename . '.tpl';
-
-		if (is_file($file)) {
-			$this->code = file_get_contents($file);
-
-			foreach ($this->filters as $filter) {
-				$filter->callback($this->code);
-			}
-
-			ob_start();
-
-			if (!$cache && function_exists('eval')) {
-				extract($this->data);
-
-				echo eval('?>' . $this->code);
+	public function render($filename, $cache = true, $template_code=null) {
+		// render from modified template code if there
+		if ($template_code) {
+			$file = DIR_CACHE.'tpl/'.$filename.'.tpl';
+			if (is_file($file)) {
+				if ($template_code != file_get_contents($file)) {
+					file_put_contents($file,$template_code,LOCK_EX);
+				}
 			} else {
-				extract($this->data);
-
-				include($this->compile($file, $this->code));
+				$this->file_force_contents($file,$template_code,LOCK_EX);
 			}
-
-			return ob_get_clean();
-		} else {
-			throw new \Exception('Error: Could not load template ' . $file . '!');
-			exit();
+			ob_start();
+			extract($this->data);
+			include( $file );
+			$output = ob_get_clean();
+			return $output;
 		}
+
+		// remove old cache file if still there
+		$file = DIR_CACHE.'tpl/'.$filename.'.tpl';
+		if (is_file( $file )) {
+			unlink( $file );
+		}
+
+		// render from PHP template file
+		$file = DIR_TEMPLATE . $filename . '.tpl';
+		if (is_file($file)) {
+			extract($this->data);
+			ob_start();
+			include($file);
+			$output = ob_get_clean();
+			return $output;
+		}
+
+		throw new \Exception('Error: Could not load template ' . $file . '!');
+		exit();
 	}
 
-	public function compile($file, $code) {
-		$hash = hash('sha256', $file . __CLASS__ . preg_replace('/[^0-9a-zA-Z_]/', '_', implode('_', array_keys($this->filters))));
-
-		$file = DIR_CACHE . substr($hash, 0, 2) . '/' . $hash . '.php';
-
-		if (!is_file($file)) {
-			$directory = dirname($file);
-
-			if (!is_dir($directory)) {
-				if (!mkdir($directory, 0777, true)) {
-					clearstatcache(true, $directory);
-				}
+	// helper function, see http://php.net/manual/en/function.file-put-contents.php#84180
+	protected function file_force_contents($dir, $contents, $flags) {
+		$parts = explode('/', substr($dir,1));
+		$file = array_pop($parts);
+		$dir = '';
+		foreach ($parts as $part) {
+			$dir .= "/$part";
+			if (!is_dir($dir)) { 
+				mkdir($dir);
 			}
-
-			$handle = fopen($file, 'w+');
-
-			fwrite($handle, $code);
-
-			fclose($handle);
 		}
-
-		return $file;
+		file_put_contents( "$dir/$file", $contents, $flags );
 	}
 }

71d12260ab7f20f24c250f419edb7e8f7a8c9732
oclabs <info@oclabs.pro>
2019-01-26 00:48:04 +0200

    Дополнение перевода

---
+++ upload/catalog/language/ru-ru/account/affiliate.php
@@ -0,0 +1,38 @@
+<?php
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Heading
+$_['heading_title']             = 'Ваша партнерская информация';
+
+// Text
+$_['text_account']              = 'Личный кабинет';
+$_['text_affiliate']            = 'Партнерcкий раздел';
+$_['text_my_affiliate']         = 'Мой партнерский аккаунт';
+$_['text_payment']              = 'Платежная информация';
+$_['text_cheque']               = 'Чек';
+$_['text_paypal']               = 'PayPal';
+$_['text_bank']                 = 'Банковский перевод';
+$_['text_success']              = 'Ваша учетная запись была успешно обновлена.';
+$_['text_agree']                = 'Я прочитал и согласен с <a href="%s" class="agree"><b>%s</b></a>';
+
+// Entry
+$_['entry_company']             = 'Компания';
+$_['entry_website']             = 'Веб-сайт';
+$_['entry_tax']                 = 'Налоговый код (ИНН)';
+$_['entry_payment']             = 'Способ оплаты';
+$_['entry_cheque']              = 'Чек, Имя получателя платежа';
+$_['entry_paypal']              = 'PayPal Email аккаунт';
+$_['entry_bank_name']           = 'Наименование банка';
+$_['entry_bank_branch_number']  = 'ABA/BSB номер (номер отделения)';
+$_['entry_bank_swift_code']     = 'SWIFT код';
+$_['entry_bank_account_name']   = 'Название счета';
+$_['entry_bank_account_number'] = 'Номер счета';
+
+// Error
+$_['error_agree']               = 'Вы должны подтвердить согласие с %s!';
+$_['error_cheque']              = 'Проверьте имя получателя!';
+$_['error_paypal']              = 'Адрес электронной почты PayPal не является действительным!';
+$_['error_bank_account_name']   = 'Требуется название счета!';
+$_['error_bank_account_number'] = 'Требуется номер счета!';
+$_['error_custom_field']        = '%s требуется!';

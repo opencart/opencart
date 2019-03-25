15bd2310a0b4974f7516b876ce29f01757d84cd0
Daniel Kerr <webmaster@opencart.com>
2019-03-25 17:05:11 +0800

    more gdpr work

--- upload/admin/language/en-gb/mail/gdpr_export.php
+++ upload/admin/language/en-gb/mail/gdpr_export.php
@@ -1,13 +1,19 @@
 <?php
 // Text
 $_['text_subject']    = '%s - General Data Personal Request';
+$_['text_request']    = 'Export Personal Data';
 $_['text_hello']      = 'Hello <strong>%s</strong>,';
 $_['text_user']       = 'User';
+
 $_['text_gdpr']       = 'Export Personal Data';
+
 $_['text_account']    = 'Account';
 $_['text_customer']   = 'Personal Information';
-$_['text_firstname']  = 'First Name';
-$_['text_lastname']   = 'Last Name';
+$_['text_address']    = 'Address';
+$_['text_addresses']  = 'Addresses';
+
+$_['text_name']       = 'Name';
+$_['text_email']      = 'E-Mail';
 $_['text_telephone']  = 'Telephone';
 $_['text_company']    = 'Company';
 $_['text_address_1']  = 'Address 1';
@@ -16,13 +22,9 @@ $_['text_postcode']   = 'Post Code';
 $_['text_city']       = 'City';
 $_['text_country']    = 'Country';
 $_['text_zone']       = 'Region / State';
-$_['text_addresses']  = 'Addresses';
-$_['text_address']    = 'Address';
+
 $_['text_history']    = 'Login History';
-$_['text_ip']         = 'IP';
+$_['text_ip']         = 'IP\'s';
 $_['text_date_added'] = 'Date Added';
 
-$_['text_thanks']   = 'Thanks,';
-
-// Entry
-$_['entry_email']     = 'E-Mail';
+$_['text_thanks']     = 'Thanks,';

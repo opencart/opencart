628d1debb12465a97a9b05b99eafc27b528cf28e
engwalid1971 <engwalid1971@gmail.com>
2019-05-02 17:37:32 +0200

    Update developer.php

--- upload/admin/controller/common/developer.php
+++ upload/admin/controller/common/developer.php
@@ -52,20 +52,30 @@ class ControllerCommonDeveloper extends Controller {
 			// Before we delete we need to make sure there is a sass file to regenerate the css
 			$file = DIR_APPLICATION  . 'view/stylesheet/bootstrap.css';
 			
-			if (is_file($file) && is_file(DIR_APPLICATION . 'view/stylesheet/sass/_bootstrap.scss')) {
+			if (is_file($file) && is_file(DIR_APPLICATION . 'view/stylesheet/scss/bootstrap.scss')) {
 				unlink($file);
 			}
 			 
-			$files = glob(DIR_CATALOG  . 'view/theme/*/stylesheet/sass/_bootstrap.scss');
+			$files = glob(DIR_CATALOG  . 'view/theme/*/stylesheet/scss/bootstrap.scss');
 			 
 			foreach ($files as $file) {
-				$file = substr($file, 0, -21) . '/bootstrap.css';
+				$file = substr($file, 0, -20) . '/bootstrap.css';
 				
 				if (is_file($file)) {
 					unlink($file);
 				}
 			}
-			
+
+			$files = glob(DIR_CATALOG  . 'view/theme/*/stylesheet/stylesheet.scss');
+			 
+			foreach ($files as $file) {
+				$file = substr($file, 0, -16) . '/stylesheet.css';
+				
+				if (is_file($file)) {
+					unlink($file);
+				}
+			}
+
 			$json['success'] = sprintf($this->language->get('text_cache'), $this->language->get('text_sass'));
 		}	
 		

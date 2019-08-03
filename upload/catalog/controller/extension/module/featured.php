69f94c3ee995fa50f4aa15b799f8fefd7c933bb0
faca5 <11090866+faca5@users.noreply.github.com>
2019-08-03 08:57:04 +0200

    Update featured.php
    Variable $product_info has been changed to $result. Variable $result is used in all other files. Keep it compatible.

--- upload/catalog/controller/extension/module/featured.php
+++ upload/catalog/controller/extension/module/featured.php
@@ -2,76 +2,61 @@
 class ControllerExtensionModuleFeatured extends Controller {
 	public function index($setting) {
 		$this->load->language('extension/module/featured');
-
 		$this->load->model('catalog/product');
-
 		$this->load->model('tool/image');
-
 		$data['products'] = array();
-
 		if (!$setting['limit']) {
 			$setting['limit'] = 4;
 		}
-
 		if (!empty($setting['product'])) {
-			$product_data = array();
-
-			foreach ($setting['product'] as $product_id) {
-				$product_info = $this->model_catalog_product->getProduct($product_id);
-
-				if ($product_info) {
-					$product_data[] = $product_info;
+			$result_data = array();
+			foreach ($setting['product'] as $result_id) {
+				$results = $this->model_catalog_product->getProduct($result_id);
+				if ($results) {
+					$result_data[] = $results;
 				}
 			}
-
-			$products = array_slice($product_data, 0, (int)$setting['limit']);
-
-			foreach ($products as $product) {
-				if ($product['image']) {
-					$image = $this->model_tool_image->resize(html_entity_decode($product['image'], ENT_QUOTES, 'UTF-8'), $setting['width'], $setting['height']);
+			$results = array_slice($result_data, 0, (int)$setting['limit']);
+			foreach ($results as $result) {
+				if ($result['image']) {
+					$image = $this->model_tool_image->resize(html_entity_decode($result['image'], ENT_QUOTES, 'UTF-8'), $setting['width'], $setting['height']);
 				} else {
 					$image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
 				}
-
 				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-					$price = $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+					$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 				} else {
 					$price = false;
 				}
-
-				if ((float)$product['special']) {
-					$special = $this->currency->format($this->tax->calculate($product['special'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				if ((float)$result['special']) {
+					$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 				} else {
 					$special = false;
 				}
-
 				if ($this->config->get('config_tax')) {
-					$tax = $this->currency->format((float)$product['special'] ? $product['special'] : $product['price'], $this->session->data['currency']);
+					$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
 				} else {
 					$tax = false;
 				}
-
 				if ($this->config->get('config_review_status')) {
-					$rating = $product['rating'];
+					$rating = $result['rating'];
 				} else {
 					$rating = false;
 				}
-
 				$data['products'][] = array(
-					'product_id'  => $product['product_id'],
+					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
-					'name'        => $product['name'],
-					'description' => utf8_substr(strip_tags(html_entity_decode($product['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+					'name'        => $result['name'],
+					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
 					'tax'         => $tax,
-					'minimum'     => $product['minimum'] > 0 ? $product['minimum'] : 1,
+					'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
 					'rating'      => $rating,
-					'href'        => $this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&product_id=' . $product['product_id'])
+					'href'        => $this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&product_id=' . $result['product_id'])
 				);
 			}
 		}
-
 		if ($data['products']) {
 			return $this->load->view('extension/module/featured', $data);
 		}

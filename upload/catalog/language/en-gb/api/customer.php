a14bd95262252ff8307b2ca562e6eddbed244ac8
dilip kumar <dilipkumar.oc571@webkul.com>
2019-09-13 18:46:38 +0530

    Fixed datepicker layout issue

--- upload/catalog/language/en-gb/api/customer.php
+++ upload/catalog/language/en-gb/api/customer.php
@@ -5,7 +5,7 @@ $_['text_success']               = 'You have successfully modified customers';
 // Error
 $_['error_permission']           = 'Warning: You do not have permission to access the API!';
 $_['error_customer']             = 'You must select a customer!';
-$_['error_customer_disable']     =  'selected customer status is disabled!';
+$_['error_customer_disable']     = 'Selected customer status is disabled!';
 $_['error_firstname']            = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']             = 'Last Name must be between 1 and 32 characters!';
 $_['error_email']                = 'E-Mail Address does not appear to be valid!';

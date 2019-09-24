1e39a3d2af63a3b499ab68f5cc0ded393e03f846
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2019-09-25 01:54:37 +0200

    Update search.php
    duble load model

--- upload/catalog/controller/product/search.php
+++ upload/catalog/controller/product/search.php
@@ -133,8 +133,6 @@ class ControllerProductSearch extends Controller {
 
 		$data['compare'] = $this->url->link('product/compare', 'language=' . $this->config->get('config_language'));
 
-		$this->load->model('catalog/category');
-
 		// 3 Level Category Search
 		$data['categories'] = array();
 

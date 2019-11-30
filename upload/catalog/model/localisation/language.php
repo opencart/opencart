74f59ed03912bf630b56f79813a18c595ba7709a
Grumvol <wforever9@gmail.com>
2019-12-01 00:37:27 +0100

    Table 'oc_language' (excess field 'image')
    Больше нет такого поля (осталось из старой версии).

--- upload/catalog/model/localisation/language.php
+++ upload/catalog/model/localisation/language.php
@@ -1,34 +1,33 @@
 <?php
 class ModelLocalisationLanguage extends Model {
-	public function getLanguage($language_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "language WHERE language_id = '" . (int)$language_id . "'");
+    public function getLanguage($language_id) {
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "language WHERE language_id = '" . (int)$language_id . "'");
 
-		return $query->row;
-	}
+        return $query->row;
+    }
 
-	public function getLanguages() {
-		$language_data = $this->cache->get('catalog.language');
+    public function getLanguages() {
+        $language_data = $this->cache->get('catalog.language');
 
-		if (!$language_data) {
-			$language_data = array();
+        if (!$language_data) {
+            $language_data = array();
 
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "language WHERE status = '1' ORDER BY sort_order, name");
+            $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "language WHERE status = '1' ORDER BY sort_order, name");
 
-			foreach ($query->rows as $result) {
-				$language_data[] = array(
-					'language_id' => $result['language_id'],
-					'name'        => $result['name'],
-					'code'        => $result['code'],
-					'locale'      => $result['locale'],
-					'image'       => $result['image'],
-					'sort_order'  => $result['sort_order'],
-					'status'      => $result['status']
-				);
-			}
+            foreach ($query->rows as $result) {
+                $language_data[] = array(
+                    'language_id' => $result['language_id'],
+                    'name'        => $result['name'],
+                    'code'        => $result['code'],
+                    'locale'      => $result['locale'],
+                    'sort_order'  => $result['sort_order'],
+                    'status'      => $result['status']
+                );
+            }
 
-			$this->cache->set('catalog.language', $language_data);
-		}
+            $this->cache->set('catalog.language', $language_data);
+        }
 
-		return $language_data;
-	}
+        return $language_data;
+    }
 }

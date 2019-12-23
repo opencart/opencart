78e12efa24c3fda3d965681bbc0abce93c17af0d
xxvirusxx <condor20_05@yahoo.it>
2019-12-23 12:57:43 +0200

    Update modification.php

--- upload/admin/controller/marketplace/modification.php
+++ upload/admin/controller/marketplace/modification.php
@@ -623,7 +623,7 @@ class ControllerMarketplaceModification extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}

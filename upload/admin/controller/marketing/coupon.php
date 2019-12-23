80cdb34c48d14ddf86e3630b836d3f4de41a8ec5
xxvirusxx <condor20_05@yahoo.it>
2019-12-23 12:50:39 +0200

    Update coupon.php

--- upload/admin/controller/marketing/coupon.php
+++ upload/admin/controller/marketing/coupon.php
@@ -553,7 +553,7 @@ class ControllerMarketingCoupon extends Controller {
 			'url'   => $this->url->link('marketing/coupon/history', 'user_token=' . $this->session->data['user_token'] . '&coupon_id=' . $coupon_id . '&page={page}')
 		));
 
-		$data['results'] = sprintf('Showing %d to %d of %d (%d Pages)', ($history_total) ? (($page - 1) * 10) + 1 : 0, ((($page - 1) * 10) > ($history_total - 10)) ? $history_total : ((($page - 1) * 10) + 10), $history_total, ceil($history_total / 10));
+		$data['results'] = sprintf($this->language->get('text_pagination'), ($history_total) ? (($page - 1) * 10) + 1 : 0, ((($page - 1) * 10) > ($history_total - 10)) ? $history_total : ((($page - 1) * 10) + 10), $history_total, ceil($history_total / 10));
 
 		$this->response->setOutput($this->load->view('marketing/coupon_history', $data));
 	}

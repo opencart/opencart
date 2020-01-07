bf7da25e9f8fbfd53ecc671467826f77a739f267
Grumvol <wforever9@gmail.com>
2020-01-07 13:42:19 +0100

    getProductOptionValues not used

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -966,12 +966,6 @@ class ModelCatalogProduct extends Model {
 		return $query->row;
 	}
 
-	public function getProductOptionValues($product_option_id) {
-		$query = $this->db->query("SELECT product_option_value_id, option_value_id, quantity, subtract, price, price_prefix, points, points_prefix, weight, weight_prefix FROM " . DB_PREFIX . "product_option_value WHERE product_option_id = '" . (int)$product_option_id . "'");
-
-		return $query->rows;
-	}
-
 	public function getProductOptionValuesByOptionId($option_id) {
 		$query = $this->db->query("SELECT DISTINCT option_value_id FROM " . DB_PREFIX . "product_option_value WHERE option_id = '" . (int)$option_id . "'");
 

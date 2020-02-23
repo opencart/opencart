fc3873773df247024989f11e2044d29f3a52aacb
Manoel Vidal <dev@opencartbrasil.com.br>
2020-02-23 07:46:43 -0300

    Fix session

--- upload/system/library/session/db.php
+++ upload/system/library/session/db.php
@@ -9,35 +9,51 @@ CREATE TABLE IF NOT EXISTS `session` (
 */
 namespace Session;
 final class DB {
-	public $expire = 3600;
-	
+	private $expire;
+
 	public function __construct($registry) {
 		$this->db = $registry->get('db');
-		
-		$this->expire = ini_get('session.gc_maxlifetime');
+
+		if (ini_get('session.gc_maxlifetime')) {
+			$gc_maxlifetime = ini_get('session.gc_maxlifetime');
+		} else {
+			$gc_maxlifetime = 3600;
+		}
+
+		$this->expire = $gc_maxlifetime;
 	}
-	
+
+	public function exists($session_id) {
+		$query = $this->db->query("SELECT `session_id` FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "'");
+
+		if ($query->num_rows) {
+			return true;
+		}
+
+		return false;
+	}
+
 	public function read($session_id) {
-		$query = $this->db->query("SELECT `data` FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "' AND expire > '" . $this->db->escape(date('Y-m-d H:i:s'))  . "'");
-		
+		$query = $this->db->query("SELECT `data` FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "' AND expire > NOW()");
+
 		if ($query->num_rows) {
 			return json_decode($query->row['data'], true);
-		} else {
-			return false;
 		}
+
+		return array();
 	}
-	
+
 	public function write($session_id, $data) {
 		if ($session_id) {
-			$this->db->query("REPLACE INTO `" . DB_PREFIX . "session` SET session_id = '" . $this->db->escape($session_id) . "', `data` = '" . $this->db->escape($data ? json_encode($data) : '') . "', expire = '" . $this->db->escape(date('Y-m-d H:i:s', time() + $this->expire)) . "'");
+			$this->db->query("REPLACE INTO `" . DB_PREFIX . "session` SET session_id = '" . $this->db->escape($session_id) . "', `data` = '" . $this->db->escape(json_encode($data)) . "', expire = DATE_ADD(NOW(), INTERVAL ". $this->db->escape($this->expire) ." SECOND)");
 		}
-		
+
 		return true;
 	}
-	
+
 	public function destroy($session_id) {
 		$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "'");
-		
+
 		return true;
 	}
 	
@@ -55,7 +71,7 @@ final class DB {
 		}
 
 		if ((rand() % $gc_divisor) < $gc_probability) {
-			$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE expire < '" . $this->db->escape(date('Y-m-d H:i:s', time())) . "'");
+			$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE expire < NOW()");
 		}
 
 		return true;

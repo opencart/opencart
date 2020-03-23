29cd0157402e0fec2e259aad4520cd00d866d5aa
straightlight <32371528+straightlight@users.noreply.github.com>
2020-03-23 17:22:04 -0400

    Added filters on custom fields

--- upload/catalog/controller/account/address.php
+++ upload/catalog/controller/account/address.php
@@ -388,13 +388,15 @@ class ControllerAccountAddress extends Controller {
 		$data['custom_fields'] = array();
 		
 		$this->load->model('account/custom_field');
+		
+		$filter_data = array('customer_group_id'	=> $this->config->get('config_customer_group_id'),
+							 'location'				=> 'address'
+							);
 
-		$custom_fields = $this->model_account_custom_field->getCustomFields($this->config->get('config_customer_group_id'));
+		$custom_fields = $this->model_account_custom_field->getCustomFields($filter_data);
 
 		foreach ($custom_fields as $custom_field) {
-			if ($custom_field['location'] == 'address') {
-				$data['custom_fields'][] = $custom_field;
-			}
+			$data['custom_fields'][] = $custom_field;
 		}
 		
 		if (isset($this->request->post['custom_field']['address'])) {
@@ -460,17 +462,19 @@ class ControllerAccountAddress extends Controller {
 
 		// Custom field validation
 		$this->load->model('account/custom_field');
+		
+		$filter_data = array('customer_group_id'			=> $this->config->get('config_customer_group_id'),
+							 'location'						=> 'address'
+							);
 
-		$custom_fields = $this->model_account_custom_field->getCustomFields($this->config->get('config_customer_group_id'));
+		$custom_fields = $this->model_account_custom_field->getCustomFields($filter_data);
 
 		foreach ($custom_fields as $custom_field) {
-			if ($custom_field['location'] == 'address') {
-				if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']])) {
-					$this->error['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
-				} elseif (($custom_field['type'] == 'text') && !empty($custom_field['validation']) && filter_var($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/' . html_entity_decode($custom_field['validation'], ENT_QUOTES, 'UTF-8') . '/')))) {
-					$this->error['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
-				}
-			}
+			if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']])) {
+				$this->error['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
+			} elseif (($custom_field['type'] == 'text') && !empty($custom_field['validation']) && filter_var($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/' . html_entity_decode($custom_field['validation'], ENT_QUOTES, 'UTF-8') . '/')))) {
+				$this->error['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
+			}			
 		}
 
 		return !$this->error;

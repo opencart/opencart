aab52f1c5b836a8362d2c1613af548a3ff11d63d
straightlight <32371528+straightlight@users.noreply.github.com>
2020-03-23 17:29:21 -0400

    Added filters on custom fields

--- upload/catalog/controller/checkout/checkout.php
+++ upload/catalog/controller/checkout/checkout.php
@@ -127,8 +127,11 @@ class ControllerCheckoutCheckout extends Controller {
 		} else {
 			$customer_group_id = $this->config->get('config_customer_group_id');
 		}
+		
+		$filter_data = array('customer_group_id'			=> $customer_group_id
+							);
 
-		$custom_fields = $this->model_account_custom_field->getCustomFields($customer_group_id);
+		$custom_fields = $this->model_account_custom_field->getCustomFields($filter_data);
 
 		foreach ($custom_fields as $custom_field) {
 			$json[] = array(

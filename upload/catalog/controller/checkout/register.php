f04c0ae599f27d2109ccd6954283a0d88b9dfbbc
straightlight <32371528+straightlight@users.noreply.github.com>
2020-03-23 17:34:28 -0400

    Added filters on custom fields

--- upload/catalog/controller/checkout/register.php
+++ upload/catalog/controller/checkout/register.php
@@ -45,8 +45,12 @@ class ControllerCheckoutRegister extends Controller {
 
 		// Custom Fields
 		$this->load->model('account/custom_field');
+		
+		$filter_data = array('location'				=> 'account',
+							 'address'				=> true
+							);
 
-		$data['custom_fields'] = $this->model_account_custom_field->getCustomFields();
+		$data['custom_fields'] = $this->model_account_custom_field->getCustomFields($filter_data);
 
 		// Captcha
 		if ($this->config->get('captcha_' . $this->config->get('config_captcha') . '_status') && in_array('register', (array)$this->config->get('config_captcha_page'))) {
@@ -182,8 +186,11 @@ class ControllerCheckoutRegister extends Controller {
 
 			// Custom field validation
 			$this->load->model('account/custom_field');
+			
+			$filter_data = array('customer_group_id'			=> $customer_group_id
+								);
 
-			$custom_fields = $this->model_account_custom_field->getCustomFields($customer_group_id);
+			$custom_fields = $this->model_account_custom_field->getCustomFields($filter_data);
 
 			foreach ($custom_fields as $custom_field) {
 				if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['location']][$custom_field['custom_field_id']])) {

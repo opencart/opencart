b6def698739d5710cdec1488f1e1c17bda06e424
Daniel Kerr <webmaster@opencart.com>
2020-03-25 23:35:20 +0800

    Added PayPal Commerce Platform

---
+++ upload/admin/controller/extension/module/paypal_smart_button.php
@@ -0,0 +1,90 @@
+<?php
+class ControllerExtensionModulePayPalSmartButton extends Controller {
+	private $error = array();
+	
+	public function index() {
+		$this->load->language('extension/module/paypal_smart_button');
+
+		$this->load->model('extension/module/paypal_smart_button');
+		$this->load->model('setting/setting');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			$this->model_setting_setting->editSetting('module_paypal_smart_button', $this->request->post);
+
+			$this->session->data['success'] = $this->language->get('success_save');
+
+			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
+		}
+
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_extensions'),
+			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('extension/module/paypal_smart_button', 'user_token=' . $this->session->data['user_token'], true)
+		);
+
+		$data['action'] = $this->url->link('extension/module/paypal_smart_button', 'user_token=' . $this->session->data['user_token'], true);
+		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
+						
+		if (isset($this->request->post['module_paypal_smart_button_status'])) {
+			$data['status'] = $this->request->post['module_paypal_smart_button_status'];
+		} else {
+			$data['status'] = $this->config->get('module_paypal_smart_button_status');
+		}
+		
+		// Setting 		
+		$_config = new Config();
+		$_config->load('paypal_smart_button');
+		
+		$data['setting'] = $_config->get('paypal_smart_button_setting');
+		
+		if (isset($this->request->post['module_paypal_smart_button_setting'])) {
+			$data['setting'] = array_replace_recursive((array)$data['setting'], (array)$this->request->post['module_paypal_smart_button_setting']);
+		} else {
+			$data['setting'] = array_replace_recursive((array)$data['setting'], (array)$this->config->get('module_paypal_smart_button_setting'));
+		}
+				
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('extension/module/paypal_smart_button', $data));
+	}
+
+	public function install() {
+		$this->load->model('extension/module/paypal_smart_button');
+		$this->load->model('setting/setting');
+
+		$this->model_extension_module_paypal_smart_button->install();
+		
+		$setting['module_paypal_smart_button_status'] = 0;
+		
+		$this->model_setting_setting->editSetting('module_paypal_smart_button', $setting);
+	}
+
+	protected function validate() {
+		if (!$this->user->hasPermission('modify', 'extension/module/paypal_smart_button')) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
+
+		return !$this->error;
+	}
+}
---
+++ upload/catalog/language/en-gb/extension/module/paypal_smart_button.php
@@ -0,0 +1,75 @@
+<?php
+// Text
+$_['text_title']						= 'PayPal';
+$_['text_checkout_payment_address']  	= 'Billing Details';
+$_['text_checkout_shipping_address'] 	= 'Delivery Details';
+$_['text_checkout_shipping_method']  	= 'Delivery Method';
+$_['text_checkout_payment_method']  	= 'Payment Method';
+$_['text_your_details']              	= 'Your Personal Details';
+$_['text_your_address']              	= 'Your Address';
+$_['text_cart']               			= 'Shopping Cart';
+$_['text_shipping_updated']   			= 'Shipping service updated';
+$_['text_order_message']				= 'PayPal Seller Protection - %s';
+$_['text_day']                 			= 'day';
+$_['text_week']                			= 'week';
+$_['text_semi_month']          			= 'half-month';
+$_['text_month']               			= 'month';
+$_['text_year']                			= 'year';
+$_['text_trial']               			= '%s every %s %s for %s payments then ';
+$_['text_recurring']          			= '%s every %s %s';
+$_['text_payment_cancel']      			= 'until cancelled';
+$_['text_recurring_item']      			= 'Recurring Item';
+$_['text_payment_recurring']   			= 'Payment Profile';
+$_['text_trial_description']   			= '%s every %d %s(s) for %d payment(s) then';
+$_['text_payment_description'] 			= '%s every %d %s(s) for %d payment(s)';
+$_['text_payment_cancel']      			= '%s every %d %s(s) until canceled';
+
+// Column
+$_['column_image']             			= 'Image';
+$_['column_name']              			= 'Product Name';
+$_['column_model']             			= 'Model';
+$_['column_quantity']          			= 'Quantity';
+$_['column_price']             			= 'Unit Price';
+$_['column_total']             			= 'Total';
+
+// Entry
+$_['entry_email']                    	= 'E-Mail';
+$_['entry_firstname']                	= 'First Name';
+$_['entry_lastname']                 	= 'Last Name';
+$_['entry_telephone']               	= 'Telephone';
+$_['entry_company']                  	= 'Company';
+$_['entry_address_1']                	= 'Address 1';
+$_['entry_address_2']                	= 'Address 2';
+$_['entry_postcode']                 	= 'Post Code';
+$_['entry_city']                     	= 'City';
+$_['entry_country']                  	= 'Country';
+$_['entry_zone']                    	= 'Region / State';
+
+// Button
+$_['button_confirm']  					= 'Confirm';
+$_['button_shipping'] 					= 'Update shipping';
+
+// Error
+$_['error_stock']              			= 'Products marked with *** are not available in the desired quantity or not in stock!';
+$_['error_minimum']            			= 'Minimum order amount for %s is %s!';
+$_['error_required']           			= '%s required!';
+$_['error_product']            			= 'Warning: There are no products in your cart!';
+$_['error_recurring_required'] 			= 'Please select a payment recurring!';
+$_['error_unavailable'] 	  			= 'Please use the full checkout with this order!';
+$_['error_shipping']                 	= 'Warning: Shipping method required!';
+$_['error_no_shipping']    				= 'Warning: No Shipping options are available.';
+$_['error_firstname']                	= 'First Name must be between 1 and 32 characters!';
+$_['error_lastname']                 	= 'Last Name must be between 1 and 32 characters!';
+$_['error_email']                    	= 'E-Mail address does not appear to be valid!';
+$_['error_telephone']                	= 'Telephone must be between 3 and 32 characters!';
+$_['error_password']                 	= 'Password must be between 4 and 20 characters!';
+$_['error_confirm']                  	= 'Password confirmation does not match password!';
+$_['error_address_1']                	= 'Address 1 must be between 3 and 128 characters!';
+$_['error_city']                     	= 'City must be between 2 and 128 characters!';
+$_['error_postcode']                 	= 'Postcode must be between 2 and 10 characters!';
+$_['error_country']                  	= 'Please select a country!';
+$_['error_zone']                     	= 'Please select a region / state!';
+$_['error_agree']                    	= 'Warning: You must agree to the %s!';
+$_['error_address']                  	= 'Warning: You must select address!';
+$_['error_custom_field']             	= '%s required!';
+$_['error_timeout'] 	  				= 'Sorry, PayPal is currently busy. Please try again later!';

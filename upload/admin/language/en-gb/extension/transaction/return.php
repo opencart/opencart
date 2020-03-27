3c20661981ceeeb00341b7bdda6b9d9a905c069c
straightlight <32371528+straightlight@users.noreply.github.com>
2020-03-27 10:58:04 -0400

    Added transaction/return.php file.

---
+++ upload/admin/language/en-gb/extension/transaction/return.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']         = 'Return - Custom Field Approvals';
+
+// Text
+$_['text_success']          = 'Success: You have modified Return - Custom Field Approvals!';
+$_['text_list']             = 'Return - Custom Field Approvals List';
+$_['text_status_inactive']	= 'Inactive';
+$_['text_status_active']	= 'Active';
+
+// Column
+$_['column_custom_field']	= 'Custom Field';
+$_['column_status']			= 'Status';
+$_['column_action']         = 'Action';
+
+// Entry
+$_['entry_custom_field']    = 'Custom Field';
+$_['entry_status']          = 'Status';
+
+// Tab
+$_['tab_return']			= 'Return';
+$_['tab_status']			= 'Status';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify Return - Custom Field Approvals!';

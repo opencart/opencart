149473f2fe15a2eb3543453dbd708ea6b226319f
Daniel Kerr <webmaster@opencart.com>
2020-05-21 22:28:00 +0800

    fixed missing keyword issue

--- upload/admin/controller/catalog/product.php
+++ upload/admin/controller/catalog/product.php
@@ -1351,7 +1351,7 @@ class ControllerCatalogProduct extends Controller {
 			foreach ($this->request->post['product_seo_url'] as $store_id => $language) {
 				foreach ($language as $language_id => $keyword) {
 					if ($keyword) {
-						$seo_url_info = $this->model_design_seo_url->getSeoUrlByKeyword($keyword);
+						$seo_url_info = $this->model_design_seo_url->getSeoUrlByKeyword($keyword, $store_id, $language_id);
 
 						if ($seo_url_info && ($seo_url_info['key'] != 'product_id' || !isset($this->request->get['product_id']) || $seo_url_info['value'] != $this->request->get['product_id'])) {
 							$this->error['keyword'][$store_id][$language_id] = $this->language->get('error_keyword');

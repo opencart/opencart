45562e53f8282032ae8312c90269148f779c00ea
Anton <stalker780@users.noreply.github.com>
2020-06-02 16:06:54 +0300

    Product thumb - reduce repeated code
    Product thumb is a great idea and a blow of fresh air in opencart code structure.
    But we can reduce more repeated code

--- upload/catalog/controller/product/thumb.php
+++ upload/catalog/controller/product/thumb.php
@@ -1,7 +1,43 @@
 <?php
 class ControllerProductThumb extends Controller {
-	public function index($data) {
+	public function index($product_info) {
 		$this->load->language('product/thumb');
+
+		$data['product_id'] = $product_info['product_id'];
+
+		$data['name'] = $product_info['name'];
+
+		$data['description'] = utf8_substr(trim(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..';
+
+		if (is_file(DIR_IMAGE . html_entity_decode($product_info['image'], ENT_QUOTES, 'UTF-8'))) {
+			$data['thumb'] = $this->model_tool_image->resize(html_entity_decode($product_info['image'], ENT_QUOTES, 'UTF-8'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+		} else {
+			$data['thumb'] = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+		}
+
+		if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+			$data['price'] = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+		} else {
+			$data['price'] = false;
+		}
+
+		if ((float)$product_info['special']) {
+			$data['special'] = $this->currency->format($this->tax->calculate($product_info['special'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+		} else {
+			$data['special'] = false;
+		}
+
+		if ($this->config->get('config_tax')) {
+			$data['tax'] = $this->currency->format((float)$product_info['special'] ? $product_info['special'] : $product_info['price'], $this->session->data['currency']);
+		} else {
+			$data['tax'] = false;
+		}
+
+		$data['minimum'] = $product_info['minimum'] > 0 ? $product_info['minimum'] : 1;
+
+		$data['rating'] = $product_info['rating'];
+
+		$data['href'] = $this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&path=' . $this->request->get['path'] . '&product_id=' . $product_info['product_id'] . $url);
 
 		$data['review_status'] = $this->config->get('config_review_status');
 

79b193298224e2cb535b0744e626f6206b01aded
Anton <stalker780@users.noreply.github.com>
2020-06-16 15:49:22 +0300

    Product thumb - reduce repeated code
    Update for #7976
    Another try for product thumb. Now optional $url and $setting variables can also be passed to thumb controller.

--- upload/catalog/controller/product/special.php
+++ upload/catalog/controller/product/special.php
@@ -81,44 +81,7 @@ class ControllerProductSpecial extends Controller {
 		$results = $this->model_catalog_product->getSpecials($filter_data);
 
 		foreach ($results as $result) {
-			if (is_file(DIR_IMAGE . html_entity_decode($result['image'], ENT_QUOTES, 'UTF-8'))) {
-				$image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
-			} else {
-				$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
-			}
-
-			if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-				$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-			} else {
-				$price = false;
-			}
-
-			if ((float)$result['special']) {
-				$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-			} else {
-				$special = false;
-			}
-
-			if ($this->config->get('config_tax')) {
-				$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
-			} else {
-				$tax = false;
-			}
-
-			$product_data = array(
-				'product_id'  => $result['product_id'],
-				'thumb'       => $image,
-				'name'        => $result['name'],
-				'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
-				'price'       => $price,
-				'special'     => $special,
-				'tax'         => $tax,
-				'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
-				'rating'      => $result['rating'],
-				'href'        => $this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&product_id=' . $result['product_id'] . $url)
-			);
-
-			$data['products'][] = $this->load->controller('product/thumb', $product_data);
+			$data['products'][] = $this->load->controller('product/thumb', $result, $url);
 		}
 
 		$url = '';
--- upload/catalog/controller/product/thumb.php
+++ upload/catalog/controller/product/thumb.php
@@ -1,7 +1,48 @@
 <?php
 class ControllerProductThumb extends Controller {
-	public function index($data) {
+	public function index($product_info, $url = '', $setting = array()) {
 		$this->load->language('product/thumb');
+
+		$data['setting'] = $setting;
+
+		$data['product_id'] = $product_info['product_id'];
+
+		$data['name'] = $product_info['name'];
+
+		$data['description'] = utf8_substr(trim(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..';
+
+		$image_width = isset($setting['width']) ? $setting['width'] : $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width');
+		$image_height = isset($setting['height']) ? $setting['height'] : $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height');
+
+		if (is_file(DIR_IMAGE . html_entity_decode($product_info['image'], ENT_QUOTES, 'UTF-8'))) {
+			$data['thumb'] = $this->model_tool_image->resize(html_entity_decode($product_info['image'], ENT_QUOTES, 'UTF-8'), $image_width, $image_height);
+		} else {
+			$data['thumb'] = $this->model_tool_image->resize('placeholder.png', $image_width, $image_height);
+		}
+
+		if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+			$data['price'] = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+		} else {
+			$data['price'] = false;
+		}
+
+		if ((float)$product_info['special']) {
+			$data['special'] = $this->currency->format($this->tax->calculate($product_info['special'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+		} else {
+			$data['special'] = false;
+		}
+
+		if ($this->config->get('config_tax')) {
+			$data['tax'] = $this->currency->format((float)$product_info['special'] ? $product_info['special'] : $product_info['price'], $this->session->data['currency']);
+		} else {
+			$data['tax'] = false;
+		}
+
+		$data['minimum'] = $product_info['minimum'] > 0 ? $product_info['minimum'] : 1;
+
+		$data['rating'] = $product_info['rating'];
+
+		$data['href'] = $this->url->link('product/product', 'language=' . $this->config->get('config_language') . $url . '&product_id=' . $product_info['product_id']);
 
 		$data['review_status'] = $this->config->get('config_review_status');
 

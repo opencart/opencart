4106891e3114aeb5629008b78f46a0639bfb6240
Daniel Kerr <webmaster@opencart.com>
2020-07-07 17:51:19 +0800

    updated minimum requirement for php

--- upload/system/startup.php
+++ upload/system/startup.php
@@ -3,8 +3,8 @@
 error_reporting(E_ALL);
 
 // Check Version
-if (version_compare(phpversion(), '5.4.0', '<') == true) {
-	exit('PHP5.4+ Required');
+if (version_compare(phpversion(), '7.1.0', '<') == true) {
+	exit('PHP7.1+ Required');
 }
 
 if (!ini_get('date.timezone')) {

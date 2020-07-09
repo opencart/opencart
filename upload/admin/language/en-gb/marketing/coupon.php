556665f9c03755c90502b7a7d588f11067773d74
OSWorX <info@osworx.net>
2020-07-09 12:51:18 +0200

    Update coupon error text
    According to this commit https://github.com/opencart/opencart/commit/6b83ae0895cae5277e72bcd0121e68d303d1433c
    is here the corrected text.

--- upload/admin/language/en-gb/marketing/coupon.php
+++ upload/admin/language/en-gb/marketing/coupon.php
@@ -54,4 +54,4 @@ $_['help_uses_customer']  = 'The maximum number of times the coupon can be used 
 $_['error_permission']    = 'Warning: You do not have permission to modify coupons!';
 $_['error_exists']        = 'Warning: Coupon code is already in use!';
 $_['error_name']          = 'Coupon Name must be between 3 and 128 characters!';
-$_['error_code']          = 'Code must be between 3 and 10 characters!';
+$_['error_code']          = 'Code must be between 3 and 20 characters!';

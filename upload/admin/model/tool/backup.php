b67e4d8d5a00e5546af942eb09d5b66176b5efaa
Daniel Kerr <danielkerr@users.noreply.github.com>
2020-07-18 19:59:05 +0800

    https://github.com/opencart/opencart/pull/6711

--- upload/admin/model/tool/backup.php
+++ upload/admin/model/tool/backup.php
@@ -6,10 +6,8 @@ class ModelToolBackup extends Model {
 		$query = $this->db->query("SHOW TABLES FROM `" . DB_DATABASE . "`");
 
 		foreach ($query->rows as $result) {
-			if (utf8_substr($result['Tables_in_' . DB_DATABASE], 0, strlen(DB_PREFIX)) == DB_PREFIX) {
-				if (isset($result['Tables_in_' . DB_DATABASE])) {
-					$table_data[] = $result['Tables_in_' . DB_DATABASE];
-				}
+			if (isset($result['Tables_in_' . DB_DATABASE]) && utf8_substr($result['Tables_in_' . DB_DATABASE], 0, strlen(DB_PREFIX)) == DB_PREFIX) {
+				$table_data[] = $result['Tables_in_' . DB_DATABASE];
 			}
 		}
 

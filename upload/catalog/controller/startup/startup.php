cc7924742dad4cbf6d716116745d5369aa8db8c5
Daniel Kerr <danielkerr@users.noreply.github.com>
2020-07-20 00:00:40 +0800

    Update startup.php

--- upload/catalog/controller/startup/startup.php
+++ upload/catalog/controller/startup/startup.php
@@ -243,8 +243,6 @@ class ControllerStartupStartup extends Controller {
 		$this->registry->set('tax', new Cart\Tax($this->registry));
 
 		if (isset($this->session->data['shipping_address'])) {
-			print_r($this->session->data);
-
 			$this->tax->setShippingAddress($this->session->data['shipping_address']['country_id'], $this->session->data['shipping_address']['zone_id']);
 		} elseif ($this->config->get('config_tax_default') == 'shipping') {
 			$this->tax->setShippingAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));

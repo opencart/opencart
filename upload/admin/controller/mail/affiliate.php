5966eae68943807aa10df250d94e9643bdae08df
Ekrem KAYA <webmaster@e-piksel.com>
2020-07-21 00:23:25 +0300

    Fix links in approve mails https://github.com/opencart/opencart/pull/7940/files

--- upload/admin/controller/mail/affiliate.php
+++ upload/admin/controller/mail/affiliate.php
@@ -2,24 +2,24 @@
 class ControllerMailAffiliate extends Controller {
 	public function approve(&$route, &$args, &$output) {
 		$this->load->model('customer/customer');
-		
+
 		$customer_info = $this->model_customer_customer->getCustomer($args[0]);
 
 		if ($customer_info) {
 			$this->load->model('setting/store');
-	
+
 			$store_info = $this->model_setting_store->getStore($customer_info['store_id']);
-	
+
 			if ($store_info) {
 				$store_name = html_entity_decode($store_info['name'], ENT_QUOTES, 'UTF-8');
-				$store_url = $store_info['url'] . 'index.php?route=account/login';
+				$store_url = $store_info['url'];
 			} else {
 				$store_name = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
-				$store_url = HTTP_CATALOG . 'index.php?route=account/login';
+				$store_url = HTTP_CATALOG;
 			}
 
 			$this->load->model('localisation/language');
-			
+
 			$language_info = $this->model_localisation_language->getLanguage($customer_info['language_id']);
 
 			if ($language_info) {
@@ -27,18 +27,18 @@ class ControllerMailAffiliate extends Controller {
 			} else {
 				$language_code = $this->config->get('config_language');
 			}
-			
+
 			$language = new Language($language_code);
 			$language->load($language_code);
 			$language->load('mail/affiliate_approve');
-							
+
 			$subject = sprintf($language->get('text_subject'), $store_name);
-			
+
 			$data['text_welcome'] = sprintf($language->get('text_welcome'), $store_name);
-						
-			$data['login'] = $store_url . 'index.php?route=account/login';
+
+			$data['login'] = $store_url . 'index.php?route=affiliate/login';
 			$data['store'] = $store_name;
-	
+
 			$mail = new Mail($this->config->get('config_mail_engine'));
 			$mail->parameter = $this->config->get('config_mail_parameter');
 			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
@@ -46,7 +46,7 @@ class ControllerMailAffiliate extends Controller {
 			$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
 			$mail->smtp_port = $this->config->get('config_mail_smtp_port');
 			$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
-	
+
 			$mail->setTo($customer_info['email']);
 			$mail->setFrom($this->config->get('config_email'));
 			$mail->setSender($store_name);
@@ -55,10 +55,10 @@ class ControllerMailAffiliate extends Controller {
 			$mail->send();
 		}
 	}
-	
+
 	public function deny(&$route, &$args, &$output) {
 		$this->load->model('customer/customer');
-		
+
 		$customer_info = $this->model_customer_customer->getCustomer($args[0]);
 
 		if ($customer_info) {
@@ -68,14 +68,14 @@ class ControllerMailAffiliate extends Controller {
 
 			if ($store_info) {
 				$store_name = html_entity_decode($store_info['name'], ENT_QUOTES, 'UTF-8');
-				$store_url = $store_info['url'] . 'index.php?route=account/login';
+				$store_url = $store_info['url'];
 			} else {
 				$store_name = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
-				$store_url = HTTP_CATALOG . 'index.php?route=account/login';
+				$store_url = HTTP_CATALOG;
 			}
 
 			$this->load->model('localisation/language');
-			
+
 			$language_info = $this->model_localisation_language->getLanguage($customer_info['language_id']);
 
 			if ($language_info) {
@@ -87,14 +87,14 @@ class ControllerMailAffiliate extends Controller {
 			$language = new Language($language_code);
 			$language->load($language_code);
 			$language->load('mail/affiliate_deny');
-				
-			$subject = sprintf($language->get('text_subject'), $store_name);	
-				
+
+			$subject = sprintf($language->get('text_subject'), $store_name);
+
 			$data['text_welcome'] = sprintf($language->get('text_welcome'), $store_name);
-			
-			$data['contact'] = $store_url . 'index.php?route=information/contact';	
+
+			$data['contact'] = $store_url . 'index.php?route=information/contact';
 			$data['store'] = $store_name;
-			
+
 			$mail = new Mail($this->config->get('config_mail_engine'));
 			$mail->parameter = $this->config->get('config_mail_parameter');
 			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
@@ -111,4 +111,4 @@ class ControllerMailAffiliate extends Controller {
 			$mail->send();
 		}
 	}
-}	
+}

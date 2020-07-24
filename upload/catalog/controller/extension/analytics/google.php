2223306ec5ba98b9850fa9b711e2e78bd2eeff19
straightlight <32371528+straightlight@users.noreply.github.com>
2020-07-24 10:59:40 -0400

    Delete google.php

--- upload/catalog/controller/extension/analytics/google.php
+++
@@ -1,6 +0,0 @@
-<?php
-class ControllerExtensionAnalyticsGoogle extends Controller {
-    public function index() {
-		return html_entity_decode($this->config->get('analytics_google_code'), ENT_QUOTES, 'UTF-8');
-	}
-}

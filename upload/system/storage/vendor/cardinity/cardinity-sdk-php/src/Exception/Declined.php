1f86fdbe48d22804ea4615c51ab7214989339469
straightlight <32371528+straightlight@users.noreply.github.com>
2020-07-24 08:43:40 -0400

    Delete Declined.php

--- upload/system/storage/vendor/cardinity/cardinity-sdk-php/src/Exception/Declined.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Cardinity\Exception;
-
-class Declined extends Request
-{
-    protected $code = 402;
-    protected $message = 'Request Failed – Your request was valid but it was declined.';
-}

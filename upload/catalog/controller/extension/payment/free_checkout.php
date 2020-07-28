d2f014357a32307c4b3e37a9aa66e9b1a21fe992
Ekrem KAYA <webmaster@e-piksel.com>
2020-07-28 22:35:57 +0300

    Fixed free checkout json confirm and clear whitespace

--- upload/catalog/controller/extension/payment/free_checkout.php
+++ upload/catalog/controller/extension/payment/free_checkout.php
@@ -1,16 +1,21 @@
 <?php
 class ControllerExtensionPaymentFreeCheckout extends Controller {
 	public function index() {
-		$data['continue'] = $this->url->link('checkout/success', 'language=' . $this->config->get('config_language'));
-
-		return $this->load->view('extension/payment/free_checkout', $data);
+		return $this->load->view('extension/payment/free_checkout');
 	}
 
 	public function confirm() {
+		$json = array();
+
 		if ($this->session->data['payment_method']['code'] == 'free_checkout') {
 			$this->load->model('checkout/order');
 
 			$this->model_checkout_order->addHistory($this->session->data['order_id'], $this->config->get('payment_free_checkout_order_status_id'));
+
+			$json['redirect'] = str_replace( '&amp;', '&', $this->url->link('checkout/success', 'language=' . $this->config->get('config_language')));
 		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
 	}
 }

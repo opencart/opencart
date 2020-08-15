e085b24a54e4acce0557c709bbc95263223c5dcf
engwalid1971 <engwalid1971@gmail.com>
2020-08-16 01:35:16 +0200

    Updated language files

--- upload/admin/language/en-gb/design/banner.php
+++ upload/admin/language/en-gb/design/banner.php
@@ -1,26 +1,26 @@
 <?php
 // Heading
-$_['heading_title']     = 'Banners';
+$_['heading_title']    = 'Banners';
 
 // Text
-$_['text_success']      = 'Success: You have modified banners!';
-$_['text_list']         = 'Banner List';
-$_['text_add']          = 'Add Banner';
-$_['text_edit']         = 'Edit Banner';
-$_['text_default']      = 'Default';
+$_['text_success']     = 'Success: You have modified banners!';
+$_['text_list']        = 'Banner List';
+$_['text_add']         = 'Add Banner';
+$_['text_edit']        = 'Edit Banner';
+$_['text_default']     = 'Default';
 
 // Column
-$_['column_name']       = 'Banner Name';
-$_['column_status']     = 'Status';
-$_['column_action']     = 'Action';
+$_['column_name']      = 'Banner Name';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Action';
 
 // Entry
-$_['entry_name']        = 'Banner Name';
-$_['entry_title']       = 'Title';
-$_['entry_link']        = 'Link';
-$_['entry_image']       = 'Image';
-$_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_name']       = 'Banner Name';
+$_['entry_title']      = 'Title';
+$_['entry_link']       = 'Link';
+$_['entry_image']      = 'Image';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify banners!';

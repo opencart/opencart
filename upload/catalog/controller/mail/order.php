649198196ff08e7171bb20d576187a7c931c9c0f
GautamMKGarg <GautamMKgarg@gmail.com>
2020-08-18 02:00:09 +0530

    Update order.php
    The language Editor option was not working for order emails. Hence fixed it.

--- upload/catalog/controller/mail/order.php
+++ upload/catalog/controller/mail/order.php
@@ -57,34 +57,32 @@ class ControllerMailOrder extends Controller {
 		}
 
 		// Load the language for any mails that might be required to be sent out
-		$language = new Language($order_info['language_code']);
-		$language->load($order_info['language_code']);
-		$language->load('mail/order_add');
+		$this->load->language('mail/order_add');
 
 		// HTML Mail
-		$data['title'] = sprintf($language->get('text_subject'), $order_info['store_name'], $order_info['order_id']);
+		$data['title'] = sprintf($this->language->get('text_subject'), $order_info['store_name'], $order_info['order_id']);
 
-		$data['text_greeting'] = sprintf($language->get('text_greeting'), $order_info['store_name']);
-		$data['text_link'] = $language->get('text_link');
-		$data['text_download'] = $language->get('text_download');
-		$data['text_order_detail'] = $language->get('text_order_detail');
-		$data['text_instruction'] = $language->get('text_instruction');
-		$data['text_order_id'] = $language->get('text_order_id');
-		$data['text_date_added'] = $language->get('text_date_added');
-		$data['text_payment_method'] = $language->get('text_payment_method');
-		$data['text_shipping_method'] = $language->get('text_shipping_method');
-		$data['text_email'] = $language->get('text_email');
-		$data['text_telephone'] = $language->get('text_telephone');
-		$data['text_ip'] = $language->get('text_ip');
-		$data['text_order_status'] = $language->get('text_order_status');
-		$data['text_payment_address'] = $language->get('text_payment_address');
-		$data['text_shipping_address'] = $language->get('text_shipping_address');
-		$data['text_product'] = $language->get('text_product');
-		$data['text_model'] = $language->get('text_model');
-		$data['text_quantity'] = $language->get('text_quantity');
-		$data['text_price'] = $language->get('text_price');
-		$data['text_total'] = $language->get('text_total');
-		$data['text_footer'] = $language->get('text_footer');
+		$data['text_greeting'] = sprintf($this->language->get('text_greeting'), $order_info['store_name']);
+		$data['text_link'] = $this->language->get('text_link');
+		$data['text_download'] = $this->language->get('text_download');
+		$data['text_order_detail'] = $this->language->get('text_order_detail');
+		$data['text_instruction'] = $this->language->get('text_instruction');
+		$data['text_order_id'] = $this->language->get('text_order_id');
+		$data['text_date_added'] = $this->language->get('text_date_added');
+		$data['text_payment_method'] = $this->language->get('text_payment_method');
+		$data['text_shipping_method'] = $this->language->get('text_shipping_method');
+		$data['text_email'] = $this->language->get('text_email');
+		$data['text_telephone'] = $this->language->get('text_telephone');
+		$data['text_ip'] = $this->language->get('text_ip');
+		$data['text_order_status'] = $this->language->get('text_order_status');
+		$data['text_payment_address'] = $this->language->get('text_payment_address');
+		$data['text_shipping_address'] = $this->language->get('text_shipping_address');
+		$data['text_product'] = $this->language->get('text_product');
+		$data['text_model'] = $this->language->get('text_model');
+		$data['text_quantity'] = $this->language->get('text_quantity');
+		$data['text_price'] = $this->language->get('text_price');
+		$data['text_total'] = $this->language->get('text_total');
+		$data['text_footer'] = $this->language->get('text_footer');
 
 		$this->load->model('tool/image');
 
@@ -106,7 +104,7 @@ class ControllerMailOrder extends Controller {
 		}
 
 		$data['order_id'] = $order_info['order_id'];
-		$data['date_added'] = date($language->get('date_format_short'), strtotime($order_info['date_added']));
+		$data['date_added'] = date($this->language->get('date_format_short'), strtotime($order_info['date_added']));
 		$data['payment_method'] = $order_info['payment_method'];
 		$data['shipping_method'] = $order_info['shipping_method'];
 		$data['email'] = $order_info['email'];
@@ -277,25 +275,23 @@ class ControllerMailOrder extends Controller {
 		$mail->setTo($order_info['email']);
 		$mail->setFrom($from);
 		$mail->setSender(html_entity_decode($order_info['store_name'], ENT_QUOTES, 'UTF-8'));
-		$mail->setSubject(html_entity_decode(sprintf($language->get('text_subject'), $order_info['store_name'], $order_info['order_id']), ENT_QUOTES, 'UTF-8'));
+		$mail->setSubject(html_entity_decode(sprintf($this->language->get('text_subject'), $order_info['store_name'], $order_info['order_id']), ENT_QUOTES, 'UTF-8'));
 		$mail->setHtml($this->load->view('mail/order_add', $data));
 		$mail->send();
 	}
 
 	public function edit($order_info, $order_status_id, $comment, $notify) {
-		$language = new Language($order_info['language_code']);
-		$language->load($order_info['language_code']);
-		$language->load('mail/order_edit');
+		$this->load->language('mail/order_edit');
 
-		$data['text_order_id'] = $language->get('text_order_id');
-		$data['text_date_added'] = $language->get('text_date_added');
-		$data['text_order_status'] = $language->get('text_order_status');
-		$data['text_link'] = $language->get('text_link');
-		$data['text_comment'] = $language->get('text_comment');
-		$data['text_footer'] = $language->get('text_footer');
+		$data['text_order_id'] = $this->language->get('text_order_id');
+		$data['text_date_added'] = $this->language->get('text_date_added');
+		$data['text_order_status'] = $this->language->get('text_order_status');
+		$data['text_link'] = $this->language->get('text_link');
+		$data['text_comment'] = $this->language->get('text_comment');
+		$data['text_footer'] = $this->language->get('text_footer');
 
 		$data['order_id'] = $order_info['order_id'];
-		$data['date_added'] = date($language->get('date_format_short'), strtotime($order_info['date_added']));
+		$data['date_added'] = date($this->language->get('date_format_short'), strtotime($order_info['date_added']));
 		$data['store_url'] = $this->config->get('config_url');
 		$data['store'] = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
 
@@ -334,7 +330,7 @@ class ControllerMailOrder extends Controller {
 		$mail->setTo($order_info['email']);
 		$mail->setFrom($from);
 		$mail->setSender(html_entity_decode($order_info['store_name'], ENT_QUOTES, 'UTF-8'));
-		$mail->setSubject(html_entity_decode(sprintf($language->get('text_subject'), $order_info['store_name'], $order_info['order_id']), ENT_QUOTES, 'UTF-8'));
+		$mail->setSubject(html_entity_decode(sprintf($this->language->get('text_subject'), $order_info['store_name'], $order_info['order_id']), ENT_QUOTES, 'UTF-8'));
 		$mail->setText($this->load->view('mail/order_edit', $data));
 		$mail->send();
 	}

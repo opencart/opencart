e24580b0a930ede34e29434a916d87c0066638db
Ekrem KAYA <webmaster@e-piksel.com>
2020-08-17 19:59:54 +0300

    Fix require dir for codemirror

--- upload/admin/view/javascript/codemirror/mode/htmlmixed.js
+++ upload/admin/view/javascript/codemirror/mode/htmlmixed.js
@@ -3,9 +3,9 @@
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"), require("../xml/xml"), require("../javascript/javascript"), require("../css/css"));
+    mod(require("../lib/codemirror"), require("./xml"), require("./javascript"), require("./css"));
   else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror", "../xml/xml", "../javascript/javascript", "../css/css"], mod);
+    define(["../lib/codemirror", "./xml", "./javascript", "./css"], mod);
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {

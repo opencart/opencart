acf6dea18343c19d76f9ddba6cefbf6bce66f8de
arshidkv12 <arshidkv12@gmail.com>
2020-09-04 16:09:40 +0530

    Bug fixed

--- upload/system/engine/event.php
+++ upload/system/engine/event.php
@@ -62,7 +62,7 @@ class Event {
 			if (preg_match('/^' . str_replace(['\*', '\?'], ['.*', '.'], preg_quote($value['trigger'], '/')) . '/', $event)) {
 				$result = $value['action']->execute($this->registry, $args);
 
-				if (!is_null($result) && !($result instanceof Exception)) {
+				if (!is_null($result) && !($result instanceof \Exception)) {
 					return $result;
 				}
 			}

8c678fdb190c55ae6d2f8aa02475030ce4e42a40
Alexandr <qwerty541zxc@gmail.com>
2020-09-04 20:27:08 +0300

    code style fixes

--- upload/catalog/controller/checkout/guest_shipping.php
+++ upload/catalog/controller/checkout/guest_shipping.php
@@ -1,6 +1,6 @@
 <?php
 namespace Application\Controller\Checkout;
-class GuestShipping extends \System\Engine\Controller{
+class GuestShipping extends \System\Engine\Controller {
 	public function index() {
 		$this->load->language('checkout/checkout');
 

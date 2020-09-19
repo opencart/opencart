1b5d51929fbeeef19fd396887406ceb6e9ec1dda
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-19 17:12:27 -0400

    Added methods for libraries to follow.

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -966,6 +966,45 @@ class Product extends \Opencart\System\Engine\Model {
 
 		return $query->rows;
 	}
+	
+	public function getOptionsByProductOptionId($product_option_id, $product_id) {
+		$product_option_data = [];
+
+		$product_option_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_option po LEFT JOIN `" . DB_PREFIX . "option` o ON (po.option_id = o.option_id) LEFT JOIN " . DB_PREFIX . "option_description od ON (o.option_id = od.option_id) WHERE `po`.`product_option_id` = '" . (int)$product_option_id . "' AND po.product_id = '" . (int)$product_id . "' AND od.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY o.sort_order");
+
+		foreach ($product_option_query->rows as $product_option) {
+			$product_option_value_data = [];
+
+			$product_option_value_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_option_value pov LEFT JOIN " . DB_PREFIX . "option_value ov ON (pov.option_value_id = ov.option_value_id) LEFT JOIN " . DB_PREFIX . "option_value_description ovd ON (ov.option_value_id = ovd.option_value_id) WHERE pov.product_id = '" . (int)$product_id . "' AND pov.product_option_id = '" . (int)$product_option['product_option_id'] . "' AND ovd.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY ov.sort_order");
+
+			foreach ($product_option_value_query->rows as $product_option_value) {
+				$product_option_value_data[$product_option_value['product_option_value_id']] = [
+					'product_option_value_id' => $product_option_value['product_option_value_id'],
+					'option_value_id'         => $product_option_value['option_value_id'],
+					'name'                    => $product_option_value['name'],
+					'image'                   => $product_option_value['image'],
+					'quantity'                => $product_option_value['quantity'],
+					'subtract'                => $product_option_value['subtract'],
+					'price'                   => $product_option_value['price'],
+					'price_prefix'            => $product_option_value['price_prefix'],
+					'weight'                  => $product_option_value['weight'],
+					'weight_prefix'           => $product_option_value['weight_prefix']
+				];
+			}
+
+			$product_option_data[] = [
+				'product_option_id'    => $product_option['product_option_id'],
+				'product_option_value' => $product_option_value_data,
+				'option_id'            => $product_option['option_id'],
+				'name'                 => $product_option['name'],
+				'type'                 => $product_option['type'],
+				'value'                => $product_option['value'],
+				'required'             => $product_option['required']
+			];
+		}
+
+		return $product_option_data;
+	}
 
 	public function getImages($product_id) {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_image WHERE product_id = '" . (int)$product_id . "' ORDER BY sort_order ASC");
@@ -978,11 +1017,23 @@ class Product extends \Opencart\System\Engine\Model {
 
 		return $query->rows;
 	}
+	
+	public function getOnetimeDiscount($product_id, $discount_quantity) {
+		$query = $this->db->query("SELECT price FROM " . DB_PREFIX . "product_discount WHERE product_id = '" . (int)$product_id . "' AND customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND quantity <= '" . (int)$discount_quantity . "' AND ((date_start = '0000-00-00' OR date_start < NOW()) AND (date_end = '0000-00-00' OR date_end > NOW())) ORDER BY quantity DESC, priority ASC, price ASC LIMIT 1");
+
+		return $query->row;
+	}
 
 	public function getSpecials($product_id) {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_special WHERE product_id = '" . (int)$product_id . "' ORDER BY priority, price");
 
 		return $query->rows;
+	}
+	
+	public function getOnetimeSpecial($product_id) {
+		$query = $this->db->query("SELECT price FROM " . DB_PREFIX . "product_special WHERE product_id = '" . (int)$product_id . "' AND customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND ((date_start = '0000-00-00' OR date_start < NOW()) AND (date_end = '0000-00-00' OR date_end > NOW())) ORDER BY priority ASC, price ASC LIMIT 1");
+		
+		return $query->row;
 	}
 
 	public function getRewards($product_id) {
@@ -995,6 +1046,12 @@ class Product extends \Opencart\System\Engine\Model {
 		}
 
 		return $product_reward_data;
+	}
+	
+	public function getReward($product_id) {
+		$query = $this->db->query("SELECT points FROM " . DB_PREFIX . "product_reward WHERE product_id = '" . (int)$product_id . "' AND customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "'");
+		
+		return $query->row;
 	}
 
 	public function getDownloads($product_id) {

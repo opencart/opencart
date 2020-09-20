d4536e39e07e268a153b17ac7f5213ef242060cf
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-20 12:26:06 -0400

    Added integer to GET return status ID request.

--- upload/extension/opencart/admin/controller/report/sale_return.php
+++ upload/extension/opencart/admin/controller/report/sale_return.php
@@ -92,7 +92,7 @@ class SaleReturn extends \Opencart\System\Engine\Controller {
 		}
 
 		if (isset($this->request->get['filter_return_status_id'])) {
-			$filter_return_status_id = $this->request->get['filter_return_status_id'];
+			$filter_return_status_id = (int)$this->request->get['filter_return_status_id'];
 		} else {
 			$filter_return_status_id = 0;
 		}

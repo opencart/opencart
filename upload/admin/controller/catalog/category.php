0347bcf9e45b7105444d074ee99b56583ca0590c
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-20 08:47:06 -0400

    Added integer on $page get request.

--- upload/admin/controller/catalog/category.php
+++ upload/admin/controller/catalog/category.php
@@ -157,7 +157,7 @@ class Category extends \Opencart\System\Engine\Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}

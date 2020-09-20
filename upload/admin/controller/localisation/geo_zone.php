dbf2ddcfaa766fbf058d1f10cd5f7e381a945623
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-20 09:11:34 -0400

    Added integer on $page get request.

--- upload/admin/controller/localisation/geo_zone.php
+++ upload/admin/controller/localisation/geo_zone.php
@@ -125,7 +125,7 @@ class GeoZone extends \Opencart\System\Engine\Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}

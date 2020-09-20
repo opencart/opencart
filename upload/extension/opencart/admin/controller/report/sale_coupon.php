8b75c893b7f0acd6565cb006a2871cfde8b61a62
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-20 10:02:25 -0400

    Added integer on $page get request.

--- upload/extension/opencart/admin/controller/report/sale_coupon.php
+++ upload/extension/opencart/admin/controller/report/sale_coupon.php
@@ -86,7 +86,7 @@ class SaleCoupon extends \Opencart\System\Engine\Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}

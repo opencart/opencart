3cc7c3600587204315b92c8194f4d86b15ff7882
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-24 10:23:38 -0400

    Added backward escaped quotes

--- upload/catalog/model/localisation/return_reason.php
+++ upload/catalog/model/localisation/return_reason.php
@@ -3,9 +3,9 @@ namespace Opencart\Application\Model\Localisation;
 class ReturnReason extends \Opencart\System\Engine\Model {
 	public function getReturnReasons($data = []) {
 		if ($data) {
-			$sql = "SELECT * FROM " . DB_PREFIX . "return_reason WHERE language_id = '" . (int)$this->config->get('config_language_id') . "'";
+			$sql = "SELECT * FROM `" . DB_PREFIX . "return_reason` WHERE `language_id` = '" . (int)$this->config->get('config_language_id') . "'";
 
-			$sql .= " ORDER BY name";
+			$sql .= " ORDER BY `name`";
 
 			if (isset($data['return']) && ($data['return'] == 'DESC')) {
 				$sql .= " DESC";
@@ -32,7 +32,7 @@ class ReturnReason extends \Opencart\System\Engine\Model {
 			$return_reason_data = $this->cache->get('return_reason.' . (int)$this->config->get('config_language_id'));
 
 			if (!$return_reason_data) {
-				$query = $this->db->query("SELECT return_reason_id, name FROM " . DB_PREFIX . "return_reason WHERE language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY name");
+				$query = $this->db->query("SELECT `return_reason_id`, `name` FROM `" . DB_PREFIX . "return_reason` WHERE `language_id` = '" . (int)$this->config->get('config_language_id') . "' ORDER BY `name`");
 
 				$return_reason_data = $query->rows;
 

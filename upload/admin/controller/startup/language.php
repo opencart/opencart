828b9efa222a0a233b132df55b4ee349904b091f
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-24 11:34:02 -0400

    Added backward escaped quotes

--- upload/admin/controller/startup/language.php
+++ upload/admin/controller/startup/language.php
@@ -3,7 +3,7 @@ namespace Opencart\Application\Controller\Startup;
 class Language extends \Opencart\System\Engine\Controller {
 	public function index() {
 		// Language
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "language` WHERE code = '" . $this->db->escape($this->config->get('config_admin_language')) . "'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "language` WHERE `code` = '" . $this->db->escape($this->config->get('config_admin_language')) . "'");
 
 		if ($query->num_rows) {
 			$this->config->set('config_language_id', $query->row['language_id']);
@@ -13,6 +13,7 @@ class Language extends \Opencart\System\Engine\Controller {
 		$language = new \Opencart\System\Library\Language();
 		$language->addPath(DIR_LANGUAGE . $this->config->get('config_admin_language') . '/');
 		$language->load($this->config->get('config_admin_language'));
+		
 		$this->registry->set('language', $language);
 	}
 }

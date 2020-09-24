2572c2a22707ba3f66f7d58dda75e70f10ed7141
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-24 10:20:08 -0400

    Added backward escaped quotes

--- upload/catalog/model/localisation/location.php
+++ upload/catalog/model/localisation/location.php
@@ -2,7 +2,7 @@
 namespace Opencart\Application\Model\Localisation;
 class Location extends \Opencart\System\Engine\Model {
 	public function getLocation($location_id) {
-		$query = $this->db->query("SELECT location_id, name, address, geocode, telephone, fax, image, open, comment FROM " . DB_PREFIX . "location WHERE location_id = '" . (int)$location_id . "'");
+		$query = $this->db->query("SELECT `location_id`, `name`, `address`, `geocode`, `telephone`, `fax`, `image`, `open`, `comment` FROM `" . DB_PREFIX . "location` WHERE `location_id` = '" . (int)$location_id . "'");
 
 		return $query->row;
 	}

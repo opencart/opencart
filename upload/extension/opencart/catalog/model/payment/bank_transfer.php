3e60c3c64dcfa5431221d2779b4d70f72d90a29c
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-25 08:47:48 -0400

    Added backward escaped quotes

--- upload/extension/opencart/catalog/model/payment/bank_transfer.php
+++ upload/extension/opencart/catalog/model/payment/bank_transfer.php
@@ -4,7 +4,7 @@ class BankTransfer extends \Opencart\System\Engine\Model {
 	public function getMethod($address, $total) {
 		$this->load->language('extension/payment/bank_transfer');
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_bank_transfer_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('payment_bank_transfer_geo_zone_id') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')");
 
 		if ($this->config->get('payment_bank_transfer_total') > 0 && $this->config->get('payment_bank_transfer_total') > $total) {
 			$status = false;

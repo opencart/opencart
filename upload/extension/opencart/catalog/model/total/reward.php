0ae92d43042a8502ff7cde5b0cf2cb9262e92c5d
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-25 09:17:32 -0400

    Added backward escaped quotes

--- upload/extension/opencart/catalog/model/total/reward.php
+++ upload/extension/opencart/catalog/model/total/reward.php
@@ -67,13 +67,13 @@ class Reward extends \Opencart\System\Engine\Model {
 		$this->load->model('account/customer');
 
 		if ($this->model_account_customer->getRewardTotal($order_info['customer_id']) >= $points) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "customer_reward SET customer_id = '" . (int)$order_info['customer_id'] . "', order_id = '" . (int)$order_info['order_id'] . "', description = '" . $this->db->escape(sprintf($this->language->get('text_order_id'), (int)$order_info['order_id'])) . "', points = '" . (float)-$points . "', date_added = NOW()");
+			$this->db->query("INSERT INTO `" . DB_PREFIX . "customer_reward` SET `customer_id` = '" . (int)$order_info['customer_id'] . "', `order_id` = '" . (int)$order_info['order_id'] . "', `description` = '" . $this->db->escape(sprintf($this->language->get('text_order_id'), (int)$order_info['order_id'])) . "', `points` = '" . (float) - $points . "', `date_added` = NOW()");
 		} else {
 			return $this->config->get('config_fraud_status_id');
 		}
 	}
 
 	public function unconfirm($order_id) {
-		$this->db->query("DELETE FROM " . DB_PREFIX . "customer_reward WHERE order_id = '" . (int)$order_id . "' AND points < 0");
+		$this->db->query("DELETE FROM `" . DB_PREFIX . "customer_reward` WHERE `order_id` = '" . (int)$order_id . "' AND `points` < '0'");
 	}
 }

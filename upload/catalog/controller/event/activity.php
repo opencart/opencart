6dbf29fe63d91c9e59f26e8e9e6f7c585ff8e811
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-27 12:57:02 -0400

    Trimmed lines of codes.

--- upload/catalog/controller/event/activity.php
+++ upload/catalog/controller/event/activity.php
@@ -55,8 +55,7 @@ class Activity extends \Opencart\System\Engine\Controller {
 			}	
 		}
 	}
-
-		
+	
 	// catalog/model/account/customer/deleteLoginAttempts/after
 	public function login(&$route, &$args, &$output) {
 		if (isset($this->request->get['route']) && ($this->request->get['route'] == 'account/login' || $this->request->get['route'] == 'checkout/login/save') && $this->config->get('config_customer_activity')) {

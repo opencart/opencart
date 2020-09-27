08dfcfc29e7e83744dab23a8faa95fcbc242ada1
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-27 12:55:41 -0400

    Added config locations

--- upload/admin/controller/event/theme.php
+++ upload/admin/controller/event/theme.php
@@ -2,10 +2,14 @@
 namespace Opencart\Application\Controller\Event;
 class Theme extends \Opencart\System\Engine\Controller {
 	public function index(&$route, &$args, &$code) {
+		// system/config/admin.php
+		// system/config/catalog.php
 		if (substr($filename, 0, 19) == 'extension/opencart/') {
 			$filename = substr($filename, 19);
 		}
 
+		// system/config/admin.php
+		// system/config/catalog.php
 		if (substr($route, 0, strpos('/')) == 'extension') {
 			//file_get_contents(EXTENSIONDIR_TEMPLATE . )
 		}

872f8bfce4142bcef50440d08004b037d1e1f323
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-05 09:40:35 +0200

    Warning: A non-numeric value encountered php7+

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -28,7 +28,7 @@ class ControllerProductCategory extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}

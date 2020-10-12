9f20c46e1d0062605f42a942a87112741cd86b57
Prabhat Kumar <prabhatkumar.oc082@webkul.com>
2020-10-12 12:41:05 +0530

    removed unwanted changes for front-end

--- upload/catalog/controller/information/contact.php
+++ upload/catalog/controller/information/contact.php
@@ -82,27 +82,28 @@ class ControllerInformationContact extends Controller {
 
 		$this->load->model('localisation/location');
 
-		$locations = $this->model_localisation_location->getLocations();
+		foreach((array)$this->config->get('config_location') as $location_id) {
+			$location_info = $this->model_localisation_location->getLocation($location_id);
 
-		foreach($locations as $location) {
+			if ($location_info) {
+				if ($location_info['image']) {
+					$image = $this->model_tool_image->resize($location_info['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_height'));
+				} else {
+					$image = false;
+				}
 
-			if ($location['image']) {
-				$image = $this->model_tool_image->resize($location['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_location_height'));
-			} else {
-				$image = false;
+				$data['locations'][] = array(
+					'location_id' => $location_info['location_id'],
+					'name'        => $location_info['name'],
+					'address'     => nl2br($location_info['address']),
+					'geocode'     => $location_info['geocode'],
+					'telephone'   => $location_info['telephone'],
+					'fax'         => $location_info['fax'],
+					'image'       => $image,
+					'open'        => nl2br($location_info['open']),
+					'comment'     => $location_info['comment']
+				);
 			}
-
-			$data['locations'][] = array(
-				'location_id' => $location['location_id'],
-				'name'        => $location['name'],
-				'address'     => nl2br($location['address']),
-				'geocode'     => $location['geocode'],
-				'telephone'   => $location['telephone'],
-				'fax'         => $location['fax'],
-				'image'       => $image,
-				'open'        => nl2br($location['open']),
-				'comment'     => $location['comment']
-			);
 		}
 
 		if (isset($this->request->post['name'])) {
--- upload/catalog/model/localisation/location.php
+++ upload/catalog/model/localisation/location.php
@@ -1,14 +1,8 @@
 <?php
 class ModelLocalisationLocation extends Model {
 	public function getLocation($location_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "location WHERE location_id = '" . (int)$location_id . "'");
+		$query = $this->db->query("SELECT location_id, name, address, geocode, telephone, fax, image, open, comment FROM " . DB_PREFIX . "location WHERE location_id = '" . (int)$location_id . "'");
 
 		return $query->row;
-	}
-
-	public function getLocations() {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "location ORDER BY name ASC");
-
-		return $query->rows;
 	}
 }

741fc3d3b8cd65bab014b0820bf5859e892d0172
straightlight <32371528+straightlight@users.noreply.github.com>
2020-10-15 21:14:32 -0400

    Change last / to |

--- upload/admin/controller/cron/currency.php
+++ upload/admin/controller/cron/currency.php
@@ -2,6 +2,6 @@
 namespace Opencart\Application\Controller\Cron;
 class Currency extends \Opencart\System\Engine\Controller {
 	public function index($cron_id, $code, $cycle, $date_added, $date_modified) {
-		$this->load->controller('extension/currency/' . $this->config->get('config_currency_engine') . '/currency', $this->config->get('config_currency'));
+		$this->load->controller('extension/currency/' . $this->config->get('config_currency_engine') . '|currency', $this->config->get('config_currency'));
 	}
 }

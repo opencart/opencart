4f75fb2d58e7038cfb09595d67ae55795b447d7b
straightlight <32371528+straightlight@users.noreply.github.com>
2020-10-19 04:55:16 -0400

    Added description and country tables.

--- upload/catalog/model/localisation/currency.php
+++ upload/catalog/model/localisation/currency.php
@@ -13,7 +13,7 @@ class Currency extends \Opencart\System\Engine\Model {
 		if (!$currency_data) {
 			$currency_data = [];
 
-			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "currency` WHERE `status` = '1' ORDER BY `title` ASC");
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "currency` c INNER JOIN `" . DB_PREFIX . "currency_description` cd ON (cd.`currency_id` = c.`currency_id`) INNER JOIN `" . DB_PREFIX . "currency_to_country` c2c ON (cd.`currency_id` = c2c.`currency_id`) WHERE c.`status` = '1' AND cd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND c2c.`country_id` = '" . (int)$this->config->get('config_country_id') . "' ORDER BY cd.`title` ASC");
 
 			foreach ($query->rows as $result) {
 				$currency_data[$result['code']] = [

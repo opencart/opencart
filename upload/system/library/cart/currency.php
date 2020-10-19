b4a44a959cee8556d0c54c7bfd41b5bdc59e1746
straightlight <32371528+straightlight@users.noreply.github.com>
2020-10-19 05:06:16 -0400

    Added validation to push.

--- upload/system/library/cart/currency.php
+++ upload/system/library/cart/currency.php
@@ -8,7 +8,7 @@ class Currency {
 		$this->language = $registry->get('language');
 		$this->config = $registry->get('config');
 
-		$query = $this->db->query("SELECT *, c2cp.`country_id` AS `push` FROM `" . DB_PREFIX . "currency` c INNER JOIN `" . DB_PREFIX . "currency_description` cd ON (c.`currency_id` = cd.`currency_id`) INNER JOIN `" . DB_PREFIX . "currency_to_country c2c ON (c.`currency_id` = c2c.`currency_id`) LEFT JOIN `" . DB_PREFIX . "currency_to_country_push` c2cp ON (c.`currency_id` = c2cp.`currency_id`) WHERE c.`status` = '1' AND cd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND c2c.`country_id` = '" . (int)$this->config->get('config_country_id') . "' AND c2cp.`country_id` = c2c.`country_id`");
+		$query = $this->db->query("SELECT *, c2c.`country_id` AS country_id, c2cp.`country_id` AS `push` FROM `" . DB_PREFIX . "currency` c INNER JOIN `" . DB_PREFIX . "currency_description` cd ON (c.`currency_id` = cd.`currency_id`) INNER JOIN `" . DB_PREFIX . "currency_to_country c2c ON (c.`currency_id` = c2c.`currency_id`) LEFT JOIN `" . DB_PREFIX . "currency_to_country_push` c2cp ON (c.`currency_id` = c2cp.`currency_id`) WHERE c.`status` = '1' AND cd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND c2c.`country_id` = '" . (int)$this->config->get('config_country_id') . "'");
 
 		foreach ($query->rows as $result) {
 			$this->currencies[$result['code']] = [
@@ -17,7 +17,7 @@ class Currency {
 				'symbol_left'   => $result['symbol_left'],
 				'symbol_right'  => $result['symbol_right'],
 				'decimal_place' => $result['decimal_place'],
-				'push'		=> $result['push'],
+				'push'		=> ($result['push'] && ($result['push'] == $result['country_id']) ? $result['push'] : 0),
 				'value'         => $result['value']
 			];
 		}

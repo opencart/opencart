17a9b8145b8a6147399672383c8a821a28aa33fa
straightlight <32371528+straightlight@users.noreply.github.com>
2020-10-18 21:51:01 -0400

    Added support for multi-country per language.

--- upload/system/library/cart/currency.php
+++ upload/system/library/cart/currency.php
@@ -8,7 +8,7 @@ class Currency {
 		$this->language = $registry->get('language');
 		$this->config = $registry->get('config');
 
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "currency` c LEFT JOIN `" . DB_PREFIX . "currency_description` cd ON (c.`currency_id` = cd.`currency_id`) WHERE cd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND cd.`country_id` = '" . (int)$this->config->get('config_country_id') . "'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "currency` c LEFT JOIN `" . DB_PREFIX . "currency_description` cd ON (c.`currency_id` = cd.`currency_id`) LEFT JOIN `" . DB_PREFIX . "currency_to_country c2c ON (c2c.`currency_id` = cd.`currency_id`) WHERE c.`status` = '1' AND cd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND c2c.`country_id` = '" . (int)$this->config->get('config_country_id') . "'");
 
 		foreach ($query->rows as $result) {
 			$this->currencies[$result['code']] = [
@@ -17,7 +17,7 @@ class Currency {
 				'symbol_left'   => $result['symbol_left'],
 				'symbol_right'  => $result['symbol_right'],
 				'decimal_place' => $result['decimal_place'],
-				'push'		=> $result['push'],
+				'push'			=> $result['push'],
 				'value'         => $result['value']
 			];
 		}

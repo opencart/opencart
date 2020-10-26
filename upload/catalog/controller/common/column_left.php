8321b11fddfca9eb40c11f34ba729ae508cd0a3f
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-26 23:13:42 +0200

    fix load href
    There is no point in loading like this:
    https://demo.opencart.com/index.php?route=common/column_left

--- upload/catalog/controller/common/column_left.php
+++ upload/catalog/controller/common/column_left.php
@@ -1,7 +1,7 @@
 <?php
 namespace Opencart\Application\Controller\Common;
 class ColumnLeft extends \Opencart\System\Engine\Controller {
-	public function index() {
+	public function index($setting) {
 		$this->load->model('design/layout');
 
 		if (isset($this->request->get['route'])) {

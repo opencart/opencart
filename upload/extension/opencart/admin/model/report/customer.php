915d776d6809598f551a63551d9e7dab3bed7b6a
straightlight <32371528+straightlight@users.noreply.github.com>
2020-11-07 22:09:15 -0500

    Added escaped quotes as per master branch

--- upload/extension/opencart/admin/model/report/customer.php
+++ upload/extension/opencart/admin/model/report/customer.php
@@ -95,25 +95,25 @@ class Customer extends \Opencart\System\Engine\Model {
 		return $customer_data;
 	}
 
-	public function getOrders($data = array()) {
-		$sql = "SELECT c.customer_id, CONCAT(c.firstname, ' ', c.lastname) AS customer, c.email, cgd.name AS customer_group, c.status, `o`.`order_id`, SUM(op.quantity) AS products, `o`.total AS total, `o`.`currency_code`, `o`.`currency_value`, (SELECT oca.`affiliate_id` FROM `" . DB_PREFIX . "order_customer_affiliate` oca WHERE oca.`order_id` = `o`.`order_id` GROUP BY oca.`order_id`) AS affiliate_id, SUM((SELECT SUM(oca.`commission`) FROM `" . DB_PREFIX . "order_customer_affiliate` oca WHERE oca.`order_id` = `o`.`order_id` GROUP BY oca.`order_id`)) AS commission FROM `" . DB_PREFIX . "order` `o` LEFT JOIN `" . DB_PREFIX . "order_product` op ON (`o`.`order_id` = op.`order_id`) LEFT JOIN `" . DB_PREFIX . "customer` c ON (o.customer_id = c.customer_id) LEFT JOIN `" . DB_PREFIX . "customer_group_description` cgd ON (c.customer_group_id = cgd.customer_group_id) WHERE o.customer_id > 0 AND cgd.language_id = '" . (int)$this->config->get('config_language_id') . "'";
+	public function getOrders($data = []) {
+		$sql = "SELECT c.`customer_id`, CONCAT(c.`firstname`, ' ', c.`lastname`) AS customer, c.`email`, cgd.`name` AS customer_group, c.`status`, `o`.`order_id`, SUM(op.`quantity`) AS products, `o`.total AS total, `o`.`currency_code`, `o`.`currency_value`, (SELECT oca.`affiliate_id` FROM `" . DB_PREFIX . "order_customer_affiliate` oca WHERE oca.`order_id` = `o`.`order_id` GROUP BY oca.`order_id`) AS affiliate_id, SUM((SELECT SUM(oca.`commission`) FROM `" . DB_PREFIX . "order_customer_affiliate` oca WHERE oca.`order_id` = `o`.`order_id` GROUP BY oca.`order_id`)) AS commission FROM `" . DB_PREFIX . "order` `o` LEFT JOIN `" . DB_PREFIX . "order_product` op ON (`o`.`order_id` = op.`order_id`) LEFT JOIN `" . DB_PREFIX . "customer` c ON (o.`customer_id` = c.`customer_id`) LEFT JOIN `" . DB_PREFIX . "customer_group_description` cgd ON (c.`customer_group_id` = cgd.`customer_group_id`) WHERE o.`customer_id` > '0' AND cgd.`language_id` = '" . (int)$this->config->get('config_language_id') . "'";
 
 		if (!empty($data['filter_date_start'])) {
-			$sql .= " AND DATE(o.date_added) >= '" . $this->db->escape($data['filter_date_start']) . "'";
+			$sql .= " AND DATE(`o`.`date_added`) >= '" . $this->db->escape($data['filter_date_start']) . "'";
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$sql .= " AND DATE(o.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$sql .= " AND DATE(`o`.`date_added`) <= '" . $this->db->escape($data['filter_date_end']) . "'";
 		}
 
 		if (!empty($data['filter_customer'])) {
-			$sql .= " AND CONCAT(c.firstname, ' ', c.lastname) LIKE '" . $this->db->escape($data['filter_customer']) . "'";
+			$sql .= " AND CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE '" . $this->db->escape($data['filter_customer']) . "'";
 		}
 
 		if (!empty($data['filter_order_status_id'])) {
-			$sql .= " AND o.order_status_id = '" . (int)$data['filter_order_status_id'] . "'";
+			$sql .= " AND `o`.`order_status_id` = '" . (int)$data['filter_order_status_id'] . "'";
 		} else {
-			$sql .= " AND o.order_status_id > '0'";
+			$sql .= " AND `o`.`order_status_id` > '0'";
 		}
 		
 		if (!empty($data['filter_affiliate'])) {
@@ -122,7 +122,7 @@ class Customer extends \Opencart\System\Engine\Model {
 
 		$sql .= " GROUP BY o.order_id";
 
-		$sql = "SELECT t.customer_id, t.customer, t.email, t.customer_group, t.status, COUNT(DISTINCT t.order_id) AS orders, SUM(t.products) AS products, SUM(t.total) AS total FROM (" . $sql . ") AS t GROUP BY t.customer_id ORDER BY total DESC";
+		$sql = "SELECT t.`customer_id`, t.`customer`, t.`email`, t.`customer_group`, t.`status`, COUNT(DISTINCT t.`order_id`) AS orders, SUM(t.`products`) AS products, SUM(t.`total`) AS total FROM (" . $sql . ") AS t GROUP BY t.`customer_id` ORDER BY `total` DESC";
 
 		if (isset($data['start']) || isset($data['limit'])) {
 			if ($data['start'] < 0) {
@@ -141,25 +141,25 @@ class Customer extends \Opencart\System\Engine\Model {
 		return $query->rows;
 	}
 
-	public function getTotalOrders($data = array()) {
-		$sql = "SELECT COUNT(DISTINCT o.customer_id) AS total FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "customer` c ON (o.customer_id = c.customer_id) WHERE o.customer_id > '0'";
+	public function getTotalOrders($data = []) {
+		$sql = "SELECT COUNT(DISTINCT o.`customer_id`) AS total FROM `" . DB_PREFIX . "order` `o` LEFT JOIN `" . DB_PREFIX . "customer` c ON (o.`customer_id` = c.`customer_id`) WHERE o.`customer_id` > '0'";
 
 		if (!empty($data['filter_date_start'])) {
-			$sql .= " AND DATE(o.date_added) >= '" . $this->db->escape($data['filter_date_start']) . "'";
+			$sql .= " AND DATE(`o`.`date_added`) >= '" . $this->db->escape($data['filter_date_start']) . "'";
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$sql .= " AND DATE(o.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$sql .= " AND DATE(`o`.`date_added`) <= '" . $this->db->escape($data['filter_date_end']) . "'";
 		}
 
 		if (!empty($data['filter_customer'])) {
-			$sql .= " AND CONCAT(c.firstname, ' ', c.lastname) LIKE '" . $this->db->escape($data['filter_customer']) . "'";
+			$sql .= " AND CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE '" . $this->db->escape($data['filter_customer']) . "'";
 		}
 
 		if (!empty($data['filter_order_status_id'])) {
-			$sql .= " AND o.order_status_id = '" . (int)$data['filter_order_status_id'] . "'";
+			$sql .= " AND o.`order_status_id` = '" . (int)$data['filter_order_status_id'] . "'";
 		} else {
-			$sql .= " AND o.order_status_id > '0'";
+			$sql .= " AND o.`order_status_id` > '0'";
 		}
 		
 		if (!empty($data['filter_affiliate'])) {

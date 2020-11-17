2a3aa1bc713b0a0c1ad7e368e29536473f5d99ab
Ekrem KAYA <webmaster@e-piksel.com>
2020-11-17 18:39:57 +0300

    Review list unused columns have been removed.

--- upload/catalog/model/catalog/review.php
+++ upload/catalog/model/catalog/review.php
@@ -16,7 +16,7 @@ class Review extends \Opencart\System\Engine\Model {
 			$limit = 20;
 		}
 
-		$query = $this->db->query("SELECT r.`review_id`, r.`author`, r.`rating`, r.`text`, p.`product_id`, pd.`name`, p.`price`, p.`image`, r.`date_added` FROM `" . DB_PREFIX . "review` r LEFT JOIN `" . DB_PREFIX . "product` p ON (r.`product_id` = p.`product_id`) LEFT JOIN `" . DB_PREFIX . "product_description` pd ON (p.`product_id` = pd.`product_id`) WHERE p.`product_id` = '" . (int)$product_id . "' AND p.`date_available` <= NOW() AND p.`status` = '1' AND r.`status` = '1' AND pd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' ORDER BY r.`date_added` DESC LIMIT " . (int)$start . "," . (int)$limit);
+		$query = $this->db->query("SELECT r.`author`, r.`rating`, r.`text`, r.`date_added` FROM `" . DB_PREFIX . "review` r LEFT JOIN `" . DB_PREFIX . "product` p ON (r.`product_id` = p.`product_id`) LEFT JOIN `" . DB_PREFIX . "product_description` pd ON (p.`product_id` = pd.`product_id`) WHERE r.`product_id` = '" . (int)$product_id . "' AND p.`date_available` <= NOW() AND p.`status` = '1' AND r.`status` = '1' AND pd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' ORDER BY r.`date_added` DESC LIMIT " . (int)$start . "," . (int)$limit);
 
 		return $query->rows;
 	}

0d42b246d673d4bc1ea0dc1910f9148109fafdfe
straightlight <32371528+straightlight@users.noreply.github.com>
2020-11-20 07:56:26 -0500

    Added customer group ID lookup

--- upload/catalog/model/account/activity.php
+++ upload/catalog/model/account/activity.php
@@ -8,6 +8,12 @@ class Activity extends \Opencart\System\Engine\Model {
 			$customer_id = 0;
 		}
 
-		$this->db->query("INSERT INTO `" . DB_PREFIX . "customer_activity` SET `customer_id` = '" . (int)$customer_id . "', `key` = '" . $this->db->escape($key) . "', `data` = '" . $this->db->escape(json_encode($data)) . "', `ip` = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "', `date_added` = NOW()");
+		if (isset($data['customer_group_id']) && is_array($this->config->get('config_customer_group_display')) && in_array($data['customer_group_id'], $this->config->get('config_customer_group_display'))) {
+			$customer_group_id = $data['customer_group_id'];
+		} else {
+			$customer_group_id = $this->config->get('config_customer_group_id');
+		}
+
+		$this->db->query("INSERT INTO `" . DB_PREFIX . "customer_activity` SET `customer_id` = '" . (int)$customer_id . "', `customer_group_id` = '" . (int)$customer_group_id . "', `key` = '" . $this->db->escape($key) . "', `data` = '" . $this->db->escape(json_encode($data)) . "', `ip` = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "', `date_added` = NOW()");
 	}
 }

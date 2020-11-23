d1babb7cb055d2c293975e36253d0639cc0e71b0
Daniel Kerr <webmaster@opencart.com>
2020-11-23 23:18:34 +0800

    Update cookiecuttr.php

--- upload/extension/opencart/catalog/controller/other/cookiecuttr.php
+++ upload/extension/opencart/catalog/controller/other/cookiecuttr.php
@@ -1,18 +1,23 @@
 <?php
 namespace Opencart\Application\Controller\Extension\Opencart\Other;
 class Cookiecuttr extends \Opencart\System\Engine\Controller {
-	public function index() {
-		if ($this->config->get('other_cookie_id') && !isset($this->request->cookie['policy'])) {
-			$this->load->language('extension\opencart\other/cookiecuttr');
+	public function index(&$route, &$args, &$output) {
+		if ($this->config->get('other_cookiecuttr_status')) {
+			$key = 'cookiecuttr.' . $this->config->get('config_theme');
 
-			$data['text_cookie'] = sprintf($this->language->get('text_cookie'), $this->url->link('information/information', 'language=' . $this->config->get('config_language') . '&information_id=' . $this->config->get('config_cookie_id')));
+			$cookiecuttr = $this->cache->get($key);
 
-			return $this->load->view('extension/opencart/other/cookiecuttr', $data);
+			if (!$cookiecuttr) {
+				$this->load->language('extensison/opencart/other/cookiecuttr');
+
+				//$data['text_cookie'] = sprintf($this->language->get('text_cookie'), $this->url->link('information/information', 'language=' . $this->config->get('config_language') . '&information_id=' . $this->config->get('config_cookie_id')));
+
+				$output = str_replace('</body></html>', $this->load->view('extension/opencart/other/cookiecuttr') . '</body></html>', $output);
+
+				$this->cache->set($key, $output);
+			} else {
+				$output = $cookiecuttr;
+			}
 		}
-	}
-
-	public function event(&$route, &$args, &$output) {
-		echo 'hi';
-
 	}
 }

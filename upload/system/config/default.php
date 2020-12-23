d17f8f822ddc1ffbe72894b9f607e9b50a2ac757
danielkerr <danielkerr@webmaster.com>
2020-12-23 20:08:56 +0800

    added some session config entries

--- upload/system/config/default.php
+++ upload/system/config/default.php
@@ -37,9 +37,11 @@ $_['cache_expire']           = 3600;
 // Session
 $_['session_engine']         = 'file'; // db or file
 $_['session_name']           = 'OCSESSID';
+$_['session_domain']         = 'domain';
+$_['session_path']           = 'path';
 $_['session_expire']         = 360000;
 $_['session_probability']    = 1;
-$_['session_divisor']        = 4;
+$_['session_divisor']        = 5;
 
 // Template
 $_['template_engine']        = 'twig';

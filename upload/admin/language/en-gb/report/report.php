2ade5e866c55bd27a56e4d78890075f2863960de
condor2 <condor20_05@yahoo.it>
2020-12-26 13:43:30 +0200

    Removed unused language variable in Report

--- upload/admin/language/en-gb/report/report.php
+++ upload/admin/language/en-gb/report/report.php
@@ -4,6 +4,5 @@ $_['heading_title'] = 'Reports';
 
 // Text
 $_['text_success']  = 'Success: You have modified reports!';
-$_['text_list']     = 'Report List';
 $_['text_type']     = 'Choose the report type';
 $_['text_filter']   = 'Filter';

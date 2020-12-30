dd531d8763b3e1229a2a0e8d553228e746ccea47
condor2 <condor20_05@yahoo.it>
2020-12-30 09:50:18 +0200

    Added missing language variable in Upload

--- upload/admin/language/en-gb/tool/upload.php
+++ upload/admin/language/en-gb/tool/upload.php
@@ -5,6 +5,7 @@ $_['heading_title']     = 'Uploads';
 // Text
 $_['text_success']      = 'Success: You have modified uploads!';
 $_['text_list']         = 'Upload List';
+$_['text_filter']       = 'Filter';
 
 // Column
 $_['column_name']       = 'Upload Name';

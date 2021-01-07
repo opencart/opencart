cd3ece19b0b93becbaa6e0869a4b8e5f6c7746bf
dilip kumar <dilipkumar.oc571@webkul.com>
2021-01-07 11:06:34 +0530

    changes in summernote file

--- upload/admin/view/javascript/summernote/opencart.js
+++ upload/admin/view/javascript/summernote/opencart.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// Override summernotes image manager
 	$('[data-toggle=\'summernote\']').each(function() {
 		var element = this;
-		
+
 		if ($(this).attr('data-lang')) {
 			$('head').append('<script type="text/javascript" src="view/javascript/summernote/lang/summernote-' + $(this).attr('data-lang') + '.js"></script>');
 		}
@@ -17,8 +17,8 @@ $(document).ready(function() {
 				htmlMode: true,
 				lineNumbers: true,
 				theme: 'monokai'
-			},			
-			fontsize: ['8', '9', '10', '11', '12', '14', '16', '18', '20', '24', '30', '36', '48' , '64'],
+			},
+			fontSizes: ['8', '9', '10', '11', '12', '14', '16', '18', '20', '24', '30', '36', '48' , '64'],
 			toolbar: [
 				['style', ['style']],
 				['font', ['bold', 'underline', 'clear']],
@@ -37,18 +37,18 @@ $(document).ready(function() {
 					['float', ['floatLeft', 'floatRight', 'floatNone']],
 					['remove', ['removeMedia']]
 				],
-			},			
+			},
 			buttons: {
     			image: function() {
 					var ui = $.summernote.ui;
-							
+
 					// create button
 					var button = ui.button({
 						contents: '<i class="note-icon-picture" />',
 						tooltip: $.summernote.lang[$.summernote.options.lang].image.image,
 						click: function () {
 							$('#modal-image').remove();
-							
+
 							$.ajax({
 								url: 'index.php?route=common/filemanager&user_token=' + getURLVar('user_token'),
 								dataType: 'html',
@@ -62,21 +62,21 @@ $(document).ready(function() {
 								},
 								success: function(html) {
 									$('body').append('<div id="modal-image" class="modal">' + html + '</div>');
-									
+
 									$('#modal-image').modal('show');
-									
+
 									$('#modal-image').delegate('a.thumbnail', 'click', function(e) {
 										e.preventDefault();
-										
+
 										$(element).summernote('insertImage', $(this).attr('href'));
-																	
+
 										$('#modal-image').modal('hide');
 									});
 								}
-							});						
+							});
 						}
 					});
-				
+
 					return button.render();
 				}
   			}

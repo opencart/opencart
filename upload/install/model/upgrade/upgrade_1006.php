69758bf2efcf527c3b75ce7b2e512ecdfd7755ba
danielkerr <danielkerr@webmaster.com>
2021-01-16 16:03:00 +0800

    Update upgrade_1006.php

--- upload/install/model/upgrade/upgrade_1006.php
+++ upload/install/model/upgrade/upgrade_1006.php
@@ -17,7 +17,8 @@ class Upgrade1006 extends \Opencart\System\Engine\Model {
 
 		// Update the config.php by adding a DB_PORT
 		if (is_file(DIR_OPENCART . 'config.php')) {
-			$files = glob(DIR_OPENCART . '{config.php,admin/config.php}', GLOB_BRACE);
+			$files[] = DIR_OPENCART . 'config.php';
+			$files[] = DIR_OPENCART . 'admin/config.php';
 
 			foreach ($files as $file) {
 				$upgrade = true;

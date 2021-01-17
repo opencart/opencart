6718aeba16aef3414eb11f714f70a2356d297375
danielkerr <danielkerr@webmaster.com>
2021-01-17 22:04:12 +0800

    https://github.com/opencart/opencart/issues/9015

--- upload/admin/controller/startup/language.php
+++ upload/admin/controller/startup/language.php
@@ -2,6 +2,15 @@
 namespace Opencart\Application\Controller\Startup;
 class Language extends \Opencart\System\Engine\Controller {
 	public function index() {
+		// Added this code so that backup and restore doesn't show text_restore
+		$code = $this->config->get('config_language_admin');
+
+		if ($code) {
+			$this->session->data['language_admin'] = $code;
+		} elseif (isset($this->session->data['language_admin'])) {
+			$this->config->set('config_language_admin', $this->session->data['language_admin']);
+		}
+
 		// Language
 		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "language` WHERE `code` = '" . $this->db->escape($this->config->get('config_language_admin')) . "'");
 

5312d67bd8adf59eacf6b7d1d7e3ab8914e63ff9
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-01-17 16:46:34 +0300

    Renamed footer to copyright. Moved scripts to footer

--- upload/install/controller/common/footer.php
+++ upload/install/controller/common/footer.php
@@ -7,7 +7,7 @@ class Footer extends \Opencart\System\Engine\Controller {
 		$data['text_project'] = $this->language->get('text_project');
 		$data['text_documentation'] = $this->language->get('text_documentation');
 		$data['text_support'] = $this->language->get('text_support');
-		$data['text_footer'] = $this->language->get('text_footer');
+		$data['text_copyright'] = $this->language->get('text_copyright');
 
 		return $this->load->view('common/footer', $data);
 	}

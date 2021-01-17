4de7ac6954e9cc9d7d63e5feafbee11ee3ba1cf7
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-01-17 16:47:57 +0300

    Deleted unused data. Added direction and lang

--- upload/install/controller/common/header.php
+++ upload/install/controller/common/header.php
@@ -5,11 +5,9 @@ class Header extends \Opencart\System\Engine\Controller {
 		$this->load->language('common/header');
 		
 		$data['title'] = $this->document->getTitle();
-		$data['description'] = $this->document->getDescription();
 		$data['base'] = HTTP_SERVER;
-		$data['links'] = $this->document->getLinks();
-		$data['styles'] = $this->document->getStyles();
-		$data['scripts'] = $this->document->getScripts();
+		$data['lang'] = $this->language->get('code');
+		$data['direction'] = $this->language->get('direction');
 
 		return $this->load->view('common/header', $data);
 	}

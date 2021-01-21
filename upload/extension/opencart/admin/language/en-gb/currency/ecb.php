f9258a6ee68ffec64f0980e123a16fe137031171
condor2 <condor20_05@yahoo.it>
2021-01-21 12:41:00 +0200

    Typo language ecb.php

--- upload/extension/opencart/admin/language/en-gb/currency/ecb.php
+++ upload/extension/opencart/admin/language/en-gb/currency/ecb.php
@@ -4,7 +4,7 @@ $_['heading_title']    = 'European Central Bank Currency Converter';
 
 // Text
 $_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified fixer ECB rates!';
+$_['text_success']     = 'Success: You have modified ECB rates!';
 $_['text_edit']        = 'Edit European Central Bank';
 $_['text_support']     = 'This extension requires at EUR currency to be available currency option.';
 

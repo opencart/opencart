9ebda47c3cad71208dbbf1799a42352e817ca736
condor2 <condor20_05@yahoo.it>
2021-01-29 17:45:14 +0200

    restored text_credit
    I found it :)

--- upload/admin/language/en-gb/mail/transaction.php
+++ upload/admin/language/en-gb/mail/transaction.php
@@ -3,3 +3,4 @@
 $_['mail_text_subject']  = '%s - Affiliate Credit';
 $_['mail_text_received'] = 'You have received %s credit!';
 $_['mail_text_total']    = 'Your total amount of credit is now %s.';
+$_['text_credit']        = 'Your account credit can deducted from your next purchase.'; 

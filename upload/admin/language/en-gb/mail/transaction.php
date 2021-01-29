3e96c537d997ce165e71efd63a3c0e6408b4cbab
condor2 <condor20_05@yahoo.it>
2021-01-29 17:27:03 +0200

    Added new language variables mail/transaction
    I didn't found text_credit in any controller.

--- upload/admin/language/en-gb/mail/transaction.php
+++ upload/admin/language/en-gb/mail/transaction.php
@@ -1,6 +1,5 @@
 <?php
 // Text
-$_['text_subject']  = '%s - Affiliate Credit';
-$_['text_received'] = 'You have received %s credit!';
-$_['text_total']    = 'Your total amount of credit is now %s.';
-$_['text_credit']   = 'Your account credit can deducted from your next purchase.';
+$_['mail_text_subject']  = '%s - Affiliate Credit';
+$_['mail_text_received'] = 'You have received %s credit!';
+$_['mail_text_total']    = 'Your total amount of credit is now %s.';

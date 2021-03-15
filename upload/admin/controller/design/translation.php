3293f91f66da42a5fb7cacba90919aa3dfe9c335
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-03-14 20:49:09 -0400

    Removed duplicated array_shift

--- upload/admin/controller/design/translation.php
+++ upload/admin/controller/design/translation.php
@@ -479,7 +479,6 @@ class Translation extends \Opencart\System\Engine\Controller {
 			$directory = DIR_EXTENSION . $part[1] . '/catalog/language/';
 
 			array_shift($part);
-			array_shift($part);
 
 			$route = implode('/', $part);
 		}

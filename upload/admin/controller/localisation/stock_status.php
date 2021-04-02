ac4323a861098f5ebceb3232c3b2a52e1a9fb58e
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-01 22:58:13 -0400

    Fixed public save() method

--- upload/admin/controller/localisation/stock_status.php
+++ upload/admin/controller/localisation/stock_status.php
@@ -238,13 +238,17 @@ class StockStatus extends \Opencart\System\Engine\Controller {
 	}
 
 	public function save(): void {
+		$this->load->language('localisation/stock_status');
+		
+		$json = [];
+		
 		if (!$this->user->hasPermission('modify', 'localisation/stock_status')) {
-			$this->error['warning'] = $this->language->get('error_permission');
+			$json['warning'] = $this->language->get('error_permission');
 		}
 
 		foreach ($this->request->post['stock_status'] as $language_id => $value) {
 			if ((utf8_strlen($value['name']) < 3) || (utf8_strlen($value['name']) > 32)) {
-				$this->error['name'][$language_id] = $this->language->get('error_name');
+				$json['name'][$language_id] = $this->language->get('error_name');
 			}
 		}
 

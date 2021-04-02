6ec99a981829cae95f85fe7d7bb3a0e34cf7d197
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-01 23:03:46 -0400

    Fixed public save() method

--- upload/admin/controller/localisation/zone.php
+++ upload/admin/controller/localisation/zone.php
@@ -345,12 +345,16 @@ class Zone extends \Opencart\System\Engine\Controller {
 	}
 
 	public function save(): void {
+		$this->load->model('localisation/zone');
+		
+		$json = [];
+		
 		if (!$this->user->hasPermission('modify', 'localisation/zone')) {
-			$this->error['warning'] = $this->language->get('error_permission');
+			$json['warning'] = $this->language->get('error_permission');
 		}
 
 		if ((utf8_strlen($this->request->post['name']) < 1) || (utf8_strlen($this->request->post['name']) > 64)) {
-			$this->error['name'] = $this->language->get('error_name');
+			$json['name'] = $this->language->get('error_name');
 		}
 
 		$this->response->addHeader('Content-Type: application/json');

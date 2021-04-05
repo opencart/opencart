a1059c72fb6d0a821b8f7bd02eee1b47059700d4
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-05 09:02:14 -0400

    Added error key

--- upload/admin/controller/design/layout.php
+++ upload/admin/controller/design/layout.php
@@ -326,11 +326,11 @@ class Layout extends \Opencart\System\Engine\Controller {
 		$json = [];
 		
 		if (!$this->user->hasPermission('modify', 'design/layout')) {
-			$json['warning'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		if ((utf8_strlen(trim($this->request->post['name'])) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
-			$json['name'] = $this->language->get('error_name');
+			$json['error']['name'] = $this->language->get('error_name');
 		}
 
 		$this->response->addHeader('Content-Type: application/json');
@@ -343,7 +343,7 @@ class Layout extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (isset($this->request->post['selected'])) {
-			$selected = $this->request->post['selected'];
+			$selected = (array)$this->request->post['selected'];
 		} else {
 			$selected = [];
 		}
@@ -353,9 +353,13 @@ class Layout extends \Opencart\System\Engine\Controller {
 		}
 
 		$this->load->model('setting/store');
+		
 		$this->load->model('catalog/product');
+		
 		$this->load->model('catalog/category');
+		
 		$this->load->model('catalog/manufacturer');
+		
 		$this->load->model('catalog/information');
 
 		foreach ($selected as $layout_id) {

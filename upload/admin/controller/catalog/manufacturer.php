0dfc39e4142e9144cad7a0d92468e553cadd21bb
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-05 16:08:26 -0400

    Removed $this->error instanced variables

--- upload/admin/controller/catalog/manufacturer.php
+++ upload/admin/controller/catalog/manufacturer.php
@@ -1,8 +1,6 @@
 <?php
 namespace Opencart\Admin\Controller\Catalog;
 class Manufacturer extends \Opencart\System\Engine\Controller {
-	private array $error = [];
-
 	public function index(): void {
 		$this->load->language('catalog/manufacturer');
 
@@ -107,20 +105,6 @@ class Manufacturer extends \Opencart\System\Engine\Controller {
 			];
 		}
 
-		if (isset($this->error['warning'])) {
-			$data['error_warning'] = $this->error['warning'];
-		} else {
-			$data['error_warning'] = '';
-		}
-
-		if (isset($this->session->data['success'])) {
-			$data['success'] = $this->session->data['success'];
-
-			unset($this->session->data['success']);
-		} else {
-			$data['success'] = '';
-		}
-
 		if (isset($this->request->post['selected'])) {
 			$data['selected'] = (array)$this->request->post['selected'];
 		} else {
@@ -173,24 +157,6 @@ class Manufacturer extends \Opencart\System\Engine\Controller {
 
 	protected function getForm(): void {
 		$data['text_form'] = !isset($this->request->get['manufacturer_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
-
-		if (isset($this->error['warning'])) {
-			$data['error_warning'] = $this->error['warning'];
-		} else {
-			$data['error_warning'] = '';
-		}
-
-		if (isset($this->error['name'])) {
-			$data['error_name'] = $this->error['name'];
-		} else {
-			$data['error_name'] = '';
-		}
-
-		if (isset($this->error['keyword'])) {
-			$data['error_keyword'] = $this->error['keyword'];
-		} else {
-			$data['error_keyword'] = '';
-		}
 
 		$url = '';
 

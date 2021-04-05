dfe30bc302dd06ffee8d2b6d874d630b0aac8740
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-05 08:47:34 -0400

    Added error key

--- upload/admin/controller/localisation/return_reason.php
+++ upload/admin/controller/localisation/return_reason.php
@@ -243,12 +243,12 @@ class ReturnReason extends \Opencart\System\Engine\Controller {
 		$json = [];
 		
 		if (!$this->user->hasPermission('modify', 'localisation/return_reason')) {
-			$json['warning'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		foreach ($this->request->post['return_reason'] as $language_id => $value) {
 			if ((utf8_strlen($value['name']) < 3) || (utf8_strlen($value['name']) > 128)) {
-				$json['name'][$language_id] = $this->language->get('error_name');
+				$json['error']['name'][$language_id] = $this->language->get('error_name');
 			}
 		}
 
@@ -262,7 +262,7 @@ class ReturnReason extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (isset($this->request->post['selected'])) {
-			$selected = $this->request->post['selected'];
+			$selected = (array)$this->request->post['selected'];
 		} else {
 			$selected = [];
 		}

b25545bdbf1e2fe69604341f4442b2414f575e46
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-05 08:56:39 -0400

    Made various changes

--- upload/admin/controller/localisation/country.php
+++ upload/admin/controller/localisation/country.php
@@ -362,11 +362,11 @@ class Country extends \Opencart\System\Engine\Controller {
 		$json = [];
 		
 		if (!$this->user->hasPermission('modify', 'localisation/country')) {
-			$json['warning'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		if ((utf8_strlen($this->request->post['name']) < 1) || (utf8_strlen($this->request->post['name']) > 128)) {
-			$json['name'] = $this->language->get('error_name');
+			$json['error']['name'] = $this->language->get('error_name');
 		}
 
 		$this->response->addHeader('Content-Type: application/json');
@@ -379,7 +379,7 @@ class Country extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (isset($this->request->post['selected'])) {
-			$selected = $this->request->post['selected'];
+			$selected = (array)$this->request->post['selected'];
 		} else {
 			$selected = [];
 		}
@@ -389,8 +389,11 @@ class Country extends \Opencart\System\Engine\Controller {
 		}
 
 		$this->load->model('setting/store');
+		
 		$this->load->model('customer/customer');
+		
 		$this->load->model('localisation/zone');
+		
 		$this->load->model('localisation/geo_zone');
 
 		foreach ($this->request->post['selected'] as $country_id) {

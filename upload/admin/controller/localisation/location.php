aee1858298e082c9a0f20a61fcbd74aa2e44cd28
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 17:58:38 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/localisation/location.php
+++ upload/admin/controller/localisation/location.php
@@ -155,7 +155,7 @@ class Location extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('localisation/location_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['location_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

95a16eabc2da46651ead46a6802d75fa03b1cd12
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 17:54:16 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/localisation/weight_class.php
+++ upload/admin/controller/localisation/weight_class.php
@@ -157,7 +157,7 @@ class WeightClass extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('localisation/weight_class_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['weight_class_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

d459308eb7c2b034872493e52863af8a19901b0f
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 18:06:39 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/customer/custom_field.php
+++ upload/admin/controller/customer/custom_field.php
@@ -196,7 +196,7 @@ class CustomField extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('customer/custom_field_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['custom_field_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

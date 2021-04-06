3ce6544aff6809033d0d09e52d9a6176e43d4125
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 17:58:02 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/localisation/order_status.php
+++ upload/admin/controller/localisation/order_status.php
@@ -153,7 +153,7 @@ class OrderStatus extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('localisation/order_status_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['order_status_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

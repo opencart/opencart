3cb22643dabd18d19a775469565f0a4732cac4a4
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 17:56:51 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/localisation/return_reason.php
+++ upload/admin/controller/localisation/return_reason.php
@@ -153,7 +153,7 @@ class ReturnReason extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('localisation/return_reason_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['return_reason_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

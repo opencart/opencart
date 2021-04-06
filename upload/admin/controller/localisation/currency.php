2eb744629f28b0826b2732b58c67f79215069d8e
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 18:01:00 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/localisation/currency.php
+++ upload/admin/controller/localisation/currency.php
@@ -196,7 +196,7 @@ class Currency extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('localisation/currency_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['currency_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

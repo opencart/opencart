a14bfa13b9c9ec41ee8b605c0f30441f4a43405c
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 18:02:19 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/design/translation.php
+++ upload/admin/controller/design/translation.php
@@ -163,7 +163,7 @@ class Translation extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('design/translation_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['translation_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

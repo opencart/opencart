3f106ad7fd520dea661d97e4d3c086ffef9a89c2
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 18:04:46 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/customer/customer_group.php
+++ upload/admin/controller/customer/customer_group.php
@@ -155,7 +155,7 @@ class CustomerGroup extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('customer/customer_group_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['customer_group_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

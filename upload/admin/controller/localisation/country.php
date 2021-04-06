e248e16bf44fa8d2954cabc3fc904a2f942b887e
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 18:01:46 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/localisation/country.php
+++ upload/admin/controller/localisation/country.php
@@ -220,7 +220,7 @@ class Country extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('localisation/country_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['country_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

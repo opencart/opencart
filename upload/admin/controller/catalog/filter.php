fc67bf8da6faf415fa804aa9d8892770530e39f7
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 18:10:21 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/catalog/filter.php
+++ upload/admin/controller/catalog/filter.php
@@ -153,7 +153,7 @@ class Filter extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('catalog/filter_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['filter_group_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

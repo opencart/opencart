4f4a5d6460a73af6c14e97976439fba24219ae03
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 17:54:45 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/localisation/tax_rate.php
+++ upload/admin/controller/localisation/tax_rate.php
@@ -183,7 +183,7 @@ class TaxRate extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('localisation/tax_rate_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['tax_rate_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

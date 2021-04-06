d61aeb98804fa1083a3b470291d7321035995f53
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 17:57:27 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/localisation/return_action.php
+++ upload/admin/controller/localisation/return_action.php
@@ -153,7 +153,7 @@ class ReturnAction extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('localisation/return_action_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['return_action_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

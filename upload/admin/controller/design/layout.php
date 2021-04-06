8af24a2e7af4dd80dac23c9a99a85ea3a8a53b11
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-06 18:03:52 -0400

    Changed private getForm() to public form() method

--- upload/admin/controller/design/layout.php
+++ upload/admin/controller/design/layout.php
@@ -153,7 +153,7 @@ class Layout extends \Opencart\System\Engine\Controller {
 		$this->response->setOutput($this->load->view('design/layout_list', $data));
 	}
 
-	protected function getForm(): void {
+	public function form(): void {
 		$data['text_form'] = !isset($this->request->get['layout_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
 
 		$url = '';

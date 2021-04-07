1d64ebd5acaf9f73cef6d61c48b5f76fbeabe82a
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-07 18:36:14 -0400

    Added JSON warning key

--- upload/admin/controller/extension/analytics.php
+++ upload/admin/controller/extension/analytics.php
@@ -86,7 +86,7 @@ class Analytics extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (!$this->user->hasPermission('modify', 'extension/analytics')) {
-			$json['error'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		if (!$json) {
@@ -115,7 +115,7 @@ class Analytics extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (!$this->user->hasPermission('modify', 'extension/analytics')) {
-			$json['error'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		if (!$json) {

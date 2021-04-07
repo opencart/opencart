acd33268fc686be9822f530355431350fa528de2
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-07 18:44:55 -0400

    Added warning JSON key

--- upload/admin/controller/extension/other.php
+++ upload/admin/controller/extension/other.php
@@ -63,7 +63,7 @@ class Other extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (!$this->user->hasPermission('modify', 'extension/other')) {
-			$json['error'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		if (!$json) {
@@ -92,7 +92,7 @@ class Other extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (!$this->user->hasPermission('modify', 'extension/other')) {
-			$json['error'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		if (!$json) {

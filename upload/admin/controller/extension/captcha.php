495eb674d57512cb7c6087e3e759a7e6cd9225c6
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-04-07 18:38:25 -0400

    Added warning JSON key

--- upload/admin/controller/extension/captcha.php
+++ upload/admin/controller/extension/captcha.php
@@ -63,7 +63,7 @@ class Captcha extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (!$this->user->hasPermission('modify', 'extension/captcha')) {
-			$json['error'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		if (!$json) {
@@ -92,7 +92,7 @@ class Captcha extends \Opencart\System\Engine\Controller {
 		$json = [];
 
 		if (!$this->user->hasPermission('modify', 'extension/captcha')) {
-			$json['error'] = $this->language->get('error_permission');
+			$json['error']['warning'] = $this->language->get('error_permission');
 		}
 
 		if (!$json) {

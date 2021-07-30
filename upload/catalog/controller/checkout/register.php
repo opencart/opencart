ecdfc02bd65264c5a78c4e4f78f8ca68419c324e
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-07-30 19:37:33 -0400

    Removed empty line on break

--- upload/catalog/controller/checkout/register.php
+++ upload/catalog/controller/checkout/register.php
@@ -236,7 +236,6 @@ class Register extends \Opencart\System\Engine\Controller {
 
 			if ($product['minimum'] > $product_total) {
 				$json['redirect'] = $this->url->link('checkout/cart', 'language=' . $this->config->get('config_language'), true);
-
 				break;
 			}
 		}

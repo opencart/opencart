9306ef5703e306109abdf38825546815b63a01cc
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-07-30 20:22:07 -0400

    Removed trim function

--- upload/admin/controller/user/user.php
+++ upload/admin/controller/user/user.php
@@ -279,11 +279,11 @@ class User extends \Opencart\System\Engine\Controller {
 			}
 		}
 
-		if ((utf8_strlen(trim($this->request->post['firstname'])) < 1) || (utf8_strlen(trim($this->request->post['firstname'])) > 32)) {
+		if ((utf8_strlen($this->request->post['firstname']) < 1) || (utf8_strlen($this->request->post['firstname']) > 32)) {
 			$json['error']['firstname'] = $this->language->get('error_firstname');
 		}
 
-		if ((utf8_strlen(trim($this->request->post['lastname'])) < 1) || (utf8_strlen(trim($this->request->post['lastname'])) > 32)) {
+		if ((utf8_strlen($this->request->post['lastname']) < 1) || (utf8_strlen($this->request->post['lastname']) > 32)) {
 			$json['error']['lastname'] = $this->language->get('error_lastname');
 		}
 

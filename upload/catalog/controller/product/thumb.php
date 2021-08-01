d6b98411fad052a9c38a92b58e64fe08f7fe2ad5
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-08-01 18:11:43 -0400

    Added int cast

--- upload/catalog/controller/product/thumb.php
+++ upload/catalog/controller/product/thumb.php
@@ -4,7 +4,7 @@ class Thumb extends \Opencart\System\Engine\Controller {
 	public function index(array $data): string {
 		$this->load->language('product/thumb');
 
-		$data['review_status'] = $this->config->get('config_review_status');
+		$data['review_status'] = (int)$this->config->get('config_review_status');
 
 		return $this->load->view('product/thumb', $data);
 	}

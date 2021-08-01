40e9c3f184ab992cc3113c263151d5d2b3f1c166
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-08-01 17:36:50 -0400

    Added int cast with 1

--- upload/catalog/controller/api/cart.php
+++ upload/catalog/controller/api/cart.php
@@ -195,7 +195,7 @@ class Cart extends \Opencart\System\Engine\Controller {
 		array_multisort($sort_order, SORT_ASC, $results);
 
 		foreach ($results as $result) {
-			if ($this->config->get('total_' . $result['code'] . '_status')) {
+			if ((int)$this->config->get('total_' . $result['code'] . '_status') == 1) {
 				$this->load->model('extension/' . $result['extension'] . '/total/' . $result['code']);
 
 				// __call can not pass-by-reference so we get PHP to call it as an anonymous function.

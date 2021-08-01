3483f02c2e3d9ff82478b43a519138e76e0d3b84
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-08-01 11:32:15 -0400

    Made various changes

--- upload/catalog/controller/account/register.php
+++ upload/catalog/controller/account/register.php
@@ -44,13 +44,13 @@ class Register extends \Opencart\System\Engine\Controller {
 
 		$data['customer_groups'] = [];
 
-		if (is_array($this->config->get('config_customer_group_display'))) {
+		if (!empty($this->config->get('config_customer_group_display')) && is_array($this->config->get('config_customer_group_display'))) {
 			$this->load->model('account/customer_group');
 
 			$customer_groups = $this->model_account_customer_group->getCustomerGroups();
 
 			foreach ($customer_groups as $customer_group) {
-				if (in_array($customer_group['customer_group_id'], $this->config->get('config_customer_group_display'))) {
+				if (in_array($customer_group['customer_group_id'], (array)$this->config->get('config_customer_group_display'))) {
 					$data['customer_groups'][] = $customer_group;
 				}
 			}
@@ -152,7 +152,7 @@ class Register extends \Opencart\System\Engine\Controller {
 
 		// Customer Group
 		if (in_array($this->request->post['customer_group_id'], (array)$this->config->get('config_customer_group_display'))) {
-			$customer_group_id = $this->request->post['customer_group_id'];
+			$customer_group_id = (int)$this->request->post['customer_group_id'];
 		} else {
 			$customer_group_id = $this->config->get('config_customer_group_id');
 		}
@@ -160,7 +160,7 @@ class Register extends \Opencart\System\Engine\Controller {
 		// Custom field validation
 		$this->load->model('account/custom_field');
 
-		$custom_fields = $this->model_account_custom_field->getCustomFields((int)$customer_group_id);
+		$custom_fields = $this->model_account_custom_field->getCustomFields($customer_group_id);
 
 		foreach ($custom_fields as $custom_field) {
 

a9157ddd1c31b3755469739d4452011868734d8b
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-08-01 09:26:20 -0400

    Update column_left.php

--- upload/admin/controller/common/column_left.php
+++ upload/admin/controller/common/column_left.php
@@ -2,7 +2,7 @@
 namespace Opencart\Admin\Controller\Common;
 class ColumnLeft extends \Opencart\System\Engine\Controller {
 	public function index(): string {
-		if (isset($this->request->get['user_token']) && isset($this->session->data['user_token']) && (filter_var($this->request->get['user_token'], FILTER_SANITIZE_STRING) == $this->session->data['user_token'])) {
+		if (isset($this->request->get['user_token']) && isset($this->session->data['user_token']) && (hash_equals($this->request->get['user_token'], $this->session->data['user_token']))) {
 			$this->load->language('common/column_left');
 
 			// Create a 3 level menu array

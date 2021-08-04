d5b7ac2a91e05cc6c9901c4b815553b0940e1db0
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-08-04 07:27:00 -0400

    Aligned all texts

--- upload/admin/language/en-gb/tool/upload.php
+++ upload/admin/language/en-gb/tool/upload.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']     = 'Uploads';
+$_['heading_title']      = 'Uploads';
 
 // Text
-$_['text_success']      = 'Success: You have modified uploads!';
-$_['text_list']         = 'Upload List';
-$_['text_filter']       = 'Filter';
+$_['text_success']       = 'Success: You have modified uploads!';
+$_['text_list']          = 'Upload List';
+$_['text_filter']        = 'Filter';
 
 // Column
-$_['column_name']       = 'Upload Name';
-$_['column_filename']   = 'Filename';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_name']        = 'Upload Name';
+$_['column_filename']    = 'Filename';
+$_['column_date_added']  = 'Date Added';
+$_['column_action']      = 'Action';
 
 // Entry
-$_['entry_name']        = 'Upload Name';
-$_['entry_filename']    = 'Filename';
-$_['entry_date_added']  = 'Date Added';
+$_['entry_name']         = 'Upload Name';
+$_['entry_filename']     = 'Filename';
+$_['entry_date_added']   = 'Date Added';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify uploads!';
+$_['error_permission']   = 'Warning: You do not have permission to modify uploads!';
 $_['error_not_found']    = 'Error: Could not find file %s !';
 $_['error_headers_sent'] = 'Error: Headers already sent out!';

debef8dba2703b3222ce3e9344d846c888b432fb
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-08-05 06:58:10 -0400

    Removed emoty space

--- upload/catalog/language/en-gb/checkout/checkout.php
+++ upload/catalog/language/en-gb/checkout/checkout.php
@@ -33,10 +33,6 @@ $_['text_address_existing']          = 'I want to use an existing address';
 //$_['text_shipping_method']           = 'Please select the preferred shipping method to use on this order.';
 //$_['text_payment_method']            = 'Please select the preferred payment method to use on this order.';
 
-
-
-
-
 $_['text_comments']                  = 'Add Comments About Your Order';
 $_['text_recurring_item']            = 'Recurring Item';
 $_['text_payment_recurring']         = 'Payment Profile';

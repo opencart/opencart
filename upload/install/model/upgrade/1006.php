3f5533fa57cf9f4e3ad66bcdfe74fde16ecec5b0
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-08-06 08:01:58 -0400

    Made various changes

--- upload/install/model/upgrade/1006.php
+++ upload/install/model/upgrade/1006.php
@@ -172,14 +172,13 @@ class ModelUpgrade1006 extends Model {
 		$this->db->query("UPDATE `" . DB_PREFIX . "information_description` SET `description` = REPLACE (description , 'data/', 'catalog/')");
 	}
 
-	private function recursive_move($src, $dest){
-
+	private function recursive_move($src, $dest) {
 	    // If source is not a directory stop processing
 	    if (!is_dir($src)) return false;
 
 	    // If the destination directory does not exist create it
-	    if(!is_dir($dest)) {
-	        if(!@mkdir($dest)) {
+	    if (!is_dir($dest)) {
+	        if (!@mkdir($dest)) {
 	            // If the destination directory could not be created stop processing
 	    		return false;
 	        }
@@ -187,16 +186,18 @@ class ModelUpgrade1006 extends Model {
 
 	    // Open the source directory to read in files
 	    $i = new DirectoryIterator($src);
-	    foreach($i as $f) {
-	        if($f->isFile() && !file_exists("$dest/" . $f->getFilename())) {
+		
+	    foreach ($i as $f) {
+	        if ($f->isFile() && !file_exists("$dest/" . $f->getFilename())) {
 	            @rename($f->getRealPath(), "$dest/" . $f->getFilename());
-	        } elseif(!$f->isDot() && $f->isDir()) {
+	        } elseif (!$f->isDot() && $f->isDir()) {
 	            $this->recursive_move($f->getRealPath(), "$dest/$f");
+			
 	            @unlink($f->getRealPath());
 	        }
 	    }
 
-		// Remove source folder after move
+	    // Remove source folder after move
 	    @unlink($src);
 	}
 }

17ab5c31c8547a5f10c6266a7812e20da6b3ff05
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-08-06 08:05:02 -0400

    Update general.php

--- upload/system/helper/general.php
+++ upload/system/helper/general.php
@@ -23,15 +23,17 @@ function token($length = 32) {
 if (!function_exists('hash_equals')) {
 	function hash_equals($known_string, $user_string) {
 		$known_string = (string)$known_string;
+		
 		$user_string = (string)$user_string;
 
 		if (strlen($known_string) != strlen($user_string)) {
 			return false;
 		} else {
 			$res = $known_string ^ $user_string;
+			
 			$ret = 0;
 
-			for($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
+			for ($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
 
 			return !$ret;
 		}

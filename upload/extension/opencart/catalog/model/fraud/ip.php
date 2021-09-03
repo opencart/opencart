e84a46e5300aff3cd50b0ef4affa010f79eae9d0
Arshid <arshidkv12@gmail.com>
2021-09-03 19:16:52 +0530

    Update ip.php

--- upload/extension/opencart/catalog/model/fraud/ip.php
+++ upload/extension/opencart/catalog/model/fraud/ip.php
@@ -1,7 +1,7 @@
 <?php
 namespace Opencart\Catalog\Model\Extension\Opencart\Fraud;
 class Ip extends \Opencart\System\Engine\Model {
-	public function check(array $order_info): int {
+	public function check(array $order_info): ?int {
 		$status = false;
 
 		if ($order_info['customer_id']) {
@@ -28,6 +28,5 @@ class Ip extends \Opencart\System\Engine\Model {
 		if ($status) {
 			return (int)$this->config->get('fraud_ip_order_status_id');
 		}
-		return 0;
 	}
 }

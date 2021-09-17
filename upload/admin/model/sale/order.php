97b27ae8760f1fba8394d4a0a51b76c2218b027c
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2021-09-17 09:27:03 +0200

    fix history data != total

--- upload/admin/model/sale/order.php
+++ upload/admin/model/sale/order.php
@@ -473,7 +473,7 @@ class Order extends \Opencart\System\Engine\Model {
 			$limit = 10;
 		}
 
-		$query = $this->db->query("SELECT oh.`date_added`, os.`name` AS status, oh.`comment`, oh.`notify` FROM `" . DB_PREFIX . "order_history` oh LEFT JOIN `" . DB_PREFIX . "order_status` os ON oh.`order_status_id` = os.`order_status_id` WHERE oh.`order_id` = '" . (int)$order_id . "' AND os.`language_id` = '" . (int)$this->config->get('config_language_id') . "' ORDER BY oh.`date_added` DESC LIMIT " . (int)$start . "," . (int)$limit);
+		$query = $this->db->query("SELECT oh.`date_added`, oh.`comment`, oh.`notify`, (SELECT os.`name` FROM `" . DB_PREFIX . "order_status` os WHERE os.`order_status_id` = oh.`order_status_id` AND os.`language_id` = '" . (int)$this->config->get('config_language_id') . "') AS status FROM `" . DB_PREFIX . "order_history` oh WHERE oh.`order_id` = '" . (int)$this->request->get['order_id'] . "' ORDER BY oh.`date_added` ASC LIMIT " . (int)$start . "," . (int)$limit);
 
 		return $query->rows;
 	}

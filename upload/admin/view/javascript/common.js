b2d9eba9797d15e5c43f4815d1537e87703d1c7b
Daniel Kerr <webmaster@opencart.com>
2021-09-21 22:45:52 +0800

    more admin bs5 changes. updated forms to be inline with more html5 standards

--- upload/admin/view/javascript/common.js
+++ upload/admin/view/javascript/common.js
@@ -93,12 +93,12 @@ $(document).ready(function() {
 
     // Makes tooltips work on ajax generated content
     $(document).ajaxStop(function() {
-        $('[data-bs-toggle=\'tooltip\']').each(function() {
-            var option = []
-
+        $('[data-bs-toggle=\'tooltip\']').each(function(i, element) {
             handler = bootstrap.Tooltip.getInstance(element);
 
             if (!handler) {
+                var option = []
+
                 new bootstrap.Tooltip(element, option);
             } else {
                 console.log(handler);
@@ -159,26 +159,35 @@ oc.error = function(key, message) {
 }
 
 // Forms
-$(document).on('submit', '[data-oc-toggle=\'ajax\']', function(e) {
+$(document).submit('form[data-oc-toggle=\'ajax\']', function(e) {
     e.preventDefault();
+
+    console.log(e);
 
     var element = this;
 
     console.log(element);
 
-    var action = $(element).attr('action');
+    var form = e.target;
 
-    var method = $(element).attr('method');
+    var action = $(form).attr('action');
 
-    if (!method) {
+    console.log(action);
+
+    var method = $(form).attr('method');
+
+    if (typeof method == 'undefined') {
         method = 'post';
     }
 
+    console.log(method);
+
     var enctype = $(element).attr('enctype');
 
-    if (!enctype) {
-        enctype = 'application/x-www-form-urlencoded; charset=UTF-8';
+    if (typeof enctype == 'undefined') {
+        enctype = 'application/x-www-form-urlencoded';
     }
+    console.log(enctype);
 
     // https://github.com/opencart/opencart/issues/9690
     if (typeof CKEDITOR != 'undefined') {
@@ -189,17 +198,17 @@ $(document).on('submit', '[data-oc-toggle=\'ajax\']', function(e) {
 
     $.ajax({
         url: action,
-        type: 'post',
-        data: $(element).serialize(),
+        type: method,
+        data: $(form).serialize(),
         dataType: 'json',
         cache: false,
         contentType: enctype,
         processData: false,
         beforeSend: function() {
-            $(element).button('loading');
+            //$(element).button('loading');
         },
         complete: function() {
-            $(element).button('reset');
+          // $(element).button('reset');
         },
         success: function(json) {
             $(element).find('.is-invalid').removeClass('is-invalid');
@@ -256,7 +265,7 @@ $(document).on('submit', '[data-oc-toggle=\'ajax\']', function(e) {
 
             // Replace any form values that correspond to form names.
             for (key in json) {
-                $(form).find('[name=\'' + key + '\']').val(json[key]);
+                $(element).find('[name=\'' + key + '\']').val(json[key]);
             }
         },
         error: function(xhr, ajaxOptions, thrownError) {
@@ -264,6 +273,8 @@ $(document).on('submit', '[data-oc-toggle=\'ajax\']', function(e) {
             console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
         }
     });
+
+
 });
 
 // Upload
@@ -332,117 +343,6 @@ $(document).on('click', '[data-oc-upload]', function() {
         }
     }, 500);
 });
-
-/*
-+function($) {
-    'use strict';
-
-    // BUTTON PUBLIC CLASS DEFINITION
-    // ==============================
-
-    var Button = function(element, options) {
-        this.$element = $(element)
-        this.options = $.extend({}, Button.DEFAULTS, options)
-        this.isLoading = false
-    }
-
-    Button.VERSION = '3.3.5'
-
-    Button.DEFAULTS = {
-        loadingText: 'loading...'
-    }
-
-    Button.prototype.setState = function(state) {
-        var d = 'disabled'
-        var $el = this.$element
-        var val = $el.is('input') ? 'val' : 'html'
-        var data = $el.data()
-
-        state += 'Text'
-
-        if (data.resetText == null) $el.data('resetText', $el[val]())
-
-        // push to event loop to allow forms to submit
-        setTimeout($.proxy(function() {
-            $el[val](data[state] == null ? this.options[state] : data[state])
-
-            if (state == 'loadingText') {
-                this.isLoading = true
-                $el.addClass(d).attr(d, d)
-            } else if (this.isLoading) {
-                this.isLoading = false
-                $el.removeClass(d).removeAttr(d)
-            }
-        }, this), 0)
-    }
-
-    Button.prototype.toggle = function() {
-        var changed = true
-        var $parent = this.$element.closest('[data-bs-toggle="buttons"]')
-
-        if ($parent.length) {
-            var $input = this.$element.find('input')
-            if ($input.prop('type') == 'radio') {
-                if ($input.prop('checked')) changed = false
-                $parent.find('.active').removeClass('active')
-                this.$element.addClass('active')
-            } else if ($input.prop('type') == 'checkbox') {
-                if (($input.prop('checked')) !== this.$element.hasClass('active')) changed = false
-                this.$element.toggleClass('active')
-            }
-            $input.prop('checked', this.$element.hasClass('active'))
-            if (changed) $input.trigger('change')
-        } else {
-            this.$element.attr('aria-pressed', !this.$element.hasClass('active'))
-            this.$element.toggleClass('active')
-        }
-    }
-
-
-    // BUTTON PLUGIN DEFINITION
-    // ========================
-
-    function Plugin(option) {
-        return this.each(function() {
-            var $this = $(this)
-            var data = $this.data('bs.button')
-            var options = typeof option == 'object' && option
-
-            if (!data) $this.data('bs.button', (data = new Button(this, options)))
-
-            if (option == 'toggle') data.toggle()
-            else if (option) data.setState(option)
-        })
-    }
-
-    var old = $.fn.button
-
-    $.fn.button = Plugin
-    $.fn.button.Constructor = Button
-
-
-    // BUTTON NO CONFLICT
-    // ==================
-
-    $.fn.button.noConflict = function() {
-        $.fn.button = old
-        return this
-    }
-
-
-    // BUTTON DATA-API
-    // ===============
-
-    $(document).on('click.bs.button.data-api', '[data-bs-toggle^="button"]', function(e) {
-        var $btn = $(e.target)
-        if (!$btn.hasClass('btn')) $btn = $btn.closest('.btn')
-        Plugin.call($btn, 'toggle')
-        if (!($(e.target).is('input[type="radio"]') || $(e.target).is('input[type="checkbox"]'))) e.preventDefault()
-    }).on('focus.bs.button.data-api blur.bs.button.data-api', '[data-bs-toggle^="button"]', function(e) {
-        $(e.target).closest('.btn').toggleClass('focus', /^focus(in)?$/.test(e.type))
-    })
-}(jQuery);
-*/
 
 // Image Manager
 $(document).on('click', '[data-oc-toggle=\'image\']', function(e) {

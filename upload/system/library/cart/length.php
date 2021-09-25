39f5f80dc6d1f07a7ded663b820719acb728f2b8
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2021-09-25 15:27:22 -0400

    Added array type on lengths instance variable

--- upload/system/library/cart/length.php
+++ upload/system/library/cart/length.php
@@ -1,7 +1,7 @@
 <?php
 namespace Opencart\System\Library\Cart;
 class Length {
-	private $lengths = [];
+	private array $lengths = [];
 	private $language;
 
 	public function __construct(\Opencart\System\Engine\Registry $registry) {

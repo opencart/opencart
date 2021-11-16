540575b925f2ee06fb7973b13d67452b0944d979
ADDCreative <2039130+ADDCreative@users.noreply.github.com>
2021-11-16 09:44:03 +0000

    Split base64 encoded lines in mail

--- upload/system/library/mail/mail.php
+++ upload/system/library/mail/mail.php
@@ -28,7 +28,7 @@ class Mail {
 			$message  = '--' . $boundary . PHP_EOL;
 			$message .= 'Content-Type: text/plain; charset="utf-8"' . PHP_EOL;
 			$message .= 'Content-Transfer-Encoding: base64' . PHP_EOL . PHP_EOL;
-			$message .= base64_encode($this->text) . PHP_EOL;
+			$message .= chunk_split(base64_encode($this->text)) . PHP_EOL;
 		} else {
 			$message  = '--' . $boundary . PHP_EOL;
 			$message .= 'Content-Type: multipart/alternative; boundary="' . $boundary . '_alt"' . PHP_EOL . PHP_EOL;
@@ -37,15 +37,15 @@ class Mail {
 			$message .= 'Content-Transfer-Encoding: base64' . PHP_EOL . PHP_EOL;
 
 			if ($this->text) {
-				$message .= base64_encode($this->text) . PHP_EOL;
+				$message .= chunk_split(base64_encode($this->text)) . PHP_EOL;
 			} else {
-				$message .= base64_encode('This is a HTML email and your email client software does not support HTML email!') . PHP_EOL;
+				$message .= chunk_split(base64_encode('This is a HTML email and your email client software does not support HTML email!')) . PHP_EOL;
 			}
 
 			$message .= '--' . $boundary . '_alt' . PHP_EOL;
 			$message .= 'Content-Type: text/html; charset="utf-8"' . PHP_EOL;
 			$message .= 'Content-Transfer-Encoding: base64' . PHP_EOL . PHP_EOL;
-			$message .= base64_encode($this->html) . PHP_EOL;
+			$message .= chunk_split(base64_encode($this->html)) . PHP_EOL;
 			$message .= '--' . $boundary . '_alt--' . PHP_EOL;
 		}
 

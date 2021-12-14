772b0f8779975c77341a07a2dfe1b0df72c8e2a9
Daniel Kerr <webmaster@opencart.com>
2021-12-14 23:57:55 +0800

    more order editor work

--- upload/catalog/language/en-gb/api/sale/cart.php
+++ upload/catalog/language/en-gb/api/sale/cart.php
@@ -1,10 +1,23 @@
 <?php
 // Text
-$_['text_success']   = 'Success: You have modified your shopping cart!';
+$_['text_success']             = 'Success: You have modified your shopping cart!';
+$_['text_day']                 = 'day';
+$_['text_week']                = 'week';
+$_['text_semi_month']          = 'half-month';
+$_['text_month']               = 'month';
+$_['text_year']                = 'year';
+$_['text_recurring']           = '%s every %s %s';
+$_['text_recurring_payment']   = 'Payment Profile';
+$_['text_recurring_item']      = 'Recurring Item';
+$_['text_recurring_cancel']    = 'until cancelled';
+$_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
+$_['text_payment_cancel']      = '%s every %d %s(s) until canceled';
+$_['text_trial']               = '%s every %s %s for %s payments then ';
+$_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
 
 // Error
-$_['error_stock']    = 'Products marked with *** are not available in the desired quantity or not in stock!';
-$_['error_minimum']  = 'Minimum order amount for %s is %s!';
-$_['error_store']    = 'Product can not be bought from the store you have choosen!';
-$_['error_required'] = '%s required!';
-$_['error_product']  = 'Warning: Product could not be found!';
+$_['error_stock']               = 'Products marked with *** are not available in the desired quantity or not in stock!';
+$_['error_minimum']             = 'Minimum order amount for %s is %s!';
+$_['error_store']               = 'Product can not be bought from the store you have choosen!';
+$_['error_required']            = '%s required!';
+$_['error_product']             = 'Warning: Product could not be found!';

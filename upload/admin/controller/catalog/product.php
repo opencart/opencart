b0e3746a9dba09e00e0a40aeea48f6b86704ace2
Daniel Kerr <webmaster@opencart.com>
2022-01-10 22:01:17 +0800

    https://github.com/opencart/opencart/issues/10360

--- upload/admin/controller/catalog/product.php
+++ upload/admin/controller/catalog/product.php
@@ -1219,66 +1219,64 @@ class Product extends \Opencart\System\Engine\Controller {
 			$limit = 5;
 		}
 
-		if ($filter_name || $filter_model) {
-			$this->load->model('catalog/product');
-			$this->load->model('catalog/option');
+		$this->load->model('catalog/product');
+		$this->load->model('catalog/option');
 
-			$filter_data = [
-				'filter_name'  => $filter_name,
-				'filter_model' => $filter_model,
-				'start'        => 0,
-				'limit'        => $limit
-			];
+		$filter_data = [
+			'filter_name'  => $filter_name,
+			'filter_model' => $filter_model,
+			'start'        => 0,
+			'limit'        => $limit
+		];
 
-			$results = $this->model_catalog_product->getProducts($filter_data);
+		$results = $this->model_catalog_product->getProducts($filter_data);
 
-			foreach ($results as $result) {
-				$option_data = [];
+		foreach ($results as $result) {
+			$option_data = [];
 
-				$product_options = $this->model_catalog_product->getOptions($result['product_id']);
+			$product_options = $this->model_catalog_product->getOptions($result['product_id']);
 
-				foreach ($product_options as $product_option) {
-					$option_info = $this->model_catalog_option->getOption($product_option['option_id']);
+			foreach ($product_options as $product_option) {
+				$option_info = $this->model_catalog_option->getOption($product_option['option_id']);
 
-					if ($option_info) {
-						$product_option_value_data = [];
+				if ($option_info) {
+					$product_option_value_data = [];
 
-						foreach ($product_option['product_option_value'] as $product_option_value) {
-							$option_value_info = $this->model_catalog_option->getValue($product_option_value['option_value_id']);
+					foreach ($product_option['product_option_value'] as $product_option_value) {
+						$option_value_info = $this->model_catalog_option->getValue($product_option_value['option_value_id']);
 
-							if ($option_value_info) {
-								$product_option_value_data[] = [
-									'product_option_value_id' => $product_option_value['product_option_value_id'],
-									'option_value_id'         => $product_option_value['option_value_id'],
-									'name'                    => $option_value_info['name'],
-									'price'                   => (float)$product_option_value['price'] ? $this->currency->format($product_option_value['price'], $this->config->get('config_currency')) : false,
-									'price_prefix'            => $product_option_value['price_prefix']
-								];
-							}
+						if ($option_value_info) {
+							$product_option_value_data[] = [
+								'product_option_value_id' => $product_option_value['product_option_value_id'],
+								'option_value_id'         => $product_option_value['option_value_id'],
+								'name'                    => $option_value_info['name'],
+								'price'                   => (float)$product_option_value['price'] ? $this->currency->format($product_option_value['price'], $this->config->get('config_currency')) : false,
+								'price_prefix'            => $product_option_value['price_prefix']
+							];
 						}
-
-						$option_data[] = [
-							'product_option_id'    => $product_option['product_option_id'],
-							'product_option_value' => $product_option_value_data,
-							'option_id'            => $product_option['option_id'],
-							'name'                 => $option_info['name'],
-							'type'                 => $option_info['type'],
-							'value'                => $product_option['value'],
-							'required'             => $product_option['required']
-						];
 					}
+
+					$option_data[] = [
+						'product_option_id'    => $product_option['product_option_id'],
+						'product_option_value' => $product_option_value_data,
+						'option_id'            => $product_option['option_id'],
+						'name'                 => $option_info['name'],
+						'type'                 => $option_info['type'],
+						'value'                => $product_option['value'],
+						'required'             => $product_option['required']
+					];
 				}
-
-				$json[] = [
-					'product_id' => $result['product_id'],
-					'name'       => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
-					'model'      => $result['model'],
-					'option'     => $option_data,
-					'price'      => $result['price']
-				];
 			}
-		}
 
+			$json[] = [
+				'product_id' => $result['product_id'],
+				'name'       => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+				'model'      => $result['model'],
+				'option'     => $option_data,
+				'price'      => $result['price']
+			];
+		}
+		
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
 	}

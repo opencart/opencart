757842cb8881ec83a8738e7323da33b147aed94f
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-01-19 21:29:26 -0500

    More backward escaped quotes

--- upload/catalog/model/localisation/country.php
+++ upload/catalog/model/localisation/country.php
@@ -2,7 +2,7 @@
 namespace Opencart\Catalog\Model\Localisation;
 class Country extends \Opencart\System\Engine\Model {
 	public function getCountry(int $country_id): array {
-		$query = $this->db->query("SELECT *, c.`name` FROM `" . DB_PREFIX . "country` c LEFT JOIN `" . DB_PREFIX . "address_format` af ON (c.`address_format_id` = af.`address_format_id`) WHERE c.`country_id` = '" . (int)$country_id . "' AND c.`status` = '1'");
+		$query = $this->db->query("SELECT *, c.`name` FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "address_format` af ON (c.`address_format_id` = af.`address_format_id`) WHERE c.`country_id` = '" . (int)$country_id . "' AND c.`status` = '1'");
 
 		return $query->row;
 	}
@@ -11,7 +11,7 @@ class Country extends \Opencart\System\Engine\Model {
 		$country_data = $this->cache->get('country.catalog');
 
 		if (!$country_data) {
-			$query = $this->db->query("SELECT *, c.`name` FROM `" . DB_PREFIX . "country` c LEFT JOIN `" . DB_PREFIX . "address_format` af ON (c.`address_format_id` = af.`address_format_id`) WHERE c.`status` = '1' ORDER BY c.`name` ASC");
+			$query = $this->db->query("SELECT *, c.`name` FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "address_format` af ON (c.`address_format_id` = af.`address_format_id`) WHERE c.`status` = '1' ORDER BY c.`name` ASC");
 
 			$country_data = $query->rows;
 

3dbd6c09bd5ee3e68198e234ece42591c174a8c3
Daniel Kerr <webmaster@opencart.com>
2022-03-08 19:20:28 +0800

    https://github.com/opencart/opencart/issues/10441

--- upload/admin/controller/sale/order.php
+++ upload/admin/controller/sale/order.php
@@ -414,7 +414,8 @@ class Order extends \Opencart\System\Engine\Controller {
 		$data['error_upload_size'] = sprintf($this->language->get('error_upload_size'), $this->config->get('config_file_max_size'));
 
 		$data['config_file_max_size'] = ((int)$this->config->get('config_file_max_size') * 1024 * 1024);
-
+		$data['config_telephone_required'] = $this->config->get('config_telephone_required');
+		
 		$url = '';
 
 		if (isset($this->request->get['filter_order_id'])) {

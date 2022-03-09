2fcfbc0a17ac9f194db8daaf4627e21223d6e2f1
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-03-09 14:47:43 -0500

    Fixed Cart Recurring Lookup

--- upload/extension/opencart/catalog/model/payment/bank_transfer.php
+++ upload/extension/opencart/catalog/model/payment/bank_transfer.php
@@ -6,7 +6,7 @@ class BankTransfer extends \Opencart\System\Engine\Model {
 
 		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('payment_bank_transfer_geo_zone_id') . "' AND `country_id` = '" . (int)$address['country_id'] . "' AND (`zone_id` = '" . (int)$address['zone_id'] . "' OR `zone_id` = '0')");
 
-		if ($this->cart->hasRecuirring()) {
+		if ($this->cart->hasRecurring()) {
 			$status = false;
 		} elseif (!$this->config->get('payment_bank_transfer_geo_zone_id')) {
 			$status = true;

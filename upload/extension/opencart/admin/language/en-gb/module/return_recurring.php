29a97fdde4498a541189fbafb936220c01885caf
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-04-21 12:04:42 -0400

    [Master] - Added return recurring language def

---
+++ upload/extension/opencart/admin/language/en-gb/module/return_recurring.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title']    	= 'Return Recurring';
+
+// Text
+$_['text_extension']   	= 'Extensions';
+$_['text_edit']        	= 'Edit Return Recurring Module';
+$_['text_info']		= 'To change the status of this extension module, please visit the: Extensions > Events page!';
+
+// Error
+$_['error_permission'] 	= 'Warning: You do not have permission to modify return recurring module!';

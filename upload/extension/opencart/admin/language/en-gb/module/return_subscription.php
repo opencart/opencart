1a50f09071fc91c30ff10bc9eae9eebf812baa29
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-04-21 13:39:55 -0400

    Renamed recurring to subscription

--- upload/extension/opencart/admin/language/en-gb/module/return_subscription.php
+++ upload/extension/opencart/admin/language/en-gb/module/return_subscription.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']    	= 'Return Recurring';
+$_['heading_title']    	= 'Return Subscription';
 
 // Text
 $_['text_extension']   	= 'Extensions';
-$_['text_edit']        	= 'Edit Return Recurring Module';
-$_['text_info']		= 'To change the status of this extension module, please visit the: Extensions > Events page!';
+$_['text_edit']        	= 'Edit Return Subscription Module';
+$_['text_info']			= 'To change the status of this extension module, please visit the: Extensions > Events page!';
 
 // Error
-$_['error_permission'] 	= 'Warning: You do not have permission to modify return recurring module!';
+$_['error_permission'] 	= 'Warning: You do not have permission to modify return subscription module!';

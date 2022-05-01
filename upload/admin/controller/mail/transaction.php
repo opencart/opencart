83f828ad93278a521b6e295965b8ff3bda08b244
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-01 13:18:21 -0400

    Added backslashes on new instances

--- upload/admin/controller/mail/transaction.php
+++ upload/admin/controller/mail/transaction.php
@@ -45,7 +45,7 @@ class ControllerMailTransaction extends Controller {
 			$data['text_received'] = sprintf($this->language->get('text_received'), $this->currency->format($amount, $this->config->get('config_currency')));
 			$data['text_total'] = sprintf($this->language->get('text_total'), $this->currency->format($this->model_customer_customer->getTransactionTotal($customer_id), $this->config->get('config_currency')));
 			
-			$mail = new Mail($this->config->get('config_mail_engine'));
+			$mail = new \Mail($this->config->get('config_mail_engine'));
 			$mail->parameter = $this->config->get('config_mail_parameter');
 			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 			$mail->smtp_username = $this->config->get('config_mail_smtp_username');

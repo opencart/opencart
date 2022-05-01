3b7cdfd9803e0a389d1033f9ea510dac3ccda738
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-01 13:16:17 -0400

    Added backslashes on new instances

--- upload/admin/controller/mail/reward.php
+++ upload/admin/controller/mail/reward.php
@@ -45,7 +45,7 @@ class ControllerMailReward extends Controller {
 			$data['text_received'] = sprintf($this->language->get('text_received'), $points);
 			$data['text_total'] = sprintf($this->language->get('text_total'), $this->model_customer_customer->getRewardTotal($customer_id));
 
-			$mail = new Mail($this->config->get('config_mail_engine'));
+			$mail = new \Mail($this->config->get('config_mail_engine'));
 			$mail->protocol = $this->config->get('config_mail_protocol');
 			$mail->parameter = $this->config->get('config_mail_parameter');
 			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');

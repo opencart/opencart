24bbe895e34caf42aff640136f515799e220609b
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-01 18:51:44 -0400

    Added backslashes on new instances

--- upload/system/library/language.php
+++ upload/system/library/language.php
@@ -76,7 +76,7 @@ class Language {
 			$this->data = array_merge($this->data, $_);
 		} else {
 			// Put the language into a sub key
-			$this->data[$key] = new Language($this->directory);
+			$this->data[$key] = new \Language($this->directory);
 			$this->data[$key]->load($filename);
 		}
 		

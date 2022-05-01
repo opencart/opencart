b35de61bb4198814da44bfa6e6de338db0428892
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-01 13:10:55 -0400

    Added backslashes on new instances

--- upload/admin/controller/mail/affiliate.php
+++ upload/admin/controller/mail/affiliate.php
@@ -28,7 +28,7 @@ class ControllerMailAffiliate extends Controller {
 				$language_code = $this->config->get('config_language');
 			}
 
-			$language = new Language($language_code);
+			$language = new \Language($language_code);
 			$language->load($language_code);
 			$language->load('mail/affiliate_approve');
 
@@ -39,7 +39,7 @@ class ControllerMailAffiliate extends Controller {
 			$data['login'] = $store_url . 'index.php?route=affiliate/login';
 			$data['store'] = $store_name;
 
-			$mail = new Mail($this->config->get('config_mail_engine'));
+			$mail = new \Mail($this->config->get('config_mail_engine'));
 			$mail->parameter = $this->config->get('config_mail_parameter');
 			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 			$mail->smtp_username = $this->config->get('config_mail_smtp_username');
@@ -84,7 +84,7 @@ class ControllerMailAffiliate extends Controller {
 				$language_code = $this->config->get('config_language');
 			}
 
-			$language = new Language($language_code);
+			$language = new \Language($language_code);
 			$language->load($language_code);
 			$language->load('mail/affiliate_deny');
 
@@ -95,7 +95,7 @@ class ControllerMailAffiliate extends Controller {
 			$data['contact'] = $store_url . 'index.php?route=information/contact';
 			$data['store'] = $store_name;
 
-			$mail = new Mail($this->config->get('config_mail_engine'));
+			$mail = new \Mail($this->config->get('config_mail_engine'));
 			$mail->parameter = $this->config->get('config_mail_parameter');
 			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 			$mail->smtp_username = $this->config->get('config_mail_smtp_username');

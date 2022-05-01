e3f4664382c43e4e1105710e7a4767b7197cfc25
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-01 17:16:45 -0400

    Made various changes

--- upload/catalog/model/extension/payment/g2apay.php
+++ upload/catalog/model/extension/payment/g2apay.php
@@ -1,6 +1,5 @@
 <?php
 class ModelExtensionPaymentG2APay extends Model {
-
 	public function getMethod($address, $total) {
 		$this->load->language('extension/payment/g2apay');
 
@@ -38,7 +37,6 @@ class ModelExtensionPaymentG2APay extends Model {
 		$this->db->query("UPDATE `" . DB_PREFIX . "g2apay_order` SET `g2apay_transaction_id` = '" . $this->db->escape($g2apay_transaction_id) . "', `modified` = now() WHERE `order_id` = '" . (int)$order_info['order_id'] . "'");
 
 		$this->addTransaction($g2apay_order_id, $type, $order_info);
-
 	}
 
 	public function addTransaction($g2apay_order_id, $type, $order_info) {
@@ -46,13 +44,9 @@ class ModelExtensionPaymentG2APay extends Model {
 	}
 
 	public function getG2aOrder($order_id) {
-		$qry = $this->db->query("SELECT * FROM `" . DB_PREFIX . "g2apay_order` WHERE `order_id` = '" . (int)$order_id . "' LIMIT 1");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "g2apay_order` WHERE `order_id` = '" . (int)$order_id . "' LIMIT 1");
 
-		if ($qry->num_rows) {
-			return $qry->row;
-		} else {
-			return false;
-		}
+		return $query->row;
 	}
 
 	public function sendCurl($url, $fields) {
@@ -62,6 +56,7 @@ class ModelExtensionPaymentG2APay extends Model {
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_POST, true);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $fields);
+		
 		$response = curl_exec($curl);
 
 		curl_close($curl);
@@ -70,9 +65,10 @@ class ModelExtensionPaymentG2APay extends Model {
 	}
 
 	public function logger($message) {
-		if ($this->config->get('payment_g2apay_debug') == 1) {
-			$log = new Log('g2apay.log');
+		if ($this->config->get('payment_g2apay_debug')) {			
 			$backtrace = debug_backtrace();
+			
+			$log = new \Log('g2apay.log');
 			$log->write('Origin: ' . $backtrace[6]['class'] . '::' . $backtrace[6]['function']);
 			$log->write(print_r($message, 1));
 		}

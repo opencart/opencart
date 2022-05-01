12934213f8f6a3f597df6d58c4b92bccf77d2dfd
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-01 16:40:09 -0400

    Added backslashes on new instances

--- upload/catalog/model/extension/module/paypal_smart_button.php
+++ upload/catalog/model/extension/module/paypal_smart_button.php
@@ -1,6 +1,5 @@
 <?php
-class ModelExtensionModulePayPalSmartButton extends Model {
-	
+class ModelExtensionModulePayPalSmartButton extends Model {	
 	public function hasProductInCart($product_id, $option = array(), $recurring_id = 0) {
 		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "cart WHERE api_id = '" . (isset($this->session->data['api_id']) ? (int)$this->session->data['api_id'] : 0) . "' AND customer_id = '" . (int)$this->customer->getId() . "' AND session_id = '" . $this->db->escape($this->session->getId()) . "' AND product_id = '" . (int)$product_id . "' AND recurring_id = '" . (int)$recurring_id . "' AND `option` = '" . $this->db->escape(json_encode($option)) . "'");
 		
@@ -19,9 +18,9 @@ class ModelExtensionModulePayPalSmartButton extends Model {
 		return $query->row;
 	}
 	
-	public function log($data, $title = null) {
+	public function addLog($data, $title = null) {
 		if ($this->config->get('payment_paypal_debug')) {
-			$log = new Log('paypal.log');
+			$log = new \Log('paypal.log');
 			$log->write('PayPal debug (' . $title . '): ' . json_encode($data));
 		}
 	}

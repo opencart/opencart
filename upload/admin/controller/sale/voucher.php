19a32f47dad936ae73d543a984e9f0a21ded0e0b
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-01 13:29:51 -0400

    Added backslashes on new instances

--- upload/admin/controller/sale/voucher.php
+++ upload/admin/controller/sale/voucher.php
@@ -586,7 +586,7 @@ class ControllerSaleVoucher extends Controller {
 						if ($order_info) {
 							$this->load->model('localisation/language');
 			
-							$language = new Language($order_info['language_code']);
+							$language = new \Language($order_info['language_code']);
 							$language->load($order_info['language_code']);
 							$language->load('mail/voucher');
 			
@@ -611,7 +611,7 @@ class ControllerSaleVoucher extends Controller {
 							$data['store_url'] = $order_info['store_url'];
 							$data['message'] = nl2br($voucher_info['message']);
 			
-							$mail = new Mail($this->config->get('config_mail_engine'));
+							$mail = new \Mail($this->config->get('config_mail_engine'));
 							$mail->parameter = $this->config->get('config_mail_parameter');
 							$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 							$mail->smtp_username = $this->config->get('config_mail_smtp_username');
@@ -650,7 +650,7 @@ class ControllerSaleVoucher extends Controller {
 							$data['store_url'] = HTTP_CATALOG;
 							$data['message'] = nl2br($voucher_info['message']);
 			
-							$mail = new Mail($this->config->get('config_mail_engine'));
+							$mail = new \Mail($this->config->get('config_mail_engine'));
 							$mail->parameter = $this->config->get('config_mail_parameter');
 							$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 							$mail->smtp_username = $this->config->get('config_mail_smtp_username');

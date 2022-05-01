ec9c09009ef6bc063c0a7c9bbe85f055a1934b43
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-01 15:55:04 -0400

    Added backslashes on new instances

--- upload/catalog/controller/extension/payment/klarna_invoice.php
+++ upload/catalog/controller/extension/payment/klarna_invoice.php
@@ -1,13 +1,13 @@
 <?php
 class ControllerExtensionPaymentKlarnaInvoice extends Controller {
 	public function index() {
+		$this->load->language('extension/payment/klarna_invoice');
+		
 		$this->load->model('checkout/order');
 
 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
 		if ($order_info) {
-			$this->load->language('extension/payment/klarna_invoice');
-
 			$data['days'] = array();
 
 			for ($i = 1; $i <= 31; $i++) {
@@ -368,7 +368,7 @@ class ControllerExtensionPaymentKlarnaInvoice extends Controller {
 				$response = curl_exec($curl);
 
 				if (curl_errno($curl)) {
-					$log = new Log('klarna_invoice.log');
+					$log = new \Log('klarna_invoice.log');
 					$log->write('HTTP Error for order #' . $order_info['order_id'] . '. Code: ' . curl_errno($curl) . ' message: ' . curl_error($curl));
 
 					$json['error'] = $this->language->get('error_network');
@@ -378,12 +378,12 @@ class ControllerExtensionPaymentKlarnaInvoice extends Controller {
 					if (isset($match[1])) {
 						preg_match('/<member><name>faultCode<\/name><value><int>([0-9]+)<\/int><\/value><\/member>/', $response, $match2);
 
-						$log = new Log('klarna_invoice.log');
+						$log = new \Log('klarna_invoice.log');
 						$log->write('Failed to create an invoice for order #' . $order_info['order_id'] . '. Message: ' . utf8_encode($match[1]) . ' Code: ' . $match2[1]);
 
 						$json['error'] = utf8_encode($match[1]);
 					} else {
-						$xml = new DOMDocument();
+						$xml = new \DOMDocument();
 						$xml->loadXML($response);
 
 						$invoice_number = $xml->getElementsByTagName('string')->item(0)->nodeValue;

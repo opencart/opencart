c50b4ba87fc98efdd57fef7dc51f154eb591fb99
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-05-12 16:44:34 -0400

    Cleaned code

--- upload/system/helper/general.php
+++ upload/system/helper/general.php
@@ -135,7 +135,6 @@ function format_size($file = '', $max = true) {
 	}
 
 	if ($size) {
-
 		$suffix = [];
 
 		$suffix = [
@@ -147,7 +146,6 @@ function format_size($file = '', $max = true) {
 		
 		$code = !empty($suffix[intval(log($size, 1024))]) ? $suffix[intval(log($size, 1024))] : end($suffix);
 		$format_size = $size/(1024 ** array_search($code, $suffix));
-
 		
 		return [$code, $format_size, $size];	
 	}

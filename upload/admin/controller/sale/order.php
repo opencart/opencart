c6e63785e33aded94bafd76ac1b0df086177cc10
picasolutions <105952216+picasolutions@users.noreply.github.com>
2022-05-20 23:55:30 +0100

    Update order.php for vivification error in PHP 8.1
    $session->data is not yet an array at line 391.  So PHP 8.1 warns that auto vivification is deprecated.
    Check for the condition, and ensure that it is an array.

--- upload/admin/controller/sale/order.php
+++ upload/admin/controller/sale/order.php
@@ -387,6 +387,7 @@ class ControllerSaleOrder extends Controller {
 			
 			$this->model_user_api->addApiSession($api_info['api_id'], $session->getId(), $this->request->server['REMOTE_ADDR']);
 			
+			if (!is_array($session->data))  $session->data = [];
 			$session->data['api_id'] = $api_info['api_id'];
 
 			$data['api_token'] = $session->getId();

aeefca6aa70277df907f14785f9a8b4d323915d7
OSWorX <info@osworx.net>
2022-05-24 15:28:48 +0200

    Update en-gb.php
    Typo

--- upload/catalog/language/en-gb/en-gb.php
+++ upload/catalog/language/en-gb/en-gb.php
@@ -84,7 +84,7 @@ $_['error_upload_8']        = 'Warning: File upload stopped by extension!';
 $_['error_upload_999']      = 'Warning: No error code available!';
 $_['error_upload_size']     = 'Warning: The uploaded file exceeds the %smb max file size!';
 $_['error_curl']            = 'CURL: Error Code(%s): %s';
-$_['error_session']         = 'Warning: Session has expired please submit the foprm again!';
+$_['error_session']         = 'Warning: Session has expired please submit the form again!';
 
 /* When doing translations only include the matching language code */
 // Datepicker

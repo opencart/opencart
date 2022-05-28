c235bb08cf02ef7dc6ca819fc6d2db79863af8b5
Daniel Kerr <webmaster@opencart.com>
2022-05-29 04:12:36 +0800

    webp added to image resizer

--- upload/catalog/model/tool/image.php
+++ upload/catalog/model/tool/image.php
@@ -14,7 +14,7 @@ class Image extends \Opencart\System\Engine\Model {
 		if (!is_file(DIR_IMAGE . $image_new) || (filemtime(DIR_IMAGE . $image_old) > filemtime(DIR_IMAGE . $image_new))) {
 			list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
 				 
-			if (!in_array($image_type, [IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF])) {
+			if (!in_array($image_type, [IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF, IMAGETYPE_WEBP])) {
 				return $this->config->get('config_url') . 'image/' . $image_old;
 			}
 						

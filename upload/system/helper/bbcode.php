9d225fd4d468ab57fc9bdf5c17b29b8259651a5f
Daniel Kerr <webmaster@opencart.com>
2022-06-02 01:26:13 +0800

    https://github.com/opencart/opencart/issues/10983

--- upload/system/helper/bbcode.php
+++ upload/system/helper/bbcode.php
@@ -62,8 +62,8 @@ function bbcode_decode(string $string) {
 	$replace[12] = '<span style="color: $1;">$2</span>';
 
 	// YouTube
-	$pattern[9] = '/\[youtube\](.*?)\[\/youtube\]/is';
-	$replace[9] = '<iframe width="560" height="315" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>';
+	$pattern[13] = '/\[youtube\](.*?)\[\/youtube\]/is';
+	$replace[13] = '<iframe width="560" height="315" src="http://www.youtube.com/embed/$1" allowfullscreen></iframe>';
 
 	$string = preg_replace($pattern, $replace, $string);
 

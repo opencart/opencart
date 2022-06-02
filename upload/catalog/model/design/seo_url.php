9abeb2f4589bf578cb330fc23a1543d66fa10a03
mohammad abedy <m.h.o.abedi@gmail.com>
2022-06-03 01:28:55 +0430

    pdo problem solved

--- upload/catalog/model/design/seo_url.php
+++ upload/catalog/model/design/seo_url.php
@@ -2,7 +2,7 @@
 namespace Opencart\Catalog\Model\Design;
 class SeoUrl extends \Opencart\System\Engine\Model {
 	public function getSeoUrlByKeyword(string $keyword): array {
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "seo_url` WHERE (`keyword` = '" . $this->db->escape($keyword) . "' OR `keyword` LIKE '%/" . $this->db->escape($keyword) . "') AND `store_id` = '" . (int)$this->config->get('config_store_id') . "' AND `language_id` = '" . (int)$this->config->get('config_language_id') . "'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "seo_url` WHERE (`keyword` = '" . $this->db->escape($keyword) . "' OR `keyword` LIKE '" . $this->db->escape("%".$keyword) . "') AND `store_id` = '" . (int)$this->config->get('config_store_id') . "' AND `language_id` = '" . (int)$this->config->get('config_language_id') . "'");
 
 		return $query->row;
 	}

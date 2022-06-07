b5fda3594518dfdf58ff4d1790e65da022c36271
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-06-07 13:35:08 -0400

    Added backward escaped quotes

--- upload/catalog/model/localisation/country.php
+++ upload/catalog/model/localisation/country.php
@@ -8,13 +8,13 @@ class Country extends \Opencart\System\Engine\Model {
 	}
 
 	public function getCountryByIsoCode2($iso_code_2): array {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE `iso_code_2` = '" . $this->db->escape($iso_code_2) . "' AND `status` = '1'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `iso_code_2` = '" . $this->db->escape($iso_code_2) . "' AND `status` = '1'");
 
 		return $query->row;
 	}
 
 	public function getCountryByIsoCode3($iso_code_3): array {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE `iso_code_3` = '" . $this->db->escape($iso_code_3) . "' AND `status` = '1'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `iso_code_3` = '" . $this->db->escape($iso_code_3) . "' AND `status` = '1'");
 
 		return $query->row;
 	}
@@ -23,7 +23,7 @@ class Country extends \Opencart\System\Engine\Model {
 		$country_data = $this->cache->get('country.catalog');
 
 		if (!$country_data) {
-			$query = $this->db->query("SELECT *, c.name FROM `" . DB_PREFIX . "country` c LEFT JOIN `" . DB_PREFIX . "address_format` af ON (`c`.`address_format_id` = `af`.`address_format_id`) WHERE `c`.`status` = '1' ORDER BY c.`name` ASC");
+			$query = $this->db->query("SELECT *, c.`name` FROM `" . DB_PREFIX . "country` c LEFT JOIN `" . DB_PREFIX . "address_format` af ON (`c`.`address_format_id` = `af`.`address_format_id`) WHERE `c`.`status` = '1' ORDER BY c.`name` ASC");
 
 			$country_data = $query->rows;
 

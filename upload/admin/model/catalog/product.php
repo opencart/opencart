f0e11fd2857f7cc9a2c3b898517fbdf6a2d58bc5
condor2 <condor20_05@yahoo.it>
2022-06-11 23:32:49 +0300

    [3.0.x.x] Removed is_null from filter price-Product model

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -653,7 +653,7 @@ class ModelCatalogProduct extends Model {
 			$sql .= " AND p.model LIKE '" . $this->db->escape($data['filter_model']) . "%'";
 		}
 
-		if (isset($data['filter_price']) && !is_null($data['filter_price'])) {
+		if (isset($data['filter_price']) && $data['filter_price'] !== '') {
 			$sql .= " AND p.price LIKE '" . $this->db->escape($data['filter_price']) . "%'";
 		}
 

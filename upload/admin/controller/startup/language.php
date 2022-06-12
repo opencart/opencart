c10b554f5b711a73d8fd7fce20bcc3928a3dca94
Daniel Kerr <webmaster@opencart.com>
2022-06-12 22:26:44 +0800

    https://github.com/opencart/opencart/issues/11222

--- upload/admin/controller/startup/language.php
+++ upload/admin/controller/startup/language.php
@@ -2,28 +2,33 @@
 namespace Opencart\Admin\Controller\Startup;
 class Language extends \Opencart\System\Engine\Controller {
 	public function index(): void {
-		// Added this code so that backup and restore doesn't show text_restore
-		$code = $this->config->get('config_language_admin');
-
-		if ($code) {
-			$this->session->data['language_admin'] = $code;
-		} elseif (isset($this->session->data['language_admin'])) {
-			$this->config->set('config_language_admin', $this->session->data['language_admin']);
-		}
-
-		// Language
 		$this->load->model('localisation/language');
 
-		$language_info = $this->model_localisation_language->getLanguageByCode($this->config->get('config_language_admin'));
+		$languages = $this->model_localisation_language->getLanguages();
 
-		if ($language_info) {
-			$this->config->set('config_language_id', $language_info['language_id']);
+		$language_codes = array_column($languages, 'language_id', 'code');
+
+		$code = '';
+
+		if (isset($this->session->data['language_admin'])) {
+			$code = $this->session->data['language_admin'];
 		}
 
+		// Language not available then use default
+		if (!array_key_exists($code, $language_codes)) {
+			$code = $this->config->get('config_language_admin');
+		}
+
+		// Set the config language_id
+		$this->config->set('config_language_id', $language_codes[$code]);
+		$this->config->set('config_language_admin', $code);
+
+		$this->session->data['language_admin'] = $code;
+
 		// Language
-		$language = new \Opencart\System\Library\Language($this->config->get('config_language_admin'));
+		$language = new \Opencart\System\Library\Language($code);
 		$language->addPath(DIR_LANGUAGE);
-		$language->load($this->config->get('config_language_admin'));
+		$language->load($code);
 		
 		$this->registry->set('language', $language);
 	}

efc034654eddcd60e8f1a6d5cc3c993de59a88b3
Daniel Kerr <webmaster@opencart.com>
2022-06-14 03:51:10 +0800

    https://github.com/opencart/opencart/issues/11231

--- upload/admin/language/en-gb/marketplace/installer.php
+++ upload/admin/language/en-gb/marketplace/installer.php
@@ -1,25 +1,23 @@
 <?php
 // Heading
-$_['heading_title']     = 'Extension Installer';
+$_['heading_title']          = 'Extension Installer';
 
 // Text
-$_['text_upload']       = 'Success: The extension has been uploaded!';
-$_['text_install']      = 'Success: The extension has been installed!';
-$_['text_uninstall']    = 'Success: The extension has been uninstalled!';
-$_['text_delete']       = 'Success: The extension has been deleted!';
-$_['text_progress']     = 'Install Progress';
-$_['text_installed']    = 'Installed Extensions';
-$_['text_unknown']		= 'Unknown';
+$_['text_upload']            = 'Success: The extension has been uploaded!';
+$_['text_success']           = 'Success: You have modified extensions!';
+$_['text_progress']          = 'Install Progress';
+$_['text_installed']         = 'Installed Extensions';
+$_['text_unknown']		     = 'Unknown';
 
 // Column
-$_['column_image']      = 'Image';
-$_['column_name']       = 'Extension Name';
-$_['column_version']    = 'Version';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_image']           = 'Image';
+$_['column_name']            = 'Extension Name';
+$_['column_version']         = 'Version';
+$_['column_date_added']      = 'Date Added';
+$_['column_action']          = 'Action';
 
 // Entry
-$_['entry_progress']    = 'Progress';
+$_['entry_progress']         = 'Progress';
 
 // Error
 $_['error_permission']       = 'Warning: You do not have permission to modify extensions!';
@@ -28,7 +26,7 @@ $_['error_installed']        = 'Extension already installed!';
 $_['error_filename']         = 'Filename must be between 3 and 128 characters!';
 $_['error_file']             = 'Install file %s could not be found!';
 $_['error_file_exists']      = 'File already exist!';
-$_['error_file_type']         = 'Invalid file type!';
+$_['error_file_type']        = 'Invalid file type!';
 $_['error_directory']        = 'Install directory %s could not be found!';
 $_['error_directory_exists'] = 'Path %s already exists!';
 $_['error_unzip']            = 'Zip file could not be opened!';

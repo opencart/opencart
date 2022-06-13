5fb3a793120605edce26d6c165fe3b4ccec20dc2
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-06-13 06:00:37 -0400

    Added format_size() helper function

--- upload/catalog/controller/checkout/shipping_address.php
+++ upload/catalog/controller/checkout/shipping_address.php
@@ -4,10 +4,13 @@ class ShippingAddress extends \Opencart\System\Engine\Controller {
 	public function index(): string {
 		$this->load->language('checkout/shipping_address');
 
-		$data['error_upload_size'] = sprintf($this->language->get('error_upload_size'), $this->config->get('config_file_max_size'));
+		[$code, $format_size, $size] = format_size();
 
-		$data['config_checkout_address'] = $this->config->get('config_checkout_address');
-		$data['config_file_max_size'] = ((int)$this->config->get('config_file_max_size') * 1024 * 1024);
+		$data['error_upload_size'] = sprintf($this->language->get('error_format_' . $code), $format_size);
+
+		$data['config_file_max_size'] = ((int)preg_filter('/[^0-9]/', '', ini_get('upload_max_filesize')) * 1024 * 1024);
+
+		$data['config_checkout_address'] = $this->config->get('config_checkout_address');		
 
 		$data['upload'] = $this->url->link('tool/upload', 'language=' . $this->config->get('config_language'));
 

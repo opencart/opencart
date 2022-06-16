205875527fa4b3962712cb248dbd902b6622df2c
Daniel Kerr <webmaster@opencart.com>
2022-06-17 02:49:44 +0800

    Update install.php

--- upload/install/model/install/install.php
+++ upload/install/model/install/install.php
@@ -85,7 +85,7 @@ class Install extends \Opencart\System\Engine\Model {
 		$db->query("DELETE FROM `" . $data['db_prefix'] . "setting` WHERE `key` = 'config_email'");
 		$db->query("INSERT INTO `" . $data['db_prefix'] . "setting` SET `code` = 'config', `key` = 'config_email', `value` = '" . $db->escape($data['email']) . "'");
 		$db->query("DELETE FROM `" . $data['db_prefix'] . "setting` WHERE `key` = 'config_encryption'");
-		$db->query("INSERT INTO `" . $data['db_prefix'] . "setting` SET `code` = 'config', `key` = 'config_encryption', `value` = '" . $db->escape(token(1024)) . "'");
+		$db->query("INSERT INTO `" . $data['db_prefix'] . "setting` SET `code` = 'config', `key` = 'config_encryption', `value` = '" . $db->escape(token(512)) . "'");
 
 		$db->query("INSERT INTO `" . $data['db_prefix'] . "api` SET `username` = 'Default', `key` = '" . $db->escape(token(256)) . "', `status` = '1', `date_added` = NOW(), `date_modified` = NOW()");
 

7b3d103dd4c2cc65b9165fcc66c10a3ce2f5b1d7
condor2 <condor20_05@yahoo.it>
2022-06-18 10:08:55 +0300

    Update custom_field.php

--- upload/admin/model/customer/custom_field.php
+++ upload/admin/model/customer/custom_field.php
@@ -87,7 +87,7 @@ class CustomField extends \Opencart\System\Engine\Model {
 	}
 
 	public function getCustomFields(array $data = []): array {
-			$sql = "SELECT * FROM `" . DB_PREFIX . "custom_field_customer_group` cfcg LEFT JOIN `" . DB_PREFIX . "custom_field` cf ON (cfcg.`custom_field_id` = cf.`custom_field_id`) LEFT JOIN `" . DB_PREFIX . "custom_field_description` cfd ON (cf.`custom_field_id` = cfd.`custom_field_id`) WHERE cfd.`language_id` = '" . (int)$this->config->get('config_language_id') . "'";
+		$sql = "SELECT * FROM `" . DB_PREFIX . "custom_field` cf LEFT JOIN `" . DB_PREFIX . "custom_field_description` cfd ON (cf.`custom_field_id` = cfd.`custom_field_id`) WHERE cfd.`language_id` = '" . (int)$this->config->get('config_language_id') . "'";
 
 		$sort_data = [
 			'cfd.name',

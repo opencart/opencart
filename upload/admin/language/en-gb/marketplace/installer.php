340243898c498c4125fc5931c7623b7d54290404
Daniel Kerr <webmaster@opencart.com>
2022-06-27 03:54:34 +0800

    Update installer.php

--- upload/admin/language/en-gb/marketplace/installer.php
+++ upload/admin/language/en-gb/marketplace/installer.php
@@ -21,14 +21,13 @@ $_['entry_progress']         = 'Progress';
 // Error
 $_['error_permission']       = 'Warning: You do not have permission to modify extensions!';
 $_['error_install']          = 'Warning: Could not find install.json!';
-
+$_['error_default']          = 'Default extension could not be uninstalled or deleted!';
 $_['error_extension']        = 'Installed extension could not be found!';
 $_['error_installed']        = 'Extension already installed!';
 $_['error_name']             = 'Name must be between 3 and 128 characters!';
 $_['error_version']          = 'Version must be between 3 and 128 characters!';
 $_['error_author']           = 'Author must be between 3 and 128 characters!';
 $_['error_link']             = 'Link must be between 3 and 128 characters!';
-
 $_['error_filename']         = 'Filename must be between 3 and 128 characters!';
 $_['error_file']             = 'Install file %s could not be found!';
 $_['error_file_exists']      = 'File already exist!';

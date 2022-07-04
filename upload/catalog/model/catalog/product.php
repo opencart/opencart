b867ba943251d9b81d2dd989dc113da03099ce0e
JNeuhoff <neuhoff@mhccorp.com>
2022-07-04 13:39:57 +0100

    PHP 8.x support for catalog/model/product.php

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -260,6 +260,7 @@ class ModelCatalogProduct extends Model {
 		$product_data = $this->cache->get('product.latest.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit);
 
 		if (!$product_data) {
+			$product_data = array();
 			$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' ORDER BY p.date_added DESC LIMIT " . (int)$limit);
 
 			foreach ($query->rows as $result) {

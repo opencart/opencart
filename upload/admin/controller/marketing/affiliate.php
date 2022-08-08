9465bced5db44313460bcffc332e22693d9c56f9
Daniel Kerr <webmaster@opencart.com>
2022-08-09 02:37:42 +0800

    Update affiliate.php

--- upload/admin/controller/marketing/affiliate.php
+++ upload/admin/controller/marketing/affiliate.php
@@ -739,4 +739,52 @@ class Affiliate extends \Opencart\System\Engine\Controller {
 
 		return $this->load->view('marketing/affiliate_report', $data);
 	}
+
+	public function autocomplete(): void {
+		$json = [];
+
+		if (isset($this->request->get['filter_name']) || isset($this->request->get['filter_email'])) {
+			if (isset($this->request->get['filter_name'])) {
+				$filter_name = $this->request->get['filter_name'];
+			} else {
+				$filter_name = '';
+			}
+
+			if (isset($this->request->get['filter_email'])) {
+				$filter_email = $this->request->get['filter_email'];
+			} else {
+				$filter_email = '';
+			}
+
+			$filter_data = [
+				'filter_name'      => $filter_name,
+				'filter_email'     => $filter_email,
+				'start'            => 0,
+				'limit'            => 5
+			];
+
+			$this->load->model('marketing/affiliate');
+
+			$results = $this->model_marketing_affiliate->getAffiliates($filter_data);
+
+			foreach ($results as $result) {
+				$json[] = [
+					'customer_id' => $result['customer_id'],
+					'name'        => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+					'email'       => $result['email']
+				];
+			}
+		}
+
+		$sort_order = [];
+
+		foreach ($json as $key => $value) {
+			$sort_order[$key] = $value['name'];
+		}
+
+		array_multisort($sort_order, SORT_ASC, $json);
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
 }

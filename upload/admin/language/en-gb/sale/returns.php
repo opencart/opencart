7eb915189b17d2f6ec37fd38a3162704ca8ebb76
pemaier <pemaier@users.noreply.github.com>
2022-08-22 13:08:18 +0300

    Update returns.php
    From
    $_['error_return']         = 'Warning could not find selected return!';
    To
    $_['error_return']         = 'Warning: Could not find selected return!';

--- upload/admin/language/en-gb/sale/returns.php
+++ upload/admin/language/en-gb/sale/returns.php
@@ -55,7 +55,7 @@ $_['help_product']         = '(Autocomplete)';
 // Error
 $_['error_warning']        = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']     = 'Warning: You do not have permission to modify returns!';
-$_['error_return']         = 'Warning could not find selected return!';
+$_['error_return']         = 'Warning: Could not find selected return!';
 $_['error_order_id']       = 'Order ID required!';
 $_['error_firstname']      = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';

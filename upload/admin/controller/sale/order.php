8c9390233c3c75b5edd529f030a5bd7828eae087
Pham Toan <phamtoan1904@gmail.com>
2022-08-30 11:35:56 +0700

    Update order.php
    fixed bug load order history when seeing order from sale/order with page > 1  
    it likes admin/index.php?route=sale/order|info&user_token=1bef9c62ea988977358cdf135665fc02&order_id=14&page=3
    this condition will set page only from Ajax load => route == 'sale/order|history'

--- upload/admin/controller/sale/order.php
+++ upload/admin/controller/sale/order.php
@@ -1721,7 +1721,7 @@ class Order extends \Opencart\System\Engine\Controller {
 			$order_id = 0;
 		}
 
-		if (isset($this->request->get['page'])) {
+		if (isset($this->request->get['page']) && $this->request->get['route'] == 'sale/order|history') {
 			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;

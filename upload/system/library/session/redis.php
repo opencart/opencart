66e83ec5892193ccb53298396267155d7b2d65d4
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-09-25 17:57:44 -0400

    Added comment text on methods

--- upload/system/library/session/redis.php
+++ upload/system/library/session/redis.php
@@ -1,7 +1,11 @@
 <?php
 namespace Opencart\System\Library\Session;
-class Redis
-{
+class Redis {
+	/**
+	 * Construct
+	 *
+	 * @param    object  $registry
+	 */
 	public function __construct(\Opencart\System\Engine\Registry $registry)	{
 		$this->config = $registry->get('config');
 
@@ -14,6 +18,13 @@ class Redis
 		}
 	}
 
+	/**
+	 * Read
+	 *
+	 * @param    string  $session_id
+	 *
+	 * @return	 array
+	 */
 	public function read(string $session_id): array	{
 		$data = $this->redis->get($this->prefix . $session_id);
 		if (is_null($data) || empty($data))
@@ -21,6 +32,14 @@ class Redis
 		return json_decode($data, true);
 	}
 
+	/**
+	 * Write
+	 *
+	 * @param    string  $session_id
+	 * @param    array   $data
+	 *
+	 * @return	 bool
+	 */
 	public function write(string $session_id, array $data): bool {
 		if ($session_id) {
 			$this->redis->set($this->prefix . $session_id, $data ? json_encode($data) : '', $this->config->get('session_expire'));
@@ -28,13 +47,25 @@ class Redis
 
 		return true;
 	}
-
+	
+	/**
+	 * Destroy
+	 *
+	 * @param    string  $session_id
+	 *
+	 * @return	 bool
+	 */
 	public function destroy(string $session_id): bool {
 		$this->redis->unlink($this->prefix . $session_id);
 
 		return true;
 	}
 
+	/**
+	 * GC
+	 *
+	 * @return	 bool
+	 */
 	public function gc(): bool {
 		// Redis will take care of Garbage Collection itself.
 

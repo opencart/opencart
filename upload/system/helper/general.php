2c995537ac82eb8d35a10a9f32fa0177d52938d8
Daniel Kerr <webmaster@opencart.com>
2022-10-28 03:04:14 +0800

    added new functions too replace the issue of ut8_ and Opencart\System\Helper\Utf8 being to long also repetitive file manipulation tasks.

--- upload/system/helper/general.php
+++ upload/system/helper/general.php
@@ -1,31 +1,144 @@
 <?php
-namespace Opencart\System\Helper\General;
-function token(int $length = 32): string {
-	return substr(bin2hex(random_bytes($length)), 0, $length);
+// String
+function oc_strlen(string $string) {
+	return mb_strlen($string);
 }
 
-/**
- * Backwards support for timing safe hash string comparisons
- *
- * http://php.net/manual/en/function.hash-equals.php
- */
+function oc_strpos(string $string, string $needle, int $offset = 0) {
+	return mb_strpos($string, $needle, $offset);
+}
 
-if (!function_exists('hash_equals')) {
-	function hash_equals(string $known_string, string $user_string) {
-		$known_string = $known_string;
-		$user_string = $user_string;
+function oc_strrpos(string $string, string $needle, int $offset = 0) {
+	return mb_strrpos($string, $needle, $offset);
+}
 
-		if (strlen($known_string) != strlen($user_string)) {
-			return false;
-		} else {
-			$res = $known_string ^ $user_string;
-			$ret = 0;
+function oc_substr(string $string, int $offset, ?int $length = null) {
+	return mb_substr($string, $offset, $length);
+}
 
-			for ($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
+function oc_strtoupper(string $string) {
+	return mb_strtoupper($string);
+}
 
-			return !$ret;
+function oc_strtolower(string $string) {
+	return mb_strtolower($string);
+}
+
+// File
+function oc_file_move(string $file, string $destination): bool {
+	if (!is_file($file)) {
+		return false;
+	}
+
+	if (is_file($destination)) {
+		unlink($destination);
+	}
+
+	return rename($file, $destination);
+}
+
+function oc_file_copy(string $file, string $destination): bool {
+	if (!is_file($file)) {
+		return false;
+	}
+
+	if (is_file($destination)) {
+		unlink($destination);
+	}
+
+	return copy($file, $destination);
+}
+
+function oc_file_delete(string $file): bool {
+	if (!is_file($file)) {
+		return false;
+	}
+
+	return unlink($file);
+}
+
+// Directory
+function oc_directory_move(string $directory, string $destination): bool {
+
+
+
+	return true;
+}
+
+function oc_directory_copy(string $directory, string $destination): bool {
+	if (!is_dir($directory)) {
+		return false;
+	}
+
+	if (is_dir($destination)) {
+		return false;
+	}
+
+	$files = [];
+
+	// Make path into an array
+	$directory = [$directory];
+
+	// While the path array is still populated keep looping through
+	while (count($directory) != 0) {
+		$next = array_shift($directory);
+
+		if (is_dir($next)) {
+			foreach (glob(trim($next, '/') . '/{*,.[!.]*,..?*}', GLOB_BRACE) as $delete) {
+				// If directory add to path array
+				$directory[] = $delete;
+			}
+		}
+
+		// Add the file to the files to be deleted array
+		$files[] = $next;
+	}
+
+	return true;
+}
+
+function oc_directory_delete(string $directory): bool {
+	$files = [];
+
+	// Make path into an array
+	$path = [$directory];
+
+	// While the path array is still populated keep looping through
+	while (count($path) != 0) {
+		$next = array_shift($path);
+
+		if (is_dir($next)) {
+			foreach (glob(trim($next, '/') . '/{*,.[!.]*,..?*}', GLOB_BRACE) as $delete) {
+				// If directory add to path array
+				$path[] = $delete;
+			}
+		}
+
+		// Add the file to the files to be deleted array
+		$files[] = $next;
+	}
+
+	// Reverse sort the file array
+	rsort($files);
+
+	foreach ($files as $delete) {
+		// If file just delete
+		if (is_file($delete)) {
+			unlink($delete);
+		}
+
+		// If directory use the remove directory function
+		if (is_dir($delete)) {
+			rmdir($delete);
 		}
 	}
+
+	return true;
+}
+
+// Other
+function oc_token(int $length = 32): string {
+	return substr(bin2hex(random_bytes($length)), 0, $length);
 }
 
 function date_added(string $date): array {

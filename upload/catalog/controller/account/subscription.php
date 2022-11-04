fb62838f9ec31337db0ef527bd22d51263e44a72
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-11-03 22:26:34 -0400

    Fixed subscription_id with order_id in parameter

--- upload/catalog/controller/account/subscription.php
+++ upload/catalog/controller/account/subscription.php
@@ -147,7 +147,7 @@ class Subscription extends \Opencart\System\Engine\Controller {
 			// Orders
             $this->load->model('account/order');
 
-            $order_product = $this->model_account_order->getOrderProduct($this->request->get['subscription_id'], $subscription_info['order_product_id']);
+            $order_product = $this->model_account_order->getOrderProduct($subscription_info['order_id'], $subscription_info['order_product_id']);
 
             $data['order_id'] = $subscription_info['order_id'];
             $data['reference'] = $subscription_info['reference'];

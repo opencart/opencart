470fb942ba0fccfdfcc658fc8f6ba6f822ce77a9
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-11-05 13:57:55 -0400

    Realigned line of codes

--- upload/catalog/language/en-gb/cron/subscription.php
+++ upload/catalog/language/en-gb/cron/subscription.php
@@ -1,6 +1,6 @@
 <?php
 // Error
-$_['error_extension']       = 'Warning: Payment method extension could not be found!';
-$_['error_recurring']       = 'Warning: Payment method does not have recurring payment method!';
-$_['error_payment']         = 'Warning: Payment method %s could not be found!';
-$_['error_price']           = 'Error: A subscription price must be set!';
+$_['error_extension'] = 'Warning: Payment method extension could not be found!';
+$_['error_recurring'] = 'Warning: Payment method does not have recurring payment method!';
+$_['error_payment']   = 'Warning: Payment method %s could not be found!';
+$_['error_price']     = 'Error: A subscription price must be set!';

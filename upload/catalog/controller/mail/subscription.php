58c49502dd75f079b9ddbc0e010035d17c1f1dac
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-11-11 15:00:55 -0500

    Renamed getSettingValue to getValue model method

--- upload/catalog/controller/mail/subscription.php
+++ upload/catalog/controller/mail/subscription.php
@@ -257,7 +257,7 @@ class Subscription extends \Opencart\System\Engine\Controller {
                                                 $subscription_status_id = $this->{'model_extension_payment_' . $payment_info['code']}->promotion($value['subscription_id']);
 
                                                 if ($store_info) {
-                                                    $config_subscription_active_status_id = $this->model_setting_setting->getSettingValue('config_subscription_active_status_id', $store_info['store_id']);
+                                                    $config_subscription_active_status_id = $this->model_setting_setting->getValue('config_subscription_active_status_id', $store_info['store_id']);
                                                 } else {
                                                     $config_subscription_active_status_id = $this->config->get('config_subscription_active_status_id');
                                                 }

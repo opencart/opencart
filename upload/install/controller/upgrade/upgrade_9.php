edfa67e99fb8f049f40644a17a0277e798c6b1af
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-11-15 11:23:22 -0500

    Added filter_var lookup

--- upload/install/controller/upgrade/upgrade_9.php
+++ upload/install/controller/upgrade/upgrade_9.php
@@ -747,6 +747,12 @@ class Upgrade9 extends \Opencart\System\Engine\Controller {
                 foreach ($seo_urls as $seo_url) {
                     $value = preg_replace('/[^a-zA-Z0-9_|\/\.]/', '', str_replace('|', '.', $seo_url['value']));
 
+                    if (!filter_var($value, FILTER_VALIDATE_INT)) {
+                        $value = $this->db->escape($value);
+                    } else {
+                        $value = (int)$value;
+                    }
+
                     $this->db->query("INSERT INTO `" . DB_PREFIX . "seo_url` SET `store_id` = '" . (int)$seo_url['store_id'] . "', `language_id` = '" . (int)$seo_url['language_id'] . "', `key` = '" . $this->db->escape($seo_url['key']) . "', `value` = '" . $value . "', `keyword` = '" . $this->db->escape($seo_url['keyword']) . "', `sort_order` = '" . (int)$seo_url['sort_order'] . "'");
                 }
             } catch (\ErrorException $exception) {

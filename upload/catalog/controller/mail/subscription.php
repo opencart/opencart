c7f4d967e116246ce48e9260c5f07f7cf5c0dd93
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-11-20 16:27:47 -0500

    Added trial status for the emails

--- upload/catalog/controller/mail/subscription.php
+++ upload/catalog/controller/mail/subscription.php
@@ -363,7 +363,7 @@ class Subscription extends \Opencart\System\Engine\Controller {
                                         // and not based on promotional products, only subscribed
                                         // customers can receive the emails; either by automation
                                         // or on-demand.
-                                        if ($value['status'] && $subscription['status']) {
+                                        if (($value['trial_status'] && $subscription['trial_status']) || ($value['status'] && $subscription['status'])) {
                                             // Mail
                                             if ($this->config->get('config_mail_engine')) {
                                                 $mail = new \Opencart\System\Library\Mail($this->config->get('config_mail_engine'));

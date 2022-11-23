37a28950de4ad4e686d05a7e900e61f97e798d1e
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-11-23 13:57:37 -0500

    Renamed LCASE to LOWER

--- upload/catalog/model/catalog/information.php
+++ upload/catalog/model/catalog/information.php
@@ -8,7 +8,7 @@ class Information extends \Opencart\System\Engine\Model {
 	}
 
 	public function getInformations(): array {
-		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "information` i LEFT JOIN `" . DB_PREFIX . "information_description` id ON (i.`information_id` = id.`information_id`) LEFT JOIN `" . DB_PREFIX . "information_to_store` i2s ON (i.`information_id` = i2s.`information_id`) WHERE id.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND i2s.`store_id` = '" . (int)$this->config->get('config_store_id') . "' AND i.`status` = '1' ORDER BY i.`sort_order`, LCASE(id.`title`) ASC");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "information` i LEFT JOIN `" . DB_PREFIX . "information_description` id ON (i.`information_id` = id.`information_id`) LEFT JOIN `" . DB_PREFIX . "information_to_store` i2s ON (i.`information_id` = i2s.`information_id`) WHERE id.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND i2s.`store_id` = '" . (int)$this->config->get('config_store_id') . "' AND i.`status` = '1' ORDER BY i.`sort_order`, LOWER(id.`title`) ASC");
 
 		return $query->rows;
 	}

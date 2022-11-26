ca63e63e4285928c330cf8f624bf71b73edebf8e
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-11-26 14:37:06 -0500

    Added text_canceled language definition

--- upload/catalog/language/en-gb/mail/subscription_alert.php
+++ upload/catalog/language/en-gb/mail/subscription_alert.php
@@ -7,3 +7,4 @@ $_['text_subscription_id']      = 'Subscription ID';
 $_['text_date_added']           = 'Date Added:';
 $_['text_subscription_status']  = 'Subscription Status:';
 $_['text_comment']              = 'The comments for your subscription are:';
+$_['text_canceled']             = 'Success: The Subscription profile has been canceled!';

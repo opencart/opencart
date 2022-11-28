bf4a1d84ef31d67301927a619e784523b66d3344
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-11-28 11:28:21 -0500

    Reverted this change

--- upload/catalog/language/en-gb/account/subscription.php
+++ upload/catalog/language/en-gb/account/subscription.php
@@ -47,5 +47,5 @@ $_['column_amount']                        = 'Amount';
 $_['button_return']                        = 'Return';
 
 // Error
-$_['error_not_canceled']                   = 'Error: %s';
+$_['error_not_cancelled']                  = 'Error: %s';
 $_['error_not_found']                      = 'Could not cancel subscription';

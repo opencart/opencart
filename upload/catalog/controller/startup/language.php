5e1401c18060cb7efe6f2e4322317d44df09dc23
Daniel Kerr <webmaster@opencart.com>
2022-11-29 00:37:37 +0800

    https://github.com/opencart/opencart/issues/11633

--- upload/catalog/controller/startup/language.php
+++ upload/catalog/controller/startup/language.php
@@ -1,7 +1,7 @@
 <?php
 namespace Opencart\Catalog\Controller\Startup;
 class Language extends \Opencart\System\Engine\Controller {
-	public static $extension = '';
+	private static $extension = '';
 
 	public function index(): void {
 		if (isset($this->request->get['language'])) {
@@ -14,40 +14,53 @@ class Language extends \Opencart\System\Engine\Controller {
 
 		$language_info = $this->model_localisation_language->getLanguageByCode($code);
 
-		if (!$language_info) {
-			// If no language can be found, we use the default one
-			$language_info = $this->model_localisation_language->getLanguageByCode($this->config->get('config_language'));
-		}
-
 		if ($language_info) {
-			// 1. initialize new language class
-			$language = new \Opencart\System\Library\Language($this->config->get('config_language'));
-
-			// 2. if extension switch to default extension language directory
-			if (!$language_info['extension']) {
-				$language->addPath(DIR_LANGUAGE);
-			} else {
+			// If extension switch add language directory
+			if ($language_info['extension']) {
 				self::$extension = $language_info['extension'];
 
-				$language->addPath(DIR_LANGUAGE);
-				$language->addPath('extension/' . $language_info['extension'], DIR_EXTENSION . $language_info['extension'] . '/catalog/language/');
+				$this->language->addPath('extension/' . $language_info['extension'], DIR_EXTENSION . $language_info['extension'] . '/catalog/language/');
 			}
 
 			// Set the config language_id key
 			$this->config->set('config_language_id', $language_info['language_id']);
 			$this->config->set('config_language', $language_info['code']);
 
-			$this->registry->set('language', $language);
-
 			$this->load->language('default');
+		} else {
+			$url_data = $this->request->get;
+
+			if (isset($url_data['route'])) {
+				$route = $url_data['route'];
+			} else {
+				$route = $this->config->get('action_default');
+			}
+
+			unset($url_data['route']);
+			unset($url_data['language']);
+
+			$url = '';
+
+			if ($url_data) {
+				$url .= '&' . urldecode(http_build_query($url_data));
+			}
+
+			// If no language can be found, we use the default one
+			$this->response->redirect($this->url->link($route, 'language=' . $this->config->get('config_language') . $url, true));
 		}
 	}
 	
 	// Fill the language up with default values
 	public function after(&$route, &$prefix, &$code, &$output): void {
+		if ($code) {
+			$language = $code;
+		} else {
+			$language = $this->config->get('config_language');
+		}
+
 		// Use language->load so it's not triggering infinite loops
-		if (oc_substr($route, 0, 10) != 'extension/' && self::$extension) {
-			$this->load->language('extension/' . self::$extension . '/' . $route, $prefix, $this->config->get('config_language'));
+		if (oc_substr($route, 0, 10) != 'extension/') {
+			$this->load->language('extension/' . self::$extension . '/' . $route, $prefix, $language);
 		}
 	}
 }

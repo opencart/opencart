769a3bdf5418a5a40d2d81a2dc1cc030b2c60a9f
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2022-12-03 21:16:50 -0500

    Renamed language->load to load->language; comment

--- upload/catalog/controller/startup/language.php
+++ upload/catalog/controller/startup/language.php
@@ -58,7 +58,7 @@ class Language extends \Opencart\System\Engine\Controller {
 			$language = $this->config->get('config_language');
 		}
 
-		// Use language->load so it's not triggering infinite loops
+		// Use >load->language so it's not triggering infinite loops
 		if (oc_substr($route, 0, 10) != 'extension/' && self::$extension) {
 			$this->load->language('extension/' . self::$extension . '/' . $route, $prefix, $language);
 		}

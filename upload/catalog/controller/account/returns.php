0033286579ccc5b71d62fe5ce8c89ddc8ec51f7a
condor2 <condor20_05@yahoo.it>
2022-12-06 14:38:42 +0200

    Updated returns pagination code like in downlod controller

--- upload/catalog/controller/account/returns.php
+++ upload/catalog/controller/account/returns.php
@@ -41,13 +41,15 @@ class Returns extends \Opencart\System\Engine\Controller {
 			'href' => $this->url->link('account/returns', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token'] . $url)
 		];
 
+		$limit = $this->config->get('config_pagination');
+
 		$data['returns'] = [];
 
 		$this->load->model('account/returns');
 
 		$return_total = $this->model_account_returns->getTotalReturns();
 
-		$results = $this->model_account_returns->getReturns(($page - 1) * 10, 10);
+		$results = $this->model_account_returns->getReturns(($page - 1) * $limit, $limit);
 
 		foreach ($results as $result) {
 			$data['returns'][] = [
@@ -63,11 +65,11 @@ class Returns extends \Opencart\System\Engine\Controller {
 		$data['pagination'] = $this->load->controller('common/pagination', [
 			'total' => $return_total,
 			'page'  => $page,
-			'limit' => $this->config->get('config_pagination'),
+			'limit' => $limit,
 			'url'   => $this->url->link('account/returns', 'language=' . $this->config->get('config_language') . '&page={page}')
 		]);
 
-		$data['results'] = sprintf($this->language->get('text_pagination'), ($return_total) ? (($page - 1) * 10) + 1 : 0, ((($page - 1) * 10) > ($return_total - 10)) ? $return_total : ((($page - 1) * 10) + 10), $return_total, ceil($return_total / 10));
+		$data['results'] = sprintf($this->language->get('text_pagination'), ($return_total) ? (($page - 1) * $limit) + 1 : 0, ((($page - 1) * $limit) > ($return_total - $limit)) ? $return_total : ((($page - 1) * $limit) + $limit), $return_total, ceil($return_total / $limit));
 
 		$data['continue'] = $this->url->link('account/account', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token']);
 

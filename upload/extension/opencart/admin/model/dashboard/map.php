4b2e195ba0e1a15fd1fc49e32ab4ff8d9316d419
opencartbot <customocean@gmail.com>
2023-01-27 12:39:18 +0200

    Fix the admin map when the payment address is disabled
    When the payment address is disabled - we not have 'payment_country_id' an get an error in the admin map:
     TypeError:  strtolower(): Argument #1 ($string) must be of type string, null given, called in /extension/opencart/admin/controller/dashboard/map.php on line 84

--- upload/extension/opencart/admin/model/dashboard/map.php
+++ upload/extension/opencart/admin/model/dashboard/map.php
@@ -11,8 +11,12 @@ class Map extends \Opencart\System\Engine\Model {
 		}
 
 		if ($implode) {
-			$query = $this->db->query("SELECT COUNT(*) AS `total`, SUM(o.`total`) AS `amount`, c.`iso_code_2` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "country` c ON (o.`payment_country_id` = c.`country_id`) WHERE o.`order_status_id` IN(" . implode(',', $implode) . ") GROUP BY o.`payment_country_id`");
-
+			if ($this->config->get('config_checkout_payment_address')) {
+				$query = $this->db->query("SELECT COUNT(*) AS `total`, SUM(o.`total`) AS `amount`, c.`iso_code_2` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "country` c ON (o.`payment_country_id` = c.`country_id`) WHERE o.`order_status_id` IN(" . implode(',', $implode) . ") GROUP BY o.`payment_country_id`");
+			} else {
+				$query = $this->db->query("SELECT COUNT(*) AS `total`, SUM(o.`total`) AS `amount`, c.`iso_code_2` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "country` c ON (o.`shipping_country_id` = c.`country_id`) WHERE o.`order_status_id` IN(" . implode(',', $implode) . ") GROUP BY o.`shipping_country_id`");
+			}
+			
 			return $query->rows;
 		} else {
 			return [];

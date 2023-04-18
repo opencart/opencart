5df4f7f373b7f99016cfeba416956f4fae535bc3
Daniel Kerr <webmaster@opencart.com>
2023-04-19 01:24:52 +0800

    https://github.com/opencart/opencart/issues/12271

--- upload/admin/controller/tool/upgrade.php
+++ upload/admin/controller/tool/upgrade.php
@@ -48,7 +48,7 @@ class Upgrade extends \Opencart\System\Engine\Controller {
 		if ($response_info) {
 			$data['latest_version'] = $response_info['version'];
 			$data['date_added'] = date($this->language->get('date_format_short'), strtotime($response_info['date_added']));
-			$data['log'] = $response_info['log'];
+			$data['log'] = nl2br($response_info['log']);
 
 			if (!version_compare(VERSION, $response_info['version'], '>=')) {
 				$data['upgrade'] = true;

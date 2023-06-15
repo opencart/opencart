aae7cc257947fc2900035a0b03d546127fa44b91
Daniel Kerr <webmaster@opencart.com>
2023-06-16 05:38:45 +0800

    https://github.com/opencart/opencart/issues/12311

--- upload/catalog/controller/checkout/voucher.php
+++ upload/catalog/controller/checkout/voucher.php
@@ -126,7 +126,7 @@ class Voucher extends \Opencart\System\Engine\Controller {
 				'from_email'       => $this->request->post['from_email'],
 				'voucher_theme_id' => $this->request->post['voucher_theme_id'],
 				'message'          => $this->request->post['message'],
-				'amount'           => $this->currency->convert($this->request->post['amount'], $this->session->data['currency'], $this->config->get('config_currency'))
+				'amount'           => $this->currency->convert((int)$this->request->post['amount'], $this->session->data['currency'], $this->config->get('config_currency'))
 			];
 
 			unset($this->session->data['shipping_method']);

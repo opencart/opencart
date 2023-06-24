1264d8eba089fb5bca9726f818a4b45ce2f6a88d
Daniel Kerr <webmaster@opencart.com>
2023-06-25 01:41:54 +0800

    https://github.com/opencart/opencart/issues/12377

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -295,35 +295,26 @@ class Product extends \Opencart\System\Engine\Model {
 	}
 
 	public function getRelated(int $product_id): array {
-		$query = $this->db->query("SELECT DISTINCT *, pd.`name` AS name, `p`.`image`, " . $this->statement['discount'] . ", " . $this->statement['special'] . ", " . $this->statement['reward'] . ", " . $this->statement['review'] . " 
-		FROM `" . DB_PREFIX . "product_related` `pr`  
-		LEFT JOIN `" . DB_PREFIX . "product_to_store` `p2s` ON (`p2s`.`product_id` = `pr`.`product_id`) 
-		LEFT JOIN `" . DB_PREFIX . "product` `p` ON (`p`.`product_id` = `pr`.`related_id` AND p.`status` = '1' AND p.`date_available` <= NOW() ) 
-		LEFT JOIN `" . DB_PREFIX . "product_description` `pd` ON (`p`.`product_id` = `pd`.`product_id`) WHERE pr.`product_id` = '" . (int)$product_id . "' AND `pd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND p2s.`store_id` = '" . (int)$this->config->get('config_store_id') . "'");
+		$query = $this->db->query("SELECT DISTINCT *, pd.`name` AS name, `p`.`image`, " . $this->statement['discount'] . ", " . $this->statement['special'] . ", " . $this->statement['reward'] . ", " . $this->statement['review'] . " FROM `" . DB_PREFIX . "product_related` `pr`  LEFT JOIN `" . DB_PREFIX . "product_to_store` `p2s` ON (`p2s`.`product_id` = `pr`.`product_id`) LEFT JOIN `" . DB_PREFIX . "product` `p` ON (`p`.`product_id` = `pr`.`related_id` AND `p`.`status` = '1' AND `p`.`date_available` <= NOW()) LEFT JOIN `" . DB_PREFIX . "product_description` `pd` ON (`p`.`product_id` = `pd`.`product_id`) WHERE `pr`.`product_id` = '" . (int)$product_id . "' AND `pd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND `p2s`.`store_id` = '" . (int)$this->config->get('config_store_id') . "'");
 
 		return $query->rows;
 	}
 
 	public function getLatest(int $limit): array {
-		$sql = "SELECT `p`.`product_id` 
+		$sql = "SELECT DISTINCT *, pd.`name` AS name, `p`.`image`, " . $this->statement['discount'] . ", " . $this->statement['special'] . ", " . $this->statement['reward'] . ", " . $this->statement['review'] . " 
 			FROM  `" . DB_PREFIX . "product_to_store` `p2s`
-			LEFT JOIN `" . DB_PREFIX . "product` `p` ON (`p`.`product_id` = `p2s`.`product_id` AND `p`.`status` = '1' ) 
-			WHERE 
-			AND `p`.`date_available` <= NOW() 
-			AND `p2s`.`store_id` = '" . (int)$this->config->get('config_store_id') . "' 
-			ORDER BY `p`.`date_added`
-DESC LIMIT " . (int)$limit;
+			LEFT JOIN `" . DB_PREFIX . "product` `p` ON (`p`.`product_id` = `p2s`.`product_id` AND `p`.`status` = '1' AND `p`.`date_available` <= NOW()) 
+			LEFT JOIN `" . DB_PREFIX . "product_description` `pd` ON (p.`product_id` = `pd`.`product_id`) 
+			WHERE `p2s`.`store_id` = '" . (int)$this->config->get('config_store_id') . "' AND `pd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' ORDER BY `p`.`date_added` DESC LIMIT " . (int)$limit;
 
 		$product_data = $this->cache->get('product.' . md5($sql));
 
 		if (!$product_data) {
-			$query = $this->db->query();
+			$query = $this->db->query($sql);
 
-			foreach ($query->rows as $result) {
-				$product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
-			}
+			$product_data = $query->rows;
 
-			$this->cache->set('product.latest.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit, $product_data);
+			$this->cache->set('product.' . md5($sql), $product_data);
 		}
 
 		return (array)$product_data;
@@ -331,6 +322,9 @@ DESC LIMIT " . (int)$limit;
 
 	public function getSpecials(array $data = []): array {
 		$sql = "SELECT DISTINCT ps.`product_id`, (SELECT AVG(`rating`) FROM `" . DB_PREFIX . "review` r1 WHERE r1.`product_id` = ps.`product_id` AND r1.`status` = '1' GROUP BY r1.`product_id`) AS rating 
+		
+		
+		
 		
 		FROM `" . DB_PREFIX . "product_special` ps LEFT JOIN `" . DB_PREFIX . "product` p ON (ps.`product_id` = p.`product_id`) 
 		LEFT JOIN `" . DB_PREFIX . "product_description` pd ON (p.`product_id` = pd.`product_id`) 

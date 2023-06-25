838e863db6f5257f86820b4a1f7d7e6a5a7e3942
Daniel Kerr <webmaster@opencart.com>
2023-06-25 23:11:22 +0800

    bestsellers updated

--- upload/extension/opencart/catalog/controller/module/featured.php
+++ upload/extension/opencart/catalog/controller/module/featured.php
@@ -53,7 +53,7 @@ class Featured extends \Opencart\System\Engine\Controller {
 					'product_id'  => $product['product_id'],
 					'thumb'       => $image,
 					'name'        => $product['name'],
-					'description' => oc_substr(strip_tags(html_entity_decode($product['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..',
+					'description' => oc_substr(trim(strip_tags(html_entity_decode($product['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('config_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
 					'tax'         => $tax,

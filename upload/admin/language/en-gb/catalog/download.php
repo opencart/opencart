543d8cb7a303e4fec6cc9c32083f1bf400e42500
ppalashturov <ppalashturov@users.noreply.github.com>
2023-07-06 16:14:31 +0300

    Update download.php

--- upload/admin/language/en-gb/catalog/download.php
+++ upload/admin/language/en-gb/catalog/download.php
@@ -1,7 +1,6 @@
 <?php
 // Heading
 $_['heading_title']            = 'Downloads';
-$_['heading_title']            = 'Downloads';
 
 // Text
 $_['text_success']             = 'Success: You have modified downloads!';

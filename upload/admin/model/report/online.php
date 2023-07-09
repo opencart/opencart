3b1a887ef7429ce7956f57107b6331b8a6f838b3
Daniel Kerr <webmaster@opencart.com>
2023-07-10 07:16:04 +0800

    Update online.php

--- upload/admin/model/report/online.php
+++ upload/admin/model/report/online.php
@@ -2,23 +2,23 @@
 namespace Opencart\Admin\Model\Report;
 class Online extends \Opencart\System\Engine\Model {
 	public function getOnline(array $data = []): array {
-		$sql = "SELECT co.`ip`, co.`customer_id`, co.`url`, co.`referer`, co.`date_added` FROM `" . DB_PREFIX . "customer_online` co LEFT JOIN `" . DB_PREFIX . "customer` c ON (co.`customer_id` = c.`customer_id`)";
+		$sql = "SELECT `co`.`ip`, `co`.`customer_id`, `co`.`url`, `co`.`referer`, `co`.`date_added` FROM `" . DB_PREFIX . "customer_online` `co` LEFT JOIN `" . DB_PREFIX . "customer` `c` ON (`co`.`customer_id` = `c`.`customer_id`)";
 
 		$implode = [];
 
 		if (!empty($data['filter_ip'])) {
-			$implode[] = "co.`ip` LIKE '" . $this->db->escape((string)$data['filter_ip']) . "'";
+			$implode[] = "`co`.`ip` LIKE '" . $this->db->escape((string)$data['filter_ip']) . "'";
 		}
 
 		if (!empty($data['filter_customer'])) {
-			$implode[] = "co.`customer_id` > '0' AND CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE '" . $this->db->escape((string)$data['filter_customer']) . "'";
+			$implode[] = "`co`.`customer_id` > '0' AND CONCAT(`c`.`firstname`, ' ', `c`.`lastname`) LIKE '" . $this->db->escape((string)$data['filter_customer']) . "'";
 		}
 
 		if ($implode) {
 			$sql .= " WHERE " . implode(" AND ", $implode);
 		}
 
-		$sql .= " ORDER BY co.`date_added` DESC";
+		$sql .= " ORDER BY `co`.`date_added` DESC";
 
 		if (isset($data['start']) || isset($data['limit'])) {
 			if ($data['start'] < 0) {
@@ -38,16 +38,16 @@ class Online extends \Opencart\System\Engine\Model {
 	}
 
 	public function getTotalOnline(array $data = []): int {
-		$sql = "SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "customer_online` co LEFT JOIN `" . DB_PREFIX . "customer` c ON (co.`customer_id` = c.`customer_id`)";
+		$sql = "SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "customer_online` `co` LEFT JOIN `" . DB_PREFIX . "customer` `c` ON (`co`.`customer_id` = `c`.`customer_id`)";
 
 		$implode = [];
 
 		if (!empty($data['filter_ip'])) {
-			$implode[] = "co.`ip` LIKE '" . $this->db->escape((string)$data['filter_ip']) . "'";
+			$implode[] = "`co`.`ip` LIKE '" . $this->db->escape((string)$data['filter_ip']) . "'";
 		}
 
 		if (!empty($data['filter_customer'])) {
-			$implode[] = "co.`customer_id` > '0' AND CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE '" . $this->db->escape((string)$data['filter_customer']) . "'";
+			$implode[] = "`co`.`customer_id` > '0' AND CONCAT(`c`.`firstname`, ' ', `c`.`lastname`) LIKE '" . $this->db->escape((string)$data['filter_customer']) . "'";
 		}
 
 		if ($implode) {

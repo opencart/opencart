43c5f6af390d85a14cf948c2267c285c550abd35
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-07-10 20:40:28 -0400

    Updated upgrade/upgrade_9 controller with new order and subscription fields

--- upload/install/controller/upgrade/upgrade_9.php
+++ upload/install/controller/upgrade/upgrade_9.php
@@ -67,6 +67,139 @@ class Upgrade9 extends \Opencart\System\Engine\Controller {
 					}
 				}
 			}
+
+			// Order Subscription
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "order' AND COLUMN_NAME = 'subscription_id'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD COLUMN `subscription_id` int(11) NOT NULL AFTER `order_id`");
+			}
+
+			// Order Transaction
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "order' AND COLUMN_NAME = 'transaction_id'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD COLUMN `transaction_id` varchar(100) NOT NULL AFTER `invoice_prefix`");
+			}
+
+			// Order Payment Address ID
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "order' AND COLUMN_NAME = 'payment_address_id'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD COLUMN `payment_address_id` int(11) NOT NULL AFTER `custom_field`");
+			}
+
+			// Order Shipping Address ID
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "order' AND COLUMN_NAME = 'shipping_address_id'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD COLUMN `shipping_address_id` int(11) NOT NULL AFTER `payment_method`");
+			}
+
+			// Order Language Code
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "order' AND COLUMN_NAME = 'language_code'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD COLUMN `language_code` varchar(5) NOT NULL AFTER `language_id`");
+			}
+
+			// Subscription Customer Payment ID
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'customer_payment_id'");
+
+			if ($query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` DROP COLUMN `customer_payment_id`");
+			}
+
+			// Subscription Name
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'name'");
+
+			if ($query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` DROP COLUMN `name`");
+			}
+
+			// Subscription Description
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'description'");
+
+			if ($query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` DROP COLUMN `description`");
+			}
+
+			// Subscription Reference
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'reference'");
+
+			if ($query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` DROP COLUMN `reference`");
+			}
+
+			// Subscription Comment
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'comment'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `comment` text NOT NULL AFTER `date_next`");
+			}
+
+			// Subscription Affiliate ID
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'affiliate_id'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `affiliate_id` int(11) NOT NULL AFTER `subscription_status_id`");
+			}
+
+			// Subscription Marketing ID
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'marketing_id'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `marketing_id` int(11) NOT NULL AFTER `affiliate_id`");
+			}
+
+			// Subscription Tracking
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'tracking'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `tracking` varchar(64) NOT NULL AFTER `marketing_id`");
+			}
+
+			// Subscription Language ID
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'language_id'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `language_id` int(11) NOT NULL AFTER `tracking`");
+			}
+
+			// Subscription Currency ID
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'currency_id'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `currency_id` int(11) NOT NULL AFTER `language_id`");
+			}
+
+			// Subscription IP
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'ip'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `ip` varchar(40) NOT NULL AFTER `ip`");
+			}
+
+			// Subscription Forwarded IP
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'forwarded_ip'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `forwarded_ip` varchar(40) NOT NULL AFTER `ip`");
+			}
+
+			// Subscription User Agent
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'user_agent'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `user_agent` varchar(255) NOT NULL AFTER `forwarded_ip`");
+			}
+
+			// Subscription Accept Language
+			$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "subscription' AND COLUMN_NAME = 'accept_language'");
+
+			if (!$query->num_rows) {
+				$this->db->query("ALTER TABLE `" . DB_PREFIX . "subscription` ADD COLUMN `accept_language` varchar(255) NOT NULL AFTER `user_agent`");
+			}
 		} catch (\ErrorException $exception) {
 			$json['error'] = sprintf($this->language->get('error_exception'), $exception->getCode(), $exception->getMessage(), $exception->getFile(), $exception->getLine());
 		}

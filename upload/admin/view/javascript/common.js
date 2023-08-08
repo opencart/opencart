0885ce2b0d02260bd62701a6f0219bb6b650d841
Daniel Kerr <webmaster@opencart.com>
2023-08-08 15:24:37 +0100

    updated ajax form so can work with buttons

--- upload/admin/view/javascript/common.js
+++ upload/admin/view/javascript/common.js
@@ -162,31 +162,8 @@ $(document).ready(function () {
 });
 
 // Forms
-$(document).on('submit', 'form[data-oc-toggle=\'ajax\']', function (e) {
-    e.preventDefault();
-
-    console.log(e);
-
+$(document).on('submit', 'form', function (e) {
     var element = this;
-
-   // return true;
-
-    // Form attributes
-    var form = e.target;
-
-    var action = $(form).attr('action');
-
-    var method = $(form).attr('method');
-
-    if (method === undefined) {
-        method = 'post';
-    }
-
-    var enctype = $(form).attr('enctype');
-
-    if (enctype === undefined) {
-        enctype = 'application/x-www-form-urlencoded';
-    }
 
     if (e.originalEvent !== undefined && e.originalEvent.submitter !== undefined) {
         var button = e.originalEvent.submitter;
@@ -194,104 +171,140 @@ $(document).on('submit', 'form[data-oc-toggle=\'ajax\']', function (e) {
         var button = '';
     }
 
-    // Form button overrides
-    var formaction = $(button).attr('formaction');
+    var status = false;
 
-    if (formaction !== undefined) {
-        action = formaction;
+    var ajax = $(element).attr('data-oc-toggle');
+
+    if (ajax == 'ajax') {
+        status = true;
     }
 
-    var formmethod = $(button).attr('formmethod');
+    var ajax = $(button).attr('data-oc-toggle');
 
-    if (formmethod !== undefined) {
-        method = formmethod;
+    if (ajax == 'ajax') {
+        status = true;
     }
 
-    var formenctype = $(button).attr('formenctype');
+    if (status) {
+        e.preventDefault();
 
-    if (formenctype !== undefined) {
-        enctype = formenctype;
-    }
+        // Form attributes
+        var form = e.target;
 
-    if (button) {
-        var formaction = $(button).attr('data-type');
-    }
+        var action = $(form).attr('action');
 
-    console.log('element ' + element);
-    console.log('action ' + action);
-    console.log('button ' + button);
-    console.log('formaction ' + formaction);
-    console.log('method ' + method);
-    console.log('enctype ' + enctype);
-    console.log($(element).serialize());
+        var method = $(form).attr('method');
 
-    // https://github.com/opencart/opencart/issues/9690
-    if (typeof CKEDITOR != 'undefined') {
-        for (instance in CKEDITOR.instances) {
-            CKEDITOR.instances[instance].updateElement();
+        if (method === undefined) {
+            method = 'post';
         }
-    }
 
-    $.ajax({
-        url: action.replaceAll('&amp;', '&'),
-        type: method,
-        data: $(form).serialize(),
-        dataType: 'json',
-        contentType: enctype,
-        beforeSend: function () {
-            $(button).button('loading');
-        },
-        complete: function () {
-            $(button).button('reset');
-        },
-        success: function (json, textStatus) {
-            console.log(json);
-            console.log(textStatus);
+        var enctype = $(form).attr('enctype');
 
-            $('.alert-dismissible').remove();
-            $(element).find('.is-invalid').removeClass('is-invalid');
-            $(element).find('.invalid-feedback').removeClass('d-block');
-
-            if (json['redirect']) {
-                location = json['redirect'];
-            }
-
-            if (typeof json['error'] == 'string') {
-                $('#alert').prepend('<div class="alert alert-danger alert-dismissible"><i class="fa-solid fa-circle-exclamation"></i> ' + json['error'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
-            }
-
-            if (typeof json['error'] == 'object') {
-                if (json['error']['warning']) {
-                    $('#alert').prepend('<div class="alert alert-danger alert-dismissible"><i class="fa-solid fa-circle-exclamation"></i> ' + json['error']['warning'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
-                }
-
-                for (key in json['error']) {
-                    $('#input-' + key.replaceAll('_', '-')).addClass('is-invalid').find('.form-control, .form-select, .form-check-input, .form-check-label').addClass('is-invalid');
-                    $('#error-' + key.replaceAll('_', '-')).html(json['error'][key]).addClass('d-block');
-                }
-            }
-
-            if (json['success']) {
-                $('#alert').prepend('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json['success'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
-
-                // Refresh
-                var url = $(form).attr('data-oc-load');
-                var target = $(form).attr('data-oc-target');
-
-                if (url !== undefined && target !== undefined) {
-                    $(target).load(url);
-                }
-            }
-
-            // Replace any form values that correspond to form names.
-            for (key in json) {
-                $(element).find('[name=\'' + key + '\']').val(json[key]);
-            }
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+        if (enctype === undefined) {
+            enctype = 'application/x-www-form-urlencoded';
         }
-    });
+
+        // Form button overrides
+        var formaction = $(button).attr('formaction');
+
+        if (formaction !== undefined) {
+            action = formaction;
+        }
+
+        var formmethod = $(button).attr('formmethod');
+
+        if (formmethod !== undefined) {
+            method = formmethod;
+        }
+
+        var formenctype = $(button).attr('formenctype');
+
+        if (formenctype !== undefined) {
+            enctype = formenctype;
+        }
+
+        if (button) {
+            var formaction = $(button).attr('data-type');
+        }
+
+        console.log(e);
+        console.log('element ' + element);
+        console.log('action ' + action);
+        console.log('button ' + button);
+        console.log('formaction ' + formaction);
+        console.log('method ' + method);
+        console.log('enctype ' + enctype);
+        console.log($(element).serialize());
+
+        // https://github.com/opencart/opencart/issues/9690
+        if (typeof CKEDITOR != 'undefined') {
+            for (instance in CKEDITOR.instances) {
+                CKEDITOR.instances[instance].updateElement();
+            }
+        }
+
+        $.ajax({
+            url: action.replaceAll('&amp;', '&'),
+            type: method,
+            data: $(form).serialize(),
+            dataType: 'json',
+            contentType: enctype,
+            beforeSend: function () {
+                $(button).button('loading');
+            },
+            complete: function () {
+                $(button).button('reset');
+            },
+            success: function (json, textStatus) {
+                console.log(json);
+                console.log(textStatus);
+
+                $('.alert-dismissible').remove();
+                $(element).find('.is-invalid').removeClass('is-invalid');
+                $(element).find('.invalid-feedback').removeClass('d-block');
+
+                if (json['redirect']) {
+                    location = json['redirect'];
+                }
+
+                if (typeof json['error'] == 'string') {
+                    $('#alert').prepend('<div class="alert alert-danger alert-dismissible"><i class="fa-solid fa-circle-exclamation"></i> ' + json['error'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+                }
+
+                if (typeof json['error'] == 'object') {
+                    if (json['error']['warning']) {
+                        $('#alert').prepend('<div class="alert alert-danger alert-dismissible"><i class="fa-solid fa-circle-exclamation"></i> ' + json['error']['warning'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+                    }
+
+                    for (key in json['error']) {
+                        $('#input-' + key.replaceAll('_', '-')).addClass('is-invalid').find('.form-control, .form-select, .form-check-input, .form-check-label').addClass('is-invalid');
+                        $('#error-' + key.replaceAll('_', '-')).html(json['error'][key]).addClass('d-block');
+                    }
+                }
+
+                if (json['success']) {
+                    $('#alert').prepend('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json['success'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+
+                    // Refresh
+                    var url = $(form).attr('data-oc-load');
+                    var target = $(form).attr('data-oc-target');
+
+                    if (url !== undefined && target !== undefined) {
+                        $(target).load(url);
+                    }
+                }
+
+                // Replace any form values that correspond to form names.
+                for (key in json) {
+                    $(element).find('[name=\'' + key + '\']').val(json[key]);
+                }
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+            }
+        });
+    }
 });
 
 // Upload

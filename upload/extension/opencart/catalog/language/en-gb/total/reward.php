3d643e28d88be3487912e92f67f09a5ed300e766
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-08-08 21:35:21 -0400

    Edited text in extension/opencart/reward

--- upload/extension/opencart/catalog/language/en-gb/total/reward.php
+++ upload/extension/opencart/catalog/language/en-gb/total/reward.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title'] = 'Use Reward Points (Available %s)';
+$_['heading_title'] = 'Use Reward Points (Available: %s)';
 
 // Text
-$_['text_reward']   = 'Reward Points (%s)';
+$_['text_reward']   = 'Reward Points: (%s)';
 $_['text_order_id'] = 'Order ID: #%s';
 $_['text_success']  = 'Success: Your reward points discount has been applied!';
 $_['text_remove']   = 'Success: Your reward points discount has been removed!';
 
 // Entry
-$_['entry_reward']  = 'Points to use (Max %s)';
+$_['entry_reward']  = 'Points to use (Max: %s)';
 
 // Error
 $_['error_reward']  = 'Warning: Please enter the amount of reward points to use!';

59fd0c71d75384d50266aaf55d42788bab01d3cd
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-08-08 21:40:23 -0400

    Aligned = in system/config/admin

--- upload/system/config/admin.php
+++ upload/system/config/admin.php
@@ -3,23 +3,23 @@
 $_['site_url']          = HTTP_SERVER;
 
 // Database
-$_['db_autostart']      = true;
-$_['db_engine']         = DB_DRIVER; // mysqli, pdo or pgsql
-$_['db_hostname']       = DB_HOSTNAME;
-$_['db_username']       = DB_USERNAME;
-$_['db_password']       = DB_PASSWORD;
-$_['db_database']       = DB_DATABASE;
-$_['db_port']           = DB_PORT;
+$_['db_autostart']       = true;
+$_['db_engine']          = DB_DRIVER; // mysqli, pdo or pgsql
+$_['db_hostname']        = DB_HOSTNAME;
+$_['db_username']        = DB_USERNAME;
+$_['db_password']        = DB_PASSWORD;
+$_['db_database']        = DB_DATABASE;
+$_['db_port']            = DB_PORT;
 
 // Session
-$_['session_autostart'] = false;
-$_['session_engine']    = 'db'; // db or file
+$_['session_autostart']  = false;
+$_['session_engine']     = 'db'; // db or file
 
 // Error
-$_['error_display']     = true;
+$_['error_display']      = true;
 
 // Actions
-$_['action_pre_action'] = [
+$_['action_pre_action']  = [
 	'startup/setting',
 	'startup/session',
 	'startup/language',

c1c11bb947cac705eda3abf73552babbce817ee3
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-08-08 21:00:00 -0400

    Modified error texts

--- upload/catalog/language/en-gb/api/sale/customer.php
+++ upload/catalog/language/en-gb/api/sale/customer.php
@@ -3,11 +3,11 @@
 $_['text_success']         = 'Success: You have successfully modified customers!';
 
 // Error
-$_['error_customer']       = 'Warning: Customer could not be found!';
-$_['error_customer_group'] = 'Customer Group does not appear to be valid!';
-$_['error_firstname']      = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']          = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']      = 'Telephone must be between 3 and 32 characters!';
-$_['error_custom_field']   = '%s required!';
-$_['error_regex']          = '%s is not a valid input!';
+$_['error_customer']       = 'Error: Customer could not be found!';
+$_['error_customer_group'] = 'Error: Customer Group does not appear to be valid!';
+$_['error_firstname']      = 'Error: First Name must be between 1 and 32 characters!';
+$_['error_lastname']       = 'Error: Last Name must be between 1 and 32 characters!';
+$_['error_email']          = 'Error: E-Mail Address does not appear to be valid!';
+$_['error_telephone']      = 'Error: Telephone must be between 3 and 32 characters!';
+$_['error_custom_field']   = 'Error: %s required!';
+$_['error_regex']          = 'Error: %s is not a valid input!';

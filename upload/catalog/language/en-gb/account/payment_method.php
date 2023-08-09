a8d509681a9ae8d06f4c3abbb29d099ddde44e1b
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-08-08 20:50:13 -0400

    Edited text_success on account/payment_method

--- upload/catalog/language/en-gb/account/payment_method.php
+++ upload/catalog/language/en-gb/account/payment_method.php
@@ -5,7 +5,7 @@ $_['heading_title']         = 'Payment Methods';
 // Text
 $_['text_account']          = 'Account';
 $_['text_payment_method']   = 'Payment Method Entries';
-$_['text_success']          = 'Your payment method has been successfully deleted';
+$_['text_success']          = 'Success: Your payment method has been successfully deleted.';
 $_['text_no_results']       = 'You have no payment methods in your account.';
 
 // Column

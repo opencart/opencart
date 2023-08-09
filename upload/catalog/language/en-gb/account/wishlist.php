62bb4c898f03a126405ec146de38acf2286a884f
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-08-09 13:56:25 -0400

    Replaced with exclamation point

--- upload/catalog/language/en-gb/account/wishlist.php
+++ upload/catalog/language/en-gb/account/wishlist.php
@@ -8,7 +8,7 @@ $_['text_instock']    = 'In Stock';
 $_['text_wishlist']   = 'Wish List (%s)';
 $_['text_login']      = 'You must <a href="%s">login</a> or <a href="%s">create an account</a> to save <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
 $_['text_success']    = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
-$_['text_remove']     = 'Success: You have removed an item from your wishlist.';
+$_['text_remove']     = 'Success: You have removed an item from your wishlist!';
 $_['text_no_results'] = 'Your wish list is empty.';
 
 // Column

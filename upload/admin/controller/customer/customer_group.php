40b04beaee07849c1023aa1f9402e98a0d12e9ae
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-08-11 08:42:13 -0400

    Testing commit in customer/customer_group

--- upload/admin/controller/customer/customer_group.php
+++ upload/admin/controller/customer/customer_group.php
@@ -137,8 +137,8 @@ class CustomerGroup extends \Opencart\System\Engine\Controller {
 			$url .= '&page=' . $this->request->get['page'];
 		}
 
-		$data['sort_name'] = $this->url->link('customer/customer_group.list', 'user_token=' . $this->session->data['user_token'] . '&sort=cgd.name' . $url);
 		$data['sort_sort_order'] = $this->url->link('customer/customer_group.list', 'user_token=' . $this->session->data['user_token'] . '&sort=cg.sort_order' . $url);
+		$data['sort_name'] = $this->url->link('customer/customer_group.list', 'user_token=' . $this->session->data['user_token'] . '&sort=cgd.name' . $url);
 
 		$url = '';
 

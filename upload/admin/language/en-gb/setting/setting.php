03965038c7ee7cf7913bec7995c8cac34dd57d22
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-08-11 09:11:02 -0400

    Trimmed semi-colon in setting/setting

--- upload/admin/language/en-gb/setting/setting.php
+++ upload/admin/language/en-gb/setting/setting.php
@@ -39,7 +39,7 @@ $_['text_return']                         = 'Return';
 $_['text_captcha']                        = 'Captcha';
 $_['text_hour']                           = 'Hour(s)';
 $_['text_none']                           = 'None';
-$_['text_lax']                            = 'Lax' ;
+$_['text_lax']                            = 'Lax';
 $_['text_strict']                         = 'Strict';
 
 // Entry

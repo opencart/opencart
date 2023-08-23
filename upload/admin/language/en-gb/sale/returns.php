26452f7bcfde103ac571de90618df639ee915798
Daniel Kerr <webmaster@opencart.com>
2023-08-23 11:22:42 +0800

    https://github.com/opencart/opencart/issues/12508

--- upload/admin/language/en-gb/sale/returns.php
+++ upload/admin/language/en-gb/sale/returns.php
@@ -61,5 +61,5 @@ $_['error_firstname']      = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';
 $_['error_email']          = 'E-Mail Address does not appear to be valid!';
 $_['error_telephone']      = 'Telephone must be between 3 and 32 characters!';
-$_['error_product']        = 'Product Name must be greater than 3 and less than 255 characters!';
-$_['error_model']          = 'Product Model must be greater than 3 and less than 64 characters!';
+$_['error_product']        = 'Product Name must be greater than 1 and less than 255 characters!';
+$_['error_model']          = 'Product Model must be greater than 1 and less than 64 characters!';

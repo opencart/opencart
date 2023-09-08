104c88e8139f7684f50cc2f5f3144dd048fd97b7
Daniel Kerr <webmaster@opencart.com>
2023-09-09 04:11:05 +0800

    https://github.com/opencart/opencart/pull/12507

---
+++ upload/admin/language/en-gb/cms/antispam.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']           = 'Anti-Spam';
+
+// Text
+$_['text_success']            = 'Success: You have modified anti-spam!';
+$_['text_list']               = 'Anti-Spam List';
+$_['text_add']                = 'Add Anti-Spam';
+$_['text_edit']               = 'Edit Anti-Spam';
+
+// Column
+$_['column_keyword']          = 'Keyword';
+$_['column_action']           = 'Action';
+
+// Entry
+$_['entry_keyword']           = 'Keyword';
+
+// Error
+$_['error_warning']           = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']        = 'Warning: You do not have permission to modify articles!';
+$_['error_keyword']           = 'SEO URL must be between 1 and 64 characters!';
+$_['error_keyword_exists']    = 'SEO URL must be unique!';
+$_['error_keyword_character'] = 'Keyword can only use characters a-z, 0-9, - and _!';

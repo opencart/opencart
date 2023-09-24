f88ab8d6478dcd2a2186320b436d5729ef1e2e4b
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-09-24 19:52:39 -0400

    Added int cast on page

--- upload/admin/controller/cms/comment.php
+++ upload/admin/controller/cms/comment.php
@@ -82,7 +82,7 @@ class Comment extends \Opencart\System\Engine\Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}

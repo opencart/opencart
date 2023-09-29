716850d74dbdeb98d14384ab6e9d155bd89361f8
Daniel Kerr <webmaster@opencart.com>
2023-09-29 10:29:11 +0800

    https://github.com/opencart/opencart/pull/12740

--- upload/catalog/controller/cms/blog.php
+++ upload/catalog/controller/cms/blog.php
@@ -240,6 +240,8 @@ class Blog extends \Opencart\System\Engine\Controller {
 			];
 		}
 
+		$data['language'] = $this->config->get('config_language');
+
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['column_right'] = $this->load->controller('common/column_right');
 		$data['content_top'] = $this->load->controller('common/content_top');

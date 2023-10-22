124d4ab82b430f4bbe8d680e49290e261c4eb806
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-10-22 09:14:30 -0400

    Update customer_activity.php

--- upload/extension/opencart/admin/controller/report/customer_activity.php
+++ upload/extension/opencart/admin/controller/report/customer_activity.php
@@ -124,6 +124,8 @@ class CustomerActivity extends \Opencart\System\Engine\Controller {
 			$page = 1;
 		}
 
+		$limit = 20;
+
 		$data['activities'] = [];
 
 		$filter_data = [
@@ -131,8 +133,8 @@ class CustomerActivity extends \Opencart\System\Engine\Controller {
 			'filter_ip'         => $filter_ip,
 			'filter_date_start'	=> $filter_date_start,
 			'filter_date_end'	=> $filter_date_end,
-			'start'             => ($page - 1) * 20,
-			'limit'             => 20
+			'start'             => ($page - 1) * $limit,
+			'limit'             => $limit
 		];
 
 		$this->load->model('extension/opencart/report/customer');
@@ -182,11 +184,11 @@ class CustomerActivity extends \Opencart\System\Engine\Controller {
 		$data['pagination'] = $this->load->controller('common/pagination', [
 			'total' => $activity_total,
 			'page'  => $page,
-			'limit' => $this->config->get('config_pagination'),
+			'limit' => $limit,
 			'url'   => $this->url->link('extension/opencart/report/customer_activity.report', 'user_token=' . $this->session->data['user_token'] . '&code=customer_activity' . $url . '&page={page}')
 		]);
 
-		$data['results'] = sprintf($this->language->get('text_pagination'), ($activity_total) ? (($page - 1) * $this->config->get('config_pagination')) + 1 : 0, ((($page - 1) * $this->config->get('config_pagination')) > ($activity_total - $this->config->get('config_pagination'))) ? $activity_total : ((($page - 1) * $this->config->get('config_pagination')) + $this->config->get('config_pagination')), $activity_total, ceil($activity_total / $this->config->get('config_pagination')));
+		$data['results'] = sprintf($this->language->get('text_pagination'), ($activity_total) ? (($page - 1) * $limit) + 1 : 0, ((($page - 1) * $limit) > ($activity_total - $limit)) ? $activity_total : ((($page - 1) * $limit) + $limit), $activity_total, ceil($activity_total / $limit));
 
 		$data['filter_customer'] = $filter_customer;
 		$data['filter_ip'] = $filter_ip;

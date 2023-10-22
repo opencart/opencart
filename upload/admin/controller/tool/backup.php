f29b8792826d810c84c9ea8c4bc565952a7ce0aa
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-10-22 09:00:03 -0400

    Update backup.php

--- upload/admin/controller/tool/backup.php
+++ upload/admin/controller/tool/backup.php
@@ -146,6 +146,8 @@ class Backup extends \Opencart\System\Engine\Controller {
 			$page = 1;
 		}
 
+		$limit = 200;
+
 		if (!$this->user->hasPermission('modify', 'tool/backup')) {
 			$json['error'] = $this->language->get('error_permission');
 		}
@@ -165,7 +167,7 @@ class Backup extends \Opencart\System\Engine\Controller {
 				$output .= 'TRUNCATE TABLE `' . $this->db->escape($table) . '`;' . "\n\n";
 			}
 
-			$results = $this->model_tool_backup->getRecords($table, ($page - 1) * 200, 200);
+			$results = $this->model_tool_backup->getRecords($table, ($page - 1) * $limit, $limit);
 
 			foreach ($results as $result) {
 				$fields = '';
@@ -199,7 +201,7 @@ class Backup extends \Opencart\System\Engine\Controller {
 
 			$record_total = $this->model_tool_backup->getTotalRecords($table);
 
-			if (($page * 200) >= $record_total) {
+			if (($page * $limit) >= $record_total) {
 				$output .= "\n";
 
 				if (isset($backup[$position + 1])) {
@@ -223,12 +225,12 @@ class Backup extends \Opencart\System\Engine\Controller {
 
 			if (!$table) {
 				$json['success'] = $this->language->get('text_success');
-			} elseif (($page * 200) >= $record_total) {
-				$json['text'] = sprintf($this->language->get('text_backup'), $table, ($page - 1) * 200, $record_total);
+			} elseif (($page * $limit) >= $record_total) {
+				$json['text'] = sprintf($this->language->get('text_backup'), $table, ($page - 1) * $limit, $record_total);
 
 				$json['next'] = $this->url->link('tool/backup.backup', 'user_token=' . $this->session->data['user_token'] . '&filename=' . urlencode($filename) . '&table=' . $table . '&page=1', true);
 			} else {
-				$json['text'] = sprintf($this->language->get('text_backup'), $table, ($page - 1) * 200, $page * 200);
+				$json['text'] = sprintf($this->language->get('text_backup'), $table, ($page - 1) * $limit, $page * $limit);
 
 				$json['next'] = $this->url->link('tool/backup.backup', 'user_token=' . $this->session->data['user_token'] . '&filename=' . urlencode($filename) . '&table=' . $table . '&page=' . ($page + 1), true);
 			}

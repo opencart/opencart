4d521d01fbd02eb318daf7ccc0490541c159b909
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-10-22 09:17:32 -0400

    Update customer.php

--- upload/extension/opencart/admin/controller/report/customer.php
+++ upload/extension/opencart/admin/controller/report/customer.php
@@ -140,15 +140,17 @@ class Customer extends \Opencart\System\Engine\Controller {
 			$page = 1;
 		}
 
+		$limit = 20;
+
 		$data['customers'] = [];
 
 		$filter_data = [
-			'filter_date_start'	=> $filter_date_start,
-			'filter_date_end'	=> $filter_date_end,
-			'filter_group'           => $filter_group,
-			'start'             => ($page - 1) * 20,
-			'limit'             => 20
-		];
+            'filter_date_start' => $filter_date_start,
+            'filter_date_end'   => $filter_date_end,
+            'filter_group'      => $filter_group,
+            'start'             => ($page - 1) * $limit,
+            'limit'             => $limit
+        ];
 
 		$this->load->model('extension/opencart/report/customer');
 
@@ -181,11 +183,11 @@ class Customer extends \Opencart\System\Engine\Controller {
 		$data['pagination'] = $this->load->controller('common/pagination', [
 			'total' => $customer_total,
 			'page'  => $page,
-			'limit' => $this->config->get('config_pagination'),
+			'limit' => $limit,
 			'url'   => $this->url->link('extension/opencart/report/customer.report', 'user_token=' . $this->session->data['user_token'] . '&code=customer' . $url . '&page={page}')
 		]);
 
-		$data['results'] = sprintf($this->language->get('text_pagination'), ($customer_total) ? (($page - 1) * $this->config->get('config_pagination')) + 1 : 0, ((($page - 1) * $this->config->get('config_pagination')) > ($customer_total - $this->config->get('config_pagination'))) ? $customer_total : ((($page - 1) * $this->config->get('config_pagination')) + $this->config->get('config_pagination')), $customer_total, ceil($customer_total / $this->config->get('config_pagination')));
+		$data['results'] = sprintf($this->language->get('text_pagination'), ($customer_total) ? (($page - 1) * $limit) + 1 : 0, ((($page - 1) * $limit) > ($customer_total - $limit)) ? $customer_total : ((($page - 1) * $limit) + $limit), $customer_total, ceil($customer_total / $limit));
 
 		$data['filter_date_start'] = $filter_date_start;
 		$data['filter_date_end'] = $filter_date_end;

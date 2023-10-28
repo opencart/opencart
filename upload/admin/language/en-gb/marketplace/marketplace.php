538787f1a4c90f19196b46303632427a59a9e407
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-10-28 08:44:15 -0400

    Update marketplace.php

--- upload/admin/language/en-gb/marketplace/marketplace.php
+++ upload/admin/language/en-gb/marketplace/marketplace.php
@@ -62,7 +62,7 @@ $_['tab_documentation']  = 'Documentation';
 $_['tab_download']       = 'Download';
 $_['tab_comment']        = 'Comment';
 
-// Button
+// Buttons
 $_['button_api']         = 'Marketplace API';
 $_['button_purchase']    = 'Purchase';
 $_['button_view_all']    = 'View all extensions';

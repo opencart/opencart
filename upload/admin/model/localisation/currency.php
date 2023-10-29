febdedd4074ff1eebf166f11663b7ecd2bf1e054
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-10-29 15:12:46 -0400

    Update currency.php

--- upload/admin/model/localisation/currency.php
+++ upload/admin/model/localisation/currency.php
@@ -115,7 +115,7 @@ class Currency extends \Opencart\System\Engine\Model {
 			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
 		}
 
-		$results = (array)$this->cache->get('currency.' . md5($sql));
+		$results = $this->cache->get('currency.' . md5($sql));
 
 		if (!$results) {
 			$query = $this->db->query($sql);

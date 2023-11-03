8660b0d7a9ddf36b414d7e12749865f37bb83706
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-11-02 20:07:54 -0400

    Update customer_transaction.php

--- upload/extension/opencart/admin/model/report/customer_transaction.php
+++ upload/extension/opencart/admin/model/report/customer_transaction.php
@@ -12,7 +12,7 @@ class CustomerTransaction extends \Opencart\System\Engine\Model {
 	 * @return array
 	 */
 	public function getTransactions(array $data = []): array {
-		$sql = "SELECT ct.`customer_id`, CONCAT(c.`firstname`, ' ', c.`lastname`) AS customer, c.`email`, cgd.`name` AS customer_group, c.`status`, SUM(ct.`amount`) AS `total` FROM `" . DB_PREFIX . "customer_transaction` ct LEFT JOIN `" . DB_PREFIX . "customer` c ON (ct.`customer_id` = c.`customer_id`) LEFT JOIN `" . DB_PREFIX . "customer_group_description` cgd ON (c.`customer_group_id` = cgd.`customer_group_id`) WHERE cgd.`language_id` = '" . (int)$this->config->get('config_language_id') . "'";
+		$sql = "SELECT ct.`customer_id`, CONCAT(`c`.`firstname`, ' ', `c`.`lastname`) AS customer, `c`.`email`, cgd.`name` AS customer_group, `c`.`status`, SUM(ct.`amount`) AS `total` FROM `" . DB_PREFIX . "customer_transaction` ct LEFT JOIN `" . DB_PREFIX . "customer` `c` ON (ct.`customer_id` = `c`.`customer_id`) LEFT JOIN `" . DB_PREFIX . "customer_group_description` cgd ON (`c`.`customer_group_id` = cgd.`customer_group_id`) WHERE cgd.`language_id` = '" . (int)$this->config->get('config_language_id') . "'";
 
 		if (!empty($data['filter_date_start'])) {
 			$sql .= " AND DATE(ct.`date_added`) >= DATE('" . $this->db->escape((string)$data['filter_date_start']) . "')";
@@ -23,7 +23,7 @@ class CustomerTransaction extends \Opencart\System\Engine\Model {
 		}
 
 		if (!empty($data['filter_customer'])) {
-			$sql .= " AND CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE '" . $this->db->escape((string)$data['filter_customer']) . "'";
+			$sql .= " AND CONCAT(`c`.`firstname`, ' ', `c`.`lastname`) LIKE '" . $this->db->escape((string)$data['filter_customer']) . "'";
 		}
 
 		$sql .= " GROUP BY ct.`customer_id` ORDER BY total DESC";
@@ -51,7 +51,7 @@ class CustomerTransaction extends \Opencart\System\Engine\Model {
 	 * @return int
 	 */
 	public function getTotalTransactions(array $data = []): int {
-		$sql = "SELECT COUNT(DISTINCT ct.`customer_id`) AS `total` FROM `" . DB_PREFIX . "customer_transaction` ct LEFT JOIN `" . DB_PREFIX . "customer` c ON (ct.`customer_id` = c.`customer_id`)";
+		$sql = "SELECT COUNT(DISTINCT ct.`customer_id`) AS `total` FROM `" . DB_PREFIX . "customer_transaction` ct LEFT JOIN `" . DB_PREFIX . "customer` `c` ON (ct.`customer_id` = `c`.`customer_id`)";
 
 		$implode = [];
 
@@ -64,7 +64,7 @@ class CustomerTransaction extends \Opencart\System\Engine\Model {
 		}
 
 		if (!empty($data['filter_customer'])) {
-			$implode[] = "CONCAT(c.`firstname`, ' ', c.`lastname`) LIKE '" . $this->db->escape((string)$data['filter_customer']) . "'";
+			$implode[] = "CONCAT(`c`.`firstname`, ' ', `c`.`lastname`) LIKE '" . $this->db->escape((string)$data['filter_customer']) . "'";
 		}
 
 		if ($implode) {

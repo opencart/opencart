6677f0ebd4aedbcc4c0e1b78b1a9f39284ab9cec
pemaier <pemaier@users.noreply.github.com>
2023-11-05 12:12:43 +0300

    Update topic.php - added getTotalTopics

--- upload/catalog/model/cms/topic.php
+++ upload/catalog/model/cms/topic.php
@@ -45,4 +45,15 @@ class Topic extends \Opencart\System\Engine\Model {
 
 		return $topic_data;
 	}
+	
+	/**
+	 * @return int
+	 */
+	public function getTotalTopics(): int {
+		$sql = "SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "topic` `t` LEFT JOIN `" . DB_PREFIX . "topic_description` `td` ON (`t`.`topic_id` = `td`.`topic_id`) LEFT JOIN `" . DB_PREFIX . "topic_to_store` `t2s` ON (`t`.`topic_id` = `t2s`.`topic_id`) WHERE `td`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND `t2s`.`store_id` = '" . (int)$this->config->get('config_store_id') . "'";
+
+		$query = $this->db->query($sql);
+
+		return (int)$query->row['total'];
+	}
 }

4f65a4f546574d3eb6a39096e5acc114b2e51f84
softmonke studio <134335160+softmonke@users.noreply.github.com>
2023-11-07 14:28:01 +0800

    Update reward.php

--- upload/extension/opencart/catalog/controller/total/reward.php
+++ upload/extension/opencart/catalog/controller/total/reward.php
@@ -30,6 +30,7 @@ class Reward extends \Opencart\System\Engine\Controller {
 
 				$data['save'] = $this->url->link('extension/opencart/total/reward.save', 'language=' . $this->config->get('config_language'), true);
 				$data['list'] = $this->url->link('checkout/cart.list', 'language=' . $this->config->get('config_language'), true);
+				$data['header_cart'] = $this->url->link('common/cart.info', 'language=' . $this->config->get('config_language'), true);
 
 				if (isset($this->session->data['reward'])) {
 					$data['reward'] = $this->session->data['reward'];

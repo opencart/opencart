57442423ebd3072917f7d50590649d8f6e6a0ebd
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-11-11 14:02:28 -0500

    Update comment.php

--- upload/catalog/language/en-gb/cms/comment.php
+++ upload/catalog/language/en-gb/cms/comment.php
@@ -5,6 +5,7 @@ $_['heading_title']   = 'Blog';
 // Text
 $_['text_refine']     = 'Refine Search';
 $_['text_all']        = 'All';
+$_['text_more']       = 'see more replies...';
 $_['text_blog']       = 'Blog';
 $_['text_by']         = 'By';
 $_['text_tags']       = 'Tags:';
@@ -13,6 +14,9 @@ $_['text_write']      = 'Write a comment';
 $_['text_login']      = 'Please <a href="%s">login</a> or <a href="%s">register</a> to comment';
 $_['text_no_results'] = 'Be the first to write a comment for this article.';
 $_['text_queue']      = 'Your comment has been added to our moderation queue!';
+$_['text_reply']      = 'Reply';
+$_['text_refresh']    = 'refresh';
+$_['text_remove']     = 'Remove';
 $_['text_success']    = 'Thank you for your comment. It has been submitted to the webmaster for approval.';
 
 // Entry

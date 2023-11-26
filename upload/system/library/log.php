bd9ed3dacbbb260a472321b8da202c2140f36f65
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-11-26 11:23:55 -0500

    Update log.php

--- upload/system/library/log.php
+++ upload/system/library/log.php
@@ -17,10 +17,10 @@ class Log {
 	private string $file;
 
 	/**
-	 * Constructor
-	 *
-	 * @param	string	$filename
- 	*/
+     * Constructor
+     *
+     * @param string $filename
+     */
 	public function __construct(string $filename) {
 		$this->file = DIR_LOGS . $filename;
 	}
@@ -28,9 +28,9 @@ class Log {
 	/**
      * Write
      *
-     * @param	string	$message
-	 *
-	 * @return  void
+     * @param string $message
+     *
+     * @return void
      */
 	public function write(string $message): void {
 		file_put_contents($this->file, date('Y-m-d H:i:s') . ' - ' . print_r($message, true) . "\n", FILE_APPEND);

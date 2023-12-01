646a21c00be16a17134339e9ef16bedee80b5c9d
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-11-30 22:23:31 -0500

    Update address.php

--- upload/admin/controller/customer/address.php
+++ upload/admin/controller/customer/address.php
@@ -264,7 +264,7 @@ class Address extends \Opencart\System\Engine\Controller {
 			$custom_fields = $this->model_customer_custom_field->getCustomFields($filter_data);
 
 			foreach ($custom_fields as $custom_field) {
-				if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['custom_field_id']])) {
+				if (empty($this->request->post['custom_field'][$custom_field['custom_field_id']]) && $custom_field['required']) {
 					$json['error']['address_custom_field_' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
 				} elseif (($custom_field['type'] == 'text') && !empty($custom_field['validation']) && !preg_match(html_entity_decode($custom_field['validation'], ENT_QUOTES, 'UTF-8'), $this->request->post['custom_field'][$custom_field['custom_field_id']])) {
 					$json['error']['address_custom_field_' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_regex'), $custom_field['name']);

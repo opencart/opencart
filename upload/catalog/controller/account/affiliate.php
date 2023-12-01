4eef4e1921438abceabc183259fda14242ff7431
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-11-30 22:25:59 -0500

    Update affiliate.php

--- upload/catalog/controller/account/affiliate.php
+++ upload/catalog/controller/account/affiliate.php
@@ -223,7 +223,7 @@ class Affiliate extends \Opencart\System\Engine\Controller {
 
 			foreach ($custom_fields as $custom_field) {
 				if ($custom_field['location'] == 'affiliate') {
-					if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['custom_field_id']])) {
+					if (empty($this->request->post['custom_field'][$custom_field['custom_field_id']]) && $custom_field['required']) {
 						$json['error']['custom_field_' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
 					} elseif (($custom_field['type'] == 'text') && !empty($custom_field['validation']) && !preg_match(html_entity_decode($custom_field['validation'], ENT_QUOTES, 'UTF-8'), $this->request->post['custom_field'][$custom_field['custom_field_id']])) {
 						$json['error']['custom_field_' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_regex'), $custom_field['name']);

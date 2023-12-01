075a0c6899c184e71e0284fa396c8fd500d393bf
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-11-30 22:53:23 -0500

    Update gdpr.php

--- upload/catalog/controller/information/gdpr.php
+++ upload/catalog/controller/information/gdpr.php
@@ -127,7 +127,6 @@ class Gdpr extends \Opencart\System\Engine\Controller {
 			foreach ($results as $result) {
 				if ($result['action'] == $action) {
 					$status = false;
-
 					break;
 				}
 			}

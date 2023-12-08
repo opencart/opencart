538ae62a576898ec2ab4b3f5e6587a3c7f81f671
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-12-08 08:47:43 -0500

    Update statistics.php

--- upload/admin/controller/event/statistics.php
+++ upload/admin/controller/event/statistics.php
@@ -6,59 +6,60 @@ namespace Opencart\Admin\Controller\Event;
  * @package Opencart\Admin\Controller\Event
  */
 class Statistics extends \Opencart\System\Engine\Controller {
-	// admin/model/catalog/review/addReview/after
 	/**
-	 * @param string $route
-	 * @param array  $args
-	 * @param mixed  $output
-	 *
-	 * @return void
-	 */
+     * admin/model/catalog/review/addReview/after
+     * 
+     * @param string $route
+     * @param array  $args
+     * @param mixed  $output
+     *
+     * @return void
+     */
 	public function addReview(string &$route, array &$args, &$output): void {
 		$this->load->model('report/statistics');
 
 		$this->model_report_statistics->addValue('review', 1);
 	}
 
-	// admin/model/catalog/review/deleteReview/after
-
 	/**
-	 * @param string $route
-	 * @param array  $args
-	 * @param mixed  $output
-	 *
-	 * @return void
-	 */
+     * admin/model/catalog/review/deleteReview/after
+     *
+     * @param string $route
+     * @param array  $args
+     * @param mixed  $output
+     *
+     * @return void
+     */
 	public function deleteReview(string &$route, array &$args, &$output): void {
 		$this->load->model('report/statistics');
 
 		$this->model_report_statistics->removeValue('review', 1);
 	}
 
-	// admin/model/sale/returns/addReturn/after
-
 	/**
-	 * @param string $route
-	 * @param array  $args
-	 * @param mixed  $output
-	 *
-	 * @return void
-	 */
+     * admin/model/sale/returns/addReturn/after
+     *
+     * @param string $route
+     * @param array  $args
+     * @param mixed  $output
+     *
+     * @return void
+     */
 	public function addReturn(string &$route, array &$args, &$output): void {
 		$this->load->model('report/statistics');
 
 		$this->model_report_statistics->addValue('returns', 1);
 	}
 
-	// admin/model/sale/returns/deleteReturn/after
-
 	/**
-	 * @param string $route
-	 * @param array  $args
-	 * @param mixed  $output
-	 *
-	 * @return void
-	 */
+     * admin/model/sale/returns/deleteReturn/after
+     *
+     * @param string $route
+     * @param array  $args
+     * @param mixed  $output
+     *
+     * @return void
+     */
 	public function deleteReturn(string &$route, array &$args, &$output): void {
 		$this->load->model('report/statistics');
 

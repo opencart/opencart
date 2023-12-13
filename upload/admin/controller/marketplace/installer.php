ef065a6b5600a6432829b01b40df8931ad9a31e2
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-12-13 14:28:14 -0500

    Update installer.php

--- upload/admin/controller/marketplace/installer.php
+++ upload/admin/controller/marketplace/installer.php
@@ -26,7 +26,7 @@ class Installer extends \Opencart\System\Engine\Controller {
 			'href' => $this->url->link('marketplace/installer', 'user_token=' . $this->session->data['user_token'])
 		];
 
-		// Use the  for the max file size
+		// Use the configuration option to get the max file size
 		$data['error_upload_size'] = sprintf($this->language->get('error_file_size'), ini_get('upload_max_filesize'));
 
 		$data['config_file_max_size'] = ((int)preg_filter('/[^0-9]/', '', ini_get('upload_max_filesize')) * 1024 * 1024);

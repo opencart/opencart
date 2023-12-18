2f2290741b5e5396c9de93e3f5acb59064f386cf
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2023-12-18 12:26:30 -0500

    Create db_schema.php

---
+++ upload/extension/opencart/admin/language/en-gb/module/db_schema.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']    = 'Database Schema (developers)';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified Database Schema module!';
+$_['text_edit']        = 'Edit Database Schema Module';
+$_['text_report']      = 'Report';
+$_['text_parent']      = 'Parent Table';
+$_['text_child']       = 'Table Relationships';
+$_['text_index']       = 'Indexes';
+
+// Column
+$_['column_table']     = 'Table';
+$_['column_field']     = 'Column';
+$_['column_type']      = 'Type';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Button
+$_['button_report']    = 'Report';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Database Schema module!';

71b005d937b126be616d93c6ffac75e72b52aee3
TheCartpenter <scoutch@gmail.com>
2023-12-19 08:13:52 -0500

    Revert "Update customer.php"
    This reverts commit 8ab6a5625dd4b4acfce48acebd7d732444086ff7.

--- upload/admin/controller/customer/customer.php
+++ upload/admin/controller/customer/customer.php
@@ -696,7 +696,7 @@ class Customer extends \Opencart\System\Engine\Controller {
 		$custom_fields = $this->model_customer_custom_field->getCustomFields($filter_data);
 
 		foreach ($custom_fields as $custom_field) {
-			if (empty($this->request->post['custom_field'][$custom_field['custom_field_id']]) && $custom_field['required']) {
+			if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['custom_field_id']])) {
 				$json['error']['custom_field_' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
 			} elseif ((!empty($custom_field['validation']) && !preg_match(html_entity_decode($custom_field['validation'], ENT_QUOTES, 'UTF-8'), $this->request->post['custom_field'][$custom_field['custom_field_id']]) && $custom_field['type'] == 'text')) {
 				$json['error']['custom_field_' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_regex'), $custom_field['name']);

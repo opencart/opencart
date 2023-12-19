eea69d7fc2655b3e0b146563a7e4dfd39beaaba3
TheCartpenter <scoutch@gmail.com>
2023-12-19 08:15:29 -0500

    Revert "Update payment_address.php"
    This reverts commit 639eed4cad36baec584e375955e88e87cef45690.

--- upload/catalog/controller/api/sale/payment_address.php
+++ upload/catalog/controller/api/sale/payment_address.php
@@ -72,7 +72,7 @@ class PaymentAddress extends \Opencart\System\Engine\Controller {
 
 		foreach ($custom_fields as $custom_field) {
 			if ($custom_field['location'] == 'address') {
-				if (empty($this->request->post['custom_field'][$custom_field['custom_field_id']]) && $custom_field['required']) {
+				if ($custom_field['required'] && empty($this->request->post['custom_field'][$custom_field['custom_field_id']])) {
 					$json['error']['custom_field_' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
 				} elseif ((!empty($custom_field['validation']) && $custom_field['type'] == 'text') && !preg_match(html_entity_decode($custom_field['validation'], ENT_QUOTES, 'UTF-8'), $this->request->post['custom_field'][$custom_field['custom_field_id']])) {
 					$json['error']['custom_field_' . $custom_field['custom_field_id']] = sprintf($this->language->get('error_regex'), $custom_field['name']);

16ab1539c3f80d4b633782396d7cdec7de835771
Anders Jenbo <anders@jenbo.dk>
2023-12-28 22:23:35 +0100

    Update upload/catalog/controller/cron/subscription.php

--- upload/catalog/controller/cron/subscription.php
+++ upload/catalog/controller/cron/subscription.php
@@ -169,6 +169,7 @@ class Subscription extends \Opencart\System\Engine\Controller {
 				if (!$error) {
 					// Subscription
 					$order_data = [];
+
 					$order_data['subscription_id'] = $order_info['subscription_id'];
 
 					// Store Details

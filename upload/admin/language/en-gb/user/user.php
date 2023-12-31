9e4e8d7866155c59ba34603ad62c4a5f0ac8bb95
pemaier <pemaier@users.noreply.github.com>
2023-12-31 22:38:15 +0300

    Update user.php
    Added new text entries

--- upload/admin/language/en-gb/user/user.php
+++ upload/admin/language/en-gb/user/user.php
@@ -12,9 +12,13 @@ $_['text_password']         = 'Password';
 $_['text_other']            = 'Other';
 $_['text_login']            = 'Login History';
 $_['text_authorize']        = 'Authorize History';
+$_['text_filter']           = 'Filter';
 
 // Column
 $_['column_username']       = 'Username';
+$_['column_name']           = 'Name';
+$_['column_email']          = 'E-Mail';
+$_['column_user_group']     = 'User Group';
 $_['column_status']         = 'Status';
 $_['column_ip']             = 'IP';
 $_['column_user_agent']     = 'User Agent';
@@ -31,6 +35,10 @@ $_['entry_lastname']        = 'Last Name';
 $_['entry_email']           = 'E-Mail';
 $_['entry_image']           = 'Image';
 $_['entry_status']          = 'Status';
+$_['entry_name']            = 'Name';
+$_['entry_ip']              = 'IP';
+$_['entry_date_from']       = 'Date From';
+$_['entry_date_to']         = 'Date To';
 
 // Tabs
 $_['tab_authorize']         = 'Authorize';

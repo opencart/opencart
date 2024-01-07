e40376524b9d1e920d5383ecd00fcaf68a5def40
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2024-01-07 13:37:56 -0500

    Update upload/extension/opencart/catalog/model/payment/opayo.php
    Co-authored-by: Anders Jenbo <anders@jenbo.dk>

--- upload/extension/opencart/catalog/model/payment/opayo.php
+++ upload/extension/opencart/catalog/model/payment/opayo.php
@@ -576,7 +576,7 @@ class Opayo extends \Opencart\System\Engine\Model {
 	 *
 	 * @param int   $subscription_id
 	 * @param int   $order_id
-	 * @param array $response_data
+	 * @param array<string, mixed> $response_data
 	 * @param int   $type
 	 *
 	 * @return void

52157c1a91427e1eceedfe8ae5035a93043b3be9
Anders Jenbo <anders@jenbo.dk>
2024-01-08 23:00:53 +0100

    Update upload/system/library/log.php

--- upload/system/library/log.php
+++ upload/system/library/log.php
@@ -34,7 +34,7 @@ class Log {
 	 *
 	 * @return void
 	 */
-	public function write(mixed $message): void {
+	public function write($message): void {
 		file_put_contents($this->file, date('Y-m-d H:i:s') . ' - ' . print_r($message, true) . "\n", FILE_APPEND);
 	}
 }

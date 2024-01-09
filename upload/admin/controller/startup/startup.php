ca951e43fdd9cf1f24d457f28a986dbd86a4ccba
condor2 <condor20_05@yahoo.it>
2024-01-09 11:38:44 +0200

    [3.0.x.x] Updated startup.php
    I think is the correct solution

--- upload/admin/controller/startup/startup.php
+++ upload/admin/controller/startup/startup.php
@@ -64,6 +64,6 @@ class ControllerStartupStartup extends Controller {
 		$this->registry->set('cart', new Cart\Cart($this->registry));
 		
 		// Encryption
-		$this->registry->set('encryption', new Encryption($this->config->get('config_encryption')));
+		$this->registry->set('encryption', new Encryption());
 	}
 }

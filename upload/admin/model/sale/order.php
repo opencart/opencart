6681f39f200414a9beb4a819c240ec9f22a9cd4b
Anders Jenbo <anders@jenbo.dk>
2024-01-09 22:33:18 +0100

    Filter empty result from explode()

--- upload/admin/model/sale/order.php
+++ upload/admin/model/sale/order.php
@@ -565,6 +565,7 @@ class Order extends \Opencart\System\Engine\Model {
 			$implode = [];
 
 			$order_statuses = explode(',', $data['filter_order_status']);
+			$order_statuses = array_filter($order_statuses);
 
 			foreach ($order_statuses as $order_status_id) {
 				$implode[] = "`order_status_id` = '" . (int)$order_status_id . "'";

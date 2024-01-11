e12a1fec54249690af9e45b52e44259d2f9adbfd
ADDCreative <2039130+ADDCreative@users.noreply.github.com>
2024-01-11 15:28:23 +0000

    Add missing type cast for Opayo
    Also use user_token from session.

--- upload/admin/controller/extension/payment/opayo.php
+++ upload/admin/controller/extension/payment/opayo.php
@@ -153,9 +153,9 @@ class ControllerExtensionPaymentOpayo extends Controller {
 
 				$data['auto_settle'] = $opayo_order['settle_type'];
 
-				$data['order_id'] = $this->request->get['order_id'];
+				$data['order_id'] = (int)$this->request->get['order_id'];
 				
-				$data['user_token'] = $this->request->get['user_token'];
+				$data['user_token'] = $this->session->data['user_token'];
 
 				return $this->load->view('extension/payment/opayo/order', $data);
 			}

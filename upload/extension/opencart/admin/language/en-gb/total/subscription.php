6732851099e9bbe27560194898b64f0732ed604e
TheCartpenter <scoutch@gmail.com>
2024-01-23 20:09:13 -0500

    Added admin total extension files

---
+++ upload/extension/opencart/admin/language/en-gb/total/subscription.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'Subscription';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified subscription total!';
+$_['text_edit']        = 'Edit Subscription';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify subscription total!';

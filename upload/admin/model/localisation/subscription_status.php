38069c2a8db088981dd5b0646564a6d37a25a48d
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2024-01-27 11:56:04 -0500

    Update subscription_status.php

--- upload/admin/model/localisation/subscription_status.php
+++ upload/admin/model/localisation/subscription_status.php
@@ -19,6 +19,8 @@ class SubscriptionStatus extends \Opencart\System\Engine\Model {
 		foreach ($data['subscription_status'] as $language_id => $value) {
 			if (isset($subscription_status_id)) {
 				$this->db->query("INSERT INTO `" . DB_PREFIX . "subscription_status` SET `subscription_status_id` = '" . (int)$subscription_status_id . "', `language_id` = '" . (int)$language_id . "', `name` = '" . $this->db->escape($value['name']) . "'");
+
+				$subscription_status_id = $this->db->getLastId();
 			} else {
 				$this->db->query("INSERT INTO `" . DB_PREFIX . "subscription_status` SET `language_id` = '" . (int)$language_id . "', `name` = '" . $this->db->escape($value['name']) . "'");
 

8af859f4de497a493695d493243d024dbdb1350f
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2024-02-06 02:57:36 +0300

    in pagination path double

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -403,7 +403,7 @@ class Category extends \Opencart\System\Engine\Controller {
 				'total' => $product_total,
 				'page'  => $page,
 				'limit' => $limit,
-				'url'   => $this->url->link('product/category', 'language=' . $this->config->get('config_language') . '&path=' . $this->request->get['path'] . $url . '&page={page}')
+				'url'   => $this->url->link('product/category', 'language=' . $this->config->get('config_language') . $url . '&page={page}')
 			]);
 
 			$data['results'] = sprintf($this->language->get('text_pagination'), ($product_total) ? (($page - 1) * $limit) + 1 : 0, ((($page - 1) * $limit) > ($product_total - $limit)) ? $product_total : ((($page - 1) * $limit) + $limit), $product_total, ceil($product_total / $limit));

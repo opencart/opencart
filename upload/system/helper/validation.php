4c33379f276c3fc0760ffba9da3d8096a10e5228
Daniel Kerr <webmaster@opencart.com>
2024-02-10 05:29:08 +0800

    Update validation.php

--- upload/system/helper/validation.php
+++ upload/system/helper/validation.php
@@ -1,4 +1,17 @@
 <?php
-function oc_validate_length($string, $minimum, $maximum) {
+/**
+ * Validate Length
+ *
+ * @param string $string
+ * @param int    $minimum
+ * @param int    $maximum
+ *
+ * @return bool
+ */
+function oc_validate_length(string $string, int $minimum, int $maximum): bool {
 	return (strlen(trim($string)) >= $minimum && strlen(trim($string)) <= $maximum);
+}
+
+function oc_validate_email(string $email): bool {
+	return filter_var($email, FILTER_VALIDATE_EMAIL);
 }

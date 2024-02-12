20e53426e31801ed9e811f52bf0a677645f9a928
mhcwebdesign <github@mhccorp.com>
2024-02-12 15:29:37 +0000

    Opayo and Worldpay fixes
    Minor Opayo payment updates from github/dreamvention , small workarounds for phpstan issues

--- upload/admin/model/extension/payment/opayo.php
+++ upload/admin/model/extension/payment/opayo.php
@@ -247,7 +247,7 @@ class ModelExtensionPaymentOpayo extends Model {
 		$this->db->query("UPDATE `" . DB_PREFIX . "order_recurring` SET `status` = '" . (int)$status . "' WHERE `order_recurring_id` = '" . (int)$order_recurring_id . "'");
 	}
 
-	public function sendCurl($url, $payment_data, $i = null) {
+	public function sendCurl($url, $payment_data) {
 		$curl = curl_init($url);
 
 		curl_setopt($curl, CURLOPT_PORT, 443);

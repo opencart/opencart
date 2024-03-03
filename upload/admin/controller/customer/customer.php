9392b6e3f075d5f072eb164f2e2e09baa942cabe
TheCartpenter <scoutch@gmail.com>
2024-03-03 08:17:21 -0500

    More model conventional name fixes

--- upload/admin/controller/customer/customer.php
+++ upload/admin/controller/customer/customer.php
@@ -795,7 +795,7 @@ class Customer extends \Opencart\System\Engine\Controller {
 			$this->load->model('customer/customer');
 
 			foreach ($selected as $customer_id) {
-				$this->model_customer_customer->deleteCustomer($customer_id);
+				$this->model_customer_customer->deleteCustomers($customer_id);
 			}
 
 			$json['success'] = $this->language->get('text_success');
@@ -1421,7 +1421,7 @@ class Customer extends \Opencart\System\Engine\Controller {
 		if (!$json) {
 			$this->load->model('customer/customer');
 
-			$this->model_customer_customer->deleteAuthorize($authorize_info['customer_id'], $customer_authorize_id);
+			$this->model_customer_customer->deleteAuthorizes($authorize_info['customer_id'], $customer_authorize_id);
 
 			$json['success'] = $this->language->get('text_success');
 		}

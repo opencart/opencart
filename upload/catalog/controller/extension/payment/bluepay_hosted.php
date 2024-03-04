fb661f6c8924eba7249fbe7c5953bcc76ed1f6a6
ADDCreative <2039130+ADDCreative@users.noreply.github.com>
2024-03-04 16:35:19 +0000

    Add BluePay Hosted enabled check

--- upload/catalog/controller/extension/payment/bluepay_hosted.php
+++ upload/catalog/controller/extension/payment/bluepay_hosted.php
@@ -67,7 +67,7 @@ class ControllerExtensionPaymentBluePayHosted extends Controller {
 
 		$response_data = $this->request->get;
 
-		if (isset($this->session->data['order_id'])) {
+		if (isset($this->session->data['order_id']) && $this->config->get('payment_bluepay_hosted_status')) {
 			$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
 			if ($response_data['Result'] == 'APPROVED') {

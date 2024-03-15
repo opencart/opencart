132b22ca5e1b5f25cb8a6ba76ab3af544962e253
Daniel Kerr <webmaster@opencart.com>
2024-03-15 18:45:12 +0800

    Update customer.php

--- upload/catalog/controller/api/sale/customer.php
+++ upload/catalog/controller/api/sale/customer.php
@@ -9,16 +9,10 @@ class Customer extends \Opencart\System\Engine\Controller {
 	/**
 	 * @return void
 	 */
-	public function index(): void {
+	public function index(): string {
 		$this->load->language('api/sale/customer');
 
 		$json = [];
-
-		if ($this->session->data['order_id']) {
-			$order_id = $this->session->data['order_id'];
-		} else {
-			$order_id = 0;
-		}
 
 		$keys = [
 			'customer_id',
@@ -27,7 +21,7 @@ class Customer extends \Opencart\System\Engine\Controller {
 			'lastname',
 			'email',
 			'telephone',
-			'account_custom_field'
+			'custom_field'
 		];
 
 		foreach ($keys as $key) {
@@ -103,19 +97,9 @@ class Customer extends \Opencart\System\Engine\Controller {
 				'custom_field'      => !empty($this->request->post['custom_field']) && is_array($this->request->post['custom_field']) ? $this->request->post['custom_field'] : []
 			];
 
-			if ($order_id) {
-
-				$this->load->model('checkout/order');
-
-				$this->model_checkout_order->editOrder($order_id, $this->session->data['customer']);
-			}
-
 			$json['success'] = $this->language->get('text_success');
-
-			unset($this->session->data['reward']);
 		}
 
-		$this->response->addHeader('Content-Type: application/json');
-		$this->response->setOutput(json_encode($json));
+		return $json;
 	}
 }

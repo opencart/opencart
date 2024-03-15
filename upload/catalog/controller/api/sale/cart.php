518a5e73f85b01e30bc5349daa3c10795cc0432c
Daniel Kerr <webmaster@opencart.com>
2024-03-15 18:45:13 +0800

    Update cart.php

--- upload/catalog/controller/api/sale/cart.php
+++ upload/catalog/controller/api/sale/cart.php
@@ -10,93 +10,7 @@ class Cart extends \Opencart\System\Engine\Controller {
 	 * @return void
 	 */
 	public function index(): void {
-		$this->load->language('api/sale/cart');
 
-		$json = [];
-
-		// Stock
-		if (!$this->cart->hasStock() && (!$this->config->get('config_stock_checkout') || $this->config->get('config_stock_warning'))) {
-			$json['error']['stock'] = $this->language->get('error_stock');
-		}
-
-		$totals = [];
-		$taxes = $this->cart->getTaxes();
-		$total = 0;
-
-		$this->load->model('checkout/cart');
-
-		($this->model_checkout_cart->getTotals)($totals, $taxes, $total);
-
-		$json['products'] = [];
-
-		$products = $this->model_checkout_cart->getProducts();
-
-		foreach ($products as $product) {
-			$description = '';
-
-			if ($product['subscription']) {
-				if ($product['subscription']['trial_status']) {
-					$trial_price = $this->currency->format($this->tax->calculate($product['subscription']['trial_price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-					$trial_cycle = $product['subscription']['trial_cycle'];
-					$trial_frequency = $this->language->get('text_' . $product['subscription']['trial_frequency']);
-					$trial_duration = $product['subscription']['trial_duration'];
-
-					$description .= sprintf($this->language->get('text_subscription_trial'), $trial_price, $trial_cycle, $trial_frequency, $trial_duration);
-				}
-
-				$price = $this->currency->format($this->tax->calculate($product['subscription']['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-				$cycle = $product['subscription']['cycle'];
-				$frequency = $this->language->get('text_' . $product['subscription']['frequency']);
-				$duration = $product['subscription']['duration'];
-
-				if ($duration) {
-					$description .= sprintf($this->language->get('text_subscription_duration'), $price, $cycle, $frequency, $duration);
-				} else {
-					$description .= sprintf($this->language->get('text_subscription_cancel'), $price, $cycle, $frequency);
-				}
-			}
-
-			$json['products'][] = [
-				'cart_id'      => $product['cart_id'],
-				'product_id'   => $product['product_id'],
-				'name'         => $product['name'],
-				'model'        => $product['model'],
-				'option'       => $product['option'],
-				'subscription' => $description,
-				'quantity'     => $product['quantity'],
-				'stock'        => $product['stock'],
-				'minimum'      => $product['minimum'],
-				'reward'       => $product['reward'],
-				'price'        => $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']),
-				'total'        => $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')) * $product['quantity'], $this->session->data['currency'])
-			];
-		}
-
-		$json['vouchers'] = [];
-
-		$vouchers = $this->model_checkout_cart->getVouchers();
-
-		foreach ($vouchers as $key => $voucher) {
-			$json['vouchers'][] = [
-				'key'         => $key,
-				'description' => sprintf($this->language->get('text_for'), $this->currency->format($voucher['amount'], $this->session->data['currency']), $voucher['to_name']),
-				'amount'      => $this->currency->format($voucher['amount'], $this->session->data['currency'])
-			];
-		}
-
-		$json['totals'] = [];
-
-		foreach ($totals as $total) {
-			$json['totals'][] = [
-				'title' => $total['title'],
-				'text'  => $this->currency->format($total['value'], $this->session->data['currency'])
-			];
-		}
-
-		$json['shipping_required'] = $this->cart->hasShipping();
-
-		$this->response->addHeader('Content-Type: application/json');
-		$this->response->setOutput(json_encode($json));
 	}
 
 	/**
@@ -239,6 +153,97 @@ class Cart extends \Opencart\System\Engine\Controller {
 		$json['success'] = $this->language->get('text_success');
 
 		unset($this->session->data['reward']);
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+
+
+	public function getCart() {
+		$this->load->language('api/sale/cart');
+
+		$json = [];
+
+		// Stock
+		if (!$this->cart->hasStock() && (!$this->config->get('config_stock_checkout') || $this->config->get('config_stock_warning'))) {
+			$json['error']['stock'] = $this->language->get('error_stock');
+		}
+
+		$totals = [];
+		$taxes = $this->cart->getTaxes();
+		$total = 0;
+
+		$this->load->model('checkout/cart');
+
+		($this->model_checkout_cart->getTotals)($totals, $taxes, $total);
+
+		$json['products'] = [];
+
+		$products = $this->model_checkout_cart->getProducts();
+
+		foreach ($products as $product) {
+			$description = '';
+
+			if ($product['subscription']) {
+				if ($product['subscription']['trial_status']) {
+					$trial_price = $this->currency->format($this->tax->calculate($product['subscription']['trial_price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+					$trial_cycle = $product['subscription']['trial_cycle'];
+					$trial_frequency = $this->language->get('text_' . $product['subscription']['trial_frequency']);
+					$trial_duration = $product['subscription']['trial_duration'];
+
+					$description .= sprintf($this->language->get('text_subscription_trial'), $trial_price, $trial_cycle, $trial_frequency, $trial_duration);
+				}
+
+				$price = $this->currency->format($this->tax->calculate($product['subscription']['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+				$cycle = $product['subscription']['cycle'];
+				$frequency = $this->language->get('text_' . $product['subscription']['frequency']);
+				$duration = $product['subscription']['duration'];
+
+				if ($duration) {
+					$description .= sprintf($this->language->get('text_subscription_duration'), $price, $cycle, $frequency, $duration);
+				} else {
+					$description .= sprintf($this->language->get('text_subscription_cancel'), $price, $cycle, $frequency);
+				}
+			}
+
+			$json['products'][] = [
+				'cart_id'      => $product['cart_id'],
+				'product_id'   => $product['product_id'],
+				'name'         => $product['name'],
+				'model'        => $product['model'],
+				'option'       => $product['option'],
+				'subscription' => $description,
+				'quantity'     => $product['quantity'],
+				'stock'        => $product['stock'],
+				'minimum'      => $product['minimum'],
+				'reward'       => $product['reward'],
+				'price'        => $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']),
+				'total'        => $this->currency->format($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')) * $product['quantity'], $this->session->data['currency'])
+			];
+		}
+
+		$json['vouchers'] = [];
+
+		$vouchers = $this->model_checkout_cart->getVouchers();
+
+		foreach ($vouchers as $key => $voucher) {
+			$json['vouchers'][] = [
+				'key'         => $key,
+				'description' => sprintf($this->language->get('text_for'), $this->currency->format($voucher['amount'], $this->session->data['currency']), $voucher['to_name']),
+				'amount'      => $this->currency->format($voucher['amount'], $this->session->data['currency'])
+			];
+		}
+
+		$json['totals'] = [];
+
+		foreach ($totals as $total) {
+			$json['totals'][] = [
+				'title' => $total['title'],
+				'text'  => $this->currency->format($total['value'], $this->session->data['currency'])
+			];
+		}
+
+		$json['shipping_required'] = $this->cart->hasShipping();
 
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));

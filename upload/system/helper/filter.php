e4cf095d353fec25279c89d779cf6f2ffa6441af
TheCartpenter <scoutch@gmail.com>
2024-04-27 21:25:22 -0400

    More PHPDocs work

--- upload/system/helper/filter.php
+++ upload/system/helper/filter.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Filter Keyword
+ *
+ * @param string $string
+ *
+ * @return string
+ */
 function oc_filter_keyword(string $string): string {
 	return urlencode(html_entity_decode($string, ENT_QUOTES, 'UTF-8'));
 }

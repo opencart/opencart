403299a351233a10d6c35a38aa2ffa9ad13f90ba
Daniel Kerr <webmaster@opencart.com>
2024-07-23 18:08:10 +0800

    Update step_3.php

--- upload/install/controller/install/step_3.php
+++ upload/install/controller/install/step_3.php
@@ -176,7 +176,7 @@ class Step3 extends \Opencart\System\Engine\Controller {
 			$output .= 'define(\'DB_PASSWORD\', \'' . addslashes(html_entity_decode($this->request->post['db_password'], ENT_QUOTES, 'UTF-8')) . '\');' . "\n";
 			$output .= 'define(\'DB_DATABASE\', \'' . addslashes($this->request->post['db_database']) . '\');' . "\n";
 			$output .= 'define(\'DB_PORT\', \'' . addslashes($this->request->post['db_port']) . '\');' . "\n";
-			$output .= 'define(\'DB_PREFIX\', \'' . addslashes($this->request->post['db_prefix']) . '\');';
+			$output .= 'define(\'DB_PREFIX\', \'' . addslashes($this->request->post['db_prefix']) . '\');' . "\n\n";
 
 			if (!empty($this->request->post['db_ssl_key'])) {
 				$output .= 'define(\'DB_SSL_KEY\', \'' . addslashes($this->request->post['db_ssl_key']) . '\');' . "\n";

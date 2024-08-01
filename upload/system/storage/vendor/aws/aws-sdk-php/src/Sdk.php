b6643e4073b8f1f501e375e2b0124de3860330ca
TheCartpenter <scoutch@gmail.com>
2024-08-01 15:08:28 -0400

    Upgraded aws/aws-sdk-php vendor to v3.317.0

--- upload/system/storage/vendor/aws/aws-sdk-php/src/Sdk.php
+++ upload/system/storage/vendor/aws/aws-sdk-php/src/Sdk.php
@@ -632,6 +632,8 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionSSMContacts(array $args = [])
  * @method \Aws\SSMIncidents\SSMIncidentsClient createSSMIncidents(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionSSMIncidents(array $args = [])
+ * @method \Aws\SSMQuickSetup\SSMQuickSetupClient createSSMQuickSetup(array $args = [])
+ * @method \Aws\MultiRegionClient createMultiRegionSSMQuickSetup(array $args = [])
  * @method \Aws\SSO\SSOClient createSSO(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionSSO(array $args = [])
  * @method \Aws\SSOAdmin\SSOAdminClient createSSOAdmin(array $args = [])
@@ -775,7 +777,7 @@ namespace Aws;
  */
 class Sdk
 {
-    const VERSION = '3.316.10';
+    const VERSION = '3.317.0';
 
     /** @var array Arguments for creating clients */
     private $args;

270a07d04e04b24d4ca67357b71f0588a42fa873
Ekrem KAYA <webmaster@e-piksel.com>
2024-09-05 17:01:55 +0300

    Update smtp.php
    https://github.com/opencart/opencart/issues/14116

--- upload/system/library/mail/smtp.php
+++ upload/system/library/mail/smtp.php
@@ -30,6 +30,7 @@ class Smtp extends \stdClass {
 			$header .= 'Reply-To: =?UTF-8?B?' . base64_encode($this->reply_to) . '?= <' . $this->reply_to . '>' . PHP_EOL;
 		}
 
+		$header .= 'Message-ID: <' . base_convert(str_replace(['.', ' '], '', microtime()), 10, 36) . '.' . base_convert(bin2hex(openssl_random_pseudo_bytes(8)), 16, 36) . substr($this->from, strrpos($this->from, '@')) . '>' . PHP_EOL;
 		$header .= 'Return-Path: ' . $this->from . PHP_EOL;
 		$header .= 'X-Mailer: PHP/' . phpversion() . PHP_EOL;
 		$header .= 'Content-Type: multipart/mixed; boundary="' . $boundary . '"' . PHP_EOL . PHP_EOL;

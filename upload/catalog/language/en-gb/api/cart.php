7677e249201f709f0d513255c7db8f57867b3899
Daniel Kerr <danielkerr@users.noreply.github.com>
2024-10-23 05:16:34 +0800

    https://github.com/opencart/opencart/issues/11458

--- upload/catalog/language/en-gb/api/cart.php
+++ upload/catalog/language/en-gb/api/cart.php
@@ -7,6 +7,10 @@ $_['text_subscription']  = 'Subscription';
 $_['error_product']      = 'Warning: Product could not be found!';
 $_['error_stock']        = 'Products marked with *** are not available in the desired quantity or not in stock!';
 $_['error_minimum']      = 'Minimum order amount for %s is %s!';
+
+
 $_['error_option']       = 'Option could not be found!';
+
+
 $_['error_required']     = '%s required!';
 $_['error_subscription'] = 'Please select a subscription plan!';

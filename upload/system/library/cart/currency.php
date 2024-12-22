5ce785fa14cb623d8cc14f8179edee32e22e5c6a
TheCartpenter <scoutch@gmail.com>
2024-12-22 13:07:54 -0500

    Aligned result code array of library/cart/currency

--- upload/system/library/cart/currency.php
+++ upload/system/library/cart/currency.php
@@ -31,7 +31,14 @@ class Currency {
 		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "currency`");
 
 		foreach ($query->rows as $result) {
-			$this->currencies[$result['code']] = ['currency_id' => $result['currency_id'], 'title' => $result['title'], 'symbol_left' => $result['symbol_left'], 'symbol_right' => $result['symbol_right'], 'decimal_place' => $result['decimal_place'], 'value' => $result['value']];
+			$this->currencies[$result['code']] = [
+				'currency_id'   => $result['currency_id'],
+				'title'         => $result['title'],
+				'symbol_left'   => $result['symbol_left'],
+				'symbol_right'  => $result['symbol_right'],
+				'decimal_place' => $result['decimal_place'],
+				'value'         => $result['value']
+			];
 		}
 	}
 

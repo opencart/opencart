6c5ab0afce5c0b92bfec219d30441bc460370edf
TheCartpenter <scoutch@gmail.com>
2024-12-25 08:25:48 -0500

    Fixed common/menu route

--- upload/catalog/controller/common/menu.php
+++ upload/catalog/controller/common/menu.php
@@ -3,7 +3,7 @@ namespace Opencart\Catalog\Controller\Common;
 /**
  * Class Menu
  * 
- * Can be called from $this->load->controller('common/maintenance');
+ * Can be called from $this->load->controller('common/menu');
  *
  * @package Opencart\Catalog\Controller\Common
  */

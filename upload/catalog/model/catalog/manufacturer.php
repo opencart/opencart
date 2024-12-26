a57723234022819b6fb49a5549c90b2a272710ed
TheCartpenter <scoutch@gmail.com>
2024-12-25 21:10:10 -0500

    Removed backticks on data sort

--- upload/catalog/model/catalog/manufacturer.php
+++ upload/catalog/model/catalog/manufacturer.php
@@ -39,7 +39,7 @@ class Manufacturer extends \Opencart\System\Engine\Model {
 		];
 
 		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-			$sql .= " ORDER BY `" . $data['sort'] . "`";
+			$sql .= " ORDER BY " . $data['sort'];
 		} else {
 			$sql .= " ORDER BY `name`";
 		}
--- upload/catalog/model/cms/article.php
+++ upload/catalog/model/cms/article.php
@@ -264,7 +264,7 @@ class Article extends \Opencart\System\Engine\Model {
 		];
 
 		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-			$sql .= " ORDER BY `" . $data['sort'] . "`";
+			$sql .= " ORDER BY " . $data['sort'];
 		} else {
 			$sql .= " ORDER BY `date_added`";
 		}

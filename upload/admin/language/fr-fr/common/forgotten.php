6e970ff625853bc2f06d5973494f18037a720ff0
pemaier <pemaier@users.noreply.github.com>
2025-01-03 21:44:57 +0300

    Update forgotten.php

--- upload/admin/language/fr-fr/common/forgotten.php
+++ upload/admin/language/fr-fr/common/forgotten.php
@@ -7,7 +7,7 @@ $_['heading_reset'] = 'Réinitialisez votre mot de passe';
 $_['text_email']    = 'Entrez l\'adresse e-mail associée à votre compte. Cliquez sur Soumettre pour recevoir un lien de réinitialisation de mot de passe par e-mail.';
 $_['text_success']  = 'Un e-mail contenant un lien de confirmation a été envoyé à votre adresse e-mail administrateur.';
 $_['text_password'] = 'Entrez le nouveau mot de passe que vous souhaitez utiliser.';
-$_['text_reset']    = 'Succès : Votre mot de passe a été mis à jour avec succès.';
+$_['text_reset']    = 'Succès: Votre mot de passe a été mis à jour avec succès.';
 
 // Entry
 $_['entry_password'] = 'Mot de passe';
@@ -15,7 +15,7 @@ $_['entry_confirm']  = 'Confirmer';
 $_['entry_email']    = 'Adresse E-Mail';
 
 // Error
-$_['error_email']    = 'Attention : L\'adresse e-mail n\'a pas été trouvée dans nos dossiers!';
+$_['error_email']    = 'Attention: L\'adresse e-mail n\'a pas été trouvée dans nos dossiers!';
 $_['error_code']     = 'Le code de réinitialisation ne correspond pas!';
 $_['error_disabled'] = 'La réinitialisation du mot de passe a été désactivée!';
 $_['error_password'] = 'Le mot de passe doit contenir entre 4 et 20 caractères!';

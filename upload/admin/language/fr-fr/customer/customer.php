c1f61add4b1edf08bbf4d83ec5384258bf467da7
pemaier <pemaier@users.noreply.github.com>
2025-01-03 19:44:57 +0300

    Update customer.php

--- upload/admin/language/fr-fr/customer/customer.php
+++ upload/admin/language/fr-fr/customer/customer.php
@@ -3,7 +3,7 @@
 $_['heading_title']         = 'Clients';
 
 // Text
-$_['text_success']          = 'Succès : Vous avez modifié les clients !';
+$_['text_success']          = 'Succès : Vous avez modifié les clients!';
 $_['text_list']             = 'Liste des Clients';
 $_['text_add']              = 'Ajouter un Client';
 $_['text_edit']             = 'Modifier un Client';
@@ -84,21 +84,21 @@ $_['help_safe']             = 'Définir à vrai pour éviter que ce client soit 
 $_['help_points']           = 'Utilisez un signe moins pour supprimer des points.';
 
 // Error
-$_['error_warning']         = 'Attention : Veuillez vérifier le formulaire pour les erreurs !';
-$_['error_permission']      = 'Attention : Vous n\'avez pas la permission de modifier les clients !';
-$_['error_customer']        = 'Attention : Le client n\'existe pas !';
-$_['error_exists']          = 'Attention : L\'adresse e-mail est déjà enregistrée !';
-$_['error_address']         = 'Attention : L\'adresse n\'existe pas !';
-$_['error_firstname']       = 'Le prénom doit contenir entre 1 et 32 caractères !';
-$_['error_lastname']        = 'Le nom doit contenir entre 1 et 32 caractères !';
-$_['error_email']           = 'L\'adresse e-mail ne semble pas être valide !';
-$_['error_telephone']       = 'Le téléphone doit contenir entre 3 et 32 caractères !';
-$_['error_password']        = 'Le mot de passe doit contenir entre 4 et 20 caractères !';
-$_['error_confirm']         = 'Le mot de passe et la confirmation du mot de passe ne correspondent pas !';
-$_['error_address_1']       = 'L\'adresse 1 doit contenir entre 3 et 128 caractères !';
-$_['error_city']            = 'La ville doit contenir entre 2 et 128 caractères !';
-$_['error_postcode']        = 'Le code postal doit contenir entre 2 et 10 caractères pour ce pays !';
-$_['error_country']         = 'Veuillez sélectionner un pays !';
-$_['error_zone']            = 'Veuillez sélectionner une région / un état !';
-$_['error_custom_field']    = '%s requis !';
-$_['error_regex']           = '%s n\'est pas une entrée valide !';
+$_['error_warning']         = 'Attention : Veuillez vérifier le formulaire pour les erreurs!';
+$_['error_permission']      = 'Attention : Vous n\'avez pas la permission de modifier les clients!';
+$_['error_customer']        = 'Attention : Le client n\'existe pas!';
+$_['error_exists']          = 'Attention : L\'adresse e-mail est déjà enregistrée!';
+$_['error_address']         = 'Attention : L\'adresse n\'existe pas!';
+$_['error_firstname']       = 'Le prénom doit contenir entre 1 et 32 caractères!';
+$_['error_lastname']        = 'Le nom doit contenir entre 1 et 32 caractères!';
+$_['error_email']           = 'L\'adresse e-mail ne semble pas être valide!';
+$_['error_telephone']       = 'Le téléphone doit contenir entre 3 et 32 caractères!';
+$_['error_password']        = 'Le mot de passe doit contenir entre 4 et 20 caractères!';
+$_['error_confirm']         = 'Le mot de passe et la confirmation du mot de passe ne correspondent pas!';
+$_['error_address_1']       = 'L\'adresse 1 doit contenir entre 3 et 128 caractères!';
+$_['error_city']            = 'La ville doit contenir entre 2 et 128 caractères!';
+$_['error_postcode']        = 'Le code postal doit contenir entre 2 et 10 caractères pour ce pays!';
+$_['error_country']         = 'Veuillez sélectionner un pays!';
+$_['error_zone']            = 'Veuillez sélectionner une région / un état!';
+$_['error_custom_field']    = '%s requis!';
+$_['error_regex']           = '%s n\'est pas une entrée valide!';

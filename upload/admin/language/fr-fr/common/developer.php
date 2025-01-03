2f8d12462db242aadb227fc1aa6c036f787db8e8
pemaier <pemaier@users.noreply.github.com>
2025-01-03 19:40:06 +0300

    Update developer.php

--- upload/admin/language/fr-fr/common/developer.php
+++ upload/admin/language/fr-fr/common/developer.php
@@ -3,10 +3,10 @@
 $_['heading_title']    = 'Paramètres Développeur';
 
 // Text
-$_['text_success']     = 'Succès : Vous avez modifié les paramètres développeur !';
+$_['text_success']     = 'Succès : Vous avez modifié les paramètres développeur!';
 $_['text_theme']       = 'Thème';
 $_['text_sass']        = 'SASS';
-$_['text_cache']       = 'Succès : Vous avez vidé le cache %s !';
+$_['text_cache']       = 'Succès : Vous avez vidé le cache %s!';
 
 // Column
 $_['column_component'] = 'Composant';
@@ -22,4 +22,4 @@ $_['button_on']        = 'Activé';
 $_['button_off']       = 'Désactivé';
 
 // Error
-$_['error_permission'] = 'Attention : Vous n\'avez pas la permission de modifier les paramètres développeur !';
+$_['error_permission'] = 'Attention : Vous n\'avez pas la permission de modifier les paramètres développeur!';

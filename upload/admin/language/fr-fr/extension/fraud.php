a73c631d7d6c41dab37ab8df519b5192005d55cc
pemaier <pemaier@users.noreply.github.com>
2025-01-03 21:57:59 +0300

    Update fraud.php

--- upload/admin/language/fr-fr/extension/fraud.php
+++ upload/admin/language/fr-fr/extension/fraud.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Anti-Fraude';
 
 // Texte
-$_['text_success']     = 'Succès : Vous avez modifié les paramètres anti-fraude!';
+$_['text_success']     = 'Succès: Vous avez modifié les paramètres anti-fraude!';
 $_['text_list']        = 'Liste Anti-Fraude';
 
 // Colonne
@@ -12,5 +12,5 @@ $_['column_status']    = 'Statut';
 $_['column_action']    = 'Action';
 
 // Erreur
-$_['error_permission'] = 'Attention : Vous n\'êtes pas autorisé à modifier les paramètres anti-fraude!';
-$_['error_extension']  = 'Attention : L\'extension n\'existe pas!';
+$_['error_permission'] = 'Attention: Vous n\'êtes pas autorisé à modifier les paramètres anti-fraude!';
+$_['error_extension']  = 'Attention: L\'extension n\'existe pas!';

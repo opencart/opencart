fc2b075ee1bfbe2677e684824bda1da73c4ee555
pemaier <pemaier@users.noreply.github.com>
2025-01-03 19:50:40 +0300

    Update currency.php

--- upload/admin/language/fr-fr/extension/currency.php
+++ upload/admin/language/fr-fr/extension/currency.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Taux de Change';
 
 // Text
-$_['text_success']     = 'Succès : Vous avez modifié les taux de change !';
+$_['text_success']     = 'Succès : Vous avez modifié les taux de change!';
 $_['text_list']        = 'Liste des Taux de Change';
 
 // Column
@@ -12,5 +12,5 @@ $_['column_status']    = 'Statut';
 $_['column_action']    = 'Action';
 
 // Error
-$_['error_permission'] = 'Attention : Vous n\'avez pas la permission de modifier les taux de change !';
-$_['error_extension']  = 'Attention : L\'extension n\'existe pas !';
+$_['error_permission'] = 'Attention : Vous n\'avez pas la permission de modifier les taux de change!';
+$_['error_extension']  = 'Attention : L\'extension n\'existe pas!';

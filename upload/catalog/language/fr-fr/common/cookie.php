a28f7a8379bf19d0530c69ff7d595ec77b393da9
pemaier <pemaier@users.noreply.github.com>
2025-01-04 00:45:29 +0300

    Update cookie.php

--- upload/catalog/language/fr-fr/common/cookie.php
+++ upload/catalog/language/fr-fr/common/cookie.php
@@ -1,8 +1,8 @@
 <?php
 // Texte
-$_['text_success'] = 'Merci de nous avoir fait part de votre choix !';
+$_['text_success'] = 'Merci de nous avoir fait part de votre choix!';
 $_['text_cookie']  = 'Ce site utilise des cookies. Pour plus d\'informations <a href="%s" class="alert-link modal-link">cliquez ici</a>.';
 
 // Boutons
-$_['button_agree']    = 'Oui, ça me convient !';
-$_['button_disagree'] = 'Non merci !';
+$_['button_agree']    = 'Oui, ça me convient!';
+$_['button_disagree'] = 'Non merci!';

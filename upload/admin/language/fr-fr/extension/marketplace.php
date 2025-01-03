dda23658bda9579c263bcb757abe8460c315f57b
pemaier <pemaier@users.noreply.github.com>
2025-01-03 19:52:22 +0300

    Update marketplace.php

--- upload/admin/language/fr-fr/extension/marketplace.php
+++ upload/admin/language/fr-fr/extension/marketplace.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Places de marché';
 
 // Texte
-$_['text_success']     = 'Succès : Vous avez modifié les places de marché !';
+$_['text_success']     = 'Succès : Vous avez modifié les places de marché!';
 $_['text_list']        = 'Liste des places de marché';
 
 // Colonne
@@ -12,5 +12,5 @@ $_['column_status']    = 'Statut';
 $_['column_action']    = 'Action';
 
 // Erreur
-$_['error_permission'] = 'Attention : Vous n\'êtes pas autorisé à modifier les places de marché !';
-$_['error_extension']  = 'Attention : L\'extension n\'existe pas !';
+$_['error_permission'] = 'Attention : Vous n\'êtes pas autorisé à modifier les places de marché!';
+$_['error_extension']  = 'Attention : L\'extension n\'existe pas!';

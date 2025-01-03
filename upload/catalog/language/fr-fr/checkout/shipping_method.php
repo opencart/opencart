70d28ed7643ba2ffcd35aff6d53e7b3f8afe7db7
pemaier <pemaier@users.noreply.github.com>
2025-01-04 00:41:34 +0300

    Update shipping_method.php

--- upload/catalog/language/fr-fr/checkout/shipping_method.php
+++ upload/catalog/language/fr-fr/checkout/shipping_method.php
@@ -5,14 +5,14 @@ $_['heading_title']          = 'Mode de livraison';
 // Texte
 $_['text_shipping_method']   = 'Options de mode de livraison';
 $_['text_shipping']          = 'Veuillez sélectionner le mode de livraison préféré à utiliser pour cette commande.';
-$_['text_success']           = 'Succès : Vous avez modifié le mode de livraison !';
+$_['text_success']           = 'Succès: Vous avez modifié le mode de livraison!';
 
 // Entrée
 $_['entry_shipping_method']  = 'Choisir un mode de livraison...';
 
 // Erreur
-$_['error_customer']         = 'Client requis !';
-$_['error_payment_address']  = 'Adresse de facturation requise !';
-$_['error_shipping_address'] = 'Adresse de livraison requise !';
-$_['error_shipping_method']  = 'Mode de livraison requis !';
-$_['error_no_shipping']      = 'Aucune option de livraison n\'est disponible. Veuillez <a href="%s">nous contacter</a> pour obtenir de l\'aide !';
+$_['error_customer']         = 'Client requis!';
+$_['error_payment_address']  = 'Adresse de facturation requise!';
+$_['error_shipping_address'] = 'Adresse de livraison requise!';
+$_['error_shipping_method']  = 'Mode de livraison requis!';
+$_['error_no_shipping']      = 'Aucune option de livraison n\'est disponible. Veuillez <a href="%s">nous contacter</a> pour obtenir de l\'aide!';

b33a0f0514d949a6a3c74586aa3016fe47f7e6ec
pemaier <pemaier@users.noreply.github.com>
2025-01-03 14:52:34 +0300

    Update filter_group.php

--- upload/admin/language/fr-fr/catalog/filter_group.php
+++ upload/admin/language/fr-fr/catalog/filter_group.php
@@ -3,7 +3,7 @@
 $_['heading_title']     = 'Groupes de filtres';
 
 // Texte
-$_['text_success']      = 'Succès : Vous avez modifié les groupes de filtres !';
+$_['text_success']      = 'Succès : Vous avez modifié les groupes de filtres!';
 $_['text_list']         = 'Liste des groupes de filtres';
 $_['text_add']          = 'Ajouter un groupe de filtres';
 $_['text_edit']         = 'Modifier un groupe de filtres';
@@ -18,7 +18,7 @@ $_['entry_name']        = 'Nom du groupe de filtres';
 $_['entry_sort_order']  = 'Ordre de tri';
 
 // Erreur
-$_['error_warning']     = 'Attention : Veuillez vérifier attentivement le formulaire pour des erreurs !';
-$_['error_permission']  = 'Attention : Vous n\'avez pas la permission de modifier les filtres !';
-$_['error_name']        = 'Le nom du groupe de filtres doit contenir entre 1 et 64 caractères !';
-$_['error_filter']      = 'Attention : Ce groupe de filtres ne peut pas être supprimé car il est actuellement assigné à %s filtres !';
+$_['error_warning']     = 'Attention : Veuillez vérifier attentivement le formulaire pour des erreurs!';
+$_['error_permission']  = 'Attention : Vous n\'avez pas la permission de modifier les groupes de filtres!';
+$_['error_name']        = 'Le nom du groupe de filtres doit contenir entre 1 et 64 caractères!';
+$_['error_filter']      = 'Attention : Ce groupe de filtres ne peut pas être supprimé car il est actuellement assigné à %s filtres!';

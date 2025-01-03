a9e23b98c3c28c0c22458b5d73bb6d7d85c76194
pemaier <pemaier@users.noreply.github.com>
2025-01-03 20:12:21 +0300

    Update reward.php

--- upload/admin/language/fr-fr/mail/reward.php
+++ upload/admin/language/fr-fr/mail/reward.php
@@ -1,5 +1,5 @@
 <?php
 // Texte
 $_['text_subject']  = '%s - Points de Récompense';
-$_['text_received'] = 'Vous avez reçu %s points de récompense !';
+$_['text_received'] = 'Vous avez reçu %s points de récompense!';
 $_['text_total']    = 'Votre total de points de récompense est maintenant de %s.';

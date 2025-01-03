dfc8297c3106272cc2e3a5e57f76aa86043670d4
pemaier <pemaier@users.noreply.github.com>
2025-01-03 22:06:20 +0300

    Update location.php

--- upload/admin/language/fr-fr/localisation/location.php
+++ upload/admin/language/fr-fr/localisation/location.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Emplacements des Magasins';
 
 // Texte
-$_['text_success']     = 'Succès : Vous avez modifié les emplacements des magasins!';
+$_['text_success']     = 'Succès: Vous avez modifié les emplacements des magasins!';
 $_['text_list']        = 'Liste des Emplacements des Magasins';
 $_['text_add']         = 'Ajouter un Emplacement de Magasin';
 $_['text_edit']        = 'Modifier un Emplacement de Magasin';
@@ -26,10 +26,10 @@ $_['entry_comment']    = 'Commentaire';
 // Aide
 $_['help_geocode']     = 'Veuillez saisir manuellement le géocode de l\'emplacement de votre magasin.';
 $_['help_open']        = 'Remplissez les horaires d\'ouverture de vos magasins.';
-$_['help_comment']     = 'Ce champ est pour toutes les notes spéciales que vous souhaitez communiquer au client, par exemple : Le magasin n\'accepte pas les chèques.';
+$_['help_comment']     = 'Ce champ est pour toutes les notes spéciales que vous souhaitez communiquer au client, par exemple: Le magasin n\'accepte pas les chèques.';
 
 // Erreur
-$_['error_permission'] = 'Attention : Vous n\'avez pas la permission de modifier les emplacements des magasins!';
+$_['error_permission'] = 'Attention: Vous n\'avez pas la permission de modifier les emplacements des magasins!';
 $_['error_name']       = 'Le nom du magasin doit contenir entre 3 et 32 caractères!';
 $_['error_address']    = 'L\'adresse doit contenir entre 3 et 128 caractères!';
 $_['error_telephone']  = 'Le numéro de téléphone doit contenir entre 3 et 32 caractères!';

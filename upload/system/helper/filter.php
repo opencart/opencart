b4faf3d30f798e2fb718bf1f327f3a6eb9e9b1f8
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-01-19 19:38:58 +0800

    Update filter.php

--- upload/system/helper/filter.php
+++ upload/system/helper/filter.php
@@ -1,11 +1,26 @@
 <?php
 /**
- * Filter Keyword
+ * Filter data
  *
- * @param string $string
+ * This function is used to filter data based on the filter array
+ *
+ * @param string $data
+ * @param array $filter
  *
  * @return string
  */
-function oc_filter_keyword(string $string): string {
-	return urlencode(html_entity_decode($string, ENT_QUOTES, 'UTF-8'));
+function oc_filter_data(string $data, array $filter = []): string {
+	$output_data = [];
+
+	foreach ($filter as $key => $value) {
+		if (!isset($data[$key]) || gettype($data[$key]) != gettype($value)) {
+			if (!is_array($value)) {
+				$output_data[] = $value;
+			} else {
+				$output_data[] = oc_filter_data($value, $data[$key]);
+			}
+		}
+	}
+
+	return $output_data;
 }

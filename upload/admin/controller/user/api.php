ff9ccf2a22c88576c909713b3fad358e08caacc0
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-02-17 18:53:02 +0800

    Update api.php

--- upload/admin/controller/user/api.php
+++ upload/admin/controller/user/api.php
@@ -122,7 +122,6 @@ class Api extends \Opencart\System\Engine\Controller {
 
 		foreach ($results as $result) {
 			$data['apis'][] = [
-				'status'        => $result['status'],
 				'date_added'    => date($this->language->get('date_format_short'), strtotime($result['date_added'])),
 				'date_modified' => date($this->language->get('date_format_short'), strtotime($result['date_modified'])),
 				'edit'          => $this->url->link('user/api.form', 'user_token=' . $this->session->data['user_token'] . '&api_id=' . $result['api_id'] . $url)

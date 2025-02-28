3ac8de25159df96887386bb1c8e1b559739f3ab6
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:24 +0800

    New translations cart.php (German)

--- de/upload/catalog/language/de-DE/checkout/cart.php
+++ de/upload/catalog/language/de-DE/checkout/cart.php
@@ -1,38 +1,38 @@
 <?php
 // Heading
-$_['heading_title']              = 'Shopping Cart';
+$_['heading_title']              = 'Warenkorb';
 
 // Text
-$_['text_success']               = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">shopping cart</a>!';
-$_['text_edit']                  = 'Success: You have modified your shopping cart!';
-$_['text_remove']                = 'Success: You have removed an item from your shopping cart!';
-$_['text_login']                 = 'Attention: You must <a href="%s">login</a> or <a href="%s">create an account</a> to view prices!';
-$_['text_no_results']            = 'Your shopping cart is empty!';
-$_['text_model']                 = 'Model';
-$_['text_subscription']          = 'Subscription';
-$_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
-$_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
-$_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
-$_['text_day']                   = 'day';
-$_['text_week']                  = 'week';
-$_['text_semi_month']            = 'half-month';
-$_['text_month']                 = 'month';
-$_['text_year']                  = 'year';
-$_['text_points']                = 'Reward Points';
-$_['text_next']                  = 'What would you like to do next?';
-$_['text_next_choice']           = 'Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.';
+$_['text_success']               = 'Erfolg: Sie haben <a href="%s">%s</a> zu Ihrem <a href="%s">Einkaufswagen</a>hinzugefügt!';
+$_['text_edit']                  = 'Erfolg: Sie haben Ihren Einkaufswagen geändert!';
+$_['text_remove']                = 'Erfolg: Sie haben einen Artikel aus Ihrem Einkaufswagen entfernt!';
+$_['text_login']                 = 'Achtung: Sie müssen sich <a href="%s">anmelden</a> oder <a href="%s">ein Konto erstellen</a> um die Preise anzuzeigen!';
+$_['text_no_results']            = 'Ihr Warenkorb ist leer!';
+$_['text_model']                 = 'Modell';
+$_['text_subscription']          = 'Abonnement';
+$_['text_subscription_trial']    = '%s alle %d %s(s) für %d Zahlung(en) dann ';
+$_['text_subscription_duration'] = '%s alle %d %s(s) für %d Zahlung(en)';
+$_['text_subscription_cancel']   = '%s alle %d %s(s) bis zum Abbruch';
+$_['text_day']                   = 'Tag';
+$_['text_week']                  = 'Woche';
+$_['text_semi_month']            = 'halber Monat';
+$_['text_month']                 = 'Monat';
+$_['text_year']                  = 'Jahr';
+$_['text_points']                = 'Belohnungspunkte';
+$_['text_next']                  = 'Was möchten Sie als Nächstes tun?';
+$_['text_next_choice']           = 'Wählen Sie aus, ob Sie einen Rabattcode oder Bonuspunkte haben, die Sie verwenden möchten, oder ob Sie Ihre Lieferkosten schätzen möchten.';
 
 // Column
-$_['column_image']               = 'Image';
-$_['column_name']                = 'Product';
-$_['column_quantity']            = 'Quantity';
-$_['column_price']               = 'Unit Price';
-$_['column_total']               = 'Total';
+$_['column_image']               = 'Bild';
+$_['column_name']                = 'Produkt';
+$_['column_quantity']            = 'Menge';
+$_['column_price']               = 'Stückpreis';
+$_['column_total']               = 'Gesamt';
 
 // Error
-$_['error_stock']                = 'Products marked with *** are not available in the desired quantity or not in stock!';
-$_['error_minimum']              = 'Minimum order amount is %s!';
-$_['error_required']             = '%s required!';
-$_['error_regex']                = '%s is not a valid input!';
-$_['error_product']              = 'Warning: Product could not be found!';
-$_['error_subscription']         = 'Subscription Plan is not valid!';
+$_['error_stock']                = 'Mit *** gekennzeichnete Produkte sind nicht in der gewünschten Menge verfügbar bzw. nicht auf Lager!';
+$_['error_minimum']              = 'Der Mindestbestellwert beträgt %s!';
+$_['error_required']             = '%s erforderlich!';
+$_['error_regex']                = '%s ist keine gültige Eingabe!';
+$_['error_product']              = 'Achtung: Produkt konnte nicht gefunden werden!';
+$_['error_subscription']         = 'Der Abonnementplan ist ungültig!';

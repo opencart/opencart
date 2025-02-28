fdd3c0239046683d0acd3e10976cbc79ee9ed933
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:42 +0800

    New translations special.php (German)

--- de/upload/catalog/language/de-DE/product/special.php
+++ de/upload/catalog/language/de-DE/product/special.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']    = 'Special Offers';
+$_['heading_title']    = 'Sonderangebote';
 
 // Text
-$_['text_no_results']  = 'There are no special offer products to list.';
-$_['text_compare']     = 'Product Compare (%s)';
-$_['text_sort']        = 'Sort By';
-$_['text_default']     = 'Default';
+$_['text_no_results']  = 'Es sind keine Sonderangebote zum Auflisten vorhanden.';
+$_['text_compare']     = 'Produktvergleich (%s)';
+$_['text_sort']        = 'Sortieren nach';
+$_['text_default']     = 'Standard';
 $_['text_name_asc']    = 'Name (A - Z)';
 $_['text_name_desc']   = 'Name (Z - A)';
-$_['text_price_asc']   = 'Price (Low &gt; High)';
-$_['text_price_desc']  = 'Price (High &gt; Low)';
-$_['text_rating_asc']  = 'Rating (Lowest)';
-$_['text_rating_desc'] = 'Rating (Highest)';
-$_['text_model_asc']   = 'Model (A - Z)';
-$_['text_model_desc']  = 'Model (Z - A)';
-$_['text_limit']       = 'Show';
+$_['text_price_asc']   = 'Preis (Niedrig &gt; Hoch)';
+$_['text_price_desc']  = 'Preis (Hoch &gt; Niedrig)';
+$_['text_rating_asc']  = 'Bewertung (niedrigste)';
+$_['text_rating_desc'] = 'Bewertung (Höchste)';
+$_['text_model_asc']   = 'Modell (A - Z)';
+$_['text_model_desc']  = 'Modell (Z - A)';
+$_['text_limit']       = 'Zeigen';

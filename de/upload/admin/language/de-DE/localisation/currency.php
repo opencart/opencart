72326853dab627d704515d1b755159c89ac4c89f
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:15:03 +0800

    New translations currency.php (German)

--- de/upload/admin/language/de-DE/localisation/currency.php
+++ de/upload/admin/language/de-DE/localisation/currency.php
@@ -1,40 +1,40 @@
 <?php
 // Heading
-$_['heading_title']        = 'Currencies';
+$_['heading_title']        = 'Währungen';
 
 // Text
-$_['text_success']         = 'Success: You have modified currencies!';
-$_['text_list']            = 'Currency List';
-$_['text_add']             = 'Add Currency';
-$_['text_edit']            = 'Edit Currency';
-$_['text_iso']             = 'You can find a full list of ISO currency codes and settings <a href="http://www.xe.com/iso4217.php" target="_blank" class="alert-link">here</a>.';
+$_['text_success']         = 'Erfolg: Sie haben Währungen geändert!';
+$_['text_list']            = 'Währungsliste';
+$_['text_add']             = 'Währung hinzufügen';
+$_['text_edit']            = 'Währung bearbeiten';
+$_['text_iso']             = 'Eine vollständige Liste der ISO-Währungscodes und -Einstellungen <a href="http://www.xe.com/iso4217.php" target="_blank" class="alert-link">finden Sie hier</a>.';
 
 // Column
-$_['column_title']         = 'Currency Title';
+$_['column_title']         = 'Währung Titel';
 $_['column_code']          = 'Code';
-$_['column_value']         = 'Value';
+$_['column_value']         = 'Wert';
 $_['column_status']        = 'Status';
-$_['column_date_modified'] = 'Last Updated';
-$_['column_action']        = 'Action';
+$_['column_date_modified'] = 'Zuletzt aktualisiert';
+$_['column_action']        = 'Aktion';
 
 // Entry
-$_['entry_title']          = 'Currency Title';
+$_['entry_title']          = 'Währung Titel';
 $_['entry_code']           = 'Code';
-$_['entry_value']          = 'Value';
-$_['entry_symbol_left']    = 'Symbol Left';
-$_['entry_symbol_right']   = 'Symbol Right';
-$_['entry_decimal_place']  = 'Decimal Places';
+$_['entry_value']          = 'Wert';
+$_['entry_symbol_left']    = 'Symbol links';
+$_['entry_symbol_right']   = 'Symbol rechts';
+$_['entry_decimal_place']  = 'Dezimalstellen';
 $_['entry_status']         = 'Status';
 
 // Help
-$_['help_code']            = 'Do not change if this is your default currency.';
-$_['help_value']           = 'The value of your default currency in the current currency unit. Set to 1 for your default currency.';
+$_['help_code']            = 'Nicht ändern, wenn dies Ihre Standardwährung ist.';
+$_['help_value']           = 'Der Wert Ihrer Standardwährung in der aktuellen Währungseinheit. Setzen Sie den Wert für Ihre Standardwährung auf 1.';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modify currencies!';
-$_['error_extension']      = 'Warning: The currency extension could not be found!';
-$_['error_title']          = 'Currency Title must be between 3 and 32 characters!';
-$_['error_code']           = 'Currency Code must contain 3 characters!';
-$_['error_default']        = 'Warning: This currency cannot be deleted as it is currently assigned as the default store currency!';
-$_['error_store']          = 'Warning: This currency cannot be deleted as it is currently assigned to %s stores!';
-$_['error_order']          = 'Warning: This currency cannot be deleted as it is currently assigned to %s orders!';
+$_['error_permission']     = 'Achtung: Sie sind nicht berechtigt, Währungen zu ändern!';
+$_['error_extension']      = 'Achtung: Die Währungserweiterung konnte nicht gefunden werden!';
+$_['error_title']          = 'Der Währungstitel muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_code']           = 'Der Währungscode muss 3 Zeichen enthalten!';
+$_['error_default']        = 'Achtung: Diese Währung kann nicht gelöscht werden, da sie aktuell als Standard-Shopwährung festgelegt ist!';
+$_['error_store']          = 'Achtung: Diese Währung kann nicht gelöscht werden, da sie aktuell %s Geschäften zugewiesen ist!';
+$_['error_order']          = 'Achtung: Diese Währung kann nicht gelöscht werden, da sie aktuell %s Bestellungen zugewiesen ist!';

8123bfb4d51d37dce0afba7e9e44e0636f641286
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:23 +0800

    New translations backup.php (German)

--- de/upload/admin/language/de-DE/tool/backup.php
+++ de/upload/admin/language/de-DE/tool/backup.php
@@ -1,32 +1,32 @@
 <?php
 // Heading
-$_['heading_title']      = 'Backup &amp; Restore';
+$_['heading_title']      = 'Backup &amp; Wiederherstellen';
 
 // Text
-$_['text_success']       = 'Success: You have successfully modified your database!';
-$_['text_progress']      = 'Backup Progress';
-$_['text_backup']        = 'Backing up table %s records %s to %s records';
-$_['text_restore']       = 'Restoring %s of %s';
-$_['text_option']        = 'Backup Options';
-$_['text_history']       = 'Backup History';
-$_['text_import']        = 'For large backup files it is better to upload the SQL file via FTP to the <strong>~/storage/backup/</strong> directory.';
+$_['text_success']       = 'Erfolg: Sie haben Ihre Datenbank erfolgreich geändert!';
+$_['text_progress']      = 'Sicherungsfortschritt';
+$_['text_backup']        = 'Sichern der Tabellen %s Datensätze %s bis %s Datensätze';
+$_['text_restore']       = 'Wiederherstellen von %s von %s';
+$_['text_option']        = 'Backup-Optionen';
+$_['text_history']       = 'Sicherungsverlauf';
+$_['text_import']        = 'Bei großen Sicherungsdateien ist es besser, die SQL-Datei per FTP in das Verzeichnis <strong>~/storage/backup/</strong> hochzuladen.';
 
 // Column
-$_['column_filename']    = 'Filename';
-$_['column_size']        = 'Size';
-$_['column_date_added']  = 'Date Added';
-$_['column_action']      = 'Action';
+$_['column_filename']    = 'Dateiname';
+$_['column_size']        = 'Größe';
+$_['column_date_added']  = 'Datum hinzugefügt';
+$_['column_action']      = 'Aktion';
 
 // Entry
-$_['entry_progress']     = 'Progress';
+$_['entry_progress']     = 'Fortschritt';
 $_['entry_export']       = 'Export';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify Backup &amp; Restore!';
-$_['error_export']       = 'Warning: You must select at least one table to export!';
-$_['error_table']        = 'Table %s is not in the allowed list!';
-$_['error_file']         = 'File could not be found!';
-$_['error_directory']    = 'Directory could not be found!';
-$_['error_not_found']    = 'Error: Could not find file %s !';
-$_['error_headers_sent'] = 'Error: Headers already sent out!';
-$_['error_upload_size']  = 'Upload file cannot be larger than %s!';
+$_['error_permission']   = 'Warnung: Sie sind nicht berechtigt, Backup &amp; Restore zu ändern!';
+$_['error_export']       = 'Achtung: Sie müssen mindestens eine Tabelle zum Exportieren auswählen!';
+$_['error_table']        = 'Tabelle %s ist nicht in der Erlaubtliste!';
+$_['error_file']         = 'Datei konnte nicht gefunden werden!';
+$_['error_directory']    = 'Verzeichnis konnte nicht gefunden werden!';
+$_['error_not_found']    = 'Fehler: Datei %s konnte nicht gefunden werden!';
+$_['error_headers_sent'] = 'Fehler: Header bereits versendet!';
+$_['error_upload_size']  = 'Die hochgeladene Datei darf nicht größer als %ssein!';

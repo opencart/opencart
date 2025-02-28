afb2b4cba9d19df2592cb967ea85c88f50d21b3d
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:07 +0800

    New translations promotion.php (German)

--- de/upload/admin/language/de-DE/marketplace/promotion.php
+++ de/upload/admin/language/de-DE/marketplace/promotion.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_recommended'] = 'Recommended';
-$_['text_install']     = 'Install';
-$_['text_uninstall']   = 'Uninstall';
-$_['text_delete']      = 'Delete';
+$_['text_recommended'] = 'Empfohlen';
+$_['text_install']     = 'Installieren';
+$_['text_uninstall']   = 'Deinstallieren';
+$_['text_delete']      = 'Löschen';

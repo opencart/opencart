7c7c72968005b6e797a6b18ab43efdfc69e0e2b0
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:20:26 +0800

    New translations coupon.php (German)

--- de/upload/extension/opencart/catalog/language/de-DE/checkout/coupon.php
+++ de/upload/extension/opencart/catalog/language/de-DE/checkout/coupon.php
@@ -1,13 +1,13 @@
 <?php
 // Heading
-$_['heading_title'] = 'Use Coupon Code';
+$_['heading_title'] = 'Gutscheincode verwenden';
 
 // Text
-$_['text_success']  = 'Success: Your coupon discount has been applied!';
+$_['text_success']  = 'Erfolg: Ihr Gutscheinrabatt wurde angewendet!';
 
 // Entry
-$_['entry_coupon']  = 'Enter your coupon here';
+$_['entry_coupon']  = 'Geben Sie hier Ihren Gutschein ein';
 
 // Error
-$_['error_coupon']  = 'Warning: Coupon is either invalid, expired or reached its usage limit!';
-$_['error_status']  = 'Warning: Coupons are not enabled on this store!';
+$_['error_coupon']  = 'Achtung: Der Gutschein ist entweder ungültig, abgelaufen oder hat sein Einlöselimit erreicht!';
+$_['error_status']  = 'Achtung: Gutscheine sind in diesem Shop nicht möglich!';

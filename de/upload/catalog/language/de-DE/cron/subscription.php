833bc784f5950025ac200ab40585cc8df0cec2bb
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:57 +0800

    New translations subscription.php (German)

--- de/upload/catalog/language/de-DE/cron/subscription.php
+++ de/upload/catalog/language/de-DE/cron/subscription.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_log']               = 'Check the subscription log for more information.';
+$_['text_log']               = 'Weitere Informationen finden Sie im Abonnementprotokoll.';
 
 // Error
-$_['error_store'] 		     = 'Warning: Store not found!';
-$_['error_language']         = 'Warning: Language could not be found!';
-$_['error_currency']         = 'Warning: Currency could not be found!';
-$_['error_customer']         = 'Warning: Customer could not be found!';
-$_['error_payment_address']  = 'Warning: Payment address could not be found!';
-$_['error_shipping_address'] = 'Warning: Shipping address could not be found!';
-$_['error_product']          = 'Warning: Product %s could not be found!';
-$_['error_option']           = 'Warning: Option %s could not be found!';
-$_['error_shipping_method']  = 'Warning: Shipping method required!';
-$_['error_payment_method']   = 'Warning: Payment method required!';
-$_['error_extension']        = 'Warning: Payment method extension could not be found!';
+$_['error_store'] 		     = 'Achtung: Store nicht gefunden!';
+$_['error_language']         = 'Achtung: Sprache konnte nicht gefunden werden!';
+$_['error_currency']         = 'Achtung: Währung konnte nicht gefunden werden!';
+$_['error_customer']         = 'Achtung: Kunde konnte nicht gefunden werden!';
+$_['error_payment_address']  = 'Achtung: Zahlungsadresse konnte nicht gefunden werden!';
+$_['error_shipping_address'] = 'Achtung: Lieferadresse konnte nicht gefunden werden!';
+$_['error_product']          = 'Achtung: Produkt %s konnte nicht gefunden werden!';
+$_['error_option']           = 'Warnung: Option %s konnte nicht gefunden werden!';
+$_['error_shipping_method']  = 'Achtung: Versandart erforderlich!';
+$_['error_payment_method']   = 'Achtung: Zahlungsmethode erforderlich!';
+$_['error_extension']        = 'Achtung: Zahlungsmethoden-Erweiterung konnte nicht gefunden werden!';

e3088b3bc610038b2d695664c68a34b1e3bac7e9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 04:18:16 +0800

    New translations profile.php (English, United Kingdom)

---
+++ en-GB/upload/admin/language/en-gb/user/profile.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']            = 'Profile';
+
+// Text
+$_['text_success']             = 'Success: You have modified your profile!';
+$_['text_edit']                = 'Edit Your Profile';
+$_['text_user']                = 'Users Details';
+$_['text_password']            = 'Password';
+
+// Entry
+$_['entry_username']           = 'Username';
+$_['entry_password']           = 'Password';
+$_['entry_confirm']            = 'Confirm';
+$_['entry_firstname']          = 'First Name';
+$_['entry_lastname']           = 'Last Name';
+$_['entry_email']              = 'E-Mail';
+$_['entry_image']              = 'Image';
+
+// Error
+$_['error_permission']         = 'Warning: You do not have permission to modify your profile!';
+$_['error_username_exists']    = 'Warning: Username is already in use!';
+$_['error_username']           = 'Username must be between 3 and 20 characters!';
+$_['error_firstname']          = 'First Name must be between 1 and 32 characters!';
+$_['error_lastname']           = 'Last Name must be between 1 and 32 characters!';
+$_['error_email']              = 'E-Mail Address does not appear to be valid!';
+$_['error_email_exists']       = 'Warning: E-Mail Address is already registered!';
+$_['error_password']           = 'Password must contain a %s and be between %d and 20 characters!';
+$_['error_password_uppercase'] = 'uppercase';
+$_['error_password_lowercase'] = 'lowercase';
+$_['error_password_number']    = 'number';
+$_['error_password_symbol']    = 'symbol';
+$_['error_password_length']    = 'Password must be between %d and 20 characters!';
+$_['error_confirm']            = 'Password and password confirmation do not match!';

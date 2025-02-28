14f97b24cd8c64e43300c655416e767ba4cdece2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 05:20:19 +0800

    New translations step_4.php (German)

---
+++ de/upload/install/language/de-DE/install/step_4.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']               = 'Installation complete';
+
+// Text
+$_['text_step_4']                 = 'Ready to start selling!';
+$_['text_catalog']                = 'Go to your Online Shop';
+$_['text_admin']                  = 'Login to your Administration';
+$_['text_extension']              = 'Visit the extensions store';
+$_['text_mail']                   = 'Join the mailing list';
+$_['text_mail_description']       = 'Stay informed of OpenCart updates and events.';
+$_['text_facebook']               = 'Like us on Facebook';
+$_['text_facebook_description']   = 'Tell us how much you like OpenCart!';
+$_['text_facebook_visit']         = 'Visit our Facebook page';
+$_['text_forum']                  = 'Community forums';
+$_['text_forum_description']      = 'Get help from other OpenCart users';
+$_['text_forum_visit']            = 'Visit our forums';
+$_['text_commercial']             = 'Commercial support';
+$_['text_commercial_description'] = 'Development services from OpenCart partners';
+$_['text_commercial_visit']       = 'Visit our partner page';
+
+// Button
+$_['button_mail']                 = 'Join here';
+
+// Error
+$_['error_warning']               = 'Don\'t forget to delete your installation directory!';

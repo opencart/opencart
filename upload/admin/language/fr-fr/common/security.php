92ef805fdeb40c73801bcc80ad3f26c37ea60071
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2025-02-28 10:26:24 -0500

    Update security.php
    Added new language definitions.

--- upload/admin/language/fr-fr/common/security.php
+++ upload/admin/language/fr-fr/common/security.php
@@ -8,10 +8,18 @@ $_['text_install_description']  = 'Votre répertoire d\'installation doit être 
 $_['text_install_success']      = 'Succès: Le répertoire d\'installation a été supprimé!';
 $_['text_storage']              = 'Chemin de stockage';
 $_['text_storage_description']  = 'Il est très important de déplacer le répertoire de stockage en dehors du répertoire web (par exemple, public_html, www ou htdocs).';
+$_['text_storage_move']         = 'Déplacement de %s envers %s de %s fichiers de stockage';
 $_['text_storage_success']      = 'Succès: Le répertoire de stockage a été déplacé!';
+$_['text_storage_delete']             = 'Supprimer le répertoire de stockage précédent';
+$_['text_storage_delete_description'] = 'Le répertoire de stockage précédent doit être supprimé!';
+$_['text_storage_delete_success']     = 'Succès: Le répertoire de stockage précédent a été supprimé!';
 $_['text_admin']                = 'Renommer le répertoire admin';
 $_['text_admin_description']    = 'Veuillez saisir un nouveau nom de répertoire admin dans le champ ci-dessous.';
+$_['text_admin_move']           = 'Déplacement de %s envers %s de %s fichiers admin';
 $_['text_admin_success']        = 'Succès: Le répertoire admin a été renommé!';
+$_['text_admin_delete']             = 'Supprimer le répertoire admin précédent';
+$_['text_admin_delete_description'] = 'Le répertoire admin précédent doit être supprimé!';
+$_['text_admin_delete_success']     = 'Succès: Le répertoire admin précédent a été supprimé!';
 $_['text_path']                 = 'Chemin';
 
 // Entry
@@ -22,14 +30,17 @@ $_['entry_name']                = 'Nom du Répertoire';
 
 // Button
 $_['button_move']               = 'Déplacer';
-$_['button_rename']             = 'Renommer';
+
+// Help
+$_['help_storage']              = 'Le nom du répertoire de stockage doit débuter comme suit: `storage_`.';
 
 // Error
 $_['error_permission']          = 'Attention: Vous n\'avez pas la permission de modifier la sécurité!';
 $_['error_install']             = 'Attention: Le répertoire d\'installation n\'existe pas!';
 $_['error_storage']             = 'Attention: Le répertoire de stockage n\'existe pas!';
-$_['error_storage_exists']      = 'Attention: Le nom du répertoire de stockage existe déjà!';
+$_['error_storage_root']        = 'Attention: Le répertoire de stockage doit être situé en dehors du répertoire racine!';
+$_['error_storage_name']        = 'Attention: Le nom du répertoire de stockage doit débuter comme suit: `storage_`. Exemple: `storage_`.';
 $_['error_admin']               = 'Attention: Le répertoire admin n\'existe pas!';
 $_['error_admin_exists']        = 'Attention: Le nom du répertoire admin existe déjà!';
-$_['error_admin_name']          = 'Attention: Le nom admin ne peut pas être `admin` car il est accessible au public!';
 $_['error_writable']            = 'Attention: Les fichiers config.php et admin/config.php doivent être rendus modifiables!';
+$_['error_remove']              = 'Attention: Le répertoire à supprimer n\'existe pas!';

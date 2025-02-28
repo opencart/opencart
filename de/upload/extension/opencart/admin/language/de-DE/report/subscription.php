36f8b71a196b80153991559ccff5bf16b3b8370a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:56 +0800

    New translations subscription.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/report/subscription.php
+++ de/upload/extension/opencart/admin/language/de-DE/report/subscription.php
@@ -1,33 +1,33 @@
 <?php
 // Heading
-$_['heading_title']             = 'Subscriptions Report';
+$_['heading_title']             = 'Abonnementsbericht';
 
 // Text
-$_['text_extension']            = 'Extensions';
-$_['text_edit']                 = 'Edit Subscriptions Report';
-$_['text_success']              = 'Success: You have modified subscriptions report!';
+$_['text_extension']            = 'Erweiterungen';
+$_['text_edit']                 = 'Bericht zu Abonnements bearbeiten';
+$_['text_success']              = 'Erfolg: Sie haben den Abonnementbericht geändert!';
 $_['text_filter']               = 'Filter';
-$_['text_year']                 = 'Years';
-$_['text_month']                = 'Months';
-$_['text_week']                 = 'Weeks';
-$_['text_day']                  = 'Days';
-$_['text_all_status']           = 'All Statuses';
+$_['text_year']                 = 'Jahre';
+$_['text_month']                = 'Monate';
+$_['text_week']                 = 'Wochen';
+$_['text_day']                  = 'Tage';
+$_['text_all_status']           = 'Alle Status';
 
 // Column
-$_['column_date_start']         = 'Date Start';
-$_['column_date_end']           = 'Date End';
-$_['column_subscriptions']      = 'No. Subscriptions';
-$_['column_products']           = 'No. Products';
-$_['column_tax']                = 'Tax';
-$_['column_total']              = 'Total';
+$_['column_date_start']         = 'Datum Beginn';
+$_['column_date_end']           = 'Datum Ende';
+$_['column_subscriptions']      = 'Nr. Abonnements';
+$_['column_products']           = 'Nr. Produkte';
+$_['column_tax']                = 'Steuer';
+$_['column_total']              = 'Gesamt';
 
 // Entry
-$_['entry_date_start']          = 'Date Start';
-$_['entry_date_end']            = 'Date End';
-$_['entry_group']               = 'Group By';
-$_['entry_subscription_status'] = 'Subscription Status';
+$_['entry_date_start']          = 'Datum Beginn';
+$_['entry_date_end']            = 'Datum Ende';
+$_['entry_group']               = 'Gruppieren nach';
+$_['entry_subscription_status'] = 'Abonnementstatus';
 $_['entry_status']              = 'Status';
-$_['entry_sort_order']          = 'Sort Order';
+$_['entry_sort_order']          = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']          = 'Warning: You do not have permission to modify subscriptions report!';
+$_['error_permission']          = 'Achtung: Sie sind nicht berechtigt, den Abonnementbericht zu ändern!';

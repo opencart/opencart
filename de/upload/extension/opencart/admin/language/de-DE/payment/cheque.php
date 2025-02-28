3deb931cd9836f2c1273d0165e478d81fb5f5d58
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:30 +0800

    New translations cheque.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/payment/cheque.php
+++ de/upload/extension/opencart/admin/language/de-DE/payment/cheque.php
@@ -1,19 +1,19 @@
 <?php
 // Heading
-$_['heading_title']      = 'Cheque / Money Order';
+$_['heading_title']      = 'Scheck / Zahlungsanweisung';
 
 // Text
-$_['text_extension']     = 'Extensions';
-$_['text_success']       = 'Success: You have modified cheque / money order account details!';
-$_['text_edit']          = 'Edit Cheque / Money Order';
+$_['text_extension']     = 'Erweiterungen';
+$_['text_success']       = 'Erfolg: Sie haben die Scheck-/Zahlungsanweisungskontodaten geändert!';
+$_['text_edit']          = 'Scheck/Zahlungsanweisung bearbeiten';
 
 // Entry
-$_['entry_payable']      = 'Payable To';
-$_['entry_order_status'] = 'Order Status';
-$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_payable']      = 'Zahlbar bis';
+$_['entry_order_status'] = 'Bestellstatus';
+$_['entry_geo_zone']     = 'Geozone';
 $_['entry_status']       = 'Status';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_sort_order']   = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment cheque / money order!';
-$_['error_payable']      = 'Payable To required!';
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, Zahlungsschecks/Zahlungsanweisungen zu ändern!';
+$_['error_payable']      = 'Zahlbar an erforderlich!';

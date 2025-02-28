e12018025194381018bfadbd7b110c9b5c01040e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:54 +0800

    New translations sale_tax.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/report/sale_tax.php
+++ de/upload/extension/opencart/admin/language/de-DE/report/sale_tax.php
@@ -1,32 +1,32 @@
 <?php
 // Heading
-$_['heading_title']      = 'Tax Report';
+$_['heading_title']      = 'Steuerbericht';
 
 // Text
-$_['text_extension']     = 'Extensions';
-$_['text_edit']          = 'Edit Tax Report';
-$_['text_success']       = 'Success: You have modified tax report!';
+$_['text_extension']     = 'Erweiterungen';
+$_['text_edit']          = 'Steuerbericht bearbeiten';
+$_['text_success']       = 'Erfolg: Sie haben den Steuerbericht geändert!';
 $_['text_filter']        = 'Filter';
-$_['text_year']          = 'Years';
-$_['text_month']         = 'Months';
-$_['text_week']          = 'Weeks';
-$_['text_day']           = 'Days';
-$_['text_all_status']    = 'All Statuses';
+$_['text_year']          = 'Jahre';
+$_['text_month']         = 'Monate';
+$_['text_week']          = 'Wochen';
+$_['text_day']           = 'Tage';
+$_['text_all_status']    = 'Alle Status';
 
 // Column
-$_['column_date_start']  = 'Date Start';
-$_['column_date_end']    = 'Date End';
-$_['column_title']       = 'Tax Title';
-$_['column_orders']      = 'No. Orders';
-$_['column_total']       = 'Total';
+$_['column_date_start']  = 'Datum Beginn';
+$_['column_date_end']    = 'Datum Ende';
+$_['column_title']       = 'Steuertitel';
+$_['column_orders']      = 'Anzahl Bestellungen';
+$_['column_total']       = 'Gesamt';
 
 // Entry
-$_['entry_date_start']   = 'Date Start';
-$_['entry_date_end']     = 'Date End';
-$_['entry_group']        = 'Group By';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_date_start']   = 'Datum Beginn';
+$_['entry_date_end']     = 'Datum Ende';
+$_['entry_group']        = 'Gruppieren nach';
+$_['entry_order_status'] = 'Bestellstatus';
 $_['entry_status']       = 'Status';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_sort_order']   = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify tax report!';
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, den Steuerbericht zu ändern!';

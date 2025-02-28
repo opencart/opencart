c2b1430b64930f92d19758243e78222272b23821
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:20:54 +0800

    New translations free.php (German)

--- de/upload/extension/opencart/catalog/language/de-DE/shipping/free.php
+++ de/upload/extension/opencart/catalog/language/de-DE/shipping/free.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']    = 'Free Shipping';
+$_['heading_title']    = 'Kostenloser Versand';
 
 // Text
-$_['text_description'] = 'Free Shipping';
+$_['text_description'] = 'Kostenloser Versand';

9cc7429ff685a83dee46ceb828fafc0500cabb32
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:07 +0800

    New translations ip.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/fraud/ip.php
+++ de/upload/extension/opencart/admin/language/de-DE/fraud/ip.php
@@ -3,28 +3,28 @@
 $_['heading_title']      = 'IP';
 
 // Text
-$_['text_extension']     = 'Extensions';
-$_['text_fraud']         = 'Anti-Fraud';
-$_['text_success']       = 'Success: You have modified Anti-Fraud IP!';
-$_['text_edit']          = 'Edit Anti-Fraud IP';
-$_['text_ip_add']        = 'Add IP Address';
-$_['text_ip_list']       = 'Fraud IP Address List';
+$_['text_extension']     = 'Erweiterungen';
+$_['text_fraud']         = 'Betrugsbekämpfung';
+$_['text_success']       = 'Erfolg: Sie haben die Anti-Fraud-IP geändert!';
+$_['text_edit']          = 'Betrugsschutz-IP bearbeiten';
+$_['text_ip_add']        = 'IP-Adresse hinzufügen';
+$_['text_ip_list']       = 'Liste betrügerischer IP-Adressen';
 
 // Column
 $_['column_ip']          = 'IP';
-$_['column_total']       = 'Total Accounts';
-$_['column_date_added']  = 'Date Added';
-$_['column_action']      = 'Action';
+$_['column_total']       = 'Konten insgesamt';
+$_['column_date_added']  = 'Datum hinzugefügt';
+$_['column_action']      = 'Aktion';
 
 // Entry
 $_['entry_ip']           = 'IP';
 $_['entry_status']       = 'Status';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_order_status'] = 'Bestellstatus';
 
 // Help
-$_['help_order_status']  = 'Customers that have a banned IP on their accounts will be assigned this order status and will not be allowed to reach the complete status automatically.';
+$_['help_order_status']  = 'Kunden, die eine gesperrte IP auf ihrem Konto haben, wird dieser Bestellstatus zugewiesen und dürfen den vollständigen Status nicht automatisch erreichen.';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify Anti-Fraud IP!';
-$_['error_required']     = 'IP Address required!';
-$_['error_invalid']      = 'IP Address invalid!';
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, die Anti-Fraud-IP zu ändern!';
+$_['error_required']     = 'IP-Adresse erforderlich!';
+$_['error_invalid']      = 'IP-Adresse ungültig!';

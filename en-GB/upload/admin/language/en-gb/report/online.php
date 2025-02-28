80c5eb30bb90a6cca4baf42c4dc54a2fc3c8b177
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 05:12:45 +0800

    New translations online.php (English, United Kingdom)

---
+++ en-GB/upload/admin/language/en-gb/report/online.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']     = 'Online Report';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified customers online report!';
+$_['text_list']         = 'Online List';
+$_['text_filter']       = 'Filter';
+$_['text_guest']        = 'Guest';
+
+// Column
+$_['column_ip']         = 'IP';
+$_['column_customer']   = 'Customer';
+$_['column_url']        = 'Last Page Visited';
+$_['column_referer']    = 'Referer';
+$_['column_date_added'] = 'Last Click';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_ip']          = 'IP';
+$_['entry_customer']    = 'Customer';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify customers online report!';

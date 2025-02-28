3590ad3e02e47158fc1ace17be14d36e11863daa
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:15:47 +0800

    New translations subscription.php (German)

--- de/upload/admin/language/de-DE/mail/subscription.php
+++ de/upload/admin/language/de-DE/mail/subscription.php
@@ -1,10 +1,10 @@
 <?php
 // Text
-$_['text_subject']             = '%s - Subscription';
-$_['text_subscription_id']     = 'Subscription ID';
-$_['text_date_added']          = 'Subscription Date:';
-$_['text_subscription_status'] = 'Your subscription has been added to the following status:';
-$_['text_comment']             = 'The comments for your subscription are:';
-$_['text_payment_method']      = 'Payment Method';
-$_['text_payment_code']        = 'Payment Code';
-$_['text_footer']              = 'Please reply to this email if you have any questions.';
+$_['text_subject']             = '%s - Abonnement';
+$_['text_subscription_id']     = 'Abonnement-ID';
+$_['text_date_added']          = 'Abonnementdatum:';
+$_['text_subscription_status'] = 'Ihrem Abonnement wurde der folgende Status hinzugefügt:';
+$_['text_comment']             = 'Die Kommentare zu Ihrem Abonnement lauten:';
+$_['text_payment_method']      = 'Zahlungsmethode';
+$_['text_payment_code']        = 'Zahlungscode';
+$_['text_footer']              = 'Bitte antworten Sie auf diese E-Mail, wenn Sie Fragen haben.';

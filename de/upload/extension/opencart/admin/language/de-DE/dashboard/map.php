67ced5aa0a0a1b536e9c52fe900b7afb15feef07
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:59 +0800

    New translations map.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/dashboard/map.php
+++ de/upload/extension/opencart/admin/language/de-DE/dashboard/map.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']    = 'World Map';
+$_['heading_title']    = 'Weltkarte';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified dashboard map!';
-$_['text_edit']        = 'Edit Dashboard Map';
-$_['text_order']       = 'Orders';
-$_['text_sale']        = 'Sales';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben die Dashboard-Karte geändert!';
+$_['text_edit']        = 'Dashboard-Karte bearbeiten';
+$_['text_order']       = 'Bestellungen';
+$_['text_sale']        = 'Verkäufe';
 
 // Entry
 $_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+$_['entry_width']      = 'Breite';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard map!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, die Dashboard-Karte zu ändern!';

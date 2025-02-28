3b4b5e893c05d046e47b112a3363c37a2e08a09c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:33 +0800

    New translations compare.php (German)

--- de/upload/catalog/language/de-DE/product/compare.php
+++ de/upload/catalog/language/de-DE/product/compare.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']     = 'Product Comparison';
+$_['heading_title']     = 'Produktvergleich';
 
 // Text
-$_['text_product']      = 'Product Details';
-$_['text_name']         = 'Product';
-$_['text_image']        = 'Image';
-$_['text_price']        = 'Price';
-$_['text_model']        = 'Model';
-$_['text_manufacturer'] = 'Brand';
-$_['text_availability'] = 'Availability';
-$_['text_rating']       = 'Rating';
-$_['text_reviews']      = 'Based on %s reviews.';
-$_['text_summary']      = 'Summary';
-$_['text_weight']       = 'Weight';
-$_['text_dimension']    = 'Dimensions (L x W x H)';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_success']      = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">product comparison</a>!';
-$_['text_remove']       = 'Success: You have modified your product comparison!';
-$_['text_no_results']   = 'You have not chosen any products to compare.';
+$_['text_product']      = 'Produktdetails';
+$_['text_name']         = 'Produkt';
+$_['text_image']        = 'Bild';
+$_['text_price']        = 'Preis';
+$_['text_model']        = 'Modell';
+$_['text_manufacturer'] = 'Marke';
+$_['text_availability'] = 'Verfügbarkeit';
+$_['text_rating']       = 'Bewertungen';
+$_['text_reviews']      = 'Basierend auf %s Bewertungen.';
+$_['text_summary']      = 'Zusammenfassung';
+$_['text_weight']       = 'Gewicht';
+$_['text_dimension']    = 'Abmessungen (L x B x H)';
+$_['text_compare']      = 'Produktvergleich (%s)';
+$_['text_success']      = 'Erfolg: Sie haben <a href="%s">%s</a> zu Ihrem <a href="%s">Produktvergleich</a>hinzugefügt!';
+$_['text_remove']       = 'Erfolg: Sie haben Ihren Produktvergleich geändert!';
+$_['text_no_results']   = 'Sie haben keine Produkte zum Vergleichen ausgewählt.';
 
 // Error
-$_['error_product']     = 'Warning: Product could not be found!';
+$_['error_product']     = 'Achtung: Produkt konnte nicht gefunden werden!';

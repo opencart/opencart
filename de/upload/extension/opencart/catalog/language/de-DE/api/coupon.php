15b1053e45e1fde2ec09193ed8b2e049329df883
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 05:18:47 +0800

    New translations coupon.php (German)

---
+++ de/upload/extension/opencart/catalog/language/de-DE/api/coupon.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_success']   = 'Success: Your coupon discount has been applied!';
+
+// Error
+$_['error_customer'] = 'Warning: Customer details required!';
+$_['error_product']  = 'Warning: Products required!';
+$_['error_coupon']   = 'Warning: Coupon is either invalid, expired or reached its usage limit!';
+$_['error_status']   = 'Warning: Coupons are not enabled on this store!';
+$_['error_confirm']  = 'Warning: Please check the coupon form carefully for errors!';

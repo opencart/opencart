ed38055c5f352c41b264ccf7ffbdf6ffe8642df3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:15:33 +0800

    New translations customer_approve.php (German)

--- de/upload/admin/language/de-DE/mail/customer_approve.php
+++ de/upload/admin/language/de-DE/mail/customer_approve.php
@@ -1,10 +1,10 @@
 <?php
 // Text
-$_['text_subject'] = '%s - Your Account has been activated!';
-$_['text_welcome'] = 'Welcome and thank you for registering at %s!';
-$_['text_login']   = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_service'] = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_thanks']  = 'Thanks,';
+$_['text_subject'] = '%s – Ihr Konto wurde aktiviert!';
+$_['text_welcome'] = 'Willkommen und danke für Ihre Registrierung bei %s!';
+$_['text_login']   = 'Ihr Konto wurde nun erstellt und Sie können sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website besuchen oder die folgende URL aufrufen:';
+$_['text_service'] = 'Nach der Anmeldung haben Sie Zugriff auf weitere Dienste. Sie können beispielsweise frühere Bestellungen prüfen, Rechnungen ausdrucken und Ihre Kontoinformationen bearbeiten.';
+$_['text_thanks']  = 'Danke,';
 
 // Button
 $_['button_login'] = 'Login';

14a962b5eed3414917f70eb6b36633346fd0f2c1
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:14:09 +0800

    New translations header.php (German)

--- de/upload/admin/language/de-DE/common/header.php
+++ de/upload/admin/language/de-DE/common/header.php
@@ -3,13 +3,13 @@
 $_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_notification']      = 'Notifications';
-$_['text_notification_all']  = 'Show All';
-$_['text_notification_none'] = 'There are no notifications';
-$_['text_profile']           = 'Your Profile';
-$_['text_store']             = 'Stores';
-$_['text_help']              = 'Help';
+$_['text_notification']      = 'Benachrichtigungen';
+$_['text_notification_all']  = 'Alles zeigen';
+$_['text_notification_none'] = 'Es gibt keine Benachrichtigungen';
+$_['text_profile']           = 'Ihr Profil';
+$_['text_store']             = 'Geschäfte';
+$_['text_help']              = 'Helfen';
 $_['text_homepage']          = 'OpenCart Homepage';
 $_['text_support']           = 'Support Forum';
-$_['text_documentation']     = 'Documentation';
-$_['text_logout']            = 'Logout';
+$_['text_documentation']     = 'Dokumentation';
+$_['text_logout']            = 'Ausloggen';

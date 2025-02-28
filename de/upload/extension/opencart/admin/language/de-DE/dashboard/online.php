034e026f42ba13c307f6aa2772aaf6346b58f772
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:00 +0800

    New translations online.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/dashboard/online.php
+++ de/upload/extension/opencart/admin/language/de-DE/dashboard/online.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'People Online';
+$_['heading_title']    = 'Leute online';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified dashboard online!';
-$_['text_edit']        = 'Edit Dashboard Online';
-$_['text_view']        = 'View more...';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das Dashboard online geändert!';
+$_['text_edit']        = 'Dashboard online bearbeiten';
+$_['text_view']        = 'Mehr anzeigen...';
 
 // Entry
 $_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+$_['entry_width']      = 'Breite';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard online!';
+$_['error_permission'] = 'Warnung: Sie sind nicht berechtigt, das Dashboard online zu ändern!';

c1bdcc783d84b29377ee69aca9e31bdb54969da4
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:30 +0800

    New translations payment_address.php (German)

--- de/upload/catalog/language/de-DE/checkout/payment_address.php
+++ de/upload/catalog/language/de-DE/checkout/payment_address.php
@@ -1,33 +1,33 @@
 <?php
 // Heading
-$_['heading_title']         = 'Payment Address';
+$_['heading_title']         = 'Zahlungsadresse';
 
 // Text
-$_['text_address_new']      = 'I want to use a new address';
-$_['text_address_existing'] = 'I want to use an existing address';
-$_['text_success']          = 'Success: You have changed payment address!';
+$_['text_address_new']      = 'Ich möchte eine neue Adresse verwenden';
+$_['text_address_existing'] = 'Ich möchte eine bestehende Adresse verwenden';
+$_['text_success']          = 'Erfolg: Sie haben die Zahlungsadresse geändert!';
 
 // Entry
-$_['entry_address']         = 'Choose Address';
-$_['entry_firstname']       = 'First Name';
-$_['entry_lastname']        = 'Last Name';
-$_['entry_company']         = 'Company';
-$_['entry_address_1']       = 'Address 1';
-$_['entry_address_2']       = 'Address 2';
-$_['entry_postcode']        = 'Post Code';
-$_['entry_city']            = 'City';
-$_['entry_country']         = 'Country';
-$_['entry_zone']            = 'Region / State';
+$_['entry_address']         = 'Adresse auswählen';
+$_['entry_firstname']       = 'Vorname';
+$_['entry_lastname']        = 'Nachname';
+$_['entry_company']         = 'Unternehmen';
+$_['entry_address_1']       = 'Adresse 1';
+$_['entry_address_2']       = 'Adresse 2';
+$_['entry_postcode']        = 'Postleitzahl';
+$_['entry_city']            = 'Stadt';
+$_['entry_country']         = 'Land';
+$_['entry_zone']            = 'Region/Staat';
 
 // Error
-$_['error_customer']        = 'Customer required!';
-$_['error_address']         = 'Payment address could not be found!';
-$_['error_firstname']       = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']        = 'Last Name must be between 1 and 32 characters!';
-$_['error_address_1']       = 'Address 1 must be between 3 and 128 characters!';
-$_['error_city']            = 'City must be between 2 and 128 characters!';
-$_['error_postcode']        = 'Postcode must be between 2 and 10 characters!';
-$_['error_country']         = 'Please select a country!';
-$_['error_zone']            = 'Please select a region / state!';
-$_['error_custom_field']    = '%s required!';
-$_['error_regex']           = '%s is not a valid input!';
+$_['error_customer']        = 'Kunde gesucht!';
+$_['error_address']         = 'Zahlungsadresse konnte nicht gefunden werden!';
+$_['error_firstname']       = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']        = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_address_1']       = 'Adresse 1 muss zwischen 3 und 128 Zeichen lang sein!';
+$_['error_city']            = 'Die Stadt muss zwischen 2 und 128 Zeichen lang sein!';
+$_['error_postcode']        = 'Die Postleitzahl muss zwischen 2 und 10 Zeichen lang sein!';
+$_['error_country']         = 'Bitte wählen Sie ein Land!';
+$_['error_zone']            = 'Bitte wählen Sie eine Region / ein Bundesland!';
+$_['error_custom_field']    = '%s erforderlich!';
+$_['error_regex']           = '%s ist keine gültige Eingabe!';

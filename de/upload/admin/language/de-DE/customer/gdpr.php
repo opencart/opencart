9ae769c7f873f5f406ed12731ece61b1cb473ae3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:14:21 +0800

    New translations gdpr.php (German)

--- de/upload/admin/language/de-DE/customer/gdpr.php
+++ de/upload/admin/language/de-DE/customer/gdpr.php
@@ -1,36 +1,36 @@
 <?php
 // Heading
-$_['heading_title']     = 'GDPR Approvals';
+$_['heading_title']     = 'DSGVO-Genehmigungen';
 
 // Text
-$_['text_success']      = 'Success: You have modified GDPR approvals!';
-$_['text_list']         = 'GDPR Approval List';
-$_['text_info']         = '<strong>GDPR</strong> account deletion requests will process after <strong>%s days</strong> so any fraud detection, chargebacks or refunds can be processed.';
-$_['text_approve']      = 'Approve';
-$_['text_deny']         = 'Deny';
-$_['text_delete']       = 'Delete';
-$_['text_unverified']   = 'Unverified';
-$_['text_pending']      = 'Pending';
-$_['text_processing']   = 'Processing';
-$_['text_complete']     = 'Complete';
-$_['text_denied']       = 'Denied';
+$_['text_success']      = 'Erfolg: Sie haben DSGVO-Genehmigungen geändert!';
+$_['text_list']         = 'DSGVO-Freigabeliste';
+$_['text_info']         = '<strong>DSGVO</strong> Anträge auf Kontolöschung werden nach <strong>%s Tagen</strong> bearbeitet, damit etwaige Betrugserkennungen, Rückbuchungen oder Rückerstattungen verarbeitet werden können.';
+$_['text_approve']      = 'Genehmigen';
+$_['text_deny']         = 'Leugnen';
+$_['text_delete']       = 'Löschen';
+$_['text_unverified']   = 'Nicht verifiziert';
+$_['text_pending']      = 'Ausstehend';
+$_['text_processing']   = 'Verarbeitung';
+$_['text_complete']     = 'Vollständig';
+$_['text_denied']       = 'Bestritten';
 $_['text_export']       = 'Export';
-$_['text_remove']       = 'Remove';
+$_['text_remove']       = 'Entfernen';
 $_['text_filter']       = 'Filter';
 
 // Column
 $_['column_email']      = 'E-Mail';
-$_['column_request']    = 'Request';
+$_['column_request']    = 'Anfrage';
 $_['column_status']     = 'Status';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_date_added'] = 'Datum hinzugefügt';
+$_['column_action']     = 'Aktion';
 
 // Entry
 $_['entry_email']       = 'E-Mail';
-$_['entry_action']      = 'Action';
+$_['entry_action']      = 'Aktion';
 $_['entry_status']      = 'Status';
-$_['entry_date_from']   = 'Date From';
-$_['entry_date_to']     = 'Date To';
+$_['entry_date_from']   = 'Stammen aus';
+$_['entry_date_to']     = 'Datum bis';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify GDPR approvals!';
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, DSGVO-Genehmigungen zu ändern!';

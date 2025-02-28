70f4d9362670ca73767539187c58442871e693a1
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:31 +0800

    New translations api.php (German)

--- de/upload/admin/language/de-DE/user/api.php
+++ de/upload/admin/language/de-DE/user/api.php
@@ -3,30 +3,30 @@
 $_['heading_title']        = 'APIs';
 
 // Text
-$_['text_success']         = 'Success: You have modified APIs!';
-$_['text_list']            = 'API List';
-$_['text_add']             = 'Add API';
-$_['text_edit']            = 'Edit API';
-$_['text_ip']              = 'Below you can create a list of IP\'s allowed to access the API. Your current IP is %s';
-$_['text_history']         = 'History';
+$_['text_success']         = 'Erfolg: Sie haben APIs geändert!';
+$_['text_list']            = 'API-Liste';
+$_['text_add']             = 'API hinzufügen';
+$_['text_edit']            = 'API bearbeiten';
+$_['text_ip']              = 'Unten können Sie eine Liste der IPs erstellen, die auf die API zugreifen dürfen. Ihre aktuelle IP ist %s';
+$_['text_history']         = 'Geschichte';
 
 // Column
-$_['column_username']      = 'API Username';
+$_['column_username']      = 'API-Benutzername';
 $_['column_status']        = 'Status';
 $_['column_ip']            = 'IP';
-$_['column_call']          = 'Call';
-$_['column_date_added']    = 'Date Added';
-$_['column_date_modified'] = 'Date modified';
-$_['column_action']        = 'Action';
+$_['column_call']          = 'Anruf';
+$_['column_date_added']    = 'Datum hinzugefügt';
+$_['column_date_modified'] = 'Änderungsdatum';
+$_['column_action']        = 'Aktion';
 
 // Entry
-$_['entry_username']       = 'API Username';
-$_['entry_key']            = 'API Key';
+$_['entry_username']       = 'API-Benutzername';
+$_['entry_key']            = 'API-Schlüssel';
 $_['entry_status']         = 'Status';
 $_['entry_ip']             = 'IP';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modified APIs!';
-$_['error_username']       = 'API Username must be between 3 and 20 characters!';
-$_['error_key']            = 'API Key must be between 64 and 256 characters!';
-$_['error_ip']             = 'You must have at least one IP added to the allowed list!';
+$_['error_permission']     = 'Achtung: Sie haben keine Berechtigung, APIs zu ändern!';
+$_['error_username']       = 'Der API-Benutzername muss zwischen 3 und 20 Zeichen lang sein!';
+$_['error_key']            = 'Der API-Schlüssel muss zwischen 64 und 256 Zeichen lang sein!';
+$_['error_ip']             = 'Sie müssen mindestens eine IP zur Zulassungsliste hinzugefügt haben!';

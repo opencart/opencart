ac7d0bcf5d65774286dc1dacb1aaccedd32735a3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:41 +0800

    New translations search.php (German)

--- de/upload/catalog/language/de-DE/product/search.php
+++ de/upload/catalog/language/de-DE/product/search.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Search';
-$_['heading_tag']       = 'Tag - ';
+$_['heading_title']     = 'Suchen';
+$_['heading_tag']       = 'Etikett - ';
 
 // Text
-$_['text_search']       = 'Products meeting the search criteria';
-$_['text_keyword']      = 'Keywords';
-$_['text_category']     = 'All Categories';
-$_['text_sub_category'] = 'Search in subcategories';
-$_['text_no_results']   = 'There is no product that matches the search criteria.';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_sort']         = 'Sort By';
-$_['text_default']      = 'Default';
+$_['text_search']       = 'Produkte, die den Suchkriterien entsprechen';
+$_['text_keyword']      = 'Schlagwörter';
+$_['text_category']     = 'Alle Kategorien';
+$_['text_sub_category'] = 'Suche in Unterkategorien';
+$_['text_no_results']   = 'Es gibt kein Produkt, das den Suchkriterien entspricht.';
+$_['text_compare']      = 'Produktvergleich (%s)';
+$_['text_sort']         = 'Sortieren nach';
+$_['text_default']      = 'Standard';
 $_['text_name_asc']     = 'Name (A - Z)';
 $_['text_name_desc']    = 'Name (Z - A)';
-$_['text_price_asc']    = 'Price (Low &gt; High)';
-$_['text_price_desc']   = 'Price (High &gt; Low)';
-$_['text_rating_asc']   = 'Rating (Lowest)';
-$_['text_rating_desc']  = 'Rating (Highest)';
-$_['text_model_asc']    = 'Model (A - Z)';
-$_['text_model_desc']   = 'Model (Z - A)';
-$_['text_limit']        = 'Show';
+$_['text_price_asc']    = 'Preis (Niedrig &gt; Hoch)';
+$_['text_price_desc']   = 'Preis (Hoch &gt; Niedrig)';
+$_['text_rating_asc']   = 'Bewertung (niedrigste)';
+$_['text_rating_desc']  = 'Bewertung (Höchste)';
+$_['text_model_asc']    = 'Modell (A - Z)';
+$_['text_model_desc']   = 'Modell (Z - A)';
+$_['text_limit']        = 'Zeigen';
 
 // Entry
-$_['entry_search']      = 'Search Criteria';
-$_['entry_description'] = 'Search in product descriptions';
+$_['entry_search']      = 'Suchkriterien';
+$_['entry_description'] = 'Suche in Produktbeschreibungen';

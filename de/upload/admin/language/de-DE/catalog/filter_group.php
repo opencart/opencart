f373e758d3cf9fd75f30264ec0a11116a40db5c9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:13:41 +0800

    New translations filter_group.php (German)

--- de/upload/admin/language/de-DE/catalog/filter_group.php
+++ de/upload/admin/language/de-DE/catalog/filter_group.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filter Groups';
+$_['heading_title']     = 'Filtergruppen';
 
 // Text
-$_['text_success']      = 'Success: You have modified filter groups!';
-$_['text_list']         = 'Filter Group List';
-$_['text_add']          = 'Add Filter Group';
-$_['text_edit']         = 'Edit Filter Group';
+$_['text_success']      = 'Erfolg: Sie haben Filtergruppen geändert!';
+$_['text_list']         = 'Filtergruppenliste';
+$_['text_add']          = 'Filtergruppe hinzufügen';
+$_['text_edit']         = 'Filtergruppe bearbeiten';
 
 // Column
-$_['column_name']       = 'Filter Group Name';
-$_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_name']       = 'Filtergruppenname';
+$_['column_sort_order'] = 'Sortierreihenfolge';
+$_['column_action']     = 'Aktion';
 
 // Entry
-$_['entry_name']        = 'Filter Group Name';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_name']        = 'Filtergruppenname';
+$_['entry_sort_order']  = 'Sortierreihenfolge';
 
 // Error
-$_['error_warning']     = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']  = 'Warning: You do not have permission to modify filter groups!';
-$_['error_name']        = 'Filter Group Name must be between 1 and 64 characters!';
-$_['error_filter']      = 'Warning: This filter group cannot be deleted as it is currently assigned to %s filters!';
+$_['error_warning']     = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']  = 'Achtung: Sie haben keine Berechtigung Filtergruppen zu ändern!';
+$_['error_name']        = 'Der Filtergruppenname muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_filter']      = 'Achtung: Diese Filtergruppe kann nicht gelöscht werden, da ihr aktuell %s Filter zugewiesen sind!';

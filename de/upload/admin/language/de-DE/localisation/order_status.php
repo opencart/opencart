bc594fbbcf23544a016ded6148ae4aacf3bcde60
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:15:11 +0800

    New translations order_status.php (German)

--- de/upload/admin/language/de-DE/localisation/order_status.php
+++ de/upload/admin/language/de-DE/localisation/order_status.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']    = 'Order Statuses';
+$_['heading_title']    = 'Bestellstatus';
 
 // Text
-$_['text_success']     = 'Success: You have modified order statuses!';
-$_['text_list']        = 'Order Status List';
-$_['text_add']         = 'Add Order Status';
-$_['text_edit']        = 'Edit Order Status';
+$_['text_success']     = 'Erfolg: Sie haben den Bestellstatus geändert!';
+$_['text_list']        = 'Bestellstatusliste';
+$_['text_add']         = 'Bestellstatus hinzufügen';
+$_['text_edit']        = 'Bestellstatus bearbeiten';
 
 // Column
-$_['column_name']      = 'Order Status Name';
-$_['column_action']    = 'Action';
+$_['column_name']      = 'Bestellstatusname';
+$_['column_action']    = 'Aktion';
 
 // Entry
-$_['entry_name']       = 'Order Status Name';
+$_['entry_name']       = 'Bestellstatusname';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify order statuses!';
-$_['error_name']       = 'Order Status Name must be between 3 and 32 characters!';
-$_['error_default']    = 'Warning: This order status cannot be deleted as it is currently assigned as the default store order status!';
-$_['error_download']   = 'Warning: This order status cannot be deleted as it is currently assigned as the default download status!';
-$_['error_order']      = 'Warning: This order status cannot be deleted as it is currently assigned to %s orders!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Bestellstatus zu ändern!';
+$_['error_name']       = 'Der Name des Bestellstatus muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_default']    = 'Achtung: Dieser Bestellstatus kann nicht gelöscht werden, da er aktuell als Standardbestellstatus des Shops zugewiesen ist!';
+$_['error_download']   = 'Achtung: Dieser Bestellstatus kann nicht gelöscht werden, da er aktuell als Standard-Download-Status zugewiesen ist!';
+$_['error_order']      = 'Achtung: Dieser Bestellstatus kann nicht gelöscht werden, da er aktuell %s Bestellungen zugewiesen ist!';

b45ea88b9fb1990a9513ab3743fa3ef8c5857d1e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:24 +0800

    New translations register.php (German)

--- de/upload/catalog/language/de-DE/mail/register.php
+++ de/upload/catalog/language/de-DE/mail/register.php
@@ -1,18 +1,18 @@
 <?php
 // Text
-$_['text_subject']        = '%s - Thank you for registering';
-$_['text_welcome']        = 'Welcome and thank you for registering at %s!';
-$_['text_login']          = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_service']        = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_thanks']         = 'Thanks,';
-$_['text_new_customer']   = 'New customer';
-$_['text_signup']         = 'A new customer has signed up:';
-$_['text_customer_group'] = 'Customer Group:';
-$_['text_firstname']      = 'First Name:';
-$_['text_lastname']       = 'Last Name:';
+$_['text_subject']        = '%s - Vielen Dank für die Registrierung';
+$_['text_welcome']        = 'Willkommen und danke für Ihre Registrierung bei %s!';
+$_['text_login']          = 'Ihr Konto wurde nun erstellt und Sie können sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website besuchen oder die folgende URL aufrufen:';
+$_['text_approval']       = 'Ihr Konto muss genehmigt werden, bevor Sie sich anmelden können. Nach der Genehmigung können Sie sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website oder die folgende URL besuchen:';
+$_['text_service']        = 'Nach der Anmeldung haben Sie Zugriff auf weitere Dienste. Sie können beispielsweise frühere Bestellungen prüfen, Rechnungen ausdrucken und Ihre Kontoinformationen bearbeiten.';
+$_['text_thanks']         = 'Danke,';
+$_['text_new_customer']   = 'Neuer Kunde';
+$_['text_signup']         = 'Ein neuer Kunde hat sich angemeldet:';
+$_['text_customer_group'] = 'Kundengruppe:';
+$_['text_firstname']      = 'Vorname:';
+$_['text_lastname']       = 'Nachname:';
 $_['text_email']          = 'E-Mail:';
-$_['text_telephone']      = 'Telephone:';
+$_['text_telephone']      = 'Telefon:';
 
 // Button
 $_['button_login']        = 'Login';

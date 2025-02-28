e550c928995d0c6bf9ccee523240c8d14ef506b4
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:14:25 +0800

    New translations layout.php (German)

--- de/upload/admin/language/de-DE/design/layout.php
+++ de/upload/admin/language/de-DE/design/layout.php
@@ -3,36 +3,36 @@
 $_['heading_title']       = 'Layouts';
 
 // Text
-$_['text_success']        = 'Success: You have modified layouts!';
-$_['text_list']           = 'Layout List';
-$_['text_add']            = 'Add Layout';
-$_['text_edit']           = 'Edit Layout';
-$_['text_remove']         = 'Remove';
-$_['text_route']          = 'Choose the store and routes to be used with this layout';
-$_['text_module']         = 'Choose the position of the modules';
-$_['text_default']        = 'Default';
-$_['text_content_top']    = 'Content Top';
-$_['text_content_bottom'] = 'Content Bottom';
-$_['text_column_left']    = 'Column Left';
-$_['text_column_right']   = 'Column Right';
+$_['text_success']        = 'Erfolg: Sie haben Layouts geändert!';
+$_['text_list']           = 'Layoutliste';
+$_['text_add']            = 'Layout hinzufügen';
+$_['text_edit']           = 'Layout bearbeiten';
+$_['text_remove']         = 'Entfernen';
+$_['text_route']          = 'Wählen Sie das Geschäft und die Routen aus, die mit diesem Layout verwendet werden sollen';
+$_['text_module']         = 'Wählen Sie die Position der Module';
+$_['text_default']        = 'Standard';
+$_['text_content_top']    = 'Inhalt Top';
+$_['text_content_bottom'] = 'Inhalt unten';
+$_['text_column_left']    = 'Spalte links';
+$_['text_column_right']   = 'Spalte rechts';
 
 // Column
-$_['column_name']         = 'Layout Name';
-$_['column_action']       = 'Action';
+$_['column_name']         = 'Layoutname';
+$_['column_action']       = 'Aktion';
 
 // Entry
-$_['entry_name']          = 'Layout Name';
-$_['entry_store']         = 'Store';
+$_['entry_name']          = 'Layoutname';
+$_['entry_store']         = 'Speichern';
 $_['entry_route']         = 'Route';
-$_['entry_module']        = 'Module';
+$_['entry_module']        = 'Modul';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify layouts!';
-$_['error_name']          = 'Layout Name must be between 3 and 64 characters!';
-$_['error_module']        = 'Module required!';
-$_['error_default']       = 'Warning: This layout cannot be deleted as it is currently assigned as the default store layout!';
-$_['error_store']         = 'Warning: This layout cannot be deleted as it is currently assigned to %s stores!';
-$_['error_product']       = 'Warning: This layout cannot be deleted as it is currently assigned to %s products!';
-$_['error_category']      = 'Warning: This layout cannot be deleted as it is currently assigned to %s categories!';
-$_['error_manufacturer']  = 'Warning: This layout cannot be deleted as it is currently assigned to %s manufacturers!';
-$_['error_information']   = 'Warning: This layout cannot be deleted as it is currently assigned to %s information pages!';
+$_['error_permission']    = 'Achtung: Sie sind nicht berechtigt, Layouts zu ändern!';
+$_['error_name']          = 'Der Layoutname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_module']        = 'Modul erforderlich!';
+$_['error_default']       = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell als Standard-Shop-Layout zugewiesen ist!';
+$_['error_store']         = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Stores zugewiesen ist!';
+$_['error_product']       = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Produkten zugewiesen ist!';
+$_['error_category']      = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Kategorien zugeordnet ist!';
+$_['error_manufacturer']  = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Herstellern zugeordnet ist!';
+$_['error_information']   = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Informationsseiten zugeordnet ist!';

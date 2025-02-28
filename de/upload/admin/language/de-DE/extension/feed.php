5bbd577c6068d32124932ce0919339f092f7c059
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 05:10:39 +0800

    New translations feed.php (German)

---
+++ de/upload/admin/language/de-DE/extension/feed.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'Feeds';
+
+// Text
+$_['text_success']     = 'Success: You have modified feeds!';
+$_['text_list']        = 'Feed List';
+
+// Column
+$_['column_name']      = 'Product Feed Name';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Action';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify feeds!';
+$_['error_extension']  = 'Warning: Extension does not exist!';

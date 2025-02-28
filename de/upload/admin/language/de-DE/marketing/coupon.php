50f2182369e24e15bc8a7e5d6960c36c11f36d28
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:15:53 +0800

    New translations coupon.php (German)

--- de/upload/admin/language/de-DE/marketing/coupon.php
+++ de/upload/admin/language/de-DE/marketing/coupon.php
@@ -1,56 +1,56 @@
 <?php
 // Heading
-$_['heading_title']       = 'Coupons';
+$_['heading_title']       = 'Gutscheine';
 
 // Text
-$_['text_success']        = 'Success: You have modified coupons!';
-$_['text_list']           = 'Coupon List';
-$_['text_add']            = 'Add Coupon';
-$_['text_edit']           = 'Edit Coupon';
-$_['text_percent']        = 'Percentage';
-$_['text_amount']         = 'Fixed Amount';
-$_['text_coupon']         = 'History';
+$_['text_success']        = 'Erfolg: Sie haben Coupons geändert!';
+$_['text_list']           = 'Gutscheinliste';
+$_['text_add']            = 'Gutschein hinzufügen';
+$_['text_edit']           = 'Gutschein bearbeiten';
+$_['text_percent']        = 'Prozentsatz';
+$_['text_amount']         = 'Fester Betrag';
+$_['text_coupon']         = 'Geschichte';
 
 // Column
-$_['column_name']         = 'Coupon Name';
+$_['column_name']         = 'Gutscheinname';
 $_['column_code']         = 'Code';
-$_['column_discount']     = 'Discount';
-$_['column_date_start']   = 'Date Start';
-$_['column_date_end']     = 'Date End';
+$_['column_discount']     = 'Rabatt';
+$_['column_date_start']   = 'Datum Beginn';
+$_['column_date_end']     = 'Datum Ende';
 $_['column_status']       = 'Status';
-$_['column_order_id']     = 'Order ID';
-$_['column_customer']     = 'Customer';
-$_['column_amount']       = 'Amount';
-$_['column_date_added']   = 'Date Added';
-$_['column_action']       = 'Action';
+$_['column_order_id']     = 'Bestellnummer';
+$_['column_customer']     = 'Kunde';
+$_['column_amount']       = 'Menge';
+$_['column_date_added']   = 'Datum hinzugefügt';
+$_['column_action']       = 'Aktion';
 
 // Entry
-$_['entry_name']          = 'Coupon Name';
+$_['entry_name']          = 'Gutscheinname';
 $_['entry_code']          = 'Code';
-$_['entry_type']          = 'Type';
-$_['entry_discount']      = 'Discount';
-$_['entry_logged']        = 'Customer Login';
-$_['entry_shipping']      = 'Free Shipping';
-$_['entry_total']         = 'Total Amount';
-$_['entry_category']      = 'Category';
-$_['entry_product']       = 'Products';
-$_['entry_date']          = 'Dates';
-$_['entry_uses_total']    = 'Uses Per Coupon';
-$_['entry_uses_customer'] = 'Uses Per Customer';
+$_['entry_type']          = 'Typ';
+$_['entry_discount']      = 'Rabatt';
+$_['entry_logged']        = 'Kunden-Login';
+$_['entry_shipping']      = 'Kostenloser Versand';
+$_['entry_total']         = 'Gesamtbetrag';
+$_['entry_category']      = 'Kategorie';
+$_['entry_product']       = 'Produkte';
+$_['entry_date']          = 'Termine';
+$_['entry_uses_total']    = 'Verwendungen pro Coupon';
+$_['entry_uses_customer'] = 'Verwendungen pro Kunde';
 $_['entry_status']        = 'Status';
 
 // Help
-$_['help_code']           = 'The code the customer enters to get the discount.';
-$_['help_type']           = 'Percentage or Fixed Amount.';
-$_['help_logged']         = 'Customer must be logged in to use the coupon.';
-$_['help_total']          = 'The total amount that must be reached before the coupon is valid.';
-$_['help_category']       = 'Choose all products under selected category.';
-$_['help_product']        = 'Choose specific products the coupon will apply to. Select no products to apply coupon to entire cart.';
-$_['help_uses_total']     = 'The maximum number of times the coupon can be used by any customer. Leave blank for unlimited';
-$_['help_uses_customer']  = 'The maximum number of times the coupon can be used by a single customer. Leave blank for unlimited';
+$_['help_code']           = 'Der Code, den der Kunde eingibt, um den Rabatt zu erhalten.';
+$_['help_type']           = 'Prozentsatz oder Festbetrag.';
+$_['help_logged']         = 'Der Kunde muss angemeldet sein, um den Gutschein nutzen zu können.';
+$_['help_total']          = 'Der Gesamtbetrag, der erreicht werden muss, bevor der Gutschein gültig ist.';
+$_['help_category']       = 'Wählen Sie alle Produkte unter der ausgewählten Kategorie aus.';
+$_['help_product']        = 'Wählen Sie bestimmte Produkte aus, für die der Gutschein gültig sein soll. Wählen Sie keine Produkte aus, um den Gutschein auf den gesamten Warenkorb anzuwenden.';
+$_['help_uses_total']     = 'Die maximale Anzahl von Malen, die der Gutschein von einem Kunden verwendet werden kann. Für unbegrenzt leer lassen';
+$_['help_uses_customer']  = 'Die maximale Anzahl der Gutscheineinlösungen durch einen einzelnen Kunden. Für unbegrenzte Einlösung leer lassen';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify coupons!';
-$_['error_exists']        = 'Warning: Coupon code is already in use!';
-$_['error_name']          = 'Coupon Name must be between 3 and 128 characters!';
-$_['error_code']          = 'Code must be between 3 and 20 characters!';
+$_['error_permission']    = 'Achtung: Sie sind nicht berechtigt, Gutscheine zu ändern!';
+$_['error_exists']        = 'Achtung: Der Gutscheincode wird bereits verwendet!';
+$_['error_name']          = 'Der Gutscheinname muss zwischen 3 und 128 Zeichen lang sein!';
+$_['error_code']          = 'Der Code muss zwischen 3 und 20 Zeichen lang sein!';

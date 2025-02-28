1e2295885749e2a87d1506fd176adb3fa01437de
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:25 +0800

    New translations store.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/module/store.php
+++ de/upload/extension/opencart/admin/language/de-DE/module/store.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Store';
+$_['heading_title']    = 'Speichern';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified store module!';
-$_['text_edit']        = 'Edit Store Module';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das Store-Modul geändert!';
+$_['text_edit']        = 'Store-Modul bearbeiten';
 
 // Entry
-$_['entry_admin']      = 'Admin Users Only';
+$_['entry_admin']      = 'Nur Administratorbenutzer';
 $_['entry_status']     = 'Status';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify store module!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, das Store-Modul zu ändern!';

1ad1928144b6ab0739e6f0e6ef26edd11435d773
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:15:27 +0800

    New translations affiliate_approve.php (German)

--- de/upload/admin/language/de-DE/mail/affiliate_approve.php
+++ de/upload/admin/language/de-DE/mail/affiliate_approve.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_subject']  = '%s - Your affiliate account has been activated!';
-$_['text_welcome']  = 'Welcome and thank you for registering at %s!';
-$_['text_login']    = 'Your account has now been approved and you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_service']  = 'Upon logging in, you will be able to generate tracking codes, track commission payments and edit your account information.';
-$_['text_thanks']   = 'Thanks,';
+$_['text_subject']  = '%s – Ihr Partnerkonto wurde aktiviert!';
+$_['text_welcome']  = 'Willkommen und danke für Ihre Registrierung bei %s!';
+$_['text_login']    = 'Ihr Konto wurde nun genehmigt und Sie können sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website besuchen oder die folgende URL aufrufen:';
+$_['text_service']  = 'Nach der Anmeldung können Sie Tracking-Codes generieren, Provisionszahlungen verfolgen und Ihre Kontoinformationen bearbeiten.';
+$_['text_thanks']   = 'Danke,';

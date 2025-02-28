d370288898af223eca503661128aad5c59c1dd77
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:45 +0800

    New translations product_purchased.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/report/product_purchased.php
+++ de/upload/extension/opencart/admin/language/de-DE/report/product_purchased.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']      = 'Products Purchased Report';
+$_['heading_title']      = 'Bericht zu gekauften Produkten';
 
 // Text
-$_['text_extension']     = 'Extensions';
-$_['text_edit']          = 'Edit Products Purchased Report';
-$_['text_success']       = 'Success: You have modified products purchased report!';
+$_['text_extension']     = 'Erweiterungen';
+$_['text_edit']          = 'Bericht „Gekaufte Produkte“ bearbeiten';
+$_['text_success']       = 'Erfolg: Sie haben den Bericht über gekaufte Produkte geändert!';
 $_['text_filter']        = 'Filter';
-$_['text_all_status']    = 'All Statuses';
+$_['text_all_status']    = 'Alle Status';
 
 // Column
-$_['column_date_start']  = 'Date Start';
-$_['column_date_end']    = 'Date End';
-$_['column_name']        = 'Product Name';
-$_['column_model']       = 'Model';
-$_['column_quantity']    = 'Quantity';
-$_['column_total']       = 'Total';
+$_['column_date_start']  = 'Datum Beginn';
+$_['column_date_end']    = 'Datum Ende';
+$_['column_name']        = 'Produktname';
+$_['column_model']       = 'Modell';
+$_['column_quantity']    = 'Menge';
+$_['column_total']       = 'Gesamt';
 
 // Entry
-$_['entry_date_start']   = 'Date Start';
-$_['entry_date_end']     = 'Date End';
-$_['entry_order_status'] = 'Order Status';
+$_['entry_date_start']   = 'Datum Beginn';
+$_['entry_date_end']     = 'Datum Ende';
+$_['entry_order_status'] = 'Bestellstatus';
 $_['entry_status']       = 'Status';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_sort_order']   = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify products purchased report!';
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, den Bericht über gekaufte Produkte zu ändern!';

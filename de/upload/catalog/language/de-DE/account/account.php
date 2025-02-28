5da7468960dc2bd93fece577ec354471841ecd49
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:37 +0800

    New translations account.php (German)

--- de/upload/catalog/language/de-DE/account/account.php
+++ de/upload/catalog/language/de-DE/account/account.php
@@ -1,26 +1,26 @@
 <?php
 // Heading
-$_['heading_title']       = 'My Account';
+$_['heading_title']       = 'Mein Konto';
 
 // Text
-$_['text_account']        = 'Account';
-$_['text_my_account']     = 'My Account';
-$_['text_my_orders']      = 'My Orders';
-$_['text_my_affiliate']   = 'My Affiliate Account';
+$_['text_account']        = 'Konto';
+$_['text_my_account']     = 'Mein Konto';
+$_['text_my_orders']      = 'Meine Bestellungen';
+$_['text_my_affiliate']   = 'Mein Affiliate-Konto';
 $_['text_my_newsletter']  = 'Newsletter';
-$_['text_edit']           = 'Edit your account information';
-$_['text_password']       = 'Change your password';
-$_['text_address']        = 'Modify your address book entries';
-$_['text_payment_method'] = 'Stored payment methods';
-$_['text_wishlist']       = 'Modify your wish list';
-$_['text_order']          = 'View your order history';
-$_['text_subscription']   = 'Subscriptions';
+$_['text_edit']           = 'Bearbeiten Sie Ihre Kontoinformationen';
+$_['text_password']       = 'Ändern Sie Ihr Passwort';
+$_['text_address']        = 'Adressbucheinträge ändern';
+$_['text_payment_method'] = 'Gespeicherte Zahlungsarten';
+$_['text_wishlist']       = 'Ändern Sie Ihre Wunschliste';
+$_['text_order']          = 'Zeigen Sie Ihren Bestellverlauf an';
+$_['text_subscription']   = 'Abonnements';
 $_['text_download']       = 'Downloads';
-$_['text_reward']         = 'Your Reward Points';
-$_['text_return']         = 'View your return requests';
-$_['text_transaction']    = 'Your Transactions';
-$_['text_newsletter']     = 'Subscribe / unsubscribe to newsletter';
-$_['text_transactions']   = 'Transactions';
-$_['text_affiliate_add']  = 'Register for an affiliate account';
-$_['text_affiliate_edit'] = 'Edit your affiliate information';
-$_['text_tracking']       = 'Custom Affiliate Tracking Code';
+$_['text_reward']         = 'Ihre Prämienpunkte';
+$_['text_return']         = 'Zeigen Sie Ihre Rückgabeanfragen an';
+$_['text_transaction']    = 'Ihre Transaktionen';
+$_['text_newsletter']     = 'Newsletter abonnieren/abbestellen';
+$_['text_transactions']   = 'Transaktionen';
+$_['text_affiliate_add']  = 'Registrieren Sie sich für ein Affiliate-Konto';
+$_['text_affiliate_edit'] = 'Bearbeiten Sie Ihre Partnerinformationen';
+$_['text_tracking']       = 'Benutzerdefinierter Affiliate-Tracking-Code';

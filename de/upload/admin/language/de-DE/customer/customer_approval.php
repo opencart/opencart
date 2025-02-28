a8026b740e56581376bb321b34d06b0f768444ce
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:14:18 +0800

    New translations customer_approval.php (German)

--- de/upload/admin/language/de-DE/customer/customer_approval.php
+++ de/upload/admin/language/de-DE/customer/customer_approval.php
@@ -1,32 +1,32 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Approvals';
+$_['heading_title']         = 'Kundenzulassungen';
 
 // Text
-$_['text_success']          = 'Success: You have modified customer approvals!';
-$_['text_list']             = 'Customer Approval List';
-$_['text_default']          = 'Default';
-$_['text_customer']         = 'Customer';
-$_['text_affiliate']        = 'Affiliate';
+$_['text_success']          = 'Erfolg: Sie haben Kundenfreigaben geändert!';
+$_['text_list']             = 'Kundengenehmigungsliste';
+$_['text_default']          = 'Standard';
+$_['text_customer']         = 'Kunde';
+$_['text_affiliate']        = 'Partnerprogramm';
 $_['text_filter']           = 'Filter';
-$_['text_approve']          = 'Approve';
-$_['text_deny']             = 'Deny';
+$_['text_approve']          = 'Genehmigen';
+$_['text_deny']             = 'Leugnen';
 
 // Column
-$_['column_customer']       = 'Customer';
+$_['column_customer']       = 'Kunde';
 $_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
-$_['column_type']           = 'Type';
-$_['column_date_added']     = 'Date Added';
-$_['column_action']         = 'Action';
+$_['column_customer_group'] = 'Kundengruppe';
+$_['column_type']           = 'Typ';
+$_['column_date_added']     = 'Datum hinzugefügt';
+$_['column_action']         = 'Aktion';
 
 // Entry
-$_['entry_customer']        = 'Customer';
+$_['entry_customer']        = 'Kunde';
 $_['entry_email']           = 'E-Mail';
-$_['entry_customer_group']  = 'Customer Group';
-$_['entry_type']            = 'Type';
-$_['entry_date_from']       = 'Date From';
-$_['entry_date_to']         = 'Date To';
+$_['entry_customer_group']  = 'Kundengruppe';
+$_['entry_type']            = 'Typ';
+$_['entry_date_from']       = 'Stammen aus';
+$_['entry_date_to']         = 'Datum bis';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify customer approvals!';
+$_['error_permission']      = 'Achtung: Sie sind nicht berechtigt, Kundenfreigaben zu ändern!';

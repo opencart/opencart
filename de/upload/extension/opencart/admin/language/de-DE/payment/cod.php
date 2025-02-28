23b3eef619eece3f8e47b7893a85f6e2bcda408d
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:31 +0800

    New translations cod.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/payment/cod.php
+++ de/upload/extension/opencart/admin/language/de-DE/payment/cod.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']      = 'Cash On Delivery';
+$_['heading_title']      = 'Nachnahme';
 
 // Text
-$_['text_extension']     = 'Extensions';
-$_['text_success']       = 'Success: You have modified cash on delivery payment module!';
-$_['text_edit']          = 'Edit Cash On Delivery';
+$_['text_extension']     = 'Erweiterungen';
+$_['text_success']       = 'Erfolg: Sie haben das Zahlungsmodul „Nachnahme“ geändert!';
+$_['text_edit']          = 'Nachnahme bearbeiten';
 
 // Entry
-$_['entry_order_status'] = 'Order Status';
-$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_order_status'] = 'Bestellstatus';
+$_['entry_geo_zone']     = 'Geozone';
 $_['entry_status']       = 'Status';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_sort_order']   = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment cash on delivery!';
+$_['error_permission']   = 'Achtung: Sie besitzen keine Berechtigung die Zahlungsart Nachnahme zu ändern!';

3e98a6f28fd0a68984363f7330b9e3fef2bc4106
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:39 +0800

    New translations customer_reward.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/report/customer_reward.php
+++ de/upload/extension/opencart/admin/language/de-DE/report/customer_reward.php
@@ -1,29 +1,29 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Reward Points Report';
+$_['heading_title']         = 'Bericht zu Kundenprämienpunkten';
 
 // Text
-$_['text_extension']        = 'Extensions';
-$_['text_edit']             = 'Edit Customer Reward Points Report';
-$_['text_success']          = 'Success: You have modified customer reward points report!';
+$_['text_extension']        = 'Erweiterungen';
+$_['text_edit']             = 'Bericht zu Kundenprämienpunkten bearbeiten';
+$_['text_success']          = 'Erfolg: Sie haben den Bericht zu Kundenprämienpunkten geändert!';
 $_['text_filter']           = 'Filter';
 
 // Column
-$_['column_customer']       = 'Customer Name';
+$_['column_customer']       = 'Kundenname';
 $_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
+$_['column_customer_group'] = 'Kundengruppe';
 $_['column_status']         = 'Status';
-$_['column_points']         = 'Reward Points';
-$_['column_orders']         = 'No. Orders';
-$_['column_total']          = 'Total';
-$_['column_action']         = 'Action';
+$_['column_points']         = 'Belohnungspunkte';
+$_['column_orders']         = 'Anzahl Bestellungen';
+$_['column_total']          = 'Gesamt';
+$_['column_action']         = 'Aktion';
 
 // Entry
-$_['entry_date_start']      = 'Date Start';
-$_['entry_date_end']        = 'Date End';
-$_['entry_customer']        = 'Customer';
+$_['entry_date_start']      = 'Datum Beginn';
+$_['entry_date_end']        = 'Datum Ende';
+$_['entry_customer']        = 'Kunde';
 $_['entry_status']          = 'Status';
-$_['entry_sort_order']      = 'Sort Order';
+$_['entry_sort_order']      = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify customer reward points report!';
+$_['error_permission']      = 'Achtung: Sie sind nicht berechtigt, den Bericht zu Kundenprämienpunkten zu ändern!';

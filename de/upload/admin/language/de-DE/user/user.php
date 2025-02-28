166c86aa1225d6293f63ae78a2cdefb0664331fc
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:34 +0800

    New translations user.php (German)

--- de/upload/admin/language/de-DE/user/user.php
+++ de/upload/admin/language/de-DE/user/user.php
@@ -1,64 +1,64 @@
 <?php
 // Heading
-$_['heading_title']            = 'Users';
+$_['heading_title']            = 'Benutzer';
 
 // Text
-$_['text_success']             = 'Success: You have modified users!';
-$_['text_list']                = 'User List';
-$_['text_add']                 = 'Add User';
-$_['text_edit']                = 'Edit User';
-$_['text_user']                = 'Users Details';
-$_['text_password']            = 'Password';
-$_['text_other']               = 'Other';
-$_['text_login']               = 'Login History';
-$_['text_authorize']           = 'Authorize History';
+$_['text_success']             = 'Erfolg: Sie haben Benutzer geändert!';
+$_['text_list']                = 'Benutzerliste';
+$_['text_add']                 = 'Benutzer hinzufügen';
+$_['text_edit']                = 'Benutzer bearbeiten';
+$_['text_user']                = 'Benutzerdetails';
+$_['text_password']            = 'Passwort';
+$_['text_other']               = 'Andere';
+$_['text_login']               = 'Anmeldeverlauf';
+$_['text_authorize']           = 'Autorisierungsverlauf';
 $_['text_filter']              = 'Filter';
 
 // Column
-$_['column_username']          = 'Username';
+$_['column_username']          = 'Benutzername';
 $_['column_name']              = 'Name';
 $_['column_email']             = 'E-Mail';
-$_['column_user_group']        = 'User Group';
+$_['column_user_group']        = 'Benutzergruppe';
 $_['column_status']            = 'Status';
 $_['column_ip']                = 'IP';
-$_['column_user_agent']        = 'User Agent';
-$_['column_date_added']        = 'Date Added';
-$_['column_date_expire']       = 'Date Expire';
-$_['column_action']            = 'Action';
+$_['column_user_agent']        = 'Benutzeragent';
+$_['column_date_added']        = 'Datum hinzugefügt';
+$_['column_date_expire']       = 'Ablaufdatum';
+$_['column_action']            = 'Aktion';
 
 // Entry
-$_['entry_username']           = 'Username';
-$_['entry_user_group']         = 'User Group';
-$_['entry_password']           = 'Password';
-$_['entry_confirm']            = 'Confirm';
-$_['entry_firstname']          = 'First Name';
-$_['entry_lastname']           = 'Last Name';
+$_['entry_username']           = 'Benutzername';
+$_['entry_user_group']         = 'Benutzergruppe';
+$_['entry_password']           = 'Passwort';
+$_['entry_confirm']            = 'Bestätigen';
+$_['entry_firstname']          = 'Vorname';
+$_['entry_lastname']           = 'Nachname';
 $_['entry_email']              = 'E-Mail';
-$_['entry_image']              = 'Image';
+$_['entry_image']              = 'Bild';
 $_['entry_status']             = 'Status';
 $_['entry_name']               = 'Name';
 $_['entry_ip']                 = 'IP';
-$_['entry_date_from']          = 'Date From';
-$_['entry_date_to']            = 'Date To';
+$_['entry_date_from']          = 'Stammen aus';
+$_['entry_date_to']            = 'Datum bis';
 
 // Tabs
-$_['tab_authorize']            = 'Authorize';
+$_['tab_authorize']            = 'Autorisieren';
 $_['tab_login']                = 'Login';
 
 // Error
-$_['error_permission']         = 'Warning: You do not have permission to modify users!';
-$_['error_account']            = 'Warning: You cannot delete your own account!';
-$_['error_login']              = 'Warning: User login does not exist!';
-$_['error_username']           = 'Username must be between 3 and 20 characters!';
-$_['error_username_exists']    = 'Warning: Username is already in use!';
-$_['error_firstname']          = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']           = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']              = 'E-Mail Address does not appear to be valid!';
-$_['error_email_exists']       = 'Warning: E-Mail Address is already registered!';
-$_['error_password']           = 'Password must contain a %s and be between %d and 20 characters!';
-$_['error_password_uppercase'] = 'uppercase';
-$_['error_password_lowercase'] = 'lowercase';
-$_['error_password_number']    = 'number';
-$_['error_password_symbol']    = 'symbol';
-$_['error_password_length']    = 'Password must be between %d and 20 characters!';
-$_['error_confirm']            = 'Password and password confirmation do not match!';
+$_['error_permission']         = 'Achtung: Sie sind nicht berechtigt, Benutzer zu ändern!';
+$_['error_account']            = 'Achtung: Sie können Ihr eigenes Konto nicht löschen!';
+$_['error_login']              = 'Achtung: Benutzer-Login existiert nicht!';
+$_['error_username']           = 'Der Benutzername muss zwischen 3 und 20 Zeichen lang sein!';
+$_['error_username_exists']    = 'Achtung: Der Benutzername wird bereits verwendet!';
+$_['error_firstname']          = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']           = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_email']              = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_email_exists']       = 'Achtung: E-Mail-Adresse ist bereits registriert!';
+$_['error_password']           = 'Das Passwort muss eine %s enthalten und zwischen %d und 20 Zeichen lang sein!';
+$_['error_password_uppercase'] = 'Großbuchstaben';
+$_['error_password_lowercase'] = 'Kleinbuchstaben';
+$_['error_password_number']    = 'Nummer';
+$_['error_password_symbol']    = 'Symbol';
+$_['error_password_length']    = 'Das Passwort muss zwischen %d und 20 Zeichen lang sein!';
+$_['error_confirm']            = 'Passwort und Passwortbestätigung stimmen nicht überein!';

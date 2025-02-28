8eb874a89e2014d482a23b0bd14580cab5f9f691
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:04 +0800

    New translations contact.php (German)

--- de/upload/catalog/language/de-DE/information/contact.php
+++ de/upload/catalog/language/de-DE/information/contact.php
@@ -1,26 +1,26 @@
 <?php
 // Heading
-$_['heading_title']  = 'Contact Us';
+$_['heading_title']  = 'Kontaktieren Sie uns';
 
 // Text
-$_['text_location']  = 'Our Locations';
-$_['text_store']     = 'Our Stores';
-$_['text_contact']   = 'Contact Form';
-$_['text_address']   = 'Address';
-$_['text_telephone'] = 'Telephone';
-$_['text_open']      = 'Opening Times';
-$_['text_comment']   = 'Comments';
-$_['text_message']   = '<p>Your enquiry has been successfully sent to the store owner!</p>';
+$_['text_location']  = 'Unsere Standorte';
+$_['text_store']     = 'Unsere Filialen';
+$_['text_contact']   = 'Kontaktformular';
+$_['text_address']   = 'Adresse';
+$_['text_telephone'] = 'Telefon';
+$_['text_open']      = 'Öffnungszeiten';
+$_['text_comment']   = 'Kommentare';
+$_['text_message']   = '<p>Ihre Anfrage wurde erfolgreich an den Shop-Inhaber gesendet!</p>';
 
 // Entry
-$_['entry_name']     = 'Your Name';
-$_['entry_email']    = 'E-Mail Address';
-$_['entry_enquiry']  = 'Enquiry';
+$_['entry_name']     = 'Ihr Name';
+$_['entry_email']    = 'E-Mail-Adresse';
+$_['entry_enquiry']  = 'Anfrage';
 
 // Email
-$_['email_subject']  = 'Enquiry %s';
+$_['email_subject']  = 'Anfrage %s';
 
 // Errors
-$_['error_name']     = 'Name must be between 3 and 32 characters!';
-$_['error_email']    = 'E-Mail Address does not appear to be valid!';
-$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
+$_['error_name']     = 'Der Name muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_email']    = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_enquiry']  = 'Die Anfrage muss zwischen 10 und 3000 Zeichen lang sein!';

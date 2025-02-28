c3165149ea5ef6c0e23afc6436930d8d4a6d5d4b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:36 +0800

    New translations shipping_method.php (German)

--- de/upload/catalog/language/de-DE/checkout/shipping_method.php
+++ de/upload/catalog/language/de-DE/checkout/shipping_method.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']          = 'Shipping Method';
+$_['heading_title']          = 'Versandart';
 
 // Text
-$_['text_shipping_method']   = 'Shipping method options';
-$_['text_shipping']          = 'Please select the preferred shipping method to use for this order.';
-$_['text_success']           = 'Success: You have changed shipping method!';
+$_['text_shipping_method']   = 'Optionen für Versandarten';
+$_['text_shipping']          = 'Bitte wählen Sie die bevorzugte Versandmethode für diese Bestellung aus.';
+$_['text_success']           = 'Erfolg: Sie haben die Versandart geändert!';
 
 // Entry
-$_['entry_shipping_method']  = 'Choose shipping method...';
+$_['entry_shipping_method']  = 'Versandart auswählen...';
 
 // Error
-$_['error_customer']         = 'Customer required!';
-$_['error_payment_address']  = 'Payment address required!';
-$_['error_shipping_address'] = 'Shipping address required!';
-$_['error_shipping_method']  = 'Shipping method required!';
-$_['error_no_shipping']      = 'No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
+$_['error_customer']         = 'Kunde gesucht!';
+$_['error_payment_address']  = 'Zahlungsadresse erforderlich!';
+$_['error_shipping_address'] = 'Lieferadresse erforderlich!';
+$_['error_shipping_method']  = 'Versandart erforderlich!';
+$_['error_no_shipping']      = 'Es sind keine Versandoptionen verfügbar. Bitte <a href="%s">kontaktieren Sie uns</a> für Hilfe!';

d864936f1a553894706cedc34240b84eccf0a265
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 04:15:56 +0800

    New translations report.php (English, United Kingdom)

---
+++ en-GB/upload/admin/language/en-gb/extension/report.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']     = 'Reports';
+
+// Text
+$_['text_success']      = 'Success: You have modified reports!';
+$_['text_list']         = 'Reports List';
+
+// Column
+$_['column_name']       = 'Report Name';
+$_['column_status']     = 'Status';
+$_['column_sort_order'] = 'Sort Order';
+$_['column_action']     = 'Action';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify reports!';
+$_['error_extension']   = 'Warning: Extension does not exist!';

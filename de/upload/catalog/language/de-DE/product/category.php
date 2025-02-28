8f464bb053fd0626ae59a3b31ccd70132450921a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:32 +0800

    New translations category.php (German)

--- de/upload/catalog/language/de-DE/product/category.php
+++ de/upload/catalog/language/de-DE/product/category.php
@@ -1,17 +1,17 @@
 <?php
 // Text
-$_['text_refine']      = 'Refine Search';
-$_['text_product']     = 'Products';
-$_['text_no_results']  = 'There are no products to list in this category.';
-$_['text_compare']     = 'Product Compare (%s)';
-$_['text_sort']        = 'Sort By';
-$_['text_default']     = 'Default';
+$_['text_refine']      = 'Suche verfeinern';
+$_['text_product']     = 'Produkte';
+$_['text_no_results']  = 'Es sind keine Produkte vorhanden, die in dieser Kategorie aufgelistet werden können.';
+$_['text_compare']     = 'Produktvergleich (%s)';
+$_['text_sort']        = 'Sortieren nach';
+$_['text_default']     = 'Standard';
 $_['text_name_asc']    = 'Name (A - Z)';
 $_['text_name_desc']   = 'Name (Z - A)';
-$_['text_price_asc']   = 'Price (Low &gt; High)';
-$_['text_price_desc']  = 'Price (High &gt; Low)';
-$_['text_rating_asc']  = 'Rating (Lowest)';
-$_['text_rating_desc'] = 'Rating (Highest)';
-$_['text_model_asc']   = 'Model (A - Z)';
-$_['text_model_desc']  = 'Model (Z - A)';
-$_['text_limit']       = 'Show';
+$_['text_price_asc']   = 'Preis (Niedrig &gt; Hoch)';
+$_['text_price_desc']  = 'Preis (Hoch &gt; Niedrig)';
+$_['text_rating_asc']  = 'Bewertung (niedrigste)';
+$_['text_rating_desc'] = 'Bewertung (Höchste)';
+$_['text_model_asc']   = 'Modell (A - Z)';
+$_['text_model_desc']  = 'Modell (Z - A)';
+$_['text_limit']       = 'Zeigen';

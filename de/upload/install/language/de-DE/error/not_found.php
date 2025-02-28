3ca247cb2dd98a869c579104bb8767494b1b8af6
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:21:15 +0800

    New translations not_found.php (German)

--- de/upload/install/language/de-DE/error/not_found.php
+++ de/upload/install/language/de-DE/error/not_found.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'The page you requested cannot be found!';
+$_['heading_title'] = 'Die angeforderte Seite kann nicht gefunden werden!';
 
 // Text
-$_['text_error']    = 'The page you requested cannot be found.';
+$_['text_error']    = 'Die angeforderte Seite kann nicht gefunden werden.';

c738705d3a136f6e839fdb338edbaf308e8761e9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:13 +0800

    New translations blog.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/module/blog.php
+++ de/upload/extension/opencart/admin/language/de-DE/module/blog.php
@@ -1,33 +1,33 @@
 <?php
 // Heading
-$_['heading_title']    = 'Blog';
+$_['heading_title']    = 'Der Blog';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified blog module!';
-$_['text_next']        = 'Success: You have modified %s blog out of %s!';
-$_['text_edit']        = 'Edit Blog Module';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das Blog-Modul geändert!';
+$_['text_next']        = 'Erfolg: Sie haben %s von %sBlogs geändert!';
+$_['text_edit']        = 'Blog-Modul bearbeiten';
 
 // Form
-$_['text_desc']        = 'Descending Order';
-$_['text_asc']         = 'Ascending Order';
+$_['text_desc']        = 'Absteigende Reihenfolge';
+$_['text_asc']         = 'Aufsteigende Reihenfolge';
 $_['text_sort_name']   = 'Name';
-$_['text_sort_date']   = 'Date Added';
+$_['text_sort_date']   = 'Datum hinzugefügt';
 
 // Column
-$_['column_total']     = 'Total';
+$_['column_total']     = 'Gesamt';
 
 // Entry
-$_['entry_name']       = 'Module Name';
+$_['entry_name']       = 'Modulname';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
-$_['entry_order']      = 'Order';
-$_['entry_sort']   	   = 'Sort';
+$_['entry_width']      = 'Bildbreite';
+$_['entry_height']     = 'Bildhöhe';
+$_['entry_order']      = 'Befehl';
+$_['entry_sort']   	   = 'Sortieren';
 $_['entry_status']     = 'Status';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify blog module!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
-$_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, das Blog-Modul zu ändern!';
+$_['error_name']       = 'Der Modulname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_width']      = 'Breite erforderlich!';
+$_['error_height']     = 'Körpergröße erforderlich!';

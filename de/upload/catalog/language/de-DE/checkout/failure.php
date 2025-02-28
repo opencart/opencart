df0e48fbfb6aeca1aae0dd8cf4b9e64fd5a856ae
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:28 +0800

    New translations failure.php (German)

--- de/upload/catalog/language/de-DE/checkout/failure.php
+++ de/upload/catalog/language/de-DE/checkout/failure.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title'] = 'Failed Payment!';
+$_['heading_title'] = 'Zahlung fehlgeschlagen!';
 
 // Text
-$_['text_basket']   = 'Shopping Cart';
-$_['text_checkout'] = 'Checkout';
-$_['text_failure']  = 'Failed Payment';
-$_['text_message']  = '<p>There was a problem processing your payment and the order did not complete.</p>
+$_['text_basket']   = 'Warenkorb';
+$_['text_checkout'] = 'Kasse';
+$_['text_failure']  = 'Fehlgeschlagene Zahlung';
+$_['text_message']  = '<p>Bei der Zahlungsabwicklung ist ein Problem aufgetreten und die Bestellung wurde nicht abgeschlossen.</p>
 
-<p>Possible reasons are:</p>
+<p>Mögliche Gründe sind:</p>
 <ul>
-  <li>Insufficient funds</li>
-  <li>Verification failed</li>
+  <li>Nicht ausreichendes Guthaben</li>
+  <li>Überprüfung fehlgeschlagen</li>
 </ul>
 
-<p>Please try to order again using a different payment method.</p>
+<p>Bitte versuchen Sie, mit einer anderen Zahlungsmethode erneut zu bestellen.</p>
 
-<p>If the problem persists please <a href="%s">contact us</a> with the details of the order you are trying to place.</p>
+<p>Wenn das Problem weiterhin besteht, <a href="%s">kontaktieren Sie uns bitte</a> und geben Sie die Einzelheiten der Bestellung an, die Sie aufgeben möchten.</p>
 ';

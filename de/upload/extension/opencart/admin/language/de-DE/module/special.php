7bcfb1541a05ff9e0ae9848a99d9378c44f8b449
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:24 +0800

    New translations special.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/module/special.php
+++ de/upload/extension/opencart/admin/language/de-DE/module/special.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']    = 'Specials';
+$_['heading_title']    = 'Besonderheiten';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified module specials!';
-$_['text_edit']        = 'Edit Specials Module';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben Modul-Specials geändert!';
+$_['text_edit']        = 'Modul „Specials bearbeiten“';
 $_['text_horizontal']  = 'Horizontal';
-$_['text_vertical']    = 'Vertical';
+$_['text_vertical']    = 'Vertikal';
 
 // Entry
-$_['entry_name']       = 'Module Name';
-$_['entry_axis']       = 'Axis';
+$_['entry_name']       = 'Modulname';
+$_['entry_axis']       = 'Achse';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
+$_['entry_width']      = 'Bildbreite';
+$_['entry_height']     = 'Bildhöhe';
 $_['entry_status']     = 'Status';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify specials module!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
-$_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, das Spezialmodul zu ändern!';
+$_['error_name']       = 'Der Modulname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_width']      = 'Breite erforderlich!';
+$_['error_height']     = 'Körpergröße erforderlich!';

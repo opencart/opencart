90edbc1c06827d9ea24715a3815abf6004463c6a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 04:17:14 +0800

    New translations transaction.php (English, United Kingdom)

---
+++ en-GB/upload/admin/language/en-gb/mail/transaction.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject']  = '%s - Affiliate Credit';
+$_['text_received'] = 'You have received %s credit!';
+$_['text_total']    = 'Your total amount of credit is now %s.';
+$_['text_credit']   = 'Your account credit can deducted from your next purchase.';

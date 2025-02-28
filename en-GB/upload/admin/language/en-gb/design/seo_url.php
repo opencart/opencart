a7ae32296c93b8a9d4a1c838841e5d39b195ccdc
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 04:15:14 +0800

    New translations seo_url.php (English, United Kingdom)

---
+++ en-GB/upload/admin/language/en-gb/design/seo_url.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']           = 'SEO URL';
+
+// Text
+$_['text_success']            = 'Success: You have modified SEO URL!';
+$_['text_list']               = 'SEO URL List';
+$_['text_add']                = 'Add SEO URL';
+$_['text_edit']               = 'Edit SEO URL';
+$_['text_filter']             = 'Filter';
+$_['text_default']            = 'Default';
+
+// Column
+$_['column_key']              = 'Key';
+$_['column_value']            = 'Value';
+$_['column_keyword']          = 'Keyword';
+$_['column_sort_order']       = 'Sort Order';
+$_['column_store']            = 'Store';
+$_['column_language']         = 'Language';
+$_['column_action']           = 'Action';
+
+// Entry
+$_['entry_store']             = 'Store';
+$_['entry_language']          = 'Language';
+$_['entry_key']               = 'Key';
+$_['entry_value']             = 'Value';
+$_['entry_keyword']           = 'Keyword';
+$_['entry_sort_order']        = 'Sort Order';
+
+// Help
+$_['help_keyword']            = 'Make sure you only use characters in the a-z or 0-9 use and - or _ for spaces. Use / for categories.';
+$_['help_sort_order']         = 'The sort order of the keywords in the URL.';
+
+// Error
+$_['error_permission']        = 'Warning: You do not have permission to modify SEO URL!';
+$_['error_exists']            = 'Warning: Combination of store, language key, value, keyword already exists!';
+$_['error_key']               = 'Key must be between 1 and 64 characters!';
+$_['error_value']             = 'Value must be between 1 and 255 characters!';
+$_['error_value_exists']      = 'Value is already in use!';
+$_['error_keyword']           = 'Keyword must be between 1 and 64 characters!';
+$_['error_keyword_exists']    = 'Keyword is already in use!';
+$_['error_keyword_character'] = 'Keyword can only use characters a-z, 0-9, - and _!';

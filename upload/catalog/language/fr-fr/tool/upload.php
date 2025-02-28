88323b1f42464fa075c4d86e06572128c151958f
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2025-02-27 21:48:33 -0500

    Update upload.php
    Added new language definitions.

--- upload/catalog/language/fr-fr/tool/upload.php
+++ upload/catalog/language/fr-fr/tool/upload.php
@@ -3,6 +3,7 @@
 $_['text_upload']     = 'Votre fichier a été téléchargé avec succès!';
 
 // Erreur
+$_['error_token']     = 'Attention: Le jeton de téléchargement est invalide!';
 $_['error_filename']  = 'Le nom du fichier doit contenir entre 3 et 64 caractères!';
 $_['error_file_type'] = 'Type de fichier invalide!';
 $_['error_upload']    = 'Téléchargement requis!';

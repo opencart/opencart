940be4d545dfaf66bd444d90dfbca111a384c6ab
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:41 +0800

    New translations affiliate.php (German)

--- de/upload/catalog/language/de-DE/account/affiliate.php
+++ de/upload/catalog/language/de-DE/account/affiliate.php
@@ -1,38 +1,38 @@
 <?php
 // Heading
-$_['heading_title']             = 'Your Affiliate Information';
+$_['heading_title']             = 'Ihre Partnerinformationen';
 
 // Text
-$_['text_account']              = 'Account';
-$_['text_affiliate']            = 'Affiliate';
-$_['text_my_affiliate']         = 'My Affiliate Account';
-$_['text_payment']              = 'Payment Information';
-$_['text_cheque']               = 'Cheque';
+$_['text_account']              = 'Konto';
+$_['text_affiliate']            = 'Partnerprogramm';
+$_['text_my_affiliate']         = 'Mein Affiliate-Konto';
+$_['text_payment']              = 'Zahlungsinformationen';
+$_['text_cheque']               = 'Überprüfen';
 $_['text_paypal']               = 'PayPal';
-$_['text_bank']                 = 'Bank Transfer';
-$_['text_success']              = 'Success: Your affiliate account has been successfully updated.';
-$_['text_agree']                = 'I have read and agree to the <a href="%s" class="modal-link"><b>%s</b></a>';
+$_['text_bank']                 = 'Banküberweisung';
+$_['text_success']              = 'Erfolg: Ihr Partnerkonto wurde erfolgreich aktualisiert.';
+$_['text_agree']                = 'Ich habe die <a href="%s" class="modal-link"><b>%s</b></a>gelesen und stimme ihnen zu';
 
 // Entry
-$_['entry_company']             = 'Company';
-$_['entry_website']             = 'Web Site';
-$_['entry_tax']                 = 'Tax ID';
-$_['entry_payment_method']      = 'Payment Method';
-$_['entry_cheque']              = 'Cheque Payee Name';
-$_['entry_paypal']              = 'PayPal Email Account';
-$_['entry_bank_name']           = 'Bank Name';
-$_['entry_bank_branch_number']  = 'ABA/BSB number (Branch Number)';
-$_['entry_bank_swift_code']     = 'SWIFT Code';
-$_['entry_bank_account_name']   = 'Account Name';
-$_['entry_bank_account_number'] = 'Account Number';
+$_['entry_company']             = 'Unternehmen';
+$_['entry_website']             = 'Website';
+$_['entry_tax']                 = 'Umsatzsteuer-Identifikationsnummer';
+$_['entry_payment_method']      = 'Zahlungsmethode';
+$_['entry_cheque']              = 'Name des Scheckempfängers';
+$_['entry_paypal']              = 'PayPal-E-Mail-Konto';
+$_['entry_bank_name']           = 'Name der Bank';
+$_['entry_bank_branch_number']  = 'ABA/BSB-Nummer (Zweigstellennummer)';
+$_['entry_bank_swift_code']     = 'SWIFT-Code';
+$_['entry_bank_account_name']   = 'Kontoname';
+$_['entry_bank_account_number'] = 'Kontonummer';
 
 // Error
-$_['error_token']               = 'Warning: Affiliate token invalid!';
-$_['error_agree']               = 'Warning: You must agree to the %s!';
-$_['error_payment_method']      = 'Payment method required!';
-$_['error_cheque']              = 'Cheque Payee Name required!';
-$_['error_paypal']              = 'PayPal Email Address does not appear to be valid!';
-$_['error_bank_account_name']   = 'Account Name required!';
-$_['error_bank_account_number'] = 'Account Number required!';
-$_['error_custom_field']        = '%s required!';
-$_['error_regex']               = '%s is not a valid input!';
+$_['error_token']               = 'Achtung: Affiliate-Token ungültig!';
+$_['error_agree']               = 'Achtung: Sie müssen den %szustimmen!';
+$_['error_payment_method']      = 'Zahlungsmethode erforderlich!';
+$_['error_cheque']              = 'Name des Scheckempfängers erforderlich!';
+$_['error_paypal']              = 'Die PayPal-E-Mail-Adresse scheint ungültig zu sein!';
+$_['error_bank_account_name']   = 'Kontoname erforderlich!';
+$_['error_bank_account_number'] = 'Kontonummer erforderlich!';
+$_['error_custom_field']        = '%s erforderlich!';
+$_['error_regex']               = '%s ist keine gültige Eingabe!';

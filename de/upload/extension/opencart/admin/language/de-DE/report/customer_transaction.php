ddaa1f37262e3fd4adf31308637e68200021a7ae
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:42 +0800

    New translations customer_transaction.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/report/customer_transaction.php
+++ de/upload/extension/opencart/admin/language/de-DE/report/customer_transaction.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Transaction Report';
+$_['heading_title']         = 'Kundentransaktionsbericht';
 
 // Text
-$_['text_extension']        = 'Extensions';
-$_['text_edit']             = 'Edit Customer Transaction Report';
-$_['text_success']          = 'Success: You have modified customer transaction report!';
+$_['text_extension']        = 'Erweiterungen';
+$_['text_edit']             = 'Kundentransaktionsbericht bearbeiten';
+$_['text_success']          = 'Erfolg: Sie haben den Kundentransaktionsbericht geändert!';
 $_['text_filter']           = 'Filter';
 
 // Column
-$_['column_customer']       = 'Customer Name';
+$_['column_customer']       = 'Kundenname';
 $_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
+$_['column_customer_group'] = 'Kundengruppe';
 $_['column_status']         = 'Status';
-$_['column_total']          = 'Total';
-$_['column_action']         = 'Action';
+$_['column_total']          = 'Gesamt';
+$_['column_action']         = 'Aktion';
 
 // Entry
-$_['entry_date_start']      = 'Date Start';
-$_['entry_date_end']        = 'Date End';
-$_['entry_customer']        = 'Customer';
+$_['entry_date_start']      = 'Datum Beginn';
+$_['entry_date_end']        = 'Datum Ende';
+$_['entry_customer']        = 'Kunde';
 $_['entry_status']          = 'Status';
-$_['entry_sort_order']      = 'Sort Order';
+$_['entry_sort_order']      = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify customer transaction report!';
+$_['error_permission']      = 'Achtung: Sie sind nicht berechtigt, den Kundentransaktionsbericht zu ändern!';

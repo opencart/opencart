9ecaa3bbe48434f6571506dc1995165f2548b7b1
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 05:14:22 +0800

    New translations shipping_method.php (English, United Kingdom)

---
+++ en-GB/upload/catalog/language/en-gb/api/shipping_method.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_success']           = 'Success: Shipping method has been set!';
+
+// Error
+$_['error_customer']         = 'Warning: Customer details required!';
+$_['error_shipping_address'] = 'Warning: Shipping address required!';
+$_['error_shipping']         = 'Warning: There are no products that require shipping';
+$_['error_no_shipping']      = 'Warning: No Shipping options are available!';
+$_['error_shipping_method']  = 'Warning: Shipping method required!';

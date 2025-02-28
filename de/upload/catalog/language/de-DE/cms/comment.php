1c3e92ff84657b662bb9892390fd12851f6f2b5c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:41 +0800

    New translations comment.php (German)

--- de/upload/catalog/language/de-DE/cms/comment.php
+++ de/upload/catalog/language/de-DE/cms/comment.php
@@ -1,43 +1,43 @@
 <?php
 // Heading
-$_['heading_title']         = 'Comments';
+$_['heading_title']         = 'Kommentare';
 
 // Text
-$_['text_by']               = 'By';
-$_['text_write']            = 'Write a comment';
-$_['text_login']            = 'Please <a href="%s">login</a> or <a href="%s">register</a> to comment';
-$_['text_no_results']       = 'Be the first to write a comment for this article.';
-$_['text_sort']             = 'Sort By';
-$_['text_rating_asc']       = 'Rating (Lowest)';
-$_['text_rating_desc']      = 'Rating (Highest)';
-$_['text_date_added_asc']   = 'Date Added (Oldest)';
-$_['text_date_added_desc']  = 'Date Added (Latest)';
-$_['text_like']             = 'Like!';
-$_['text_dislike']          = 'Dislike!';
-$_['text_success']          = 'Success: Thank you for your comment!';
-$_['text_queue']            = 'Success: Your comment has been added to our moderation queue!';
-$_['text_rating']           = 'Success: Thank you for your rating!';
+$_['text_by']               = 'Von';
+$_['text_write']            = 'Einen Kommentar schreiben';
+$_['text_login']            = 'Bitte <a href="%s">anmelden</a> oder <a href="%s">registrieren</a> um zu kommentieren';
+$_['text_no_results']       = 'Schreiben Sie als Erster einen Kommentar zu diesem Artikel.';
+$_['text_sort']             = 'Sortieren nach';
+$_['text_rating_asc']       = 'Bewertung (niedrigste)';
+$_['text_rating_desc']      = 'Bewertung (Höchste)';
+$_['text_date_added_asc']   = 'Hinzugefügt am (älteste)';
+$_['text_date_added_desc']  = 'Hinzugefügt am (aktuellstes)';
+$_['text_like']             = 'Wie!';
+$_['text_dislike']          = 'Abneigung!';
+$_['text_success']          = 'Erfolg: Vielen Dank für Ihren Kommentar!';
+$_['text_queue']            = 'Erfolg: Ihr Kommentar wurde zu unserer Moderationswarteschlange hinzugefügt!';
+$_['text_rating']           = 'Erfolg: Vielen Dank für Ihre Bewertung!';
 
 // Entry
-$_['entry_author']          = 'Your Name';
-$_['entry_comment']         = 'Comment';
+$_['entry_author']          = 'Ihr Name';
+$_['entry_comment']         = 'Kommentar';
 
 // Buttons
-$_['button_comment']        = 'Post Comment';
-$_['button_reply']          = 'Post Reply';
-$_['button_replies']        = 'Show Replies';
-$_['button_more']           = 'See more replies...';
-$_['button_login']          = 'Login to Comment';
-$_['button_login_reply']    = 'Login to Reply';
-$_['button_like']           = 'Like';
-$_['button_dislike']        = 'Dislike';
+$_['button_comment']        = 'Kommentar veröffentlichen';
+$_['button_reply']          = 'Antwort schreiben';
+$_['button_replies']        = 'Antworten anzeigen';
+$_['button_more']           = 'Weitere Antworten anzeigen...';
+$_['button_login']          = 'Zum Kommentieren anmelden';
+$_['button_login_reply']    = 'Einloggen um zu antworten';
+$_['button_like']           = 'Wie';
+$_['button_dislike']        = 'Abneigung';
 
 // Error
-$_['error_article']         = 'Warning: Article could not be found!';
-$_['error_article_comment'] = 'Warning: Article comment could not be found!';
-$_['error_token']           = 'Warning: Comment token invalid!';
-$_['error_author']          = 'Your Name must be between 3 and 25 characters!';
-$_['error_comment']         = 'Comment must be between 25 and 1000 characters!';
-$_['error_interval']        = 'Warning: you must wait %s minutes before making another comment!';
-$_['error_login']           = 'You must login to comment on the article or rate it!';
-$_['error_status']          = 'Comments are disabled!';
+$_['error_article']         = 'Achtung: Artikel konnte nicht gefunden werden!';
+$_['error_article_comment'] = 'Achtung: Artikelkommentar konnte nicht gefunden werden!';
+$_['error_token']           = 'Achtung: Kommentar-Token ungültig!';
+$_['error_author']          = 'Ihr Name muss zwischen 3 und 25 Zeichen lang sein!';
+$_['error_comment']         = 'Der Kommentar muss zwischen 25 und 1000 Zeichen lang sein!';
+$_['error_interval']        = 'Achtung: Sie müssen %s Minuten warten, bevor Sie einen weiteren Kommentar abgeben können!';
+$_['error_login']           = 'Um den Artikel zu kommentieren oder zu bewerten, müssen Sie sich anmelden!';
+$_['error_status']          = 'Kommentare sind deaktiviert!';

49e40954cb319925f3d367f1f480e2d698f608a2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:50 +0800

    New translations logout.php (German)

--- de/upload/catalog/language/de-DE/account/logout.php
+++ de/upload/catalog/language/de-DE/account/logout.php
@@ -1,8 +1,8 @@
 <?php
 // Heading
-$_['heading_title'] = 'Account Logout';
+$_['heading_title'] = 'Kontoabmeldung';
 
 // Text
-$_['text_message']  = '<p>You have been logged off your account. It is now safe to leave the computer.</p><p>Your shopping cart has been saved, the items inside it will be restored whenever you log back into your account.</p>';
-$_['text_account']  = 'Account';
-$_['text_logout']   = 'Logout';
+$_['text_message']  = '<p>Sie wurden von Ihrem Konto abgemeldet. Sie können den Computer jetzt sicher verlassen.</p><p>Ihr Einkaufswagen wurde gespeichert. Die darin enthaltenen Artikel werden wiederhergestellt, wenn Sie sich wieder bei Ihrem Konto anmelden.</p>';
+$_['text_account']  = 'Konto';
+$_['text_logout']   = 'Ausloggen';

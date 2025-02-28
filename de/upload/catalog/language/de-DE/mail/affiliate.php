3e7b5f68930b61782c9916c15273c8566542f4a3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:10 +0800

    New translations affiliate.php (German)

--- de/upload/catalog/language/de-DE/mail/affiliate.php
+++ de/upload/catalog/language/de-DE/mail/affiliate.php
@@ -1,20 +1,20 @@
 <?php
 // Text
-$_['text_subject']        = '%s - Affiliate Program';
-$_['text_welcome']        = 'Thank you for joining the %s Affiliate Program!';
-$_['text_login']          = 'Your account has now been created and you can log in by using your e-mail address and password by visiting our website or at the following URL:';
-$_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your e-mail address and password by visiting our website or at the following URL:';
-$_['text_service']        = 'Upon logging in, you will be able to generate tracking codes, track commission payments and edit your account information.';
-$_['text_thanks']         = 'Thanks,';
-$_['text_new_affiliate']  = 'New Affiliate';
-$_['text_signup']         = 'A new affiliate has signed up:';
-$_['text_website']        = 'Web Site:';
-$_['text_customer_group'] = 'Customer Group:';
-$_['text_firstname']      = 'First Name:';
-$_['text_lastname']       = 'Last Name:';
-$_['text_company']        = 'Company:';
+$_['text_subject']        = '%s - Partnerprogramm';
+$_['text_welcome']        = 'Vielen Dank, dass Sie dem %s -Partnerprogramm beigetreten sind!';
+$_['text_login']          = 'Ihr Konto wurde nun erstellt und Sie können sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website besuchen oder die folgende URL aufrufen:';
+$_['text_approval']       = 'Ihr Konto muss genehmigt werden, bevor Sie sich anmelden können. Nach der Genehmigung können Sie sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website oder die folgende URL besuchen:';
+$_['text_service']        = 'Nach der Anmeldung können Sie Tracking-Codes generieren, Provisionszahlungen verfolgen und Ihre Kontoinformationen bearbeiten.';
+$_['text_thanks']         = 'Danke,';
+$_['text_new_affiliate']  = 'Neuer Partner';
+$_['text_signup']         = 'Ein neuer Partner hat sich angemeldet:';
+$_['text_website']        = 'Website:';
+$_['text_customer_group'] = 'Kundengruppe:';
+$_['text_firstname']      = 'Vorname:';
+$_['text_lastname']       = 'Nachname:';
+$_['text_company']        = 'Unternehmen:';
 $_['text_email']          = 'E-Mail:';
-$_['text_telephone']      = 'Telephone:';
+$_['text_telephone']      = 'Telefon:';
 
 // Button
 $_['button_login']        = 'Login';

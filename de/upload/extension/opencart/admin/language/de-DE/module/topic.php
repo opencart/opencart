3d3a06fea207379baadc1c68df0f79c068059231
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:27 +0800

    New translations topic.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/module/topic.php
+++ de/upload/extension/opencart/admin/language/de-DE/module/topic.php
@@ -1,14 +1,14 @@
 <?php
 // Heading
-$_['heading_title']    = 'Topic';
+$_['heading_title']    = 'Thema';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified topic module!';
-$_['text_edit']        = 'Edit Topic Module';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das Themenmodul geändert!';
+$_['text_edit']        = 'Themenmodul bearbeiten';
 
 // Entry
 $_['entry_status']     = 'Status';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify topic module!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, das Themenmodul zu ändern!';

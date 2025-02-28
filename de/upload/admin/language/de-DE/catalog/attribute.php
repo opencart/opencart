9beedd248449913f9d98499a5f24ae683caeefbc
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 04:13:53 +0800

    New translations attribute.php (German)

---
+++ de/upload/admin/language/de-DE/catalog/attribute.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Eigenschaften';
+
+// Text
+$_['text_success']           = 'Eigenschaft erfolgreich geändert';
+$_['text_list']              = 'Übersicht';
+$_['text_add']               = 'Eigenschaften hinzufügen';
+$_['text_edit']              = 'Bearbeiten';
+
+// Column
+$_['column_name']            = 'Name';
+$_['column_attribute_group'] = 'Attribute Group';
+$_['column_sort_order']      = 'Sort Order';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Attribute Name';
+$_['entry_attribute_group']  = 'Attribute Group';
+$_['entry_sort_order']       = 'Sort Order';
+
+// Error
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify attributes!';
+$_['error_attribute_group']  = 'Attribute Group required!';
+$_['error_name']             = 'Attribute Name must be between 1 and 64 characters!';
+$_['error_product']          = 'Warning: This attribute cannot be deleted as it is currently assigned to %s products!';

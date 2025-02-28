0d9789b24505479c800b29f546ade7f9b1d3743a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:10 +0800

    New translations online.php (German)

--- de/upload/admin/language/de-DE/report/online.php
+++ de/upload/admin/language/de-DE/report/online.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Online Report';
+$_['heading_title']     = 'Online-Bericht';
 
 // Text
-$_['text_extension']    = 'Extensions';
-$_['text_success']      = 'Success: You have modified customers online report!';
-$_['text_list']         = 'Online List';
+$_['text_extension']    = 'Erweiterungen';
+$_['text_success']      = 'Erfolg: Sie haben den Online-Kundenbericht geändert!';
+$_['text_list']         = 'Online-Liste';
 $_['text_filter']       = 'Filter';
-$_['text_guest']        = 'Guest';
+$_['text_guest']        = 'Gast';
 
 // Column
 $_['column_ip']         = 'IP';
-$_['column_customer']   = 'Customer';
-$_['column_url']        = 'Last Page Visited';
-$_['column_referer']    = 'Referer';
-$_['column_date_added'] = 'Last Click';
-$_['column_action']     = 'Action';
+$_['column_customer']   = 'Kunde';
+$_['column_url']        = 'Zuletzt besuchte Seite';
+$_['column_referer']    = 'Referrer';
+$_['column_date_added'] = 'Letzter Klick';
+$_['column_action']     = 'Aktion';
 
 // Entry
 $_['entry_ip']          = 'IP';
-$_['entry_customer']    = 'Customer';
+$_['entry_customer']    = 'Kunde';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify customers online report!';
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, den Online-Kundenbericht zu ändern!';

c0edf718c3727a46ea45822b2a9a7120ae855d0a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:53 +0800

    New translations order.php (German)

--- de/upload/catalog/language/de-DE/account/order.php
+++ de/upload/catalog/language/de-DE/account/order.php
@@ -1,47 +1,47 @@
 <?php
 // Heading
-$_['heading_title']              = 'Orders';
+$_['heading_title']              = 'Bestellungen';
 
 // Text
-$_['text_account']               = 'Account';
-$_['text_order']                 = 'Order #%s';
-$_['text_order_id']              = 'Order ID';
-$_['text_invoice_no']            = 'Invoice No.';
-$_['text_tbc']                   = 'TBC';
-$_['text_shipping_address']      = 'Shipping Address';
-$_['text_payment_address']       = 'Payment Address';
-$_['text_shipping_method']       = 'Shipping Method';
-$_['text_payment_method']        = 'Payment Method';
-$_['text_model']                 = 'Model';
-$_['text_points']                = 'Reward Points';
-$_['text_subscription']          = 'Subscription';
-$_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
-$_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
-$_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
-$_['text_day']                   = 'day';
-$_['text_week']                  = 'week';
-$_['text_semi_month']            = 'half-month';
-$_['text_month']                 = 'month';
-$_['text_year']                  = 'year';
-$_['text_date_next']             = 'Date Next';
-$_['text_remaining']             = 'Remaining';
-$_['text_comment']               = 'Note';
-$_['text_history']               = 'Order History';
-$_['text_success']               = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">shopping cart</a>!';
-$_['text_no_results']            = 'You have not made any previous orders!';
+$_['text_account']               = 'Konto';
+$_['text_order']                 = 'Bestellnummer%s';
+$_['text_order_id']              = 'Bestellnummer';
+$_['text_invoice_no']            = 'Rechnungsnummer';
+$_['text_tbc']                   = 'Wird noch bekannt gegeben';
+$_['text_shipping_address']      = 'Lieferadresse';
+$_['text_payment_address']       = 'Zahlungsadresse';
+$_['text_shipping_method']       = 'Versandart';
+$_['text_payment_method']        = 'Zahlungsmethode';
+$_['text_model']                 = 'Modell';
+$_['text_points']                = 'Belohnungspunkte';
+$_['text_subscription']          = 'Abonnement';
+$_['text_subscription_trial']    = '%s alle %d %s(s) für %d Zahlung(en) dann ';
+$_['text_subscription_duration'] = '%s alle %d %s(s) für %d Zahlung(en)';
+$_['text_subscription_cancel']   = '%s alle %d %s(s) bis zum Abbruch';
+$_['text_day']                   = 'Tag';
+$_['text_week']                  = 'Woche';
+$_['text_semi_month']            = 'halber Monat';
+$_['text_month']                 = 'Monat';
+$_['text_year']                  = 'Jahr';
+$_['text_date_next']             = 'Datum Nächstes';
+$_['text_remaining']             = 'Übrig';
+$_['text_comment']               = 'Notiz';
+$_['text_history']               = 'Bestellverlauf';
+$_['text_success']               = 'Erfolg: Sie haben <a href="%s">%s</a> zu Ihrem <a href="%s">Einkaufswagen</a>hinzugefügt!';
+$_['text_no_results']            = 'Sie haben noch keine Bestellungen getätigt!';
 
 // Column
-$_['column_order_id']            = 'Order ID';
-$_['column_customer']            = 'Customer';
-$_['column_product']             = 'Product';
-$_['column_product_total']       = 'No. of Products';
+$_['column_order_id']            = 'Bestellnummer';
+$_['column_customer']            = 'Kunde';
+$_['column_product']             = 'Produkt';
+$_['column_product_total']       = 'Anzahl der Produkte';
 $_['column_status']              = 'Status';
-$_['column_total']               = 'Total';
-$_['column_date_added']          = 'Date Added';
-$_['column_quantity']            = 'Quantity';
-$_['column_price']               = 'Price';
-$_['column_comment']             = 'Comment';
-$_['column_action']              = 'Action';
+$_['column_total']               = 'Gesamt';
+$_['column_date_added']          = 'Datum hinzugefügt';
+$_['column_quantity']            = 'Menge';
+$_['column_price']               = 'Preis';
+$_['column_comment']             = 'Kommentar';
+$_['column_action']              = 'Aktion';
 
 // Error
-$_['error_reorder']              = 'The product you are trying to purchase again has some issues. Please check the error messages shown below.';
+$_['error_reorder']              = 'Das Produkt, das Sie erneut kaufen möchten, weist einige Probleme auf. Bitte überprüfen Sie die unten angezeigten Fehlermeldungen.';

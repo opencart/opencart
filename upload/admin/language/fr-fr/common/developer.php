451a553eda7a2b00f996d7b5f13e8dd345c1b0b1
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2025-02-28 10:07:22 -0500

    Update developer.php
    Added new language definitions.

--- upload/admin/language/fr-fr/common/developer.php
+++ upload/admin/language/fr-fr/common/developer.php
@@ -3,18 +3,21 @@
 $_['heading_title']    = 'Paramètres Développeur';
 
 // Text
-$_['text_success']     = 'Succès: Vous avez modifié les paramètres développeur!';
+$_['text_developer_success'] = 'Succès: Vous avez modifié les paramètres développeur!';
+$_['text_cache_success']     = 'Succès: Vous avez vidé le cache!';
+$_['text_theme_success']     = 'Succès: Vous avez vidé le cache theme!';
+$_['text_sass_success']      = 'Succès: Vous avez vidé le cache SASS!';
+$_['text_vendor_success']    = 'Succès: Vous avez vidé le cache fournisseur!';
 $_['text_theme']       = 'Thème';
 $_['text_sass']        = 'SASS';
-$_['text_cache']       = 'Succès: Vous avez vidé le cache %s!';
+$_['text_cache']       = 'Cache';
+$_['text_vendor']      = 'Vendor';
 
 // Column
 $_['column_component'] = 'Composant';
 $_['column_action']    = 'Action';
 
 // Entry
-$_['entry_theme']      = 'Thème';
-$_['entry_sass']       = 'SASS';
 $_['entry_cache']      = 'Cache';
 
 // Button

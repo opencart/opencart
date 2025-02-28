a71df09744bb5b401a94e0c6f4e52dc7b5f8c34b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:29 +0800

    New translations bank_transfer.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/payment/bank_transfer.php
+++ de/upload/extension/opencart/admin/language/de-DE/payment/bank_transfer.php
@@ -1,19 +1,19 @@
 <?php
 // Heading
-$_['heading_title']      = 'Bank Transfer';
+$_['heading_title']      = 'Banküberweisung';
 
 // Text
-$_['text_extension']     = 'Extensions';
-$_['text_success']       = 'Success: You have modified bank transfer details!';
-$_['text_edit']          = 'Edit Bank Transfer';
+$_['text_extension']     = 'Erweiterungen';
+$_['text_success']       = 'Erfolg: Sie haben die Banküberweisungsdaten geändert!';
+$_['text_edit']          = 'Banküberweisung bearbeiten';
 
 // Entry
-$_['entry_bank']         = 'Bank Transfer Instructions';
-$_['entry_order_status'] = 'Order Status';
-$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_bank']         = 'Anweisungen zur Banküberweisung';
+$_['entry_order_status'] = 'Bestellstatus';
+$_['entry_geo_zone']     = 'Geozone';
 $_['entry_status']       = 'Status';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_sort_order']   = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment bank transfer!';
-$_['error_bank']         = 'Bank Transfer Instructions required!';
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, die Zahlung per Banküberweisung zu ändern!';
+$_['error_bank']         = 'Banküberweisungsanweisungen erforderlich!';

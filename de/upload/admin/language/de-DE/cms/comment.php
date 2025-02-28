feb3a9c768622ebe620fba2ee0588154c6966829
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:13:55 +0800

    New translations comment.php (German)

--- de/upload/admin/language/de-DE/cms/comment.php
+++ de/upload/admin/language/de-DE/cms/comment.php
@@ -1,31 +1,31 @@
 <?php
 // Heading
-$_['heading_title']    = 'Article Comments';
+$_['heading_title']    = 'Kommentare zum Artikel';
 
 // Text
-$_['text_success']     = 'Success: You have modified article comments!';
-$_['text_next']        = 'Success: You have modified %s to %s of %s comment ratings!';
-$_['text_list']        = 'Article Comments List';
+$_['text_success']     = 'Erfolg: Sie haben Artikelkommentare geändert!';
+$_['text_next']        = 'Erfolg: Sie haben %s auf %s von %s Kommentarbewertungen geändert!';
+$_['text_list']        = 'Artikelkommentarliste';
 $_['text_filter']      = 'Filter';
-$_['text_by']          = 'by';
-$_['text_info']        = 'Comment Information';
-$_['text_rating']      = 'Rating:';
+$_['text_by']          = 'von';
+$_['text_info']        = 'Kommentarinformationen';
+$_['text_rating']      = 'Bewertung:';
 
 // Column
-$_['column_comment']   = 'Comment';
-$_['column_action']    = 'Action';
+$_['column_comment']   = 'Kommentar';
+$_['column_action']    = 'Aktion';
 
 // Entry
-$_['entry_keyword']    = 'Keyword';
-$_['entry_article']    = 'Article';
-$_['entry_customer']   = 'Customer';
+$_['entry_keyword']    = 'Stichwort';
+$_['entry_article']    = 'Artikel';
+$_['entry_customer']   = 'Kunde';
 $_['entry_status']     = 'Status';
-$_['entry_date_from']  = 'Date From';
-$_['entry_date_to']    = 'Date To';
+$_['entry_date_from']  = 'Stammen aus';
+$_['entry_date_to']    = 'Datum bis';
 
 // Button
 $_['button_spam']      = 'Spam';
-$_['button_rating']    = 'Calculate Ratings';
+$_['button_rating']    = 'Bewertungen berechnen';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify article comments!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Artikelkommentare zu ändern!';

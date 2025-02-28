f1a670b681ddd45d71c51d9241081eebfb384318
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:58 +0800

    New translations register.php (German)

--- de/upload/catalog/language/de-DE/account/register.php
+++ de/upload/catalog/language/de-DE/account/register.php
@@ -1,39 +1,39 @@
 <?php
 // Heading
-$_['heading_title']            = 'Register Account';
+$_['heading_title']            = 'Konto registrieren';
 
 // Text
-$_['text_account']             = 'Account';
-$_['text_register']            = 'Register';
-$_['text_account_already']     = 'If you already have an account with us, please login at the <a href="%s">login page</a>.';
-$_['text_your_details']        = 'Your Personal Details';
+$_['text_account']             = 'Konto';
+$_['text_register']            = 'Registrieren';
+$_['text_account_already']     = 'Wenn Sie bereits ein Konto bei uns haben, melden Sie sich bitte auf der <a href="%s">Anmeldeseite</a>an.';
+$_['text_your_details']        = 'Ihre persönlichen Daten';
 $_['text_newsletter']          = 'Newsletter';
-$_['text_your_password']       = 'Your Password';
-$_['text_agree']               = 'I have read and agree to the <a href="%s" class="modal-link"><b>%s</b></a>';
+$_['text_your_password']       = 'Ihr Passwort';
+$_['text_agree']               = 'Ich habe die <a href="%s" class="modal-link"><b>%s</b></a>gelesen und stimme ihnen zu';
 
 // Entry
-$_['entry_customer_group']     = 'Customer Group';
-$_['entry_firstname']          = 'First Name';
-$_['entry_lastname']           = 'Last Name';
+$_['entry_customer_group']     = 'Kundengruppe';
+$_['entry_firstname']          = 'Vorname';
+$_['entry_lastname']           = 'Nachname';
 $_['entry_email']              = 'E-Mail';
-$_['entry_telephone']          = 'Telephone';
-$_['entry_newsletter']         = 'Subscribe';
-$_['entry_password']           = 'Password';
+$_['entry_telephone']          = 'Telefon';
+$_['entry_newsletter']         = 'Abonnieren';
+$_['entry_password']           = 'Passwort';
 
 // Error
-$_['error_token']              = 'Warning: Register token invalid!';
-$_['error_exists']             = 'Warning: E-Mail Address is already registered!';
-$_['error_customer_group']     = 'Customer Group does not appear to be valid!';
-$_['error_firstname']          = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']           = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']              = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']          = 'Telephone must be between 3 and 32 characters!';
-$_['error_custom_field']       = '%s required!';
-$_['error_regex']              = '%s is not a valid input!';
-$_['error_password']           = 'Password must contain a %s and be between %d and 40 characters!';
-$_['error_password_uppercase'] = 'uppercase';
-$_['error_password_lowercase'] = 'lowercase';
-$_['error_password_number']    = 'number';
-$_['error_password_symbol']    = 'symbol';
-$_['error_password_length']    = 'Password must be between %d and 40 characters!';
-$_['error_agree']              = 'Warning: You must agree to the %s!';
+$_['error_token']              = 'Achtung: Registrierungstoken ungültig!';
+$_['error_exists']             = 'Achtung: E-Mail-Adresse ist bereits registriert!';
+$_['error_customer_group']     = 'Kundengruppe scheint nicht gültig zu sein!';
+$_['error_firstname']          = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']           = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_email']              = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_telephone']          = 'Die Telefonnummer muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_custom_field']       = '%s erforderlich!';
+$_['error_regex']              = '%s ist keine gültige Eingabe!';
+$_['error_password']           = 'Das Passwort muss eine %s enthalten und zwischen %d und 40 Zeichen lang sein!';
+$_['error_password_uppercase'] = 'Großbuchstaben';
+$_['error_password_lowercase'] = 'Kleinbuchstaben';
+$_['error_password_number']    = 'Nummer';
+$_['error_password_symbol']    = 'Symbol';
+$_['error_password_length']    = 'Das Passwort muss zwischen %d und 40 Zeichen lang sein!';
+$_['error_agree']              = 'Achtung: Sie müssen den %szustimmen!';

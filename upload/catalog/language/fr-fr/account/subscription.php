997bd16e38784585024d7f5bed0c4a604fc087a3
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2025-02-27 21:34:25 -0500

    Update subscription.php
    Added new language definitions.

--- upload/catalog/language/fr-fr/account/subscription.php
+++ upload/catalog/language/fr-fr/account/subscription.php
@@ -4,14 +4,16 @@ $_['heading_title']               = 'Abonnements';
 
 // Text
 $_['text_account']                = 'Compte';
+$_['text_success']                = 'Succès: Votre inscription a été annulée avec succès!';
 $_['text_subscription']           = 'Abonnement n°%s';
-$_['text_invoice_no']             = 'Numéro de facture';
 $_['text_subscription_id']        = 'ID d\'abonnement';
 $_['text_order_id']               = 'ID de commande';
 $_['text_shipping_address']       = 'Adresse de livraison';
 $_['text_shipping_method']        = 'Méthode de livraison';
 $_['text_payment_address']        = 'Adresse de facturation';
 $_['text_payment_method']         = 'Méthode de paiement';
+$_['text_model']                  = 'Modèle';
+$_['text_subscription_plan']      = 'Plan d\'Inscription';
 $_['text_subscription_trial']     = '%s tous les %d %s(s) pour %d paiement(s) puis ';
 $_['text_subscription_duration']  = '%s tous les %d %s(s) pour %d paiement(s)';
 $_['text_subscription_cancel']    = '%s tous les %d %s(s) jusqu\'à annulation';
@@ -20,24 +22,31 @@ $_['text_week']                   = 'semaine';
 $_['text_semi_month']             = 'demi-mois';
 $_['text_month']                  = 'mois';
 $_['text_year']                   = 'année';
-$_['text_date_added']             = 'Date d\'ajout';
-$_['text_status']                 = 'Statut de l\'abonnement';
-$_['text_description']            = 'Description';
 $_['text_quantity']               = 'Quantité';
+$_['text_date_next']              = 'Prochaine Date';
+$_['text_remaining']              = 'Restant';
+$_['text_history']                = 'Subscription History';
 $_['text_order']                  = 'Historique des commandes';
-$_['text_no_results']             = 'Aucun abonnement trouvé!';
-$_['text_error']                  = 'La commande récurrente demandée est introuvable!';
-$_['text_cancelled']              = 'Le paiement récurrent a été annulé';
+$_['text_no_subscription']        = 'Aucun abonnement trouvé!';
 
 // Column
 $_['column_subscription_id']      = 'ID d\'abonnement';
 $_['column_product']              = 'Produit';
+$_['column_product_total']        = 'Non. of Products';
+$_['column_description']          = 'Description';
 $_['column_order_id']             = 'ID de commande';
 $_['column_status']               = 'Statut';
+$_['column_quantity']             = 'Quantity';
+$_['column_trial_price']          = 'Trial Price';
+$_['column_price']                = 'Prix d\'Essai';
 $_['column_total']                = 'Total';
 $_['column_comment']              = 'Commentaire';
 $_['column_date_added']           = 'Date d\'ajout';
 
+// Button
+$_['button_cancel']               = 'Annuler l\'Inscription';
+
 // Error
-$_['error_not_cancelled']         = 'Erreur: %s';
-$_['error_not_found']             = 'Impossible d\'annuler l\'abonnement';
+$_['error_subscription']         = 'L\'inscription n\'a pu être trouvée!';
+$_['error_duration']             = 'L\'inscription ne peut être annulée jusqu\'à ce que %s commandes soient atteintes!';
+$_['error_canceled']             = 'L\'inscription est déjà annulée!';

8301fb55d9e574fd843560db90b85b0a9bdbb9ee
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:41 +0800

    New translations customer_search.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/report/customer_search.php
+++ de/upload/extension/opencart/admin/language/de-DE/report/customer_search.php
@@ -1,31 +1,31 @@
 <?php
 // Heading
-$_['heading_title']     = 'Customer Searches Report';
+$_['heading_title']     = 'Bericht zu Kundensuchvorgängen';
 
 // Text
-$_['text_extension']    = 'Extensions';
-$_['text_edit']         = 'Edit Customer Searches Report';
-$_['text_success']      = 'Success: You have modified customer searches report!';
+$_['text_extension']    = 'Erweiterungen';
+$_['text_edit']         = 'Bericht „Kundensuchvorgänge“ bearbeiten';
+$_['text_success']      = 'Erfolg: Sie haben den Bericht zu Kundensuchvorgängen geändert!';
 $_['text_filter']       = 'Filter';
-$_['text_guest']        = 'Guest';
+$_['text_guest']        = 'Gast';
 $_['text_customer']     = '<a href="%s">%s</a>';
 
 // Column
-$_['column_keyword']    = 'Keyword';
-$_['column_products']   = 'Found Products';
-$_['column_category']   = 'Category';
-$_['column_customer']   = 'Customer';
+$_['column_keyword']    = 'Stichwort';
+$_['column_products']   = 'Gefundene Produkte';
+$_['column_category']   = 'Kategorie';
+$_['column_customer']   = 'Kunde';
 $_['column_ip']         = 'IP';
-$_['column_date_added'] = 'Date Added';
+$_['column_date_added'] = 'Datum hinzugefügt';
 
 // Entry
-$_['entry_date_start']  = 'Date Start';
-$_['entry_date_end']    = 'Date End';
-$_['entry_keyword']     = 'Keyword';
-$_['entry_customer']    = 'Customer';
+$_['entry_date_start']  = 'Datum Beginn';
+$_['entry_date_end']    = 'Datum Ende';
+$_['entry_keyword']     = 'Stichwort';
+$_['entry_customer']    = 'Kunde';
 $_['entry_ip']          = 'IP';
 $_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_sort_order']  = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify customer searches report!';
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, den Bericht zu Kundensuchvorgängen zu ändern!';

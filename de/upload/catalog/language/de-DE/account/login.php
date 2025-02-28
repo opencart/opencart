f343b82db2bf584ed92525ce0333aceb26842747
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:48 +0800

    New translations login.php (German)

--- de/upload/catalog/language/de-DE/account/login.php
+++ de/upload/catalog/language/de-DE/account/login.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']                = 'Account Login';
+$_['heading_title']                = 'Anmeldung Benutzerkonto';
 
 // Text
-$_['text_account']                 = 'Account';
+$_['text_account']                 = 'Konto';
 $_['text_login']                   = 'Login';
-$_['text_new_customer']            = 'New Customer';
-$_['text_register']                = 'Register Account';
-$_['text_register_account']        = 'By creating an account you will be able to shop faster, be up to date on an order\'s status, and keep track of the orders you have previously made.';
-$_['text_returning_customer']      = 'Returning Customer';
-$_['text_i_am_returning_customer'] = 'I am a returning customer';
-$_['text_forgotten']               = 'Forgotten Password';
+$_['text_new_customer']            = 'Neuer Kunde';
+$_['text_register']                = 'Konto registrieren';
+$_['text_register_account']        = 'Durch die Erstellung eines Kontos können Sie schneller einkaufen, sind über den Status einer Bestellung auf dem Laufenden und können Ihre vorherigen Bestellungen nachverfolgen.';
+$_['text_returning_customer']      = 'Wiederkehrender Kunde';
+$_['text_i_am_returning_customer'] = 'Ich bin ein wiederkehrender Kunde';
+$_['text_forgotten']               = 'Passwort vergessen';
 
 // Entry
-$_['entry_email']                  = 'E-Mail Address';
-$_['entry_password']               = 'Password';
+$_['entry_email']                  = 'E-Mail-Adresse';
+$_['entry_password']               = 'Passwort';
 
 // Error
-$_['error_token']                  = 'Warning: Invalid token session. Please login again!';
-$_['error_login']                  = 'Warning: No match for E-Mail Address and/or Password.';
-$_['error_attempts']               = 'Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.';
-$_['error_approved']               = 'Warning: Your account requires approval before you can login.';
+$_['error_token']                  = 'Achtung: Ungültige Token-Sitzung. Bitte erneut anmelden!';
+$_['error_login']                  = 'Achtung: Keine Übereinstimmung für E-Mail-Adresse und/oder Passwort.';
+$_['error_attempts']               = 'Warnung: Ihr Konto hat die zulässige Anzahl an Anmeldeversuchen überschritten. Bitte versuchen Sie es in 1 Stunde erneut.';
+$_['error_approved']               = 'Achtung: Ihr Konto muss genehmigt werden, bevor Sie sich anmelden können.';

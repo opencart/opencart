8427a9ed8086d4fd9ba7d4cd684073ea1b8c907c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:20:09 +0800

    New translations credit.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/total/credit.php
+++ de/upload/extension/opencart/admin/language/de-DE/total/credit.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Store Credit';
+$_['heading_title']    = 'Guthaben speichern';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified store credit total!';
-$_['text_edit']        = 'Edit Store Credit Total';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben den Gesamtbetrag Ihres Guthabens geändert!';
+$_['text_edit']        = 'Gesamtguthaben bearbeiten';
 
 // Entry
 $_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify store credit total!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Gesamtbetrag Ihres Guthabens zu ändern!';

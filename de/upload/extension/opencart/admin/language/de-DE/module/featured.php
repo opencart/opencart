a0710733b229c7bf3060b1c6d735938ef21b4751
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:16 +0800

    New translations featured.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/module/featured.php
+++ de/upload/extension/opencart/admin/language/de-DE/module/featured.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']    = 'Featured';
+$_['heading_title']    = 'Empfohlen';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified featured module!';
-$_['text_edit']        = 'Edit Featured Module';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das vorgestellte Modul geändert!';
+$_['text_edit']        = 'Ausgewähltes Modul bearbeiten';
 $_['text_horizontal']  = 'Horizontal';
-$_['text_vertical']    = 'Vertical';
+$_['text_vertical']    = 'Vertikal';
 
 // Entry
-$_['entry_name']       = 'Module Name';
-$_['entry_product']    = 'Products';
-$_['entry_axis']       = 'Axis';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
+$_['entry_name']       = 'Modulname';
+$_['entry_product']    = 'Produkte';
+$_['entry_axis']       = 'Achse';
+$_['entry_width']      = 'Bildbreite';
+$_['entry_height']     = 'Bildhöhe';
 $_['entry_status']     = 'Status';
 
 // Help
-$_['help_product']     = '(Autocomplete)';
+$_['help_product']     = '(Autovervollständigung)';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify featured module!';
-$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
-$_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_permission'] = 'Warnung: Sie sind nicht berechtigt, das vorgestellte Modul zu ändern!';
+$_['error_name']       = 'Der Modulname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_width']      = 'Breite erforderlich!';
+$_['error_height']     = 'Körpergröße erforderlich!';

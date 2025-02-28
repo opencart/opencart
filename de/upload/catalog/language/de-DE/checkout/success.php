18f49ee40de276fa4b75c55142fc873ba32d4c37
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:38 +0800

    New translations success.php (German)

--- de/upload/catalog/language/de-DE/checkout/success.php
+++ de/upload/catalog/language/de-DE/checkout/success.php
@@ -1,10 +1,10 @@
 <?php
 // Heading
-$_['heading_title'] = 'Your order has been placed!';
+$_['heading_title'] = 'Ihre Bestellung wurde aufgegeben!';
 
 // Text
-$_['text_basket']   = 'Shopping Cart';
-$_['text_checkout'] = 'Checkout';
-$_['text_success']  = 'Success';
-$_['text_customer'] = '<p>Your order has been successfully processed!</p><p>You can view your order history by going to the <a href="%s">my account</a> page and by clicking on <a href="%s">history</a>.</p><p>If your purchase has an associated download, you can go to the account <a href="%s">downloads</a> page to view them.</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
-$_['text_guest']    = '<p>Your order has been successfully processed!</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
+$_['text_basket']   = 'Warenkorb';
+$_['text_checkout'] = 'Kasse';
+$_['text_success']  = 'Erfolg';
+$_['text_customer'] = '<p>Ihre Bestellung wurde erfolgreich bearbeitet!</p><p>Sie können Ihren Bestellverlauf anzeigen, indem Sie auf die Seite <a href="%s">Mein Konto</a> gehen und auf <a href="%s">Verlauf</a>klicken.</p><p>Wenn mit Ihrem Kauf ein Download verbunden ist, können Sie auf der Kontoseite <a href="%s">Downloads</a> diese anzeigen.</p><p>Fragen richten Sie bitte direkt an den <a href="%s">Shopinhaber</a>.</p><p>Danke, dass Sie bei uns online eingekauft haben!</p>';
+$_['text_guest']    = '<p>Ihre Bestellung wurde erfolgreich bearbeitet!</p><p>Bei Fragen wenden Sie sich bitte an den <a href="%s">Ladenbesitzer</a>.</p><p>Danke, dass Sie bei uns online eingekauft haben!</p>';

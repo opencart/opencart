02c0328966a7b2f306913979098ebcb60121f132
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:13:51 +0800

    New translations subscription_plan.php (German)

--- de/upload/admin/language/de-DE/catalog/subscription_plan.php
+++ de/upload/admin/language/de-DE/catalog/subscription_plan.php
@@ -1,48 +1,48 @@
 <?php
 // Heading
-$_['heading_title']          = 'Subscription Plans';
+$_['heading_title']          = 'Abonnements';
 
 // Text
-$_['text_success']           = 'Success: You have modified subscription plans!';
-$_['text_list']              = 'Subscription Plan List';
-$_['text_add']               = 'Add Subscription Plan';
-$_['text_edit']              = 'Edit Subscription Plan';
-$_['text_subscription']      = 'Subscription';
-$_['text_trial']             = 'Trial';
-$_['text_day']               = 'Day';
-$_['text_week']              = 'Week';
-$_['text_semi_month']        = 'Semi Month';
-$_['text_month']             = 'Month';
-$_['text_year']              = 'Year';
+$_['text_success']           = 'Erfolg: Sie haben Abonnementpläne geändert!';
+$_['text_list']              = 'Liste der Abonnementpläne';
+$_['text_add']               = 'Abonnementplan hinzufügen';
+$_['text_edit']              = 'Abonnementplan bearbeiten';
+$_['text_subscription']      = 'Abonnement';
+$_['text_trial']             = 'Versuch';
+$_['text_day']               = 'Tag';
+$_['text_week']              = 'Woche';
+$_['text_semi_month']        = 'Halbmonat';
+$_['text_month']             = 'Monat';
+$_['text_year']              = 'Jahr';
 
 // Entry
-$_['entry_name']             = 'Subscription Plan Name';
-$_['entry_trial_duration']   = 'Trial Duration';
-$_['entry_trial_cycle']      = 'Trial Cycle';
-$_['entry_trial_frequency']  = 'Trial Frequency';
-$_['entry_trial_status']     = 'Trial Status';
-$_['entry_duration']         = 'Duration';
-$_['entry_cycle']            = 'Cycle';
-$_['entry_frequency']        = 'Frequency';
+$_['entry_name']             = 'Name des Abonnementplans';
+$_['entry_trial_duration']   = 'Dauer der Testversion';
+$_['entry_trial_cycle']      = 'Testzyklus';
+$_['entry_trial_frequency']  = 'Häufigkeit der Versuche';
+$_['entry_trial_status']     = 'Status der Studie';
+$_['entry_duration']         = 'Dauer';
+$_['entry_cycle']            = 'Zyklus';
+$_['entry_frequency']        = 'Frequenz';
 $_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Sort Order';
+$_['entry_sort_order']       = 'Sortierreihenfolge';
 
 // Column
-$_['column_name']            = 'Subscription Plan Name';
-$_['column_sort_order']      = 'Sort Order';
-$_['column_action']          = 'Action';
+$_['column_name']            = 'Name des Abonnementplans';
+$_['column_sort_order']      = 'Sortierreihenfolge';
+$_['column_action']          = 'Aktion';
 
 // Help
-$_['help_trial_duration']    = 'The duration is the number of times the user will make a payment.';
-$_['help_trial_cycle']       = 'Subscription amounts are calculated by the frequency and cycles.';
-$_['help_trial_frequency']   = 'If you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.';
-$_['help_duration']          = 'The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.';
-$_['help_cycle']             = 'Subscription amounts are calculated by the frequency and cycles.';
-$_['help_frequency']         = 'If you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.';
+$_['help_trial_duration']    = 'Die Dauer gibt an, wie oft der Benutzer eine Zahlung vornimmt.';
+$_['help_trial_cycle']       = 'Die Abonnementbeträge werden anhand der Häufigkeit und Zyklen berechnet.';
+$_['help_trial_frequency']   = 'Wenn Sie eine Häufigkeit von „Woche“ und einen Zyklus von „2“ verwenden, wird dem Benutzer alle 2 Wochen eine Rechnung gestellt.';
+$_['help_duration']          = 'Die Dauer gibt an, wie oft der Benutzer eine Zahlung tätigt. Setzen Sie diesen Wert auf 0, wenn Sie Zahlungen wünschen, bis sie storniert werden.';
+$_['help_cycle']             = 'Die Abonnementbeträge werden anhand der Häufigkeit und Zyklen berechnet.';
+$_['help_frequency']         = 'Wenn Sie eine Häufigkeit von „Woche“ und einen Zyklus von „2“ verwenden, wird dem Benutzer alle 2 Wochen eine Rechnung gestellt.';
 
 // Error
-$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']       = 'Warning: You do not have permission to modify subscription plans!';
-$_['error_name']             = 'Subscription Plan Name must be greater than 3 and less than 255 characters!';
-$_['error_trial_duration']   = 'Trial duration must be greater than 0!';
-$_['error_product']          = 'Warning: This subscription plans cannot be deleted as it is currently assigned to %s products!';
+$_['error_warning']          = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']       = 'Achtung: Sie sind nicht berechtigt, Abonnements zu ändern!';
+$_['error_name']             = 'Der Name des Abonnementplans muss länger als 3 und kürzer als 255 Zeichen sein!';
+$_['error_trial_duration']   = 'Die Testdauer muss größer als 0 sein!';
+$_['error_product']          = 'Achtung: Diese Abonnements können nicht gelöscht werden, da sie derzeit %s Produkten zugewiesen sind!';

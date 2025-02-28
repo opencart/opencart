75d88e0697efc0d0472c27f8830924940d9dae37
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:11 +0800

    New translations cart.php (German)

--- de/upload/catalog/language/de-DE/api/cart.php
+++ de/upload/catalog/language/de-DE/api/cart.php
@@ -1,15 +1,15 @@
 <?php
 // Text
-$_['text_success']       = 'Success: You have modified your shopping cart!';
-$_['text_subscription']  = 'Subscription';
+$_['text_success']       = 'Erfolg: Sie haben Ihren Einkaufswagen geändert!';
+$_['text_subscription']  = 'Abonnement';
 
 // Error
-$_['error_warning']      = 'Warning: Please check the cart carefully for errors!';
-$_['error_product']      = 'Warning: Product could not be found!';
-$_['error_stock']        = 'Product not available in the desired quantity or not in stock!';
-$_['error_minimum']      = 'Minimum order amount for %s is %s!';
-$_['error_option']       = 'Option could not be found!';
-$_['error_option_stock'] = 'Option not available in the desired quantity or not in stock!';
-$_['error_required']     = '%s required!';
-$_['error_regex']        = '%s is not a valid input!';
-$_['error_subscription'] = 'Subscription Plan is not valid!';
+$_['error_warning']      = 'Achtung: Bitte prüfen Sie den Warenkorb sorgfältig auf Fehler!';
+$_['error_product']      = 'Achtung: Produkt konnte nicht gefunden werden!';
+$_['error_stock']        = 'Artikel nicht in der gewünschten Menge verfügbar oder nicht auf Lager!';
+$_['error_minimum']      = 'Der Mindestbestellwert für %s beträgt %s!';
+$_['error_option']       = 'Option konnte nicht gefunden werden!';
+$_['error_option_stock'] = 'Option nicht in der gewünschten Menge verfügbar oder nicht auf Lager!';
+$_['error_required']     = '%s erforderlich!';
+$_['error_regex']        = '%s ist keine gültige Eingabe!';
+$_['error_subscription'] = 'Der Abonnementplan ist ungültig!';

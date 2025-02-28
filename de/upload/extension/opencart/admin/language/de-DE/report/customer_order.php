f14963206506d81324e33e0a65da95d396e460e2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:19:38 +0800

    New translations customer_order.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/report/customer_order.php
+++ de/upload/extension/opencart/admin/language/de-DE/report/customer_order.php
@@ -1,31 +1,31 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Orders Report';
+$_['heading_title']         = 'Bericht zu Kundenbestellungen';
 
 // Text
-$_['text_extension']        = 'Extensions';
-$_['text_edit']             = 'Edit Customer Orders Report';
-$_['text_success']          = 'Success: You have modified customer orders report!';
+$_['text_extension']        = 'Erweiterungen';
+$_['text_edit']             = 'Bericht zu Kundenbestellungen bearbeiten';
+$_['text_success']          = 'Erfolg: Sie haben den Bericht zu Kundenaufträgen geändert!';
 $_['text_filter']           = 'Filter';
-$_['text_all_status']       = 'All Statuses';
+$_['text_all_status']       = 'Alle Status';
 
 // Column
-$_['column_customer']       = 'Customer Name';
+$_['column_customer']       = 'Kundenname';
 $_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
+$_['column_customer_group'] = 'Kundengruppe';
 $_['column_status']         = 'Status';
-$_['column_orders']         = 'No. Orders';
-$_['column_products']       = 'No. Products';
-$_['column_total']          = 'Total';
-$_['column_action']         = 'Action';
+$_['column_orders']         = 'Anzahl Bestellungen';
+$_['column_products']       = 'Nr. Produkte';
+$_['column_total']          = 'Gesamt';
+$_['column_action']         = 'Aktion';
 
 // Entry
-$_['entry_date_start']      = 'Date Start';
-$_['entry_date_end']        = 'Date End';
-$_['entry_customer']        = 'Customer';
-$_['entry_order_status']    = 'Order Status';
+$_['entry_date_start']      = 'Datum Beginn';
+$_['entry_date_end']        = 'Datum Ende';
+$_['entry_customer']        = 'Kunde';
+$_['entry_order_status']    = 'Bestellstatus';
 $_['entry_status']          = 'Status';
-$_['entry_sort_order']      = 'Sort Order';
+$_['entry_sort_order']      = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission']      = 'Warning: You do not have permission to modify customer orders report!';
+$_['error_permission']      = 'Achtung: Sie sind nicht berechtigt, den Bericht zu Kundenbestellungen zu ändern!';

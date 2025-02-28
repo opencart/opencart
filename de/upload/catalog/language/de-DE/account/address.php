d86b5e9c591f89c162f92bf2bdcd5889d20cff84
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:16:39 +0800

    New translations address.php (German)

--- de/upload/catalog/language/de-DE/account/address.php
+++ de/upload/catalog/language/de-DE/account/address.php
@@ -1,40 +1,40 @@
 <?php
 // Heading
-$_['heading_title']      = 'Address Book';
+$_['heading_title']      = 'Adressbuch';
 
 // Text
-$_['text_account']       = 'Account';
-$_['text_address_book']  = 'Address Book Entries';
-$_['text_address_add']   = 'Add Address';
-$_['text_address_edit']  = 'Edit Address';
-$_['text_add']           = 'Your address has been successfully added';
-$_['text_edit']          = 'Your address has been successfully updated';
-$_['text_delete']        = 'Your address has been successfully deleted';
-$_['text_no_results']    = 'You have no addresses in your account.';
+$_['text_account']       = 'Konto';
+$_['text_address_book']  = 'Adressbucheinträge';
+$_['text_address_add']   = 'Adresse hinzufügen';
+$_['text_address_edit']  = 'Adresse bearbeiten';
+$_['text_add']           = 'Ihre Adresse wurde erfolgreich hinzugefügt';
+$_['text_edit']          = 'Ihre Adresse wurde erfolgreich aktualisiert';
+$_['text_delete']        = 'Ihre Adresse wurde erfolgreich gelöscht';
+$_['text_no_results']    = 'Sie haben keine Adressen in Ihrem Konto.';
 
 // Entry
-$_['entry_firstname']    = 'First Name';
-$_['entry_lastname']     = 'Last Name';
-$_['entry_company']      = 'Company';
-$_['entry_address_1']    = 'Address 1';
-$_['entry_address_2']    = 'Address 2';
-$_['entry_postcode']     = 'Post Code';
-$_['entry_city']         = 'City';
-$_['entry_country']      = 'Country';
-$_['entry_zone']         = 'Region / State';
-$_['entry_default']      = 'Default Address';
+$_['entry_firstname']    = 'Vorname';
+$_['entry_lastname']     = 'Nachname';
+$_['entry_company']      = 'Unternehmen';
+$_['entry_address_1']    = 'Adresse 1';
+$_['entry_address_2']    = 'Adresse 2';
+$_['entry_postcode']     = 'Postleitzahl';
+$_['entry_city']         = 'Stadt';
+$_['entry_country']      = 'Land';
+$_['entry_zone']         = 'Region/Staat';
+$_['entry_default']      = 'Standardadresse';
 
 // Error
-$_['error_token']        = 'Warning: Address token invalid!';
-$_['error_subscription'] = 'Warning: Address is still being used by %s active subscriptions!';
-$_['error_default']      = 'Warning: Default address required!';
-$_['error_delete']       = 'Warning: You must have at least one address!';
-$_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
-$_['error_address_1']    = 'Address must be between 3 and 128 characters!';
-$_['error_postcode']     = 'Postcode must be between 2 and 10 characters!';
-$_['error_city']         = 'City must be between 2 and 128 characters!';
-$_['error_country']      = 'Please select a country!';
-$_['error_zone']         = 'Please select a region / state!';
-$_['error_custom_field'] = '%s required!';
-$_['error_regex']        = '%s is not a valid input!';
+$_['error_token']        = 'Achtung: Adress-Token ungültig!';
+$_['error_subscription'] = 'Warnung: Die Adresse wird immer noch von %s aktiven Abonnements verwendet!';
+$_['error_default']      = 'Achtung: Standardadresse erforderlich!';
+$_['error_delete']       = 'Achtung: Sie müssen mindestens eine Adresse haben!';
+$_['error_firstname']    = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']     = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_address_1']    = 'Die Adresse muss zwischen 3 und 128 Zeichen lang sein!';
+$_['error_postcode']     = 'Die Postleitzahl muss zwischen 2 und 10 Zeichen lang sein!';
+$_['error_city']         = 'Die Stadt muss zwischen 2 und 128 Zeichen lang sein!';
+$_['error_country']      = 'Bitte wählen Sie ein Land!';
+$_['error_zone']         = 'Bitte wählen Sie eine Region / ein Bundesland!';
+$_['error_custom_field'] = '%s erforderlich!';
+$_['error_regex']        = '%s ist keine gültige Eingabe!';

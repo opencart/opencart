8e99e1d2a95a453a6993981a108707d27990df0a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:20:08 +0800

    New translations coupon.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/total/coupon.php
+++ de/upload/extension/opencart/admin/language/de-DE/total/coupon.php
@@ -3,14 +3,14 @@
 $_['heading_title']    = 'Coupon';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified coupon total!';
-$_['text_edit']        = 'Edit Coupon Total';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben den Gutscheinbetrag geändert!';
+$_['text_edit']        = 'Coupon-Gesamtbetrag bearbeiten';
 
 // Entry
 $_['entry_coupon']     = 'Coupon';
 $_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify coupon total!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Gutscheinbetrag zu ändern!';

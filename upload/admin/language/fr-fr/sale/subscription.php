1111324083052f46337217236262eea95daf32e7
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2025-02-28 11:21:34 -0500

    Update subscription.php
    Updated subscription definitions.

--- upload/admin/language/fr-fr/sale/subscription.php
+++ upload/admin/language/fr-fr/sale/subscription.php
@@ -9,13 +9,13 @@ $_['text_add']                   = 'Ajouter un Abonnement';
 $_['text_edit']                  = 'Abonnement (#%s)';
 $_['text_filter']                = 'Filtrer';
 $_['text_date_added']            = 'Date d\'Ajout';
-$_['text_order']                 = 'ID de Commande';
-$_['text_customer']              = 'Client';
-$_['text_subscription_plan']     = 'Plan d\'Abonnement';
-$_['text_product']               = 'Détails du Produit';
-$_['text_quantity']              = 'Quantité';
-$_['text_trial']                 = 'Essai';
+$_['text_order_id']              = 'ID de Commande';
+$_['text_order']                 = 'Commande';
+$_['text_product_add']           = 'Ajouter un Produit';
+$_['text_model'] 			     = 'Modèle';
 $_['text_subscription']          = 'Abonnement';
+$_['text_date_next']             = 'Prochaine Date';
+$_['text_remaining']             = 'Restant';
 $_['text_subscription_trial']    = '%s tous les %d %s(s) pendant %d paiement(s) puis ';
 $_['text_subscription_duration'] = '%s tous les %d %s(s) pendant %d paiement(s)';
 $_['text_subscription_cancel']   = '%s tous les %d %s(s) jusqu\'à annulation';
@@ -25,14 +25,13 @@ $_['text_week']                  = 'Semaine';
 $_['text_semi_month']            = 'Demi-Mois';
 $_['text_month']                 = 'Mois';
 $_['text_year']                  = 'Année';
-$_['text_date_next']             = 'Prochaine Date';
-$_['text_remaining']             = 'Restant';
-$_['text_payment_address']       = 'Adresse de Paiement';
 $_['text_payment_method']        = 'Méthode de Paiement';
-$_['text_shipping_address']      = 'Adresse de Livraison';
+$_['text_payment']               = 'Veuillez svp sélectionner la préférence de la méthode de paiement pour cet abonnement.';
 $_['text_shipping_method']       = 'Méthode de Livraison';
+$_['text_shipping']              = 'Veuillez svp sélectionner la préférence de la méthode de livraison pour cet abonnement.';
 $_['text_history']               = 'Historique';
 $_['text_history_add']           = 'Ajouter à l\'Historique';
+$_['text_log']                   = 'Rapports';
 
 // Colonne
 $_['column_subscription_id']     = 'ID d\'Abonnement';
@@ -43,13 +42,12 @@ $_['column_notify']              = 'Client Notifié';
 $_['column_status']              = 'Statut';
 $_['column_date_added']          = 'Date d\'Ajout';
 $_['column_product']             = 'Détails du Produit';
-$_['column_model']               = 'Modèle';
 $_['column_quantity']            = 'Quantité';
-$_['column_trial_price']         = 'Prix de l\'Essai';
+$_['column_trial_price']         = 'Prix d\'Essai';
 $_['column_price']               = 'Prix Unitaire';
-$_['column_total']               = 'Total';
 $_['column_amount']              = 'Montant';
 $_['column_code']                = 'Code';
+$_['column_total']               = 'Total';
 $_['column_description']         = 'Description';
 $_['column_action']              = 'Action';
 
@@ -67,17 +65,16 @@ $_['entry_notify']               = 'Notifier le Client';
 $_['entry_date_from']            = 'Date de Début';
 $_['entry_date_to']              = 'Date de Fin';
 $_['entry_subscription_status']  = 'Statut de l\'Abonnement';
-$_['entry_product']              = 'Choisir le Produit';
-$_['entry_option']               = 'Choisir les Options';
+$_['entry_product']              = 'Veuillez Choisir le Produit';
+$_['entry_option']               = 'Veuillez Choisir les Options';
 $_['entry_quantity']             = 'Quantité';
 $_['entry_payment_address']      = 'Adresse de Paiement';
-$_['entry_shipping_address']     = 'Adresse de Livraison';
+$_['entry_shipping_address']     = 'Addresse de Livraison';
 
 // Onglet
 $_['tab_order']                  = 'Commandes';
 
 // Erreur
-$_['error_permission']           = 'Attention : Vous n\'avez pas la permission de modifier les abonnements!';
-$_['error_subscription_status']  = 'Attention : Le statut de l\'abonnement doit être sélectionné!';
-$_['error_payment_method']       = 'Attention : La méthode de paiement n\'existe pas!';
-$_['error_subscription']         = 'Attention : L\'abonnement n\'est pas encore ajouté!';
+$_['error_permission']           = 'Attention: Vous n\'avez pas la permission de modifier les abonnements!';
+$_['error_subscription_status']  = 'Attention: Le statut de l\'abonnement doit être sélectionné!';
+$_['error_payment_method']       = 'Attention: La méthode de paiement n\'existe pas!';

2f0c9f86bdf7f65f311543229b5b27b2bc216c85
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 04:15:42 +0800

    New translations language.php (English, United Kingdom)

---
+++ en-GB/upload/admin/language/en-gb/extension/language.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'Languages';
+
+// Text
+$_['text_success']     = 'Success: You have modified languages!';
+$_['text_list']        = 'Language List';
+
+// Column
+$_['column_name']      = 'Language Name';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Action';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify languages!';
+$_['error_extension']  = 'Warning: Extension does not exist!';
+$_['error_default']    = 'Warning: This extension cannot be uninstalled as it is currently assigned as the administration language!';

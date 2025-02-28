5d68e59d97f498bb0e582a82a19aee2e46aa0e9e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 04:17:02 +0800

    New translations gdpr_deny.php (German)

---
+++ de/upload/admin/language/de-DE/mail/gdpr_deny.php
@@ -0,0 +1,12 @@
+<?php
+// Text
+$_['text_subject']   = '%s - GDPR request denied!';
+$_['text_export']    = 'Account Export Data Request';
+$_['text_remove']    = 'Account Deletion Request';
+$_['text_hello']     = 'Hello <strong>%s</strong>,';
+$_['text_user']      = 'User';
+$_['text_contact']   = 'Unfortunately your request has been denied. For more information you can contact the store here:';
+$_['text_thanks']    = 'Thanks,';
+
+// Button
+$_['button_contact'] = 'Contact Us';

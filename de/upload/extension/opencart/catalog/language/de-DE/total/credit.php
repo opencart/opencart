2d16ffdd8439a3e52193f74b6a0b7c3ab5012fd9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:21:01 +0800

    New translations credit.php (German)

--- de/upload/extension/opencart/catalog/language/de-DE/total/credit.php
+++ de/upload/extension/opencart/catalog/language/de-DE/total/credit.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_credit']   = 'Store Credit';
-$_['text_order_id'] = 'Order ID: #%s';
+$_['text_credit']   = 'Guthaben speichern';
+$_['text_order_id'] = 'Bestellnummer: #%s';

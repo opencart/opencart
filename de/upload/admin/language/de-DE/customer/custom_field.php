3bbf74ee8bd1226f1347caafd46225f9ca5c451b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:14:15 +0800

    New translations custom_field.php (German)

--- de/upload/admin/language/de-DE/customer/custom_field.php
+++ de/upload/admin/language/de-DE/customer/custom_field.php
@@ -1,56 +1,56 @@
 <?php
 // Heading
-$_['heading_title']        = 'Custom Fields';
+$_['heading_title']        = 'Benutzerdefinierte Felder';
 
 // Text
-$_['text_success']         = 'Success: You have modified custom fields!';
-$_['text_list']            = 'Custom Field List';
-$_['text_add']             = 'Add Custom Field';
-$_['text_edit']            = 'Edit Custom Field';
-$_['text_choose']          = 'Choose';
-$_['text_select']          = 'Select';
+$_['text_success']         = 'Erfolg: Sie haben benutzerdefinierte Felder geändert!';
+$_['text_list']            = 'Benutzerdefinierte Feldliste';
+$_['text_add']             = 'Benutzerdefiniertes Feld hinzufügen';
+$_['text_edit']            = 'Benutzerdefiniertes Feld bearbeiten';
+$_['text_choose']          = 'Wählen';
+$_['text_select']          = 'Wählen';
 $_['text_radio']           = 'Radio';
-$_['text_checkbox']        = 'Checkbox';
-$_['text_input']           = 'Input';
+$_['text_checkbox']        = 'Kontrollkästchen';
+$_['text_input']           = 'Eingang';
 $_['text_text']            = 'Text';
-$_['text_textarea']        = 'Textarea';
-$_['text_file']            = 'File';
-$_['text_date']            = 'Date';
-$_['text_datetime']        = 'Date &amp; Time';
-$_['text_time']            = 'Time';
-$_['text_account']         = 'Account';
-$_['text_address']         = 'Address';
-$_['text_affiliate']       = 'Affiliate';
-$_['text_regex']           = 'Regex';
-$_['text_custom_field']    = 'Custom Field';
-$_['text_value']           = 'Custom Field Values';
+$_['text_textarea']        = 'Textbereich';
+$_['text_file']            = 'Datei';
+$_['text_date']            = 'Datum';
+$_['text_datetime']        = 'Datum &amp; Uhrzeit';
+$_['text_time']            = 'Zeit';
+$_['text_account']         = 'Konto';
+$_['text_address']         = 'Adresse';
+$_['text_affiliate']       = 'Partnerprogramm';
+$_['text_regex']           = 'Regulärer Ausdruck';
+$_['text_custom_field']    = 'Benutzerdefiniertes Feld';
+$_['text_value']           = 'Benutzerdefinierte Feldwerte';
 
 // Column
-$_['column_name']          = 'Custom Field Name';
-$_['column_location']      = 'Location';
-$_['column_type']          = 'Type';
+$_['column_name']          = 'Benutzerdefinierter Feldname';
+$_['column_location']      = 'Standort';
+$_['column_type']          = 'Typ';
 $_['column_status']        = 'Status';
-$_['column_sort_order']    = 'Sort Order';
-$_['column_action']        = 'Action';
+$_['column_sort_order']    = 'Sortierreihenfolge';
+$_['column_action']        = 'Aktion';
 
 // Entry
-$_['entry_name']           = 'Custom Field Name';
-$_['entry_location']       = 'Location';
-$_['entry_type']           = 'Type';
-$_['entry_value']          = 'Value';
-$_['entry_validation']     = 'Validation';
-$_['entry_custom_value']   = 'Custom Field Value Name';
-$_['entry_customer_group'] = 'Customer Group';
-$_['entry_required']       = 'Required';
+$_['entry_name']           = 'Benutzerdefinierter Feldname';
+$_['entry_location']       = 'Standort';
+$_['entry_type']           = 'Typ';
+$_['entry_value']          = 'Wert';
+$_['entry_validation']     = 'Validierung';
+$_['entry_custom_value']   = 'Name des benutzerdefinierten Feldwerts';
+$_['entry_customer_group'] = 'Kundengruppe';
+$_['entry_required']       = 'Erforderlich';
 $_['entry_status']         = 'Status';
-$_['entry_sort_order']     = 'Sort Order';
+$_['entry_sort_order']     = 'Sortierreihenfolge';
 
 // Help
-$_['help_regex']           = 'Use regex. E.g: /[^a-zA-Z0-9_-]/';
-$_['help_sort_order']      = 'Use minus to count backwards from the last field in the set.';
+$_['help_regex']           = 'Verwenden Sie reguläre Ausdrücke. Beispiel: /[^a-zA-Z0-9_-]/';
+$_['help_sort_order']      = 'Verwenden Sie das Minuszeichen, um vom letzten Feld im Satz rückwärts zu zählen.';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modify custom fields!';
-$_['error_name']           = 'Custom Field Name must be between 1 and 128 characters!';
-$_['error_type']           = 'Warning: Custom Field Values required!';
-$_['error_custom_value']   = 'Custom Value Name must be between 1 and 128 characters!';
+$_['error_permission']     = 'Achtung: Sie sind nicht berechtigt, benutzerdefinierte Felder zu ändern!';
+$_['error_name']           = 'Der Name des benutzerdefinierten Felds muss zwischen 1 und 128 Zeichen lang sein!';
+$_['error_type']           = 'Achtung: Benutzerdefinierte Feldwerte erforderlich!';
+$_['error_custom_value']   = 'Der benutzerdefinierte Wertname muss zwischen 1 und 128 Zeichen lang sein!';

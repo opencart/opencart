5934bdc72977d2fa8e18da9582c4434354f1c82a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:20:02 +0800

    New translations pickup.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/shipping/pickup.php
+++ de/upload/extension/opencart/admin/language/de-DE/shipping/pickup.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']    = 'Pickup From Store';
+$_['heading_title']    = 'Abholung im Geschäft';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified pickup from store!';
-$_['text_edit']        = 'Edit Pickup From Store Shipping';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben die Abholung im Geschäft geändert!';
+$_['text_edit']        = 'Abholung im Geschäft bearbeiten – Versand';
 
 // Entry
-$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_geo_zone']   = 'Geozone';
 $_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify pickup from store!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, die Abholung im Geschäft zu ändern!';

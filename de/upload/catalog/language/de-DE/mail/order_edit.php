81911cfc17ab98a60b4393e87ce432f10a6d8ea1
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:22 +0800

    New translations order_edit.php (German)

--- de/upload/catalog/language/de-DE/mail/order_edit.php
+++ de/upload/catalog/language/de-DE/mail/order_edit.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_subject']      = '%s - Order Update %s';
-$_['text_order_id']     = 'Order ID:';
-$_['text_date_added']   = 'Date Added:';
-$_['text_order_status'] = 'Your order has been updated to the following status:';
-$_['text_comment']      = 'The comments for your order are:';
-$_['text_link']         = 'To view your order click on the link below:';
-$_['text_footer']       = 'Please reply to this email if you have any questions.';
+$_['text_subject']      = '%s - Bestellaktualisierung %s';
+$_['text_order_id']     = 'Bestellnummer:';
+$_['text_date_added']   = 'Datum hinzugefügt:';
+$_['text_order_status'] = 'Ihre Bestellung wurde auf den folgenden Status aktualisiert:';
+$_['text_comment']      = 'Die Bemerkungen zu Ihrer Bestellung lauten:';
+$_['text_link']         = 'Um Ihre Bestellung anzuzeigen, klicken Sie auf den folgenden Link:';
+$_['text_footer']       = 'Bitte antworten Sie auf diese E-Mail, wenn Sie Fragen haben.';

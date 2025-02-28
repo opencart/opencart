c0d0bbf31cde16ad1fc4cfd89ce446f7775c3fbe
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:20:19 +0800

    New translations tax.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/total/tax.php
+++ de/upload/extension/opencart/admin/language/de-DE/total/tax.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']    = 'Taxes';
+$_['heading_title']    = 'Steuern';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified taxes total!';
-$_['text_edit']        = 'Edit Tax Total';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben die Steuern insgesamt geändert!';
+$_['text_edit']        = 'Steuersumme bearbeiten';
 
 // Entry
 $_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify taxes total!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Steuerbetrag zu ändern!';

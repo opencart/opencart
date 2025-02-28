f820f5790243cbdaf5cb01c7b9ceaaaa6e8380a8
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 05:13:06 +0800

    New translations notification.php (English, United Kingdom)

---
+++ en-GB/upload/admin/language/en-gb/tool/notification.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Notifications';
+
+// Text
+$_['text_success']     = 'Success: You have modified notifications!';
+$_['text_list']        = 'Notification List';
+
+// Column
+$_['column_message']   = 'Message';
+$_['column_action']    = 'Action';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify notifications!';

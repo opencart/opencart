94fac5519af5da1808362facbb335e911fa290b0
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:13:54 +0800

    New translations article.php (German)

--- de/upload/admin/language/de-DE/cms/article.php
+++ de/upload/admin/language/de-DE/cms/article.php
@@ -1,48 +1,48 @@
 <?php
 // Heading
-$_['heading_title']           = 'Articles';
+$_['heading_title']           = 'Artikel';
 
 // Text
-$_['text_success']            = 'Success: You have modified articles!';
-$_['text_next']               = 'Success: You have modified %s to %s of %s article ratings!';
-$_['text_list']               = 'Article List';
-$_['text_add']                = 'Add Article';
-$_['text_edit']               = 'Edit Article';
-$_['text_default']            = 'Default';
-$_['text_keyword']            = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['text_success']            = 'Erfolg: Sie haben Artikel geändert!';
+$_['text_next']               = 'Erfolg: Sie haben %s bis %s von %s Artikelbewertungen geändert!';
+$_['text_list']               = 'Artikelliste';
+$_['text_add']                = 'Artikel hinzufügen';
+$_['text_edit']               = 'Artikel bearbeiten';
+$_['text_default']            = 'Standard';
+$_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL weltweit eindeutig ist.';
 
 // Column
-$_['column_name']             = 'Article Name';
-$_['column_author']           = 'Author';
-$_['column_rating']           = 'Rating';
-$_['column_date_added']       = 'Date Added';
-$_['column_action']           = 'Action';
+$_['column_name']             = 'Artikelname';
+$_['column_author']           = 'Autor';
+$_['column_rating']           = 'Bewertungen';
+$_['column_date_added']       = 'Datum hinzugefügt';
+$_['column_action']           = 'Aktion';
 
 // Entry
-$_['entry_image']             = 'Image';
-$_['entry_name']              = 'Article Name';
-$_['entry_description']       = 'Description';
-$_['entry_tag']               = 'Tags';
-$_['entry_meta_title']        = 'Meta Tag Title';
-$_['entry_meta_keyword']      = 'Meta Tag Keywords';
-$_['entry_meta_description']  = 'Meta Tag Description';
-$_['entry_topic']             = 'Topic';
-$_['entry_author']            = 'Author';
-$_['entry_store']             = 'Stores';
-$_['entry_sort_order']        = 'Sort Order';
+$_['entry_image']             = 'Bild';
+$_['entry_name']              = 'Artikelname';
+$_['entry_description']       = 'Beschreibung';
+$_['entry_tag']               = 'Schlagwörter';
+$_['entry_meta_title']        = 'Meta-Tag-Titel';
+$_['entry_meta_keyword']      = 'Meta-Tag-Schlüsselwörter';
+$_['entry_meta_description']  = 'Meta-Tag-Beschreibung';
+$_['entry_topic']             = 'Thema';
+$_['entry_author']            = 'Autor';
+$_['entry_store']             = 'Geschäfte';
+$_['entry_sort_order']        = 'Sortierreihenfolge';
 $_['entry_status']            = 'Status';
-$_['entry_keyword']           = 'Keyword';
-$_['entry_layout']            = 'Layout Override';
+$_['entry_keyword']           = 'Stichwort';
+$_['entry_layout']            = 'Layoutüberschreibung';
 
 // Button
-$_['button_rating']           = 'Calculate Ratings';
+$_['button_rating']           = 'Bewertungen berechnen';
 
 // Error
-$_['error_warning']           = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']        = 'Warning: You do not have permission to modify articles!';
-$_['error_name']              = 'Article Name must be between 1 and 255 characters!';
-$_['error_meta_title']        = 'Meta Title must be greater than 1 and less than 255 characters!';
-$_['error_keyword']           = 'SEO URL must be between 1 and 64 characters!';
-$_['error_keyword_exists']    = 'SEO URL must be unique!';
-$_['error_keyword_character'] = 'Keyword can only use characters a-z, 0-9, - and _!';
-$_['error_author']            = 'Author must be between 3 and 64 characters!';
+$_['error_warning']           = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']        = 'Achtung: Sie haben keine Berechtigung, Artikel zu ändern!';
+$_['error_name']              = 'Der Artikelname muss zwischen 1 und 255 Zeichen lang sein!';
+$_['error_meta_title']        = 'Der Meta-Titel muss größer als 1 und kleiner als 255 Zeichen sein!';
+$_['error_keyword']           = 'Die SEO-URL muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_keyword_exists']    = 'SEO-URL muss eindeutig sein!';
+$_['error_keyword_character'] = 'Das Schlüsselwort darf nur die Zeichen az, 0-9, - und _ enthalten!';
+$_['error_author']            = 'Der Autor muss zwischen 3 und 64 Zeichen lang sein!';

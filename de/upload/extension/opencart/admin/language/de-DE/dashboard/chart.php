5602e3b4216484999afb699beeacf6259e0ced63
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:56 +0800

    New translations chart.php (German)

--- de/upload/extension/opencart/admin/language/de-DE/dashboard/chart.php
+++ de/upload/extension/opencart/admin/language/de-DE/dashboard/chart.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']    = 'Sales Analytics';
+$_['heading_title']    = 'Verkaufsanalysen';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified dashboard chart!';
-$_['text_edit']        = 'Edit Dashboard Chart';
-$_['text_order']       = 'Orders';
-$_['text_customer']    = 'Customers';
-$_['text_day']         = 'Today';
-$_['text_week']        = 'Week';
-$_['text_month']       = 'Month';
-$_['text_year']        = 'Year';
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das Dashboard-Diagramm geändert!';
+$_['text_edit']        = 'Dashboard-Diagramm bearbeiten';
+$_['text_order']       = 'Bestellungen';
+$_['text_customer']    = 'Kunden';
+$_['text_day']         = 'Heute';
+$_['text_week']        = 'Woche';
+$_['text_month']       = 'Monat';
+$_['text_year']        = 'Jahr';
 
 // Entry
 $_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+$_['entry_width']      = 'Breite';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard chart!';
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Dashboard-Diagramme zu ändern!';

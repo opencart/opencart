ac34002d4dfcbe7e045eb3fa6a56f033addafcbe
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2025-02-28 09:15:46 -0500

    Update subscription.php
    Added new language definitions.

--- upload/admin/language/fr-fr/sale/subscription.php
+++ upload/admin/language/fr-fr/sale/subscription.php
@@ -39,57 +39,45 @@ $_['column_subscription_id']     = 'ID d\'Abonnement';
 $_['column_order_id']            = 'ID de Commande';
 $_['column_customer']            = 'Client';
 $_['column_comment']             = 'Commentaire';
-$_['column_description']         = 'Description';
-$_['column_amount']              = 'Montant';
 $_['column_notify']              = 'Client Notifié';
 $_['column_status']              = 'Statut';
 $_['column_date_added']          = 'Date d\'Ajout';
 $_['column_product']             = 'Détails du Produit';
+$_['column_model']               = 'Modèle';
 $_['column_quantity']            = 'Quantité';
+$_['column_trial_price']         = 'Prix de l\'Essai';
+$_['column_price']               = 'Prix Unitaire';
 $_['column_total']               = 'Total';
+$_['column_amount']              = 'Montant';
+$_['column_code']                = 'Code';
+$_['column_description']         = 'Description';
 $_['column_action']              = 'Action';
 
 // Entrée
-$_['entry_customer']             = 'Client';
 $_['entry_subscription_id']      = 'ID d\'Abonnement';
 $_['entry_order_id']             = 'ID de Commande';
+$_['entry_customer']             = 'Client';
+$_['entry_store']                = 'Magasin';
+$_['entry_language']             = 'Langage';
+$_['entry_currency']             = 'Devise';
 $_['entry_subscription_plan']    = 'Plan d\'Abonnement';
-$_['entry_trial_price']          = 'Prix de l\'Essai';
-$_['entry_trial_duration']       = 'Durée de l\'Essai';
-$_['entry_trial_remaining']      = 'Essais Restants';
-$_['entry_trial_cycle']          = 'Cycle d\'Essai';
-$_['entry_trial_frequency']      = 'Fréquence d\'Essai';
-$_['entry_trial_status']         = 'Statut de l\'Essai';
-$_['entry_price']                = 'Prix';
-$_['entry_duration']             = 'Durée';
-$_['entry_remaining']            = 'Restant';
-$_['entry_cycle']                = 'Cycle';
-$_['entry_frequency']            = 'Fréquence';
 $_['entry_date_next']            = 'Prochaine Date';
 $_['entry_comment']              = 'Commentaire';
-$_['entry_amount']               = 'Montant';
 $_['entry_notify']               = 'Notifier le Client';
-$_['entry_override']             = 'Remplacer';
 $_['entry_date_from']            = 'Date de Début';
 $_['entry_date_to']              = 'Date de Fin';
 $_['entry_subscription_status']  = 'Statut de l\'Abonnement';
-
-// Aide
-$_['help_trial_duration']        = 'La durée correspond au nombre de fois où l\'utilisateur effectuera un paiement.';
-$_['help_trial_cycle']           = 'Les montants des abonnements sont calculés en fonction de la fréquence et des cycles.';
-$_['help_trial_frequency']       = 'Si vous utilisez une fréquence de "semaine" et un cycle de "2", l\'utilisateur sera facturé toutes les 2 semaines.';
-$_['help_duration']              = 'La durée est le nombre de fois où l\'utilisateur effectuera un paiement, définissez-la à 0 pour des paiements jusqu\'à annulation.';
-$_['help_cycle']                 = 'Les montants des abonnements sont calculés en fonction de la fréquence et des cycles.';
-$_['help_frequency']             = 'Si vous utilisez une fréquence de "semaine" et un cycle de "2", l\'utilisateur sera facturé toutes les 2 semaines.';
+$_['entry_product']              = 'Choisir le Produit';
+$_['entry_option']               = 'Choisir les Options';
+$_['entry_quantity']             = 'Quantité';
+$_['entry_payment_address']      = 'Adresse de Paiement';
+$_['entry_shipping_address']     = 'Adresse de Livraison';
 
 // Onglet
 $_['tab_order']                  = 'Commandes';
 
 // Erreur
-$_['error_permission']           = 'Attention: Vous n\'avez pas la permission de modifier les abonnements!';
-$_['error_status']               = 'Erreur: Le statut de l\'abonnement ne correspond pas au statut de la boutique!';
-$_['error_subscription']         = 'Attention: L\'abonnement n\'existe pas!';
-$_['error_subscription_plan']    = 'Attention: Le plan d\'abonnement n\'existe pas!';
-$_['error_subscription_status']  = 'Attention: Le statut de l\'abonnement doit être sélectionné!';
-$_['error_payment_method']       = 'Attention: La méthode de paiement n\'existe pas!';
-$_['error_service_type']		 = 'Le statut du service n\'a pas été inclus dans cette transaction. Si vous voyez ce message d\'erreur, veuillez contacter votre développeur d\'extension qui gère les services d\'abonnement pour résoudre ce problème!';
+$_['error_permission']           = 'Attention : Vous n\'avez pas la permission de modifier les abonnements!';
+$_['error_subscription_status']  = 'Attention : Le statut de l\'abonnement doit être sélectionné!';
+$_['error_payment_method']       = 'Attention : La méthode de paiement n\'existe pas!';
+$_['error_subscription']         = 'Attention : L\'abonnement n\'est pas encore ajouté!';

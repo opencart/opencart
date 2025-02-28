56f55208ae0a2a57540aa4f9f502d1bf9be284f9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:21:24 +0800

    New translations default.php (German)

---
+++ de/upload/install/language/de-DE/default.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+$_['text_name']       = 'Englisch (Vereinigtes Königreich)';
+$_['text_loading']    = 'Laden...';
+
+// Buttons
+$_['button_continue'] = 'Weitermachen';
+$_['button_back']     = 'Zurück';
+
+// Error
+$_['error_exception'] = 'Fehlercode (%s): %s in %s in Zeile %s';

a3bc4e27ae250429dfbc858fe124e2f2e660f2ab
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:18:05 +0800

    New translations gdpr.php (German)

--- de/upload/catalog/language/de-DE/information/gdpr.php
+++ de/upload/catalog/language/de-DE/information/gdpr.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'General Data Protection Regulation Request';
+$_['heading_title']     = 'Anfrage zur Datenschutz-Grundverordnung';
 
 // Text
-$_['text_account']      = 'Account';
-$_['text_gdpr']         = 'You can view %s GDPR policy on the <a href="%s" target="_blank">%s</a> page.';
-$_['text_verification'] = 'Account Verification';
-$_['text_email']        = 'Before we can carry out any GDPR requests we must validate you account. Please enter your E-Mail Address below.';
-$_['text_action']       = 'Choose an action';
-$_['text_export']       = 'Export Personal Data';
-$_['text_remove']       = 'Remove Personal Data';
-$_['text_warning']      = 'Warning: You will lose access to your account!';
-$_['text_access']       = 'You will no longer have access to your %s account.';
-$_['text_history']      = 'You will no longer have access to your order history, invoices, wishlists or downloads.';
-$_['text_limit']        = 'Account deletion requests will be processed after <strong>%s days</strong> so any fraud detection, chargebacks or refunds can be processed.';
-$_['text_success']      = 'Success: An email has been sent to your email address!';
-$_['text_cancel']		= 'Cancel';
+$_['text_account']      = 'Konto';
+$_['text_gdpr']         = 'Sie können die %s DSGVO-Richtlinie auf der Seite <a href="%s" target="_blank">%s</a> einsehen.';
+$_['text_verification'] = 'Kontoüberprüfung';
+$_['text_email']        = 'Bevor wir DSGVO-Anfragen nachkommen können, müssen wir Ihr Konto validieren. Bitte geben Sie unten Ihre E-Mail-Adresse ein.';
+$_['text_action']       = 'Wählen Sie eine Aktion';
+$_['text_export']       = 'Persönliche Daten exportieren';
+$_['text_remove']       = 'Persönliche Daten entfernen';
+$_['text_warning']      = 'Achtung: Sie verlieren den Zugriff auf Ihr Konto!';
+$_['text_access']       = 'Sie haben keinen Zugriff mehr auf Ihr %s -Konto.';
+$_['text_history']      = 'Sie haben keinen Zugriff mehr auf Ihre Bestellhistorie, Rechnungen, Wunschlisten oder Downloads.';
+$_['text_limit']        = 'Anfragen zur Kontolöschung werden nach <strong>%s Tagen</strong> bearbeitet, damit etwaige Betrugserkennungen, Rückbuchungen oder Rückerstattungen verarbeitet werden können.';
+$_['text_success']      = 'Erfolg: Eine E-Mail wurde an Ihre E-Mail-Adresse gesendet!';
+$_['text_cancel']		= 'Stornieren';
 
 // Entry
 $_['entry_email']       = 'E-Mail';
 
 // Error
-$_['error_email']       = 'E-Mail Address does not appear to be valid!';
-$_['error_action']      = 'You must select a valid GDPR action!';
+$_['error_email']       = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_action']      = 'Sie müssen eine gültige DSGVO-Aktion auswählen!';

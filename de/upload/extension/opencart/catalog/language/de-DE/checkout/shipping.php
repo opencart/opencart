6b86c47fec1a1ba32a816c50529994b732428d28
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:20:29 +0800

    New translations shipping.php (German)

--- de/upload/extension/opencart/catalog/language/de-DE/checkout/shipping.php
+++ de/upload/extension/opencart/catalog/language/de-DE/checkout/shipping.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']        = 'Estimate Shipping &amp; Taxes';
+$_['heading_title']        = 'Geschätzte Versandkosten &amp; Steuern';
 
 // Text
-$_['text_shipping_method'] = 'Shipping method options';
-$_['text_destination']     = 'Enter your destination to get a shipping estimate.';
-$_['text_estimate']        = 'Please select the preferred shipping method to use on this order.';
-$_['text_success']         = 'Success: Your shipping estimate has been applied!';
+$_['text_shipping_method'] = 'Optionen für Versandarten';
+$_['text_destination']     = 'Geben Sie Ihr Ziel ein, um eine Versandschätzung zu erhalten.';
+$_['text_estimate']        = 'Bitte wählen Sie die bevorzugte Versandmethode für diese Bestellung aus.';
+$_['text_success']         = 'Erfolg: Ihre Versandschätzung wurde angewendet!';
 
 // Entry
-$_['entry_country']        = 'Country';
-$_['entry_zone']           = 'Region / State';
-$_['entry_postcode']       = 'Post Code';
+$_['entry_country']        = 'Land';
+$_['entry_zone']           = 'Region/Staat';
+$_['entry_postcode']       = 'Postleitzahl';
 
 // Error
-$_['error_postcode']       = 'Postcode must be between 2 and 10 characters!';
-$_['error_country']        = 'Please select a country!';
-$_['error_zone']           = 'Please select a region / state!';
-$_['error_shipping']       = 'Warning: Shipping method required!';
-$_['error_no_shipping']    = 'Warning: No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
+$_['error_postcode']       = 'Die Postleitzahl muss zwischen 2 und 10 Zeichen lang sein!';
+$_['error_country']        = 'Bitte wählen Sie ein Land!';
+$_['error_zone']           = 'Bitte wählen Sie eine Region / ein Bundesland!';
+$_['error_shipping']       = 'Achtung: Versandart erforderlich!';
+$_['error_no_shipping']    = 'Achtung: Es sind keine Versandoptionen verfügbar. Bitte <a href="%s">kontaktieren Sie uns</a> für Hilfe!';

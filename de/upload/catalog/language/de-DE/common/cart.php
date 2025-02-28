e21a66e388d568342f2b5dad32385ac404eab49e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:17:42 +0800

    New translations cart.php (German)

--- de/upload/catalog/language/de-DE/common/cart.php
+++ de/upload/catalog/language/de-DE/common/cart.php
@@ -1,18 +1,18 @@
 <?php
 // Text
-$_['text_items']                 = '%s item(s) - %s';
-$_['text_points']                = 'Reward Points';
-$_['text_subscription']          = 'Subscription';
-$_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
-$_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
-$_['text_day']                   = 'day';
-$_['text_week']                  = 'week';
-$_['text_semi_month']            = 'half-month';
-$_['text_month']                 = 'month';
-$_['text_year']                  = 'year';
-$_['text_no_results']            = 'Your shopping cart is empty!';
-$_['text_cart']                  = 'View Cart';
-$_['text_checkout']              = 'Checkout';
+$_['text_items']                 = '%s Artikel - %s';
+$_['text_points']                = 'Belohnungspunkte';
+$_['text_subscription']          = 'Abonnement';
+$_['text_subscription_duration'] = '%s alle %d %s(s) für %d Zahlung(en)';
+$_['text_subscription_cancel']   = '%s alle %d %s(s) bis zum Abbruch';
+$_['text_day']                   = 'Tag';
+$_['text_week']                  = 'Woche';
+$_['text_semi_month']            = 'halber Monat';
+$_['text_month']                 = 'Monat';
+$_['text_year']                  = 'Jahr';
+$_['text_no_results']            = 'Ihr Warenkorb ist leer!';
+$_['text_cart']                  = 'Warenkorb anzeigen';
+$_['text_checkout']              = 'Kasse';
 
 // Error
-$_['error_product']              = 'Warning: Product could not be found!';
+$_['error_product']              = 'Achtung: Produkt konnte nicht gefunden werden!';

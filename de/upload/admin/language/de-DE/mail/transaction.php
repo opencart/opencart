3485a555d1ab81b82c09ff5e2f38be7af86c7c73
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 07:15:48 +0800

    New translations transaction.php (German)

--- de/upload/admin/language/de-DE/mail/transaction.php
+++ de/upload/admin/language/de-DE/mail/transaction.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_subject']  = '%s - Affiliate Credit';
-$_['text_received'] = 'You have received %s credit!';
-$_['text_total']    = 'Your total amount of credit is now %s.';
-$_['text_credit']   = 'Your account credit can deducted from your next purchase.';
+$_['text_subject']  = '%s - Affiliate-Guthaben';
+$_['text_received'] = 'Sie haben %s Guthaben erhalten!';
+$_['text_total']    = 'Ihr Gesamtguthaben beträgt jetzt %s.';
+$_['text_credit']   = 'Ihr Guthaben kann bei Ihrem nächsten Einkauf verrechnet werden.';

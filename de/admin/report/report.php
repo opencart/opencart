a661b08304316011855906e6e5b503ba9a0b0bcc
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:05:37 +0800

    New translations report.php (German)

---
+++ de/admin/report/report.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Berichte';
+
+// Text
+$_['text_success']  = 'Erfolg: Sie haben Berichte geändert!';
+$_['text_type']     = 'Wählen Sie den Berichtstyp';
+$_['text_filter']   = 'Filter';

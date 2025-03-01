c368f129e8408f111223ca72af511647c6c189d0
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:57:59 +0800

    New translations layout.php (German)

---
+++ de/upload/admin/language/de-at/design/layout.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']       = 'Layouts';
+
+// Text
+$_['text_success']        = 'Erfolg: Sie haben Layouts geändert!';
+$_['text_list']           = 'Layoutliste';
+$_['text_add']            = 'Layout hinzufügen';
+$_['text_edit']           = 'Layout bearbeiten';
+$_['text_remove']         = 'Entfernen';
+$_['text_route']          = 'Wählen Sie das Geschäft und die Routen aus, die mit diesem Layout verwendet werden sollen';
+$_['text_module']         = 'Wählen Sie die Position der Module';
+$_['text_default']        = 'Standard';
+$_['text_content_top']    = 'Inhalt Top';
+$_['text_content_bottom'] = 'Inhalt unten';
+$_['text_column_left']    = 'Spalte links';
+$_['text_column_right']   = 'Spalte rechts';
+
+// Column
+$_['column_name']         = 'Layoutname';
+$_['column_action']       = 'Aktion';
+
+// Entry
+$_['entry_name']          = 'Layoutname';
+$_['entry_store']         = 'Speichern';
+$_['entry_route']         = 'Route';
+$_['entry_module']        = 'Modul';
+
+// Error
+$_['error_permission']    = 'Achtung: Sie sind nicht berechtigt, Layouts zu ändern!';
+$_['error_name']          = 'Der Layoutname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_module']        = 'Modul erforderlich!';
+$_['error_default']       = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell als Standard-Shop-Layout zugewiesen ist!';
+$_['error_store']         = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Stores zugewiesen ist!';
+$_['error_product']       = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Produkten zugewiesen ist!';
+$_['error_category']      = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Kategorien zugeordnet ist!';
+$_['error_manufacturer']  = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Herstellern zugeordnet ist!';
+$_['error_information']   = 'Achtung: Dieses Layout kann nicht gelöscht werden, da es aktuell %s Informationsseiten zugeordnet ist!';

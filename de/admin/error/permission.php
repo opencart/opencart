7f1c0c95fd797f90c0ad4d936db3c219b1cc26ce
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:03:29 +0800

    New translations permission.php (German)

---
+++ de/admin/error/permission.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title']   = 'Zugriff verweigert!';
+
+// Text
+$_['text_permission'] = 'Sie haben keine Berechtigung, auf diese Seite zuzugreifen. Wenden Sie sich bitte an Ihren Systemadministrator.';

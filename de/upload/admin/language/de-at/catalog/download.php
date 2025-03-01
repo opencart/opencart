29f3071e77038337649a646232dd7e214b7bca82
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:57:07 +0800

    New translations download.php (German)

---
+++ de/upload/admin/language/de-at/catalog/download.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']            = 'Downloads';
+
+// Text
+$_['text_success']             = 'Erfolg: Sie haben Downloads geändert!';
+$_['text_list']                = 'Liste herunterladen';
+$_['text_add']                 = 'Download hinzufügen';
+$_['text_edit']                = 'Bearbeiten Herunterladen';
+$_['text_upload']              = 'Ihre Datei wurde erfolgreich hochgeladen!';
+$_['text_report']              = 'Berichte';
+
+// Column
+$_['column_name']              = 'Downloadname';
+$_['column_ip']                = 'IP';
+$_['column_account']           = 'Konten';
+$_['column_store']             = 'Speichern';
+$_['column_country']           = 'Land';
+$_['column_date_added']        = 'Datum hinzugefügt';
+$_['column_action']            = 'Aktion';
+
+// Entry
+$_['entry_name']               = 'Downloadname';
+$_['entry_filename']           = 'Dateiname';
+$_['entry_mask']               = 'Maske';
+
+// Help
+$_['help_filename']            = 'Sie können über die Schaltfläche „Hochladen“ hochladen oder per FTP in das Download-Verzeichnis hochladen und die Details unten eingeben.';
+$_['help_mask']                = 'Es wird empfohlen, dass Dateiname und Maske unterschiedlich sind, um zu verhindern, dass Benutzer versuchen, eine direkte Verknüpfung mit Ihren Downloads herzustellen.';
+
+// Error
+$_['error_warning']            = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']         = 'Achtung: Sie sind nicht berechtigt, Downloads zu ändern!';
+$_['error_name']               = 'Der Downloadname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_filename']           = 'Der Dateiname muss zwischen 3 und 128 Zeichen lang sein!';
+$_['error_filename_character'] = 'Der Dateiname darf nur die Zeichen aZ, 0-9, - und _ enthalten!';
+$_['error_directory']          = 'Downloads müssen sich im Speicher-/Download-Verzeichnis befinden!';
+$_['error_exists']             = 'Datei existiert nicht!';
+$_['error_mask']               = 'Die Maske muss zwischen 3 und 128 Zeichen lang sein!';
+$_['error_mask_character']     = 'Die Maske darf nur die Zeichen aZ, 0-9, - und _ enthalten!';
+$_['error_file_type']          = 'Ungültiger Dateityp!';
+$_['error_product']            = 'Achtung: Dieser Download kann nicht gelöscht werden, da er aktuell %s Produkten zugeordnet ist!';

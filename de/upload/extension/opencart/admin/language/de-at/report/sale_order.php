0c333418da29ec5057fcba71917dc796edeb3c0a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:03:44 +0800

    New translations sale_order.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/report/sale_order.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']     = 'Verkaufsbericht';
+
+// Text
+$_['text_extension']     = 'Erweiterungen';
+$_['text_edit']          = 'Verkaufsbericht bearbeiten';
+$_['text_success']       = 'Erfolg: Sie haben den Verkaufsbericht geändert!';
+$_['text_filter']        = 'Filter';
+$_['text_year']          = 'Jahre';
+$_['text_month']         = 'Monate';
+$_['text_week']          = 'Wochen';
+$_['text_day']           = 'Tage';
+$_['text_all_status']    = 'Alle Status';
+
+// Column
+$_['column_date_start']  = 'Datum Beginn';
+$_['column_date_end']    = 'Datum Ende';
+$_['column_orders']      = 'Anzahl Bestellungen';
+$_['column_products']    = 'Nr. Produkte';
+$_['column_tax']         = 'Steuer';
+$_['column_total']       = 'Gesamt';
+
+// Entry
+$_['entry_date_start']   = 'Datum Beginn';
+$_['entry_date_end']     = 'Datum Ende';
+$_['entry_group']        = 'Gruppieren nach';
+$_['entry_order_status'] = 'Bestellstatus';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, den Verkaufsbericht zu ändern!';

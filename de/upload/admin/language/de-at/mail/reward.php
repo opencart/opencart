8981561ef5a8ce75b2113bd4c16efc4acd481b8c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:59:27 +0800

    New translations reward.php (German)

---
+++ de/upload/admin/language/de-at/mail/reward.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_subject']  = '%s - Belohnungspunkte';
+$_['text_received'] = 'Sie haben %s Prämienpunkte erhalten!';
+$_['text_total']    = 'Ihre Gesamtzahl an Prämienpunkten beträgt jetzt %s.';

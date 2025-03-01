fe30fc3fa3400cd0c9ee8e33bbb4ec5da4bba588
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:02 +0800

    New translations gdpr.php (German)

---
+++ de/catalog/information/gdpr.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']     = 'Anfrage zur Datenschutz-Grundverordnung';
+
+// Text
+$_['text_account']      = 'Konto';
+$_['text_gdpr']         = 'Sie können die %s DSGVO-Richtlinie auf der Seite <a href="%s" target="_blank">%s</a> einsehen.';
+$_['text_verification'] = 'Kontoüberprüfung';
+$_['text_email']        = 'Bevor wir DSGVO-Anfragen nachkommen können, müssen wir Ihr Konto validieren. Bitte geben Sie unten Ihre E-Mail-Adresse ein.';
+$_['text_action']       = 'Wählen Sie eine Aktion';
+$_['text_export']       = 'Persönliche Daten exportieren';
+$_['text_remove']       = 'Persönliche Daten entfernen';
+$_['text_warning']      = 'Achtung: Sie verlieren den Zugriff auf Ihr Konto!';
+$_['text_access']       = 'Sie haben keinen Zugriff mehr auf Ihr %s -Konto.';
+$_['text_history']      = 'Sie haben keinen Zugriff mehr auf Ihre Bestellhistorie, Rechnungen, Wunschlisten oder Downloads.';
+$_['text_limit']        = 'Anfragen zur Kontolöschung werden nach <strong>%s Tagen</strong> bearbeitet, damit etwaige Betrugserkennungen, Rückbuchungen oder Rückerstattungen verarbeitet werden können.';
+$_['text_success']      = 'Erfolg: Eine E-Mail wurde an Ihre E-Mail-Adresse gesendet!';
+$_['text_cancel']		= 'Stornieren';
+
+// Entry
+$_['entry_email']       = 'E-Mail';
+
+// Error
+$_['error_email']       = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_action']      = 'Sie müssen eine gültige DSGVO-Aktion auswählen!';

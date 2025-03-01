83382d56d9efe555b4cb7cb231564d9d74074a54
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:15 +0800

    New translations gdpr.php (German)

---
+++ de/catalog/mail/gdpr.php
@@ -0,0 +1,14 @@
+<?php
+// Text
+$_['text_subject']  = '%s - DSGVO-Export-/Löschanfrage!';
+$_['text_export']   = 'Datenexportanforderung';
+$_['text_remove']   = 'Antrag auf Kontolöschung';
+$_['text_gdpr']     = 'Eine DSGVO-Anfrage von dieser E-Mail-Adresse. Um diese Aktion zu bestätigen, klicken Sie bitte auf den folgenden Link:';
+$_['text_ip']       = 'Die für diese Anfrage verwendete IP-Adresse war:';
+$_['text_contact']  = 'Wenn Sie diese Anfrage nicht gestellt haben, wenden Sie sich bitte hier an den Ladenbesitzer:';
+$_['text_thanks']   = 'Danke,';
+$_['text_ignore']   = 'Wenn Sie diese Anfrage nicht erstellt haben, ignorieren Sie diese E-Mail bitte.';
+
+// Buttons
+$_['button_export'] = 'Ich bestätige den Export meiner Daten';
+$_['button_remove'] = 'Ich bestätige die Löschung meines Kontos';

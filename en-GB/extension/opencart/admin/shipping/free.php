69ecda0a43dab5cc17f0833b1c9e2541ee2f736b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:43 +0800

    New translations free.php (English, United Kingdom)

---
+++ en-GB/extension/opencart/admin/shipping/free.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = 'Free Shipping';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified free shipping!';
+$_['text_edit']        = 'Edit Free Shipping';
+
+// Entry
+$_['entry_total']      = 'Sub-Total';
+$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Help
+$_['help_total']       = 'Sub-Total amount needed before the free shipping module becomes available.';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify free shipping!';

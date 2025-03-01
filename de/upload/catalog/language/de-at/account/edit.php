e95e86a28a837fb1c56c4a6fa97792fc3fec1800
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:00:32 +0800

    New translations edit.php (German)

---
+++ de/upload/catalog/language/de-at/account/edit.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']      = 'Meine Kontoinformationen';
+
+// Text
+$_['text_account']       = 'Konto';
+$_['text_edit']          = 'Informationen bearbeiten';
+$_['text_your_details']  = 'Ihre persönlichen Daten';
+$_['text_success']       = 'Erfolg: Ihr Konto wurde erfolgreich aktualisiert.';
+
+// Entry
+$_['entry_firstname']    = 'Vorname';
+$_['entry_lastname']     = 'Nachname';
+$_['entry_email']        = 'E-Mail';
+$_['entry_telephone']    = 'Telefon';
+
+// Error
+$_['error_token']        = 'Achtung: Editiertoken ungültig!';
+$_['error_exists']       = 'Achtung: E-Mail-Adresse ist bereits registriert!';
+$_['error_firstname']    = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']     = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_email']        = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_telephone']    = 'Die Telefonnummer muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_custom_field'] = '%s erforderlich!';
+$_['error_regex']        = '%s ist keine gültige Eingabe!';

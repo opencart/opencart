e6fafcbbe79f4d3b8e4eff8d94675278ff02818e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:37 +0800

    New translations compare.php (German)

---
+++ de/catalog/product/compare.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']     = 'Produktvergleich';
+
+// Text
+$_['text_product']      = 'Produktdetails';
+$_['text_name']         = 'Produkt';
+$_['text_image']        = 'Bild';
+$_['text_price']        = 'Preis';
+$_['text_model']        = 'Modell';
+$_['text_manufacturer'] = 'Marke';
+$_['text_availability'] = 'Verfügbarkeit';
+$_['text_rating']       = 'Bewertungen';
+$_['text_reviews']      = 'Basierend auf %s Bewertungen.';
+$_['text_summary']      = 'Zusammenfassung';
+$_['text_weight']       = 'Gewicht';
+$_['text_dimension']    = 'Abmessungen (L x B x H)';
+$_['text_compare']      = 'Produktvergleich (%s)';
+$_['text_success']      = 'Erfolg: Sie haben <a href="%s">%s</a> zu Ihrem <a href="%s">Produktvergleich</a>hinzugefügt!';
+$_['text_remove']       = 'Erfolg: Sie haben Ihren Produktvergleich geändert!';
+$_['text_no_results']   = 'Sie haben keine Produkte zum Vergleichen ausgewählt.';
+
+// Error
+$_['error_product']     = 'Achtung: Produkt konnte nicht gefunden werden!';

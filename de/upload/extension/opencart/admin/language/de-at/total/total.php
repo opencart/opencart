5a6922986b94b882f4773c0f53d1c6bf5672fb21
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:04:15 +0800

    New translations total.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/total/total.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'Gesamt';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben die Gesamtsummen geändert!';
+$_['text_edit']        = 'Bearbeiten Gesamtsumme Gesamtsumme';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Gesamtsummen zu ändern!';

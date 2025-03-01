8727b92925c31429e628e37d4bf7867b29bc25ff
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 18:06:04 +0800

    New translations review.php (German)

--- de/catalog/product/review.php
+++ de/catalog/product/review.php
@@ -1,28 +1,28 @@
 <?php
 // Text
-$_['text_reviews']    = '%s reviews';
-$_['text_write']      = 'Write a review';
-$_['text_login']      = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
-$_['text_no_results'] = 'Be the first to write a review for this product.';
-$_['text_note']       = '<span class="text-danger">Note:</span> HTML is not translated!';
-$_['text_success']    = 'Thank you for your review. It has been submitted to the webmaster for approval.';
+$_['text_reviews']    = '%s Bewertungen';
+$_['text_write']      = 'Eigene Rezension verfassen';
+$_['text_login']      = 'Bitte <a href="%s">anmelden</a> oder <a href="%s">registrieren</a> um eine Bewertung abzugeben';
+$_['text_no_results'] = 'Seien Sie der Erste, der eine Bewertung für dieses Produkt schreibt.';
+$_['text_note']       = '<span class="text-danger">Hinweis:</span> HTML wird nicht übersetzt!';
+$_['text_success']    = 'Vielen Dank für Ihre Bewertung. Sie wurde dem Webmaster zur Genehmigung vorgelegt.';
 
 // Entry
-$_['entry_author']     = 'Your Name';
-$_['entry_review']     = 'Your Review';
+$_['entry_author']     = 'Ihr Name';
+$_['entry_review']     = 'Meine Bewertung';
 $_['entry_rating']     = 'Bewertungen';
-$_['entry_good']       = 'Good';
-$_['entry_bad']        = 'Bad';
+$_['entry_good']       = 'Gut';
+$_['entry_bad']        = 'Schlecht';
 
 // Tabs
-$_['tab_review']       = 'Reviews (%s)';
+$_['tab_review']       = 'Bewertungen (%s)';
 
 // Error
-$_['error_token']      = 'Warning: Review token invalid!';
+$_['error_token']      = 'Achtung: Bewertungstoken ungültig!';
 $_['error_product']    = 'Achtung: Produkt konnte nicht gefunden werden!';
-$_['error_author']     = 'Review Name must be between 3 and 25 characters!';
-$_['error_text']       = 'Review Text must be between 25 and 1000 characters!';
-$_['error_rating']     = 'Please select a review rating!';
-$_['error_login']      = 'You must login to review the product!';
-$_['error_purchased']  = 'You must have purchased this product before you can write a review!';
-$_['error_status']     = 'Reviews are disabled!';
+$_['error_author']     = 'Der Bewertungsname muss zwischen 3 und 25 Zeichen lang sein!';
+$_['error_text']       = 'Der Bewertungstext muss zwischen 25 und 1000 Zeichen lang sein!';
+$_['error_rating']     = 'Bitte geben Sie eine Bewertung ab!';
+$_['error_login']      = 'Sie müssen sich anmelden um das Produkt zu bewerten!';
+$_['error_purchased']  = 'Sie müssen dieses Produkt gekauft haben, bevor Sie eine Bewertung schreiben können!';
+$_['error_status']     = 'Bewertungen sind deaktiviert!';

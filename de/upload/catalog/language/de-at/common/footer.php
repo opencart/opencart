6274ce795a2be60200e0db311e8671967cb05fd4
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:01:36 +0800

    New translations footer.php (German)

---
+++ de/upload/catalog/language/de-at/common/footer.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_information']  = 'Information';
+$_['text_blog']         = 'Der Blog';
+$_['text_service']      = 'Kundendienst';
+$_['text_extra']        = 'Extras';
+$_['text_contact']      = 'Kontaktieren Sie uns';
+$_['text_return']       = 'Rückgaben';
+$_['text_sitemap']      = 'Sitemap';
+$_['text_gdpr']         = 'DSGVO';
+$_['text_manufacturer'] = 'Marken';
+$_['text_affiliate']    = 'Partnerprogramm';
+$_['text_special']      = 'Besonderheiten';
+$_['text_account']      = 'Mein Konto';
+$_['text_order']        = 'Bestellverlauf';
+$_['text_wishlist']     = 'Wunschliste';
+$_['text_newsletter']   = 'Newsletter';
+$_['text_powered']      = 'Unterstützt von <a href="https://www.opencart.com">OpenCart</a><br/> %s &copy; %s';

da9695f1cb3bc6bbbc4753ef34420a74dc7bf1a3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:02:37 +0800

    New translations upload.php (German)

---
+++ de/upload/catalog/language/de-at/tool/upload.php
@@ -0,0 +1,9 @@
+<?php
+// Text
+$_['text_upload']     = 'Ihre Datei wurde erfolgreich hochgeladen!';
+
+// Error
+$_['error_token']     = 'Achtung: Upload-Token ungültig!';
+$_['error_filename']  = 'Der Dateiname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_file_type'] = 'Ungültiger Dateityp!';
+$_['error_upload']    = 'Upload erforderlich!';

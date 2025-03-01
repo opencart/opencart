f7f58b3c572d0c8af4e4d5535d4dd869ecd71e77
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:02:46 +0800

    New translations activity.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/dashboard/activity.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']                = 'Jüngste Aktivität';
+
+// Text
+$_['text_extension']               = 'Erweiterungen';
+$_['text_success']                 = 'Erfolg: Sie haben die Dashboard-Aktivität geändert!';
+$_['text_edit']                    = 'Dashboard „Letzte Aktivität“ bearbeiten';
+$_['text_activity_register']       = '<a href="customer_id=%d">%s</a> hat ein neues Konto registriert.';
+$_['text_activity_edit']           = '<a href="customer_id=%d">%s</a> haben ihre Kontodetails aktualisiert.';
+$_['text_activity_password']       = '<a href="customer_id=%d">%s</a> hat das Kontopasswort aktualisiert.';
+$_['text_activity_reset']          = '<a href="customer_id=%d">%s</a> setzt das Kontopasswort zurück.';
+$_['text_activity_login']          = '<a href="customer_id=%d">%s</a> angemeldet.';
+$_['text_activity_forgotten']      = '<a href="customer_id=%d">%s</a> hat eine Zurücksetzung des Passworts angefordert.';
+$_['text_activity_address_add']    = '<a href="customer_id=%d">%s</a> hat eine neue Adresse hinzugefügt.';
+$_['text_activity_address_edit']   = '<a href="customer_id=%d">%s</a> haben ihre Adresse aktualisiert.';
+$_['text_activity_address_delete'] = '<a href="customer_id=%d">%s</a> hat eine ihrer Adressen gelöscht.';
+$_['text_activity_return_account'] = '<a href="customer_id=%d">%s</a> hat ein Produkt eingereicht <a href="return_id=%d">zurückgeben</a>.';
+$_['text_activity_return_guest']   = '%s hat ein Produkt eingereicht <a href="return_id=%d">zurückgeben</a>.';
+$_['text_activity_order_account']  = '<a href="customer_id=%d">%s</a> hat eine <a href="order_id=%d">neue Bestellung</a>hinzugefügt.';
+$_['text_activity_order_guest']    = '%s hat eine <a href="order_id=%d">neue Bestellung</a>erstellt.';
+$_['text_activity_affiliate_add']  = '<a href="customer_id=%d">%s</a> für ein Partnerkonto registriert.';
+$_['text_activity_affiliate_edit'] = '<a href="customer_id=%d">%s</a> haben ihre Partnerdetails aktualisiert.';
+$_['text_activity_transaction']    = '<a href="customer_id=%d">%s</a> Provision aus einer neuen <a href="order_id=%d">Bestellung</a>erhalten.';
+
+// Entry
+$_['entry_status']                 = 'Status';
+$_['entry_sort_order']             = 'Sortierreihenfolge';
+$_['entry_width']                  = 'Breite';
+
+// Error
+$_['error_permission']             = 'Warnung: Sie sind nicht berechtigt, die Dashboard-Aktivität zu ändern!';

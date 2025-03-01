408a9a13862abe7cb7aa67ebad3205a6ce513754
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:03:27 +0800

    New translations not_found.php (German)

---
+++ de/admin/error/not_found.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title']  = 'Seite nicht gefunden!';
+
+// Text
+$_['text_not_found'] = 'Die gesuchte Seite konnte nicht gefunden werden! Wenden Sie sich bitte an Ihren Administrator, wenn das Problem weiterhin besteht.';

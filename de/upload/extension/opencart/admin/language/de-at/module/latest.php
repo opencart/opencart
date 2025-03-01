22530e5b5081074775c3a3d90c068de20beae748
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:03:15 +0800

    New translations latest.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/module/latest.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']    = 'Letzte';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das neueste Modul geändert!';
+$_['text_edit']        = 'Neuestes Modul bearbeiten';
+$_['text_horizontal']  = 'Horizontal';
+$_['text_vertical']    = 'Vertikal';
+
+// Entry
+$_['entry_name']       = 'Modulname';
+$_['entry_axis']       = 'Achse';
+$_['entry_limit']      = 'Limit';
+$_['entry_width']      = 'Bildbreite';
+$_['entry_height']     = 'Bildhöhe';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warnung: Sie sind nicht berechtigt, das neueste Modul zu ändern!';
+$_['error_name']       = 'Der Modulname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_width']      = 'Breite erforderlich!';
+$_['error_height']     = 'Körpergröße erforderlich!';

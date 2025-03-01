63c6fbd7b84d32dd2df941f6eeb928f73a8a7b4c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:04:47 +0800

    New translations cod.php (German)

---
+++ de/upload/extension/opencart/catalog/language/de-at/payment/cod.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title']        = 'Nachnahme';
+
+// Error
+$_['error_order_id']       = 'Keine Bestellnummer in der Sitzung!';
+$_['error_payment_method'] = 'Zahlungsmethode ist falsch!';

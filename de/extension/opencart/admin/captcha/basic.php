cff76da7b9aedcb3a5d961c86a15ee9b782f7e86
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:09:18 +0800

    New translations basic.php (German)

---
+++ de/extension/opencart/admin/captcha/basic.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Einfaches Captcha';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das Basic-Captcha geändert!';
+$_['text_edit']        = 'Grundlegendes Captcha bearbeiten';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, das Basic-Captcha zu ändern!';

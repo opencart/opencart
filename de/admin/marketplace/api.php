8b7d3b4e01a950c1e73132fd47b075152e7ee754
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:05:15 +0800

    New translations api.php (German)

---
+++ de/admin/marketplace/api.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'OpenCart Marketplace-API';
+
+// Text
+$_['text_success']     = 'Erfolg: Sie haben Ihre API-Informationen geändert!';
+$_['text_signup']      = 'Bitte geben Sie Ihre OpenCart API-Informationen ein, die Sie hier <a href="https://www.opencart.com/index.php?route=account/store" target="_blank" class="alert-link">erhalten können:</a>.';
+
+// Entry
+$_['entry_username']   = 'Benutzername';
+$_['entry_secret']     = 'Geheimnis';
+
+// Error
+$_['error_permission'] = 'Warnung: Sie sind nicht berechtigt, die Marktplatz-API zu ändern!';
+$_['error_username']   = 'Benutzername erforderlich!';
+$_['error_secret']     = 'Geheimhaltung erforderlich!';

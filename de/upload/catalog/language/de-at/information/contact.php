7d273671843f4b5eb78afffccaedaa0005f521ee
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:01:54 +0800

    New translations contact.php (German)

---
+++ de/upload/catalog/language/de-at/information/contact.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']  = 'Kontaktieren Sie uns';
+
+// Text
+$_['text_location']  = 'Unsere Standorte';
+$_['text_store']     = 'Unsere Filialen';
+$_['text_contact']   = 'Kontaktformular';
+$_['text_address']   = 'Adresse';
+$_['text_telephone'] = 'Telefon';
+$_['text_open']      = 'Öffnungszeiten';
+$_['text_comment']   = 'Kommentare';
+$_['text_message']   = '<p>Ihre Anfrage wurde erfolgreich an den Shop-Inhaber gesendet!</p>';
+
+// Entry
+$_['entry_name']     = 'Ihr Name';
+$_['entry_email']    = 'E-Mail-Adresse';
+$_['entry_enquiry']  = 'Anfrage';
+
+// Email
+$_['email_subject']  = 'Anfrage %s';
+
+// Errors
+$_['error_name']     = 'Der Name muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_email']    = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_enquiry']  = 'Die Anfrage muss zwischen 10 und 3000 Zeichen lang sein!';

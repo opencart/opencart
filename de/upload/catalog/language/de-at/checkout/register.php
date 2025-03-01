c6be6740ed745904e5f0a456a508a726a1253053
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:01:22 +0800

    New translations register.php (German)

---
+++ de/upload/catalog/language/de-at/checkout/register.php
@@ -0,0 +1,56 @@
+<?php
+// Heading
+$_['heading_title']            = 'Ihre persönlichen Daten';
+
+// Text
+$_['text_login']               = 'Wenn Sie bereits ein Konto bei uns haben, melden Sie sich bitte auf der Anmeldeseite <a href="%s"><strong></strong></a>an.';
+$_['text_register']            = 'Konto registrieren';
+$_['text_guest']               = 'Gast-Checkout';
+$_['text_payment_address']     = 'Zahlungsadresse';
+$_['text_shipping_address']    = 'Lieferadresse';
+$_['text_your_password']       = 'Ihr Passwort';
+$_['text_agree']               = 'Ich habe die <a href="%s" class="modal-link"><b>%s</b></a>gelesen und stimme ihnen zu';
+$_['text_success_add']         = 'Erfolg: Ihr Konto wurde erstellt!';
+$_['text_success_guest']       = 'Erfolg: Ihre Gastkontoinformationen wurden gespeichert!';
+$_['text_success_edit']        = 'Erfolg: Ihr Konto wurde erfolgreich aktualisiert.';
+
+// Entry
+$_['entry_customer_group']     = 'Kontotyp';
+$_['entry_firstname']          = 'Vorname';
+$_['entry_lastname']           = 'Nachname';
+$_['entry_email']              = 'E-Mail';
+$_['entry_telephone']          = 'Telefon';
+$_['entry_password']           = 'Passwort';
+$_['entry_company']            = 'Unternehmen';
+$_['entry_address_1']          = 'Adresse 1';
+$_['entry_address_2']          = 'Adresse 2';
+$_['entry_postcode']           = 'Postleitzahl';
+$_['entry_city']               = 'Stadt';
+$_['entry_country']            = 'Land';
+$_['entry_zone']               = 'Region/Staat';
+$_['entry_match']              = 'Meine Liefer- und Rechnungsadresse sind identisch.';
+$_['entry_newsletter']         = 'Ich möchte den %s Newsletter abonnieren.';
+
+// Error
+$_['error_guest']              = 'Achtung: Für die Artikel in Ihrem Warenkorb ist die Registrierung eines Kontos erforderlich!';
+$_['error_firstname']          = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']           = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_customer_group']     = 'Kundengruppe scheint nicht gültig zu sein!';
+$_['error_customer_approval']  = 'Achtung: Die Nutzung dieser Kundengruppe ist freigeschaltet und nicht mit einem Gastaccount möglich.';
+$_['error_email']              = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_exists']             = 'Achtung: E-Mail-Adresse ist bereits registriert!';
+$_['error_telephone']          = 'Die Telefonnummer muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_password']           = 'Das Passwort muss eine %s enthalten und zwischen %d und 40 Zeichen lang sein!';
+$_['error_password_uppercase'] = 'Großbuchstaben';
+$_['error_password_lowercase'] = 'Kleinbuchstaben';
+$_['error_password_number']    = 'Nummer';
+$_['error_password_symbol']    = 'Symbol';
+$_['error_password_length']    = 'Das Passwort muss zwischen %d und 40 Zeichen lang sein!';
+$_['error_address_1']          = 'Adresse 1 muss zwischen 3 und 128 Zeichen lang sein!';
+$_['error_city']               = 'Die Stadt muss zwischen 2 und 128 Zeichen lang sein!';
+$_['error_postcode']           = 'Die Postleitzahl muss zwischen 2 und 10 Zeichen lang sein!';
+$_['error_country']            = 'Bitte wählen Sie ein Land!';
+$_['error_zone']               = 'Bitte wählen Sie eine Region / ein Bundesland!';
+$_['error_agree']              = 'Achtung: Sie müssen den %szustimmen!';
+$_['error_custom_field']       = '%s erforderlich!';
+$_['error_regex']              = '%s ist keine gültige Eingabe!';

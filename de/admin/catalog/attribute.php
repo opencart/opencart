72b1e99eb59839baa44445a8ebd9f691e567f6e2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-02 02:30:14 +0800

    New translations attribute.php (German)

--- de/admin/catalog/attribute.php
+++ de/admin/catalog/attribute.php
@@ -3,25 +3,25 @@
 $_['heading_title']          = 'Eigenschaften';
 
 // Text
-$_['text_success']           = 'Erfolg: Sie haben Attribute geändert!';
-$_['text_list']              = 'Attributliste';
-$_['text_add']               = 'Attribut hinzufügen';
-$_['text_edit']              = 'Attribut bearbeiten';
+$_['text_success']           = 'Erfolgreich: Sie haben Eigenschaften geändert!';
+$_['text_list']              = 'Eigenschaften Liste';
+$_['text_add']               = 'Eigenschaft hinzufügen';
+$_['text_edit']              = 'Eigenschaft bearbeiten';
 
 // Column
-$_['column_name']            = 'Attributname';
-$_['column_attribute_group'] = 'Attributgruppe';
+$_['column_name']            = 'Eigenschaft Name';
+$_['column_attribute_group'] = 'Eigenschaften-Gruppe';
 $_['column_sort_order']      = 'Sortierreihenfolge';
 $_['column_action']          = 'Aktionen';
 
 // Entry
-$_['entry_name']             = 'Attributname';
-$_['entry_attribute_group']  = 'Attributgruppe';
+$_['entry_name']             = 'Eigenschaft Name';
+$_['entry_attribute_group']  = 'Eigenschaften-Gruppe';
 $_['entry_sort_order']       = 'Sortierreihenfolge';
 
 // Error
-$_['error_warning']          = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']       = 'Achtung: Sie sind nicht berechtigt, Attribute zu ändern!';
-$_['error_attribute_group']  = 'Attributgruppe erforderlich!';
-$_['error_name']             = 'Der Attributname muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_warning']          = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']       = 'Warnung: Sie sind nicht berechtigt, Eigenschaften zu ändern!';
+$_['error_attribute_group']  = 'Eigenschaften Gruppe erforderlich!';
+$_['error_name']             = 'Der Eigenschaft Name muss zwischen 1 und 64 Zeichen lang sein!';
 $_['error_product']          = 'Warnung: Diese Eigenschaft kann nicht gelöscht werden, da sie aktuell %s Produkten zugewiesen ist!';

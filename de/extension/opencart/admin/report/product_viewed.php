99bd570b9c888fb927863b715e0b0b7bffb37ae5
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:28 +0800

    New translations product_viewed.php (German)

---
+++ de/extension/opencart/admin/report/product_viewed.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']    = 'Bericht „Angezeigte Produkte“';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_edit']        = 'Bericht „Angesehene Produkte“ bearbeiten';
+$_['text_success']     = 'Erfolg: Sie haben den Bericht „Angezeigte Produkte“ zurückgesetzt!';
+$_['text_progress']    = '%s von %sfortgeschritten!';
+
+// Column
+$_['column_name']      = 'Produktname';
+$_['column_model']     = 'Modell';
+$_['column_viewed']    = 'Angesehen';
+$_['column_percent']   = 'Prozent';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Bericht „Angezeigte Produkte“ zu ändern!';

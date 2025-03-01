cd86bebb33a4efa1bf25e6c5dd0ef02e0b67bfd4
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:04:22 +0800

    New translations return_status.php (German)

---
+++ de/admin/localisation/return_status.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']    = 'Rückgabestatus';
+
+// Text
+$_['text_success']     = 'Erfolg: Sie haben den Rückgabestatus geändert!';
+$_['text_list']        = 'Liste der Rückgabestatus';
+$_['text_add']         = 'Rückgabestatus hinzufügen';
+$_['text_edit']        = 'Retourenstatus bearbeiten';
+
+// Column
+$_['column_name']      = 'Name des Rückgabestatus';
+$_['column_action']    = 'Aktion';
+
+// Entry
+$_['entry_name']       = 'Name des Rückgabestatus';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Rückgabestatus zu ändern!';
+$_['error_name']       = 'Der Name des Rücksendestatus muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_default']    = 'Achtung: Dieser Rücksendestatus kann nicht gelöscht werden, da er aktuell als Standard-Rücksendestatus zugewiesen ist!';
+$_['error_return']     = 'Achtung: Dieser Retourenstatus kann nicht gelöscht werden, da er aktuell %s Retouren zugeordnet ist!';

15a6a1d62612987c250b60c19e9ad580ca1da70d
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:59:12 +0800

    New translations authorize_reset.php (German)

---
+++ de/upload/admin/language/de-at/mail/authorize_reset.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_subject'] = 'Versuche, den Sicherheitscode zurückzusetzen';
+$_['text_reset']   = 'Jemand hat den Sicherheitscode mehr als dreimal falsch eingegeben.';
+$_['text_link']    = 'Klicken Sie auf den untenstehenden Link, um die Kontosicherheit zurückzusetzen:';
+$_['text_ip']      = 'IP-Adresse:';
+$_['text_regards'] = 'Beste grüße';

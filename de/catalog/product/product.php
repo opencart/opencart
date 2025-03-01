67b657e869675167f67687f957050c164048f177
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:41 +0800

    New translations product.php (German)

---
+++ de/catalog/product/product.php
@@ -0,0 +1,34 @@
+<?php
+// Text
+$_['text_search']                = 'Search';
+$_['text_brand']                 = 'Brand';
+$_['text_manufacturer']          = 'Brand:';
+$_['text_model']                 = 'Product Code:';
+$_['text_reward']                = 'Reward Points:';
+$_['text_points']                = 'Price in reward points:';
+$_['text_stock']                 = 'Availability:';
+$_['text_tax']                   = 'Ex Tax:';
+$_['text_discount']              = ' or more ';
+$_['text_option']                = 'Available Options';
+$_['text_minimum']               = 'This product has a minimum quantity of %s';
+$_['text_reviews']               = '%s reviews';
+$_['text_write']                 = 'Write a review';
+$_['text_login']                 = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
+$_['text_tags']                  = 'Tags:';
+$_['text_subscription']          = 'Abonnement';
+$_['text_subscription_duration'] = '%s alle %d %s(s) für %d Zahlung(en)';
+$_['text_subscription_cancel']   = '%s alle %d %s(s) bis zum Abbruch';
+$_['text_day']                   = 'Tag';
+$_['text_week']                  = 'Woche';
+$_['text_semi_month']            = 'halber Monat';
+$_['text_month']                 = 'Monat';
+$_['text_year']                  = 'Jahr';
+
+// Entry
+$_['entry_qty']                  = 'Qty';
+$_['entry_rating']               = 'Bewertungen';
+
+// Tabs
+$_['tab_description']            = 'Beschreibung';
+$_['tab_attribute']              = 'Spezifikation';
+$_['tab_review']                 = 'Reviews (%s)';

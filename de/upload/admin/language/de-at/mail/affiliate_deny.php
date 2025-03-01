dc67574e425b54d9fe8c1730567829052f4222de
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:59:08 +0800

    New translations affiliate_deny.php (German)

---
+++ de/upload/admin/language/de-at/mail/affiliate_deny.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject'] = '%s – Ihr Partnerkonto wurde abgelehnt!';
+$_['text_welcome'] = 'Willkommen und danke für Ihre Registrierung bei %s!';
+$_['text_denied']  = 'Leider wurde Ihre Anfrage abgelehnt. Für weitere Informationen können Sie den Ladenbesitzer hier kontaktieren:';
+$_['text_thanks']  = 'Danke,';

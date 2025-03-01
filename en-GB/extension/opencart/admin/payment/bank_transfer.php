d805a9be24864e1c4aecd5f6801bd1e815a1a9ad
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:07 +0800

    New translations bank_transfer.php (English, United Kingdom)

---
+++ en-GB/extension/opencart/admin/payment/bank_transfer.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']      = 'Bank Transfer';
+
+// Text
+$_['text_extension']     = 'Extensions';
+$_['text_success']       = 'Success: You have modified bank transfer details!';
+$_['text_edit']          = 'Edit Bank Transfer';
+
+// Entry
+$_['entry_bank']         = 'Bank Transfer Instructions';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment bank transfer!';
+$_['error_bank']         = 'Bank Transfer Instructions required!';

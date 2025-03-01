910fdaa7a59a0b6ab7d295a1f9228f0066fe668b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:59 +0800

    New translations shipping.php (German)

---
+++ de/extension/opencart/admin/total/shipping.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'Versand';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben die Versandkosten geändert!';
+$_['text_edit']        = 'Versandkostensumme bearbeiten';
+
+// Entry
+$_['entry_estimator']  = 'Versandschätzer';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Gesamtversandbetrag zu ändern!';

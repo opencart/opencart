55a1b57ad2b3252df62e58e419ebb803d5075ccc
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 20:54:42 +0800

    New translations fraud.php (German)

--- de/admin/extension/fraud.php
+++ de/admin/extension/fraud.php
@@ -9,7 +9,7 @@ $_['text_list']        = 'Betrugsschutzliste';
 // Column
 $_['column_name']      = 'Name zur Betrugsbekämpfung';
 $_['column_status']    = 'Status';
-$_['column_action']    = 'Aktion';
+$_['column_action']    = 'Aktionen';
 
 // Error
 $_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Betrugsschutz zu ändern!';

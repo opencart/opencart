c1ecac3ce6810107501515f417a316b23a2fe9c2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:09:49 +0800

    New translations blog.php (English, United Kingdom)

---
+++ en-GB/extension/opencart/admin/module/blog.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']    = 'Blog';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified blog module!';
+$_['text_next']        = 'Success: You have modified %s blog out of %s!';
+$_['text_edit']        = 'Edit Blog Module';
+
+// Form
+$_['text_desc']        = 'Descending Order';
+$_['text_asc']         = 'Ascending Order';
+$_['text_sort_name']   = 'Name';
+$_['text_sort_date']   = 'Date Added';
+
+// Column
+$_['column_total']     = 'Total';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_limit']      = 'Limit';
+$_['entry_width']      = 'Image Width';
+$_['entry_height']     = 'Image Height';
+$_['entry_order']      = 'Order';
+$_['entry_sort']   	   = 'Sort';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify blog module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';

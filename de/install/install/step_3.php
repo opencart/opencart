ece0c97f81bfc839e86d1af1c411487161fca9d7
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-02 02:30:17 +0800

    New translations step_3.php (German)

--- de/install/install/step_3.php
+++ de/install/install/step_3.php
@@ -3,10 +3,10 @@
 $_['heading_title']          = 'Konfiguration';
 
 // Text
-$_['text_step_3']            = 'Geben Sie Ihre Datenbank- und Verwaltungsdetails ein';
+$_['text_step_3']            = 'Geben Sie Ihre Datenbank- und Administrationsdaten ein';
 $_['text_db_connection']     = '1. Bitte geben Sie Ihre Datenbankverbindungsdaten ein.';
 $_['text_db_advanced']       = 'Erweiterte DB-Einstellungen';
-$_['text_db_administration'] = '2. Bitte geben Sie einen Benutzernamen und ein Passwort für die Administration ein.';
+$_['text_db_administration'] = '2. Bitte geben Sie einen Benutzernamen und ein Passwort für die Administration von OpenCart ein.';
 $_['text_mysqli']            = 'MySQLi';
 $_['text_pdo']               = 'mPDO';
 $_['text_help']              = 'Informationen zum Einrichten einer Datenbank auf verschiedenen Plattformen:';
@@ -17,12 +17,12 @@ $_['text_db_ssl']            = 'Zertifikatseingaben sollten als einzelne Zeile h
 // Entry
 $_['entry_db_driver']        = 'DB-Treiber';
 $_['entry_db_hostname']      = 'Hostname';
-$_['entry_db_username']      = 'Benutzername';
+$_['entry_db_username']      = 'Datenbank-Benutzername';
 $_['entry_db_password']      = 'Passwort';
 $_['entry_db_database']      = 'Datenbank';
-$_['entry_db_port']          = 'Hafen';
+$_['entry_db_port']          = 'Port';
 $_['entry_db_prefix']        = 'Präfix';
-$_['entry_db_advanced']      = 'Fortschrittlich';
+$_['entry_db_advanced']      = 'Erweitert';
 $_['entry_db_ssl_key']       = 'SSL-Schlüssel';
 $_['entry_db_ssl_cert']      = 'SSL-Zertifikat';
 $_['entry_db_ssl_ca']        = 'SSL-Zertifizierungsstelle';
@@ -31,14 +31,14 @@ $_['entry_password']         = 'Passwort';
 $_['entry_email']            = 'E-Mail';
 
 // Error
-$_['error_db_driver']        = 'Datenbanktreiber erforderlich!';
-$_['error_db_hostname']      = 'Hostname erforderlich!';
-$_['error_db_username']      = 'Benutzername erforderlich!';
-$_['error_db_database']      = 'Datenbankname erforderlich!';
-$_['error_db_port']          = 'Datenbank-Port erforderlich!';
+$_['error_db_driver']        = 'Datenbanktreiber ist erforderlich!';
+$_['error_db_hostname']      = 'Hostname ist erforderlich!';
+$_['error_db_username']      = 'Benutzername ist erforderlich!';
+$_['error_db_database']      = 'Datenbankname ist erforderlich!';
+$_['error_db_port']          = 'Datenbank-Port ist erforderlich!';
 $_['error_db_prefix']        = 'Das DB-Präfix darf nur Kleinbuchstaben im Bereich von a bis z, 0 bis 9 und Unterstriche enthalten.';
-$_['error_db_connect']       = 'Fehler: Verbindung zur Datenbank konnte nicht hergestellt werden. Bitte stellen Sie sicher, dass Datenbankserver, Benutzername und Passwort korrekt sind!';
-$_['error_username']         = 'Benutzername erforderlich!';
-$_['error_password']         = 'Passwort erforderlich!';
+$_['error_db_connect']       = 'Fehler: Verbindung zur Datenbank konnte nicht hergestellt werden. Bitte stellen Sie sicher, dass Datenbankserver, DB-Benutzername und Passwort korrekt sind!';
+$_['error_username']         = 'Datenbank-Benutzername ist erforderlich!';
+$_['error_password']         = 'Passwort ist erforderlich!';
 $_['error_email']            = 'E-Mail-Adresse scheint nicht gültig zu sein!';
 $_['error_config']           = 'Fehler: In die Datei config.php konnte nicht geschrieben werden. Bitte überprüfen Sie, ob Sie die richtigen Berechtigungen für Folgendes festgelegt haben: ';

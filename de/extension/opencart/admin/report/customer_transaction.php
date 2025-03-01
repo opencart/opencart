ae6258c671bba2b3b8d1ee674d482d3b9ab12c47
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:22 +0800

    New translations customer_transaction.php (German)

---
+++ de/extension/opencart/admin/report/customer_transaction.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']         = 'Kundentransaktionsbericht';
+
+// Text
+$_['text_extension']        = 'Erweiterungen';
+$_['text_edit']             = 'Kundentransaktionsbericht bearbeiten';
+$_['text_success']          = 'Erfolg: Sie haben den Kundentransaktionsbericht geändert!';
+$_['text_filter']           = 'Filter';
+
+// Column
+$_['column_customer']       = 'Kundenname';
+$_['column_email']          = 'E-Mail';
+$_['column_customer_group'] = 'Kundengruppe';
+$_['column_status']         = 'Status';
+$_['column_total']          = 'Gesamt';
+$_['column_action']         = 'Aktion';
+
+// Entry
+$_['entry_date_start']      = 'Datum Beginn';
+$_['entry_date_end']        = 'Datum Ende';
+$_['entry_customer']        = 'Kunde';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission']      = 'Achtung: Sie sind nicht berechtigt, den Kundentransaktionsbericht zu ändern!';

6c55510d346cf3b687d22ac798fca1dfdb4d5a55
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:58:21 +0800

    New translations language.php (German)

---
+++ de/upload/admin/language/de-at/extension/language.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'Sprachen';
+
+// Text
+$_['text_success']     = 'Erfolg: Sie haben Sprachen geändert!';
+$_['text_list']        = 'Sprachenliste';
+
+// Column
+$_['column_name']      = 'Sprachenname';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Aktion';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Sprachen zu ändern!';
+$_['error_extension']  = 'Achtung: Erweiterung existiert nicht!';
+$_['error_default']    = 'Achtung: Diese Erweiterung kann nicht deinstalliert werden, da sie aktuell als Administrationssprache zugewiesen ist!';

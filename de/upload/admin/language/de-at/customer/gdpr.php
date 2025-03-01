3e250d1e3bd00c5337e62b009ec97823f3c56417
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:57:54 +0800

    New translations gdpr.php (German)

---
+++ de/upload/admin/language/de-at/customer/gdpr.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']     = 'DSGVO-Genehmigungen';
+
+// Text
+$_['text_success']      = 'Erfolg: Sie haben DSGVO-Genehmigungen geändert!';
+$_['text_list']         = 'DSGVO-Freigabeliste';
+$_['text_info']         = '<strong>DSGVO</strong> Anträge auf Kontolöschung werden nach <strong>%s Tagen</strong> bearbeitet, damit etwaige Betrugserkennungen, Rückbuchungen oder Rückerstattungen verarbeitet werden können.';
+$_['text_approve']      = 'Genehmigen';
+$_['text_deny']         = 'Leugnen';
+$_['text_delete']       = 'Löschen';
+$_['text_unverified']   = 'Nicht verifiziert';
+$_['text_pending']      = 'Ausstehend';
+$_['text_processing']   = 'Verarbeitung';
+$_['text_complete']     = 'Vollständig';
+$_['text_denied']       = 'Bestritten';
+$_['text_export']       = 'Export';
+$_['text_remove']       = 'Entfernen';
+$_['text_filter']       = 'Filter';
+
+// Column
+$_['column_email']      = 'E-Mail';
+$_['column_request']    = 'Anfrage';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Datum hinzugefügt';
+$_['column_action']     = 'Aktion';
+
+// Entry
+$_['entry_email']       = 'E-Mail';
+$_['entry_action']      = 'Aktion';
+$_['entry_status']      = 'Status';
+$_['entry_date_from']   = 'Stammen aus';
+$_['entry_date_to']     = 'Datum bis';
+
+// Error
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, DSGVO-Genehmigungen zu ändern!';

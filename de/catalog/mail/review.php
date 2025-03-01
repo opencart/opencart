25b78620b3e0b777301e0d08e1796a93a837f289
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:27 +0800

    New translations review.php (German)

---
+++ de/catalog/mail/review.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_subject']  = '%s - Produktbewertung';
+$_['text_waiting']  = 'Eine neue Produktbewertung wartet auf Sie.';
+$_['text_product']  = 'Produkt:';
+$_['text_reviewer'] = 'Rezensent:';
+$_['text_rating']   = 'Bewertung:';
+$_['text_review']   = 'Bewertungstext:';

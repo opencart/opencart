72c3fa281618e538fdb5e3803c10f860c504fee2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:57:22 +0800

    New translations antispam.php (German)

---
+++ de/upload/admin/language/de-at/cms/antispam.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']        = 'Anti-Spam';
+
+// Text
+$_['text_success']         = 'Erfolg: Sie haben den Anti-Spam geändert!';
+$_['text_list']            = 'Anti-Spam-Liste';
+$_['text_add']             = 'Anti-Spam hinzufügen';
+$_['text_edit']            = 'Anti-Spam bearbeiten';
+
+// Column
+$_['column_keyword']       = 'Stichwort';
+$_['column_action']        = 'Aktion';
+
+// Entry
+$_['entry_keyword']        = 'Stichwort';
+
+// Error
+$_['error_warning']        = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']     = 'Achtung: Sie sind nicht berechtigt, den Spamschutz zu ändern!';
+$_['error_keyword']        = 'Das Schlüsselwort muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_keyword_exists'] = 'Das Schlüsselwort muss eindeutig sein!';

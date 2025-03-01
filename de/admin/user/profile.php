49e96e0c15531f021b0b90e17c7192fa8592ee51
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:06:03 +0800

    New translations profile.php (German)

---
+++ de/admin/user/profile.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']            = 'Profil';
+
+// Text
+$_['text_success']             = 'Erfolg: Sie haben Ihr Profil geändert!';
+$_['text_edit']                = 'Bearbeiten Sie Ihr Profil';
+$_['text_user']                = 'Benutzerdetails';
+$_['text_password']            = 'Passwort';
+
+// Entry
+$_['entry_username']           = 'Benutzername';
+$_['entry_password']           = 'Passwort';
+$_['entry_confirm']            = 'Bestätigen';
+$_['entry_firstname']          = 'Vorname';
+$_['entry_lastname']           = 'Nachname';
+$_['entry_email']              = 'E-Mail';
+$_['entry_image']              = 'Bild';
+
+// Error
+$_['error_permission']         = 'Achtung: Sie haben keine Berechtigung, Ihr Profil zu ändern!';
+$_['error_username_exists']    = 'Achtung: Der Benutzername wird bereits verwendet!';
+$_['error_username']           = 'Der Benutzername muss zwischen 3 und 20 Zeichen lang sein!';
+$_['error_firstname']          = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']           = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_email']              = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_email_exists']       = 'Achtung: E-Mail-Adresse ist bereits registriert!';
+$_['error_password']           = 'Das Passwort muss eine %s enthalten und zwischen %d und 20 Zeichen lang sein!';
+$_['error_password_uppercase'] = 'Großbuchstaben';
+$_['error_password_lowercase'] = 'Kleinbuchstaben';
+$_['error_password_number']    = 'Nummer';
+$_['error_password_symbol']    = 'Symbol';
+$_['error_password_length']    = 'Das Passwort muss zwischen %d und 20 Zeichen lang sein!';
+$_['error_confirm']            = 'Passwort und Passwortbestätigung stimmen nicht überein!';

e77d42678d0e0f6c552d63bad17845632b83cfed
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:57:39 +0800

    New translations forgotten.php (German)

---
+++ de/upload/admin/language/de-at/common/forgotten.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']            = 'Passwort vergessen?';
+$_['heading_reset']            = 'Setzen Sie Ihr Passwort zurück';
+
+// Text
+$_['text_email']               = 'Geben Sie die mit Ihrem Konto verknüpfte E-Mail-Adresse ein. Klicken Sie auf „Senden“, um einen Link zum Zurücksetzen des Passworts per E-Mail zu erhalten.';
+$_['text_success']             = 'Eine E-Mail mit einem Bestätigungslink wurde an Ihre Administrator-E-Mail-Adresse gesendet.';
+$_['text_password']            = 'Geben Sie das neue Passwort ein, das Sie verwenden möchten.';
+$_['text_reset']               = 'Erfolg: Ihr Passwort wurde erfolgreich aktualisiert.';
+
+// Entry
+$_['entry_password']           = 'Passwort';
+$_['entry_confirm']            = 'Bestätigen';
+$_['entry_email']              = 'E-Mail-Adresse';
+
+// Error
+$_['error_email']              = 'Achtung: Die E-Mail-Adresse wurde in unseren Aufzeichnungen nicht gefunden!';
+$_['error_code']               = 'Resetcode stimmt nicht überein!';
+$_['error_disabled']           = 'Das Zurücksetzen des Passworts wurde deaktiviert!';
+$_['error_password']           = 'Das Passwort muss %s enthalten und zwischen %d und 20 Zeichen lang sein!';
+$_['error_password_uppercase'] = 'Großbuchstaben';
+$_['error_password_lowercase'] = 'Kleinbuchstaben';
+$_['error_password_number']    = 'Nummer';
+$_['error_password_symbol']    = 'Symbol';
+$_['error_password_length']    = 'Das Passwort muss zwischen %d und 40 Zeichen lang sein!';
+$_['error_confirm']            = 'Passwort und Passwortbestätigung stimmen nicht überein!';
+

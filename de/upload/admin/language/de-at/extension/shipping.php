1ef9ff8d784ffaaca1516201cfc950ef2ec8d8be
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:58:32 +0800

    New translations shipping.php (German)

---
+++ de/upload/admin/language/de-at/extension/shipping.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']     = 'Versand';
+
+// Text
+$_['text_success']      = 'Erfolg: Sie haben den Versand geändert!';
+$_['text_list']         = 'Versandliste';
+
+// Column
+$_['column_name']       = 'Versandart';
+$_['column_status']     = 'Status';
+$_['column_sort_order'] = 'Sortierreihenfolge';
+$_['column_action']     = 'Aktion';
+
+// Error
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, den Versand zu ändern!';
+$_['error_extension']   = 'Achtung: Erweiterung existiert nicht!';

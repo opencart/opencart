1a07601ebce3c8baccaa0c1e44131329568c3f3b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:02:08 +0800

    New translations gdpr_delete.php (German)

---
+++ de/upload/catalog/language/de-at/mail/gdpr_delete.php
@@ -0,0 +1,12 @@
+<?php
+// Text
+$_['text_subject']   = '%s – DSGVO-Anfrage wurde bearbeitet!';
+$_['text_request']   = 'Antrag auf Kontolöschung';
+$_['text_hello']     = 'Hallo <strong>%s</strong>,';
+$_['text_user']      = 'Benutzer';
+$_['text_delete']    = 'Ihr Antrag auf Löschung von DSGVO-Daten ist nun abgeschlossen.';
+$_['text_contact']   = 'Für weitere Informationen können Sie den Ladenbesitzer hier kontaktieren:';
+$_['text_thanks']    = 'Danke,';
+
+// Button
+$_['button_contact'] = 'Kontaktieren Sie uns';

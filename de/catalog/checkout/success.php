259d9c3b60139277d5832001d9ab40345d369a8b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:07:27 +0800

    New translations success.php (German)

---
+++ de/catalog/checkout/success.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title'] = 'Ihre Bestellung wurde aufgegeben!';
+
+// Text
+$_['text_basket']   = 'Warenkorb';
+$_['text_checkout'] = 'Kasse';
+$_['text_success']  = 'Erfolg';
+$_['text_customer'] = '<p>Ihre Bestellung wurde erfolgreich bearbeitet!</p><p>Sie können Ihren Bestellverlauf anzeigen, indem Sie auf die Seite <a href="%s">Mein Konto</a> gehen und auf <a href="%s">Verlauf</a>klicken.</p><p>Wenn mit Ihrem Kauf ein Download verbunden ist, können Sie auf der Kontoseite <a href="%s">Downloads</a> diese anzeigen.</p><p>Fragen richten Sie bitte direkt an den <a href="%s">Shopinhaber</a>.</p><p>Danke, dass Sie bei uns online eingekauft haben!</p>';
+$_['text_guest']    = '<p>Ihre Bestellung wurde erfolgreich bearbeitet!</p><p>Bei Fragen wenden Sie sich bitte an den <a href="%s">Ladenbesitzer</a>.</p><p>Danke, dass Sie bei uns online eingekauft haben!</p>';

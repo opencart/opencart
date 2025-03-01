e4d56290accff410dcc5edd00757b03c9e861219
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:58:34 +0800

    New translations theme.php (German)

---
+++ de/upload/admin/language/de-at/extension/theme.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'Themen';
+
+// Text
+$_['text_success']     = 'Erfolg: Sie haben Themen geändert!';
+
+// Column
+$_['column_name']      = 'Themenname';
+$_['column_status']    = 'Status';
+$_['column_action']    = 'Aktion';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Designs zu ändern!';
+$_['error_extension']  = 'Achtung: Erweiterung existiert nicht!';

7cd19921b83e8f0a09425b894a2345b693ddf407
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:58:42 +0800

    New translations geo_zone.php (German)

---
+++ de/upload/admin/language/de-at/localisation/geo_zone.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']      = 'Geozonen';
+
+// Text
+$_['text_success']       = 'Erfolg: Sie haben Geozonen geändert!';
+$_['text_list']          = 'Liste der Geozonen';
+$_['text_add']           = 'Geozone hinzufügen';
+$_['text_edit']          = 'Geozone bearbeiten';
+$_['text_geo_zone']      = 'Geozonen';
+
+// Column
+$_['column_name']        = 'Name der Geozone';
+$_['column_description'] = 'Beschreibung';
+$_['column_action']      = 'Aktion';
+
+// Entry
+$_['entry_name']         = 'Name der Geozone';
+$_['entry_description']  = 'Beschreibung';
+$_['entry_country']      = 'Land';
+$_['entry_zone']         = 'Zone';
+
+// Error
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, Geozonen zu ändern!';
+$_['error_name']         = 'Der Geozonenname muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_description']  = 'Beschreibung Der Name muss zwischen 3 und 255 Zeichen lang sein!';
+$_['error_tax_rate']     = 'Achtung: Diese Geozone kann nicht gelöscht werden, da sie aktuell einem oder mehreren Steuersätzen zugeordnet ist!';

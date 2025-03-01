041d66b8a9d1f32abd53a2e558896137a9630cbd
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:06 +0800

    New translations bank_transfer.php (German)

---
+++ de/extension/opencart/admin/payment/bank_transfer.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']      = 'Banküberweisung';
+
+// Text
+$_['text_extension']     = 'Erweiterungen';
+$_['text_success']       = 'Erfolg: Sie haben die Banküberweisungsdaten geändert!';
+$_['text_edit']          = 'Banküberweisung bearbeiten';
+
+// Entry
+$_['entry_bank']         = 'Anweisungen zur Banküberweisung';
+$_['entry_order_status'] = 'Bestellstatus';
+$_['entry_geo_zone']     = 'Geozone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, die Zahlung per Banküberweisung zu ändern!';
+$_['error_bank']         = 'Banküberweisungsanweisungen erforderlich!';

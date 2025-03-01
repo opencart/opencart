027b3d14f79f00e76b106330c5680e18e6fe9e4a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:00:59 +0800

    New translations cart.php (German)

---
+++ de/upload/catalog/language/de-at/api/cart.php
@@ -0,0 +1,15 @@
+<?php
+// Text
+$_['text_success']       = 'Erfolg: Sie haben Ihren Einkaufswagen geändert!';
+$_['text_subscription']  = 'Abonnement';
+
+// Error
+$_['error_warning']      = 'Achtung: Bitte prüfen Sie den Warenkorb sorgfältig auf Fehler!';
+$_['error_product']      = 'Achtung: Produkt konnte nicht gefunden werden!';
+$_['error_stock']        = 'Artikel nicht in der gewünschten Menge verfügbar oder nicht auf Lager!';
+$_['error_minimum']      = 'Der Mindestbestellwert für %s beträgt %s!';
+$_['error_option']       = 'Option konnte nicht gefunden werden!';
+$_['error_option_stock'] = 'Option nicht in der gewünschten Menge verfügbar oder nicht auf Lager!';
+$_['error_required']     = '%s erforderlich!';
+$_['error_regex']        = '%s ist keine gültige Eingabe!';
+$_['error_subscription'] = 'Der Abonnementplan ist ungültig!';

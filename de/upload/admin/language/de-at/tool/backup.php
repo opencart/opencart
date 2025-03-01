5404a179cb937dad45f44d7ad3628a37d84b5610
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:00:09 +0800

    New translations backup.php (German)

---
+++ de/upload/admin/language/de-at/tool/backup.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']      = 'Backup &amp; Wiederherstellen';
+
+// Text
+$_['text_success']       = 'Erfolg: Sie haben Ihre Datenbank erfolgreich geändert!';
+$_['text_progress']      = 'Sicherungsfortschritt';
+$_['text_backup']        = 'Sichern der Tabellen %s Datensätze %s bis %s Datensätze';
+$_['text_restore']       = 'Wiederherstellen von %s von %s';
+$_['text_option']        = 'Backup-Optionen';
+$_['text_history']       = 'Sicherungsverlauf';
+$_['text_import']        = 'Bei großen Sicherungsdateien ist es besser, die SQL-Datei per FTP in das Verzeichnis <strong>~/storage/backup/</strong> hochzuladen.';
+
+// Column
+$_['column_filename']    = 'Dateiname';
+$_['column_size']        = 'Größe';
+$_['column_date_added']  = 'Datum hinzugefügt';
+$_['column_action']      = 'Aktion';
+
+// Entry
+$_['entry_progress']     = 'Fortschritt';
+$_['entry_export']       = 'Export';
+
+// Error
+$_['error_permission']   = 'Warnung: Sie sind nicht berechtigt, Backup &amp; Restore zu ändern!';
+$_['error_export']       = 'Achtung: Sie müssen mindestens eine Tabelle zum Exportieren auswählen!';
+$_['error_table']        = 'Tabelle %s ist nicht in der Erlaubtliste!';
+$_['error_file']         = 'Datei konnte nicht gefunden werden!';
+$_['error_directory']    = 'Verzeichnis konnte nicht gefunden werden!';
+$_['error_not_found']    = 'Fehler: Datei %s konnte nicht gefunden werden!';
+$_['error_headers_sent'] = 'Fehler: Header bereits versendet!';
+$_['error_upload_size']  = 'Die hochgeladene Datei darf nicht größer als %ssein!';

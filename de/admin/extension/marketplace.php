c1c50c7a6ca23ac4286e59284d2b2dfcbb0a1043
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 20:54:44 +0800

    New translations marketplace.php (German)

--- de/admin/extension/marketplace.php
+++ de/admin/extension/marketplace.php
@@ -9,7 +9,7 @@ $_['text_list']        = 'Marktplatzliste';
 // Column
 $_['column_name']      = 'Name des Marktplatzes';
 $_['column_status']    = 'Status';
-$_['column_action']    = 'Aktion';
+$_['column_action']    = 'Aktionen';
 
 // Error
 $_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Marktplätze zu ändern!';

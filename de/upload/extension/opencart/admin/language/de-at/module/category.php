b8ce93c7440f9225ec813e5e88cdd938cc024e48
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:03:07 +0800

    New translations category.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/module/category.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']    = 'Kategorie';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben das Kategorienmodul geändert!';
+$_['text_edit']        = 'Modul „Kategorie bearbeiten“';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie haben keine Berechtigung, das Kategoriemodul zu ändern!';

fd9984a15c00eb09094e4eba7e603fa778f3e880
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:25 +0800

    New translations register.php (German)

---
+++ de/catalog/mail/register.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_subject']        = '%s - Vielen Dank für die Registrierung';
+$_['text_welcome']        = 'Willkommen und danke für Ihre Registrierung bei %s!';
+$_['text_login']          = 'Ihr Konto wurde nun erstellt und Sie können sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website besuchen oder die folgende URL aufrufen:';
+$_['text_approval']       = 'Ihr Konto muss genehmigt werden, bevor Sie sich anmelden können. Nach der Genehmigung können Sie sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website oder die folgende URL besuchen:';
+$_['text_service']        = 'Nach der Anmeldung haben Sie Zugriff auf weitere Dienste. Sie können beispielsweise frühere Bestellungen prüfen, Rechnungen ausdrucken und Ihre Kontoinformationen bearbeiten.';
+$_['text_thanks']         = 'Danke,';
+$_['text_new_customer']   = 'Neuer Kunde';
+$_['text_signup']         = 'Ein neuer Kunde hat sich angemeldet:';
+$_['text_customer_group'] = 'Kundengruppe:';
+$_['text_firstname']      = 'Vorname:';
+$_['text_lastname']       = 'Nachname:';
+$_['text_email']          = 'E-Mail:';
+$_['text_telephone']      = 'Telefon:';
+
+// Button
+$_['button_login']        = 'Login';

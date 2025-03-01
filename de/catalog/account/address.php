515d37d9d3a7d91fc31826a365a886a56c2830f9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:06:11 +0800

    New translations address.php (German)

---
+++ de/catalog/account/address.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['heading_title']      = 'Adressbuch';
+
+// Text
+$_['text_account']       = 'Konto';
+$_['text_address_book']  = 'Adressbucheinträge';
+$_['text_address_add']   = 'Adresse hinzufügen';
+$_['text_address_edit']  = 'Adresse bearbeiten';
+$_['text_add']           = 'Ihre Adresse wurde erfolgreich hinzugefügt';
+$_['text_edit']          = 'Ihre Adresse wurde erfolgreich aktualisiert';
+$_['text_delete']        = 'Ihre Adresse wurde erfolgreich gelöscht';
+$_['text_no_results']    = 'Sie haben keine Adressen in Ihrem Konto.';
+
+// Entry
+$_['entry_firstname']    = 'Vorname';
+$_['entry_lastname']     = 'Nachname';
+$_['entry_company']      = 'Unternehmen';
+$_['entry_address_1']    = 'Adresse 1';
+$_['entry_address_2']    = 'Adresse 2';
+$_['entry_postcode']     = 'Postleitzahl';
+$_['entry_city']         = 'Stadt';
+$_['entry_country']      = 'Land';
+$_['entry_zone']         = 'Region/Staat';
+$_['entry_default']      = 'Standardadresse';
+
+// Error
+$_['error_token']        = 'Achtung: Adress-Token ungültig!';
+$_['error_subscription'] = 'Warnung: Die Adresse wird immer noch von %s aktiven Abonnements verwendet!';
+$_['error_default']      = 'Achtung: Standardadresse erforderlich!';
+$_['error_delete']       = 'Achtung: Sie müssen mindestens eine Adresse haben!';
+$_['error_firstname']    = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']     = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_address_1']    = 'Die Adresse muss zwischen 3 und 128 Zeichen lang sein!';
+$_['error_postcode']     = 'Die Postleitzahl muss zwischen 2 und 10 Zeichen lang sein!';
+$_['error_city']         = 'Die Stadt muss zwischen 2 und 128 Zeichen lang sein!';
+$_['error_country']      = 'Bitte wählen Sie ein Land!';
+$_['error_zone']         = 'Bitte wählen Sie eine Region / ein Bundesland!';
+$_['error_custom_field'] = '%s erforderlich!';
+$_['error_regex']        = '%s ist keine gültige Eingabe!';

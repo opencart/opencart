c3bc50f5f0da1f68b549699adc4616d799a1586c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:57:34 +0800

    New translations developer.php (German)

---
+++ de/upload/admin/language/de-at/common/developer.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']          = 'Entwickleroptionen';
+
+// Text
+$_['text_developer_success'] = 'Erfolg: Sie haben die Entwicklereinstellungen geändert!';
+$_['text_cache_success']     = 'Erfolg: Sie haben den Cache geleert!';
+$_['text_theme_success']     = 'Erfolg: Sie haben den Theme-Cache geleert!';
+$_['text_sass_success']      = 'Erfolg: Sie haben den SASS-Cache geleert!';
+$_['text_vendor_success']    = 'Erfolg: Sie haben den Anbieter-Cache geleert!';
+$_['text_theme']             = 'Thema';
+$_['text_sass']              = 'SASS';
+$_['text_cache']             = 'Zwischenspeicher';
+$_['text_vendor']            = 'Verkäufer';
+
+// Column
+$_['column_component']       = 'Komponente';
+$_['column_action']          = 'Aktion';
+
+// Entry
+$_['entry_cache']            = 'Zwischenspeicher';
+
+// Buttons
+$_['button_on']              = 'An';
+$_['button_off']             = 'Aus';
+
+// Error
+$_['error_permission']       = 'Achtung: Sie sind nicht berechtigt, Entwicklereinstellungen zu ändern!';

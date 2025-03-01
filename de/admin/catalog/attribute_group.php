be1e59cf055d1cfa1ba0ae7faf877c2ff3ad26a4
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 20:54:02 +0800

    New translations attribute_group.php (German)

--- de/admin/catalog/attribute_group.php
+++ de/admin/catalog/attribute_group.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Attributgruppen';
+$_['heading_title']     = 'Eigenschaften-Gruppen';
 
 // Text
-$_['text_success']      = 'Erfolg: Sie haben Attributgruppen geändert!';
-$_['text_list']         = 'Attributgruppenliste';
-$_['text_add']          = 'Attributgruppe hinzufügen';
-$_['text_edit']         = 'Attributgruppe bearbeiten';
+$_['text_success']      = 'Erfolgreich: Sie haben Eigenschaften-Gruppen geändert!';
+$_['text_list']         = 'Eigenschaften-Gruppen Liste';
+$_['text_add']          = 'Eigenschaften-Gruppe hinzufügen';
+$_['text_edit']         = 'Eigenschaften-Gruppe bearbeiten';
 
 // Column
-$_['column_name']       = 'Attributgruppenname';
+$_['column_name']       = 'Eigenschaften-Gruppe';
 $_['column_sort_order'] = 'Sortierreihenfolge';
 $_['column_action']     = 'Aktion';
 
 // Entry
-$_['entry_name']        = 'Attributgruppenname';
+$_['entry_name']        = 'Eigenschaften-Gruppe Titel';
 $_['entry_sort_order']  = 'Sortierreihenfolge';
 
 // Error
-$_['error_warning']     = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']  = 'Achtung: Sie haben keine Berechtigung, Attributgruppen zu ändern!';
-$_['error_name']        = 'Der Attributgruppenname muss zwischen 1 und 64 Zeichen lang sein!';
-$_['error_attribute']   = 'Achtung: Diese Attributgruppe kann nicht gelöscht werden, da ihr aktuell %s Attribute zugewiesen sind!';
-$_['error_product']     = 'Achtung: Diese Attributgruppe kann nicht gelöscht werden, da sie aktuell %s Produkten zugeordnet ist!';
+$_['error_warning']     = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']  = 'Warnung: Sie haben keine Berechtigung Eigenschafts-Gruppen zu modifizieren!';
+$_['error_name']        = 'Der Eigenschaften-Gruppe Titel muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_attribute']   = 'Warnung: Diese Eigenschafts-Gruppe kann nicht gelöscht werden, da sie derzeit %s-Eigenschaften zugewiesen ist!';
+$_['error_product']     = 'Warnung: Diese Eigenschafts-Gruppe kann nicht gelöscht werden, da sie derzeit %s Produkten zugewiesen ist!';

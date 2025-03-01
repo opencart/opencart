8125f13054a026c2d043d12b578911f5dda02c08
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:19 +0800

    New translations customer_reward.php (German)

---
+++ de/extension/opencart/admin/report/customer_reward.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']         = 'Bericht zu Kundenprämienpunkten';
+
+// Text
+$_['text_extension']        = 'Erweiterungen';
+$_['text_edit']             = 'Bericht zu Kundenprämienpunkten bearbeiten';
+$_['text_success']          = 'Erfolg: Sie haben den Bericht zu Kundenprämienpunkten geändert!';
+$_['text_filter']           = 'Filter';
+
+// Column
+$_['column_customer']       = 'Kundenname';
+$_['column_email']          = 'E-Mail';
+$_['column_customer_group'] = 'Kundengruppe';
+$_['column_status']         = 'Status';
+$_['column_points']         = 'Belohnungspunkte';
+$_['column_orders']         = 'Anzahl Bestellungen';
+$_['column_total']          = 'Gesamt';
+$_['column_action']         = 'Aktion';
+
+// Entry
+$_['entry_date_start']      = 'Datum Beginn';
+$_['entry_date_end']        = 'Datum Ende';
+$_['entry_customer']        = 'Kunde';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission']      = 'Achtung: Sie sind nicht berechtigt, den Bericht zu Kundenprämienpunkten zu ändern!';

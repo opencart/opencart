e66efdf29f1d6bb511ef0c2f5669da9b51d427cf
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:04:32 +0800

    New translations weight_class.php (German)

---
+++ de/admin/localisation/weight_class.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']    = 'Gewichtsklassen';
+
+// Text
+$_['text_success']     = 'Erfolg: Du hast die Gewichtsklassen verändert!';
+$_['text_list']        = 'Gewichtsklassenliste';
+$_['text_add']         = 'Gewichtsklasse hinzufügen';
+$_['text_edit']        = 'Gewichtsklasse bearbeiten';
+
+// Column
+$_['column_title']     = 'Gewicht Titel';
+$_['column_unit']      = 'Gewichtseinheit';
+$_['column_value']     = 'Wert';
+$_['column_action']    = 'Aktion';
+
+// Entry
+$_['entry_title']      = 'Gewicht Titel';
+$_['entry_unit']       = 'Gewichtseinheit';
+$_['entry_value']      = 'Wert';
+
+// Help
+$_['help_value']       = 'Der Wert Ihres Standardgewichts in der aktuellen Gewichtseinheit. Setzen Sie den Wert für Ihr Standardgewicht auf 1.';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt Gewichtsklassen zu ändern!';
+$_['error_title']      = 'Der Gewichtstitel muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_unit']       = 'Die Gewichtseinheit muss zwischen 1 und 4 Zeichen lang sein!';
+$_['error_default']    = 'Achtung: Diese Gewichtsklasse kann nicht gelöscht werden, da sie aktuell als Standardgewichtsklasse des Geschäfts zugewiesen ist!';
+$_['error_product']    = 'Achtung: Diese Gewichtsklasse kann nicht gelöscht werden, da sie aktuell %s Produkten zugeordnet ist!';

e3418ec0a3ff5dae60ab21ac25f9cddc441c4a1e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:06:55 +0800

    New translations customer.php (German)

---
+++ de/catalog/api/customer.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['text_success']         = 'Sie haben Kunden erfolgreich geändert';
+
+// Error
+$_['error_customer']       = 'Achtung: Kunde konnte nicht gefunden werden!';
+$_['error_customer_group'] = 'Kundengruppe scheint nicht gültig zu sein!';
+$_['error_firstname']      = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']       = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_email']          = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_telephone']      = 'Die Telefonnummer muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_custom_field']   = '%s erforderlich!';
+$_['error_regex']          = '%s ist keine gültige Eingabe!';

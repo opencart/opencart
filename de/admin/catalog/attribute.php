bf1aaa242cd196fc312687e070bcaf412d44676e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 20:54:00 +0800

    New translations attribute.php (German)

--- de/admin/catalog/attribute.php
+++ de/admin/catalog/attribute.php
@@ -12,7 +12,7 @@ $_['text_edit']              = 'Attribut bearbeiten';
 $_['column_name']            = 'Attributname';
 $_['column_attribute_group'] = 'Attributgruppe';
 $_['column_sort_order']      = 'Sortierreihenfolge';
-$_['column_action']          = 'Aktion';
+$_['column_action']          = 'Aktionen';
 
 // Entry
 $_['entry_name']             = 'Attributname';
@@ -24,4 +24,4 @@ $_['error_warning']          = 'Achtung: Bitte prüfen Sie das Formular sorgfäl
 $_['error_permission']       = 'Achtung: Sie sind nicht berechtigt, Attribute zu ändern!';
 $_['error_attribute_group']  = 'Attributgruppe erforderlich!';
 $_['error_name']             = 'Der Attributname muss zwischen 1 und 64 Zeichen lang sein!';
-$_['error_product']          = 'Achtung: Dieses Attribut kann nicht gelöscht werden, da es aktuell %s Produkten zugewiesen ist!';
+$_['error_product']          = 'Warnung: Diese Eigenschaft kann nicht gelöscht werden, da sie aktuell %s Produkten zugewiesen ist!';

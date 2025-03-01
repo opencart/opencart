c9a7a1a2866e1b22b3eb1baf8a0f01add2727e25
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:00:17 +0800

    New translations api.php (German)

---
+++ de/upload/admin/language/de-at/user/api.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']        = 'APIs';
+
+// Text
+$_['text_success']         = 'Erfolg: Sie haben APIs geändert!';
+$_['text_list']            = 'API-Liste';
+$_['text_add']             = 'API hinzufügen';
+$_['text_edit']            = 'API bearbeiten';
+$_['text_ip']              = 'Unten können Sie eine Liste der IPs erstellen, die auf die API zugreifen dürfen. Ihre aktuelle IP ist %s';
+$_['text_history']         = 'Geschichte';
+
+// Column
+$_['column_username']      = 'API-Benutzername';
+$_['column_status']        = 'Status';
+$_['column_ip']            = 'IP';
+$_['column_call']          = 'Anruf';
+$_['column_date_added']    = 'Datum hinzugefügt';
+$_['column_date_modified'] = 'Änderungsdatum';
+$_['column_action']        = 'Aktion';
+
+// Entry
+$_['entry_username']       = 'API-Benutzername';
+$_['entry_key']            = 'API-Schlüssel';
+$_['entry_status']         = 'Status';
+$_['entry_ip']             = 'IP';
+
+// Error
+$_['error_permission']     = 'Achtung: Sie haben keine Berechtigung, APIs zu ändern!';
+$_['error_username']       = 'Der API-Benutzername muss zwischen 3 und 20 Zeichen lang sein!';
+$_['error_key']            = 'Der API-Schlüssel muss zwischen 64 und 256 Zeichen lang sein!';
+$_['error_ip']             = 'Sie müssen mindestens eine IP zur Zulassungsliste hinzugefügt haben!';

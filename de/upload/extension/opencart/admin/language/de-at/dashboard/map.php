a3588018e7e80f34aca876853e98a9960ad3aa05
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:02:51 +0800

    New translations map.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/dashboard/map.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'Weltkarte';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben die Dashboard-Karte geändert!';
+$_['text_edit']        = 'Dashboard-Karte bearbeiten';
+$_['text_order']       = 'Bestellungen';
+$_['text_sale']        = 'Verkäufe';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+$_['entry_width']      = 'Breite';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, die Dashboard-Karte zu ändern!';

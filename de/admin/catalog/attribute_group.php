2d42e24eec5f98a6e6e2a8897a824dc3f6c7b1d5
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-02 02:30:15 +0800

    New translations attribute_group.php (German)

--- de/admin/catalog/attribute_group.php
+++ de/admin/catalog/attribute_group.php
@@ -11,15 +11,15 @@ $_['text_edit']         = 'Eigenschaften-Gruppe bearbeiten';
 // Column
 $_['column_name']       = 'Eigenschaften-Gruppe';
 $_['column_sort_order'] = 'Sortierreihenfolge';
-$_['column_action']     = 'Aktion';
+$_['column_action']     = 'Aktionen';
 
 // Entry
-$_['entry_name']        = 'Eigenschaften-Gruppe Titel';
+$_['entry_name']        = 'Eigenschaften-Gruppe Name';
 $_['entry_sort_order']  = 'Sortierreihenfolge';
 
 // Error
 $_['error_warning']     = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']  = 'Warnung: Sie haben keine Berechtigung Eigenschafts-Gruppen zu modifizieren!';
-$_['error_name']        = 'Der Eigenschaften-Gruppe Titel muss zwischen 1 und 64 Zeichen lang sein!';
-$_['error_attribute']   = 'Warnung: Diese Eigenschafts-Gruppe kann nicht gelöscht werden, da sie derzeit %s-Eigenschaften zugewiesen ist!';
-$_['error_product']     = 'Warnung: Diese Eigenschafts-Gruppe kann nicht gelöscht werden, da sie derzeit %s Produkten zugewiesen ist!';
+$_['error_permission']  = 'Warnung: Sie haben keine Berechtigung Eigenschaften-Gruppen zu modifizieren!';
+$_['error_name']        = 'Der Eigenschaften-Gruppe Name muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_attribute']   = 'Warnung: Diese Eigenschaften-Gruppe kann nicht gelöscht werden, da sie derzeit %s-Eigenschaften zugewiesen ist!';
+$_['error_product']     = 'Warnung: Diese Eigenschaften-Gruppe kann nicht gelöscht werden, da sie derzeit %s Produkten zugewiesen ist!';

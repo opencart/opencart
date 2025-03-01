2d667ca11c9ad39f0d616fbd814cc5c077f07207
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:40 +0800

    New translations manufacturer.php (English, United Kingdom)

---
+++ en-GB/catalog/product/manufacturer.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = 'Find Your Favorite Brand';
+
+// Text
+$_['text_brand']       = 'Brand';
+$_['text_index']       = 'Brand Index:';
+$_['text_no_results']  = 'There are no products to list.';
+$_['text_compare']     = 'Product Compare (%s)';
+$_['text_sort']        = 'Sort By';
+$_['text_default']     = 'Default';
+$_['text_name_asc']    = 'Name (A - Z)';
+$_['text_name_desc']   = 'Name (Z - A)';
+$_['text_price_asc']   = 'Price (Low &gt; High)';
+$_['text_price_desc']  = 'Price (High &gt; Low)';
+$_['text_rating_asc']  = 'Rating (Lowest)';
+$_['text_rating_desc'] = 'Rating (Highest)';
+$_['text_model_asc']   = 'Model (A - Z)';
+$_['text_model_desc']  = 'Model (Z - A)';
+$_['text_limit']       = 'Show';

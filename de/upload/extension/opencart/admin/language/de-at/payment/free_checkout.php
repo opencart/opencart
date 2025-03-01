c442b040d575ae36dcab309f05f409e30a3432b3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:03:26 +0800

    New translations free_checkout.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/payment/free_checkout.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']      = 'Kostenlose Kaufabwicklung';
+
+// Text
+$_['text_extension']     = 'Erweiterungen';
+$_['text_success']       = 'Erfolg: Sie haben das kostenlose Checkout-Zahlungsmodul geändert!';
+$_['text_edit']          = 'Kostenlose Kasse bearbeiten';
+
+// Entry
+$_['entry_order_status'] = 'Bestellstatus';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, die Zahlung an der kostenlosen Kasse zu ändern!';

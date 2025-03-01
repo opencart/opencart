f8380af2fada6969c9f82d10fa2d4bf9a9508b3f
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:59:58 +0800

    New translations order.php (German)

---
+++ de/upload/admin/language/de-at/sale/order.php
@@ -0,0 +1,128 @@
+<?php
+// Heading
+$_['heading_title']              = 'Bestellungen';
+
+// Text
+$_['text_success']               = 'Erfolg: Sie haben Bestellungen geändert!';
+$_['text_list']                  = 'Bestellliste';
+$_['text_add']                   = 'Bestellung hinzufügen';
+$_['text_edit']                  = 'Reihenfolge (#%s)';
+$_['text_filter']                = 'Filter';
+$_['text_store']                 = 'Speichern';
+$_['text_date_added']            = 'Datum hinzugefügt';
+$_['text_customer']              = 'Kunde';
+$_['text_product_add']           = 'Produkt hinzufügen';
+$_['text_model'] 			     = 'Modell';
+$_['text_reward']                = 'Belohnungspunkte';
+$_['text_points']                = 'Punkte';
+$_['text_reward_add']            = 'Erfolg: Prämienpunkte hinzugefügt!';
+$_['text_reward_remove']         = 'Erfolg: Prämienpunkte entfernt!';
+$_['text_affiliate']             = 'Partnerprogramm';
+$_['text_commission']            = 'Kommission';
+$_['text_commission_add']        = 'Erfolg: Provision hinzugefügt!';
+$_['text_commission_remove']     = 'Erfolg: Provision abgezogen!';
+$_['text_upload']                = 'Ihre Datei wurde erfolgreich hochgeladen!';
+$_['text_subscription']          = 'Abonnement';
+$_['text_subscription_trial']    = '%s alle %d %s(s) für %d Zahlung(en) dann ';
+$_['text_subscription_duration'] = '%s alle %d %s(s) für %d Zahlung(en)';
+$_['text_subscription_cancel']   = '%s alle %d %s(s) bis zum Abbruch';
+$_['text_day']                   = 'Tag';
+$_['text_week']                  = 'Woche';
+$_['text_semi_month']            = 'halber Monat';
+$_['text_month']                 = 'Monat';
+$_['text_year']                  = 'Jahr';
+$_['text_more']                  = 'Mehr..';
+$_['text_less']                  = 'Weniger..';
+$_['text_payment_address']       = 'Zahlungsadresse';
+$_['text_payment_method']        = 'Zahlungsmethode';
+$_['text_payment']               = 'Bitte wählen Sie die gewünschte Zahlungsmethode für diese Bestellung aus.';
+$_['text_shipping_address']      = 'Lieferadresse';
+$_['text_shipping_method']       = 'Versandart';
+$_['text_shipping']              = 'Bitte wählen Sie die bevorzugte Versandmethode für diese Bestellung aus.';
+$_['text_comment']               = 'Kommentar';
+$_['text_history']               = 'Geschichte';
+$_['text_history_add']           = 'Verlauf hinzufügen';
+$_['text_browser']               = 'Browser';
+$_['text_ip']                    = 'IP-Adresse';
+$_['text_forwarded_ip']          = 'Weitergeleitete IP';
+$_['text_user_agent']            = 'Benutzeragent';
+$_['text_accept_language']       = 'Sprache akzeptieren';
+$_['text_order_id']              = 'Bestellnummer';
+$_['text_website']               = 'Website';
+$_['text_invoice']               = 'Rechnung';
+$_['text_invoice_no']            = 'Rechnungsnummer';
+$_['text_tbc']                   = 'Wird noch bekannt gegeben';
+$_['text_store_address']         = 'Adresse des Geschäfts';
+$_['text_store_telephone']       = 'Telefon speichern';
+$_['text_store_email']           = 'E-Mail speichern';
+$_['text_customer_email']        = 'Kunden E-Mail';
+$_['text_customer_telephone']    = 'Kundentelefon';
+$_['text_missing']               = 'Fehlende Bestellungen';
+$_['text_default']               = 'Standard';
+$_['text_picklist']              = 'Versandschein';
+
+// Column
+$_['column_order_id']            = 'Bestellnummer';
+$_['column_customer']            = 'Kunde';
+$_['column_store']               = 'Speichern';
+$_['column_status']              = 'Status';
+$_['column_date_added']          = 'Datum hinzugefügt';
+$_['column_date_modified']       = 'Änderungsdatum';
+$_['column_total']               = 'Gesamt';
+$_['column_product']             = 'Produkt';
+$_['column_model']               = 'Modell';
+$_['column_quantity']            = 'Menge';
+$_['column_price']               = 'Stückpreis';
+$_['column_comment']             = 'Kommentar';
+$_['column_notify']              = 'Kunde benachrichtigt';
+$_['column_location']            = 'Standort';
+$_['column_reference']           = 'Referenz';
+$_['column_weight']              = 'Produktgewicht';
+$_['column_action']              = 'Aktion';
+
+// Entry
+$_['entry_store']                = 'Speichern';
+$_['entry_customer']             = 'Kunde';
+$_['entry_customer_group']       = 'Kundengruppe';
+$_['entry_firstname']            = 'Vorname';
+$_['entry_lastname']             = 'Nachname';
+$_['entry_email']                = 'E-Mail';
+$_['entry_telephone']            = 'Telefon';
+$_['entry_address']              = 'Adresse auswählen';
+$_['entry_company']              = 'Unternehmen';
+$_['entry_address_1']            = 'Adresse 1';
+$_['entry_address_2']            = 'Adresse 2';
+$_['entry_city']                 = 'Stadt';
+$_['entry_postcode']             = 'Postleitzahl';
+$_['entry_country']              = 'Land';
+$_['entry_zone']                 = 'Region/Staat';
+$_['entry_product']              = 'Produkt auswählen';
+$_['entry_option']               = 'Option(en) auswählen';
+$_['entry_subscription']         = 'Abonnement auswählen';
+$_['entry_quantity']             = 'Menge';
+$_['entry_order_status']         = 'Bestellstatus';
+$_['entry_notify']               = 'Kunden benachrichtigen';
+$_['entry_shipping_method']      = 'Versandart';
+$_['entry_payment_method']       = 'Zahlungsmethode';
+$_['entry_override']             = 'Überschreiben';
+$_['entry_comment']              = 'Kommentar';
+$_['entry_language']             = 'Sprache';
+$_['entry_currency']             = 'Währung';
+$_['entry_affiliate']            = 'Partnerprogramm';
+$_['entry_order_id']             = 'Bestellnummer';
+$_['entry_total']                = 'Gesamt';
+$_['entry_date_from']            = 'Stammen aus';
+$_['entry_date_to']              = 'Datum bis';
+
+// Help
+$_['help_override']              = 'Wenn die Änderung des Bestellstatus der Kundenbestellung aufgrund einer Betrugsschutzerweiterung blockiert ist, aktivieren Sie die Überschreibung.';
+
+// Error
+$_['error_warning']              = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']           = 'Achtung: Sie sind nicht berechtigt, Bestellungen zu ändern!';
+$_['error_invoice_no']           = 'Achtung: Rechnungsnummer wurde bereits erstellt!';
+$_['error_order']                = 'Achtung: Bestellung existiert nicht!';
+$_['error_affiliate']            = 'Achtung: Partner existiert nicht!';
+$_['error_reward_add']           = 'Achtung: Für diese Bestellung wurden bereits Prämienpunkte gutgeschrieben!';
+$_['error_reward_guest']         = 'Achtung: Gäste können keine Prämienpunkte einlösen!';
+$_['error_commission_add']       = 'Achtung: Für diese Bestellung wurde bereits eine Affiliate-Provision hinzugefügt!';

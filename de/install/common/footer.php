38d0172c41e4c46735ea255250d4a9c2142e59db
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-02 00:06:17 +0800

    New translations footer.php (German)

--- de/install/common/footer.php
+++ de/install/common/footer.php
@@ -2,5 +2,5 @@
 // Text
 $_['text_project']       = 'Projekt Homepage';
 $_['text_documentation'] = 'Dokumentation';
-$_['text_support']       = 'Support-Foren';
+$_['text_support']       = 'Support Foren';
 $_['text_footer']        = '<a href="https://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Alle Rechte vorbehalten.';

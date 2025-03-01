234bcf23d2a80ff9acd44ab1b698aa117e865a9c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 20:54:41 +0800

    New translations feed.php (German)

--- de/admin/extension/feed.php
+++ de/admin/extension/feed.php
@@ -9,7 +9,7 @@ $_['text_list']        = 'Feedliste';
 // Column
 $_['column_name']      = 'Produkt-Feedname';
 $_['column_status']    = 'Status';
-$_['column_action']    = 'Aktion';
+$_['column_action']    = 'Aktionen';
 
 // Error
 $_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Feeds zu ändern!';

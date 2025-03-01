1d3e0e3884e167409cdc5cbd1d9e6b6957324e2e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:03:46 +0800

    New translations sale_return.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/report/sale_return.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']       = 'Retourenbericht';
+
+// Text
+$_['text_extension']      = 'Erweiterungen';
+$_['text_edit']           = 'Retourenbericht bearbeiten';
+$_['text_success']        = 'Erfolg: Sie haben den Retourenbericht geändert!';
+$_['text_filter']         = 'Filter';
+$_['text_year']           = 'Jahre';
+$_['text_month']          = 'Monate';
+$_['text_week']           = 'Wochen';
+$_['text_day']            = 'Tage';
+$_['text_all_status']     = 'Alle Status';
+
+// Column
+$_['column_date_start']   = 'Datum Beginn';
+$_['column_date_end']     = 'Datum Ende';
+$_['column_returns']      = 'Nr. Rücksendungen';
+
+// Entry
+$_['entry_date_start']    = 'Datum Beginn';
+$_['entry_date_end']      = 'Datum Ende';
+$_['entry_group']         = 'Gruppieren nach';
+$_['entry_return_status'] = 'Rückgabestatus';
+$_['entry_status']        = 'Status';
+$_['entry_sort_order']    = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission']    = 'Achtung: Sie sind nicht berechtigt, den Retourenbericht zu ändern!';

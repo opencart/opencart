fda565b0526ccabeef62afbb5accb14320eafa1f
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:07:52 +0800

    New translations subscription.php (German)

---
+++ de/catalog/cron/subscription.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_log']               = 'Weitere Informationen finden Sie im Abonnementprotokoll.';
+
+// Error
+$_['error_store'] 		     = 'Achtung: Store nicht gefunden!';
+$_['error_language']         = 'Achtung: Sprache konnte nicht gefunden werden!';
+$_['error_currency']         = 'Achtung: Währung konnte nicht gefunden werden!';
+$_['error_customer']         = 'Achtung: Kunde konnte nicht gefunden werden!';
+$_['error_payment_address']  = 'Achtung: Zahlungsadresse konnte nicht gefunden werden!';
+$_['error_shipping_address'] = 'Achtung: Lieferadresse konnte nicht gefunden werden!';
+$_['error_product']          = 'Achtung: Produkt %s konnte nicht gefunden werden!';
+$_['error_option']           = 'Warnung: Option %s konnte nicht gefunden werden!';
+$_['error_shipping_method']  = 'Achtung: Versandart erforderlich!';
+$_['error_payment_method']   = 'Achtung: Zahlungsmethode erforderlich!';
+$_['error_extension']        = 'Achtung: Zahlungsmethoden-Erweiterung konnte nicht gefunden werden!';

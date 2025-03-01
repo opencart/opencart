6830410cac3e152cab9301a4bf3bd4cdf4416aa0
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:04:58 +0800

    New translations returns.php (German)

---
+++ de/admin/mail/returns.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_subject']       = '%s - Rückgabe-Update %s';
+$_['text_return_id']     = 'Rücksende-ID:';
+$_['text_date_added']    = 'Rückreisedatum:';
+$_['text_return_status'] = 'Ihre Rücksendung wurde auf den folgenden Status aktualisiert:';
+$_['text_comment']       = 'Die Kommentare zu Ihrer Rücksendung lauten:';
+$_['text_footer']        = 'Bitte antworten Sie auf diese E-Mail, wenn Sie Fragen haben.';

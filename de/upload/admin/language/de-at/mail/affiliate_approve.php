6202246cd763f301c5a98234e6833b23c417100c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:59:06 +0800

    New translations affiliate_approve.php (German)

---
+++ de/upload/admin/language/de-at/mail/affiliate_approve.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_subject']  = '%s – Ihr Partnerkonto wurde aktiviert!';
+$_['text_welcome']  = 'Willkommen und danke für Ihre Registrierung bei %s!';
+$_['text_login']    = 'Ihr Konto wurde nun genehmigt und Sie können sich mit Ihrer E-Mail-Adresse und Ihrem Passwort anmelden, indem Sie unsere Website besuchen oder die folgende URL aufrufen:';
+$_['text_service']  = 'Nach der Anmeldung können Sie Tracking-Codes generieren, Provisionszahlungen verfolgen und Ihre Kontoinformationen bearbeiten.';
+$_['text_thanks']   = 'Danke,';

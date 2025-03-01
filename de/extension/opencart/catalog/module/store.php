5b5ae4ce0b45ec5452990ffd3246a109750476a0
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:11:31 +0800

    New translations store.php (German)

---
+++ de/extension/opencart/catalog/module/store.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'Wählen Sie einen Store';
+
+// Text
+$_['text_default']  = 'Standard';
+$_['text_store']    = 'Bitte wählen Sie das Geschäft aus, das Sie besuchen möchten.';

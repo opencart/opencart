048281b19fd69739877a6efabce74610141092ca
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:04:03 +0800

    New translations address_format.php (English, United Kingdom)

---
+++ en-GB/admin/localisation/address_format.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']           = 'Address Format';
+
+// Text
+$_['text_success']            = 'Success: You have modified address formats!';
+$_['text_list']               = 'Address Format List';
+$_['text_add']                = 'Add Address Format';
+$_['text_edit']               = 'Edit Address Format';
+
+// Column
+$_['column_name']             = 'Address Format Name';
+$_['column_address_format']   = 'Address Format';
+$_['column_action']           = 'Action';
+
+// Entry
+$_['entry_name']              = 'Address Format Name';
+$_['entry_address_format']    = 'Address Format';
+
+// Help
+$_['help_address_format']     = 'First Name = {firstname}<br/>Last Name = {lastname}<br/>Company = {company}<br/>Address 1 = {address_1}<br/>Address 2 = {address_2}<br/>City = {city}<br/>Postcode = {postcode}<br/>Zone = {zone}<br/>Zone Code = {zone_code}<br/>Country = {country}';
+
+// Error
+$_['error_permission']        = 'Warning: You do not have permission to modify address formats!';
+$_['error_name']              = 'Address Format Name must be between 1 and 128 characters!';
+$_['error_default']           = 'Warning: This address format cannot be deleted as it is currently assigned as the default address format!';
+$_['error_country']           = 'Warning: This address format cannot be deleted as it is currently assigned to %s countries!';

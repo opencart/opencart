1436633ed7cdc417f55ae0c1c56ac12f250390d5
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:03:02 +0800

    New translations security.php (English, United Kingdom)

---
+++ en-GB/admin/common/security.php
@@ -0,0 +1,47 @@
+<?php
+// Heading
+$_['heading_title']                   = 'Important Security Notification!';
+
+// Text
+$_['text_install']                    = 'Delete installation directory';
+$_['text_install_description']        = 'Your installation directory needs deleting!';
+$_['text_install_success']            = 'Success: Install directory has been deleted!';
+$_['text_storage']                    = 'Move storage directory';
+$_['text_storage_description']        = 'It is very important that you move the storage directory outside of the web directory (e.g. public_html, www or htdocs).';
+$_['text_storage_move']               = 'Moving %s to %s of %s storage files';
+$_['text_storage_success']            = 'Success: Storage directory has been moved!';
+$_['text_storage_delete']             = 'Delete previous storage directory';
+$_['text_storage_delete_description'] = 'Your previous storage directory needs deleting!';
+$_['text_storage_delete_success']     = 'Success: Previous storage directory has been deleted!';
+$_['text_admin']                      = 'Move admin directory';
+$_['text_admin_description']          = 'Please type in a new admin directory name in the field below.';
+$_['text_admin_move']                 = 'Moving %s to %s of %s admin files';
+$_['text_admin_success']              = 'Success: Admin directory has been moved!';
+$_['text_admin_delete']               = 'Delete previous admin directory';
+$_['text_admin_delete_description']   = 'Your previous admin directory needs deleting!';
+$_['text_admin_delete_success']       = 'Success: Previous admin directory has been deleted!';
+$_['text_path']                       = 'Path';
+
+// Entry
+$_['entry_path']                      = 'Path';
+$_['entry_path_current']              = 'Current Path';
+$_['entry_path_new']                  = 'New Path';
+$_['entry_name']                      = 'Directory Name';
+
+// Buttons
+$_['button_move']                     = 'Move';
+
+// Help
+$_['help_storage']                    = 'Storage directory name must begin with storage. e.g. `storage_`.';
+
+// Error
+$_['error_permission']                = 'Warning: You do not have permission to modify security!';
+$_['error_install']                   = 'Warning: Install directory does not exist!';
+$_['error_storage']                   = 'Warning: Storage directory does not exist!';
+$_['error_storage_root']              = 'Warning: Storage directory must be outside the web root!';
+$_['error_storage_name']              = 'Warning: Storage directory name must start with `storage`! e.g. `storage_`.';
+$_['error_admin']                     = 'Warning: Admin directory does not exist!';
+$_['error_admin_allowed']             = 'Warning: This admin directory name can not be used!';
+$_['error_admin_exists']              = 'Warning: Admin directory already exists!';
+$_['error_writable']                  = 'Warning: config.php and admin/config.php need to be made writable!';
+$_['error_remove']                    = 'Warning: Directory does not exist to remove!';

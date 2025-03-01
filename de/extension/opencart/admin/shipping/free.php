0d06ae206f496ee4ace95fb732c55235089f6cfa
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:42 +0800

    New translations free.php (German)

---
+++ de/extension/opencart/admin/shipping/free.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = 'Kostenloser Versand';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben den Gratisversand geändert!';
+$_['text_edit']        = 'Kostenloser Versand bearbeiten';
+
+// Entry
+$_['entry_total']      = 'Zwischensumme';
+$_['entry_geo_zone']   = 'Geozone';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+
+// Help
+$_['help_total']       = 'Erforderlicher Zwischengesamtbetrag, bevor das Modul für kostenlosen Versand verfügbar wird.';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Gratisversand zu ändern!';

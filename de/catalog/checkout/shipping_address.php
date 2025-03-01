413163b59c905554fc368c8868882064c495cc6c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 18:05:41 +0800

    New translations shipping_address.php (German)

--- de/catalog/checkout/shipping_address.php
+++ de/catalog/checkout/shipping_address.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']         = 'Shipping Address';
+$_['heading_title']         = 'Lieferadresse';
 
 // Text
 $_['text_address_new']      = 'Ich möchte eine neue Adresse verwenden';
@@ -8,7 +8,7 @@ $_['text_address_existing'] = 'Ich möchte eine bestehende Adresse verwenden';
 $_['text_success']          = 'Erfolg: Sie haben die Lieferadresse geändert!';
 
 // Entry
-$_['entry_address']         = 'Choose Address';
+$_['entry_address']         = 'Adresse auswählen';
 $_['entry_firstname']       = 'Vorname';
 $_['entry_lastname']        = 'Nachname';
 $_['entry_company']         = 'Unternehmen';
@@ -23,7 +23,7 @@ $_['entry_zone']            = 'Region/Staat';
 $_['error_address']         = 'Lieferadresse konnte nicht gefunden werden!';
 $_['error_firstname']       = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
 $_['error_lastname']        = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
-$_['error_address_1']       = 'Address 1 must be between 3 and 128 characters!';
+$_['error_address_1']       = 'Adresse 1 muss zwischen 3 und 128 Zeichen lang sein!';
 $_['error_city']            = 'Die Stadt muss zwischen 2 und 128 Zeichen lang sein!';
 $_['error_postcode']        = 'Die Postleitzahl muss zwischen 2 und 10 Zeichen lang sein!';
 $_['error_country']         = 'Bitte wählen Sie ein Land!';

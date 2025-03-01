d8dfc0b88b58c6b7ec69ade81040c6cfcfabbd28
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:03:28 +0800

    New translations customer.php (German)

---
+++ de/upload/extension/opencart/admin/language/de-at/report/customer.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']     = 'Kundenbericht';
+
+// Text
+$_['text_extension']    = 'Erweiterungen';
+$_['text_edit']         = 'Kundenbericht bearbeiten';
+$_['text_success']      = 'Erfolg: Sie haben den Kundenbericht geändert!';
+$_['text_filter']       = 'Filter';
+$_['text_year']         = 'Jahre';
+$_['text_month']        = 'Monate';
+$_['text_week']         = 'Wochen';
+$_['text_day']          = 'Tage';
+$_['text_all_status']   = 'Alle Status';
+
+// Column
+$_['column_date_start'] = 'Datum Beginn';
+$_['column_date_end']   = 'Datum Ende';
+$_['column_total']      = 'Anzahl Kunden';
+
+// Entry
+$_['entry_date_start']  = 'Datum Beginn';
+$_['entry_date_end']    = 'Datum Ende';
+$_['entry_group']       = 'Gruppieren nach';
+$_['entry_status']      = 'Status';
+$_['entry_sort_order']  = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, Kundenberichte zu ändern!';

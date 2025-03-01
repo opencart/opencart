96593fb08c81c342c0a05316c0a884b6ae8d906f
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:04:04 +0800

    New translations country.php (German)

---
+++ de/admin/localisation/country.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']           = 'Länder';
+
+// Text
+$_['text_success']            = 'Erfolg: Sie haben Länder geändert!';
+$_['text_list']               = 'Länderliste';
+$_['text_add']                = 'Land hinzufügen';
+$_['text_edit']               = 'Land bearbeiten';
+$_['text_filter']             = 'Filter';
+
+// Column
+$_['column_name']             = 'Ländername';
+$_['column_iso_code_2']       = 'ISO-Code (2)';
+$_['column_iso_code_3']       = 'ISO-Code (3)';
+$_['column_action']           = 'Aktion';
+
+// Entry
+$_['entry_name']              = 'Ländername';
+$_['entry_iso_code_2']        = 'ISO-Code (2)';
+$_['entry_iso_code_3']        = 'ISO-Code (3)';
+$_['entry_address_format']    = 'Adressformat';
+$_['entry_postcode_required'] = 'Postleitzahl erforderlich';
+$_['entry_status']            = 'Status';
+
+// Error
+$_['error_permission']        = 'Achtung: Sie sind nicht berechtigt, Länder zu ändern!';
+$_['error_name']              = 'Der Ländername muss zwischen 1 und 128 Zeichen lang sein!';
+$_['error_iso_code_2']        = 'ISO-Code 2 muss 2 Zeichen lang sein!';
+$_['error_iso_code_3']        = 'ISO-Code 3 muss zwischen 3 Zeichen liegen!';
+$_['error_default']           = 'Achtung: Dieses Land kann nicht gelöscht werden, da es aktuell als Standard-Shop-Land zugewiesen ist!';
+$_['error_store']             = 'Achtung: Dieses Land kann nicht gelöscht werden, da es aktuell %s Filialen zugeordnet ist!';
+$_['error_address']           = 'Achtung: Dieses Land kann nicht gelöscht werden, da ihm aktuell %s Adressbucheinträgen zugeordnet sind!';
+$_['error_zone']              = 'Achtung: Dieses Land kann nicht gelöscht werden, da es aktuell den Zonen %s zugeordnet ist!';
+$_['error_zone_to_geo_zone']  = 'Achtung: Dieses Land kann nicht gelöscht werden, da es aktuell den Geozonen %s zugeordnet ist!';

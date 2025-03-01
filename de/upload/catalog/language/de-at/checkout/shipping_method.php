e7233dcb42b6e62291f66b71b23a1e9670f33c14
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:01:25 +0800

    New translations shipping_method.php (German)

---
+++ de/upload/catalog/language/de-at/checkout/shipping_method.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']          = 'Versandart';
+
+// Text
+$_['text_shipping_method']   = 'Optionen für Versandarten';
+$_['text_shipping']          = 'Bitte wählen Sie die bevorzugte Versandmethode für diese Bestellung aus.';
+$_['text_success']           = 'Erfolg: Sie haben die Versandart geändert!';
+
+// Entry
+$_['entry_shipping_method']  = 'Versandart auswählen...';
+
+// Error
+$_['error_customer']         = 'Kunde gesucht!';
+$_['error_payment_address']  = 'Zahlungsadresse erforderlich!';
+$_['error_shipping_address'] = 'Lieferadresse erforderlich!';
+$_['error_shipping_method']  = 'Versandart erforderlich!';
+$_['error_no_shipping']      = 'Es sind keine Versandoptionen verfügbar. Bitte <a href="%s">kontaktieren Sie uns</a> für Hilfe!';

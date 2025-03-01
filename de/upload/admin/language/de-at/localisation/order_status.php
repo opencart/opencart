c36c98ea49fc5da9f848c59722e32c6b94ef4a51
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:58:48 +0800

    New translations order_status.php (German)

---
+++ de/upload/admin/language/de-at/localisation/order_status.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']    = 'Bestellstatus';
+
+// Text
+$_['text_success']     = 'Erfolg: Sie haben den Bestellstatus geändert!';
+$_['text_list']        = 'Bestellstatusliste';
+$_['text_add']         = 'Bestellstatus hinzufügen';
+$_['text_edit']        = 'Bestellstatus bearbeiten';
+
+// Column
+$_['column_name']      = 'Bestellstatusname';
+$_['column_action']    = 'Aktion';
+
+// Entry
+$_['entry_name']       = 'Bestellstatusname';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, den Bestellstatus zu ändern!';
+$_['error_name']       = 'Der Name des Bestellstatus muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_default']    = 'Achtung: Dieser Bestellstatus kann nicht gelöscht werden, da er aktuell als Standardbestellstatus des Shops zugewiesen ist!';
+$_['error_download']   = 'Achtung: Dieser Bestellstatus kann nicht gelöscht werden, da er aktuell als Standard-Download-Status zugewiesen ist!';
+$_['error_order']      = 'Achtung: Dieser Bestellstatus kann nicht gelöscht werden, da er aktuell %s Bestellungen zugewiesen ist!';

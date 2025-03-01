3da2a91cf465885b3eed56deaf786c505bfb0ae4
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:58:31 +0800

    New translations report.php (German)

---
+++ de/upload/admin/language/de-at/extension/report.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']     = 'Berichte';
+
+// Text
+$_['text_success']      = 'Erfolg: Sie haben Berichte geändert!';
+$_['text_list']         = 'Berichtsliste';
+
+// Column
+$_['column_name']       = 'Berichtsname';
+$_['column_status']     = 'Status';
+$_['column_sort_order'] = 'Sortierreihenfolge';
+$_['column_action']     = 'Aktion';
+
+// Error
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, Berichte zu ändern!';
+$_['error_extension']   = 'Achtung: Erweiterung existiert nicht!';

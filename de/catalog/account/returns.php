53820a10706f36d350862b4a8f1c1a537dea77ab
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:06:36 +0800

    New translations returns.php (German)

---
+++ de/catalog/account/returns.php
@@ -0,0 +1,66 @@
+<?php
+// Heading
+$_['heading_title']      = 'Rückgaben';
+
+// Text
+$_['text_account']       = 'Konto';
+$_['text_return']        = 'Rückgabeinformationen';
+$_['text_description']   = 'Bitte füllen Sie das untenstehende Formular aus, um eine RMA-Nummer anzufordern.';
+$_['text_order']         = 'Bestellinformationen';
+$_['text_customer']      = 'Ihre Informationen';
+$_['text_product']       = 'Produktinformationen';
+$_['text_message']       = '<p>Vielen Dank für Ihre Rücksendeanforderung. Ihre Anfrage wurde zur Bearbeitung an die zuständige Abteilung weitergeleitet.</p><p> Sie werden per E-Mail über den Status Ihrer Anfrage informiert.</p>';
+$_['text_return_id']     = 'Rücksende-ID';
+$_['text_order_id']      = 'Bestellnummer';
+$_['text_date_ordered']  = 'Bestelldatum';
+$_['text_date_added']    = 'Datum hinzugefügt';
+$_['text_opened']        = 'Geöffnet';
+$_['text_unopened']      = 'Ungeöffnet';
+$_['text_reason']        = 'Grund für die Rücksendung';
+$_['text_action']        = 'Aktion';
+$_['text_tbc']           = 'Wird noch bekannt gegeben';
+$_['text_history']       = 'Geschichte';
+$_['text_comment']       = 'Kommentar';
+$_['text_history']       = 'Rückgabehistorie';
+$_['text_no_results']    = 'Sie haben bisher noch keine Rücksendung getätigt!';
+$_['text_agree']         = 'Ich habe die <a href="%s" class="modal-link"><b>%s</b></a>gelesen und stimme ihnen zu';
+
+// Column
+$_['column_return_id']   = 'Rücksende-ID';
+$_['column_order_id']    = 'Bestellnummer';
+$_['column_status']      = 'Status';
+$_['column_date_added']  = 'Datum hinzugefügt';
+$_['column_product']     = 'Produkt';
+$_['column_model']       = 'Modell';
+$_['column_quantity']    = 'Menge';
+$_['column_opened']      = 'Geöffnet';
+$_['column_comment']     = 'Kommentar';
+$_['column_reason']      = 'Grund';
+$_['column_action']      = 'Aktion';
+
+// Entry
+$_['entry_order_id']     = 'Bestellnummer';
+$_['entry_date_ordered'] = 'Bestelldatum';
+$_['entry_firstname']    = 'Vorname';
+$_['entry_lastname']     = 'Nachname';
+$_['entry_email']        = 'E-Mail';
+$_['entry_telephone']    = 'Telefon';
+$_['entry_product']      = 'Produktname';
+$_['entry_model']        = 'Produktcode';
+$_['entry_quantity']     = 'Menge';
+$_['entry_reason']       = 'Grund für die Rücksendung';
+$_['entry_opened']       = 'Produkt ist geöffnet';
+$_['entry_fault_detail'] = 'Fehlerhafte oder andere Details';
+
+// Error
+$_['error_return']       = 'Die von Ihnen gewünschte Retoure konnte nicht gefunden werden!';
+$_['error_token']        = 'Achtung: Reset-Token ungültig!';
+$_['error_order_id']     = 'Bestell-ID erforderlich!';
+$_['error_firstname']    = 'Der Vorname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_lastname']     = 'Der Nachname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_email']        = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_telephone']    = 'Die Telefonnummer muss zwischen 3 und 32 Zeichen lang sein!';
+$_['error_product']      = 'Der Produktname muss länger als 3 und kürzer als 255 Zeichen sein!';
+$_['error_model']        = 'Das Produktmodell muss länger als 3 und kürzer als 64 Zeichen sein!';
+$_['error_reason']       = 'Sie müssen einen Grund für die Rückgabe des Produkts auswählen!';
+$_['error_agree']        = 'Achtung: Sie müssen den %szustimmen!';

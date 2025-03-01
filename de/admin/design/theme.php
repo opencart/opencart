a53989ba18e1af40fca5298176b5ecf1985a18d5
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:03:22 +0800

    New translations theme.php (German)

---
+++ de/admin/design/theme.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']     = 'Themen-Editor';
+
+// Text
+$_['text_success']      = 'Erfolg: Sie haben Themen geändert!';
+$_['text_add']          = 'Design hinzufügen';
+$_['text_edit']         = 'Design bearbeiten';
+$_['text_default']      = 'Standard';
+$_['text_extension']    = 'Erweiterungen';
+$_['text_code']         = 'Vorlagencode-Editor';
+$_['text_twig']         = 'Der Theme-Editor verwendet die Template-Sprache Twig. Informationen zur <a href="https://twig.symfony.com/doc/" target="_blank" class="alert-link">Twig-Syntax finden Sie hier</a>.';
+
+// Column
+$_['column_store']      = 'Speichern';
+$_['column_route']      = 'Route';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Datum hinzugefügt';
+$_['column_action']     = 'Aktion';
+
+// Entry
+$_['entry_store']       = 'Speichern';
+$_['entry_route']       = 'Vorlage auswählen';
+$_['entry_code']        = 'Code';
+$_['entry_status']      = 'Status';
+
+// Error
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, den Design-Editor zu ändern!';
+$_['error_file']        = 'Achtung: Vorlagendatei existiert nicht!';

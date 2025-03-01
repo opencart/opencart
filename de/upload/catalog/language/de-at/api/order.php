b5260eb22510afb7b06de8760952bce303912af4
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:01:02 +0800

    New translations order.php (German)

---
+++ de/upload/catalog/language/de-at/api/order.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_success']           = 'Erfolg: Sie haben Bestellungen geändert!';
+
+// Error
+$_['error_order']            = 'Achtung: Bestellung konnte nicht gefunden werden!';
+$_['error_customer']         = 'Achtung: Kundendaten erforderlich!';
+$_['error_payment_address']  = 'Achtung: Zahlungsadresse erforderlich!';
+$_['error_payment_method']   = 'Achtung: Zahlungsmethode erforderlich!';
+$_['error_shipping_address'] = 'Achtung: Lieferadresse erforderlich!';
+$_['error_shipping_method']  = 'Achtung: Versandart erforderlich!';
+$_['error_product']          = 'Achtung: Produkte erforderlich!';
+$_['error_stock']            = 'Achtung: Mit *** gekennzeichnete Produkte sind nicht in der gewünschten Menge verfügbar bzw. nicht auf Lager!';
+$_['error_minimum']          = 'Achtung: Mindestbestellmenge für %s ist %s!';
+$_['error_affiliate']        = 'Achtung: Partnerdaten erforderlich!';
+$_['error_call']             = 'API-Aufruf nicht gefunden';

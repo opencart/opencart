f3e7eae71630105ef762e72d6a024360673b272b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:05:21 +0800

    New translations step_4.php (German)

---
+++ de/upload/install/language/de-at/install/step_4.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']               = 'Installation abgeschlossen';
+
+// Text
+$_['text_step_4']                 = 'Bereit zum Verkaufen!';
+$_['text_catalog']                = 'Gehen Sie zu Ihrem Online-Shop';
+$_['text_admin']                  = 'Melden Sie sich bei Ihrer Administration an';
+$_['text_extension']              = 'Besuchen Sie den Extensions Store';
+$_['text_mail']                   = 'Mailingliste abonnieren';
+$_['text_mail_description']       = 'Bleiben Sie über OpenCart-Updates und -Events auf dem Laufenden.';
+$_['text_facebook']               = 'Like uns auf Facebook';
+$_['text_facebook_description']   = 'Sagen Sie uns, wie sehr Ihnen OpenCart gefällt!';
+$_['text_facebook_visit']         = 'Besuchen Sie unsere Facebook-Seite';
+$_['text_forum']                  = 'Community-Foren';
+$_['text_forum_description']      = 'Holen Sie sich Hilfe von anderen OpenCart-Benutzern';
+$_['text_forum_visit']            = 'Besuchen Sie unsere Foren';
+$_['text_commercial']             = 'Kommerzielle Unterstützung';
+$_['text_commercial_description'] = 'Entwicklungsservices von OpenCart-Partnern';
+$_['text_commercial_visit']       = 'Besuchen Sie unsere Partnerseite';
+
+// Button
+$_['button_mail']                 = 'Hier anmelden';
+
+// Error
+$_['error_warning']               = 'Vergessen Sie nicht, Ihr Installationsverzeichnis zu löschen!';

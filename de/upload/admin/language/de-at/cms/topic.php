89999f862c45d8982f0a2e1c5e4781f93754f712
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:57:27 +0800

    New translations topic.php (German)

---
+++ de/upload/admin/language/de-at/cms/topic.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']           = 'Themen';
+
+// Text
+$_['text_success']            = 'Erfolg: Sie haben Themen geändert!';
+$_['text_list']               = 'Themenliste';
+$_['text_add']                = 'Thema hinzufügen';
+$_['text_edit']               = 'Thema bearbeiten';
+$_['text_default']            = 'Standard';
+$_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL weltweit eindeutig ist.';
+
+// Column
+$_['column_name']             = 'Themenname';
+$_['column_sort_order']       = 'Sortierreihenfolge';
+$_['column_action']           = 'Aktion';
+
+// Entry
+$_['entry_image']             = 'Bild';
+$_['entry_name']              = 'Themenname';
+$_['entry_description']       = 'Beschreibung';
+$_['entry_meta_title']        = 'Meta-Tag-Titel';
+$_['entry_meta_keyword']      = 'Meta-Tag-Schlüsselwörter';
+$_['entry_meta_description']  = 'Meta-Tag-Beschreibung';
+$_['entry_store']             = 'Geschäfte';
+$_['entry_sort_order']        = 'Sortierreihenfolge';
+$_['entry_status']            = 'Status';
+$_['entry_keyword']           = 'Stichwort';
+
+// Error
+$_['error_warning']           = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']        = 'Achtung: Sie sind nicht berechtigt, Themen zu ändern!';
+$_['error_name']              = 'Der Themenname muss zwischen 1 und 255 Zeichen lang sein!';
+$_['error_meta_title']        = 'Der Meta-Titel muss größer als 1 und kleiner als 255 Zeichen sein!';
+$_['error_keyword']           = 'Die SEO-URL muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_keyword_exists']    = 'SEO-URL muss eindeutig sein!';
+$_['error_keyword_character'] = 'Das Schlüsselwort darf nur die Zeichen az, 0-9, - und _ enthalten!';

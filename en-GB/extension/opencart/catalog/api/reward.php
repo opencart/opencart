ecd9a1a0e8b439898269bddc7cc45d2eae325f67
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:11:08 +0800

    New translations reward.php (English, United Kingdom)

---
+++ en-GB/extension/opencart/catalog/api/reward.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+$_['text_success']   = 'Success: Your reward points discount has been applied!';
+
+// Error
+$_['error_customer'] = 'Warning: Customer details required!';
+$_['error_product']  = 'Warning: Products required!';
+$_['error_points']   = 'Warning: You don\'t have %s reward points!';
+$_['error_maximum']  = 'Warning: The maximum number of points that can be applied is %s!';
+$_['error_status']   = 'Warning: Reward points are not enabled on this store!';
+$_['error_confirm']  = 'Warning: Please check the reward form carefully for errors!';

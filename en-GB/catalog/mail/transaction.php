bfa89b5fb134413184cfa52a7eb2834b1d3c9513
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:34 +0800

    New translations transaction.php (English, United Kingdom)

---
+++ en-GB/catalog/mail/transaction.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject']  = '%s - Affiliate Commission';
+$_['text_received'] = 'Congratulations! You have received a commission payment from the %s affiliate program';
+$_['text_amount']   = 'You have received:';
+$_['text_total']    = 'Your total amount of commission is now:';

ef4a68fe7afc2c9cb2c1f83b83be13a154e4a25c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:01:33 +0800

    New translations cookie.php (German)

---
+++ de/upload/catalog/language/de-at/common/cookie.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_success']    = 'Vielen Dank, dass Sie uns Ihre Wahl mitgeteilt haben!';
+$_['text_cookie']     = 'Diese Website verwendet Cookies. Für weitere Informationen <a href="%s" class="alert-link modal-link">klicken Sie hier</a>.';
+
+// Buttons
+$_['button_agree']    = 'Ja, das ist in Ordnung!';
+$_['button_disagree'] = 'Nein danke!';

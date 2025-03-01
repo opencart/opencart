d256e21be52b160e3c41a8beea72b708a1353551
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:02:12 +0800

    New translations attribute_group.php (German)

---
+++ de/admin/catalog/attribute_group.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']     = 'Attributgruppen';
+
+// Text
+$_['text_success']      = 'Erfolg: Sie haben Attributgruppen geändert!';
+$_['text_list']         = 'Attributgruppenliste';
+$_['text_add']          = 'Attributgruppe hinzufügen';
+$_['text_edit']         = 'Attributgruppe bearbeiten';
+
+// Column
+$_['column_name']       = 'Attributgruppenname';
+$_['column_sort_order'] = 'Sortierreihenfolge';
+$_['column_action']     = 'Aktion';
+
+// Entry
+$_['entry_name']        = 'Attributgruppenname';
+$_['entry_sort_order']  = 'Sortierreihenfolge';
+
+// Error
+$_['error_warning']     = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']  = 'Achtung: Sie haben keine Berechtigung, Attributgruppen zu ändern!';
+$_['error_name']        = 'Der Attributgruppenname muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_attribute']   = 'Achtung: Diese Attributgruppe kann nicht gelöscht werden, da ihr aktuell %s Attribute zugewiesen sind!';
+$_['error_product']     = 'Achtung: Diese Attributgruppe kann nicht gelöscht werden, da sie aktuell %s Produkten zugeordnet ist!';

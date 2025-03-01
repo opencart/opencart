168c6bfa57f2fc1b24cd18b8adb1fd712afa8579
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:59:51 +0800

    New translations startup.php (German)

---
+++ de/upload/admin/language/de-at/marketplace/startup.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']     = 'Start-up';
+
+// Text
+$_['text_success']      = 'Erfolg: Sie haben den Start geändert!';
+$_['text_list']         = 'Liste der Startups';
+$_['text_info']         = 'Informationen zum Start';
+
+// Column
+$_['column_code']       = 'Startcode';
+$_['column_sort_order'] = 'Sortierreihenfolge';
+$_['column_action']     = 'Aktion';
+
+// Entry
+$_['entry_description'] = 'Beschreibung';
+
+// Error
+$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, den Start zu ändern!';

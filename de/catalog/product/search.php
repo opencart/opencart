107b16367d73d456c820da3fb9efa249c2831df8
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:47 +0800

    New translations search.php (German)

---
+++ de/catalog/product/search.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']     = 'Suchen';
+$_['heading_tag']       = 'Tag - ';
+
+// Text
+$_['text_search']       = 'Products meeting the search criteria';
+$_['text_keyword']      = 'Keywords';
+$_['text_category']     = 'All Categories';
+$_['text_sub_category'] = 'Search in subcategories';
+$_['text_no_results']   = 'There is no product that matches the search criteria.';
+$_['text_compare']      = 'Produktvergleich (%s)';
+$_['text_sort']         = 'Sortieren nach';
+$_['text_default']      = 'Standard';
+$_['text_name_asc']     = 'Name (A - Z)';
+$_['text_name_desc']    = 'Name (Z - A)';
+$_['text_price_asc']    = 'Preis (Niedrig &gt; Hoch)';
+$_['text_price_desc']   = 'Preis (Hoch &gt; Niedrig)';
+$_['text_rating_asc']   = 'Bewertung (niedrigste)';
+$_['text_rating_desc']  = 'Bewertung (Höchste)';
+$_['text_model_asc']    = 'Modell (A - Z)';
+$_['text_model_desc']   = 'Modell (Z - A)';
+$_['text_limit']        = 'Zeigen';
+
+// Entry
+$_['entry_search']      = 'Search Criteria';
+$_['entry_description'] = 'Search in product descriptions';

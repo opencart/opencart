fd1148950111248780445015e8e3f1aa770f1b94
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 15:59:30 +0800

    New translations transaction.php (German)

---
+++ de/upload/admin/language/de-at/mail/transaction.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject']  = '%s - Affiliate-Guthaben';
+$_['text_received'] = 'Sie haben %s Guthaben erhalten!';
+$_['text_total']    = 'Ihr Gesamtguthaben beträgt jetzt %s.';
+$_['text_credit']   = 'Ihr Guthaben kann bei Ihrem nächsten Einkauf verrechnet werden.';

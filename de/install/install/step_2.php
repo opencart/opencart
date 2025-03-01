5478738740c9a621ea2e1bc69f2be454c7f01f37
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-02 01:08:58 +0800

    New translations step_2.php (German)

--- de/install/install/step_2.php
+++ de/install/install/step_2.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']          = 'Vorinstallation';
+$_['heading_title']          = 'Vorbereitungen für die Installation';
 
 // Text
 $_['text_step_2']            = 'Überprüfen Sie, ob Ihr Server richtig eingerichtet ist';
@@ -10,20 +10,20 @@ $_['text_install_file']      = '3. Bitte stellen Sie sicher, dass Sie für die f
 $_['text_setting']           = 'PHP-Einstellungen';
 $_['text_current']           = 'Aktuelle Einstellungen';
 $_['text_required']          = 'Erforderliche Einstellungen';
-$_['text_extension']         = 'Erweiterungseinstellungen';
+$_['text_extension']         = 'Einstellungen der Erweiterungen';
 $_['text_db']                = 'Datenbank';
 $_['text_version']           = 'PHP-Version';
 $_['text_open_basedir']      = 'open_basedir';
 $_['text_global']            = 'Globale Einträge registrieren';
-$_['text_magic']             = 'Magische Zitate GPC';
+$_['text_magic']             = 'Magic Quotes GPC';
 $_['text_file_upload']       = 'Datei-Uploads';
-$_['text_session']           = 'Automatischer Start der Sitzung';
+$_['text_session']           = 'Session Auto Start';
 $_['text_gd']                = 'GD';
 $_['text_curl']              = 'cURL';
 $_['text_openssl']           = 'OpenSSL';
 $_['text_zlib']              = 'ZLIB';
-$_['text_zip']               = 'REISSVERSCHLUSS';
-$_['text_mbstring']          = 'mb-Zeichenfolge';
+$_['text_zip']               = 'ZIP';
+$_['text_mbstring']          = 'mbstring';
 $_['text_on']                = 'An';
 $_['text_off']               = 'Aus';
 $_['text_file']              = 'Dateien';
@@ -34,17 +34,17 @@ $_['text_missing']           = 'Fehlen';
 
 // Error
 $_['error_version']          = 'Achtung: Sie müssen PHP 8.0 oder höher verwenden, damit OpenCart funktioniert!';
-$_['error_open_basedir']     = 'Warnung: open_basedir muss den Zugriff auf %serlauben';
-$_['error_file_upload']      = 'Achtung: File_Uploads müssen aktiviert werden!';
+$_['error_open_basedir']     = 'Warnung: open_basedir muss den Zugriff auf %s erlauben';
+$_['error_file_upload']      = 'Warnung: file_uploads muß aktiviert sein!';
 $_['error_session']          = 'Warnung: OpenCart funktioniert nicht, wenn session.auto_start aktiviert ist!';
-$_['error_db']               = 'Achtung: Damit OpenCart funktioniert, muss eine Datenbankerweiterung in die php.ini geladen werden!';
+$_['error_db']               = 'Warnung: Damit OpenCart funktioniert, muss eine Datenbankerweiterung in die php.ini geladen werden!';
 $_['error_gd']               = 'Warnung: Die GD-Erweiterung muss geladen werden, damit OpenCart funktioniert!';
-$_['error_curl']             = 'Achtung: Damit OpenCart funktioniert, muss die CURL-Erweiterung geladen werden!';
+$_['error_curl']             = 'Warnung: Damit OpenCart funktioniert, muss die CURL-Erweiterung geladen werden!';
 $_['error_openssl']          = 'Warnung: Damit OpenCart funktioniert, muss die OpenSSL-Erweiterung geladen werden!';
-$_['error_zlib']             = 'Achtung: Damit OpenCart funktioniert, muss die ZLIB-Erweiterung geladen werden!';
-$_['error_zip']              = 'Achtung: Damit OpenCart funktioniert, muss die ZIP-Erweiterung geladen werden!';
+$_['error_zlib']             = 'Warnung: Damit OpenCart funktioniert, muss die ZLIB-Erweiterung geladen werden!';
+$_['error_zip']              = 'Warnung: Damit OpenCart funktioniert, muss die ZIP-Erweiterung geladen werden!';
 $_['error_mbstring']         = 'Warnung: Die mbstring-Erweiterung muss geladen werden, damit OpenCart funktioniert!';
-$_['error_catalog_exist']    = 'Achtung: config.php existiert nicht. Sie müssen config-dist.php in config.php umbenennen!';
-$_['error_catalog_writable'] = 'Achtung: config.php muss beschreibbar sein, damit OpenCart installiert werden kann!';
+$_['error_catalog_exist']    = 'Warnung: Die Datei config.php existiert nicht. Sie müssen config-dist.php in config.php umbenennen!';
+$_['error_catalog_writable'] = 'Warnung: config.php muss beschreibbar sein, damit OpenCart installiert werden kann!';
 $_['error_admin_exist']      = 'Warnung: admin/config.php existiert nicht. Sie müssen admin/config-dist.php in admin/config.php umbenennen!';
 $_['error_admin_writable']   = 'Achtung: admin/config.php muss beschreibbar sein, damit OpenCart installiert werden kann!';

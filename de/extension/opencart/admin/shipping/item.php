c1ebcc093736fd0c788c39e1a36cab9a1ea1464c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:10:44 +0800

    New translations item.php (German)

---
+++ de/extension/opencart/admin/shipping/item.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'Pro Artikel';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Erfolg: Sie haben die Versandkosten pro Artikel geändert!';
+$_['text_edit']        = 'Versand pro Artikel bearbeiten';
+
+// Entry
+$_['entry_cost']       = 'Kosten';
+$_['entry_tax_class']  = 'Steuerklasse';
+$_['entry_geo_zone']   = 'Geozone';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sortierreihenfolge';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, die Versandkosten pro Artikel zu ändern!';

48c84bda48eda9365eeeca1889ea10409271a669
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:07:15 +0800

    New translations failure.php (German)

---
+++ de/catalog/checkout/failure.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title'] = 'Zahlung fehlgeschlagen!';
+
+// Text
+$_['text_basket']   = 'Warenkorb';
+$_['text_checkout'] = 'Kasse';
+$_['text_failure']  = 'Fehlgeschlagene Zahlung';
+$_['text_message']  = '<p>Bei der Zahlungsabwicklung ist ein Problem aufgetreten und die Bestellung wurde nicht abgeschlossen.</p>
+
+<p>Mögliche Gründe sind:</p>
+<ul>
+  <li>Nicht ausreichendes Guthaben</li>
+  <li>Überprüfung fehlgeschlagen</li>
+</ul>
+
+<p>Bitte versuchen Sie, mit einer anderen Zahlungsmethode erneut zu bestellen.</p>
+
+<p>Wenn das Problem weiterhin besteht, <a href="%s">kontaktieren Sie uns bitte</a> und geben Sie die Einzelheiten der Bestellung an, die Sie aufgeben möchten.</p>
+';

63aad6f72184b0ebd5948aad6c734ed3c6ecd091
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 17:08:31 +0800

    New translations subscription_alert.php (German)

---
+++ de/catalog/mail/subscription_alert.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_subject']              = '%s - Bestellung %s - Abonnement gekündigt';
+$_['text_received']             = 'Sie haben ein gekündigtes Abonnement erhalten.';
+$_['text_orders_id']            = 'Bestellnummer:';
+$_['text_subscription_id']      = 'Abonnement-ID';
+$_['text_date_added']           = 'Datum hinzugefügt:';
+$_['text_subscription_status']  = 'Abonnementstatus:';
+$_['text_comment']              = 'Die Kommentare zu Ihrem Abonnement lauten:';
+$_['text_canceled']             = 'Erfolg: Das Abonnementprofil wurde gekündigt!';

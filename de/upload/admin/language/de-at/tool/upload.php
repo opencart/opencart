0b529f58e8ce3341b72feca45669db60b65ae3bb
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-01 16:00:16 +0800

    New translations upload.php (German)

---
+++ de/upload/admin/language/de-at/tool/upload.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']      = 'Uploads';
+
+// Text
+$_['text_success']       = 'Erfolg: Sie haben Uploads geändert!';
+$_['text_list']          = 'Upload-Liste';
+$_['text_filter']        = 'Filter';
+
+// Column
+$_['column_name']        = 'Name hochladen';
+$_['column_code']        = 'Code';
+$_['column_date_added']  = 'Datum hinzugefügt';
+$_['column_action']      = 'Aktion';
+
+// Entry
+$_['entry_name']         = 'Name hochladen';
+$_['entry_filename']     = 'Dateiname';
+$_['entry_date_from']    = 'Stammen aus';
+$_['entry_date_to']      = 'Datum bis';
+
+// Error
+$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, Uploads zu ändern!';
+$_['error_not_found']    = 'Fehler: Datei %s konnte nicht gefunden werden!';
+$_['error_headers_sent'] = 'Fehler: Header bereits versendet!';
+$_['error_upload']       = 'Datei konnte nicht hochgeladen werden!';
+$_['error_filename']     = 'Der Dateiname muss zwischen 3 und 128 Zeichen lang sein!';
+$_['error_file_type']    = 'Ungültiger Dateityp!';

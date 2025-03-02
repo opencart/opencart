11ecae5f6c7390381f517d1f153919dcbe0b2c00
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-03 00:24:59 +0800

    New translations option.php (German)

--- de/admin/catalog/option.php
+++ de/admin/catalog/option.php
@@ -3,22 +3,22 @@
 $_['heading_title']      = 'Optionen';
 
 // Text
-$_['text_success']       = 'Erfolg: Sie haben die Optionen geändert!';
-$_['text_list']          = 'Optionsliste';
-$_['text_add']           = 'Option hinzufügen';
-$_['text_edit']          = 'Bearbeitungsoption';
+$_['text_success']       = 'Erfolgreich: Sie haben die Optionen geändert!';
+$_['text_list']          = 'Optionen Liste';
+$_['text_add']           = 'Neue Option zufügen';
+$_['text_edit']          = 'Option bearbeiten';
 $_['text_choose']        = 'Wählen';
-$_['text_select']        = 'Wählen';
+$_['text_select']        = 'Select';
 $_['text_radio']         = 'Radio';
-$_['text_checkbox']      = 'Kontrollkästchen';
-$_['text_input']         = 'Eingang';
+$_['text_checkbox']      = 'Checkbox';
+$_['text_input']         = 'Eingabe';
 $_['text_text']          = 'Text';
 $_['text_textarea']      = 'Textbereich';
 $_['text_file']          = 'Datei';
 $_['text_date']          = 'Datum';
 $_['text_datetime']      = 'Datum &amp; Uhrzeit';
 $_['text_time']          = 'Zeit';
-$_['text_regex']         = 'Regulärer Ausdruck';
+$_['text_regex']         = 'Regex Ausdruck';
 $_['text_option']        = 'Option';
 $_['text_value']         = 'Optionswerte';
 
@@ -28,10 +28,10 @@ $_['column_sort_order']  = 'Sortierreihenfolge';
 $_['column_action']      = 'Aktionen';
 
 // Entry
-$_['entry_name']         = 'Optionsname';
+$_['entry_name']         = 'Option Name';
 $_['entry_type']         = 'Typ';
 $_['entry_validation']   = 'Validierung';
-$_['entry_option_value'] = 'Optionswertname';
+$_['entry_option_value'] = 'Optionswert Name';
 $_['entry_image']        = 'Bild';
 $_['entry_sort_order']   = 'Sortierreihenfolge';
 
@@ -40,9 +40,9 @@ $_['help_regex']         = 'Verwenden Sie reguläre Ausdrücke. Beispiel: /[^a-z
 
 // Error
 $_['error_warning']      = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']   = 'Achtung: Sie sind nicht berechtigt, Optionen zu ändern!';
+$_['error_permission']   = 'Warnung: Sie sind nicht berechtigt, Optionen zu ändern!';
 $_['error_name']         = 'Der Optionsname muss zwischen 1 und 128 Zeichen lang sein!';
-$_['error_type']         = 'Achtung: Optionswerte erforderlich!';
-$_['error_option_value'] = 'Der Optionswertname muss zwischen 1 und 128 Zeichen lang sein!';
-$_['error_value']        = 'Achtung: Dieser Optionswert kann nicht gelöscht werden, da er aktuell %s Produkten zugewiesen ist!';
-$_['error_product']      = 'Achtung: Diese Option kann nicht gelöscht werden, da sie aktuell %s Produkten zugewiesen ist!';
+$_['error_type']         = 'Warnung: Optionswerte sind erforderlich!';
+$_['error_option_value'] = 'Der Optionswert Name muss zwischen 1 und 128 Zeichen lang sein!';
+$_['error_value']        = 'Warnung: Dieser Optionswert kann nicht gelöscht werden, da er aktuell %s Produkten zugewiesen ist!';
+$_['error_product']      = 'Warnung: Diese Option kann nicht gelöscht werden, da sie aktuell %s Produkten zugewiesen ist!';

e0897e3e74c10e5aeb6805664049ea3a73fc3885
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-03 00:24:55 +0800

    New translations filter.php (German)

--- de/admin/catalog/filter.php
+++ de/admin/catalog/filter.php
@@ -3,24 +3,24 @@
 $_['heading_title']       = 'Filter';
 
 // Text
-$_['text_success']        = 'Erfolg: Sie haben die Filter geändert!';
-$_['text_list']           = 'Liste filtern';
-$_['text_add']            = 'Filter hinzufügen';
+$_['text_success']        = 'Erfolgreich: Sie haben den Filter geändert!';
+$_['text_list']           = 'Filter Liste';
+$_['text_add']            = 'Neuer Filter zufügen';
 $_['text_edit']           = 'Filter bearbeiten';
 
 // Column
-$_['column_name']         = 'Filtername';
-$_['column_filter_group'] = 'Filtergruppe';
+$_['column_name']         = 'Filter Name';
+$_['column_filter_group'] = 'Filter Gruppe';
 $_['column_sort_order']   = 'Sortierreihenfolge';
 $_['column_action']       = 'Aktionen';
 
 // Entry
-$_['entry_name']          = 'Filtername';
-$_['entry_filter_group']  = 'Filtergruppe';
+$_['entry_name']          = 'Filter Name';
+$_['entry_filter_group']  = 'Filter Gruppe';
 $_['entry_sort_order']    = 'Sortierreihenfolge';
 
 // Error
 $_['error_warning']       = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']    = 'Achtung: Sie sind nicht berechtigt, Filter zu ändern!';
-$_['error_name']          = 'Der Filtername muss zwischen 1 und 64 Zeichen lang sein!';
-$_['error_filter_group']  = 'Filtergruppe erforderlich!';
+$_['error_permission']    = 'Warnung: Sie sind nicht berechtigt, Filter zu ändern!';
+$_['error_name']          = 'Der Filter Name muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_filter_group']  = 'Filter Gruppe erforderlich!';

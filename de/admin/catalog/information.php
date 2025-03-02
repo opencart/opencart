edbc80b1032642869c7427bef293792fcffd8db3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-03 00:24:57 +0800

    New translations information.php (German)

--- de/admin/catalog/information.php
+++ de/admin/catalog/information.php
@@ -1,43 +1,43 @@
 <?php
 // Heading
-$_['heading_title']           = 'Information';
+$_['heading_title']           = 'Informationen';
 
 // Text
-$_['text_success']            = 'Erfolg: Sie haben Informationen geändert!';
-$_['text_list']               = 'Informationsliste';
-$_['text_add']                = 'Informationen hinzufügen';
-$_['text_edit']               = 'Informationen bearbeiten';
+$_['text_success']            = 'Erfolgreich: Sie haben Informationen geändert!';
+$_['text_list']               = 'Informationen Liste';
+$_['text_add']                = 'Neue Information zufügen';
+$_['text_edit']               = 'Information bearbeiten';
 $_['text_default']            = 'Standard';
-$_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL weltweit eindeutig ist.';
+$_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL  eindeutig ist.';
 
 // Column
-$_['column_title']            = 'Informationstitel';
+$_['column_title']            = 'Information Titel';
 $_['column_sort_order']       = 'Sortierreihenfolge';
 $_['column_action']           = 'Aktionen';
 
 // Entry
-$_['entry_title']             = 'Informationstitel';
+$_['entry_title']             = 'Information Titel';
 $_['entry_description']       = 'Beschreibung';
 $_['entry_meta_title']        = 'Meta-Tag-Titel';
 $_['entry_meta_keyword']      = 'Meta-Tag-Schlüsselwörter';
 $_['entry_meta_description']  = 'Meta-Tag-Beschreibung';
-$_['entry_store']             = 'Geschäfte';
-$_['entry_keyword']           = 'Stichwort';
+$_['entry_store']             = 'Online-Shop (Store)';
+$_['entry_keyword']           = 'SEO-URL Kennung (Schlüsselwort)';
 $_['entry_status']            = 'Status';
 $_['entry_sort_order']        = 'Sortierreihenfolge';
-$_['entry_layout']            = 'Layoutüberschreibung';
+$_['entry_layout']            = 'Layout überschreiben';
 
 // Error
 $_['error_warning']           = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']        = 'Achtung: Sie sind nicht berechtigt, Informationen zu ändern!';
+$_['error_permission']        = 'Warnung: Sie sind nicht berechtigt, Informationen zu ändern!';
 $_['error_title']             = 'Der Informationstitel muss zwischen 1 und 64 Zeichen lang sein!';
 $_['error_description']       = 'Die Beschreibung muss mehr als 3 Zeichen lang sein!';
 $_['error_meta_title']        = 'Der Meta-Titel muss größer als 1 und kleiner als 255 Zeichen sein!';
 $_['error_keyword']           = 'Die SEO-URL muss zwischen 1 und 64 Zeichen lang sein!';
 $_['error_keyword_exists']    = 'SEO-URL muss eindeutig sein!';
 $_['error_keyword_character'] = 'Das Schlüsselwort darf nur die Zeichen az, 0-9, - und _ enthalten!';
-$_['error_account']           = 'Achtung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell den Shop-Kontobedingungen zugeordnet ist!';
-$_['error_checkout']          = 'Achtung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell als Shop-Checkout-Bedingungen zugewiesen ist!';
-$_['error_affiliate']         = 'Achtung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell den Store-Affiliate-Bedingungen zugeordnet ist!';
-$_['error_return']            = 'Achtung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell den Rückgabebedingungen des Shops zugeordnet ist!';
-$_['error_store']             = 'Achtung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell von %s Shops verwendet wird!';
+$_['error_account']           = 'Warnung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell den Shop-Kontobedingungen zugeordnet ist!';
+$_['error_checkout']          = 'Warnung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell als Shop-Checkout-Bedingungen zugewiesen ist!';
+$_['error_affiliate']         = 'Warnung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell den Store-Affiliate-Bedingungen zugeordnet ist!';
+$_['error_return']            = 'Warnung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell den Rückgabebedingungen des Shops zugeordnet ist!';
+$_['error_store']             = 'Warnung: Diese Informationsseite kann nicht gelöscht werden, da sie aktuell von %s Shops verwendet wird!';

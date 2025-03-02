d593ff422ec9727e543a09898c94dc102104ca0f
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-03 00:28:14 +0800

    New translations sitemap.php (German)

--- de/catalog/information/sitemap.php
+++ de/catalog/information/sitemap.php
@@ -5,7 +5,7 @@ $_['heading_title']    = 'Sitemap';
 // Text
 $_['text_special']     = 'Sonderangebote';
 $_['text_account']     = 'Mein Konto';
-$_['text_edit']        = 'Informationen bearbeiten';
+$_['text_edit']        = 'Information bearbeiten';
 $_['text_password']    = 'Passwort';
 $_['text_address']     = 'Adressbuch';
 $_['text_history']     = 'Bestellverlauf';
@@ -13,5 +13,5 @@ $_['text_download']    = 'Downloads';
 $_['text_cart']        = 'Warenkorb';
 $_['text_checkout']    = 'Kasse';
 $_['text_search']      = 'Suchen';
-$_['text_information'] = 'Information';
+$_['text_information'] = 'Informationen';
 $_['text_contact']     = 'Kontaktieren Sie uns';

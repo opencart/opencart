c17f453d9b619a77127cd620da7929ed1ae418cc
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-02 22:56:37 +0800

    New translations download.php (German)

--- de/admin/catalog/download.php
+++ de/admin/catalog/download.php
@@ -3,39 +3,39 @@
 $_['heading_title']            = 'Downloads';
 
 // Text
-$_['text_success']             = 'Erfolg: Sie haben Downloads geändert!';
-$_['text_list']                = 'Liste herunterladen';
-$_['text_add']                 = 'Download hinzufügen';
-$_['text_edit']                = 'Bearbeiten Herunterladen';
+$_['text_success']             = 'Erfolgreich: Sie haben den Download geändert!';
+$_['text_list']                = 'Download Liste';
+$_['text_add']                 = 'Neuer Download zufügen';
+$_['text_edit']                = 'Download bearbeiten';
 $_['text_upload']              = 'Ihre Datei wurde erfolgreich hochgeladen!';
 $_['text_report']              = 'Berichte';
 
 // Column
-$_['column_name']              = 'Downloadname';
+$_['column_name']              = 'Download Name';
 $_['column_ip']                = 'IP';
-$_['column_account']           = 'Konten';
-$_['column_store']             = 'Speichern';
+$_['column_account']           = 'Kunden-Konten';
+$_['column_store']             = 'Online-Shop (Store)';
 $_['column_country']           = 'Land';
 $_['column_date_added']        = 'Datum hinzugefügt';
 $_['column_action']            = 'Aktionen';
 
 // Entry
-$_['entry_name']               = 'Downloadname';
+$_['entry_name']               = 'Download Name';
 $_['entry_filename']           = 'Dateiname';
 $_['entry_mask']               = 'Maske';
 
 // Help
-$_['help_filename']            = 'Sie können über die Schaltfläche „Hochladen“ hochladen oder per FTP in das Download-Verzeichnis hochladen und die Details unten eingeben.';
+$_['help_filename']            = 'Sie können Dateien über die Schaltfläche „Hochladen“ hochladen oder per FTP in das Download-Verzeichnis hochladen und die Details unten eingeben.';
 $_['help_mask']                = 'Es wird empfohlen, dass Dateiname und Maske unterschiedlich sind, um zu verhindern, dass Benutzer versuchen, eine direkte Verknüpfung mit Ihren Downloads herzustellen.';
 
 // Error
 $_['error_warning']            = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']         = 'Achtung: Sie sind nicht berechtigt, Downloads zu ändern!';
-$_['error_name']               = 'Der Downloadname muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_permission']         = 'Warnung: Sie sind nicht berechtigt, Downloads zu ändern!';
+$_['error_name']               = 'Der Download Name muss zwischen 3 und 64 Zeichen lang sein!';
 $_['error_filename']           = 'Der Dateiname muss zwischen 3 und 128 Zeichen lang sein!';
 $_['error_filename_character'] = 'Der Dateiname darf nur die Zeichen aZ, 0-9, - und _ enthalten!';
 $_['error_directory']          = 'Downloads müssen sich im Speicher-/Download-Verzeichnis befinden!';
-$_['error_exists']             = 'Datei existiert nicht!';
+$_['error_exists']             = 'Die Datei existiert nicht!';
 $_['error_mask']               = 'Die Maske muss zwischen 3 und 128 Zeichen lang sein!';
 $_['error_mask_character']     = 'Die Maske darf nur die Zeichen aZ, 0-9, - und _ enthalten!';
 $_['error_file_type']          = 'Ungültiger Dateityp!';

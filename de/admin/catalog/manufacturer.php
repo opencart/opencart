5a09820757ae3f0bc881d3af5ec998ff761b1d5e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-03 00:24:58 +0800

    New translations manufacturer.php (German)

--- de/admin/catalog/manufacturer.php
+++ de/admin/catalog/manufacturer.php
@@ -3,32 +3,32 @@
 $_['heading_title']           = 'Hersteller';
 
 // Text
-$_['text_success']            = 'Erfolg: Sie haben den Hersteller gewechselt!';
-$_['text_list']               = 'Herstellerliste';
-$_['text_add']                = 'Hersteller hinzufügen';
+$_['text_success']            = 'Erfolgreich: Sie haben Hersteller bearbeitet!';
+$_['text_list']               = 'Hersteller Liste';
+$_['text_add']                = 'Neuer Hersteller zufügen';
 $_['text_edit']               = 'Hersteller bearbeiten';
 $_['text_default']            = 'Standard';
-$_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL weltweit eindeutig ist.';
+$_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL  eindeutig ist.';
 
 // Column
 $_['column_image']            = 'Bild';
-$_['column_name']             = 'Herstellername';
+$_['column_name']             = 'Hersteller Name';
 $_['column_sort_order']       = 'Sortierreihenfolge';
 $_['column_action']           = 'Aktionen';
 
 // Entry
-$_['entry_name']              = 'Herstellername';
-$_['entry_store']             = 'Geschäfte';
-$_['entry_keyword']           = 'Stichwort';
+$_['entry_name']              = 'Hersteller Name';
+$_['entry_store']             = 'Online-Shop (Store)';
+$_['entry_keyword']           = 'SEO-URL Kennung (Schlüsselwort)';
 $_['entry_image']             = 'Bild';
 $_['entry_sort_order']        = 'Sortierreihenfolge';
-$_['entry_layout']            = 'Layoutüberschreibung';
+$_['entry_layout']            = 'Layout überschreiben';
 
 // Error
 $_['error_warning']           = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']        = 'Achtung: Sie sind nicht berechtigt, Hersteller zu ändern!';
-$_['error_name']              = 'Der Herstellername muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_permission']        = 'Warnung: Sie sind nicht berechtigt, Hersteller zu ändern!';
+$_['error_name']              = 'Der Hersteller Name muss zwischen 1 und 64 Zeichen lang sein!';
 $_['error_keyword']           = 'Die SEO-URL muss zwischen 1 und 64 Zeichen lang sein!';
 $_['error_keyword_exists']    = 'SEO-URL muss eindeutig sein!';
 $_['error_keyword_character'] = 'Das Schlüsselwort darf nur die Zeichen az, 0-9, - und _ enthalten!';
-$_['error_product']           = 'Achtung: Dieser Hersteller kann nicht gelöscht werden, da ihm aktuell %s Produkte zugeordnet sind!';
+$_['error_product']           = 'Warnung: Dieser Hersteller kann nicht gelöscht werden, da ihm aktuell %s Produkte zugeordnet sind!';

b4e12bb8a7641a2ae0bbe096d2b171a73f162500
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-02 22:56:37 +0800

    New translations category.php (German)

--- de/admin/catalog/category.php
+++ de/admin/catalog/category.php
@@ -3,34 +3,34 @@
 $_['heading_title']           = 'Kategorien';
 
 // Text
-$_['text_success']            = 'Erfolg: Sie haben Kategorien geändert!';
-$_['text_list']               = 'Kategorienliste';
-$_['text_add']                = 'Kategorie hinzufügen';
+$_['text_success']            = 'Erfolgreich: Sie haben die Kategorie geändert!';
+$_['text_list']               = 'Kategorien Liste';
+$_['text_add']                = 'Neue Kategorie zufügen';
 $_['text_edit']               = 'Kategorie bearbeiten';
 $_['text_filter']             = 'Filter';
 $_['text_default']            = 'Standard';
-$_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL weltweit eindeutig ist.';
+$_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL  eindeutig ist.';
 
 // Column
 $_['column_image']            = 'Bild';
-$_['column_name']             = 'Kategoriename';
+$_['column_name']             = 'Kategorie Name';
 $_['column_sort_order']       = 'Sortierreihenfolge';
 $_['column_action']           = 'Aktionen';
 
 // Entry
-$_['entry_name']              = 'Kategoriename';
+$_['entry_name']              = 'Kategorie Name';
 $_['entry_description']       = 'Beschreibung';
 $_['entry_meta_title']        = 'Meta-Tag-Titel';
 $_['entry_meta_keyword']      = 'Meta-Tag-Schlüsselwörter';
 $_['entry_meta_description']  = 'Meta-Tag-Beschreibung';
-$_['entry_store']             = 'Geschäfte';
-$_['entry_keyword']           = 'Stichwort';
-$_['entry_parent']            = 'Elternteil';
+$_['entry_store']             = 'Online-Shop (Store)';
+$_['entry_keyword']           = 'SEO-URL Kennung (Schlüsselwort)';
+$_['entry_parent']            = 'Übergeordnete Kategorie';
 $_['entry_filter']            = 'Filter';
 $_['entry_image']             = 'Bild';
 $_['entry_sort_order']        = 'Sortierreihenfolge';
 $_['entry_status']            = 'Status';
-$_['entry_layout']            = 'Layoutüberschreibung';
+$_['entry_layout']            = 'Layout überschreiben';
 
 // Help
 $_['help_parent']             = '(Autovervollständigung)';
@@ -39,7 +39,7 @@ $_['help_filter']             = '(Autovervollständigung)';
 // Error
 $_['error_warning']           = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
 $_['error_permission']        = 'Achtung: Sie sind nicht berechtigt, Kategorien zu ändern!';
-$_['error_name']              = 'Der Kategoriename muss zwischen 1 und 255 Zeichen lang sein!';
+$_['error_name']              = 'Der Kategorie Name muss zwischen 1 und 255 Zeichen lang sein!';
 $_['error_meta_title']        = 'Der Meta-Titel muss größer als 1 und kleiner als 255 Zeichen sein!';
 $_['error_parent']            = 'Die von Ihnen gewählte übergeordnete Kategorie ist ein untergeordnetes Element der aktuellen Kategorie.';
 $_['error_keyword']           = 'Die SEO-URL muss zwischen 1 und 64 Zeichen lang sein!';

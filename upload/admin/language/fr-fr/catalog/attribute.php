01c12b2f1875169c1a8e27939214cfae39575344
TheCartpenter <scoutch@gmail.com>
2025-03-02 11:20:00 -0500

    More work on language files

--- upload/admin/language/fr-fr/catalog/attribute.php
+++ upload/admin/language/fr-fr/catalog/attribute.php
@@ -1,25 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Groupes d\'Attributs';
+$_['heading_title']          = 'Groupes d\'Attributs';
 
 // Text
-$_['text_success']      = 'Succès: Vous avez modifié les groupes d\'attributs!';
-$_['text_list']         = 'Liste des Groupes d\'Attributs';
-$_['text_add']          = 'Ajouter un Groupe d\'Attributs';
-$_['text_edit']         = 'Modifier le Groupe d\'Attributs';
+$_['text_success']           = 'Succès: Vous avez modifié les groupes d\'attributs!';
+$_['text_list']              = 'Liste des Groupes d\'Attributs';
+$_['text_add']               = 'Ajouter un Groupe d\'Attributs';
+$_['text_edit']              = 'Modifier le Groupe d\'Attributs';
 
 // Column
-$_['column_name']       = 'Nom du Groupe d\'Attributs';
-$_['column_sort_order'] = 'Ordre de Tri';
-$_['column_action']     = 'Action';
+$_['column_name']            = 'Nom du Groupe d\'Attributs';
+$_['column_attribute_group'] = 'Groupe d\'Attributs';
+$_['column_sort_order']      = 'Ordre de Tri';
+$_['column_action']          = 'Action';
 
 // Entry
-$_['entry_name']        = 'Nom du Groupe d\'Attributs';
-$_['entry_sort_order']  = 'Ordre de Tri';
+$_['entry_name']             = 'Nom du Groupe d\'Attributs';
+$_['entry_attribute_group']  = 'Groupe d\'Attributs';
+$_['entry_sort_order']       = 'Ordre de Tri';
 
 // Error
-$_['error_warning']     = 'Attention: Veuillez vérifier le formulaire attentivement pour les erreurs!';
-$_['error_permission']  = 'Attention: Vous n\'avez pas l\'autorisation de modifier les groupes d\'attributs!';
-$_['error_name']        = 'Le nom du groupe d\'attributs doit contenir entre 1 et 64 caractères!';
-$_['error_attribute']   = 'Attention: Ce groupe d\'attributs ne peut pas être supprimé car il est actuellement assigné à %s attributs!';
-$_['error_product']     = 'Attention: Ce groupe d\'attributs ne peut pas être supprimé car il est actuellement assigné à %s produits!';
+$_['error_warning']          = 'Attention : Veuillez vérifier le formulaire attentivement pour les erreurs!';
+$_['error_permission']       = 'Attention : Vous n\'avez pas l\'autorisation de modifier les groupes d\'attributs!';
+$_['error_attribute_group']  = 'Le nom du groupe de l\'Attribut est requis!';
+$_['error_name']             = 'Le nom du groupe d\'attributs doit contenir entre 1 et 64 caractères!';
+$_['error_product']          = 'Attention : Ce groupe d\'attributs ne peut pas être supprimé car il est actuellement assigné à %s produits!';

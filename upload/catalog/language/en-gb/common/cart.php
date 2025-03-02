7995c80e57b3a5333f74993e7c0b1874eeca9752
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2025-03-01 19:29:29 -0500

    Update cart.php
    Added new language definitions.

--- upload/catalog/language/en-gb/common/cart.php
+++ upload/catalog/language/en-gb/common/cart.php
@@ -11,6 +11,7 @@ $_['text_semi_month']            = 'half-month';
 $_['text_month']                 = 'month';
 $_['text_year']                  = 'year';
 $_['text_no_results']            = 'Your shopping cart is empty!';
+$_['text_model']                 = 'Model';
 $_['text_cart']                  = 'View Cart';
 $_['text_checkout']              = 'Checkout';
 

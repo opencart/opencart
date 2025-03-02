f5fe04bf9ce5bf4a0a57a670a65beded10ea0279
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-02 20:32:52 +0800

    New translations category.php (German)

--- de/admin/catalog/category.php
+++ de/admin/catalog/category.php
@@ -37,7 +37,7 @@ $_['help_parent']             = '(Autovervollständigung)';
 $_['help_filter']             = '(Autovervollständigung)';
 
 // Error
-$_['error_warning']           = 'Achtung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_warning']           = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
 $_['error_permission']        = 'Achtung: Sie sind nicht berechtigt, Kategorien zu ändern!';
 $_['error_name']              = 'Der Kategoriename muss zwischen 1 und 255 Zeichen lang sein!';
 $_['error_meta_title']        = 'Der Meta-Titel muss größer als 1 und kleiner als 255 Zeichen sein!';

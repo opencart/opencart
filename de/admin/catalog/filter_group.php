a166db276485b04acdce07100a934ab919eae7cb
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-03 00:24:56 +0800

    New translations filter_group.php (German)

--- de/admin/catalog/filter_group.php
+++ de/admin/catalog/filter_group.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtergruppen';
+$_['heading_title']     = 'Filter Gruppen';
 
 // Text
-$_['text_success']      = 'Erfolg: Sie haben Filtergruppen geändert!';
-$_['text_list']         = 'Filtergruppenliste';
-$_['text_add']          = 'Filtergruppe hinzufügen';
-$_['text_edit']         = 'Filtergruppe bearbeiten';
+$_['text_success']      = 'Erfolgreich: Sie haben Filter Gruppen geändert!';
+$_['text_list']         = 'Filter Gruppenliste';
+$_['text_add']          = 'Neue Filter Gruppe hinzufügen';
+$_['text_edit']         = 'Filter Gruppe bearbeiten';
 
 // Column
-$_['column_name']       = 'Filtergruppenname';
+$_['column_name']       = 'Filter Gruppenname';
 $_['column_sort_order'] = 'Sortierreihenfolge';
 $_['column_action']     = 'Aktionen';
 
 // Entry
-$_['entry_name']        = 'Filtergruppenname';
+$_['entry_name']        = 'Filter Gruppenname';
 $_['entry_sort_order']  = 'Sortierreihenfolge';
 
 // Error
 $_['error_warning']     = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']  = 'Achtung: Sie haben keine Berechtigung Filtergruppen zu ändern!';
-$_['error_name']        = 'Der Filtergruppenname muss zwischen 1 und 64 Zeichen lang sein!';
-$_['error_filter']      = 'Achtung: Diese Filtergruppe kann nicht gelöscht werden, da ihr aktuell %s Filter zugewiesen sind!';
+$_['error_permission']  = 'Warnung: Sie haben keine Berechtigung Filtergruppen zu ändern!';
+$_['error_name']        = 'Der Filter Gruppenname muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_filter']      = 'Achtung: Diese Filter Gruppe kann nicht gelöscht werden, da ihr aktuell %s Filter zugewiesen sind!';

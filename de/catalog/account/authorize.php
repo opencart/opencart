4957c11153cf5be62b490c5c043b2796569a4850
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-03 00:27:13 +0800

    New translations authorize.php (German)

--- de/catalog/account/authorize.php
+++ de/catalog/account/authorize.php
@@ -4,12 +4,15 @@ $_['heading_title'] = 'Schützen Sie Ihr Konto';
 
 // Text
 $_['text_security'] = 'Sie müssen immer eine Sicherheitsfrage beantworten, wenn Sie sich von einem unbekannten Gerät oder Computer aus anmelden.';
-$_['text_code']     = 'Eine E-Mail mit einem Sicherheitscode wurde an Ihr E-Mail-Konto gesendet. Geben Sie den Code unten ein, um fortzufahren.';
+$_['text_code']     = 'Click the send button to send a security code to your email account. Enter the code below to continue.';
+$_['text_sent']     = 'Erfolg: Eine E-Mail mit einem Sicherheitscode wurde an Ihr E-Mail-Konto gesendet!';
+
 $_['text_locked']   = 'Ihr Konto wurde gesperrt!';
-$_['text_unlock']   = 'Ihr Konto wurde aufgrund zu vieler falscher Versuche, den Sicherheitscode einzugeben, gesperrt. Klicken Sie auf die Schaltfläche „Zurücksetzen“, um einen Link zum Zurücksetzen per E-Mail zu erhalten.';
-$_['text_unlocked'] = 'Erfolg: Ihr Konto wurde entsperrt!';
+$_['text_reset']    = 'Ihr Konto wurde aufgrund zu vieler falscher Versuche, den Sicherheitscode einzugeben, gesperrt. Klicken Sie auf die Schaltfläche „Zurücksetzen“, um einen Link zum Zurücksetzen per E-Mail zu erhalten.';
 $_['text_link']     = 'Erfolg: Eine E-Mail mit einem Link zum Zurücksetzen wurde an Ihre E-Mail-Adresse gesendet!';
-$_['text_resend']   = 'Erfolg: Eine E-Mail mit einem Sicherheitscode wurde an Ihr E-Mail-Konto gesendet!';
+
+$_['text_unlocked'] = 'Your account has been unlocked!';
+$_['text_unlock']   = 'Your account has been unlocked. You will have to re-login and repeat the 2FA process again to continue.';
 
 // Entry
 $_['entry_code']    = 'Sicherheitscode';

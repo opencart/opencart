e89b6150ba2207b5677a73dbc065e6bf330dc381
TheCartpenter <77865573+TheCartpenter@users.noreply.github.com>
2025-03-01 19:42:10 -0500

    Update location.php
    Added new language definitions.

--- upload/admin/language/fr-fr/localisation/location.php
+++ upload/admin/language/fr-fr/localisation/location.php
@@ -24,6 +24,7 @@ $_['entry_open']       = 'Horaires d\'Ouverture';
 $_['entry_comment']    = 'Commentaire';
 
 // Aide
+$_['help_telephone']   = 'Veuillez saisir le numéro de téléphone de l\'emplacement de votre magasin.';
 $_['help_geocode']     = 'Veuillez saisir manuellement le géocode de l\'emplacement de votre magasin.';
 $_['help_open']        = 'Remplissez les horaires d\'ouverture de vos magasins.';
 $_['help_comment']     = 'Ce champ est pour toutes les notes spéciales que vous souhaitez communiquer au client, par exemple: Le magasin n\'accepte pas les chèques.';

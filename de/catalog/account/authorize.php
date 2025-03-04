cccad2503b4cc8d1ccfc8478f38de6239afa4244
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-04 19:55:04 +0800

    New translations authorize.php (German)

--- de/catalog/account/authorize.php
+++ de/catalog/account/authorize.php
@@ -6,13 +6,12 @@ $_['heading_title'] = 'Schützen Sie Ihr Konto';
 $_['text_security'] = 'Sie müssen immer eine Sicherheitsfrage beantworten, wenn Sie sich von einem unbekannten Gerät oder Computer aus anmelden.';
 $_['text_code']     = 'Klicken Sie auf die Schaltfläche „Senden“, um einen Sicherheitscode an Ihr E-Mail-Konto zu senden. Geben Sie den Code unten ein, um fortzufahren.';
 $_['text_sent']     = 'Erfolg: Eine E-Mail mit einem Sicherheitscode wurde an Ihr E-Mail-Konto gesendet!';
-
 $_['text_locked']   = 'Ihr Konto wurde gesperrt!';
 $_['text_reset']    = 'Ihr Konto wurde aufgrund zu vieler falscher Versuche, den Sicherheitscode einzugeben, gesperrt. Klicken Sie auf die Schaltfläche „Zurücksetzen“, um einen Link zum Zurücksetzen per E-Mail zu erhalten.';
 $_['text_link']     = 'Erfolg: Eine E-Mail mit einem Link zum Zurücksetzen wurde an Ihre E-Mail-Adresse gesendet!';
-
 $_['text_unlocked'] = 'Ihr Konto wurde entsperrt!';
 $_['text_unlock']   = 'Ihr Konto wurde entsperrt. Sie müssen sich erneut anmelden und den 2FA-Prozess wiederholen, um fortzufahren.';
+$_['text_failed']   = 'Could not reset your security code!';
 
 // Entry
 $_['entry_code']    = 'Sicherheitscode';
@@ -22,7 +21,4 @@ $_['button_send']   = 'Schicken';
 $_['button_reset']  = 'Zurücksetzen';
 
 // Error
-$_['error_warning'] = 'Sie haben die zulässige Anzahl an Versuchen zur Eingabe des Sicherheitscodes überschritten!';
-$_['error_token']   = 'Sicherheitstoken nicht gesetzt!';
 $_['error_code']    = 'Sicherheitscode stimmt nicht überein!';
-$_['error_reset']   = 'Achtung: Ihr Sicherheitscode konnte nicht zurückgesetzt werden!';

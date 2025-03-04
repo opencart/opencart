e770f3ea25e32fe71bbb65da97c67c1af15ed572
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-05 02:29:12 +0800

    New translations authorize.php (German)

--- de/admin/common/authorize.php
+++ de/admin/common/authorize.php
@@ -4,17 +4,21 @@ $_['heading_title'] = 'Schützen Sie Ihr Konto';
 
 // Text
 $_['text_security'] = 'Sie müssen immer eine Sicherheitsfrage beantworten, wenn Sie sich von einem unbekannten Gerät oder Computer aus anmelden.';
-$_['text_code']     = 'Eine E-Mail mit einem Sicherheitscode wurde an Ihr E-Mail-Konto gesendet. Geben Sie den Code unten ein, um fortzufahren.';
+$_['text_code']     = 'Klicken Sie auf die Schaltfläche „Senden“, um einen Sicherheitscode an Ihr E-Mail-Konto zu senden. Geben Sie den Code unten ein, um fortzufahren.';
+$_['text_sent']     = 'Success: An email has been sent to your email account with a security code!';
 $_['text_locked']   = 'Ihr Konto wurde gesperrt!';
-$_['text_unlock']   = 'Ihr Konto wurde aufgrund zu vieler falscher Versuche, den Sicherheitscode einzugeben, gesperrt. Klicken Sie auf die Schaltfläche „Zurücksetzen“, um einen Link zum Zurücksetzen per E-Mail zu erhalten.';
-$_['text_unlocked'] = 'Erfolg: Ihr Konto wurde entsperrt!';
+$_['text_reset']    = 'Your account has been locked because of to many incorrect attempts at entering the security code. Click the reset button to receive a reset link via your email account.';
 $_['text_link']     = 'Erfolg: Eine E-Mail mit einem Link zum Zurücksetzen wurde an Ihre E-Mail-Adresse gesendet!';
-$_['text_resend']   = 'Erfolg: Eine E-Mail mit einem Sicherheitscode wurde an Ihr E-Mail-Konto gesendet!';
+$_['text_unlocked'] = 'Ihr Konto wurde entsperrt!';
+$_['text_unlock']   = 'Ihr Konto wurde entsperrt. Sie müssen sich erneut anmelden und den 2FA-Prozess wiederholen, um fortzufahren.';
+$_['text_failed']   = 'Could not reset your security code!';
 
 // Entry
 $_['entry_code']    = 'Sicherheitscode';
 
+// Button
+$_['button_send']   = 'Schicken';
+$_['button_reset']  = 'Zurücksetzen';
+
 // Error
-$_['error_warning'] = 'Sie haben die zulässige Anzahl an Versuchen zur Eingabe des Sicherheitscodes überschritten!';
 $_['error_code']    = 'Sicherheitscode stimmt nicht überein!';
-$_['error_reset']   = 'Achtung: Ihr Sicherheitscode konnte nicht zurückgesetzt werden!';

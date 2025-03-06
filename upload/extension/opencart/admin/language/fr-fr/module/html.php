3f77bbd39a7d95e56a6200f231270dc9a6d5ab79
TheCartpenter <scoutch@gmail.com>
2025-03-05 20:48:53 -0500

    Updated fr-fr HTML module

--- upload/extension/opencart/admin/language/fr-fr/module/html.php
+++ upload/extension/opencart/admin/language/fr-fr/module/html.php
@@ -1,11 +1,11 @@
 <?php
 // En-tête
-$_['heading_title']     = 'Composante HTML';
+$_['heading_title']     = 'Éditeur HTML';
 
 // Texte
 $_['text_extension']    = 'Extensions';
-$_['text_success']      = 'Succès: Vous avez modifié le module composante HTML!';
-$_['text_edit']         = 'Modifier le Module Composante HTML';
+$_['text_success']      = 'Succès: Vous avez modifié le module éditeur HTML!';
+$_['text_edit']         = 'Modifier le Module Éditeur HTML';
 
 // Entrée
 $_['entry_name']        = 'Nom du Module';
@@ -14,5 +14,5 @@ $_['entry_description'] = 'Description';
 $_['entry_status']      = 'Statut';
 
 // Erreur
-$_['error_permission']  = 'Attention: Vous n\'avez pas la permission de modifier le module composante HTML!';
+$_['error_permission']  = 'Attention: Vous n\'avez pas la permission de modifier le module Éditeur HTML!';
 $_['error_name']        = 'Le nom du module doit contenir entre 3 à 64 caractères!';

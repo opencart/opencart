06f6ed630e1af5956efd7d85d29b59b821eaad6b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-14 22:18:39 +0800

    New translations identifier.php (German)

---
+++ de/admin/catalog/identifier.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']      = 'Identifiers';
+
+// Text
+$_['text_success']       = 'Success: You have modified Identifiers!';
+$_['text_list']          = 'Identifier List';
+$_['text_add']           = 'Add Identifier';
+$_['text_edit']          = 'Edit Identifier';
+
+// Column
+$_['column_name']        = 'Identifier Name';
+$_['column_description'] = 'Beschreibung';
+$_['column_code']        = 'Code';
+$_['column_action']      = 'Aktionen';
+
+// Entry
+$_['entry_name']         = 'Identifier Name';
+$_['entry_description']  = 'Beschreibung';
+$_['entry_code']         = 'Code';
+$_['entry_status']       = 'Status';
+
+// Help
+$_['help_name']          = 'Example: SKU, UPC, EAN, JAN, ISBN or MPN';
+$_['help_description']   = 'Example: Stock Keeping Unit, Universal Product Code, European Article Number, Japanese Article Number, International Standard Book Number or Manufacturer Part Number';
+$_['help_code']          = 'Example: SKU, UPC, EAN, JAN, ISBN or MPN';
+
+// Error
+$_['error_warning']      = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']   = 'Warning: You do not have permission to modify identifiers!';
+$_['error_name']         = 'Identifier Name must be between 1 and 64 characters!';
+$_['error_description']  = 'Description must be between 1 and 64 characters!';
+$_['error_code']         = 'Identifier Code must be between 3 and 48 characters!';

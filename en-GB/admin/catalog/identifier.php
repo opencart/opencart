ec4fdf21c99dc58056bd6d7d49c2c985d0000154
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-15 00:11:14 +0800

    New translations identifier.php (English, United Kingdom)

--- en-GB/admin/catalog/identifier.php
+++ en-GB/admin/catalog/identifier.php
@@ -1,33 +1,30 @@
 <?php
 // Heading
-$_['heading_title']      = 'Identifiers';
+$_['heading_title']    = 'Identifiers';
 
 // Text
-$_['text_success']       = 'Success: You have modified Identifiers!';
-$_['text_list']          = 'Identifier List';
-$_['text_add']           = 'Add Identifier';
-$_['text_edit']          = 'Edit Identifier';
+$_['text_success']     = 'Success: You have modified Identifiers!';
+$_['text_list']        = 'Identifier List';
+$_['text_add']         = 'Add Identifier';
+$_['text_edit']        = 'Edit Identifier';
 
 // Column
-$_['column_name']        = 'Identifier Name';
-$_['column_description'] = 'Description';
-$_['column_code']        = 'Code';
-$_['column_action']      = 'Action';
+$_['column_name']      = 'Identifier Name';
+$_['column_code']      = 'Code';
+$_['column_action']    = 'Action';
 
 // Entry
-$_['entry_name']         = 'Identifier Name';
-$_['entry_description']  = 'Description';
-$_['entry_code']         = 'Code';
-$_['entry_status']       = 'Status';
+$_['entry_name']       = 'Identifier Name';
+$_['entry_code']       = 'Code';
+$_['entry_status']     = 'Status';
 
 // Help
-$_['help_name']          = 'Example: SKU, UPC, EAN, JAN, ISBN or MPN';
-$_['help_description']   = 'Example: Stock Keeping Unit, Universal Product Code, European Article Number, Japanese Article Number, International Standard Book Number or Manufacturer Part Number';
-$_['help_code']          = 'Example: SKU, UPC, EAN, JAN, ISBN or MPN';
+$_['help_name']        = 'Example: Stock Keeping Unit, Universal Product Code, European Article Number, Japanese Article Number, International Standard Book Number or Manufacturer Part Number';
+$_['help_code']        = 'Example: SKU, UPC, EAN, JAN, ISBN or MPN';
+$_['help_status']      = 'Display product identifier on product info page';
 
 // Error
-$_['error_warning']      = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']   = 'Warning: You do not have permission to modify identifiers!';
-$_['error_name']         = 'Identifier Name must be between 1 and 64 characters!';
-$_['error_description']  = 'Description must be between 1 and 64 characters!';
-$_['error_code']         = 'Identifier Code must be between 3 and 48 characters!';
+$_['error_warning']    = 'Warning: Please check the form carefully for errors!';
+$_['error_permission'] = 'Warning: You do not have permission to modify identifiers!';
+$_['error_name']       = 'Identifier Name must be between 1 and 64 characters!';
+$_['error_code']       = 'Identifier Code must be between 3 and 48 characters!';

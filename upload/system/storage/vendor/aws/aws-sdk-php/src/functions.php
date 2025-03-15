4ca5b78071b0edc2cfd06f126ce0a677ce1a063c
TheCartpenter <scoutch@gmail.com>
2025-03-15 12:19:33 -0400

    Updated aws/aws-sdk-php vendor to v3.342.6

--- upload/system/storage/vendor/aws/aws-sdk-php/src/functions.php
+++ upload/system/storage/vendor/aws/aws-sdk-php/src/functions.php
@@ -1,9 +1,8 @@
 <?php
 namespace Aws;
 
-use GuzzleHttp\Client;
+use GuzzleHttp\Utils;
 use Psr\Http\Message\RequestInterface;
-use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Promise\FulfilledPromise;
 
 //-----------------------------------------------------------------------------
@@ -273,7 +272,7 @@ function describe_type($input)
  */
 function default_http_handler()
 {
-    return new \Aws\Handler\GuzzleV6\GuzzleHandler();
+    return new \Aws\Handler\Guzzle\GuzzleHandler();
 }
 
 /**
@@ -283,7 +282,7 @@ function default_http_handler()
  */
 function default_user_agent()
 {
-    return \GuzzleHttp\default_user_agent();
+    return Utils::defaultUserAgent();
 }
 
 /**

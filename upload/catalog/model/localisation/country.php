bd6417e85e07e78d3728d7c62754085a65ed74ae
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-17 23:25:02 +0800

    Update country.php

--- upload/catalog/model/localisation/country.php
+++ upload/catalog/model/localisation/country.php
@@ -24,7 +24,7 @@ class Country extends \Opencart\System\Engine\Model {
 	 * $country_info = $this->model_localisation_country->getCountry($country_id);
 	 */
 	public function getCountry(int $country_id): array {
-		$query = $this->db->query("SELECT `c`.*, `cd`.`name` FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`c`.`country_id` = `cd`.`country_id`) WHERE `c`.`country_id` = '" . (int)$country_id . "' AND `zd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND `c`.`status` = '1'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`c`.`country_id` = `cd`.`country_id`) WHERE `c`.`country_id` = '" . (int)$country_id . "' AND `cd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND `c`.`status` = '1'");
 
 		return $query->row;
 	}
@@ -43,21 +43,9 @@ class Country extends \Opencart\System\Engine\Model {
 	 * $country_info = $this->model_localisation_country->getCountryByIsoCode2($iso_code_2);
 	 */
 	public function getCountryByIsoCode2(string $iso_code_2): array {
-		$query = $this->db->query("SELECT `c`.*, `cd`.`name` FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`c`.`country_id` = `cd`.`country_id`) WHERE `iso_code_2` = '" . $this->db->escape($iso_code_2) . "' AND `zd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND `c`.`status` = '1'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`c`.`country_id` = `cd`.`country_id`) WHERE `iso_code_2` = '" . $this->db->escape($iso_code_2) . "' AND `cd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND `c`.`status` = '1'");
 
-		$key = md5($sql);
-
-		$country_data = $this->cache->get('country.' . $key);
-
-		if (!$country_data) {
-			$query = $this->db->query($sql);
-
-			$country_data = $query->rows;
-
-			$this->cache->set('country.' . $key, $country_data);
-		}
-
-		return $country_data;
+		return $query->row;
 	}
 
 	/**
@@ -74,21 +62,9 @@ class Country extends \Opencart\System\Engine\Model {
 	 * $country_info = $this->model_localisation_country->getCountryByIsoCode3($iso_code_3);
 	 */
 	public function getCountryByIsoCode3(string $iso_code_3): array {
-		$query = $this->db->query("SELECT `c`.*, `cd`.`name` FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`c`.`country_id` = `cd`.`country_id`) WHERE `iso_code_3` = '" . $this->db->escape($iso_code_3) . "' AND `zd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND `c`.`status` = '1'");
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`c`.`country_id` = `cd`.`country_id`) WHERE `iso_code_3` = '" . $this->db->escape($iso_code_3) . "' AND `cd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND `c`.`status` = '1'");
 
-		$key = md5($sql);
-
-		$country_data = $this->cache->get('country.' . $key);
-
-		if (!$country_data) {
-			$query = $this->db->query($sql);
-
-			$country_data = $query->rows;
-
-			$this->cache->set('country.' . md5($sql), $country_data);
-		}
-
-		return $country_data;
+		return $query->row;
 	}
 
 	/**
@@ -105,7 +81,7 @@ class Country extends \Opencart\System\Engine\Model {
 	 * $countries = $this->model_localisation_country->getCountries();
 	 */
 	public function getCountries(): array {
-		$sql = "SELECT `c`.*, `cd`.`name` FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`c`.`country_id` = `cd`.`country_id`) WHERE `c`.`status` = '1' AND `cd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' ORDER BY `cd`.`name` ASC";
+		$sql = "SELECT * FROM `" . DB_PREFIX . "country` `c` LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`c`.`country_id` = `cd`.`country_id`) WHERE `c`.`status` = '1' AND `cd`.`language_id` = '" . (int)$this->config->get('config_language_id') . "' ORDER BY `cd`.`name` ASC";
 
 		$key = md5($sql);
 

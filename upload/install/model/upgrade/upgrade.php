3557e8f8db580f6c001d425dab3e8f4c83314acf
TheCartpenter <scoutch@gmail.com>
2025-03-21 17:57:09 -0400

    Added load model in upgrade/upgrade PHPDocs

--- upload/install/model/upgrade/upgrade.php
+++ upload/install/model/upgrade/upgrade.php
@@ -10,6 +10,8 @@ namespace Opencart\Install\Model\Upgrade;
  * @example
  *
  * $data = [];
+ * 
+ * $this->load->model('upgrade/upgrade');
  *
  * $install_model = $this->model_upgrade_upgrade($table, $data);
  */

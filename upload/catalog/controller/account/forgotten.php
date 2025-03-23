3d520ed6592b4eee3fd564734664581fb7601943
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-24 02:48:20 +0800

    Update forgotten.php

--- upload/catalog/controller/account/forgotten.php
+++ upload/catalog/controller/account/forgotten.php
@@ -216,7 +216,7 @@ class Forgotten extends \Opencart\System\Engine\Controller {
 
 			$password = html_entity_decode($post_info['password'], ENT_QUOTES, 'UTF-8');
 
-			if (!oc_validate_length($password, $this->config->get('config_password_length'), 40)) {
+			if (!oc_validate_length($password, (int)$this->config->get('config_password_length'), 40)) {
 				$json['error']['password'] = sprintf($this->language->get('error_password_length'), $this->config->get('config_password_length'));
 			}
 

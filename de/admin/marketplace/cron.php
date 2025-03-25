5cff378f4c64c25d10950375b82768eaa5bf70e2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-26 04:43:53 +0800

    New translations cron.php (German)

--- de/admin/marketplace/cron.php
+++ de/admin/marketplace/cron.php
@@ -6,7 +6,7 @@ $_['heading_title']        = 'CRON Jobs';
 $_['text_success']         = 'Erfolg: Sie haben CRON-Jobs geändert!';
 $_['text_instruction']     = 'CRON-Anweisungen';
 $_['text_list']            = 'CRON-Liste';
-$_['text_cron_1']          = 'CRON-Jobs sind geplante Aufgaben, die regelmäßig ausgeführt werden. Um Ihre Server für die Verwendung von Cron-Jobs einzurichten, können Sie die Seite <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">Opencart-Dokumentation</a> lesen.';
+$_['text_cron_1']          = 'CRON Job\'s are scheduled tasks that are run periodically. To set up your servers to use cron job you can read the <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">opencart documentation</a> page.';
 $_['text_cron_2']          = 'Sie müssen Ihre CRON-Aufgabe so einstellen, dass sie stündlich ausgeführt wird.';
 $_['text_info']            = 'CRON-Informationen';
 $_['text_hour']            = 'Stunde';
@@ -22,7 +22,6 @@ $_['column_action']        = 'Aktionen';
 
 // Entry
 $_['entry_cron']           = 'CRON-URL';
-$_['entry_description']    = 'Beschreibung';
 
 // Error
 $_['error_permission']     = 'Achtung: Sie sind nicht berechtigt, CRON-Jobs zu ändern!';

3965fa4390701c0267a65d778aeb4eb0e0e72dc9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-26 06:00:17 +0800

    Update source file ssr.php

--- upload/admin/language/en-gb/marketplace/ssr.php
+++ upload/admin/language/en-gb/marketplace/ssr.php
@@ -1,29 +1,23 @@
 <?php
 // Heading
-$_['heading_title']          = 'Static Site Rendering';
+$_['heading_title']        = 'Static Site Rendering';
 
 // Text
-$_['text_success']           = 'Success: You have modified static site rendering!';
-
-$_['text_progress']          = 'Install Progress';
-$_['text_installed']         = 'Installed Extensions';
-$_['text_info']              = 'Extension Information';
-$_['text_install']           = 'Installing files %s to %s of %s';
-$_['text_vendor']            = 'Refresh vendor files';
-$_['text_xml']               = 'Install OCMOD files';
+$_['text_success']         = 'Success: You have modified static site rendering!';
+$_['text_list']            = 'SSR List';
+$_['text_event']           = 'Static Site Rendering is used to pre render templates and static data. If you have issues you can disable or enable the SSR here.';
+$_['text_progress']        = 'SSR Progress';
+$_['text_task']            = 'SSR Tasks';
+$_['text_info']            = 'SSR Information';
 
 // Column
-$_['column_code']            = 'CRON Code';
-$_['column_name']            = 'Extension Name';
-$_['column_version']         = 'Version';
+$_['column_code']          = 'SSR Code';
+$_['column_sort_order']    = 'Sort Order';
 $_['column_date_modified'] = 'Date Modified';
-$_['column_action']          = 'Action';
+$_['column_action']        = 'Action';
 
 // Entry
-$_['entry_progress']         = 'Progress';
-$_['entry_name']             = 'Extension Name';
-$_['entry_description']      = 'Description';
-$_['entry_code']             = 'Code';
+$_['entry_progress']       = 'Progress';
 
 // Error
-$_['error_permission']       = 'Warning: You do not have permission to modify static site rendering!';
+$_['error_permission']     = 'Warning: You do not have permission to modify static site rendering!';

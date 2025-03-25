b3c1632cafb620414e87a97703fe2deb6be141ed
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-26 04:43:54 +0800

    New translations ssr.php (German)

--- de/admin/marketplace/ssr.php
+++ de/admin/marketplace/ssr.php
@@ -1,29 +1,23 @@
 <?php
 // Heading
-$_['heading_title']          = 'Static Site Rendering';
+$_['heading_title']        = 'Static Site Rendering';
 
 // Text
-$_['text_success']           = 'Success: You have modified static site rendering!';
-
-$_['text_progress']          = 'Installationsfortschritt';
-$_['text_installed']         = 'Installierte Erweiterungen';
-$_['text_info']              = 'Erweiterungsinformationen';
-$_['text_install']           = 'Installieren der Dateien %s bis %s von %s';
-$_['text_vendor']            = 'Lieferantendateien aktualisieren';
-$_['text_xml']               = 'Installieren Sie OCMOD-Dateien';
+$_['text_success']         = 'Success: You have modified static site rendering!';
+$_['text_list']            = 'SSR List';
+$_['text_event']           = 'Static Site Rendering is used to pre render templates and static data. If you have issues you can disable or enable the SSR here.';
+$_['text_progress']        = 'SSR Progress';
+$_['text_task']            = 'SSR Tasks';
+$_['text_info']            = 'SSR Information';
 
 // Column
-$_['column_code']            = 'CRON-Code';
-$_['column_name']            = 'Erweiterungsname';
-$_['column_version']         = 'Version';
+$_['column_code']          = 'SSR Code';
+$_['column_sort_order']    = 'Sortierreihenfolge';
 $_['column_date_modified'] = 'Änderungsdatum';
-$_['column_action']          = 'Aktionen';
+$_['column_action']        = 'Aktionen';
 
 // Entry
-$_['entry_progress']         = 'Fortschritt';
-$_['entry_name']             = 'Erweiterungsname';
-$_['entry_description']      = 'Beschreibung';
-$_['entry_code']             = 'Code';
+$_['entry_progress']       = 'Fortschritt';
 
 // Error
-$_['error_permission']       = 'Warning: You do not have permission to modify static site rendering!';
+$_['error_permission']     = 'Warning: You do not have permission to modify static site rendering!';

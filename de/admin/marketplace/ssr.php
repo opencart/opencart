0b49a95ed837cf320991fdc96a0c08f5ef55abc2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-26 02:19:33 +0800

    New translations ssr.php (German)

---
+++ de/admin/marketplace/ssr.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']          = 'Static Site Rendering';
+
+// Text
+$_['text_success']           = 'Success: You have modified static site rendering!';
+
+$_['text_progress']          = 'Installationsfortschritt';
+$_['text_installed']         = 'Installierte Erweiterungen';
+$_['text_info']              = 'Erweiterungsinformationen';
+$_['text_install']           = 'Installieren der Dateien %s bis %s von %s';
+$_['text_vendor']            = 'Lieferantendateien aktualisieren';
+$_['text_xml']               = 'Installieren Sie OCMOD-Dateien';
+
+// Column
+$_['column_code']            = 'CRON-Code';
+$_['column_name']            = 'Erweiterungsname';
+$_['column_version']         = 'Version';
+$_['column_date_modified'] = 'Änderungsdatum';
+$_['column_action']          = 'Aktionen';
+
+// Entry
+$_['entry_progress']         = 'Fortschritt';
+$_['entry_name']             = 'Erweiterungsname';
+$_['entry_description']      = 'Beschreibung';
+$_['entry_code']             = 'Code';
+
+// Error
+$_['error_permission']       = 'Warning: You do not have permission to modify static site rendering!';

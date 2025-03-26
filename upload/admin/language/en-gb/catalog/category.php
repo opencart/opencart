3e9d29826f421584ad338887b22fc51b7f0033b3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-27 02:42:40 +0800

    Update source file category.php

--- upload/admin/language/en-gb/catalog/category.php
+++ upload/admin/language/en-gb/catalog/category.php
@@ -14,6 +14,7 @@ $_['text_keyword']            = 'Do not use spaces, instead replace spaces with 
 // Column
 $_['column_image']            = 'Image';
 $_['column_name']             = 'Category Name';
+$_['column_status']           = 'Status';
 $_['column_sort_order']       = 'Sort Order';
 $_['column_action']           = 'Action';
 

89523ac9fc9ef1742b5d17aae9edc4367c2a5fd9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-03-28 19:52:03 +0800

    Update source file upgrade.php

--- upload/install/language/en-gb/upgrade/upgrade.php
+++ upload/install/language/en-gb/upgrade/upgrade.php
@@ -14,7 +14,7 @@ $_['text_user']         = 'Goto Admin -> Users -> User Groups and Edit the Top A
 $_['text_setting']      = 'Goto Admin and Edit the main System Settings. Update all fields and click save, even if nothing changed.';
 $_['text_store']        = 'Load the store front & press Ctrl+F5 twice to force the browser to update the css changes.';
 $_['text_success']      = 'Congratulations! You have successfully upgraded your OpenCart installation!';
-$_['text_patch']        = 'Patch %s has been applied (%s of %s)';
+$_['text_patch']        = 'Patch %s of %s has been applied';
 
 // Entry
 $_['entry_admin']       = 'Admin Name';

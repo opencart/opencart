d4e0e3cc70424dfa9ccbb2d484fc623b0931a16e
TheCartpenter <scoutch@gmail.com>
2025-03-28 06:18:17 -0400

    More fr-fr changes

--- upload/admin/language/fr-fr/marketing/marketing.php
+++ upload/admin/language/fr-fr/marketing/marketing.php
@@ -1,12 +1,12 @@
 <?php
 // En-tête
-$_['heading_title']     = 'Suivi Marketing';
+$_['heading_title']     = 'Suivi Mercatique';
 
 // Texte
-$_['text_success']      = 'Succès: Vous avez modifié le suivi marketing!';
-$_['text_list']         = 'Liste de suivi marketing';
-$_['text_add']          = 'Ajouter un suivi marketing';
-$_['text_edit']         = 'Modifier un suivi marketing';
+$_['text_success']      = 'Succès: Vous avez modifié le suivi Mercatique!';
+$_['text_list']         = 'Liste de suivi mercatique';
+$_['text_add']          = 'Ajouter un suivi mercatique';
+$_['text_edit']         = 'Modifier un suivi mercatique';
 $_['text_filter']       = 'Filtrer';
 $_['text_history']      = 'Historique';
 $_['text_history_add']  = 'Ajouter un historique';
@@ -33,11 +33,11 @@ $_['entry_date_from']   = 'Date de début';
 $_['entry_date_to']     = 'Date de fin';
 
 // Aide
-$_['help_code']         = 'Le code de suivi qui sera utilisé pour suivre les campagnes marketing.';
+$_['help_code']         = 'Le code de suivi qui sera utilisé pour suivre les campagnes mercatique.';
 $_['help_example']      = 'Pour que le système puisse suivre les références, vous devez ajouter le code de suivi à la fin de l\'URL menant à votre site.';
 
 // Erreur
-$_['error_permission']  = 'Attention: Vous n\'avez pas la permission de modifier le suivi marketing!';
+$_['error_permission']  = 'Attention: Vous n\'avez pas la permission de modifier le suivi mercatique!';
 $_['error_name']        = 'Le nom de la campagne doit comporter entre 1 et 32 caractères!';
 $_['error_code']        = 'Code de suivi requis!';
 $_['error_exists']      = 'Le code de suivi est déjà utilisé par une autre campagne!';

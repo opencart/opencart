e459719e8dd31d7662ba315561bdcdc2e72b0773
milsaware <95859352+milsaware@users.noreply.github.com>
2025-04-01 11:43:45 +0100

    Update upgrade.php

--- upload/admin/language/fr-fr/tool/upgrade.php
+++ upload/admin/language/fr-fr/tool/upgrade.php
@@ -1,30 +1,71 @@
 <?php
-// En-tête
-$_['heading_title']        = 'Mise à niveau';
+// Heading
+$_['heading_title']                = 'Mise à jour';
 
-// Texte
-$_['text_success']         = 'Succès: Vous utilisez la dernière version %s!';
-$_['text_upgrade']         = 'Vérifier la dernière version';
-$_['text_information']     = 'Informations sur la version';
-$_['text_current_version'] = 'Version actuelle';
-$_['text_latest_version']  = 'Dernière version';
-$_['text_date_added']      = 'Date de sortie';
-$_['text_change']          = 'Journal des modifications';
-$_['text_status']          = 'Statut de mise à niveau';
-$_['text_ready']           = 'Cliquez sur le bouton de mise à niveau pour passer à la dernière version...';
-$_['text_download']        = 'Téléchargement de la dernière version...';
-$_['text_install']         = 'Copie des fichiers...';
-$_['text_patch']           = 'Application des correctifs...';
+// Text
+$_['text_success']                 = 'Succès : Vous utilisez la dernière version %s !';
+$_['text_rollback_yes']            = 'Rétrogradation disponible';
+$_['text_information']             = 'Informations sur la version';
+$_['text_select_version']          = 'Sélectionner la version';
+$_['text_current_version']         = 'Version actuelle';
+$_['text_latest_version']          = 'Dernière version';
+$_['text_latest_released']         = 'Dernière version publiée';
+$_['text_change']                  = 'Journal des modifications';
+$_['text_up_to_date']              = 'Votre version d’OpenCart est à jour';
+$_['text_status']                  = 'Statut de la mise à jour';
+$_['text_ready']                   = 'Cliquez sur le bouton pour mettre à jour vers la dernière version...';
+$_['text_download']                = 'Téléchargement de la dernière version...';
+$_['text_install']                 = 'Dernière version téléchargée ... Copie des fichiers';
+$_['text_patch']                   = 'Application des correctifs...';
+$_['text_loading']                 = 'Chargement...';
+$_['text_rollback_db']             = 'Rétrogradation de la base de données ... Cela peut prendre quelques minutes';
+$_['text_rollback_files']          = 'Rétrogradation des fichiers ... Cela peut prendre quelques minutes';
+$_['text_rollback_unzip']          = 'Décompression de la sauvegarde, cela peut prendre quelques minutes';
+$_['text_clearing_rollback_files'] = 'Suppression des fichiers de rétrogradation';
+$_['text_rollback_complete']       = 'Rétrogradation terminée ! Actualisation de la page...';
+$_['text_backing_up_db']           = 'Sauvegarde de la base de données';
+$_['text_compressing_backup']      = 'Compression de la sauvegarde';
+$_['text_clearing_backup_files']   = 'Suppression des fichiers de sauvegarde';
+$_['text_downloading_update']      = 'Téléchargement de la mise à jour';
+$_['text_clearing_cache']          = 'Effacement du cache';
+$_['text_update_complete']         = 'Mise à jour terminée ! Actualisation de la page...';
 
-// Bouton
-$_['button_upgrade']       = 'Mettre à niveau';
+// Modal
+$_['modal_rollback_title']         = 'Continuer la rétrogradation ?';
+$_['modal_rollback_message']       = 'Appuyez sur OK pour continuer la rétrogradation ou Annuler';
+$_['modal_update_title']           = 'Continuer la mise à jour ?';
+$_['modal_update_message']         = 'Cela va lancer la procédure de mise à jour. Assurez-vous de ne pas actualiser la page pendant la mise à jour, sinon votre système pourrait planter';
+$_['modal_backing_up_files']       = 'Sauvegarde des fichiers';
+$_['modal_rollback_status']        = 'Statut de la rétrogradation';
+$_['modal_rollback_ready']         = 'Démarrage de la rétrogradation ... Ne rafraîchissez pas la page !';
 
-// Erreur
-$_['error_permission']     = 'Attention: Vous n\'avez pas la permission de modifier les mises à niveau!';
-$_['error_connection']     = 'Impossible de se connecter au serveur de mise à niveau!';
-$_['error_version']        = 'La version est inférieure à la version actuelle!';
-$_['error_download']       = 'La mise à niveau n\'a pas pu être téléchargée!';
-$_['error_file']           = 'Le fichier de mise à niveau est introuvable!';
-$_['error_directory']      = 'Impossible de créer le répertoire %s!';
-$_['error_copy']           = 'Impossible de copier le fichier %s vers %s!';
-$_['error_unzip']          = 'Impossible d\'ouvrir le fichier .zip!';
+// Warning
+$_['leave_warning']                = 'Une mise à jour est en cours. Fermer cette page pourrait planter votre système. Êtes-vous sûr de vouloir quitter ?';
+
+// Button
+$_['button_upgrade']               = 'Mettre à jour';
+$_['button_okay']                  = 'OK';
+
+// Error
+$_['error_permission']             = 'Avertissement : Vous n’avez pas l’autorisation de modifier les mises à jour !';
+$_['error_connection']             = 'Impossible de se connecter au serveur de mise à jour !';
+$_['error_version']                = 'La version est inférieure à la version actuelle !';
+$_['error_download']               = 'La mise à jour n’a pas pu être téléchargée !';
+$_['error_file']                   = 'Le fichier de mise à jour est introuvable !';
+$_['error_directory']              = 'Impossible de créer le répertoire %s !';
+$_['error_copy']                   = 'Impossible de copier le fichier %s vers %s !';
+$_['error_unzip']                  = 'Le fichier zip n’a pas pu être ouvert !';
+$_['error_unknown']                = 'Une erreur inconnue est survenue !';
+$_['error_db_backup_fail']         = 'Erreur : La sauvegarde de la base de données a échoué !';
+$_['error_zip_backup_fail']        = 'Erreur : Échec de la création de l’archive zip de sauvegarde !';
+
+// Zip errors
+$_['zip_error_exists']             = 'Le fichier existe déjà !';
+$_['zip_error_incons']             = 'Archive zip incohérente !';
+$_['zip_error_inval']              = 'Argument invalide !';
+$_['zip_error_memory']             = 'Échec de l’allocation de mémoire !';
+$_['zip_error_noent']              = 'Aucun fichier trouvé !';
+$_['zip_error_nozip']              = 'Ce n’est pas une archive zip !';
+$_['zip_error_open']               = 'Impossible d’ouvrir le fichier !';
+$_['zip_error_read']               = 'Erreur de lecture !';
+$_['zip_error_seek']               = 'Erreur de positionnement dans le fichier !';

d3429d51094558bcc3f8b693fafb9c93ecde95f7
milsaware <95859352+milsaware@users.noreply.github.com>
2025-04-01 11:43:15 +0100

    Update upgrade.php

--- upload/admin/language/en-gb/tool/upgrade.php
+++ upload/admin/language/en-gb/tool/upgrade.php
@@ -1,30 +1,73 @@
 <?php
 // Heading
-$_['heading_title']        = 'Upgrade';
+$_['heading_title']                = 'Upgrade';
 
 // Text
-$_['text_success']         = 'Success: You are using the latest version %s!';
-$_['text_upgrade']         = 'Check Latest Version';
-$_['text_information']     = 'Version Information';
-$_['text_current_version'] = 'Current Version';
-$_['text_latest_version']  = 'Latest Version';
-$_['text_date_added']      = 'Release Date';
-$_['text_change']          = 'Change Log';
-$_['text_status']          = 'Upgrade Status';
-$_['text_ready']           = 'Click the upgrade button to upgrade to the latest version...';
-$_['text_download']        = 'Downloading latest...';
-$_['text_install']         = 'Copying files...';
-$_['text_patch']           = 'Applying patches...';
+$_['text_success']                 = 'Success: You are using the latest version %s!';
+$_['text_rollback_yes']            = 'Rollback Available';
+$_['text_information']             = 'Version Information';
+$_['text_select_version']          = 'Select Version';
+$_['text_current_version']         = 'Current Version';
+$_['text_latest_version']          = 'Latest Version';
+$_['text_latest_released']         = 'Latest Released';
+$_['text_change']                  = 'Change Log';
+$_['text_up_to_date']              = 'Your OpenCart version is up to date';
+$_['text_status']                  = 'Upgrade Status';
+$_['text_ready']                   = 'Click the button to upgrade to the latest version...';
+$_['text_download']                = 'Downloading latest...';
+$_['text_install']                 = 'Latest version downloaded ... Copying files';
+$_['text_patch']                   = 'Applying patches...';
+$_['text_loading']                 = 'Loading...';
+$_['text_rollback_db']             = 'Rolling back database ... This may take a few minutes';
+$_['text_rollback_files']          = 'Rolling back files ... This may take a few minutes';
+$_['text_rollback_unzip']          = 'Unzipping backup, this may take a couple of minutes';
+$_['text_clearing_rollback_files'] = 'Clearing rollback files';
+$_['text_rollback_complete']       = 'Rollback complete! Refreshing the page ...';
+$_['text_backing_up_db']           = 'Backing up database';
+$_['text_compressing_backup']      = 'Compressing backup';
+$_['text_clearing_backup_files']   = 'Clearing backup files';
+$_['text_downloading_update']      = 'Downloading update';
+$_['text_clearing_cache']          = 'Clearing cache';
+$_['text_update_complete']         = 'Update complete! Refreshing the page ...';
+
+// Modal
+$_['modal_rollback_title']         = 'Continue with rollback?';
+$_['modal_rollback_message']       = 'Press OK to continue with rollback or Cancel';
+$_['modal_update_title']           = 'Continue with update?';
+$_['modal_update_message']         = 'This will begin the update procedure. Make sure you don’t refresh the page during the update, else your system may crash';
+$_['modal_backing_up_files']       = 'Backing up files';
+$_['modal_rollback_status']        = 'Rollback Status';
+$_['modal_rollback_ready']         = 'Rollback starting ... Don’t refresh the page!';
+
+// Warning
+$_['leave_warning']                = 'An update is in progress. Closing this page could crash your system. Are you sure you want to leave?';
 
 // Button
-$_['button_upgrade']       = 'Upgrade';
+$_['button_upgrade']               = 'Upgrade';
+$_['button_okay']                  = 'OK';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modify upgrades!';
-$_['error_connection']     = 'Could not connect to the upgrade server!';
-$_['error_version']        = 'Version is lower than the current version!';
-$_['error_download']       = 'Upgrade could not be downloaded!';
-$_['error_file']           = 'Upgrade file could not be found!';
-$_['error_directory']      = 'Could not create directory %s!';
-$_['error_copy']           = 'Could not copy file %s to %s!';
-$_['error_unzip']          = 'Zip file could not be opened!';
+$_['error_permission']             = 'Warning: You do not have permission to modify upgrades!';
+$_['error_connection']             = 'Could not connect to the upgrade server!';
+$_['error_version']                = 'Version is lower than the current version!';
+$_['error_download']               = 'Upgrade could not be downloaded!';
+$_['error_file']                   = 'Upgrade file could not be found!';
+$_['error_directory']              = 'Could not create directory %s!';
+$_['error_copy']                   = 'Could not copy file %s to %s!';
+$_['error_unzip']                  = 'Zip file could not be opened!';
+$_['error_unknown']                = 'An unknown error occurred!';
+$_['error_tables_drop']            = 'Error: Tables rollback failed!';
+$_['error_db_rollback']            = 'Error: Database rollback failed!';
+$_['error_db_backup_fail']         = 'Error: Database backup failed!';
+$_['error_zip_backup_fail']        = 'Error: Failed to create backup zip!';
+
+// Zip errors
+$_['zip_error_exists']             = 'File already exists!';
+$_['zip_error_incons']             = 'Zip archive inconsistent!';
+$_['zip_error_inval']              = 'Invalid argument!';
+$_['zip_error_memory']             = 'Memory allocation failure!';
+$_['zip_error_noent']              = 'No such file!';
+$_['zip_error_nozip']              = 'Not a zip archive!';
+$_['zip_error_open']               = 'Can not open file!';
+$_['zip_error_read']               = 'Read error!';
+$_['zip_error_seek']               = 'Seek error!';

1c0714a0a44fd834459764ee8398eed6368ebd87
TheCartpenter <scoutch@gmail.com>
2025-04-03 09:33:13 -0400

    Removed text comment in ssr/article fr-fr

--- upload/admin/language/fr-fr/ssr/article.php
+++ upload/admin/language/fr-fr/ssr/article.php
@@ -1,4 +1,3 @@
 <?php
-// Texte
 $_['text_article']            = 'Interprétation des rendus graphiques %s envers %s de %s articles';
 $_['text_success']            = 'Succès: Vous avez modifié les articles!';

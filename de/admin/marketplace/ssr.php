e20a0ea22370c8c6c8c0ea21b3364f84287e2630
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-05 01:30:34 +0800

    New translations ssr.php (German)

--- de/admin/marketplace/ssr.php
+++ de/admin/marketplace/ssr.php
@@ -6,18 +6,13 @@ $_['heading_title']        = 'Static Site Rendering';
 $_['text_success']         = 'Success: You have modified static site rendering!';
 $_['text_list']            = 'SSR List';
 $_['text_event']           = 'Static Site Rendering is used to pre render templates and static data. If you have issues you can disable or enable the SSR here.';
-$_['text_progress']        = 'SSR Progress';
-$_['text_task']            = 'SSR Tasks';
-$_['text_info']            = 'SSR Information';
 
 // Column
 $_['column_code']          = 'SSR Code';
+$_['column_progress']      = 'Fortschritt';
 $_['column_sort_order']    = 'Sortierreihenfolge';
 $_['column_date_modified'] = 'Änderungsdatum';
 $_['column_action']        = 'Aktionen';
-
-// Entry
-$_['entry_progress']       = 'Fortschritt';
 
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify static site rendering!';

393ab9c26b27390b4b1e5339bde340321ba1d0ee
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-05 01:30:33 +0800

    New translations installer.php (English, United Kingdom)

--- en-GB/admin/marketplace/installer.php
+++ en-GB/admin/marketplace/installer.php
@@ -45,5 +45,3 @@ $_['error_directory_exists'] = 'Path %s already exists!';
 $_['error_unzip']            = 'Zip file could not be opened!';
 $_['error_upload']           = 'File could not be uploaded!';
 $_['error_unknown']          = 'An unknown error occurred!';
-
-

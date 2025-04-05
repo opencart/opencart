228433b7031af970e100cc7017306ee845c8eec3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-06 05:45:37 +0800

    Update source file country.php

--- upload/admin/language/en-gb/ssr/country.php
+++ upload/admin/language/en-gb/ssr/country.php
@@ -1,4 +1,10 @@
 <?php
-$_['text_country'] = 'You have rendered %s to %s of %s countries';
-$_['text_country'] = 'You have rendered %s to %s of %s countries';
-$_['text_success'] = 'Success: You have modified countries!';
+// Text
+$_['text_list']        = 'You have rendered the country list';
+$_['text_next']        = 'You have rendered %s to %s of %s countries';
+$_['text_success']     = 'Success: You have modified countries!';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify countries!';
+$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']       = 'Warning: File %s could not be written!';

9dcd5bbdacbb39f0f31f3727df3d33d8d35b0b04
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-06 05:46:19 +0800

    New translations article.php (German)

--- de/admin/ssr/article.php
+++ de/admin/ssr/article.php
@@ -1,3 +1,4 @@
 <?php
 $_['text_article'] = 'You have rendered %s to %s of %s articles';
+$_['text_next']    = 'You have rendered %s to %s of %s countries';
 $_['text_success'] = 'Erfolg: Sie haben Artikel geändert!';

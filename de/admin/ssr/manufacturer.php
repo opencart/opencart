3199b5c804f494804cb77046287dfff4be193e04
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-06 05:46:28 +0800

    New translations manufacturer.php (German)

---
+++ de/admin/ssr/manufacturer.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_list']        = 'You have rendered the manufacturer list';
+$_['text_next']        = 'You have rendered %s to %s of %s manufacturers';
+$_['text_success']     = 'Erfolgreich: Sie haben Hersteller bearbeitet!';
+
+// Error
+$_['error_permission'] = 'Warnung: Sie sind nicht berechtigt, Hersteller zu ändern!';
+$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']       = 'Warning: File %s could not be written!';

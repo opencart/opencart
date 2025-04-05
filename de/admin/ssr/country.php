871906639d27af0983b2a3afc0a5b365fdd6288b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-06 05:46:22 +0800

    New translations country.php (German)

--- de/admin/ssr/country.php
+++ de/admin/ssr/country.php
@@ -1,4 +1,10 @@
 <?php
-$_['text_country'] = 'You have rendered %s to %s of %s countries';
-$_['text_country'] = 'You have rendered %s to %s of %s countries';
-$_['text_success'] = 'Erfolg: Sie haben Länder geändert!';
+// Text
+$_['text_list']        = 'You have rendered the country list';
+$_['text_next']        = 'You have rendered %s to %s of %s countries';
+$_['text_success']     = 'Erfolg: Sie haben Länder geändert!';
+
+// Error
+$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Länder zu ändern!';
+$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']       = 'Warning: File %s could not be written!';

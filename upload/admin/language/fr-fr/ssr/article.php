44e375a3b89ade0be189a62f1c0cd0651605db8a
TheCartpenter <scoutch@gmail.com>
2025-04-06 12:32:08 -0400

    Trimmed ssr/article fr-fr

--- upload/admin/language/fr-fr/ssr/article.php
+++ upload/admin/language/fr-fr/ssr/article.php
@@ -1,4 +1,4 @@
 <?php
-$_['text_article']            = 'Interprétation des rendus graphiques %s envers %s de %s articles';
-$_['text_next']               = 'Vous avez rendu %s à %s de %s pays';
-$_['text_success']            = 'Succès: Vous avez modifié les articles!';
+$_['text_article'] = 'Interprétation des rendus graphiques %s envers %s de %s articles';
+$_['text_next']    = 'Vous avez rendu %s à %s de %s pays';
+$_['text_success'] = 'Succès: Vous avez modifié les articles!';

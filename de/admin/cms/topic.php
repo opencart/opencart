b25b339320e0a389419b1881c0d3cddce7759a30
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-06 20:36:17 +0800

    New translations topic.php (German)

--- de/admin/cms/topic.php
+++ de/admin/cms/topic.php
@@ -8,6 +8,8 @@ $_['text_list']               = 'Themenliste';
 $_['text_add']                = 'Thema hinzufügen';
 $_['text_edit']               = 'Thema bearbeiten';
 $_['text_default']            = 'Standard';
+$_['text_general']            = 'Allgemein';
+$_['text_meta']               = 'Meta-Tags';
 $_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL  eindeutig ist.';
 
 // Column

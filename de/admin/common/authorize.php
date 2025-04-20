71dc91ee9a09898919328b452f759a8a53669cd9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-20 18:32:54 +0800

    New translations authorize.php (German)

--- de/admin/common/authorize.php
+++ de/admin/common/authorize.php
@@ -5,6 +5,7 @@ $_['heading_title'] = 'Schützen Sie Ihr Konto';
 // Text
 $_['text_security'] = 'Sie müssen immer eine Sicherheitsfrage beantworten, wenn Sie sich von einem unbekannten Gerät oder Computer aus anmelden.';
 $_['text_code']     = 'Klicken Sie auf die Schaltfläche „Senden“, um einen Sicherheitscode an Ihr E-Mail-Konto zu senden. Geben Sie den Code unten ein, um fortzufahren.';
+$_['text_reset']    = 'Ihr Konto wurde aufgrund von zu vielen falschen Eingabeversuchen des Sicherheitscodes gesperrt. Klicken Sie auf den Reset-Button, um einen Link zum Zurücksetzen per E-Mail zu erhalten. ';
 $_['text_sent']     = 'Erfolg: Eine E-Mail mit einem Sicherheitscode wurde an Ihr E-Mail-Konto gesendet!';
 $_['text_locked']   = 'Ihr Konto wurde gesperrt!';
 $_['text_reset']    = 'Ihr Konto wurde aufgrund von zu vielen falschen Eingabeversuchen des Sicherheitscodes gesperrt. Klicken Sie auf den Reset-Button, um einen Link zum Zurücksetzen per E-Mail zu erhalten. ';
@@ -16,7 +17,7 @@ $_['text_failed']   = 'Could not reset your security code!';
 // Entry
 $_['entry_code']    = 'Sicherheitscode';
 
-// Button
+// Buttons
 $_['button_send']   = 'Schicken';
 $_['button_reset']  = 'Zurücksetzen';
 

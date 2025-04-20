4b6ddc330dc67f52bb39b2ef45e9f6495969679d
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-20 18:32:55 +0800

    New translations extension.php (German)

--- de/admin/marketplace/extension.php
+++ de/admin/marketplace/extension.php
@@ -5,5 +5,6 @@ $_['heading_title'] = 'Erweiterungen';
 // Text
 $_['text_success']  = 'Erfolg: Sie haben Erweiterungen geändert!';
 $_['text_list']     = 'Erweiterungsliste';
+$_['text_install']  = 'Installieren';
 $_['text_type']     = 'Wählen Sie den Erweiterungstyp';
 $_['text_filter']   = 'Filter';

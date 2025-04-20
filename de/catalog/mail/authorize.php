b1e4a999390fb706e8536e43af1c735e841a2d8c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-20 18:32:58 +0800

    New translations authorize.php (German)

--- de/catalog/mail/authorize.php
+++ de/catalog/mail/authorize.php
@@ -2,5 +2,6 @@
 // Text
 $_['text_subject'] = 'Sicherheit';
 $_['text_code']    = 'Sie müssen den Sicherheitscode bei der Administrator-Sicherheitsüberprüfung eingeben.';
+$_['text_reset']   = 'Ihr Konto wurde aufgrund von zu vielen falschen Eingabeversuchen des Sicherheitscodes gesperrt. Klicken Sie auf den Reset-Button, um einen Link zum Zurücksetzen per E-Mail zu erhalten. ';
 $_['text_ip']      = 'IP-Adresse:';
 $_['text_regards'] = 'Beste grüße';

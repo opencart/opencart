a8d4ed4b94cd5a00b83c4a21ede20b6e7f0284f3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-04-27 22:37:09 +0800

    Update product.php

--- upload/admin/controller/catalog/product.php
+++ upload/admin/controller/catalog/product.php
@@ -1227,7 +1227,7 @@ class Product extends \Opencart\System\Engine\Controller {
 		$this->load->model('catalog/identifier');
 
 		foreach ($post_info['product_code'] as $key => $product_code) {
-			$identifier_info = $this->model_catalog_identifier->getIdentifierByCode($product_code['code']);
+			$identifier_info = $this->model_catalog_identifier->getIdentifier($product_code['identifier_id']);
 
 			if ($identifier_info && $identifier_info['validation'] && !oc_validate_regex($product_code['value'], $identifier_info['validation'])) {
 				$json['error']['code_' . $key] = sprintf($this->language->get('error_regex'), $product_code['code']);

100a8c70954bc2909b1f0242f71db4ef6c69fe7e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-05-19 22:27:47 +0800

    Update source file subscription_plan.php

--- upload/admin/language/en-gb/catalog/subscription_plan.php
+++ upload/admin/language/en-gb/catalog/subscription_plan.php
@@ -30,6 +30,7 @@ $_['entry_sort_order']       = 'Sort Order';
 // Column
 $_['column_name']            = 'Subscription Plan Name';
 $_['column_sort_order']      = 'Sort Order';
+$_['column_status']          = 'Status';
 $_['column_action']          = 'Action';
 
 // Help

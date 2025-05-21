e8c8406f38dd710af317489fd14ab32df2b4e2d8
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-05-21 22:55:07 +0800

    New translations identifier.php (English, United Kingdom)

---
+++ en-GB/admin/localisation/identifier.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']    = 'Identifiers';
+
+// Text
+$_['text_success']     = 'Success: You have modified Identifiers!';
+$_['text_list']        = 'Identifier List';
+$_['text_add']         = 'Add Identifier';
+$_['text_edit']        = 'Edit Identifier';
+$_['text_regex']       = 'Regex';
+
+// Column
+$_['column_name']      = 'Identifier Name';
+$_['column_code']      = 'Code';
+$_['column_action']    = 'Action';
+
+// Entry
+$_['entry_name']       = 'Identifier Name';
+$_['entry_code']       = 'Code';
+$_['entry_validation'] = 'Validation';
+$_['entry_status']     = 'Status';
+
+// Help
+$_['help_name']        = 'Example: Stock Keeping Unit, Universal Product Code, European Article Number, Japanese Article Number, International Standard Book Number or Manufacturer Part Number';
+$_['help_code']        = 'Example: SKU, UPC, EAN, JAN, ISBN or MPN';
+$_['help_regex']       = 'Use regex. E.g: /[^a-zA-Z0-9_-]/';
+$_['help_status']      = 'Display product identifier on product info page';
+
+// Error
+$_['error_warning']    = 'Warning: Please check the form carefully for errors!';
+$_['error_permission'] = 'Warning: You do not have permission to modify identifiers!';
+$_['error_name']       = 'Identifier Name must be between 1 and 64 characters!';
+$_['error_exists']     = 'Identifier does not exist!';
+$_['error_code']       = 'Identifier Code must be between 3 and 48 characters!';

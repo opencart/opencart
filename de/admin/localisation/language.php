1199cdf8d9b206cdd51e6f57f1b7fba4eaf2ab19
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-05-26 19:06:03 +0800

    New translations language.php (German)

--- de/admin/localisation/language.php
+++ de/admin/localisation/language.php
@@ -1,39 +1,39 @@
 <?php
 // Heading
-$_['heading_title']     = 'Sprachen';
+$_['heading_title']           = 'Sprachen';
 
 // Text
-$_['text_success']      = 'Erfolg: Sie haben Sprachen geändert!';
-$_['text_list']         = 'Sprachenliste';
-$_['text_add']          = 'Sprache hinzufügen';
-$_['text_edit']         = 'Sprache bearbeiten';
+$_['text_success']            = 'Erfolg: Sie haben Sprachen geändert!';
+$_['text_list']               = 'Sprachenliste';
+$_['text_add']                = 'Sprache hinzufügen';
+$_['text_edit']               = 'Sprache bearbeiten';
 
 // Column
-$_['column_name']       = 'Sprachenname';
-$_['column_code']       = 'Code';
-$_['column_sort_order'] = 'Sortierreihenfolge';
-$_['column_action']     = 'Aktionen';
+$_['column_name']             = 'Sprachenname';
+$_['column_code']             = 'Code';
+$_['column_sort_order']       = 'Sortierreihenfolge';
+$_['column_action']           = 'Aktionen';
 
 // Entry
-$_['entry_name']        = 'Sprachenname';
-$_['entry_code']        = 'Code';
-$_['entry_extension']   = 'Verlängerung';
-$_['entry_locale']      = 'Gebietsschema';
-$_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sortierreihenfolge';
+$_['entry_name']              = 'Sprachenname';
+$_['entry_code']              = 'Code';
+$_['entry_extension']         = 'Verlängerung';
+$_['entry_locale']            = 'Gebietsschema';
+$_['entry_status']            = 'Status';
+$_['entry_sort_order']        = 'Sortierreihenfolge';
 
 // Help
-$_['help_locale']       = 'Beispiel: en_US.UTF-8,en_US,en-gb,en_gb,english';
-$_['help_status']       = 'Im Sprachen-Dropdown-Menü ausblenden/anzeigen';
+$_['help_locale']             = 'USed for detecting brower language. Example: en_US.UTF-8,en_US,en-gb,en_gb,english';
+$_['help_status']             = 'Im Sprachen-Dropdown-Menü ausblenden/anzeigen';
 
 // Error
-$_['error_permission']  = 'Achtung: Sie sind nicht berechtigt, Sprachen zu ändern!';
-$_['error_exists']      = 'Achtung: Sie haben diese Sprache bereits hinzugefügt!';
-$_['error_name']        = 'Der Sprachenname muss zwischen 1 und 32 Zeichen lang sein!';
-$_['error_code']        = 'Der Sprachcode muss zwischen 2 und 5 Zeichen lang sein!';
-$_['error_file']        = 'Datei konnte nicht gefunden werden!';
-$_['error_locale']      = 'Gebietsschema erforderlich!';
-$_['error_default']     = 'Achtung: Diese Sprache kann nicht gelöscht werden, da sie aktuell als Standard-Shopsprache zugewiesen ist!';
-$_['error_admin']       = 'Achtung: Diese Sprache kann nicht gelöscht werden, da sie aktuell als Administrationssprache zugewiesen ist!';
-$_['error_store']       = 'Achtung: Diese Sprache kann nicht gelöscht werden, da sie aktuell %s Stores zugewiesen ist!';
-$_['error_order']       = 'Achtung: Diese Sprache kann nicht gelöscht werden, da sie aktuell %s Bestellungen zugeordnet ist!';
+$_['error_permission']        = 'Achtung: Sie sind nicht berechtigt, Sprachen zu ändern!';
+$_['error_exists']            = 'Achtung: Sie haben diese Sprache bereits hinzugefügt!';
+$_['error_name']              = 'Der Sprachenname muss zwischen 1 und 32 Zeichen lang sein!';
+$_['error_code']              = 'Der Sprachcode muss zwischen 2 und 5 Zeichen lang sein!';
+$_['error_file']              = 'Datei konnte nicht gefunden werden!';
+$_['error_locale']            = 'Gebietsschema erforderlich!';
+$_['error_default']           = 'Achtung: Diese Sprache kann nicht gelöscht werden, da sie aktuell als Standard-Shopsprache zugewiesen ist!';
+$_['error_admin']             = 'Achtung: Diese Sprache kann nicht gelöscht werden, da sie aktuell als Administrationssprache zugewiesen ist!';
+$_['error_store']             = 'Achtung: Diese Sprache kann nicht gelöscht werden, da sie aktuell %s Stores zugewiesen ist!';
+$_['error_order']             = 'Achtung: Diese Sprache kann nicht gelöscht werden, da sie aktuell %s Bestellungen zugeordnet ist!';

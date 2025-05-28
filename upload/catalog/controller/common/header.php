168036aaae8114d3dc927da21e286f268ae8ce5b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-05-28 20:30:53 +0800

    alerted the system so all sccs are generated as one file.

--- upload/catalog/controller/common/header.php
+++ upload/catalog/controller/common/header.php
@@ -39,7 +39,6 @@ class Header extends \Opencart\System\Engine\Controller {
 		$data['keywords'] = $this->document->getKeywords();
 
 		// Hard coding css, so they can be replaced via the event's system.
-		$data['bootstrap'] = 'catalog/view/stylesheet/bootstrap.css';
 		$data['icons'] = 'catalog/view/stylesheet/fonts/fontawesome/css/all.min.css';
 		$data['stylesheet'] = 'catalog/view/stylesheet/stylesheet.css';
 

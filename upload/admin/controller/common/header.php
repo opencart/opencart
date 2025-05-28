303c8ce9bda39a7e1fa4995585e763f430e59a8d
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-05-28 20:47:51 +0800

    more scss changes

--- upload/admin/controller/common/header.php
+++ upload/admin/controller/common/header.php
@@ -23,7 +23,6 @@ class Header extends \Opencart\System\Engine\Controller {
 		$data['keywords'] = $this->document->getKeywords();
 
 		// Hard coding css so they can be replaced via the event's system.
-		$data['icons'] = 'view/stylesheet/fonts/fontawesome/css/all.min.css';
 		$data['stylesheet'] = 'view/stylesheet/stylesheet.css';
 
 		// Hard coding scripts so they can be replaced via the event's system.

26ee73b71876374456fb588c0bd1cb31205dd7dc
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-06-13 01:34:40 +0800

    New translations menu.php (German)

--- de/admin/tool/menu.php
+++ de/admin/tool/menu.php
@@ -1,27 +1,32 @@
 <?php
 // Heading
-$_['heading_title']    = 'Admin Menu';
+$_['heading_title']     = 'Admin Menu';
 
 // Text
-$_['text_success']     = 'Success: You have modified menu!';
-$_['text_list']        = 'Menu List';
-$_['text_add']         = 'Add Menu';
-$_['text_edit']        = 'Edit Menu';
+$_['text_success']      = 'Success: You have modified menu!';
+$_['text_list']         = 'Menu List';
+$_['text_add']          = 'Add Menu';
+$_['text_edit']         = 'Edit Menu';
+$_['text_link']         = 'Link';
+$_['text_dropdown']     = 'Dropdown';
 
 // Column
-$_['column_name']      = 'Item Name';
-$_['column_code']      = 'Code';
-$_['column_route']     = 'Route';
-$_['column_action']    = 'Aktionen';
+$_['column_name']       = 'Item Name';
+$_['column_code']       = 'Code';
+$_['column_route']      = 'Route';
+$_['column_sort_order'] = 'Sortierreihenfolge';
+$_['column_action']     = 'Aktionen';
 
 // Entry
-$_['entry_name']       = 'Menu Name';
-$_['entry_code']       = 'Code';
-$_['entry_route']      = 'Route';
-$_['entry_path']       = 'Weg';
+$_['entry_name']        = 'Menu Name';
+$_['entry_code']        = 'Code';
+$_['entry_type']        = 'Typ';
+$_['entry_route']       = 'Route';
+$_['entry_path']        = 'Weg';
+$_['entry_sort_order']  = 'Sortierreihenfolge';
 
 // Error
-$_['error_warning']    = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission'] = 'Warning: You do not have permission to modify menus!';
-$_['error_name']       = 'Menu Name must be between 1 and 64 characters!';
-$_['error_code']       = 'Code must be between 1 and 64 characters!';
+$_['error_warning']     = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']  = 'Warning: You do not have permission to modify menus!';
+$_['error_name']        = 'Menu Name must be between 1 and 64 characters!';
+$_['error_code']        = 'Code must be between 1 and 64 characters!' ;

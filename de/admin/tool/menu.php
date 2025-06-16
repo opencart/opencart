b4d268ae8de9a069a14a7396da6fe1adcb562f93
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-06-17 05:14:05 +0800

    New translations menu.php (German)

--- de/admin/tool/menu.php
+++ de/admin/tool/menu.php
@@ -9,24 +9,37 @@ $_['text_add']          = 'Add Menu';
 $_['text_edit']         = 'Edit Menu';
 $_['text_link']         = 'Link';
 $_['text_dropdown']     = 'Dropdown';
+$_['text_catalog']      = 'Katalog';
+$_['text_cms']          = 'CMS';
+$_['text_extension']    = 'Erweiterungen';
+$_['text_design']       = 'Design';
+$_['text_sale']         = 'Verkäufe';
+$_['text_customer']     = 'Kunden';
+$_['text_marketing']    = 'Marketing';
+$_['text_system']       = 'System';
+$_['text_report']       = 'Berichte';
 
 // Column
 $_['column_name']       = 'Item Name';
 $_['column_code']       = 'Code';
-$_['column_route']      = 'Route';
+$_['column_type']       = 'Typ';
 $_['column_sort_order'] = 'Sortierreihenfolge';
 $_['column_action']     = 'Aktionen';
 
 // Entry
 $_['entry_name']        = 'Menu Name';
 $_['entry_code']        = 'Code';
-$_['entry_type']        = 'Typ';
 $_['entry_route']       = 'Route';
-$_['entry_path']        = 'Weg';
+$_['entry_path']        = 'Übergeordnete Kategorie';
 $_['entry_sort_order']  = 'Sortierreihenfolge';
+
+// Button
+$_['button_link']       = 'Link hinzufügen';
+$_['button_dropdown']   = 'Add Dropdown';
 
 // Error
 $_['error_warning']     = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
 $_['error_permission']  = 'Warning: You do not have permission to modify menus!';
 $_['error_name']        = 'Menu Name must be between 1 and 64 characters!';
-$_['error_code']        = 'Code must be between 1 and 64 characters!' ;
+$_['error_code']        = 'Code must be between 1 and 64 characters!';
+$_['error_route']       = 'Route required!';

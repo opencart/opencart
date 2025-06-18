d2a476853eef7af6fc0006a3f292650415aa0349
阿維 <gpx.jordan@gmail.com>
2025-06-18 19:22:34 +0800

    Update manufacturer.php
    This is the invalid string.

--- upload/admin/language/en-gb/catalog/manufacturer.php
+++ upload/admin/language/en-gb/catalog/manufacturer.php
@@ -7,7 +7,6 @@ $_['text_success']      = 'Success: You have modified manufacturers!';
 $_['text_list']         = 'Manufacturer List';
 $_['text_add']          = 'Add Manufacturer';
 $_['text_edit']         = 'Edit Manufacturer';
-$_['text_default']      = 'Default';
 $_['text_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Column

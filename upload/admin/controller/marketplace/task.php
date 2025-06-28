6ea3b6b38b3efff1493bf56c2bc9667e8c0b6ed7
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-06-29 02:05:02 +0800

    adde tasks

---
+++ upload/admin/controller/marketplace/task.php
@@ -0,0 +1,262 @@
+<?php
+namespace Opencart\Admin\Controller\Marketplace;
+/**
+ * Class Task
+ *
+ * @package Opencart\Admin\Controller\Marketplace
+ */
+class Task extends \Opencart\System\Engine\Controller {
+	/**
+	 * Index
+	 *
+	 * @return void
+	 */
+	public function index(): void {
+		$this->load->language('marketplace/task');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$url = '';
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+
+		$data['breadcrumbs'] = [];
+
+		$data['breadcrumbs'][] = [
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'])
+		];
+
+		$data['breadcrumbs'][] = [
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('marketplace/task', 'user_token=' . $this->session->data['user_token'] . $url)
+		];
+
+		$data['delete'] = $this->url->link('marketplace/task.delete', 'user_token=' . $this->session->data['user_token']);
+
+		// Example task URL
+		$data['task'] = HTTP_CATALOG . 'index.php?route=task/task';
+
+		$data['list'] = $this->getList();
+
+		$data['user_token'] = $this->session->data['user_token'];
+
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('marketplace/task', $data));
+	}
+
+	/**
+	 * List
+	 *
+	 * @return void
+	 */
+	public function list(): void {
+		$this->load->language('marketplace/task');
+
+		$this->response->setOutput($this->getList());
+	}
+
+	/**
+	 * Get List
+	 *
+	 * @return string
+	 */
+	public function getList(): string {
+		if (isset($this->request->get['page'])) {
+			$page = (int)$this->request->get['page'];
+		} else {
+			$page = 1;
+		}
+
+		$url = '';
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+
+		$data['action'] = $this->url->link('marketplace/task.list', 'user_token=' . $this->session->data['user_token'] . $url);
+
+		// Tasks
+		$data['tasks'] = [];
+
+		$filter_data = [
+			'start' => ($page - 1) * $this->config->get('config_pagination_admin'),
+			'limit' => $this->config->get('config_pagination_admin')
+		];
+
+		$this->load->model('setting/task');
+
+		$results = $this->model_setting_task->getTasks($filter_data);
+
+		foreach ($results as $result) {
+			$data['tasks'][] = ['date_added' => date($this->language->get('datetime_format'), strtotime($result['date_added']))] + $result;
+		}
+
+		// Total Tasks
+		$task_total = $this->model_setting_task->getTotalTasks();
+
+		// Pagination
+		$data['pagination'] = $this->load->controller('common/pagination', [
+			'total' => $task_total,
+			'page'  => $page,
+			'limit' => $this->config->get('config_pagination_admin'),
+			'url'   => $this->url->link('marketplace/task.list', 'user_token=' . $this->session->data['user_token'] . '&page={page}')
+		]);
+
+		$data['results'] = sprintf($this->language->get('text_pagination'), ($task_total) ? (($page - 1) * $this->config->get('config_pagination_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_pagination_admin')) > ($task_total - $this->config->get('config_pagination_admin'))) ? $task_total : ((($page - 1) * $this->config->get('config_pagination_admin')) + $this->config->get('config_pagination_admin')), $task_total, ceil($task_total / $this->config->get('config_pagination_admin')));
+
+		return $this->load->view('marketplace/task_list', $data);
+	}
+
+	/**
+	 * Run
+	 *
+	 * @return void
+	 */
+	public function run(): void {
+		$this->load->language('marketplace/task');
+
+		$json = [];
+
+		if (isset($this->request->get['task_id'])) {
+			$task_id = (int)$this->request->get['task_id'];
+		} else {
+			$task_id = 0;
+		}
+
+		if (!$this->user->hasPermission('modify', 'marketplace/task')) {
+			$json['error'] = $this->language->get('error_permission');
+		}
+
+		if (!$json) {
+			// Task
+			$this->load->model('setting/task');
+
+			$task_info = $this->model_setting_task->getTask($task_id);
+
+			if ($task_info) {
+				// Create a store instance using loader class to call controllers, models, views, libraries
+				$this->load->model('setting/store');
+
+				$store = $this->model_setting_store->createStoreInstance(0, $this->config->get('config_language'));
+
+				$store->load->controller($task_info['action'], $task_id, $task_info['code'], $task_info['cycle'], $task_info['date_added'], $task_info['date_modified']);
+
+				$store->session->destroy();
+
+				$this->model_setting_task->editTask($task_info['task_id']);
+			}
+
+			$json['success'] = $this->language->get('text_success');
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+
+	/**
+	 * Enable
+	 *
+	 * @return void
+	 */
+	public function enable(): void {
+		$this->load->language('marketplace/task');
+
+		$json = [];
+
+		if (isset($this->request->get['task_id'])) {
+			$task_id = (int)$this->request->get['task_id'];
+		} else {
+			$task_id = 0;
+		}
+
+		if (!$this->user->hasPermission('modify', 'marketplace/task')) {
+			$json['error'] = $this->language->get('error_permission');
+		}
+
+		if (!$json) {
+			// Task
+			$this->load->model('setting/task');
+
+			$this->model_setting_task->editStatus($task_id, true);
+
+			$json['success'] = $this->language->get('text_success');
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+
+	/**
+	 * Disable
+	 *
+	 * @return void
+	 */
+	public function disable(): void {
+		$this->load->language('marketplace/task');
+
+		$json = [];
+
+		if (isset($this->request->get['task_id'])) {
+			$task_id = (int)$this->request->get['task_id'];
+		} else {
+			$task_id = 0;
+		}
+
+		if (!$this->user->hasPermission('modify', 'marketplace/task')) {
+			$json['error'] = $this->language->get('error_permission');
+		}
+
+		if (!$json) {
+			// Task
+			$this->load->model('setting/task');
+
+			$this->model_setting_task->editStatus($task_id, false);
+
+			$json['success'] = $this->language->get('text_success');
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+
+	/**
+	 * Delete
+	 *
+	 * @return void
+	 */
+	public function delete(): void {
+		$this->load->language('marketplace/task');
+
+		$json = [];
+
+		if (isset($this->request->post['selected'])) {
+			$selected = (array)$this->request->post['selected'];
+		} else {
+			$selected = [];
+		}
+
+		if (!$this->user->hasPermission('modify', 'marketplace/event')) {
+			$json['error'] = $this->language->get('error_permission');
+		}
+
+		if (!$json) {
+			// Event
+			$this->load->model('setting/task');
+
+			foreach ($selected as $task_id) {
+				$this->model_setting_task->deleteTask($task_id);
+			}
+
+			$json['success'] = $this->language->get('text_success');
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+}

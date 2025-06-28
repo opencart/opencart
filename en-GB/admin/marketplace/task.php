347f178179dce67d8312077b3d6d939c46fe4ba1
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-06-29 02:59:16 +0800

    New translations task.php (English, United Kingdom)

--- en-GB/admin/marketplace/task.php
+++ en-GB/admin/marketplace/task.php
@@ -5,11 +5,10 @@ $_['heading_title']     = 'Tasks';
 // Text
 $_['text_success']      = 'Success: You have modified tasks!';
 $_['text_list']         = 'Task List';
-$_['text_info']         = 'Task Information';
 
 // Column
 $_['column_code']       = 'Task Code';
-$_['column_sort_order'] = 'Sort Order';
+$_['column_date_added'] = 'Date Added';
 $_['column_action']     = 'Action';
 
 // Error

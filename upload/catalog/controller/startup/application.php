b3810aaf3fd1dfbb3bd4be7f3e79b90e2b944e4e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-03 03:07:15 +0800

    Update application.php

--- upload/catalog/controller/startup/application.php
+++ upload/catalog/controller/startup/application.php
@@ -20,8 +20,5 @@ class Application extends \Opencart\System\Engine\Controller {
 
 		// Cart
 		$this->registry->set('cart', new \Opencart\System\Library\Cart\Cart($this->registry));
-
-		// Validation
-		$this->load->helper('validation');
 	}
 }

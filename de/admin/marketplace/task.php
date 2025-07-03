9836c9fa4ce319c5e723f826bdbd2eaab4990aa8
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-03 20:35:39 +0800

    New translations task.php (German)

--- de/admin/marketplace/task.php
+++ de/admin/marketplace/task.php
@@ -5,9 +5,14 @@ $_['heading_title']     = 'Tasks';
 // Text
 $_['text_success']      = 'Success: You have modified tasks!';
 $_['text_list']         = 'Task List';
+$_['text_pending']      = 'Ausstehend';
+$_['text_processing']   = 'Verarbeitung';
+$_['text_complete']     = 'Completed';
+$_['text_failed']       = 'Failed';
 
 // Column
 $_['column_code']       = 'Task Code';
+$_['column_status']     = 'Status';
 $_['column_date_added'] = 'Datum hinzugefügt';
 $_['column_action']     = 'Aktionen';
 

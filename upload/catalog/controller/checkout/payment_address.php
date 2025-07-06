fea9b6dbb4c3434bf16ad0cbd1de4347782240b9
mhcwebdesign <github@mhccorp.com>
2025-07-06 14:31:22 +0100

    payment address validation fix
    city can be up to 128 chars

--- upload/catalog/controller/checkout/payment_address.php
+++ upload/catalog/controller/checkout/payment_address.php
@@ -114,7 +114,7 @@ class ControllerCheckoutPaymentAddress extends Controller {
 					$json['error']['address_1'] = $this->language->get('error_address_1');
 				}
 
-				if ((utf8_strlen($this->request->post['city']) < 2) || (utf8_strlen($this->request->post['city']) > 32)) {
+				if ((utf8_strlen($this->request->post['city']) < 2) || (utf8_strlen($this->request->post['city']) > 128)) {
 					$json['error']['city'] = $this->language->get('error_city');
 				}
 

40c4b2b38ed07371e7be6cb936bf4806f553e9be
Anton Semenov <20430159+trydalcoholic@users.noreply.github.com>
2025-07-06 21:44:57 +0300

    docs(extension): fix parameter types in total extension models
    Update PHPDoc parameter types to array<int, float> for $taxes parameter
    to resolve PHPStan parameterByRef.type errors in total extension models.
    
    Signed-off-by: Anton Semenov <20430159+trydalcoholic@users.noreply.github.com>

--- upload/extension/opencart/catalog/model/total/reward.php
+++ upload/extension/opencart/catalog/model/total/reward.php
@@ -12,8 +12,8 @@ class Reward extends \Opencart\System\Engine\Model {
 	 * Get Total
 	 *
 	 * @param array<int, array<string, mixed>> $totals
-	 * @param array<int, float>                $taxes
-	 * @param float                            $total
+	 * @param  array<int, float>               &$taxes
+	 * @param  float                           &$total
 	 *
 	 * @return void
 	 */
@@ -47,7 +47,7 @@ class Reward extends \Opencart\System\Engine\Model {
 
 							foreach ($tax_rates as $tax_rate) {
 								if ($tax_rate['type'] == 'P') {
-									$taxes[$tax_rate['tax_rate_id']] -= $tax_rate['amount'];
+									$taxes[(int)$tax_rate['tax_rate_id']] -= (float)$tax_rate['amount'];
 								}
 							}
 						}

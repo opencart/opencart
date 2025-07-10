6eb353b74b659cdfd1f58b9cc4d69a77fc03c97c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-11 03:09:01 +0800

    Update country.php

--- upload/admin/controller/ssr/admin/country.php
+++ upload/admin/controller/ssr/admin/country.php
@@ -180,9 +180,7 @@ class Country extends \Opencart\System\Engine\Controller {
 					unlink($file);
 				}
 
-				$results = oc_directory_read($base . $directory, false, '/country\-.+\.json$/');
-
-				$files = glob($base . $directory . 'country-*.json');
+				$files = oc_directory_read($base . $directory, false, '/country\-.+\.json$/');
 
 				foreach ($files as $file) {
 					unlink($file);

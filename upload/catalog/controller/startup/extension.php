5d12833fcb5581e37864241be2356737e6f2231f
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-17 00:33:35 +0800

    Update extension.php

--- upload/catalog/controller/startup/extension.php
+++ upload/catalog/controller/startup/extension.php
@@ -18,21 +18,21 @@ class Extension extends \Opencart\System\Engine\Controller {
 		$results = $this->model_setting_extension->getExtensions();
 
 		foreach ($results as $result) {
-			$extension = str_replace(['_', '/'], ['', '\\'], ucwords($result['extension'], '_/'));
+			$extension = str_replace(['_', '/'], ['', '\\'], ucwords($result['code'], '_/'));
 
 			// Register controllers, models and system extension folders
-			$this->autoloader->register('Opencart\Catalog\Controller\Extension\\' . $extension, DIR_EXTENSION . $result['extension'] . '/catalog/controller/');
-			$this->autoloader->register('Opencart\Catalog\Model\Extension\\' . $extension, DIR_EXTENSION . $result['extension'] . '/catalog/model/');
-			$this->autoloader->register('Opencart\System\Library\Extension\\' . $extension, DIR_EXTENSION . $result['extension'] . '/system/library/');
+			$this->autoloader->register('Opencart\Catalog\Controller\Extension\\' . $extension, DIR_EXTENSION . $result['code'] . '/catalog/controller/');
+			$this->autoloader->register('Opencart\Catalog\Model\Extension\\' . $extension, DIR_EXTENSION . $result['code'] . '/catalog/model/');
+			$this->autoloader->register('Opencart\System\Library\Extension\\' . $extension, DIR_EXTENSION . $result['code'] . '/system/library/');
 
 			// Template directory
-			$this->template->addPath('extension/' . $result['extension'], DIR_EXTENSION . $result['extension'] . '/catalog/view/template/');
+			$this->template->addPath('extension/' . $result['code'], DIR_EXTENSION . $result['code'] . '/catalog/view/template/');
 
 			// Language directory
-			$this->language->addPath('extension/' . $result['extension'], DIR_EXTENSION . $result['extension'] . '/catalog/language/');
+			$this->language->addPath('extension/' . $result['code'], DIR_EXTENSION . $result['code'] . '/catalog/language/');
 
 			// Config directory
-			$this->config->addPath('extension/' . $result['extension'], DIR_EXTENSION . $result['extension'] . '/system/config/');
+			$this->config->addPath('extension/' . $result['code'], DIR_EXTENSION . $result['code'] . '/system/config/');
 		}
 	}
 }

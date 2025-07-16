3be9e33b6fe5dc5a8402823a29817e18b3b11e7b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-17 00:23:37 +0800

    Update catalog.php

--- upload/system/config/catalog.php
+++ upload/system/config/catalog.php
@@ -44,8 +44,7 @@ $_['action_pre_action']  = [
 // Action Events
 $_['action_event']      = [
 	'controller/*/before' => [
-		0 => 'event/modification.controller',
-		1 => 'event/language.before',
+		0 => 'event/language.before',
 		//2 => 'event/debug.before'
 	],
 	'controller/*/after' => [
@@ -53,12 +52,8 @@ $_['action_event']      = [
 		//2 => 'event/debug.after'
 	],
 	'view/*/before' => [
-		0   => 'event/modification.view',
 		500 => 'event/theme',
 		998 => 'event/language'
-	],
-	'language/*/before' => [
-		0 => 'event/modification.language'
 	],
 	'language/*/after' => [
 		0 => 'startup/language.after',

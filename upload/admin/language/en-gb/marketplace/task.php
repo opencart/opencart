56768369d0c41bbec8ffb06c986ee7e2213afca1
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-20 02:51:19 +0800

    Update source file task.php

--- upload/admin/language/en-gb/marketplace/task.php
+++ upload/admin/language/en-gb/marketplace/task.php
@@ -1,20 +1,21 @@
 <?php
 // Heading
-$_['heading_title']     = 'Tasks';
+$_['heading_title']        = 'Tasks';
 
 // Text
-$_['text_success']      = 'Success: You have modified tasks!';
-$_['text_list']         = 'Task List';
-$_['text_pending']      = 'Pending';
-$_['text_processing']   = 'Processing';
-$_['text_complete']     = 'Completed';
-$_['text_failed']       = 'Failed';
+$_['text_success']         = 'Success: You have modified tasks!';
+$_['text_list']            = 'Task List';
+$_['text_info']            = 'Task Information';
+$_['text_pending']         = 'Pending';
+$_['text_processing']      = 'Processing';
+$_['text_complete']        = 'Completed';
+$_['text_failed']          = 'Failed';
 
 // Column
-$_['column_code']       = 'Task Code';
-$_['column_status']     = 'Status';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_code']          = 'Task Code';
+$_['column_status']        = 'Status';
+$_['column_date_modified'] = 'Date Modified';
+$_['column_action']        = 'Action';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify tasks!';
+$_['error_permission']     = 'Warning: You do not have permission to modify tasks!';

37fcfa1ecc1f73fe3454b1e8b6027c950e421210
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-22 02:48:39 +0800

    New translations task.php (English, United Kingdom)

--- en-GB/admin/marketplace/task.php
+++ en-GB/admin/marketplace/task.php
@@ -14,8 +14,13 @@ $_['text_failed']          = 'Failed';
 // Column
 $_['column_code']          = 'Task Code';
 $_['column_status']        = 'Status';
+$_['column_date_added']    = 'Date Added';
 $_['column_date_modified'] = 'Date Modified';
 $_['column_action']        = 'Action';
 
+// Button
+$_['button_run']           = 'Run Task';
+
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify tasks!';
+$_['error_exists']         = 'Task does not exist!';

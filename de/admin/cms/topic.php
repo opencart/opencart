f9d8f82caa6f86637b63aa5f3928ddb5e6c700bd
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-29 02:40:20 +0800

    New translations topic.php (German)

--- de/admin/cms/topic.php
+++ de/admin/cms/topic.php
@@ -25,8 +25,8 @@ $_['entry_meta_title']        = 'Meta-Tag-Titel';
 $_['entry_meta_keyword']      = 'Meta-Tag-Schlüsselwörter';
 $_['entry_meta_description']  = 'Meta-Tag-Beschreibung';
 $_['entry_store']             = 'Online-Shop (Store)';
-$_['entry_sort_order']        = 'Sortierreihenfolge';
 $_['entry_status']            = 'Status';
+$_['entry_sort_order']        = 'Sortierreihenfolge';
 $_['entry_keyword']           = 'SEO-URL Kennung (Schlüsselwort)';
 $_['entry_layout']            = 'Layout überschreiben';
 

998bed6d76b3adfb312aec750ecba4a999630a3c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-29 02:46:33 +0800

    New translations country.php (English, United Kingdom)

---
+++ en-GB/admin/task/catalog/country.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_list']        = 'You have rendered the country list';
+$_['text_next']        = 'You have rendered %s to %s of %s countries';
+$_['text_success']     = 'Success: You have modified countries!';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify countries!';
+$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']       = 'Warning: File %s could not be written!';

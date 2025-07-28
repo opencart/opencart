7f4c01d758b400d4f006a825175939ea362e7819
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-29 02:40:41 +0800

    New translations task.php (English, United Kingdom)

--- en-GB/admin/marketplace/task.php
+++ en-GB/admin/marketplace/task.php
@@ -10,19 +10,26 @@ $_['text_filter']          = 'Filter';
 $_['text_pending']         = 'Pending';
 $_['text_processing']      = 'Processing';
 $_['text_paused']          = 'Paused';
-$_['text_complete']        = 'Completed';
+$_['text_complete']        = 'Complete';
 $_['text_failed']          = 'Failed';
 
 // Column
 $_['column_code']          = 'Task Code';
 $_['column_status']        = 'Status';
+$_['column_response']      = 'Response';
 $_['column_date_added']    = 'Date Added';
 $_['column_date_modified'] = 'Last Run';
 $_['column_action']        = 'Action';
 
+// Entry
+$_['entry_code']           = 'Task Code';
+$_['entry_status']         = 'Status';
+
 // Button
-$_['button_run']           = 'Run Task';
+$_['button_run']           = 'Run Tasks';
+$_['button_pause']         = 'Pause';
 
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify tasks!';
+$_['error_status']         = 'Task status can only be pending, processing, paused, complete or failed!';
 $_['error_exists']         = 'Task does not exist!';

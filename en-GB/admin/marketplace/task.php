35cc4688aa600b7831e51e380d4976d797dc24cb
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-07-30 20:25:13 +0800

    New translations task.php (English, United Kingdom)

--- en-GB/admin/marketplace/task.php
+++ en-GB/admin/marketplace/task.php
@@ -7,6 +7,7 @@ $_['text_success']         = 'Success: You have modified tasks!';
 $_['text_list']            = 'Task List';
 $_['text_info']            = 'Task Information';
 $_['text_filter']          = 'Filter';
+$_['text_status']          = 'Change Status';
 $_['text_pending']         = 'Pending';
 $_['text_processing']      = 'Processing';
 $_['text_paused']          = 'Paused';
@@ -16,7 +17,6 @@ $_['text_failed']          = 'Failed';
 // Column
 $_['column_code']          = 'Task Code';
 $_['column_status']        = 'Status';
-$_['column_response']      = 'Response';
 $_['column_date_added']    = 'Date Added';
 $_['column_date_modified'] = 'Last Run';
 $_['column_action']        = 'Action';
@@ -24,10 +24,14 @@ $_['column_action']        = 'Action';
 // Entry
 $_['entry_code']           = 'Task Code';
 $_['entry_status']         = 'Status';
+$_['entry_action']         = 'Action';
+$_['entry_args']           = 'Args';
+$_['entry_response']       = 'Response';
 
 // Button
 $_['button_run']           = 'Run Tasks';
 $_['button_pause']         = 'Pause';
+$_['button_clear']         = 'Clear Tasks';
 
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify tasks!';

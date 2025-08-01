5e484f3cc8b16ce217dccd1aa4aae10d7c052a30
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-02 04:01:55 +0800

    New translations restore.php (German)

--- de/admin/task/admin/restore.php
+++ de/admin/task/admin/restore.php
@@ -1,2 +1,11 @@
 <?php
-$_['text_restore'] = 'Wiederherstellen von %s von %s';
+$_['text_restore']     = 'Wiederherstellen von %s von %s';
+
+// Text
+$_['text_success']     = 'Success: Starting building the database backup!';
+$_['text_backup']      = 'Backing up table %s records %s to %s of %s records';
+
+// Error
+$_['error_table']     = 'Tabelle %s ist nicht in der Erlaubtliste!';
+$_['error_filename']  = 'Filename required!';
+$_['error_file']      = 'Datei konnte nicht gefunden werden!';

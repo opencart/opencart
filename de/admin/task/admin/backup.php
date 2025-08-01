de525985f3cfdbcddf61877ca869377a523d0c58
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-02 04:01:54 +0800

    New translations backup.php (German)

--- de/admin/task/admin/backup.php
+++ de/admin/task/admin/backup.php
@@ -1,4 +1,18 @@
 <?php
 // Text
-$_['text_success'] = 'Success: Starting building the database backup!';
-$_['text_backup']  = 'Sichern der Tabellen %s Datensätze %s bis %s Datensätze';
+$_['text_success']   = 'Success: Starting building the database backup!';
+$_['text_backup']    = 'Backing up table %s records %s to %s of %s records';
+
+// Error
+$_['error_required'] = 'Argument %s required!';
+$_['error_backup']   = 'Warning: You must select at least one table to export!';
+
+$_['error_table']    = 'Tabelle %s ist nicht in der Erlaubtliste!';
+$_['error_allowed'] = 'Tabelle %s ist nicht in der Erlaubtliste!';
+$_['error_filename'] = 'Filename required!';
+$_['error_table']    = 'Taglem required!';
+$_['error_start']    = 'Filename required!';
+$_['error_limit']    = 'Filename required!';
+$_['error_total']    = 'Filename required!';
+
+$_['error_file']     = 'Datei konnte nicht gefunden werden!';

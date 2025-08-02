a0f7f4a64537e72b280f40a10886953288cc3459
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-02 20:01:05 +0800

    Update source file restore.php

--- upload/admin/language/en-gb/task/admin/restore.php
+++ upload/admin/language/en-gb/task/admin/restore.php
@@ -1,11 +1,10 @@
 <?php
-$_['text_restore']     = 'Restoring %s of %s';
-
 // Text
-$_['text_success']     = 'Success: Starting building the database backup!';
-$_['text_backup']      = 'Backing up table %s records %s to %s of %s records';
+$_['text_success']   = 'Success: Starting building the database backup!';
+$_['text_backup']    = 'Restoring previous database backup (Progress %s)';
 
 // Error
-$_['error_table']     = 'Table %s is not in the allowed list!';
-$_['error_filename']  = 'Filename required!';
-$_['error_file']      = 'File could not be found!';
+$_['error_required'] = 'Argument %s required!';
+$_['error_table']    = 'Table %s is not allowed to be backed up!';
+$_['error_filename'] = 'Filename required!';
+$_['error_file']     = 'File could not be found!';

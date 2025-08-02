e2e4610983d10fa9cacd7379f68fe7d5f6ede52c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-03 02:41:46 +0800

    Update stock_status.php

--- upload/admin/controller/task/admin/stock_status.php
+++ upload/admin/controller/task/admin/stock_status.php
@@ -39,7 +39,7 @@ class StockStatus extends \Opencart\System\Engine\Controller {
 
 		$this->load->model('localisation/language');
 
-		$language_info = $this->model_localisation_language->getLanguage($args['language_id']);
+		$language_info = $this->model_localisation_language->getLanguage((int)$args['language_id']);
 
 		if (!$language_info) {
 			return ['error' => $this->language->get('error_language')];

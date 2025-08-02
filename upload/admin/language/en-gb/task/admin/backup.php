22a6ba286f4c3a6eb5e73c286d014c81af72ca20
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-02 18:54:34 +0800

    Update source file backup.php

--- upload/admin/language/en-gb/task/admin/backup.php
+++ upload/admin/language/en-gb/task/admin/backup.php
@@ -1,4 +1,18 @@
 <?php
 // Text
-$_['text_success'] = 'Success: Starting building the database backup!';
-$_['text_backup']  = 'Backing up table %s records %s to %s records';
+$_['text_success']   = 'Success: Starting building the database backup!';
+$_['text_backup']    = 'Backing up table %s records %s to %s of %s records';
+
+// Error
+$_['error_required'] = 'Argument %s required!';
+$_['error_backup']   = 'Warning: You must select at least one table to export!';
+
+$_['error_table']    = 'Table %s is not in the allowed list!';
+$_['error_allowed'] = 'Table %s is not in the allowed list!';
+$_['error_filename'] = 'Filename required!';
+$_['error_table']    = 'Taglem required!';
+$_['error_start']    = 'Filename required!';
+$_['error_limit']    = 'Filename required!';
+$_['error_total']    = 'Filename required!';
+
+$_['error_file']     = 'File could not be found!';

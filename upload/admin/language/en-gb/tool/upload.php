30b9a6223f1f33bd191581227e776c5b9d1fd3db
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-02 20:01:12 +0800

    Update source file upload.php

--- upload/admin/language/en-gb/tool/upload.php
+++ upload/admin/language/en-gb/tool/upload.php
@@ -24,5 +24,5 @@ $_['error_permission']   = 'Warning: You do not have permission to modify upload
 $_['error_not_found']    = 'Error: Could not find file %s !';
 $_['error_headers_sent'] = 'Error: Headers already sent out!';
 $_['error_upload']       = 'File could not be uploaded!';
-$_['error_filename']     = 'Filename must be between 3 and 128 characters!';
+$_['error_filename']     = 'Filename must be between 5 and 128 characters!';
 $_['error_file_type']    = 'Invalid file type!';

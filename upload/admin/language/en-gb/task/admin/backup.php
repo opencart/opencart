5f4d11be78e78ee48f7a63c00c9be479938c2d5d
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-02 20:01:02 +0800

    Update source file backup.php

--- upload/admin/language/en-gb/task/admin/backup.php
+++ upload/admin/language/en-gb/task/admin/backup.php
@@ -5,14 +5,6 @@ $_['text_backup']    = 'Backing up table %s records %s to %s of %s records';
 
 // Error
 $_['error_required'] = 'Argument %s required!';
-$_['error_backup']   = 'Warning: You must select at least one table to export!';
-
-$_['error_table']    = 'Table %s is not in the allowed list!';
-$_['error_allowed'] = 'Table %s is not in the allowed list!';
-$_['error_filename'] = 'Filename required!';
-$_['error_table']    = 'Taglem required!';
-$_['error_start']    = 'Filename required!';
-$_['error_limit']    = 'Filename required!';
-$_['error_total']    = 'Filename required!';
-
+$_['error_table']    = 'Table %s is not allowed to be backed up!';
+$_['error_filename'] = 'Filename must be between 5 and 128 characters!';
 $_['error_file']     = 'File could not be found!';

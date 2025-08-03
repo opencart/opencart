adb5b94f77cb8be9cd37a96a40160374e7bbc30b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-03 22:42:04 +0800

    Update source file country.php

--- upload/admin/language/en-gb/task/catalog/country.php
+++ upload/admin/language/en-gb/task/catalog/country.php
@@ -1,10 +1,11 @@
 <?php
 // Text
-$_['text_list']        = 'You have rendered the country list';
-$_['text_next']        = 'You have rendered %s to %s of %s countries';
-$_['text_success']     = 'Success: You have modified countries!';
+$_['text_success']    = 'Starting generating country files!';
+$_['text_list']       = 'Generating %s country list files!';
+$_['text_info']       = 'Generating %s %s country information file!';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify countries!';
-$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']       = 'Warning: File %s could not be written!';
+$_['error_store']     = 'Warning: Store could not be found!';
+$_['error_language']  = 'Warning: Language could not be found!';
+$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Warning: File %s could not be written!';

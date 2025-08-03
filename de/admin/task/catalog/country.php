2fc7c809131ef5edaab9fcf4b542b3cdfb4ecbd3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-03 22:42:47 +0800

    New translations country.php (German)

--- de/admin/task/catalog/country.php
+++ de/admin/task/catalog/country.php
@@ -1,10 +1,11 @@
 <?php
 // Text
-$_['text_list']        = 'You have rendered the country list';
-$_['text_next']        = 'You have rendered %s to %s of %s countries';
-$_['text_success']     = 'Erfolg: Sie haben Länder geändert!';
+$_['text_success']    = 'Starting generating country files!';
+$_['text_list']       = 'Generating %s country list files!';
+$_['text_info']       = 'Generating %s %s country information file!';
 
 // Error
-$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Länder zu ändern!';
-$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']       = 'Warning: File %s could not be written!';
+$_['error_store']     = 'Warning: Store could not be found!';
+$_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
+$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Warning: File %s could not be written!';

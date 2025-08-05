16cbace9eda549609cf0a9633d53bddd93a118d3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-06 03:23:43 +0800

    New translations backup.php (English, United Kingdom)

--- en-GB/admin/task/system/backup.php
+++ en-GB/admin/task/system/backup.php
@@ -1,2 +1,10 @@
 <?php
-$_['text_backup'] = 'Backing up table %s records %s to %s records';
+// Text
+$_['text_success']   = 'Success: Starting building the database backup!';
+$_['text_backup']    = 'Backing up table %s records %s to %s of %s records';
+
+// Error
+$_['error_required'] = 'Argument %s required!';
+$_['error_table']    = 'Table %s is not allowed to be backed up!';
+$_['error_filename'] = 'Filename must be between 5 and 128 characters!';
+$_['error_file']     = 'File could not be found!';

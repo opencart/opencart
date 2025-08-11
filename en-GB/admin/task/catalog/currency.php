7d29f9a3d005149aeb7e348a42a320d73f275292
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-12 00:42:14 +0800

    New translations currency.php (English, United Kingdom)

--- en-GB/admin/task/catalog/currency.php
+++ en-GB/admin/task/catalog/currency.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_success']     = 'Generating currency tasks!';
-$_['text_list']        = 'Generating %%s %s currency list!';
-$_['text_clear']       = 'Currency information cleared!';
+$_['text_task']       = 'Generating currency tasks!';
+$_['text_list']       = 'Generating %%s %s currency list!';
+$_['text_clear']      = 'Currency information cleared!';
 
 // Error
-$_['error_store']      = 'Warning: Store could not be found!';
-$_['error_language']   = 'Warning: Language could not be found!';
-$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']       = 'Warning: File %s could not be written!';
+$_['error_store']     = 'Warning: Store could not be found!';
+$_['error_language']  = 'Warning: Language could not be found!';
+$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Warning: File %s could not be written!';

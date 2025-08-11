a7091e941fdff2fbc3e80b4e3953499b2e0fd059
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-11 09:39:51 +0800

    Update source file currency.php

--- upload/admin/language/en-gb/task/admin/currency.php
+++ upload/admin/language/en-gb/task/admin/currency.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_success']    = 'Starting generating currency files!';
-$_['text_list']       = 'Generating %s currency list files!';
+$_['text_success']    = 'Generating currency tasks!';
+$_['text_list']       = 'Generating %s currency list!';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify currencies!';
-$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']       = 'Warning: File %s could not be written!';
+$_['error_language']  = 'Warning: Language could not be found!';
+$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Warning: File %s could not be written!';

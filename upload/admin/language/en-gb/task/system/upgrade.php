119b517223ebbd378f273d6ae1bb0d2bffb2e123
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-12 02:07:06 +0800

    Update source file upgrade.php

--- upload/admin/language/en-gb/task/system/upgrade.php
+++ upload/admin/language/en-gb/task/system/upgrade.php
@@ -1,12 +1,3 @@
 <?php
 // Text
 $_['text_success']    = 'Checking for upgrade!';
-$_['text_list']       = 'Generating %s country list files!';
-$_['text_info']       = 'Generating %s %s country information file!';
-$_['text_download']   = 'Generating %s %s country information file!';
-
-
-// Error
-$_['error_language']  = 'Warning: Language could not be found!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';

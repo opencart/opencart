bfb412d223cb79d48b6f3bad329ed53c983e5722
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-11 22:29:14 +0800

    New translations manufacturer.php (German)

--- de/admin/task/catalog/manufacturer.php
+++ de/admin/task/catalog/manufacturer.php
@@ -5,6 +5,5 @@ $_['text_next']        = 'You have rendered %s to %s of %s manufacturers';
 $_['text_success']     = 'Erfolgreich: Sie haben Hersteller bearbeitet!';
 
 // Error
-$_['error_permission'] = 'Warnung: Sie sind nicht berechtigt, Hersteller zu ändern!';
 $_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
 $_['error_file']       = 'Warning: File %s could not be written!';

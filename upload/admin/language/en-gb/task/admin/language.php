e6f891d4f9daaa3f1a10f7d868b98df5cbe57b86
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-11 09:39:53 +0800

    Update source file language.php

--- upload/admin/language/en-gb/task/admin/language.php
+++ upload/admin/language/en-gb/task/admin/language.php
@@ -1,10 +1,9 @@
 <?php
 // Text
-$_['text_task']        = 'You have rendered the language list';
-$_['text_next']        = 'You have rendered %s to %s of %s languages';
-$_['text_success']     = 'Success: You have modified languages!';
+$_['text_success']    = 'Generating language tasks!';
+$_['text_list']       = 'Generating %s language list!';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify languages!';
-$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']       = 'Warning: File %s could not be written!';
+$_['error_language']  = 'Warning: Language could not be found!';
+$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Warning: File %s could not be written!';

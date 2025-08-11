7969892794ffb8da4ccc96be350a2433abc297da
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-11 19:18:21 +0800

    New translations currency.php (English, United Kingdom)

--- en-GB/admin/task/catalog/currency.php
+++ en-GB/admin/task/catalog/currency.php
@@ -1,8 +1,11 @@
 <?php
 // Text
-$_['text_success']     = 'Success: You have modified currencies!';
+$_['text_success']     = 'Generating currency tasks!';
+$_['text_list']        = 'Generating %s currency list!';
+$_['text_clear']       = 'Currency information cleared!';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify currencies!';
+$_['error_store']      = 'Warning: Store could not be found!';
+$_['error_language']   = 'Warning: Language could not be found!';
 $_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
 $_['error_file']       = 'Warning: File %s could not be written!';

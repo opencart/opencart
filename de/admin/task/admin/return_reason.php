afff50e9bac6b78cf1f1fc0fa1453aa2f28317a4
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-11 19:18:30 +0800

    New translations return_reason.php (German)

--- de/admin/task/admin/return_reason.php
+++ de/admin/task/admin/return_reason.php
@@ -2,6 +2,7 @@
 // Text
 $_['text_success']    = 'Generating return reason tasks!';
 $_['text_list']       = 'Generating %s return reason list!';
+$_['text_clear']      = 'Return Reason information cleared!';
 
 // Error
 $_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';

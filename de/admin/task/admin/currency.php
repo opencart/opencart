9394f588918744ab0752516d0f2fb33b5bc90eeb
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-11 08:34:54 +0800

    New translations currency.php (German)

--- de/admin/task/admin/currency.php
+++ de/admin/task/admin/currency.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_success']    = 'Starting generating currency files!';
-$_['text_list']       = 'Generating %s currency list files!';
+$_['text_success']    = 'Generating currency tasks!';
+$_['text_list']       = 'Generating %s currency list!';
 
 // Error
-$_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, Währungen zu ändern!';
-$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']       = 'Warning: File %s could not be written!';
+$_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
+$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Warning: File %s could not be written!';

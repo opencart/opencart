3abd4d59cb6667890b6f257b6f36f92fdcb6740a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-11 19:18:32 +0800

    New translations translation.php (German)

--- de/admin/task/admin/translation.php
+++ de/admin/task/admin/translation.php
@@ -2,6 +2,7 @@
 // Text
 $_['text_success']    = 'Generating translation tasks!';
 $_['text_list']       = 'Generating %s translation list!';
+$_['text_clear']      = 'Translation information cleared!';
 
 // Error
 $_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';

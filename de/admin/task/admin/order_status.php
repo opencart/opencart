d6cc99abe1b963c5ca51d07c0df7b6dc0e3773b7
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-11 08:35:16 +0800

    New translations order_status.php (German)

--- de/admin/task/admin/order_status.php
+++ de/admin/task/admin/order_status.php
@@ -1,8 +1,7 @@
 <?php
 // Text
-$_['text_success']    = 'Starting generating country files!';
-$_['text_list']       = 'Generating %s country list files!';
-$_['text_info']       = 'Generating %s %s country information file!';
+$_['text_success']    = 'Generating order status tasks!';
+$_['text_list']       = 'Generating %s order status list!';
 
 // Error
 $_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';

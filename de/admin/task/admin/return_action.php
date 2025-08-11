4f408972946239ec7111dd23e17e1162b3fac317
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-12 00:42:26 +0800

    New translations return_action.php (German)

--- de/admin/task/admin/return_action.php
+++ de/admin/task/admin/return_action.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_success']    = 'Generating return action tasks!';
+$_['text_task']       = 'Generating return action tasks!';
 $_['text_list']       = 'Generating %s return action list!';
 $_['text_clear']      = 'Return Action information cleared!';
 

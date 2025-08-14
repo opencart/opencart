4b718390d7651e12e0ebeeeae6b463dcba558b4a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-14 19:01:06 +0800

    New translations mail.php (German)

--- de/admin/task/system/mail.php
+++ de/admin/task/system/mail.php
@@ -1,1 +1,14 @@
 <?php
+// Text
+$_['text_success']  = 'Success: E-Mmail sent!';
+
+// Error
+$_['error_to']      = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_from']    = 'E-Mail-Adresse scheint nicht gültig zu sein!';
+$_['error_sender']  = 'E-Mail-Betreff erforderlich!';
+
+
+$_['error_subject'] = 'E-Mail-Betreff erforderlich!';
+
+$_['error_message'] = 'E-Mail-Nachricht erforderlich!';
+$_['error_email']   = 'Keine E-Mail Empfänger!';

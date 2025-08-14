120202f51e16a1d728b7924f4ab72abcf38fdc08
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-14 18:50:12 +0800

    Update mail.php

--- upload/admin/language/en-gb/task/system/mail.php
+++ upload/admin/language/en-gb/task/system/mail.php
@@ -1,1 +1,14 @@
 <?php
+// Text
+$_['text_success']  = 'Success: E-Mmail sent!';
+
+// Error
+$_['error_to']      = 'E-Mail Address does not appear to be valid!';
+$_['error_from']    = 'E-Mail Address does not appear to be valid!';
+$_['error_sender']  = 'E-Mail Subject required!';
+
+
+$_['error_subject'] = 'E-Mail Subject required!';
+
+$_['error_message'] = 'E-Mail Message required!';
+$_['error_email']   = 'No E-Mail recipients!';

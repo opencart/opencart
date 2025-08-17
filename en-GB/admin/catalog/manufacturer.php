803ffd500d4748cbcdab7c6117d624ddaa86e107
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-18 06:12:38 +0800

    New translations manufacturer.php (English, United Kingdom)

--- en-GB/admin/catalog/manufacturer.php
+++ en-GB/admin/catalog/manufacturer.php
@@ -27,6 +27,7 @@ $_['entry_meta_title']        = 'Meta Tag Title';
 $_['entry_meta_keyword']      = 'Meta Tag Keywords';
 $_['entry_meta_description']  = 'Meta Tag Description';
 $_['entry_store']             = 'Stores';
+$_['entry_language']          = 'Language';
 $_['entry_keyword']           = 'Keyword';
 $_['entry_image']             = 'Image';
 $_['entry_status']            = 'Status';

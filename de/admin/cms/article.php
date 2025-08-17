23970eb2d83d1fe6de26f306c9c121370c7519e3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-18 06:12:33 +0800

    New translations article.php (German)

--- de/admin/cms/article.php
+++ de/admin/cms/article.php
@@ -32,6 +32,7 @@ $_['entry_meta_description']  = 'Meta-Tag-Beschreibung';
 $_['entry_topic']             = 'Thema';
 $_['entry_author']            = 'Autor';
 $_['entry_store']             = 'Online-Shop (Store)';
+$_['entry_language']          = 'Sprache';
 $_['entry_status']            = 'Status';
 $_['entry_sort_order']        = 'Sortierreihenfolge';
 $_['entry_keyword']           = 'SEO-URL Kennung (Schlüsselwort)';

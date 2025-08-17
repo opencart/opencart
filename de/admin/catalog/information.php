a165ed407f91e1ec4732e9a274e9839c5539a7f8
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-17 12:37:35 +0800

    New translations information.php (German)

--- de/admin/catalog/information.php
+++ de/admin/catalog/information.php
@@ -7,6 +7,7 @@ $_['text_success']            = 'Erfolgreich: Sie haben Informationen geändert!
 $_['text_list']               = 'Informationen Liste';
 $_['text_add']                = 'Neue Information zufügen';
 $_['text_edit']               = 'Information bearbeiten';
+$_['text_filter']             = 'Filter';
 $_['text_general']            = 'Allgemein';
 $_['text_meta']               = 'Meta-Tags';
 $_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL  eindeutig ist.';

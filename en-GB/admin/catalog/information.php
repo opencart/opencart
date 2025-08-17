d746977ff8cfd0557481882dd9651adebbf808f9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-18 06:12:36 +0800

    New translations information.php (English, United Kingdom)

--- en-GB/admin/catalog/information.php
+++ en-GB/admin/catalog/information.php
@@ -25,6 +25,7 @@ $_['entry_meta_title']        = 'Meta Tag Title';
 $_['entry_meta_keyword']      = 'Meta Tag Keywords';
 $_['entry_meta_description']  = 'Meta Tag Description';
 $_['entry_store']             = 'Stores';
+$_['entry_language']          = 'Language';
 $_['entry_keyword']           = 'Keyword';
 $_['entry_status']            = 'Status';
 $_['entry_sort_order']        = 'Sort Order';

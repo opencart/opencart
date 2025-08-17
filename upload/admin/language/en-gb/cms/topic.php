d6c940becfc8abc1bc94361adbbd6ce44db39962
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-17 12:36:22 +0800

    Update source file topic.php

--- upload/admin/language/en-gb/cms/topic.php
+++ upload/admin/language/en-gb/cms/topic.php
@@ -7,6 +7,7 @@ $_['text_success']            = 'Success: You have modified topics!';
 $_['text_list']               = 'Topic List';
 $_['text_add']                = 'Add Topic';
 $_['text_edit']               = 'Edit Topic';
+$_['text_filter']             = 'Filter';
 $_['text_general']            = 'General';
 $_['text_meta']               = 'Meta Tags';
 $_['text_keyword']            = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';

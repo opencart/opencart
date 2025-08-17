b39d6d84b9f6d7226be47f35e04c8c14f7275417
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-17 12:37:36 +0800

    New translations manufacturer.php (German)

--- de/admin/catalog/manufacturer.php
+++ de/admin/catalog/manufacturer.php
@@ -7,6 +7,7 @@ $_['text_success']            = 'Erfolgreich: Sie haben Hersteller bearbeitet!';
 $_['text_list']               = 'Hersteller Liste';
 $_['text_add']                = 'Neuer Hersteller zufügen';
 $_['text_edit']               = 'Hersteller bearbeiten';
+$_['text_filter']             = 'Filter';
 $_['text_general']            = 'Allgemein';
 $_['text_meta']               = 'Meta-Tags';
 $_['text_keyword']            = 'Verwenden Sie keine Leerzeichen, ersetzen Sie Leerzeichen stattdessen durch - und stellen Sie sicher, dass die SEO-URL  eindeutig ist.';

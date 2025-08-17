fca09dbc4c3260e73347559a9d2eda8e8d2879f9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-18 06:12:40 +0800

    New translations article.php (English, United Kingdom)

--- en-GB/admin/cms/article.php
+++ en-GB/admin/cms/article.php
@@ -32,6 +32,7 @@ $_['entry_meta_description']  = 'Meta Tag Description';
 $_['entry_topic']             = 'Topic';
 $_['entry_author']            = 'Author';
 $_['entry_store']             = 'Stores';
+$_['entry_language']          = 'Language';
 $_['entry_status']            = 'Status';
 $_['entry_sort_order']        = 'Sort Order';
 $_['entry_keyword']           = 'Keyword';

e44bfacd683a32771c55baeecbbc978f76a74173
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-18 08:43:51 +0800

    Update source file topic.php

--- upload/admin/language/en-gb/cms/topic.php
+++ upload/admin/language/en-gb/cms/topic.php
@@ -26,6 +26,7 @@ $_['entry_meta_title']        = 'Meta Tag Title';
 $_['entry_meta_keyword']      = 'Meta Tag Keywords';
 $_['entry_meta_description']  = 'Meta Tag Description';
 $_['entry_store']             = 'Stores';
+$_['entry_language']          = 'Language';
 $_['entry_status']            = 'Status';
 $_['entry_sort_order']        = 'Sort Order';
 $_['entry_keyword']           = 'Keyword';

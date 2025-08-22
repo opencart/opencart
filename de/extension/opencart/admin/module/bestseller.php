14aa0a3aef14ef1996e6f8c126e9129f0fcc076f
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-22 13:42:42 +0800

    New translations bestseller.php (German)

--- de/extension/opencart/admin/module/bestseller.php
+++ de/extension/opencart/admin/module/bestseller.php
@@ -19,8 +19,6 @@ $_['column_total']     = 'Gesamt';
 $_['entry_name']       = 'Modulname';
 $_['entry_axis']       = 'Achse';
 $_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Bildbreite';
-$_['entry_height']     = 'Bildhöhe';
 $_['entry_status']     = 'Status';
 
 // Button
@@ -29,5 +27,3 @@ $_['button_sync']      = 'Bestsellerliste erstellen';
 // Error
 $_['error_permission'] = 'Achtung: Sie sind nicht berechtigt, das Bestseller-Modul zu ändern!';
 $_['error_name']       = 'Der Modulname muss zwischen 3 und 64 Zeichen lang sein!';
-$_['error_width']      = 'Breite erforderlich!';
-$_['error_height']     = 'Körpergröße erforderlich!';

162e03f5331d71659fc49daa3efce2c1f68c4bab
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-29 19:17:19 +0800

    New translations storage.php (English, United Kingdom)

--- en-GB/admin/task/system/storage.php
+++ en-GB/admin/task/system/storage.php
@@ -1,11 +1,12 @@
 <?php
 // Text
-$_['text_success']  = 'Preparing to move the storage directory!';
-$_['text_move']     = 'Moving %s to %s of %s storage files';
-$_['text_config']   = 'Success: Updated config files.';
+$_['text_success']     = 'Preparing to move storage directory';
+$_['text_move']        = 'Copying storage files %s';
+$_['text_config']      = 'Updated config files';
 
 // Error
-$_['error_storage'] = 'Warning: Storage directory does not exist!';
-$_['error_root']    = 'Warning: Storage directory must be outside the web root!';
-$_['error_name']    = 'Warning: Storage directory name must start with `storage`! e.g. `storage_`.';
-$_['error_remove']  = 'Warning: Directory does not exist to remove!';
+$_['error_required']   = 'Warning: %s required!';
+$_['error_root']       = 'Warning: Storage directory must be outside the web root!';
+$_['error_name']       = 'Warning: Storage directory name must start with `storage`! e.g. `storage_`.';
+$_['error_exists_old'] = 'Warning: Old storage directory does not exist!';
+$_['error_exists_new'] = 'Warning: New storage directory already exists!';

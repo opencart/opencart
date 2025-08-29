7edec76ebc37347e6490c47bdd34759072d454c9
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-29 19:17:15 +0800

    New translations admin.php (German)

--- de/admin/task/system/admin.php
+++ de/admin/task/system/admin.php
@@ -1,13 +1,13 @@
 <?php
 // Text
-$_['text_success']   = 'Success: Admin directory has been moved!';
-$_['text_move']      = 'Moving admin files %s to %s of %s';
-$_['text_config']    = 'Updated config files.';
+$_['text_success']     = 'Preparing to move admin directory';
+$_['text_move']        = 'Copying admin files %s';
+$_['text_config']      = 'Updated config files';
 
 // Error
-$_['error_required'] = 'Warning: %s required!';
-$_['error_name']     = 'Warning: New admin name required!';
-$_['error_admin']    = 'Warning: Old admin directory does not exist!';
-$_['error_exists']   = 'Warning: New admin directory already exists!';
-$_['error_allowed']  = 'Achtung: Dieser Admin-Verzeichnisname kann nicht verwendet werden!';
-$_['error_writable'] = 'Achtung: config.php und admin/config.php müssen beschreibbar gemacht werden!';
+$_['error_required']   = 'Warning: %s required!';
+$_['error_name']       = 'Warning: New admin name required!';
+$_['error_exists_old'] = 'Warning: Old admin directory does not exist!';
+$_['error_exists_new'] = 'Warning: New admin directory already exists!';
+$_['error_allowed']    = 'Achtung: Dieser Admin-Verzeichnisname kann nicht verwendet werden!';
+$_['error_writable']   = 'Achtung: config.php und admin/config.php müssen beschreibbar gemacht werden!';

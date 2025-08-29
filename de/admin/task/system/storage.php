e8264dc6ae1ad9d0e70ef547c48d88af8128060a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-29 19:17:18 +0800

    New translations storage.php (German)

--- de/admin/task/system/storage.php
+++ de/admin/task/system/storage.php
@@ -1,11 +1,12 @@
 <?php
 // Text
-$_['text_success']  = 'Preparing to move the storage directory!';
-$_['text_move']     = 'Verschieben von %s nach %s von %s Speicherdateien';
-$_['text_config']   = 'Success: Updated config files.';
+$_['text_success']     = 'Preparing to move storage directory';
+$_['text_move']        = 'Copying storage files %s';
+$_['text_config']      = 'Updated config files';
 
 // Error
-$_['error_storage'] = 'Achtung: Speicherverzeichnis existiert nicht!';
-$_['error_root']    = 'Achtung: Das Speicherverzeichnis muss außerhalb des Web-Roots liegen!';
-$_['error_name']    = 'Warnung: Der Name des Speicherverzeichnisses muss mit „storage“ beginnen! zB „storage_“.';
-$_['error_remove']  = 'Warnung: Das zu löschende Verzeichnis existiert nicht!';
+$_['error_required']   = 'Warning: %s required!';
+$_['error_root']       = 'Achtung: Das Speicherverzeichnis muss außerhalb des Web-Roots liegen!';
+$_['error_name']       = 'Warnung: Der Name des Speicherverzeichnisses muss mit „storage“ beginnen! zB „storage_“.';
+$_['error_exists_old'] = 'Warning: Old storage directory does not exist!';
+$_['error_exists_new'] = 'Warning: New storage directory already exists!';

85d40f53d69a1f0e3cdb38fe4534cdfb79dd969b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-29 16:57:07 +0800

    New translations storage.php (German)

--- de/admin/task/system/storage.php
+++ de/admin/task/system/storage.php
@@ -1,1 +1,11 @@
 <?php
+// Text
+$_['text_success']  = 'Preparing to move the storage directory!';
+$_['text_move']     = 'Verschieben von %s nach %s von %s Speicherdateien';
+$_['text_config']   = 'Success: Updated config files.';
+
+// Error
+$_['error_storage'] = 'Achtung: Speicherverzeichnis existiert nicht!';
+$_['error_root']    = 'Achtung: Das Speicherverzeichnis muss außerhalb des Web-Roots liegen!';
+$_['error_name']    = 'Warnung: Der Name des Speicherverzeichnisses muss mit „storage“ beginnen! zB „storage_“.';
+$_['error_remove']  = 'Warnung: Das zu löschende Verzeichnis existiert nicht!';

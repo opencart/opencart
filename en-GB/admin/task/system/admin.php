7f94e32c17b67a9b88e9a2644513bee967d2e822
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-29 16:57:06 +0800

    New translations admin.php (English, United Kingdom)

--- en-GB/admin/task/system/admin.php
+++ en-GB/admin/task/system/admin.php
@@ -1,1 +1,13 @@
 <?php
+// Text
+$_['text_success']   = 'Success: Admin directory has been moved!';
+$_['text_move']      = 'Moving admin files %s to %s of %s';
+$_['text_config']    = 'Updated config files.';
+
+// Error
+$_['error_required'] = 'Warning: %s required!';
+$_['error_name']     = 'Warning: New admin name required!';
+$_['error_admin']    = 'Warning: Old admin directory does not exist!';
+$_['error_exists']   = 'Warning: New admin directory already exists!';
+$_['error_allowed']  = 'Warning: This admin directory name can not be used!';
+$_['error_writable'] = 'Warning: config.php and admin/config.php need to be made writable!';

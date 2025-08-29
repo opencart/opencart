a01eab8bd77a5ba9ffec29c2421fffbe560daf62
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-08-29 21:39:48 +0800

    New translations restore.php (German)

--- de/admin/task/system/restore.php
+++ de/admin/task/system/restore.php
@@ -4,7 +4,7 @@ $_['text_success']   = 'Success: Starting to restore database!';
 $_['text_restore']   = 'Restoring previous backup %s';
 
 // Error
-$_['error_required'] = 'Argument %s required!';
+$_['error_required'] = '%s erforderlich!';
 $_['error_table']    = 'Table %s is not allowed to be backed up!';
 $_['error_filename'] = 'Filename required!';
 $_['error_file']     = 'Datei konnte nicht gefunden werden!';

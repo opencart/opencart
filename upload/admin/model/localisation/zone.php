e9a92289f49a34c35ea328c00fd683cee2e425f1
OpenCartBot <customocean@gmail.com>
2025-09-03 02:49:48 +0300

    Fix zone name in zone list

--- upload/admin/model/localisation/zone.php
+++ upload/admin/model/localisation/zone.php
@@ -155,7 +155,7 @@ class Zone extends \Opencart\System\Engine\Model {
 			$language_id = $this->config->get('config_language_id');
 		}
 
-		$sql = "SELECT *, `cd`.`name` AS `country` FROM `" . DB_PREFIX . "zone` `z` LEFT JOIN `" . DB_PREFIX . "zone_description` `zd` ON (`z`.`zone_id` = `zd`.`zone_id`) LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`z`.`country_id` = `cd`.`country_id`) WHERE `zd`.`language_id` = '" . (int)$language_id . "' AND `cd`.`language_id` = '" . (int)$language_id . "'";
+		$sql = "SELECT *, `zd`.`name` AS `name`, `cd`.`name` AS `country` FROM `" . DB_PREFIX . "zone` `z` LEFT JOIN `" . DB_PREFIX . "zone_description` `zd` ON (`z`.`zone_id` = `zd`.`zone_id`) LEFT JOIN `" . DB_PREFIX . "country_description` `cd` ON (`z`.`country_id` = `cd`.`country_id`) WHERE `zd`.`language_id` = '" . (int)$language_id . "' AND `cd`.`language_id` = '" . (int)$language_id . "'";
 
 		if (!empty($data['filter_name'])) {
 			$sql .= " AND LCASE(`zd`.`name`) LIKE '" . $this->db->escape(oc_strtolower($data['filter_name']) . '%') . "'";

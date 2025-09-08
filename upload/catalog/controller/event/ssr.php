fc38cbd08cbbde7fb1877737976990e3c11f9472
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-08 18:30:12 +0800

    more ssr work

--- upload/catalog/controller/event/ssr.php
+++ upload/catalog/controller/event/ssr.php
@@ -1,7 +1,7 @@
 <?php
 namespace Opencart\Catalog\Controller\Event;
 /**
- * Class Ssr
+ * Class SSR
  *
  * @package Opencart\Catalog\Controller\Event
  */
@@ -9,8 +9,12 @@ class Ssr extends \Opencart\System\Engine\Controller {
 	/**
 	 * Index
 	 *
+	 * Creates a static page from the response from selected pages.
+	 *
 	 * Triggers
 	 *
+	 * catalog/controller/cms/article/after
+	 * catalog/controller/cms/topic/after
 	 * catalog/controller/product/category/after
 	 * catalog/controller/product/compare/after
 	 * catalog/controller/product/manufacturer/after
@@ -26,9 +30,45 @@ class Ssr extends \Opencart\System\Engine\Controller {
 	 *
 	 * @return void
 	 */
-	public function index(string &$route, array &$args, string &$code): void {
+	public function index(string &$route, array &$args, string &$output): void {
+		// 1. Get the main response to cache
+		$output = $this->response->getOutput();
 
+		print_r($this>request->server);
+		
+		// Create the directory and file names.
+		$this->load->model('design/seo_url');
 
+		// Create the directory and file names.
+		$keywords = [];
+
+		foreach ($this->request->get as $key => $value) {
+			$seo_url_info = $this->model_design_seo_url->getSeoUrlByKeyValue($key, $value, $this->config->get('config_store_id'), $this->config->get('config_language_id'));
+
+			if ($seo_url_info) {
+				$keywords[] = $seo_url_info;
+			}
+		}
+
+		$sort_order = [];
+
+		foreach ($keywords as $key => $value) {
+			$sort_order[$key] = $value['sort_order'];
+		}
+
+		array_multisort($sort_order, SORT_ASC, $keywords);
+
+		$base = DIR_OPENCART . 'shop/';
+		$directory = parse_url($store_info['url'], PHP_URL_HOST) . '/' . $language_info['code'] . '/html/' . implode('/', $keywords) . '/';
+		$filename = 'index.html';
+
+		if (!oc_directory_create($base . $directory, 0777)) {
+
+		}
+
+		if (!file_put_contents($base . $directory . $filename, $output)) {
+
+		}
 
 	}
 }

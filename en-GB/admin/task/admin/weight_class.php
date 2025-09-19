2bcfa5f8dbd619d43ce23623d6633065ed3c6714
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-20 03:39:01 +0800

    New translations weight_class.php (English, United Kingdom)

--- en-GB/admin/task/admin/weight_class.php
+++ en-GB/admin/task/admin/weight_class.php
@@ -1,10 +1,11 @@
 <?php
 // Text
-$_['text_task']       = 'Generating weight class tasks!';
+$_['text_task']       = 'Generating weight class task list!';
 $_['text_list']       = 'Generating %s weight class list!';
-$_['text_clear']      = 'Weight Class information cleared!';
+$_['text_clear']      = 'Weight Class data cleared!';
 
 // Error
-$_['error_language']  = 'Warning: Language could not be found!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';
+$_['error_required']  = 'Error: %s required!';
+$_['error_language']  = 'Error: Language could not be found!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

730acb8780c8f2d2534a7d13bc20ccb722ed6a5a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-20 03:38:44 +0800

    New translations return_action.php (German)

--- de/admin/task/admin/return_action.php
+++ de/admin/task/admin/return_action.php
@@ -1,10 +1,11 @@
 <?php
 // Text
-$_['text_task']       = 'Generating return action tasks!';
+$_['text_task']       = 'Generating return action task list!';
 $_['text_list']       = 'Generating %s return action list!';
-$_['text_clear']      = 'Return Action information cleared!';
+$_['text_clear']      = 'Return Action data cleared!';
 
 // Error
+$_['error_required']  = 'Error: %s required!';
 $_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
 $_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
 $_['error_file']      = 'Warning: File %s could not be written!';

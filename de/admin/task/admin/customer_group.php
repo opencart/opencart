bcac1d53a00e3647ce2ad9e682bed2e0aadc348f
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-19 19:48:04 +0800

    New translations customer_group.php (German)

--- de/admin/task/admin/customer_group.php
+++ de/admin/task/admin/customer_group.php
@@ -1,10 +1,11 @@
 <?php
 // Text
-$_['text_task']       = 'Generating customer group tasks!';
+$_['text_task']       = 'Generating customer group task list!';
 $_['text_list']       = 'Generating %s customer group list!';
+$_['text_info']       = 'Generating %s %s customer group information!';
 $_['text_clear']      = 'Customer Group information cleared!';
 
 // Error
-$_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';
+$_['error_language']  = 'Error: Language could not be found!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

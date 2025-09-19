1220ea9ea7ce234f638b579f6e8d3e031202a97a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-20 03:38:47 +0800

    New translations subscription_status.php (German)

--- de/admin/task/admin/subscription_status.php
+++ de/admin/task/admin/subscription_status.php
@@ -1,10 +1,11 @@
 <?php
 // Text
-$_['text_task']       = 'Generating subscription status tasks!';
+$_['text_task']       = 'Generating subscription status task list!';
 $_['text_list']       = 'Generating %s subscription status list!';
-$_['text_clear']      = 'Subscription Status information cleared!';
+$_['text_clear']      = 'Subscription Status data cleared!';
 
 // Error
-$_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';
+$_['error_required']  = 'Error: %s required!';
+$_['error_language']  = 'Error: Language could not be found!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

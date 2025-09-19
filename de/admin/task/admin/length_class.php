10a7837390894669547b277ff633dcf706be2187
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-20 00:03:26 +0800

    New translations length_class.php (German)

--- de/admin/task/admin/length_class.php
+++ de/admin/task/admin/length_class.php
@@ -1,10 +1,11 @@
 <?php
 // Text
-$_['text_task']       = 'Generating length class tasks!';
+$_['text_task']       = 'Generating length class task list!';
 $_['text_list']       = 'Generating %s length class list!';
-$_['text_clear']      = 'Length Class information cleared!';
+$_['text_clear']      = 'Length Class data cleared!';
 
 // Error
+$_['error_required']  = 'Error: %s required!';
 $_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
 $_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
 $_['error_file']      = 'Warning: File %s could not be written!';

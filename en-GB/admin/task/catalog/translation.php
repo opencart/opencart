a4886f4a9b9f3933bc9757c1eea4225c64f6b9e3
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-20 19:54:37 +0800

    New translations translation.php (English, United Kingdom)

--- en-GB/admin/task/catalog/translation.php
+++ en-GB/admin/task/catalog/translation.php
@@ -1,9 +1,10 @@
 <?php
 // Text
-$_['text_success']    = 'Generating translation tasks!';
+$_['text_task']       = 'Generating translation task list!';
 $_['text_list']       = 'Generating %s %s %s translation list!';
 
 // Error
+$_['error_required']  = 'Error: %s required!';
 $_['error_language']  = 'Warning: Language could not be found!';
 $_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
 $_['error_file']      = 'Warning: File %s could not be written!';

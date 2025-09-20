aad68574f827f116e2931a030e111d899565b461
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-20 23:57:24 +0800

    New translations banner.php (German)

--- de/admin/task/catalog/banner.php
+++ de/admin/task/catalog/banner.php
@@ -2,9 +2,10 @@
 // Text
 $_['text_task']       = 'Generating banner task list!';
 $_['text_list']       = 'Generating %s %s banner list!';
-$_['text_clear']      = 'Banner information cleared!';
+$_['text_clear']      = 'Banner data cleared!';
 
 // Error
+$_['error_required']  = 'Error: %s required!';
 $_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
 $_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
 $_['error_file']      = 'Warning: File %s could not be written!';

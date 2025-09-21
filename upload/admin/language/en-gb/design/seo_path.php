67771b235af1228a943b2bb63d256d73ba622ef6
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-22 02:32:59 +0800

    Update source file seo_path.php

---
+++ upload/admin/language/en-gb/design/seo_path.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']        = 'SEO Path';
+
+// Text
+$_['text_success']         = 'Success: You have modified SEO Paths!';
+$_['text_list']            = 'SEO Path List';
+$_['text_add']             = 'Add SEO Path';
+$_['text_edit']            = 'Edit SEO Path';
+$_['text_filter']          = 'Filter';
+
+// Column
+$_['column_query_match']   = 'Query Match';
+$_['column_query_replace'] = 'Query Replace';
+$_['column_path_match']    = 'Path Match';
+$_['column_path_replace']  = 'Path Replace';
+$_['column_sort_order']    = 'Sort Order';
+$_['column_action']        = 'Action';
+
+// Entry
+$_['entry_query_match']    = 'Query Match';
+$_['entry_query_replace']  = 'Query Replace';
+$_['entry_path_match']     = 'Path Match';
+$_['entry_path_replace']   = 'Path Replace';
+$_['entry_sort_order']     = 'Sort Order';
+
+// Help
+$_['help_sort_order']      = 'The sort order of the keywords in the URL.';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify SEO URL!';

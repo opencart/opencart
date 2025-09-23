755634b589a5f87547080c833613042380cc1b7c
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-24 05:11:40 +0800

    New translations seo_regex.php (German)

---
+++ de/admin/design/seo_regex.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']        = 'SEO Regexes';
+
+// Text
+$_['text_success']         = 'Success: You have modified SEO regexes!';
+$_['text_list']            = 'SEO Regex List';
+$_['text_add']             = 'Add SEO Regex';
+$_['text_edit']            = 'Edit SEO Regex';
+
+// Column
+$_['column_query_match']   = 'Query Match';
+$_['column_query_replace'] = 'Query Replace';
+$_['column_path_match']    = 'Path Match';
+$_['column_path_replace']  = 'Path Replace';
+$_['column_sort_order']    = 'Sortierreihenfolge';
+$_['column_action']        = 'Aktionen';
+
+// Entry
+$_['entry_query_match']    = 'Query Match';
+$_['entry_query_replace']  = 'Query Replace';
+$_['entry_path_match']     = 'Path Match';
+$_['entry_path_replace']   = 'Path Replace';
+$_['entry_sort_order']     = 'Sortierreihenfolge';
+
+// Help
+$_['help_sort_order']      = 'Die Sortierreihenfolge der Schlüsselwörter in der URL.';
+
+// Error
+$_['error_permission']     = 'Warning: You do not have permission to modify SEO regexes!';

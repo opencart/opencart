2aba652db37c677e0ba3f7fc18dc83382cda6495
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-09-24 05:11:41 +0800

    New translations seo_regex.php (English, United Kingdom)

---
+++ en-GB/admin/design/seo_regex.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']        = 'SEO Regexes';
+
+// Text
+$_['text_success']         = 'Success: You have modified SEO regexes!';
+$_['text_list']            = 'SEO Regex List';
+$_['text_add']             = 'Add SEO Regex';
+$_['text_edit']            = 'Edit SEO Regex';
+
+// Column
+$_['column_query_match']   = 'Query Match';
+$_['column_query_replace'] = 'Query Replace';
+$_['column_path_match']    = 'Path Match';
+$_['column_path_replace']  = 'Path Replace';
+$_['column_sort_order']    = 'Sort Order';
+$_['column_action']        = 'Action';
+
+// Entry
+$_['entry_query_match']    = 'Query Match';
+$_['entry_query_replace']  = 'Query Replace';
+$_['entry_path_match']     = 'Path Match';
+$_['entry_path_replace']   = 'Path Replace';
+$_['entry_sort_order']     = 'Sort Order';
+
+// Help
+$_['help_sort_order']      = 'The sort order of the keywords in the URL.';
+
+// Error
+$_['error_permission']     = 'Warning: You do not have permission to modify SEO regexes!';

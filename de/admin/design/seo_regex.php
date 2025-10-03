12ec232e0d340fd4cd7ac3474856744fe2c8192d
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-04 00:01:20 +0800

    New translations seo_regex.php (German)

--- de/admin/design/seo_regex.php
+++ de/admin/design/seo_regex.php
@@ -1,30 +1,32 @@
 <?php
 // Heading
-$_['heading_title']        = 'SEO Regexes';
+$_['heading_title']     = 'SEO Regexes';
 
 // Text
-$_['text_success']         = 'Success: You have modified SEO regexes!';
-$_['text_list']            = 'SEO Regex List';
-$_['text_add']             = 'Add SEO Regex';
-$_['text_edit']            = 'Edit SEO Regex';
+$_['text_success']      = 'Success: You have modified SEO regexes!';
+$_['text_list']         = 'SEO Regex List';
+$_['text_add']          = 'Add SEO Regex';
+$_['text_edit']         = 'Edit SEO Regex';
 
 // Column
-$_['column_query_match']   = 'Query Match';
-$_['column_query_replace'] = 'Query Replace';
-$_['column_keyword_match']    = 'Path Match';
-$_['column_keyword_replace']  = 'Path Replace';
-$_['column_sort_order']    = 'Sortierreihenfolge';
-$_['column_action']        = 'Aktionen';
+$_['column_key']        = 'Schlüssel';
+$_['column_match']      = 'Match Regex';
+$_['column_replace']    = 'Replace';
+$_['column_keyword']    = 'Keyword Regex';
+$_['column_value']      = 'Wert';
+$_['column_sort_order'] = 'Sortierreihenfolge';
+$_['column_action']     = 'Aktionen';
 
 // Entry
-$_['entry_query_match']    = 'Query Match';
-$_['entry_query_replace']  = 'Query Replace';
-$_['entry_keyword_match']     = 'Path Match';
-$_['entry_keyword_replace']   = 'Path Replace';
-$_['entry_sort_order']     = 'Sortierreihenfolge';
+$_['entry_key']         = 'Schlüssel';
+$_['entry_match']       = 'Match Regex';
+$_['entry_replace']     = 'Replace';
+$_['entry_keyword']     = 'Keyword Regex';
+$_['entry_value']       = 'Wert';
+$_['entry_sort_order']  = 'Sortierreihenfolge';
 
 // Help
-$_['help_sort_order']      = 'Die Sortierreihenfolge der Schlüsselwörter in der URL.';
+$_['help_sort_order']   = 'Die Sortierreihenfolge der Schlüsselwörter in der URL.';
 
 // Error
-$_['error_permission']     = 'Warning: You do not have permission to modify SEO regexes!';
+$_['error_permission']  = 'Warning: You do not have permission to modify SEO regexes!';

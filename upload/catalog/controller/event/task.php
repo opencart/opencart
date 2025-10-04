99176d1a8214aef342f2791c2a52c5c978599f10
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-04 16:11:53 +0800

    Create task.php

---
+++ upload/catalog/controller/event/task.php
@@ -0,0 +1,45 @@
+<?php
+namespace Opencart\Catalog\Controller\Event;
+/**
+ * Class Task
+ *
+ * @package Opencart\Admin\Controller\Event
+ */
+class Task extends \Opencart\System\Engine\Controller {
+	/**
+	 * Index
+	 *
+	 * Starts task queue if not running
+	 *
+	 * model/setting/task.addTask/after
+	 *
+	 * @param string            $route
+	 * @param array<int, mixed> $args
+	 * @param mixed             $output
+	 *
+	 * @return void
+	 */
+	public function index(string &$route, array &$args, &$output): void {
+		$this->load->model('setting/task');
+
+		$task_total = $this->model_setting_task->getTotalTasks(['filter_status' => 'processing']);
+
+		if ($task_total) {
+			return;
+		}
+
+		/*
+		$handle = popen(DIR_APPLICATION . 'index.php start > ' . DIR_LOGS . 'test.log 2>&1', 'r');
+
+		//echo "'$handle'; " . gettype($handle) . "\n";
+
+		$read = fread($handle, 4096);
+
+		echo $read;
+
+		pclose($handle);
+		*/
+
+		exec('php ' . DIR_APPLICATION . 'index.php start > ' . DIR_LOGS . 'test.log 2>&1');
+	}
+}

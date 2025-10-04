80ece20c42a4da4a3d355a3e3939431db089458e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-04 16:03:00 +0800

    Update ssr.php

--- upload/catalog/controller/event/ssr.php
+++ upload/catalog/controller/event/ssr.php
@@ -42,7 +42,7 @@ class Ssr extends \Opencart\System\Engine\Controller {
 		$this->load->model('design/seo_url');
 
 		foreach ($this->request->get as $key => $value) {
-			$seo_url_info = $this->model_design_seo_url->getSeoUrlByKeyValue($key, $value, $this->config->get('config_store_id'), $this->config->get('config_language_id'));
+			$seo_url_info = $this->model_design_seo_url->getSeoUrlByKeyValue($key, $value);
 
 			if ($seo_url_info) {
 				$keywords[] = $seo_url_info;
@@ -61,6 +61,8 @@ class Ssr extends \Opencart\System\Engine\Controller {
 		$base = DIR_OPENCART . 'shop/';
 		$directory = parse_url($this->config->get('config_url'), PHP_URL_HOST) . '/' . implode('/', array_column($keywords, 'keyword')) . '/';
 		$filename = 'index.html';
+
+		echo $directory . "\n";
 
 		oc_directory_create($base . $directory, 0777);
 

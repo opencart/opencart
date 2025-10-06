7678d98caa7af33c122f1e4305a274b1903c6c53
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-07 00:48:50 +0800

    Update notification.php

--- upload/admin/controller/cron/notification.php
+++ upload/admin/controller/cron/notification.php
@@ -12,51 +12,14 @@ class Notification extends \Opencart\System\Engine\Controller {
 	 * @return void
 	 */
 	public function index(): void {
-		if (empty($this->request->cookie['notification'])) {
-			$curl = curl_init();
+		$task_data = [
+			'code'   => 'currency',
+			'action' => 'task/system/notification',
+			'args'   => []
+		];
 
-			// Gets the latest information from opencart.com about news, updates and security.
-			curl_setopt($curl, CURLOPT_URL, OPENCART_SERVER . 'index.php?route=api/notification');
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($curl, CURLOPT_HEADER, false);
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
-			curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+		$this->load->model('setting/task');
 
-			$response = curl_exec($curl);
-
-			$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-			curl_close($curl);
-
-			// Notification
-			if ($status == 200) {
-				$notification = json_decode($response, true);
-			} else {
-				$notification = [];
-			}
-
-			if (isset($notification['notification'])) {
-				$this->load->model('tool/notification');
-
-				foreach ($notification['notifications'] as $result) {
-					$notification_info = $this->model_tool_notification->addNotification($result['notification_id']);
-
-					if (!$notification_info) {
-						$this->model_tool_notification->addNotification($result);
-					}
-				}
-			}
-
-			// Only grab the
-			$option = [
-				'expires'  => time() + 3600 * 24 * 7,
-				'path'     => $this->config->get('session_path'),
-				'secure'   => $this->request->server['HTTPS'],
-				'httponly' => false,
-				'SameSite' => $this->config->get('session_samesite')
-			];
-
-			setcookie('notification', '1', $option);
-		}
+		$this->model_setting_task->addTask($task_data);
 	}
 }

e00f21a8060adadb71da151d534f14d6385b67e2
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-15 01:54:14 +0800

    Update banner.php

--- upload/admin/language/en-gb/task/catalog/banner.php
+++ upload/admin/language/en-gb/task/catalog/banner.php
@@ -2,10 +2,13 @@
 // Text
 $_['text_task']       = 'Generating banner task list!';
 $_['text_list']       = 'Generating %s %s banner list!';
+$_['text_skip']       = 'Skipping %s %s %s banner information!';
 $_['text_clear']      = 'Banner data cleared!';
 
 // Error
 $_['error_required']  = 'Error: %s required!';
+$_['error_store']     = 'Error: Store could not be found!';
 $_['error_language']  = 'Error: Language could not be found!';
+$_['error_banner']    = 'Error: Banner does not exist!';
 $_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
 $_['error_file']      = 'Error: File %s could not be written!';

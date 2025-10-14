12ddd64c4c6da21010fa3bb1fa8f9f61b6a18538
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-15 02:51:40 +0800

    Update source file translation.php

--- upload/admin/language/en-gb/task/catalog/translation.php
+++ upload/admin/language/en-gb/task/catalog/translation.php
@@ -1,11 +1,12 @@
 <?php
 // Text
 $_['text_task']       = 'Generating translation task list!';
-$_['text_list']       = 'Generating %s %s %s translation list!';
+$_['text_write']      = 'Generating %s %s %s translation data!';
 $_['text_clear']      = 'Translation data cleared!';
 
 // Error
 $_['error_required']  = 'Error: %s required!';
-$_['error_language']  = 'Warning: Language could not be found!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';
+$_['error_store']     = 'Error: Store could not be found!';
+$_['error_language']  = 'Error: Language could not be found!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

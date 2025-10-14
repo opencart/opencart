5b7d64ff2f3d18a3896da2cb6a5ebd298c3696cd
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-15 02:51:35 +0800

    Update source file currency.php

--- upload/admin/language/en-gb/task/catalog/currency.php
+++ upload/admin/language/en-gb/task/catalog/currency.php
@@ -1,11 +1,12 @@
 <?php
 // Text
 $_['text_task']       = 'Generating currency task list!';
-$_['text_list']       = 'Generating %s %s currency list!';
+$_['text_list']       = 'Generating %s %s currency data!';
 $_['text_clear']      = 'Currency data cleared!';
 
 // Error
-$_['error_store']     = 'Warning: Store could not be found!';
-$_['error_language']  = 'Warning: Language could not be found!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';
+$_['error_required']  = 'Error: %s required!';
+$_['error_store']     = 'Error: Store could not be found!';
+$_['error_language']  = 'Error: Language could not be found!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

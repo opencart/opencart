f42b9b77386eb8dbdf1aebfbc4efc7b1c6de8496
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-15 01:52:07 +0800

    New translations language.php (German)

--- de/admin/task/admin/language.php
+++ de/admin/task/admin/language.php
@@ -4,6 +4,6 @@ $_['text_success']    = 'Generated language list!';
 $_['text_clear']      = 'Language data cleared!';
 
 // Error
-$_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';
+$_['error_language']  = 'Error: Language could not be found!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

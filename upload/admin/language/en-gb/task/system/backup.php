d401a890e5fb5adf1d5f9228db259cb9b6c72d6e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-15 02:51:42 +0800

    Update source file backup.php

--- upload/admin/language/en-gb/task/system/backup.php
+++ upload/admin/language/en-gb/task/system/backup.php
@@ -4,7 +4,7 @@ $_['text_success']   = 'Preparing database backup!';
 $_['text_backup']    = 'Backing up table %s %s';
 
 // Error
-$_['error_required'] = 'Warning: %s required!';
-$_['error_table']    = 'Warning: Table %s is not allowed to be backed up!';
-$_['error_filename'] = 'Filename must be between 5 and 128 characters!';
-$_['error_file']     = 'Warning: Warning: File could not be found!';
+$_['error_required'] = 'Error: %s required!';
+$_['error_table']    = 'Error: Table %s is not allowed to be backed up!';
+$_['error_filename'] = 'Error: Filename must be between 5 and 128 characters!';
+$_['error_file']     = 'Error: File could not be found!';

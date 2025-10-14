6ed945a3e2c5303ffc33dd96b7933706d993d85e
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-15 02:51:25 +0800

    Update source file return_action.php

--- upload/admin/language/en-gb/task/admin/return_action.php
+++ upload/admin/language/en-gb/task/admin/return_action.php
@@ -1,11 +1,11 @@
 <?php
 // Text
 $_['text_task']       = 'Generating return action task list!';
-$_['text_list']       = 'Generating %s return action list!';
+$_['text_list']       = 'Generating %s return action data!';
 $_['text_clear']      = 'Return Action data cleared!';
 
 // Error
 $_['error_required']  = 'Error: %s required!';
-$_['error_language']  = 'Warning: Language could not be found!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';
+$_['error_language']  = 'Error: Language could not be found!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

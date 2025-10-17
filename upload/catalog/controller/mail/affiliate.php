da92ef80701d4830da3a24c7f0c8e1751d7dd512
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-17 23:22:12 +0800

    Update affiliate.php

--- upload/catalog/controller/mail/affiliate.php
+++ upload/catalog/controller/mail/affiliate.php
@@ -106,7 +106,11 @@ class Affiliate extends \Opencart\System\Engine\Controller {
 		$tos = explode(',', (string)$this->config->get('config_mail_alert_email'));
 
 		foreach ($tos as $to) {
-			$emails[] = trim($to);
+			$to = trim($to);
+
+			if (oc_validate_email($to)) {
+				$emails[] = $to;
+			}
 		}
 
 		$this->load->model('setting/task');

b2a5273f835a5d2c8b1a18af4a9bb34f8339ec74
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-20 21:51:41 +0800

    Update developer.php

--- upload/admin/controller/common/developer.php
+++ upload/admin/controller/common/developer.php
@@ -127,7 +127,7 @@ class Developer extends \Opencart\System\Engine\Controller {
 			$json['error'] = $this->language->get('error_permission');
 		}
 
-		$file = DIR_OPENCART . 'static/html/stylesheet/stylesheet.scss';
+		$file = DIR_CATALOG . 'view/stylesheet/stylesheet.scss';
 
 		if (!is_file($file)) {
 			$json['error'] = $this->language->get('error_stylesheet');

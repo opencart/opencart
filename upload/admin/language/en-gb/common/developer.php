466584ae957e1e6ca79e6b461f8e89b54cfaa3f6
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-10-21 09:58:57 +0800

    Update source file developer.php

--- upload/admin/language/en-gb/common/developer.php
+++ upload/admin/language/en-gb/common/developer.php
@@ -6,8 +6,8 @@ $_['heading_title']             = 'Developer Options';
 $_['text_html_success']         = 'Success: You have cleared the HTML files!';
 $_['text_cache_success']        = 'Success: You have cleared the cache!';
 $_['text_theme_success']        = 'Success: You have cleared the theme cache!';
-$_['text_sass_catalog_success'] = 'Success: You have modified the catalog SASS!';
-$_['text_sass_admin_success']   = 'Success: You have modified the admin SASS!';
+$_['text_sass_catalog_success'] = 'Success: Generating the catalog SASS added to the task queue!';
+$_['text_sass_admin_success']   = 'Success: Generating the admin SASS added to the task queue!';
 $_['text_vendor_success']       = 'Success: You have modified the vendor cache!';
 $_['text_html']                 = 'HTML';
 $_['text_cache']                = 'Cache';
@@ -26,3 +26,4 @@ $_['help_vendor']               = 'Rebuilds the included vendor file.';
 
 // Error
 $_['error_permission']          = 'Warning: You do not have permission to modify developer settings!';
+$_['error_file']                = 'Error: File %s could not be found!';

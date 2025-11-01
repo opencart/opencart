adedbe7d605d10e30ad6e9eb4d75352557df1e8b
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-11-01 23:10:23 +0800

    Update customer_reward.php

--- upload/extension/opencart/admin/controller/report/customer_reward.php
+++ upload/extension/opencart/admin/controller/report/customer_reward.php
@@ -171,12 +171,10 @@ class CustomerReward extends \Opencart\System\Engine\Controller {
 		$url = http_build_query(array_diff_key($this->request->get, array_flip($remove)));
 
 		// Pagination
-		$data['pagination'] = $this->load->controller('common/pagination', [
-			'total' => $customer_total,
-			'page'  => $page,
-			'limit' => $this->config->get('config_pagination'),
-			'url'   => $this->url->link('extension/opencart/report/customer_reward.list', 'user_token=' . $this->session->data['user_token'] . '&code=customer_reward' . $url . '&page=' . $page)
-		]);
+		$data['total'] = $customer_total;
+		$data['page'] = $page;
+		$data['limit'] = $this->config->get('config_pagination_admin');
+		$data['pagination'] = $this->url->link('extension/opencart/report/customer_reward.list', 'user_token=' . $this->session->data['user_token'] . '&code=customer_reward' . $url . '&page={page}');
 
 		$data['results'] = sprintf($this->language->get('text_pagination'), ($customer_total) ? (($page - 1) * $this->config->get('config_pagination')) + 1 : 0, ((($page - 1) * $this->config->get('config_pagination')) > ($customer_total - $this->config->get('config_pagination'))) ? $customer_total : ((($page - 1) * $this->config->get('config_pagination')) + $this->config->get('config_pagination')), $customer_total, ceil($customer_total / $this->config->get('config_pagination')));
 

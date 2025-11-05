b183b07faaaa98e836b7a343825a46b279f7f073
mhcwebdesign <github@mhccorp.com>
2025-11-05 15:05:26 +0000

    [3.0.x.x] Removal of Squareup
    The built-in squareup payment extension was based on the usage of an old discontinued square API and won't work anymore.

--- upload/system/library/squareup/cron.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$squareup_dir = dirname(__FILE__);
-
-require_once $squareup_dir . DIRECTORY_SEPARATOR . 'cron_functions.php';
-
-if ($index = squareup_init($squareup_dir)) {
-    require_once $index;
-}

03c78a949de3e1228312fb7bc9b68fe81626500a
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-11-18 02:57:53 +0800

    Create local.js

---
+++ upload/assets/framework/library/local.js
@@ -0,0 +1,21 @@
+export default class local {
+    get(key) {
+        return localStorage.getItem(key);
+    }
+
+    set(key, value) {
+        localStorage.setItem(key, value);
+    }
+
+    has(key) {
+        return localStorage.getItem(key) !== null;
+    }
+
+    remove(key) {
+        localStorage.removeItem(key);
+    }
+
+    clear() {
+        Storage.clear();
+    }
+}

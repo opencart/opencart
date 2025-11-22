8319d1839e2226fd8f596f9c12f1eee3ff94306f
OSWorX <info@osworx.net>
2025-11-22 18:25:58 +0100

    [php 8.5 Compatibility] curl.php
    Removed deprecated curl_close()

--- upload/system/library/curl.php
+++ upload/system/library/curl.php
@@ -67,8 +67,6 @@ class Curl {
 
 		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-		curl_close($curl);
-
 		if ($status == 200) {
 			$response_info = json_decode($response, true);
 		} else {

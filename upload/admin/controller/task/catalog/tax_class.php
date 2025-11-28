695c99b23e180d73348ce452dcd651eee450fb24
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-11-29 07:22:17 +0800

    Update tax_class.php

--- upload/admin/controller/task/catalog/tax_class.php
+++ upload/admin/controller/task/catalog/tax_class.php
@@ -1,1 +1,239 @@
 <?php
+namespace Opencart\Admin\Controller\Task\Catalog;
+/**
+ * Class Tax Class
+ *
+ * @package Opencart\Admin\Controller\Task\Catalog
+ */
+class TaxClass extends \Opencart\System\Engine\Controller {
+	/**
+	 * Index
+	 *
+	 * Generate tax class task list.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function index(array $args = []): array {
+		$this->load->language('task/catalog/tax_class');
+
+		$this->load->model('setting/task');
+
+		$this->load->model('setting/store');
+
+		$stores = $this->model_setting_store->getStores();
+
+		$this->load->model('localisation/language');
+
+		$languages = $this->model_localisation_language->getLanguages();
+
+		foreach ($stores as $store) {
+			foreach ($languages as $language) {
+				$task_data = [
+					'code'   => 'tax_class',
+					'action' => 'task/catalog/tax_class.list',
+					'args'   => [
+						'store_id'    => $store['store_id'],
+						'language_id' => $language['language_id']
+					]
+				];
+
+				$this->model_setting_task->addTask($task_data);
+			}
+		}
+
+		return ['success' => $this->language->get('text_task')];
+	}
+
+	/**
+	 * List
+	 *
+	 * Generate JSON customer group list file.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function list(array $args = []): array {
+		$this->load->language('task/catalog/tax_class');
+
+		$required = [
+			'store_id',
+			'language_id'
+		];
+
+		foreach ($required as $value) {
+			if (!array_key_exists($value, $args)) {
+				return ['error' => sprintf($this->language->get('error_required'), $value)];
+			}
+		}
+
+		// Store
+		$this->load->model('setting/store');
+
+		$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
+
+		if (!$store_info) {
+			return ['error' => $this->language->get('error_store')];
+		}
+
+		// Language
+		$this->load->model('localisation/language');
+
+		$language_info = $this->model_localisation_language->getLanguage((int)$args['language_id']);
+
+		if (!$language_info) {
+			return ['error' => $this->language->get('error_language')];
+		}
+
+		$this->load->model('setting/task');
+
+		$this->load->model('localisation/tax_class');
+
+		$tax_classes = $this->model_localisation_tax_class->getTaxClasses();
+
+		foreach ($tax_classes as $tax_class) {
+			// Add a task for generating the country info data
+			$task_data = [
+				'code'   => 'tax_class',
+				'action' => 'task/catalog/tax_class.info',
+				'args'   => [
+					'tax_class_id' => $tax_class['tax_class_id'],
+					'store_id'     => $store_info['store_id'],
+					'language_id'  => $language_info['language_id']
+				]
+			];
+
+			$this->model_setting_task->addTask($task_data);
+		}
+
+		$base = DIR_CATALOG . 'view/data/';
+		$directory = parse_url($store_info['url'], PHP_URL_HOST) . '/' . $language_info['code'] . '/localisation/';
+		$filename = 'tax_class.json';
+
+		if (!oc_directory_create($base . $directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		}
+
+		if (!file_put_contents($base . $directory . $filename, json_encode($tax_classes))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		}
+
+		return ['success' => sprintf($this->language->get('text_list'), $language_info['name'])];
+	}
+
+	/**
+	 * Info
+	 *
+	 * Generate tax class information.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function info(array $args = []): array {
+		$this->load->language('task/catalog/tax_class');
+
+		$required = [
+			'tax_class_id',
+			'store_id'
+		];
+
+		foreach ($required as $value) {
+			if (!array_key_exists($value, $args)) {
+				return ['error' => sprintf($this->language->get('error_required'), $value)];
+			}
+		}
+
+		// Store
+		$this->load->model('setting/store');
+
+		$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
+
+		if (!$store_info) {
+			return ['error' => $this->language->get('error_store')];
+		}
+
+		// Language
+		$this->load->model('localisation/language');
+
+		$language_info = $this->model_localisation_language->getLanguage((int)$args['language_id']);
+
+		if (!$language_info) {
+			return ['error' => $this->language->get('error_language')];
+		}
+
+		// Customer Group
+		$this->load->model('localisation/tax_class');
+
+		$customer_group_info = $this->model_customer_customer_group->getCustomerGroup((int)$args['customer_group_id']);
+
+		if (!$customer_group_info) {
+			return ['error' => $this->language->get('error_customer_group')];
+		}
+
+		$description_info = $this->model_customer_customer_group->getDescription($customer_group_info['customer_group_id'], $language_info['language_id']);
+
+		if (!$description_info) {
+			return ['error' => $this->language->get('error_description')];
+		}
+
+		// Custom Fields
+		$filter_data = [
+			'filter_customer_group_id' => $customer_group_info['customer_group_id'],
+			'filter_language_id'       => $language_info['language_id']
+		];
+
+		$this->load->model('localisation/tax_rate');
+
+		$custom_fields = $this->model_localisation_tax_rate->getTaxRates($filter_data);
+
+		$base = DIR_CATALOG . 'view/data/';
+		$directory = parse_url($store_info['url'], PHP_URL_HOST) . '/' . $language_info['code'] . '/customer/';
+		$filename = 'customer_group-' . $customer_group_info['customer_group_id'] . '.json';
+
+		if (!oc_directory_create($base . $directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		}
+
+		if (!file_put_contents($base . $directory . $filename, json_encode($customer_group_info + $description_info + ['custom_field' => $custom_fields]))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		}
+
+		return ['success' => sprintf($this->language->get('text_info'), $language_info['name'], $customer_group_info['name'])];
+	}
+
+	/**
+	 * Clear
+	 *
+	 * Delete generated JSON country files.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function clear(array $args = []): array {
+		$this->load->language('task/catalog/tax_class');
+
+		$this->load->model('setting/store');
+
+		$stores = $this->model_setting_store->getStores();
+
+		$this->load->model('localisation/language');
+
+		$languages = $this->model_localisation_language->getLanguages();
+
+		foreach ($stores as $store) {
+			foreach ($languages as $language) {
+				$file = DIR_CATALOG . 'view/data/' . parse_url($store['url'], PHP_URL_HOST) . '/' . $language['code'] . '/customer/customer_group.json';
+
+				if (is_file($file)) {
+					unlink($file);
+				}
+			}
+		}
+
+		return ['success' => $this->language->get('text_clear')];
+	}
+}

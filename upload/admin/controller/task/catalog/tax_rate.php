9e5633d09dfa88427e1e987f4960926ee61138c1
Daniel Kerr <danielkerr@users.noreply.github.com>
2025-12-03 20:47:54 +0800

    Create tax_rate.php

---
+++ upload/admin/controller/task/catalog/tax_rate.php
@@ -0,0 +1,106 @@
+<?php
+namespace Opencart\Admin\Controller\Task\Catalog;
+/**
+ * Class Tax Rate
+ *
+ * @package Opencart\Admin\Controller\Task\Catalog
+ */
+class TaxRate extends \Opencart\System\Engine\Controller {
+	/**
+	 * Index
+	 *
+	 * Generate tax rate task list.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function index(array $args = []): array {
+		$this->load->language('task/catalog/tax_rate');
+
+		$this->load->model('setting/task');
+
+		// Geo Zones
+		$this->load->model('localisation/geo_zone');
+
+		$geo_zones = $this->model_localisation_geo_zone->getGeoZones();
+
+		foreach ($geo_zones as $geo_zone) {
+			$task_data = [
+				'code'   => 'tax_rate',
+				'action' => 'task/catalog/tax_rate.info',
+				'args'   => ['geo_zone_id' => $geo_zone['geo_zone_id']]
+			];
+
+			$this->model_setting_task->addTask($task_data);
+		}
+
+		return ['success' => $this->language->get('text_task')];
+	}
+
+	/**
+	 * Info
+	 *
+	 * Generate tax rate information.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function info(array $args = []): array {
+		$this->load->language('task/catalog/tax_rate');
+
+		if (!array_key_exists('geo_zone_id', $args)) {
+			return ['error' => $this->language->get('error_required')];
+		}
+
+		$this->load->model('localisation/geo_zone');
+
+		$geo_zone_info = $this->model_localisation_geo_zone->getGeoZone($args['geo_zone_id']);
+
+		if (!$geo_zone_info) {
+			return ['error' => $this->language->get('error_geo_zone')];
+		}
+
+		$this->load->model('localisation/tax_rate');
+
+		$tax_rates = $this->model_localisation_tax_rate->getTaxRatesByGeoZoneId($geo_zone_info['geo_zone_id']);
+
+		$directory = DIR_CATALOG . 'view/data/localisation/';
+		$filename = 'tax_rate-' . $geo_zone_info['geo_zone_id'] . '.json';
+
+		if (!oc_directory_create($directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		}
+
+		if (!file_put_contents($directory . $filename, json_encode($tax_rates))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		}
+
+		return ['success' => sprintf($this->language->get('text_info'), $geo_zone_info['name'])];
+	}
+
+	/**
+	 * Clear
+	 *
+	 * Delete generated JSON tax rate files.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function clear(array $args = []): array {
+		$this->load->language('task/catalog/tax_rate');
+
+		$files = oc_directory_read( DIR_CATALOG . 'view/data/localisation/', false, '/tax_rate\-.+\.json$/');
+
+		foreach ($files as $file) {
+			if (is_file($file)) {
+				unlink($file);
+			}
+		}
+
+		return ['success' => $this->language->get('text_clear')];
+	}
+}
+

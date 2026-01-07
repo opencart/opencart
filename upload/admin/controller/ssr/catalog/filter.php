2929e74ea1520cdd15219af3e9f75758599dea68
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-07 22:22:24 +0800

    Update filter.php

--- upload/admin/controller/ssr/catalog/filter.php
+++ upload/admin/controller/ssr/catalog/filter.php
@@ -6,15 +6,39 @@ namespace Opencart\admin\controller\ssr\catalog;
  * @package Opencart\Admin\Controller\Event
  */
 class Filter extends \Opencart\System\Engine\Controller {
-	public function add(string &$route, array &$args, &$output): void {
+	public function editFilter(string &$route, array &$args, &$output): void {
+		$this->load->model('catalog/product');
 
+		$results = $this->model_catalog_product->getProductsByFilterId($args[0]);
+
+		$this->load->model('setting/task');
+
+		foreach ($results as $result) {
+			$task_data = [
+				'code'   => 'product.info.' . $result['product_id'],
+				'action' => 'task/catalog/product.info',
+				'args'   => ['product_id' => $result['product_id']]
+			];
+
+			$this->model_setting_task->addTask($task_data);
+		}
 	}
 
-	public function edit(string &$route, array &$args, &$output): void {
+	public function deleteFilter(string &$route, array &$args, &$output): void {
+		$this->load->model('catalog/product');
 
-	}
+		$results = $this->model_catalog_product->getProductsByFilterId($args[0]);
 
-	public function delete(string &$route, array &$args, &$output): void {
+		$this->load->model('setting/task');
 
+		foreach ($results as $result) {
+			$task_data = [
+				'code'   => 'product.info.' . $result['product_id'],
+				'action' => 'task/catalog/product.info',
+				'args'   => ['product_id' => $result['product_id']]
+			];
+
+			$this->model_setting_task->addTask($task_data);
+		}
 	}
 }

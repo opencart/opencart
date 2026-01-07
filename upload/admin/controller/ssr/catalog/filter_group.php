d30908341039ccc8306bf4f2f5714b8cc1d72c83
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-07 22:22:27 +0800

    Update filter_group.php

--- upload/admin/controller/ssr/catalog/filter_group.php
+++ upload/admin/controller/ssr/catalog/filter_group.php
@@ -3,40 +3,38 @@ namespace Opencart\admin\controller\ssr\catalog;
 /**
  * Class Filter Group
  *
+ * Called using admin/model/catalog/filter_group/addFilter/after
+ * Called using admin/model/catalog/filter_group/editFilter/after
+ * Called using admin/model/catalog/filter_group/catalog/deleteFilter/after
+ *
  * @package Opencart\Admin\Controller\Event
  */
 class FilterGroup extends \Opencart\System\Engine\Controller {
 	/**
-	 * Index
+	 * addFilter
 	 *
 	 * Adds task to generate new filter group list
-	 *
-	 * Called using admin/model/catalog/filter_group/addFilter/after
-	 * Called using admin/model/catalog/filter_group/editFilter/after
-	 * Called using admin/model/catalog/filter_group/catalog/deleteFilter/after
 	 *
 	 * @param string                $route
 	 * @param array<string, string> $args
 	 *
 	 * @return void
 	 */
-	public function index(string &$route, array &$args, &$output): void {
-		$files = oc_directory_read(DIR_OPENCART . 'view/html/');
+	public function editFilterGroup(string &$route, array &$args, &$output): void {
+		$this->load->model('catalog/product');
 
-		foreach ($files as $file) {
-			oc_directory_delete($file);
+		$results = $this->model_catalog_product->getProductsByFilterGroupId($args[0]);
+
+		$this->load->model('setting/task');
+
+		foreach ($results as $result) {
+			$task_data = [
+				'code'   => 'product.info.' . $result['product_id'],
+				'action' => 'task/catalog/product.info',
+				'args'   => ['product_id' => $result['product_id']]
+			];
+
+			$this->model_setting_task->addTask($task_data);
 		}
-	}
-
-	public function add(string &$route, array &$args, &$output): void {
-
-	}
-
-	public function edit(string &$route, array &$args, &$output): void {
-
-	}
-
-	public function delete(string &$route, array &$args, &$output): void {
-
 	}
 }

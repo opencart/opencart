92b94ce4a1533d5ab8c5ec5bc0c47cebc6d20290
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-09 03:30:01 +0800

    New translations category.php (German)

--- de/admin/task/catalog/category.php
+++ de/admin/task/catalog/category.php
@@ -1,10 +1,1 @@
 <?php
-// Text
-$_['text_success']    = 'Starting generating country files!';
-$_['text_list']       = 'Generating %s country list files!';
-$_['text_info']       = 'Generating %s %s country information file!';
-
-// Error
-$_['error_language']  = 'Achtung: Sprache konnte nicht gefunden werden!';
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';

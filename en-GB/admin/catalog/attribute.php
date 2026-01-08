84e190cfa4f9b844c3e86d8901191d4c00fa523a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-09 04:56:04 +0800

    New translations attribute.php (English, United Kingdom)

--- en-GB/admin/catalog/attribute.php
+++ en-GB/admin/catalog/attribute.php
@@ -7,21 +7,22 @@ $_['text_success']           = 'Modify';
 $_['text_list']              = 'listing';
 $_['text_add']               = 'Attribute';
 $_['text_edit']              = 'Attribute';
+$_['text_attribute_group']   = 'Attribute Group';
+$_['text_attribute']         = 'Attributes';
 
 // Column
-$_['column_name']            = 'Sergioavila97sbd ';
-$_['column_attribute_group'] = 'Attribute Group';
+$_['column_attribute_group'] = 'Attribute Group Name';
 $_['column_sort_order']      = 'Sort Order';
 $_['column_action']          = 'Action';
 
 // Entry
-$_['entry_name']             = 'Sergioavila97sbd ';
 $_['entry_attribute_group']  = 'Attribute Group';
+$_['entry_name']             = 'Sergioavila97sbd ';
 $_['entry_sort_order']       = 'Sort Order';
 
 // Error
 $_['error_warning']          = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']       = 'Warning: You do not have permission to modify attributes!';
-$_['error_attribute_group']  = 'Attribute Group required!';
+$_['error_attribute_group']  = 'Attribute Group Name must be between 1 and 64 characters!';
 $_['error_name']             = 'Attribute Name must be between 1 and 64 characters!';
 $_['error_product']          = 'Warning: This attribute cannot be deleted as it is currently assigned to %s products!';

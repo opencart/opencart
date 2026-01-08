2175355c4d7a5c0a3a2c5f3f6cfd682a8df18f39
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-09 03:31:46 +0800

    New translations manufacturer.php (English, United Kingdom)

--- en-GB/admin/task/catalog/manufacturer.php
+++ en-GB/admin/task/catalog/manufacturer.php
@@ -1,9 +1,1 @@
 <?php
-// Text
-$_['text_task']       = 'Success: You have modified manufacturers!';
-$_['text_list']       = 'You have rendered the manufacturer list';
-$_['text_next']       = 'You have rendered %s to %s of %s manufacturers';
-
-// Error
-$_['error_directory'] = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Warning: File %s could not be written!';

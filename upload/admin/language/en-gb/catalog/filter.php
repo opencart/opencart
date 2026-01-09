6952f0a067d5e54c5a16b7a9f662398135e86abe
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-09 23:38:10 +0800

    Update source file filter.php

--- upload/admin/language/en-gb/catalog/filter.php
+++ upload/admin/language/en-gb/catalog/filter.php
@@ -7,20 +7,22 @@ $_['text_success']        = 'Success: You have modified filters!';
 $_['text_list']           = 'Filter List';
 $_['text_add']            = 'Add Filter';
 $_['text_edit']           = 'Edit Filter';
+$_['text_filter_group']   = 'Filter Group';
+$_['text_filter']         = 'Filters';
 
 // Column
-$_['column_name']         = 'Filter Name';
 $_['column_filter_group'] = 'Filter Group';
 $_['column_sort_order']   = 'Sort Order';
 $_['column_action']       = 'Action';
 
 // Entry
+$_['entry_filter_group']  = 'Filter Group Name';
 $_['entry_name']          = 'Filter Name';
-$_['entry_filter_group']  = 'Filter Group';
 $_['entry_sort_order']    = 'Sort Order';
 
 // Error
 $_['error_warning']       = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']    = 'Warning: You do not have permission to modify filters!';
+$_['error_filter_group']  = 'Filter Group Name must be between 1 and 64 characters!';
 $_['error_name']          = 'Filter Name must be between 1 and 64 characters!';
-$_['error_filter_group']  = 'Filter Group required!';
+$_['error_filter']        = 'Warning: This filter group cannot be deleted as it is currently assigned to %s filters!';

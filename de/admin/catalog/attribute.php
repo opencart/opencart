c25b112eb33d8ad0c608584d309a4db5f543f2da
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-09 21:59:27 +0800

    New translations attribute.php (German)

--- de/admin/catalog/attribute.php
+++ de/admin/catalog/attribute.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']          = 'Eigenschaften';
+$_['heading_title']         = 'Eigenschaften';
 
 // Text
-$_['text_success']           = 'Erfolgreich: Sie haben Eigenschaften geändert!';
-$_['text_list']              = 'Eigenschaften Liste';
-$_['text_add']               = 'Eigenschaft hinzufügen';
-$_['text_edit']              = 'Eigenschaft bearbeiten';
-$_['text_attribute_group']   = 'Attribute Group';
-$_['text_attribute']         = 'Eigenschaften';
+$_['text_success']          = 'Erfolgreich: Sie haben Eigenschaften geändert!';
+$_['text_list']             = 'Eigenschaften Liste';
+$_['text_add']              = 'Eigenschaft hinzufügen';
+$_['text_edit']             = 'Eigenschaft bearbeiten';
+$_['text_attribute_group']  = 'Attribute Group';
+$_['text_attribute']        = 'Eigenschaften';
 
 // Column
-$_['column_attribute_group'] = 'Eigenschaften-Gruppe';
-$_['column_sort_order']      = 'Sortierreihenfolge';
-$_['column_action']          = 'Aktionen';
+$_['column_name']           = 'Attribute Group';
+$_['column_sort_order']     = 'Sortierreihenfolge';
+$_['column_action']         = 'Aktionen';
 
 // Entry
-$_['entry_attribute_group']  = 'Eigenschaften-Gruppe';
-$_['entry_name']             = 'Eigenschaft Name';
-$_['entry_sort_order']       = 'Sortierreihenfolge';
+$_['entry_attribute_group'] = 'Eigenschaften-Gruppe';
+$_['entry_name']            = 'Eigenschaft Name';
+$_['entry_sort_order']      = 'Sortierreihenfolge';
 
 // Error
-$_['error_warning']          = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
-$_['error_permission']       = 'Warnung: Sie sind nicht berechtigt, Eigenschaften zu ändern!';
-$_['error_attribute_group']  = 'Der Eigenschaften-Gruppe Name muss zwischen 1 und 64 Zeichen lang sein!';
-$_['error_name']             = 'Der Eigenschaft Name muss zwischen 1 und 64 Zeichen lang sein!';
-$_['error_product']          = 'Warnung: Diese Eigenschaft kann nicht gelöscht werden, da sie aktuell %s Produkten zugewiesen ist!';
+$_['error_warning']         = 'Warnung: Bitte prüfen Sie das Formular sorgfältig auf Fehler!';
+$_['error_permission']      = 'Warnung: Sie sind nicht berechtigt, Eigenschaften zu ändern!';
+$_['error_attribute_group'] = 'Der Eigenschaften-Gruppe Name muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_name']            = 'Der Eigenschaft Name muss zwischen 1 und 64 Zeichen lang sein!';
+$_['error_product']         = 'Warnung: Diese Eigenschaften-Gruppe kann nicht gelöscht werden, da sie derzeit %s Produkten zugewiesen ist!';

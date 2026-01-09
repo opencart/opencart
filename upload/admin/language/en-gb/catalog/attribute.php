e222f2800ae5c3a8660d11258fee81b7974b0c4e
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-09 23:38:09 +0800

    Update source file attribute.php

--- upload/admin/language/en-gb/catalog/attribute.php
+++ upload/admin/language/en-gb/catalog/attribute.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']          = 'Attributes';
+$_['heading_title']         = 'Attributes';
 
 // Text
-$_['text_success']           = 'Success: You have modified attributes!';
-$_['text_list']              = 'Attribute List';
-$_['text_add']               = 'Add Attribute';
-$_['text_edit']              = 'Edit Attribute';
-$_['text_attribute_group']   = 'Attribute Group';
-$_['text_attribute']         = 'Attributes';
+$_['text_success']          = 'Success: You have modified attributes!';
+$_['text_list']             = 'Attribute List';
+$_['text_add']              = 'Add Attribute';
+$_['text_edit']             = 'Edit Attribute';
+$_['text_attribute_group']  = 'Attribute Group';
+$_['text_attribute']        = 'Attributes';
 
 // Column
-$_['column_attribute_group'] = 'Attribute Group Name';
-$_['column_sort_order']      = 'Sort Order';
-$_['column_action']          = 'Action';
+$_['column_name']           = 'Attribute Group';
+$_['column_sort_order']     = 'Sort Order';
+$_['column_action']         = 'Action';
 
 // Entry
-$_['entry_attribute_group']  = 'Attribute Group';
-$_['entry_name']             = 'Attribute Name';
-$_['entry_sort_order']       = 'Sort Order';
+$_['entry_attribute_group'] = 'Attribute Group Name';
+$_['entry_name']            = 'Attribute Name';
+$_['entry_sort_order']      = 'Sort Order';
 
 // Error
-$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']       = 'Warning: You do not have permission to modify attributes!';
-$_['error_attribute_group']  = 'Attribute Group Name must be between 1 and 64 characters!';
-$_['error_name']             = 'Attribute Name must be between 1 and 64 characters!';
-$_['error_product']          = 'Warning: This attribute cannot be deleted as it is currently assigned to %s products!';
+$_['error_warning']         = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']      = 'Warning: You do not have permission to modify attributes!';
+$_['error_attribute_group'] = 'Attribute Group Name must be between 1 and 64 characters!';
+$_['error_name']            = 'Attribute Name must be between 1 and 64 characters!';
+$_['error_product']         = 'Warning: This attribute group cannot be deleted as it is currently assigned to %s products!';

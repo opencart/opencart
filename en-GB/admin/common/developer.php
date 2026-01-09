1ad0c1c60222311611c5019e7b6a4c3679aa3f0b
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-10 04:19:32 +0800

    New translations developer.php (English, United Kingdom)

--- en-GB/admin/common/developer.php
+++ en-GB/admin/common/developer.php
@@ -6,13 +6,13 @@ $_['heading_title']             = 'Developer Options';
 $_['text_html_success']         = 'Success: You have cleared the HTML files!';
 $_['text_cache_success']        = 'Success: You have cleared the cache!';
 $_['text_theme_success']        = 'Success: You have cleared the theme cache!';
-$_['text_sass_catalog_success'] = 'Success: Generating the catalog SASS added to the task queue!';
+$_['text_sass_catalog_success'] = 'Success: Generating the catalogue SASS added to the task queue!';
 $_['text_sass_admin_success']   = 'Success: Generating the admin SASS added to the task queue!';
 $_['text_vendor_success']       = 'Success: You have modified the vendor cache!';
 $_['text_html']                 = 'HTML';
 $_['text_cache']                = 'Cache';
 $_['text_theme']                = 'Theme';
-$_['text_sass_catalog']         = 'Catalog SASS';
+$_['text_sass_catalog']         = 'Catalogue SASS';
 $_['text_sass_admin']           = 'Admin SASS';
 $_['text_vendor']               = 'Vendor';
 
@@ -20,7 +20,7 @@ $_['text_vendor']               = 'Vendor';
 $_['help_html']                 = 'Clears all stored HTML files.';
 $_['help_cache']                = 'Clears all stored cache data files.';
 $_['help_theme']                = 'Clears all stored theme data files.';
-$_['help_sass_catalog']         = 'Rebuilds the stored catalog SASS files.';
+$_['help_sass_catalog']         = 'Rebuilds the stored catalogue SASS files.';
 $_['help_sass_admin']           = 'Rebuilds the stored admin SASS files.';
 $_['help_vendor']               = 'Rebuilds the included vendor file.';
 

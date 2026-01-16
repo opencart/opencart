3b8ddde8a521d972b85ad0c3a96e208de20742d6
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-17 03:59:18 +0800

    Update checkbox.js

--- upload/assets/framework/input/checkbox.js
+++ upload/assets/framework/input/checkbox.js
@@ -38,11 +38,11 @@ class XCheckbox extends WebComponent {
 
     render() {
         return `<div class="row mb-3${custom_field.required ? 'required' : ''}">
-        <label class="col-sm-2 col-form-label">{{custom_field.name}}</label>
+        <label class="col-sm-2 col-form-label">${custom_field.name}</label>
          <div class="col-sm-10">
             <div id="' +  + '">
 
-        for (let custom_field_value of custom_field.custom_field_value) {
+        ${for (let custom_field_value of custom_field.custom_field_value) {
         <div class"form-check">';
             <input type="checkbox" name="custom_field[' + custom_field.custom_field_id + '][]" value="{{ custom_field_value.custom_field_value_id }}" id="input-custom-value-{{ custom_field_value.custom_field_value_id }}" class="form-check-input"{% if address_custom_field[custom_field.custom_field_id] and custom_field_value.custom_field_value_id in address_custom_field[custom_field.custom_field_id] %} checked{% endif %}/>';
             <label for="input-custom-value-{{ custom_field_value.custom_field_value_id }}" className="form-check-label">{{custom_field_value.name}}</label>';

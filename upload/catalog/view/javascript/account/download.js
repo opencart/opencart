d0b5ce4022c88a887f15a5b798a8af1e8f43846c
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-17 03:59:24 +0800

    Update download.js

--- upload/catalog/view/javascript/account/download.js
+++ upload/catalog/view/javascript/account/download.js
@@ -1,9 +1,16 @@
 import { WebComponent } from '../component.js';
+import { loader } from '../index.js';
 
-class XAccount extends WebComponent {
+const language = loader.language('account/download');
+
+class AccountDownload extends WebComponent {
     async connected() {
+        let data = {};
 
+        data.downloads = {};
+
+        this.innerHTML = this.load.template('account/download', { ...data, ...language });
     }
 }
 
-customElements.define('x-account', XAccount);
+customElements.define('account-download', AccountDownload);

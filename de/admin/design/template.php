8fed917948309149e5c1468f26ca0f6f08e53490
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-19 17:46:19 +0800

    New translations template.php (German)

---
+++ de/admin/design/template.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']     = 'Template Editor';
+
+// Text
+$_['text_success']      = 'Success: You have modified templates!';
+$_['text_add']          = 'Add Template';
+$_['text_edit']         = 'Edit Template';
+$_['text_filter']       = 'Filter';
+$_['text_extension']    = 'Erweiterungen';
+$_['text_code']         = 'Vorlagencode-Editor';
+$_['text_twig']         = 'Der Theme-Editor verwendet die Template-Sprache Twig. Informationen zur <a href="https://twig.symfony.com/doc/" target="_blank" class="alert-link">Twig-Syntax finden Sie hier</a>.';
+
+// Column
+$_['column_store']      = 'Online-Shop (Store)';
+$_['column_route']      = 'Route';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Datum hinzugefügt';
+$_['column_action']     = 'Aktionen';
+
+// Entry
+$_['entry_store']       = 'Online-Shop (Store)';
+$_['entry_route']       = 'Vorlage auswählen';
+$_['entry_code']        = 'Code';
+$_['entry_status']      = 'Status';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify the template editor!';
+$_['error_file']        = 'Achtung: Vorlagendatei existiert nicht!';

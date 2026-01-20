7f8a6971ca8d00897e32103c8c4328bd73980dc2
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-21 03:21:17 +0800

    Update db_schema.php

--- upload/system/helper/db_schema.php
+++ upload/system/helper/db_schema.php
@@ -7590,19 +7590,28 @@ function oc_db_schema() {
 		'primary' => [
 			'translation_id'
 		],
+		'index' => [
+			[
+				'name' => 'route',
+				'key'  => [
+					'route'
+				]
+			],
+			[
+				'name' => 'key',
+				'key'  => [
+					'key'
+				]
+			]
+		],
 		'engine'  => 'InnoDB',
 		'charset' => 'utf8mb4',
 		'collate' => 'utf8mb4_unicode_ci'
 	];
 
 	$tables[] = [
-		'name'  => 'translation_edit',
+		'name'  => 'translation_description',
 		'field' => [
-			[
-				'name'           => 'translation_edit_id',
-				'type'           => 'int(11)',
-				'auto_increment' => true
-			],
 			[
 				'name'           => 'translation_id',
 				'type'           => 'int(11)',
@@ -7618,36 +7627,16 @@ function oc_db_schema() {
 				'type' => 'int(11)'
 			],
 			[
-				'name' => 'route',
-				'type' => 'varchar(64)'
-			],
-			[
-				'name' => 'key',
-				'type' => 'varchar(64)'
-			],
-			[
 				'name' => 'value',
 				'type' => 'text'
-			],
-			[
-				'name'    => 'status',
-				'type'    => 'tinyint(1)',
-				'default' => '0'
-			],
-			[
-				'name' => 'date_added',
-				'type' => 'datetime'
 			]
 		],
 		'primary' => [
-			'translation_edit_id'
+			'translation_id',
+			'store_id',
+			'language_id'
 		],
 		'foreign' => [
-			[
-				'key'   => 'translation_id',
-				'table' => 'translation',
-				'field' => 'translation_id'
-			],
 			[
 				'key'   => 'store_id',
 				'table' => 'store',
@@ -7676,18 +7665,6 @@ function oc_db_schema() {
 				'name' => 'language',
 				'key'  => [
 					'language_id'
-				]
-			],
-			[
-				'name' => 'route',
-				'key'  => [
-					'route'
-				]
-			],
-			[
-				'name' => 'key',
-				'key'  => [
-					'key'
 				]
 			]
 		],

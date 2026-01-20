9491f9348c7e9e94e7cc613e86d87880522a1129
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-01-21 02:01:18 +0800

    New translations translation.php (German)

--- de/admin/design/translation.php
+++ de/admin/design/translation.php
@@ -1,35 +1,34 @@
 <?php
 // Heading
-$_['heading_title']     = 'Spracheneditor';
+$_['heading_title']        = 'Spracheneditor';
 
 // Text
-$_['text_success']      = 'Erfolg: Sie haben den Spracheditor geändert!';
-$_['text_list']         = 'Übersetzungsliste';
-$_['text_add']          = 'Übersetzung hinzufügen';
-$_['text_edit']         = 'Übersetzung bearbeiten';
-$_['text_filter']       = 'Filter';
-$_['text_store']        = 'Online-Shop (Store)';
-$_['text_language']     = 'Sprache';
+$_['text_success']         = 'Erfolg: Sie haben den Spracheditor geändert!';
+$_['text_list']            = 'Übersetzungsliste';
+$_['text_add']             = 'Übersetzung hinzufügen';
+$_['text_edit']            = 'Übersetzung bearbeiten';
+$_['text_filter']          = 'Filter';
+$_['text_store']           = 'Online-Shop (Store)';
+$_['text_language']        = 'Sprache';
+$_['text_translation']     = 'Translation';
+$_['text_translation_add'] = 'Übersetzung hinzufügen';
 
 // Column
-$_['column_store']      = 'Online-Shop (Store)';
-$_['column_language']   = 'Sprache';
-$_['column_route']      = 'Route';
-$_['column_key']        = 'Schlüssel';
-$_['column_value']      = 'Wert';
-$_['column_status']     = 'Status';
-$_['column_date_added'] = 'Datum hinzugefügt';
-$_['column_action']     = 'Aktionen';
+$_['column_route']         = 'Route';
+$_['column_key']           = 'Schlüssel';
+$_['column_status']        = 'Status';
+$_['column_date_added']    = 'Datum hinzugefügt';
+$_['column_action']        = 'Aktionen';
 
 // Entry
-$_['entry_store']       = 'Online-Shop (Store)';
-$_['entry_language']    = 'Sprache';
-$_['entry_route']       = 'Route';
-$_['entry_key']         = 'Schlüssel';
-$_['entry_default']     = 'Standard';
-$_['entry_value']       = 'Wert';
-$_['entry_status']      = 'Status';
+$_['entry_store']          = 'Online-Shop (Store)';
+$_['entry_language']       = 'Sprache';
+$_['entry_route']          = 'Route';
+$_['entry_key']            = 'Schlüssel';
+$_['entry_default']        = 'Standard';
+$_['entry_value']          = 'Wert';
+$_['entry_status']         = 'Status';
 
 // Error
-$_['error_permission']  = 'Achtung: Sie haben keine Berechtigung, den Spracheditor zu ändern!';
-$_['error_key']         = 'Der Schlüssel muss zwischen 3 und 64 Zeichen lang sein!';
+$_['error_permission']     = 'Achtung: Sie haben keine Berechtigung, den Spracheditor zu ändern!';
+$_['error_key']            = 'Der Schlüssel muss zwischen 3 und 64 Zeichen lang sein!';

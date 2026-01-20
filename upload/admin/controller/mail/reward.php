72d5bdc5bdf9831b93eee6d2c1ab8269ae94a900
mhcwebdesign <github@mhccorp.com>
2026-01-20 15:40:52 +0000

    [3.0.x.x] Removal of obsolete config_mail_protocol
    The config_mail_protocol has been replaced with config_mail_engine

--- upload/admin/controller/mail/reward.php
+++ upload/admin/controller/mail/reward.php
@@ -46,7 +46,6 @@ class ControllerMailReward extends Controller {
 			$data['text_total'] = sprintf($this->language->get('text_total'), $this->model_customer_customer->getRewardTotal($customer_id));
 
 			$mail = new Mail($this->config->get('config_mail_engine'));
-			$mail->protocol = $this->config->get('config_mail_protocol');
 			$mail->parameter = $this->config->get('config_mail_parameter');
 			$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 			$mail->smtp_username = $this->config->get('config_mail_smtp_username');
--- upload/system/library/googleshopping/googleshopping.php
+++ upload/system/library/googleshopping/googleshopping.php
@@ -955,11 +955,8 @@ class Googleshopping extends Library {
         $subject = $this->language->get('text_cron_email_subject');
         $message = sprintf($this->language->get('text_cron_email_message'), implode('<br/>', $report));
 
-        $mail = new \Mail();
-
-        $mail->protocol = $this->config->get('config_mail_protocol');
+		$mail = new Mail($this->config->get('config_mail_engine'));
         $mail->parameter = $this->config->get('config_mail_parameter');
-
         $mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
         $mail->smtp_username = $this->config->get('config_mail_smtp_username');
         $mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, "UTF-8");

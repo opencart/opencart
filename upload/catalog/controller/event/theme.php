194dd66c6590ad1effb9201de61a9517ec64bfde
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-03 19:18:48 +0800

    Update theme.php

--- upload/catalog/controller/event/theme.php
+++ upload/catalog/controller/event/theme.php
@@ -21,10 +21,10 @@ class Theme extends \Opencart\System\Engine\Controller {
 		// If there is a theme override, we should get it
 		$this->load->model('design/theme');
 
-		$theme_info = $this->model_design_theme->getTheme($route, $this->config->get('config_theme'));
+		//$theme_info = $this->model_design_theme->getTheme($route, $this->config->get('config_theme'));
 
-		if ($theme_info) {
-			$code = html_entity_decode($theme_info['code'], ENT_QUOTES, 'UTF-8');
-		}
+		//if ($theme_info) {
+		//	$code = html_entity_decode($theme_info['code'], ENT_QUOTES, 'UTF-8');
+		//}
 	}
 }

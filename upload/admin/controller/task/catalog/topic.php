f6120f5c32ad2b984019203fadef3e067a76a84a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-09 23:01:32 +0800

    Update topic.php

--- upload/admin/controller/task/catalog/topic.php
+++ upload/admin/controller/task/catalog/topic.php
@@ -6,21 +6,107 @@ namespace Opencart\Admin\Controller\Task\Catalog;
  * @package Opencart\Admin\Controller\Task\Catalog
  */
 class Topic extends \Opencart\System\Engine\Controller {
+
+
+
+
+
+
 	/**
 	 * Index
 	 *
-	 * Generate all country data.
+	 * Generate all topic data.
 	 *
 	 * @param array<string, string> $args
 	 *
 	 * @return array
 	 */
-	public function addTopic(array $args = []): array {
+	public function info(array $args = []): array {
 		$this->load->language('task/catalog/topic');
 
+		if (!array_key_exists('topic_id', $args)) {
+			return ['error' => $this->language->get('error_required')];
+		}
 
+		$this->load->model('cms/topic');
 
+		$topic_info = $this->model_cms_topic->getTopic((int)$args['topic_id']);
+
+		if (!$topic_info || !$topic_info['status']) {
+			return ['error' => $this->language->get('error_topic')];
+		}
+
+		$this->load->model('setting/task');
+
+		$store_ids = $this->model_cms_topic->getStores($topic_info['topic_id']);
+
+		foreach ($store_ids as $store_id) {
+			$task_data = [
+				'code'   => 'topic.info.' . $store_id,
+				'action' => 'task/catalog/topic.info',
+				'args'   => [
+					'topic_id' => $topic_info['topic_id'],
+					'store_id' => $store_id
+				]
+			];
+
+			$this->model_setting_task->addTask($task_data);
+		}
 
 		return ['success' => $this->language->get('text_task')];
+	}
+
+	/**
+	 * Info
+	 *
+	 * Generate country information.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function _info(array $args = []): array {
+		$this->load->language('task/catalog/topic');
+
+		if (!array_key_exists('topic_id', $args)) {
+			return ['error' => $this->language->get('error_required')];
+		}
+
+		$store_info = [
+			'name' => $this->config->get('config_name'),
+			'url'  => HTTP_CATALOG
+		];
+
+		if ($args['store_id']) {
+			$this->load->model('setting/store');
+
+			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
+
+			if (!$store_info) {
+				return ['error' => $this->language->get('error_store')];
+			}
+		}
+
+		$this->load->model('cms/topic');
+
+		$topic_info = $this->model_cms_topic->getTopic((int)$args['topic_id']);
+
+		if (!$topic_info || !$topic_info['status']) {
+			return ['error' => $this->language->get('error_topic')];
+		}
+
+		$base = DIR_CATALOG . 'view/data/';
+		$directory = parse_url($store_info['url'], PHP_URL_HOST) . '/cms/';
+		$filename = 'topic-' . $topic_info['topic_id'] . '.json';
+
+		if (!oc_directory_create($base . $directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		}
+
+		if (!file_put_contents($base . $directory . $filename, json_encode($topic_info + ['description' => $this->model_cms_topic->getDescriptions($topic_info['topic_id'])]))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		}
+
+		return ['success' => sprintf($this->language->get('text_info'), $store_info['name'], $topic_info['name'])];
 	}
 }

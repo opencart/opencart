55766eb677854b3b23390fce3aa7fb2ab0a9bbc0
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-10 02:07:03 +0800

    Update currency.php

--- upload/admin/controller/event/currency.php
+++ upload/admin/controller/event/currency.php
@@ -23,12 +23,20 @@ class Currency extends \Opencart\System\Engine\Controller {
 	 */
 	public function index(string &$route, array &$args, &$output): void {
 		$task_data = [
-			'code'   => 'currency',
-			'action' => 'task/catalog/currency',
+			'code'   => 'admin.currency',
+			'action' => 'task/admin/currency',
 			'args'   => []
 		];
 
 		$this->load->model('setting/task');
+
+		$this->model_setting_task->addTask($task_data);
+
+		$task_data = [
+			'code'   => 'currency',
+			'action' => 'task/catalog/currency',
+			'args'   => []
+		];
 
 		$this->model_setting_task->addTask($task_data);
 	}

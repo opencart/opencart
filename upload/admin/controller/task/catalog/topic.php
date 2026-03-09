f34aa40aeb0f71bf4053f7a0c1a5be39888b0365
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-09 23:08:56 +0800

    Update topic.php

--- upload/admin/controller/task/catalog/topic.php
+++ upload/admin/controller/task/catalog/topic.php
@@ -3,17 +3,108 @@ namespace Opencart\Admin\Controller\Task\Catalog;
 /**
  * Class Topic
  *
+ *  Generates topic data for all stores.
+ *
  * @package Opencart\Admin\Controller\Task\Catalog
  */
 class Topic extends \Opencart\System\Engine\Controller {
+	/**
+	 * List
+	 *
+	 * Generate information task list.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function index(array $args = []): array {
+		$this->load->language('task/catalog/information');
 
+		$this->load->model('setting/store');
+		$this->load->model('setting/task');
 
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
 
+		foreach ($store_ids as $store_id) {
+			$task_data = [
+				'code'   => 'information.list.' . $store_id,
+				'action' => 'task/catalog/information.list',
+				'args'   => ['store_id' => $store_id]
+			];
 
+			$this->model_setting_task->addTask($task_data);
+		}
 
+		return ['success' => $this->language->get('text_task')];
+	}
 
 	/**
-	 * Index
+	 * _list
+	 *
+	 * Generate country list by store and language.
+	 *
+	 * @param array<string, string> $args
+	 *
+	 * @return array
+	 */
+	public function list(array $args = []): array {
+		$this->load->language('task/catalog/information');
+
+		$store_info = [
+			'store_id' => 0,
+			'name'     => $this->config->get('config_name'),
+			'url'      => HTTP_CATALOG
+		];
+
+		if ($args['store_id']) {
+			$this->load->model('setting/store');
+
+			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
+
+			if (!$store_info) {
+				return ['error' => $this->language->get('error_store')];
+			}
+		}
+
+		$information_data = [];
+
+		$this->load->model('catalog/information');
+
+		$information_ids = $this->model_catalog_information->getStoresByStoreId($store_info['store_id']);
+
+		foreach ($information_ids as $information_id) {
+			$information_info = $this->model_catalog_information->getInformation($information_id);
+
+			if ($information_info && $information_info['status']) {
+				$information_data[] = $information_info + ['description' => $this->model_localisation_country->getDesciptions($information_info['information_id'])];
+			}
+		}
+
+		$sort_order = [];
+
+		foreach ($information_data as $key => $value) {
+			$sort_order[$key] = $value['name'];
+		}
+
+		array_multisort($sort_order, SORT_ASC, $information_data);
+
+		$base = DIR_CATALOG . 'view/data/';
+		$directory = parse_url($store_info['url'], PHP_URL_HOST) . '/catalog/';
+		$filename = 'information.json';
+
+		if (!oc_directory_create($base . $directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		}
+
+		if (!file_put_contents($base . $directory . $filename, json_encode($information_data))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		}
+
+		return ['success' => sprintf($this->language->get('text_list'), $store_info['name'])];
+	}
+
+	/**
+	 * Info
 	 *
 	 * Generate all topic data.
 	 *

40dd41106ad4145ef25ed3ec68218a3abb9fa6f6
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-11 12:18:03 +0800

    Update source file language.php

--- upload/admin/language/en-gb/task/admin/language.php
+++ upload/admin/language/en-gb/task/admin/language.php
@@ -4,6 +4,5 @@ $_['text_success']    = 'Generated language list!';
 $_['text_clear']      = 'Language data cleared!';
 
 // Error
-$_['error_language']  = 'Error: Language could not be found!';
 $_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
 $_['error_file']      = 'Error: File %s could not be written!';

3c1b8f9cbb385d6fcfea23fd4c134ecb20746e93
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-11 12:10:22 +0800

    Update article.php

--- upload/admin/language/en-gb/task/catalog/article.php
+++ upload/admin/language/en-gb/task/catalog/article.php
@@ -2,9 +2,10 @@
 // Text
 $_['text_task']       = 'Generating article task list!';
 $_['text_info']       = 'Generating %s article information!';
-$_['text_clear']      = 'Country data cleared!';
+$_['text_delete']     = 'Article %s data deleted!';
 
 // Error
-$_['error_required']  = 'Error: Country ID required!';
+$_['error_required']  = 'Error: Article ID required!';
+$_['error_article']   = 'Error: Article does not exist!';
 $_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
 $_['error_file']      = 'Error: File %s could not be written!';

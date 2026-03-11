5ea3551043f6cc1fa3a8ee6298220c8409e463e1
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-11 12:19:02 +0800

    New translations information.php (German)

--- de/admin/task/catalog/information.php
+++ de/admin/task/catalog/information.php
@@ -1,1 +1,11 @@
 <?php
+// Text
+$_['text_task']       = 'Generating information task list!';
+$_['text_list']       = 'Generating information lists!';
+$_['text_info']       = 'Generating %s information information!';
+$_['text_clear']      = 'Information data cleared!';
+
+// Error
+$_['error_required']  = 'Error: Information ID required!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

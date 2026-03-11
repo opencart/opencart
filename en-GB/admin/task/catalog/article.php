a500573367e3fd9bcf0414b1c37c4851b134aca0
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-11 12:19:04 +0800

    New translations article.php (English, United Kingdom)

--- en-GB/admin/task/catalog/article.php
+++ en-GB/admin/task/catalog/article.php
@@ -1,1 +1,11 @@
 <?php
+// Text
+$_['text_task']       = 'Generating article task list!';
+$_['text_info']       = 'Generating %s article information!';
+$_['text_delete']     = 'Article %s data deleted!';
+
+// Error
+$_['error_required']  = 'Error: Article ID required!';
+$_['error_article']   = 'Error: Article does not exist!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

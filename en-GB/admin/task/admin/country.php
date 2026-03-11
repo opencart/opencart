e44a843534f6e837535a87446b4c7fdde64c8ac9
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-11 11:20:25 +0800

    New translations country.php (English, United Kingdom)

--- en-GB/admin/task/admin/country.php
+++ en-GB/admin/task/admin/country.php
@@ -1,14 +1,11 @@
 <?php
 // Text
-$_['text_task']         = 'Generating country task list!';
-$_['text_list']         = 'Generating %s country data!';
-$_['text_info']         = 'Generating %s %s country data!';
-$_['text_clear']        = 'Country data cleared!';
+$_['text_list']       = 'Generating country list!';
+$_['text_info']       = 'Generating %s country info!';
+$_['text_delete']     = 'Country %s info deleted!';
 
 // Error
-$_['error_required']    = 'Error: %s required!';
-$_['error_country']     = 'Error: Country does not exist!';
-$_['error_description'] = 'Error: Country description does not exist!';
-$_['error_language']    = 'Error: Language could not be found!';
-$_['error_directory']   = 'Error: Directory %s does not exist or is not writable!';
-$_['error_file']        = 'Error: File %s could not be written!';
+$_['error_required']  = 'Error: %s required!';
+$_['error_country']   = 'Error: Country does not exist!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

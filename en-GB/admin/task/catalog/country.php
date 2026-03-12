39784fe91b77c156166acedd9b6469a90a3dac3d
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-12 21:52:20 +0800

    New translations country.php (English, United Kingdom)

--- en-GB/admin/task/catalog/country.php
+++ en-GB/admin/task/catalog/country.php
@@ -3,7 +3,6 @@
 $_['text_task']       = 'Generating all country data!';
 $_['text_list']       = 'Generating country lists!';
 $_['text_info']       = 'Generating %s country information!';
-$_['text_clear']      = 'Country data cleared!';
 
 // Error
 $_['error_required']  = 'Error: Country ID required!';

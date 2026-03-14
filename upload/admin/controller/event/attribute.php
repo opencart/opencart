e5bd91feb5507667454ebc72746b3988c16a8300
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-14 16:00:09 +0800

    Update attribute.php

--- upload/admin/controller/event/attribute.php
+++ upload/admin/controller/event/attribute.php
@@ -36,35 +36,4 @@ class Attribute extends \Opencart\System\Engine\Controller {
 			$this->model_setting_task->addTask($task_data);
 		}
 	}
-
-	/*
-	 * Delete Attribute
-	 *
-	 * Adds task to generate new product data.
-	 *
-	 * Trigger admin/model/catalog/product/deleteAttribute/after
-	 *
-	 * @param string                $route
-	 * @param array<string, string> $args
-	 * @param array<string, string> $output
-	 *
-	 * @return void
-	 */
-	public function deleteAttribute(string &$route, array &$args, &$output): void {
-		$this->load->model('catalog/product');
-
-		$results = $this->model_catalog_product->getProductsByAttributeId($args[0]);
-
-		$this->load->model('setting/task');
-
-		foreach ($results as $result) {
-			$task_data = [
-				'code'   => 'product.info.' . $result['product_id'],
-				'action' => 'task/catalog/product.info',
-				'args'   => ['product_id' => $result['product_id']]
-			];
-
-			$this->model_setting_task->addTask($task_data);
-		}
-	}
 }

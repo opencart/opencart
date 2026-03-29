ad7146097a27a4b032c6c7dc061553ccabf68fef
Oleg Kasyanov <oleg@batumi.org>
2026-03-30 02:13:55 +0400

    Fixed - PHP Warning:  Undefined array key "en-gb'nvOpzp;..." in catalog/controller/common/language.php

--- upload/catalog/controller/common/language.php
+++ upload/catalog/controller/common/language.php
@@ -18,25 +18,24 @@ class Language extends \Opencart\System\Engine\Controller {
 
 		$data['action'] = $this->url->link('common/language.save', 'language=' . $this->config->get('config_language'));
 
-		$data['code'] = $this->request->get['language'] ?? $this->config->get('config_language');
-
-		// Languages
-		$data['languages'] = [];
-
 		$this->load->model('localisation/language');
-
 		$results = $this->model_localisation_language->getLanguages();
 
-		foreach ($results as $result) {
-			if ($result['status']) {
-				$data['languages'][$result['code']] = $result;
-			}
-		}
+        $data['languages'] = [];
+        $code = $this->config->get('config_language');
+        $requestLng = $this->request->get['language'] ?? '';
 
-		$code = $data['code'];
+		foreach ($results as $result) {
+            $data['languages'][$result['code']] = $result;
+
+            if ($result['code'] == $requestLng) {
+                $code = $result['code'];
+            }
+		}
 
 		$data['name'] = $data['languages'][$code]['name'];
 		$data['image'] = $data['languages'][$code]['image'];
+        $data['code'] = $code;
 
 		// Build the url
 		$url_data = $this->request->get;

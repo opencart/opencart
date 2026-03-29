4c563010d28bef76ba62e64aa7757c60133e966f
Oleg Kasyanov <oleg@batumi.org>
2026-03-29 22:11:34 +0400

    Added some additional form text
    https://github.com/opencart/opencart/commit/0a8b25aaedae97e1b21784263527941448724693

--- upload/admin/language/en-gb/catalog/category.php
+++ upload/admin/language/en-gb/catalog/category.php
@@ -9,11 +9,14 @@ $_['text_add']                = 'Add Category';
 $_['text_edit']               = 'Edit Category';
 $_['text_filter']             = 'Filter';
 $_['text_default']            = 'Default';
+$_['text_general']            = 'General';
+$_['text_meta']               = 'Meta Tags';
 $_['text_keyword']            = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Column
 $_['column_image']            = 'Image';
 $_['column_name']             = 'Category Name';
+$_['column_status']           = 'Status';
 $_['column_sort_order']       = 'Sort Order';
 $_['column_action']           = 'Action';
 

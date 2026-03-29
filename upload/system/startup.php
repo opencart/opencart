8d053b3604ca2ea2472f06197d07ff2fd1bec89f
Oleg Kasyanov <oleg@batumi.org>
2026-03-29 22:30:55 +0400

    Removed upload/system/helper/filter.php as unused
    partially used - https://github.com/opencart/opencart/commit/d34254714d6d08995851f8fec2ea58987ff2e0ae

--- upload/system/startup.php
+++ upload/system/startup.php
@@ -58,5 +58,4 @@ require_once(DIR_SYSTEM . 'engine/config.php');
 
 // Helper
 require_once(DIR_SYSTEM . 'helper/general.php');
-require_once(DIR_SYSTEM . 'helper/filter.php');
 require_once(DIR_SYSTEM . 'helper/validation.php');

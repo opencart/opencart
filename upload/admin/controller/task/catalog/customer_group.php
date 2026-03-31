c07ce872bd50d8c92c25c653099c2715f5532601
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-03-31 10:02:04 +0800

    Update customer_group.php

--- upload/admin/controller/task/catalog/customer_group.php
+++ upload/admin/controller/task/catalog/customer_group.php
@@ -78,7 +78,7 @@ class CustomerGroup extends \Opencart\System\Engine\Controller {
 			foreach ($customer_group_ids as $customer_group_id) {
 				$customer_group_info = $this->model_customer_customer_group->getCustomerGroup($customer_group_id);
 
-				if ($customer_group_info && $customer_group_info['status']) {
+				if ($customer_group_info) {
 					$customer_group_data[] = array_merge($customer_group_info, ['description' => $this->model_customer_customer_group->getDescriptions($customer_group_info['customer_group_id'])]);
 				}
 			}
@@ -126,7 +126,7 @@ class CustomerGroup extends \Opencart\System\Engine\Controller {
 
 		$customer_group_info = $this->model_customer_customer_group->getCustomerGroup((int)$args['customer_group_id']);
 
-		if (!$customer_group_info || !$customer_group_info['status']) {
+		if (!$customer_group_info) {
 			return ['error' => $this->language->get('error_customer_group')];
 		}
 
@@ -187,7 +187,7 @@ class CustomerGroup extends \Opencart\System\Engine\Controller {
 
 		$customer_group_info = $this->model_customer_customer_group->getCustomerGroup((int)$args['customer_group_id']);
 
-		if (!$customer_group_info || !$customer_group_info['status']) {
+		if (!$customer_group_info) {
 			return ['error' => $this->language->get('error_customer_group')];
 		}
 

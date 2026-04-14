1c709e86b4f6fccb89f8e557074d3b4ff2d0dffe
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-04-15 05:36:16 +0800

    New translations authorize.php (German)

--- de/admin/common/authorize.php
+++ de/admin/common/authorize.php
@@ -8,7 +8,6 @@ $_['text_code']     = 'Klicken Sie auf die Schaltfläche „Senden“, um einen 
 $_['text_reset']    = 'Ihr Konto wurde aufgrund von zu vielen falschen Eingabeversuchen des Sicherheitscodes gesperrt. Klicken Sie auf den Reset-Button, um einen Link zum Zurücksetzen per E-Mail zu erhalten. ';
 $_['text_sent']     = 'Erfolg: Eine E-Mail mit einem Sicherheitscode wurde an Ihr E-Mail-Konto gesendet!';
 $_['text_locked']   = 'Ihr Konto wurde gesperrt!';
-$_['text_reset']    = 'Ihr Konto wurde aufgrund von zu vielen falschen Eingabeversuchen des Sicherheitscodes gesperrt. Klicken Sie auf den Reset-Button, um einen Link zum Zurücksetzen per E-Mail zu erhalten. ';
 $_['text_link']     = 'Erfolg: Eine E-Mail mit einem Link zum Zurücksetzen wurde an Ihre E-Mail-Adresse gesendet!';
 $_['text_unlocked'] = 'Ihr Konto wurde entsperrt!';
 $_['text_unlock']   = 'Ihr Konto wurde entsperrt. Sie müssen sich erneut anmelden und den 2FA-Prozess wiederholen, um fortzufahren.';

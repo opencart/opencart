36b14ff35135480c0a6a27bc2f10be658e3d9680
Ryan Hobrock <19723994+ryanhobrock@users.noreply.github.com>
2026-05-28 13:42:23 -0500

    Enforce PHP 8.1 minimum version in installers.

--- upload/install/language/fr-fr/install/step_2.php
+++ upload/install/language/fr-fr/install/step_2.php
@@ -33,7 +33,7 @@ $_['text_unwritable']        = 'Non modifiable';
 $_['text_missing']           = 'Manquant';
 
 // Erreur
-$_['error_version']          = 'Attention: Vous devez utiliser PHP 8.0 ou une version supérieure pour qu\'OpenCart fonctionne !';
+$_['error_version']          = 'Attention: Vous devez utiliser PHP 8.1 ou une version supérieure pour qu\'OpenCart fonctionne !';
 $_['error_open_basedir']     = 'Attention: open_basedir doit permettre l\'accès à %s';
 $_['error_file_upload']      = 'Attention: file_uploads doit être activé !';
 $_['error_session']          = 'Attention: OpenCart ne fonctionnera pas avec session.auto_start activé !';

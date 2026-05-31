ed0ee477788387912e8ce60984e492a69a2a136d
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-05-31 18:39:48 +0800

    Update article.php

--- upload/admin/controller/task/catalog/article.php
+++ upload/admin/controller/task/catalog/article.php
@@ -32,66 +32,6 @@ class Article extends \Opencart\System\Engine\Controller {
 			return ['error' => $this->language->get('error_article')];
 		}
 
-		$this->load->model('setting/task');
-
-		$store_ids = $this->model_cms_article->getStores($article_info['article_id']);
-
-		foreach ($store_ids as $store_id) {
-			$task_data = [
-				'code'   => 'article.info.' . $store_id . '.' . $article_info['article_id'],
-				'action' => 'task/catalog/article.info',
-				'args'   => [
-					'article_id' => $article_info['article_id'],
-					'store_id'   => $store_id
-				]
-			];
-
-			$this->model_setting_task->addTask($task_data);
-		}
-
-		return ['success' => $this->language->get('text_task')];
-	}
-
-	/**
-	 * Info
-	 *
-	 * Generate country information.
-	 *
-	 * @param array<string, string> $args
-	 *
-	 * @return array
-	 */
-	public function info(array $args = []): array {
-		$this->load->language('task/catalog/article');
-
-		if (!array_key_exists('article_id', $args)) {
-			return ['error' => $this->language->get('error_required')];
-		}
-
-		$store_info = [
-			'store_id' => 0,
-			'name'     => $this->config->get('config_name'),
-			'url'      => HTTP_CATALOG
-		];
-
-		if ($args['store_id']) {
-			$this->load->model('setting/store');
-
-			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
-
-			if (!$store_info) {
-				return ['error' => $this->language->get('error_store')];
-			}
-		}
-
-		$this->load->model('cms/article');
-
-		$article_info = $this->model_cms_article->getArticle((int)$args['article_id']);
-
-		if (!$article_info || !$article_info['status']) {
-			return ['error' => $this->language->get('error_article')];
-		}
-
 		// Description
 		$description_data = [];
 
@@ -120,15 +60,37 @@ class Article extends \Opencart\System\Engine\Controller {
 			'date_modified' => $article_info['date_modified']
 		];
 
-		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/cms/';
-		$filename = 'article-' . $article_info['article_id'] . '.yaml';
+		$this->load->model('setting/task');
 
-		if (!oc_directory_create($directory, 0777)) {
-			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
-		}
+		$store_ids = $this->model_cms_article->getStores($article_info['article_id']);
 
-		if (!file_put_contents($directory . $filename, oc_yaml_encode($article_data))) {
-			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		foreach ($store_ids as $store_id) {
+			$store_info = [
+				'store_id' => 0,
+				'name'     => $this->config->get('config_name'),
+				'url'      => HTTP_CATALOG
+			];
+
+			if ($store_id) {
+				$this->load->model('setting/store');
+
+				$store_info = $this->model_setting_store->getStore((int)$store_id);
+
+				if (!$store_info) {
+					return ['error' => $this->language->get('error_store')];
+				}
+			}
+
+			$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/cms/';
+			$filename = 'article-' . $article_info['article_id'] . '.yaml';
+
+			if (!oc_directory_create($directory, 0777)) {
+				return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+			}
+
+			if (!file_put_contents($directory . $filename, oc_yaml_encode($article_data))) {
+				return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+			}
 		}
 
 		return ['success' => sprintf($this->language->get('text_info'), $store_info['name'], $article_info['name'])];

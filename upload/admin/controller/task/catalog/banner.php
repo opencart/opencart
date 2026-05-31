2658b924f021bd6389f421cf4224be428b6e1c7f
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-05-31 18:39:52 +0800

    Update banner.php

--- upload/admin/controller/task/catalog/banner.php
+++ upload/admin/controller/task/catalog/banner.php
@@ -32,67 +32,6 @@ class Banner extends \Opencart\System\Engine\Controller {
 			return ['error' => $this->language->get('error_banner')];
 		}
 
-		$this->load->model('setting/store');
-		$this->load->model('setting/task');
-
-		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
-
-		foreach ($store_ids as $store_id) {
-			$task_data = [
-				'code'   => 'banner.info.' . $store_id . '.' . $banner_info['banner_id'],
-				'action' => 'task/catalog/banner.info',
-				'args'   => [
-					'banner_id' => $banner_info['banner_id'],
-					'store_id'  => $store_id
-				]
-			];
-
-			$this->model_setting_task->addTask($task_data);
-		}
-
-		return ['success' => $this->language->get('text_task')];
-	}
-
-	/**
-	 * Info
-	 *
-	 * Generate banner information.
-	 *
-	 * @param array<string, string> $args
-	 *
-	 * @return array
-	 */
-	public function info(array $args = []): array {
-		$this->load->language('task/catalog/banner');
-
-		if (!array_key_exists('banner_id', $args)) {
-			return ['error' => $this->language->get('error_required')];
-		}
-
-		$store_info = [
-			'store_id' => 0,
-			'name'     => $this->config->get('config_name'),
-			'url'      => HTTP_CATALOG
-		];
-
-		if ($args['store_id']) {
-			$this->load->model('setting/store');
-
-			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
-
-			if (!$store_info) {
-				return ['error' => $this->language->get('error_store')];
-			}
-		}
-
-		$this->load->model('design/banner');
-
-		$banner_info = $this->model_design_banner->getBanner((int)$args['banner_id']);
-
-		if (!$banner_info || !$banner_info['status']) {
-			return ['error' => $this->language->get('error_banner')];
-		}
-
 		// Image
 		$image_data = [];
 
@@ -107,18 +46,41 @@ class Banner extends \Opencart\System\Engine\Controller {
 			];
 		}
 
-		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/design/';
-		$filename = 'banner-' . $banner_info['banner_id'] . '.yaml';
+		$this->load->model('setting/store');
+		$this->load->model('setting/task');
 
-		if (!oc_directory_create($directory, 0777)) {
-			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
+
+		foreach ($store_ids as $store_id) {
+			$store_info = [
+				'store_id' => 0,
+				'name'     => $this->config->get('config_name'),
+				'url'      => HTTP_CATALOG
+			];
+
+			if ($store_id) {
+				$this->load->model('setting/store');
+
+				$store_info = $this->model_setting_store->getStore((int)$store_id);
+
+				if (!$store_info) {
+					return ['error' => $this->language->get('error_store')];
+				}
+			}
+
+			$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/design/';
+			$filename = 'banner-' . $banner_info['banner_id'] . '.yaml';
+
+			if (!oc_directory_create($directory, 0777)) {
+				return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+			}
+
+			if (!file_put_contents($directory . $filename, oc_yaml_encode($image_data))) {
+				return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+			}
 		}
 
-		if (!file_put_contents($directory . $filename, oc_yaml_encode($image_data))) {
-			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
-		}
-
-		return ['success' => sprintf($this->language->get('text_info'), $store_info['name'], $banner_info['name'])];
+		return ['success' => $this->language->get('text_task')];
 	}
 
 	/**

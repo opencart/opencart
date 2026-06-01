d5906ae2e376d87bb1e214a4951d523c4035efdd
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-01 15:54:19 +0800

    Update currency.php

--- upload/admin/controller/task/catalog/currency.php
+++ upload/admin/controller/task/catalog/currency.php
@@ -3,7 +3,7 @@ namespace Opencart\Admin\Controller\Task\Catalog;
 /**
  * Class Currency
  *
- * Generates currencyinformation for all stores.
+ * Generates currency information for all stores.
  *
  * @package Opencart\Admin\Controller\Task\Catalog
  */

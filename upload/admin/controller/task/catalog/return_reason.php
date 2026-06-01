f918599ce242509cf279ec6eca8c06b07239fe3e
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-01 23:37:12 +0800

    Update return_reason.php

--- upload/admin/controller/task/catalog/return_reason.php
+++ upload/admin/controller/task/catalog/return_reason.php
@@ -20,53 +20,6 @@ class ReturnReason extends \Opencart\System\Engine\Controller {
 	public function index(array $args = []): array {
 		$this->load->language('task/catalog/return_reason');
 
-		$this->load->model('setting/store');
-		$this->load->model('setting/task');
-
-		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
-
-		foreach ($store_ids as $store_id) {
-			$task_data = [
-				'code'   => 'return_reason.' . $store_id,
-				'action' => 'task/catalog/return_reason.list',
-				'args'   => ['store_id' => $store_id]
-			];
-
-			$this->model_setting_task->addTask($task_data);
-		}
-
-		return ['success' => $this->language->get('text_task')];
-	}
-
-	/**
-	 * List
-	 *
-	 * Generate JSON return reason list file.
-	 *
-	 * @param array<string, string> $args
-	 *
-	 * @return array
-	 */
-	public function list(array $args = []): array {
-		$this->load->language('task/catalog/return_reason');
-
-		// Store
-		$store_info = [
-			'store_id' => 0,
-			'name'     => $this->config->get('config_name'),
-			'url'      => HTTP_CATALOG
-		];
-
-		if ($args['store_id']) {
-			$this->load->model('setting/store');
-
-			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
-
-			if (!$store_info) {
-				return ['error' => $this->language->get('error_store')];
-			}
-		}
-
 		$return_reason_data = [];
 
 		$this->load->model('localisation/return_reason');
@@ -88,17 +41,40 @@ class ReturnReason extends \Opencart\System\Engine\Controller {
 			];
 		}
 
-		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
-		$filename = 'return_reason.yaml';
+		$this->load->model('setting/store');
 
-		if (!oc_directory_create($directory, 0777)) {
-			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
+
+		foreach ($store_ids as $store_id) {
+			// Store
+			$store_info = [
+				'store_id' => 0,
+				'name'     => $this->config->get('config_name'),
+				'url'      => HTTP_CATALOG
+			];
+
+			if ($args['store_id']) {
+				$this->load->model('setting/store');
+
+				$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
+
+				if (!$store_info) {
+					return ['error' => $this->language->get('error_store')];
+				}
+			}
+
+			$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
+			$filename = 'return_reason.yaml';
+
+			if (!oc_directory_create($directory, 0777)) {
+				return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+			}
+
+			if (!file_put_contents($directory . $filename, oc_yaml_encode($return_reason_data))) {
+				return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+			}
 		}
 
-		if (!file_put_contents($directory . $filename, oc_yaml_encode($return_reason_data))) {
-			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
-		}
-
-		return ['success' => sprintf($this->language->get('text_list'), $store_info['name'])];
+		return ['success' => $this->language->get('text_task')];
 	}
 }

c1293cc4b7b8957ba6947595ac215d9c6962f055
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-02 18:55:23 +0800

    New translations manufacturer.php (English, United Kingdom)
    [ci skip]

--- en-GB/admin/task/catalog/manufacturer.php
+++ en-GB/admin/task/catalog/manufacturer.php
@@ -1,1 +1,12 @@
 <?php
+// Text
+$_['text_list']          = 'Generating manufacturer information list!';
+$_['text_info']          = 'Generating %s manufacturer information!';
+$_['text_delete']        = 'Manufacturer %s information deleted!';
+
+// Error
+$_['error_required']     = 'Error: Manufacturer ID required!';
+$_['error_store']        = 'Error: Store could not be found!';
+$_['error_manufacturer'] = 'Error: Manufacturer does not exist!';
+$_['error_directory']    = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']         = 'Error: File %s could not be written!';

2aa10d6b55c72ee1f18234b816e8a0d6d75cade6
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-02 18:55:25 +0800

    New translations product.php (English, United Kingdom)
    [ci skip]

--- en-GB/admin/task/catalog/product.php
+++ en-GB/admin/task/catalog/product.php
@@ -1,1 +1,12 @@
 <?php
+// Text
+$_['text_list']       = 'Generating product information list!';
+$_['text_info']       = 'Generating %s product information!';
+$_['text_delete']     = 'Product %s information deleted!';
+
+// Error
+$_['error_required']  = 'Error: Product ID required!';
+$_['error_store']     = 'Error: Store could not be found!';
+$_['error_product']   = 'Error: Product does not exist!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

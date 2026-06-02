ad2eb1724818cafe75e467ba04424bb92e3f2b0a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-02 20:27:09 +0800

    Update source file topic.php
    [ci skip]

--- upload/admin/language/en-gb/task/catalog/topic.php
+++ upload/admin/language/en-gb/task/catalog/topic.php
@@ -1,1 +1,12 @@
 <?php
+// Text
+$_['text_list']       = 'Generating topic information list!';
+$_['text_info']       = 'Generating %s topic information!';
+$_['text_delete']     = 'Topic %s information deleted!';
+
+// Error
+$_['error_required']  = 'Error: Topic ID required!';
+$_['error_store']     = 'Error: Store could not be found!';
+$_['error_topic']     = 'Error: Topic does not exist!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

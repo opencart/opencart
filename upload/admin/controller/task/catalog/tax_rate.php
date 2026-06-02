5e42876491d3da17bedc014ed3f74d40a87f3703
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-02 16:33:15 +0800

    Update tax_rate.php

--- upload/admin/controller/task/catalog/tax_rate.php
+++ upload/admin/controller/task/catalog/tax_rate.php
@@ -30,69 +30,6 @@ class TaxRate extends \Opencart\System\Engine\Controller {
 			return ['error' => $this->language->get('error_geo_zone')];
 		}
 
-		$this->load->model('setting/store');
-		$this->load->model('setting/task');
-
-		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
-
-		foreach ($store_ids as $store_id) {
-			$task_data = [
-				'code'   => 'tax_rate.info.' . $store_id . '.' . $geo_zone_info['geo_zone_id'],
-				'action' => 'task/catalog/tax_rate.info',
-				'args'   => [
-					'geo_zone_id' => $geo_zone_info['geo_zone_id'],
-					'store_id'    => $store_id
-				]
-			];
-
-			$this->model_setting_task->addTask($task_data);
-		}
-
-		return ['success' => sprintf($this->language->get('text_info'), $geo_zone_info['name'])];
-	}
-
-	/**
-	 * Info
-	 *
-	 * Generate tax rate information.
-	 *
-	 * @param array<string, string> $args
-	 *
-	 * @return array
-	 */
-	public function info(array $args = []): array {
-		$this->load->language('task/catalog/tax_rate');
-
-		if (!array_key_exists('geo_zone_id', $args)) {
-			return ['error' => $this->language->get('error_required')];
-		}
-
-		// Store
-		$store_info = [
-			'store_id' => 0,
-			'name'     => $this->config->get('config_name'),
-			'url'      => HTTP_CATALOG
-		];
-
-		if ($args['store_id']) {
-			$this->load->model('setting/store');
-
-			$store_info = $this->model_setting_store->getStore($args['store_id']);
-
-			if (!$store_info) {
-				return ['error' => $this->language->get('error_store')];
-			}
-		}
-
-		// Geo Zone
-		$this->load->model('localisation/geo_zone');
-
-		$geo_zone_info = $this->model_localisation_geo_zone->getGeoZone($args['geo_zone_id']);
-
-		if (!$geo_zone_info) {
-			return ['error' => $this->language->get('error_geo_zone')];
-		}
-
 		$tax_rate_data = [];
 
 		$this->load->model('localisation/tax_rate');
@@ -105,27 +42,50 @@ class TaxRate extends \Opencart\System\Engine\Controller {
 			foreach ($customer_groups as $customer_group_id) {
 				$tax_rate_data[$customer_group_id] = [
 					'tax_rule_id'       => $tax_rate['tax_rule_id'],
-				    'tax_rate_id'       => $tax_rate['tax_rate_id'],
-				    'tax_class_id'      => $tax_rate['tax_class_id'],
-				    'name'              => $tax_rate['name'],
-				    'rate'              => $tax_rate['rate'],
-				    'type'              => $tax_rate['type'],
-				    'priority'          => $tax_rate['priority'],
-				    'geo_zone'          => $tax_rate['geo_zone'],
+					'tax_rate_id'       => $tax_rate['tax_rate_id'],
+					'tax_class_id'      => $tax_rate['tax_class_id'],
+					'name'              => $tax_rate['name'],
+					'rate'              => $tax_rate['rate'],
+					'type'              => $tax_rate['type'],
+					'priority'          => $tax_rate['priority'],
+					'geo_zone'          => $tax_rate['geo_zone'],
 					'customer_group_id' => $customer_group_id
 				];
 			}
 		}
 
-		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
-		$filename = 'tax_rate-' . $geo_zone_info['geo_zone_id'] . '.yaml';
+		$this->load->model('setting/store');
 
-		if (!oc_directory_create($directory, 0777)) {
-			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
-		}
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
 
-		if (!file_put_contents($directory . $filename, oc_yaml_encode($tax_rate_data))) {
-			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		foreach ($store_ids as $store_id) {
+			// Store
+			$store_info = [
+				'store_id' => 0,
+				'name'     => $this->config->get('config_name'),
+				'url'      => HTTP_CATALOG
+			];
+
+			if ($store_id) {
+				$this->load->model('setting/store');
+
+				$store_info = $this->model_setting_store->getStore($store_id);
+
+				if (!$store_info) {
+					return ['error' => $this->language->get('error_store')];
+				}
+			}
+
+			$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
+			$filename = 'tax_rate-' . $geo_zone_info['geo_zone_id'] . '.yaml';
+
+			if (!oc_directory_create($directory, 0777)) {
+				return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+			}
+
+			if (!file_put_contents($directory . $filename, oc_yaml_encode($tax_rate_data))) {
+				return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+			}
 		}
 
 		return ['success' => sprintf($this->language->get('text_info'), $geo_zone_info['name'])];

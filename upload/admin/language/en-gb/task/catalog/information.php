3abaa961caf8271f3e34019cffc36d40a841757a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-02 20:26:58 +0800

    Update source file information.php
    [ci skip]

--- upload/admin/language/en-gb/task/catalog/information.php
+++ upload/admin/language/en-gb/task/catalog/information.php
@@ -1,11 +1,12 @@
 <?php
 // Text
-$_['text_task']       = 'Generating information task list!';
-$_['text_list']       = 'Generating information lists!';
-$_['text_info']       = 'Generating %s information information!';
-$_['text_clear']      = 'Information data cleared!';
+$_['text_list']         = 'Generating information list!';
+$_['text_info']         = 'Generating %s information information!';
+$_['text_delete']       = 'Information %s information deleted!';
 
 // Error
-$_['error_required']  = 'Error: Information ID required!';
-$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
-$_['error_file']      = 'Error: File %s could not be written!';
+$_['error_required']    = 'Error: Information ID required!';
+$_['error_store']       = 'Error: Store could not be found!';
+$_['error_information'] = 'Error: Information does not exist!';
+$_['error_directory']   = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']        = 'Error: File %s could not be written!';

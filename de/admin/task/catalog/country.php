545eb26e9fc68114f92f230ddedc9d24c89fd349
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-02 18:51:13 +0800

    New translations country.php (German)
    [ci skip]

--- de/admin/task/catalog/country.php
+++ de/admin/task/catalog/country.php
@@ -1,10 +1,12 @@
 <?php
 // Text
-$_['text_task']       = 'Generating all country data!';
-$_['text_list']       = 'Generating country lists!';
+$_['text_list']       = 'Generating country information list!';
 $_['text_info']       = 'Generating %s country information!';
+$_['text_delete']     = 'Country %s information deleted!';
 
 // Error
 $_['error_required']  = 'Error: Country ID required!';
+$_['error_store']     = 'Error: Store could not be found!';
+$_['error_country']   = 'Error: Country does not exist!';
 $_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
 $_['error_file']      = 'Error: File %s could not be written!';

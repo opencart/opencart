799123555cb7e6d123826298415cab74285a3a88
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-02 18:51:41 +0800

    New translations tax_rate.php (German)
    [ci skip]

--- de/admin/task/catalog/tax_rate.php
+++ de/admin/task/catalog/tax_rate.php
@@ -1,1 +1,11 @@
 <?php
+// Text
+$_['text_info']       = 'Generating %s tax rate information!';
+$_['text_delete']     = 'Tax Rate %s information deleted!';
+
+// Error
+$_['error_required']  = 'Error: Tax Rate ID required!';
+$_['error_store']     = 'Error: Store could not be found!';
+$_['error_tax_rate']  = 'Error: Tax Rate does not exist!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

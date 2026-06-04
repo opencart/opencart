41b467a38cadd42a49b0523c25181afe664d7f06
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-05 03:03:30 +0800

    Update Information.php

--- upload/admin/controller/task/catalog/Information.php
+++ upload/admin/controller/task/catalog/Information.php
@@ -20,68 +20,64 @@ class Information extends \Opencart\System\Engine\Controller {
 	public function index(array $args = []): array {
 		$this->load->language('task/catalog/information');
 
-		$this->load->model('setting/store');
+		// Store
+		$store_info = [
+			'store_id' => 0,
+			'name'     => $this->config->get('config_name'),
+			'url'      => HTTP_CATALOG
+		];
 
-		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
+		if ($args['store_id']) {
+			$this->load->model('setting/store');
 
-		foreach ($store_ids as $store_id) {
-			$store_info = [
-				'store_id' => 0,
-				'name'     => $this->config->get('config_name'),
-				'url'      => HTTP_CATALOG
-			];
+			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
 
-			if ($store_id) {
-				$this->load->model('setting/store');
-
-				$store_info = $this->model_setting_store->getStore((int)$store_id);
-
-				if (!$store_info) {
-					return ['error' => $this->language->get('error_store')];
-				}
-			}
-
-			$information_data = [];
-
-			$filter_data = [
-				'filter_store_id' => $store_info['store_id'],
-				'filter_status'   => true,
-				'sort'            => 'sort_order',
-				'order'           => 'ASC',
-			];
-
-			$this->load->model('catalog/information');
-
-			$results = $this->model_catalog_information->getInformations($filter_data);
-
-			foreach ($results as $result) {
-				$description_data = [];
-
-				$descriptions = $this->model_catalog_information->getDescriptions($result['information_id']);
-
-				foreach ($descriptions as $code => $description) {
-					$description_data[$code] = ['title' => $description['title']];
-				}
-
-				$information_data[] = [
-					'information_id' => $result['information_id'],
-					'description'    => $description_data
-				];
-			}
-
-			$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/catalog/';
-			$filename = 'information.yaml';
-
-			if (!oc_directory_create($directory, 0777)) {
-				return ['error' => sprintf($this->language->get('error_directory'), $directory)];
-			}
-
-			if (!file_put_contents($directory . $filename, oc_yaml_encode($information_data))) {
-				return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+			if (!$store_info) {
+				return ['error' => $this->language->get('error_store')];
 			}
 		}
 
-		return ['success' => $this->language->get('text_list')];
+		// Information
+		$information_data = [];
+
+		$filter_data = [
+			'filter_store_id' => $store_info['store_id'],
+			'filter_status'   => true,
+			'sort'            => 'sort_order',
+			'order'           => 'ASC',
+		];
+
+		$this->load->model('catalog/information');
+
+		$results = $this->model_catalog_information->getInformations($filter_data);
+
+		foreach ($results as $result) {
+			$description_data = [];
+
+			$descriptions = $this->model_catalog_information->getDescriptions($result['information_id']);
+
+			foreach ($descriptions as $code => $description) {
+				$description_data[$code] = ['title' => $description['title']];
+			}
+
+			$information_data[] = [
+				'information_id' => $result['information_id'],
+				'description'    => $description_data
+			];
+		}
+
+		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/catalog/';
+		$filename = 'information.yaml';
+
+		if (!oc_directory_create($directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		}
+
+		if (!file_put_contents($directory . $filename, oc_yaml_encode($information_data))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		}
+
+		return ['success' => sprintf($this->language->get('text_list'), $store_info['name'])];
 	}
 
 	/**
@@ -100,14 +96,33 @@ class Information extends \Opencart\System\Engine\Controller {
 			return ['error' => $this->language->get('error_required')];
 		}
 
+		// Store
+		$store_info = [
+			'store_id' => 0,
+			'name'     => $this->config->get('config_name'),
+			'url'      => HTTP_CATALOG
+		];
+
+		if ($args['store_id']) {
+			$this->load->model('setting/store');
+
+			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
+
+			if (!$store_info) {
+				return ['error' => $this->language->get('error_store')];
+			}
+		}
+
+		// Information
 		$this->load->model('catalog/information');
 
 		$information_info = $this->model_catalog_information->getInformation((int)$args['information_id']);
 
-		if (!$information_info || !$information_info['status']) {
+		if (!$information_info || !$information_info['status'] || !in_array($information_info['information_id'], $this->model_catalog_information->getStores($information_info['information_id']))) {
 			return ['error' => $this->language->get('error_information')];
 		}
 
+		// Description
 		$description_data = [];
 
 		$descriptions = $this->model_catalog_information->getDescriptions($information_info['information_id']);
@@ -127,38 +142,18 @@ class Information extends \Opencart\System\Engine\Controller {
 			'description'    => $description_data
 		];
 
-		$store_ids = $this->model_catalog_information->getStores($information_info['information_id']);
+		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/catalog/';
+		$filename = 'information-' . $information_info['information_id'] . '.yaml';
 
-		foreach ($store_ids as $store_id) {
-			$store_info = [
-				'store_id' => 0,
-				'name'     => $this->config->get('config_name'),
-				'url'      => HTTP_CATALOG
-			];
-
-			if ($store_id) {
-				$this->load->model('setting/store');
-
-				$store_info = $this->model_setting_store->getStore((int)$store_id);
-
-				if (!$store_info) {
-					return ['error' => $this->language->get('error_store')];
-				}
-			}
-
-			$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/catalog/';
-			$filename = 'information-' . $information_info['information_id'] . '.yaml';
-
-			if (!oc_directory_create($directory, 0777)) {
-				return ['error' => sprintf($this->language->get('error_directory'), $directory)];
-			}
-
-			if (!file_put_contents($directory . $filename, oc_yaml_encode($information_data))) {
-				return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
-			}
+		if (!oc_directory_create($directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
 		}
 
-		return ['success' => sprintf($this->language->get('text_info'), $information_info['title'])];
+		if (!file_put_contents($directory . $filename, oc_yaml_encode($information_data))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
+		}
+
+		return ['success' => sprintf($this->language->get('text_info'), $store_info['name'], $information_info['title'])];
 	}
 
 	/**

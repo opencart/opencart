91bd6ccae43def01e14ce4d8d11c2873c1b9353a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-04 21:23:53 +0800

    Update currency.php

--- upload/admin/controller/task/catalog/currency.php
+++ upload/admin/controller/task/catalog/currency.php
@@ -20,59 +20,53 @@ class Currency extends \Opencart\System\Engine\Controller {
 	public function index(array $args = []): array {
 		$this->load->language('task/catalog/currency');
 
-		$this->load->model('setting/store');
+		$store_info = [
+			'store_id' => 0,
+			'name'     => $this->config->get('config_name'),
+			'url'      => HTTP_CATALOG
+		];
 
-		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
+		if ($args['store_id']) {
+			$this->load->model('setting/store');
 
-		foreach ($store_ids as $store_id) {
-			$store_info = [
-				'store_id' => 0,
-				'name'     => $this->config->get('config_name'),
-				'url'      => HTTP_CATALOG
-			];
+			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
 
-			if ($args['store_id']) {
-				$this->load->model('setting/store');
-
-				$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
-
-				if (!$store_info) {
-					return ['error' => $this->language->get('error_store')];
-				}
+			if (!$store_info) {
+				return ['error' => $this->language->get('error_store')];
 			}
+		}
 
-			$currency_data = [];
+		$currency_data = [];
 
-			$this->load->model('setting/setting');
-			$this->load->model('localisation/currency');
+		$this->load->model('setting/setting');
+		$this->load->model('localisation/currency');
 
-			$currencies = (array)$this->model_setting_setting->getValue('config_currency_list', $store_info['store_id']);
+		$currencies = (array)$this->model_setting_setting->getValue('config_currency_list', $store_info['store_id']);
 
-			foreach ($currencies as $code) {
-				$currency_info = $this->model_localisation_currency->getCurrencyByCode((string)$code);
+		foreach ($currencies as $code) {
+			$currency_info = $this->model_localisation_currency->getCurrencyByCode((string)$code);
 
-				if ($currency_info && $currency_info['status']) {
-					$currency_data[$currency_info['code']] = [
-						'title'         => $currency_info['title'],
-						'code'          => $currency_info['code'],
-						'symbol_left'   => $currency_info['symbol_left'],
-						'symbol_right'  => $currency_info['symbol_right'],
-						'decimal_place' => $currency_info['decimal_place'],
-						'value'         => $currency_info['value']
-					];
-				}
+			if ($currency_info && $currency_info['status']) {
+				$currency_data[$currency_info['code']] = [
+					'title'         => $currency_info['title'],
+					'code'          => $currency_info['code'],
+					'symbol_left'   => $currency_info['symbol_left'],
+					'symbol_right'  => $currency_info['symbol_right'],
+					'decimal_place' => $currency_info['decimal_place'],
+					'value'         => $currency_info['value']
+				];
 			}
+		}
 
-			$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
-			$filename = 'currency.yaml';
+		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
+		$filename = 'currency.yaml';
 
-			if (!oc_directory_create($directory, 0777)) {
-				return ['error' => sprintf($this->language->get('error_directory'), $directory)];
-			}
+		if (!oc_directory_create($directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		}
 
-			if (!file_put_contents($directory . $filename, oc_yaml_encode($currency_data))) {
-				return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
-			}
+		if (!file_put_contents($directory . $filename, oc_yaml_encode($currency_data))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
 		}
 
 		return ['success' => $this->language->get('text_list')];

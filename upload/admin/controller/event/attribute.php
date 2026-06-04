54e0102687a714c3513a6efff2066bf4d07c3ea0
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-04 20:28:04 +0800

    Update attribute.php

--- upload/admin/controller/event/attribute.php
+++ upload/admin/controller/event/attribute.php
@@ -25,15 +25,23 @@ class Attribute extends \Opencart\System\Engine\Controller {
 		$results = $this->model_catalog_product->getProductsByAttributeId($args[0]);
 
 		$this->load->model('setting/task');
+		$this->load->model('setting/store');
 
-		foreach ($results as $result) {
-			$task_data = [
-				'code'   => 'product.' . $result['product_id'],
-				'action' => 'task/catalog/product',
-				'args'   => ['product_id' => $result['product_id']]
-			];
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
 
-			$this->model_setting_task->addTask($task_data);
+		foreach ($store_ids as $store_id) {
+			foreach ($results as $result) {
+				$task_data = [
+					'code'   => 'product.' . $store_id . '.' . $result['product_id'],
+					'action' => 'task/catalog/product',
+					'args'   => [
+						'product_id' => $result['product_id'],
+						'store_id'   => $store_id
+					]
+				];
+
+				$this->model_setting_task->addTask($task_data);
+			}
 		}
 	}
 }

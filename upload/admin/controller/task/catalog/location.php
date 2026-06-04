4ba36b30cdb25458caa5cd401443bdab817f97c3
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-05 03:03:39 +0800

    Update location.php

--- upload/admin/controller/task/catalog/location.php
+++ upload/admin/controller/task/catalog/location.php
@@ -20,61 +20,55 @@ class Location extends \Opencart\System\Engine\Controller {
 	public function index(array $args = []): array {
 		$this->load->language('task/catalog/location');
 
-		$this->load->model('setting/store');
+		$store_info = [
+			'store_id' => 0,
+			'name'     => $this->config->get('config_name'),
+			'url'      => HTTP_CATALOG
+		];
 
-		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
+		if ($args['store_id']) {
+			$this->load->model('setting/store');
 
-		foreach ($store_ids as $store_id) {
-			$store_info = [
-				'store_id' => 0,
-				'name'     => $this->config->get('config_name'),
-				'url'      => HTTP_CATALOG
-			];
+			$store_info = $this->model_setting_store->getStore((int)$args['store_id']);
 
-			if ($store_id) {
-				$this->load->model('setting/store');
-
-				$store_info = $this->model_setting_store->getStore((int)$store_id);
-
-				if (!$store_info) {
-					return ['error' => $this->language->get('error_store')];
-				}
+			if (!$store_info) {
+				return ['error' => $this->language->get('error_store')];
 			}
+		}
 
-			// Location
-			$location_data = [];
+		// Location
+		$location_data = [];
 
-			$this->load->model('setting/setting');
-			$this->load->model('localisation/location');
+		$this->load->model('setting/setting');
+		$this->load->model('localisation/location');
 
-			$location_ids = (array)$this->model_setting_setting->getValue('config_location_list', $store_info['store_id']);
+		$location_ids = (array)$this->model_setting_setting->getValue('config_location_list', $store_info['store_id']);
 
-			foreach ($location_ids as $location_id) {
-				$location_info = $this->model_localisation_location->getLocation($location_id);
+		foreach ($location_ids as $location_id) {
+			$location_info = $this->model_localisation_location->getLocation($location_id);
 
-				if ($location_info) {
-					$location_data[] = [
-						'location_id' => $location_info['location_id'],
-						'name'        => $location_info['name'],
-						'address'     => $location_info['address'],
-						'telephone'   => $location_info['telephone'],
-						'image'       => $location_info['image'],
-						'open'        => $location_info['open'],
-						'comment'     => $location_info['comment']
-					];
-				}
+			if ($location_info) {
+				$location_data[] = [
+					'location_id' => $location_info['location_id'],
+					'name'        => $location_info['name'],
+					'address'     => $location_info['address'],
+					'telephone'   => $location_info['telephone'],
+					'image'       => $location_info['image'],
+					'open'        => $location_info['open'],
+					'comment'     => $location_info['comment']
+				];
 			}
+		}
 
-			$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
-			$filename = 'location.yaml';
+		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
+		$filename = 'location.yaml';
 
-			if (!oc_directory_create($directory, 0777)) {
-				return ['error' => sprintf($this->language->get('error_directory'), $directory)];
-			}
+		if (!oc_directory_create($directory, 0777)) {
+			return ['error' => sprintf($this->language->get('error_directory'), $directory)];
+		}
 
-			if (!file_put_contents($directory . $filename, oc_yaml_encode($location_data))) {
-				return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
-			}
+		if (!file_put_contents($directory . $filename, oc_yaml_encode($location_data))) {
+			return ['error' => sprintf($this->language->get('error_file'), $directory . $filename)];
 		}
 
 		return ['success' => $this->language->get('text_task')];

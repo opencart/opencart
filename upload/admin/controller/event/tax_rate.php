d8f019bd6a4b801f9d17853a7449779ffcefed5d
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-09 03:22:55 +0800

    Update tax_rate.php

--- upload/admin/controller/event/tax_rate.php
+++ upload/admin/controller/event/tax_rate.php
@@ -57,6 +57,8 @@ class TaxRate extends \Opencart\System\Engine\Controller {
 
 		$tax_rate_info = $this->model_localisation_tax_rate->getTaxRate($args[0]);
 
+		$this->load->model('setting/store');
+
 		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
 
 		foreach ($store_ids as $store_id) {
@@ -99,23 +101,29 @@ class TaxRate extends \Opencart\System\Engine\Controller {
 	 * @return void
 	 */
 	public function deleteTaxRate(string &$route, array &$args): void {
+		$this->load->model('setting/task');
+
 		$this->load->model('localisation/tax_rate');
 
 		$tax_rate_info = $this->model_localisation_tax_rate->getTaxRate($args[0]);
 
-		if ($tax_rate_info) {
-			$task_data = [
-				'code'   => 'tax_rate.delete.' . $tax_rate_info['geo_zone_id'],
-				'action' => 'task/admin/tax_rate.delete',
-				'args'   => [
-					'geo_zone_id' => $tax_rate_info['geo_zone_id'],
-					'store_id'   => $store_id
-				]
-			];
+		$this->load->model('setting/store');
 
-			$this->load->model('setting/task');
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
 
-			$this->model_setting_task->addTask($task_data);
+		foreach ($store_ids as $store_id) {
+			if ($tax_rate_info) {
+				$task_data = [
+					'code'   => 'tax_rate.delete.' .  $store_id . '.' . $tax_rate_info['geo_zone_id'],
+					'action' => 'task/admin/tax_rate.delete',
+					'args'   => [
+						'geo_zone_id' => $tax_rate_info['geo_zone_id'],
+						'store_id'    => $store_id
+					]
+				];
+
+				$this->model_setting_task->addTask($task_data);
+			}
 		}
 	}
 }

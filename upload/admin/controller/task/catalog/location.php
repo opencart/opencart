37de2537be75f6a034062f972b27b3315e90001c
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-10 23:53:24 +0800

    Update location.php

--- upload/admin/controller/task/catalog/location.php
+++ upload/admin/controller/task/catalog/location.php
@@ -60,7 +60,7 @@ class Location extends \Opencart\System\Engine\Controller {
 			}
 		}
 
-		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
+		$directory = DIR_OPENCART . 'shop/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
 		$filename = 'location.yaml';
 
 		if (!oc_directory_create($directory, 0777)) {

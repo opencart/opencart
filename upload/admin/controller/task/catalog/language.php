c3fcef1c757da43131320b15820d46befde94125
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-10 23:53:22 +0800

    Update language.php

--- upload/admin/controller/task/catalog/language.php
+++ upload/admin/controller/task/catalog/language.php
@@ -57,7 +57,7 @@ class Language extends \Opencart\System\Engine\Controller {
 			}
 		}
 
-		$directory = DIR_CATALOG . 'view/data/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
+		$directory = DIR_OPENCART . 'shop/' . parse_url($store_info['url'], PHP_URL_HOST) . '/localisation/';
 		$filename = 'language.yaml';
 
 		if (!oc_directory_create($directory, 0777)) {

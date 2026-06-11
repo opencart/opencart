fbc5c34e1b10fc609c27d3531bce5d309cf71f41
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-12 03:04:41 +0800

    Update developer.php

--- upload/admin/language/en-gb/common/developer.php
+++ upload/admin/language/en-gb/common/developer.php
@@ -1,30 +1,29 @@
 <?php
 // Heading
-$_['heading_title']             = 'Developer Options';
+$_['heading_title']        = 'Developer Options';
 
 // Text
-$_['text_html_success']         = 'Success: You have cleared the HTML files!';
-$_['text_cache_success']        = 'Success: You have cleared the cache!';
+$_['text_rebuild_success'] = 'Success: You have cleared the HTML files!';
+$_['text_theme_success']   = 'Success: You have cleared the theme cache!';
+$_['text_sass_success']    = 'Success: Generating the catalog SASS added to the task queue!';
 
-$_['text_theme_success']        = 'Success: You have cleared the theme cache!';
+$_['text_cache_success']   = 'Success: You have cleared the cache!';
+$_['text_vendor_success']  = 'Success: You have modified the vendor cache!';
 
-$_['text_sass_catalog_success'] = 'Success: Generating the catalog SASS added to the task queue!';
-$_['text_vendor_success']       = 'Success: You have modified the vendor cache!';
-
-$_['text_rebuild']              = 'Rebuild Store';
-$_['text_template']             = 'Rebuild Templates';
-$_['text_sass']                 = 'Rebuild Stylesheet';
-$_['text_clear']                = 'Clear All';
-$_['text_cache']                = 'Cache';
-$_['text_vendor']               = 'Vendor';
+$_['text_rebuild']         = 'Rebuild Store';
+$_['text_template']        = 'Rebuild Templates';
+$_['text_sass']            = 'Rebuild Stylesheet';
+$_['text_clear']           = 'Clear All';
+$_['text_cache']           = 'Cache';
+$_['text_vendor']          = 'Vendor';
 
 // Column
-$_['column_store']              = 'Store';
-$_['column_action']             = 'Action';
+$_['column_store']         = 'Store';
+$_['column_action']        = 'Action';
 
 // Help
-$_['help_cache']                = 'Clears all stored cache data files.';
-$_['help_vendor']               = 'Rebuilds the included vendor file.';
+$_['help_cache']           = 'Clears all stored cache data files.';
+$_['help_vendor']          = 'Rebuilds the included vendor file.';
 
 // Error
-$_['error_permission']          = 'Warning: You do not have permission to modify developer settings!';
+$_['error_permission']     = 'Warning: You do not have permission to modify developer settings!';

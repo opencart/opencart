a2205e3a17b794457c8990d4f428258e7de6c055
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-11 17:57:15 +0800

    New translations developer.php (German)
    [ci skip]

--- de/admin/common/developer.php
+++ de/admin/common/developer.php
@@ -5,25 +5,26 @@ $_['heading_title']             = 'Entwickleroptionen';
 // Text
 $_['text_html_success']         = 'Success: You have cleared the HTML files!';
 $_['text_cache_success']        = 'Erfolg: Sie haben den Cache geleert!';
+
 $_['text_theme_success']        = 'Erfolg: Sie haben den Theme-Cache geleert!';
+
 $_['text_sass_catalog_success'] = 'Success: Generating the catalog SASS added to the task queue!';
-$_['text_sass_admin_success']   = 'Success: Generating the admin SASS added to the task queue!';
 $_['text_vendor_success']       = 'Success: You have modified the vendor cache!';
-$_['text_html']                 = 'HTML';
+
+$_['text_rebuild']              = 'Rebuild store';
+$_['text_template']             = 'Rebuild Template';
+$_['text_sass']                 = 'Rebuild SASS';
+$_['text_clear']                = 'Clear All';
 $_['text_cache']                = 'Zwischenspeicher';
-$_['text_theme']                = 'Thema';
-$_['text_sass_catalog']         = 'Catalog SASS';
-$_['text_sass_admin']           = 'Admin SASS';
 $_['text_vendor']               = 'Verkäufer';
 
+// Column
+$_['column_store']              = 'Online-Shop (Store)';
+$_['column_action']             = 'Aktionen';
+
 // Help
-$_['help_html']                 = 'Clears all stored HTML files.';
 $_['help_cache']                = 'Clears all stored cache data files.';
-$_['help_theme']                = 'Clears all stored theme data files.';
-$_['help_sass_catalog']         = 'Rebuilds the stored catalog SASS files.';
-$_['help_sass_admin']           = 'Rebuilds the stored admin SASS files.';
 $_['help_vendor']               = 'Rebuilds the included vendor file.';
 
 // Error
 $_['error_permission']          = 'Achtung: Sie sind nicht berechtigt, Entwicklereinstellungen zu ändern!';
-$_['error_file']                = 'Error: SASS File %s could not be found!';

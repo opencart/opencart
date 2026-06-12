4a4972dc8d09fc2132612911d1bc584b958dddbc
Jayaseelan Samuel <79314753+smartsw33t@users.noreply.github.com>
2026-06-12 10:01:53 +0530

    Tamil translation of Subscription.php

---
+++ upload/catalog/language/ta-in/account/subscription.php
@@ -0,0 +1,52 @@
+<?php
+// Heading
+$_['heading_title']              = 'சந்தாக்கள்';
+
+// Text
+$_['text_account']               = 'கணக்கு';
+$_['text_success']               = 'வெற்றி: உங்கள் சந்தா வெற்றிகரமாக ரத்துசெய்யப்பட்டது!';
+$_['text_subscription']          = 'சந்தா #%s';
+$_['text_subscription_id']       = 'சந்தா அடையாளம்';
+$_['text_order_id']              = 'வாங்கக்கேட்டதன் அடையாளம்';
+$_['text_shipping_address']      = 'அனுப்பல் முகவரி';
+$_['text_shipping_method']       = 'அனுப்பல் முறை';
+$_['text_payment_address']       = 'கட்டண முகவரி';
+$_['text_payment_method']        = 'கட்டண முறை';
+$_['text_model']                 = 'மாதிரி';
+$_['text_subscription_plan']     = 'சந்தாத் திட்டம்';
+$_['text_subscription_trial']    = '%s ஒவ்வொரு %d %s காலத்துக்கு %d கட்டணம், பின் ';
+$_['text_subscription_duration'] = '%s ஒவ்வொரு %d %s காலத்துக்கு %d கட்டணம்';
+$_['text_subscription_cancel']   = '%s ஒவ்வொரு %d %s  காலத்துக்கு ரத்துசெய்யும் வரை';
+$_['text_day']                   = 'நாள்';
+$_['text_week']                  = 'வாரம்';
+$_['text_semi_month']            = 'அரை மாதம்';
+$_['text_month']                 = 'மாதம்';
+$_['text_year']                  = 'ஆண்டு';
+$_['text_quantity']              = 'எண்ணிக்கை';
+$_['text_date_next']             = 'தேதி அடுத்து';
+$_['text_remaining']             = 'மீதம்';
+$_['text_history']               = 'இதுவரையிலான சந்தாக்கள்';
+$_['text_order']                 = 'இதுவரை வாங்கக்கேட்டவை';
+$_['text_no_subscription']       = 'சந்தா எதுவும் காணப்படவில்லை!';
+
+// Column
+$_['column_subscription_id']     = 'சந்தா அடையாளம்';
+$_['column_product']             = 'பொருள்';
+$_['column_product_total']       = 'பொருட்களின் எண்ணிக்கை';
+$_['column_description']         = 'விவரம்';
+$_['column_order_id']            = 'வாங்கக்கேட்டதன் அடையாளம்';
+$_['column_status']              = 'நிலை';
+$_['column_quantity']            = 'அளவு';
+$_['column_trial_price']         = 'பரிசோதிப்பு விலை';
+$_['column_price']               = 'விலை';
+$_['column_total']               = 'மொத்தம்';
+$_['column_comment']             = 'கருத்து';
+$_['column_date_added']          = 'சேர்த்த தேதி';
+
+// Button
+$_['button_cancel']              = 'சந்தாவை ரத்துசெய்க';
+
+// Error
+$_['error_subscription']         = 'சந்தாவைக் கண்டறிய முடியவில்லை!';
+$_['error_duration']             = 'மேலும் %s வாங்கக்கேட்கும் வரை சந்தாவை ரத்துசெய்ய முடியாது!';
+$_['error_canceled']             = 'சந்தா முன்பே ரத்துசெய்யப்பட்டது!';

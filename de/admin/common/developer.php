117530232753a9b0f91494fd3249ceb85b02fbba
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-17 01:59:27 +0800

    New translations developer.php (German)
    [ci skip]

--- de/admin/common/developer.php
+++ de/admin/common/developer.php
@@ -3,23 +3,40 @@
 $_['heading_title']        = 'Entwickleroptionen';
 
 // Text
-$_['text_rebuild_success'] = 'Success: You have cleared the HTML files!';
-$_['text_theme_success']   = 'Erfolg: Sie haben den Theme-Cache geleert!';
-$_['text_sass_success']    = 'Success: Generating the catalog SASS added to the task queue!';
+$_['text_ssr']             = 'Static Site Rendering';
+$_['text_storage']         = 'Local Storage';
 
+$_['text_success']         = 'Success: You have added the Static Site Rendering tasks!';
+$_['text_clear_success']   = 'Success: You have cleared the HTML files!';
 $_['text_cache_success']   = 'Erfolg: Sie haben den Cache geleert!';
 $_['text_vendor_success']  = 'Success: You have modified the vendor cache!';
-
-$_['text_rebuild']         = 'Rebuild Store';
-$_['text_template']        = 'Rebuild Templates';
-$_['text_sass']            = 'Rebuild Stylesheet';
-$_['text_clear']           = 'Clear All';
+$_['text_run']             = 'Run';
+$_['text_clear']           = 'Klar';
 $_['text_cache']           = 'Zwischenspeicher';
 $_['text_vendor']          = 'Verkäufer';
-
-// Column
-$_['column_store']         = 'Online-Shop (Store)';
-$_['column_action']        = 'Aktionen';
+$_['text_article']         = 'Artikel';
+$_['text_banner']          = 'Banner';
+$_['text_category']        = 'Kategorien';
+$_['text_comment']         = 'Kommentare';
+$_['text_country']         = 'Länder';
+$_['text_currency']        = 'Währungen';
+$_['text_customer_group']  = 'Kundengruppen';
+$_['text_filter']          = 'Filter';
+$_['text_information']     = 'Informationen';
+$_['text_language']        = 'Sprache';
+$_['text_location']        = 'Standort';
+$_['text_manufacturer']    = 'Hersteller';
+$_['text_product']         = 'Produkt';
+$_['text_return_reason']   = 'Rückgabegrund';
+$_['text_review']          = 'Review';
+$_['text_sass']            = 'SASS';
+$_['text_setting']         = 'Setting';
+$_['text_store']           = 'Online-Shop (Store)';
+$_['text_tag']             = 'Tagsw';
+$_['text_tax_rate']        = 'Steuersätze';
+$_['text_template']        = 'Template';
+$_['text_topic']           = 'Thema';
+$_['text_translation']     = 'Translation';
 
 // Help
 $_['help_cache']           = 'Clears all stored cache data files.';

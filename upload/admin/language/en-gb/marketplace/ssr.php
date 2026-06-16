efed1414cd107deb3055f6fe60b58c06a29917b3
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-16 23:31:09 +0800

    Create ssr.php

---
+++ upload/admin/language/en-gb/marketplace/ssr.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']        = 'Static Site Rendering';
+
+// Text
+$_['text_success']         = 'Success: You have modified static site rendering!';
+$_['text_instruction']     = 'SSR Instructions';
+$_['text_list']            = 'SSR List';
+$_['text_info']            = 'SSR Information';
+
+// Column
+$_['column_code']          = 'SSR Code';
+$_['column_status']        = 'Status';
+$_['column_date_modified'] = 'Last Run';
+$_['column_action']        = 'Action';
+
+// Button
+$_['button_run']           = 'Render';
+
+// Error
+$_['error_permission']     = 'Warning: You do not have permission to modify SSR!';
+$_['error_exists']         = 'SSR does not exist!';

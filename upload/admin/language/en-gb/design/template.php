f8e1109af9d1df777dd03b68baccbce25afff968
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-24 18:29:52 +0800

    Update template.php

--- upload/admin/language/en-gb/design/template.php
+++ upload/admin/language/en-gb/design/template.php
@@ -4,12 +4,13 @@ $_['heading_title']     = 'Template Editor';
 
 // Text
 $_['text_success']      = 'Success: You have modified templates!';
+$_['text_list']         = 'Translation List';
 $_['text_add']          = 'Add Template';
 $_['text_edit']         = 'Edit Template';
 $_['text_filter']       = 'Filter';
 $_['text_extension']    = 'Extensions';
 $_['text_code']         = 'Template Code Editor';
-$_['text_twig']         = 'The theme editor uses the template language Twig. You can read about <a href="https://twig.symfony.com/doc/" target="_blank" class="alert-link">Twig syntax here</a>.';
+$_['text_curlytag']     = 'The theme editor uses the template language CurlyTag. You can read about <a href="https://www.curlytag.com/" target="_blank" class="alert-link">CurlyTag syntax here</a>.';
 
 // Column
 $_['column_store']      = 'Store';

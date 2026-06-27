5d4deb6e53c9da31caa5af4aa2c9de4c0727a8c4
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-27 20:34:20 +0800

    Update menu.js

--- upload/catalog/view/javascript/common/menu.js
+++ upload/catalog/view/javascript/common/menu.js
@@ -1,6 +1,9 @@
 import { Controller } from '../component.js';
 import { loader } from '../index.js';
 
+// Config
+const config = await loader.config('default');
+
 // Language
 const language = await loader.language('common/menu');
 
@@ -11,10 +14,28 @@ export default class extends Controller {
     async render() {
         let data = {};
 
-        data.categories = categories;
+        data.categories = [];
 
-        console.log('menu');
-        console.log(categories);
+        for (let category of categories) {
+            let children_data = [];
+
+            for (let child of category.children) {
+                children_data.push({
+                    name: child.description[config.config_language].name,
+                    path: child.path,
+                    product_total: child.product_total
+                });
+            }
+
+            data.categories.push({
+                name: category.description[config.config_language].name,
+                path: category.path,
+                children: children_data,
+                product_total: category.product_total
+            });
+        }
+
+        console.log(data);
 
         return loader.template('common/menu', { ...data,  ...language });
     }

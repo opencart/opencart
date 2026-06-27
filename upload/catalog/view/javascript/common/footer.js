d76fa89377e8081251e8c11420d1237076ff08e5
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-27 20:34:24 +0800

    Update footer.js

--- upload/catalog/view/javascript/common/footer.js
+++ upload/catalog/view/javascript/common/footer.js
@@ -11,7 +11,7 @@ const language = await loader.language('common/footer');
 const articles = await loader.storage('cms/article-1');
 
 // Information
-const informations = await loader.storage('information/information');
+const informations = await loader.storage('catalog/information');
 
 export default class extends Controller {
     async render() {
@@ -21,16 +21,27 @@ export default class extends Controller {
         data.articles = Object.values(articles).length;
 
         // Information Pages
-        data.informations = Object.values(informations);
+        data.informations = [];
 
-        data.gdpr = config.config_gdpr_id ? true : false;
-        data.affiliate = config.config_affiliate_status ? true : false;
+        if (informations.length && config.config_language in informations.description) {
 
-        let date = new Date();
+            for (let information of informations) {
+                information.description[config.config_language];
 
-        data.year = date.getFullYear();
+                console.log(information);
 
-        return await loader.template('common/footer', { ...data, ...language, ...config });
+                data.informations.push(information);
+            }
+
+            data.gdpr = config.config_gdpr_id ? true : false;
+            data.affiliate = config.config_affiliate_status ? true : false;
+
+            let date = new Date();
+
+            data.year = date.getFullYear();
+
+            return await loader.template('common/footer', {...data, ...language, ...config});
+        }
     }
 
     onClick(e) {

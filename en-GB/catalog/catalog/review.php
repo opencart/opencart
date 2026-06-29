48e6446ceb317a706c7f93e5e7ae82d52255102c
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-30 04:15:35 +0800

    New translations review.php (English, United Kingdom)
    [ci skip]

---
+++ en-GB/catalog/catalog/review.php
@@ -0,0 +1,28 @@
+<?php
+// Text
+$_['text_reviews']    = '%s reviews';
+$_['text_write']      = 'Write a review';
+$_['text_login']      = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
+$_['text_no_results'] = 'Be the first to write a review for this product.';
+$_['text_note']       = '<span class="text-danger">Note:</span> HTML is not translated!';
+$_['text_success']    = 'Thank you for your review. It has been submitted to the webmaster for approval.';
+
+// Entry
+$_['entry_author']     = 'Your Name';
+$_['entry_review']     = 'Your Review';
+$_['entry_rating']     = 'Rating';
+$_['entry_good']       = 'Good';
+$_['entry_bad']        = 'Bad';
+
+// Tab
+$_['tab_review']       = 'Reviews (%s)';
+
+// Error
+$_['error_token']      = 'Warning: Review token invalid!';
+$_['error_product']    = 'Warning: Product could not be found!';
+$_['error_author']     = 'Review Name must be between 3 and 25 characters!';
+$_['error_text']       = 'Review Text must be between 25 and 1000 characters!';
+$_['error_rating']     = 'Please select a review rating!';
+$_['error_login']      = 'You must login to review the product!';
+$_['error_purchased']  = 'You must have purchased this product before you can write a review!';
+$_['error_status']     = 'Reviews are disabled!';

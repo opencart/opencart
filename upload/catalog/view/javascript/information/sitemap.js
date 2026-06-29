0abb3d28a8e755b1c93abfbeb432f9c570b6c525
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-06-30 04:08:39 +0800

    Update sitemap.js

--- upload/catalog/view/javascript/information/sitemap.js
+++ upload/catalog/view/javascript/information/sitemap.js
@@ -1,14 +1,41 @@
 import { Controller } from '../component.js';
 import { loader } from '../index.js';
 
+// Config
+const config = await loader.config('default');
+
 // Language
 const language = await loader.language('information/sitemap');
 
+// Storage
+const categories = await loader.storage('catalog/category');
+
 export default class extends Controller {
-    render() {
+    async render() {
         let data = {};
 
-        data.categories = {};
+        data.categories = [];
+
+        for (let category of categories) {
+            let children = [];
+
+            for (let child of category.children) {
+                children.push({
+                    name: child.description[config.config_language].name,
+                    path: child.path,
+                    product_total: child.product_total
+                });
+            }
+
+            data.categories.push({
+                name: category.description[config.config_language].name,
+                path: category.path,
+                children: children,
+                product_total: category.product_total
+            });
+        }
+
+        data.config_product_count = config.config_product_count;
 
         return loader.template('information/sitemap', { ...data, ...language });
     }

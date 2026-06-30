a2b39de2c02c9645bbca557e2fb193840d776346
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-01 00:38:30 +0800

    Update include.js

--- upload/assets/framework/component/include.js
+++ upload/assets/framework/component/include.js
@@ -19,19 +19,11 @@ customElements.define('x-include', class extends WebComponent {
         // Get the source HTML to load
         if (!this.src) return;
 
-        console.log(this.src);
+        //console.log(this.src);
 
         let object = await import(config.config_path + this.src);
 
-        console.log(object);
-
         let controller = new object.default(this);
-
-        //controller.set(attributes);
-
-
-        //controller.setAttribute
-
 
         let output = await controller.render();
 

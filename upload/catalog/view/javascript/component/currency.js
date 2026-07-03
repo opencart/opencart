29e18e3770e54f62280d270d3ab5d43df7e425f6
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-03 23:33:34 +0800

    Create currency.js

---
+++ upload/catalog/view/javascript/component/currency.js
@@ -0,0 +1,54 @@
+import { WebComponent } from '../component.js';
+import { loader } from '../index.js';
+
+// library
+const local = await loader.library('local');
+
+// Config
+const config = await loader.config('default');
+
+// Language
+const language = await loader.language('component/currency');
+
+// Storage
+const currencies = await loader.storage('localisation/currency');
+
+customElements.define('component-currency', class extends WebComponent {
+    async render() {
+        let data = {};
+
+        // Config stored currency code
+        data.code = config.config_currency;
+
+        // Local storage currency code
+        if (local.has('currency')) {
+            data.code = local.get('currency');
+        }
+
+        if (data.code in currencies) {
+            data.symbol_left = currencies[data.code].symbol_left;
+            data.symbol_right = currencies[data.code].symbol_right;
+        } else {
+            data.symbol_left = '';
+            data.symbol_right = '';
+        }
+
+        data.currencies = Object.values(currencies);
+
+        return loader.template('component/currency', { ...data, ...language });
+    }
+
+    onClick(e) {
+        e.preventDefault();
+
+        let code = e.target.getAttribute('href');
+
+        local.set('currency', code);
+
+        let elements = document.querySelectorAll('x-currency');
+
+        for (let element of elements) {
+
+        }
+    }
+});

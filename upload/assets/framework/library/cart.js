983bd0e316c85d54dbe3562518befe8b8b17c0b8
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-07 05:14:24 +0800

    Update cart.js

--- upload/assets/framework/library/cart.js
+++ upload/assets/framework/library/cart.js
@@ -1,13 +1,13 @@
 import { loader } from './loader.js';
 
 // library
-const session = await loader.library('session');
-const tax = await loader.library('tax');
+//const session = await loader.library('session');
+//const tax = await loader.library('tax');
 
 // Config
 //this.config = loader.config('catalog');
 
-const data = session.get('cart');
+//const data = session.get('cart');
 
 export default class Cart {
     constructor() {

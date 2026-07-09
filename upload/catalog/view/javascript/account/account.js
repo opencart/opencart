448775e49f9d3cdc769c759cb8ea8124ea0e839d
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-09 18:36:56 +0800

    Update account.js

--- upload/catalog/view/javascript/account/account.js
+++ upload/catalog/view/javascript/account/account.js
@@ -7,4 +7,12 @@ export default class extends Controller {
     render() {
         return loader.template('account/account', language);
     }
+
+    onClick(e) {
+        e.preventDefault();
+
+        let target = document.getElementById('content');
+
+        target.src = e.target.getAttribute('href');
+    }
 };

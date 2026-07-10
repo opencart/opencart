80fda6ba3d4a5f2fe6d858519f6d572348a24795
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-10 18:51:27 +0800

    New translations product_list.php (German)
    [ci skip]

--- de/catalog/catalog/product_list.php
+++ de/catalog/catalog/product_list.php
@@ -1,1 +1,16 @@
 <?php
+// Text
+$_['text_product']     = 'Produkte';
+$_['text_no_results']  = 'There are no products to list in this category.';
+$_['text_compare']     = 'Produktvergleich (%s)';
+$_['text_sort']        = 'Sortieren nach';
+$_['text_default']     = 'Standard';
+$_['text_name_asc']    = 'Name (A - Z)';
+$_['text_name_desc']   = 'Name (Z - A)';
+$_['text_price_asc']   = 'Preis (Niedrig &gt; Hoch)';
+$_['text_price_desc']  = 'Preis (Hoch &gt; Niedrig)';
+$_['text_rating_asc']  = 'Bewertung (niedrigste)';
+$_['text_rating_desc'] = 'Bewertung (Höchste)';
+$_['text_model_asc']   = 'Modell (A - Z)';
+$_['text_model_desc']  = 'Modell (Z - A)';
+$_['text_limit']       = 'Zeigen';

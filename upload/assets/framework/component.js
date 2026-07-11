a7b44d07dedb6854496a615a75997cb7de9021e8
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-11 16:26:16 +0800

    Update component.js

--- upload/assets/framework/component.js
+++ upload/assets/framework/component.js
@@ -1,19 +1,21 @@
 export { WebComponent } from './library/webcomponent.js';
 export { Controller } from './library/controller.js';
 
-// General
-//import('./component/alert.js');
-import('./component/date.js');
-import('./component/modal.js');
-import('./component/include.js');
-import('./component/pagination.js');
-import('./component/price.js');
+document.addEventListener('DOMContentLoaded', async () => {
+    // General
+    //import('./component/alert.js');
+    import('./component/date.js');
+    import('./component/modal.js');
+    import('./component/include.js');
+    import('./component/pagination.js');
+    import('./component/price.js');
 
-// Input
-import('./input/autocomplete.js');
-import('./input/checkbox.js');
-//import('./input/markdown.js');
-import('./input/country.js');
-import('./input/zone.js');
-//import('./input/file.js');
-import('./input/switch.js');
+    // Input
+    import('./input/autocomplete.js');
+    import('./input/checkbox.js');
+    //import('./input/markdown.js');
+    import('./input/country.js');
+    import('./input/zone.js');
+    //import('./input/file.js');
+    import('./input/switch.js');
+});

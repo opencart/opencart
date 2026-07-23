087708daf314adcaa0a3147272548acd70d85ec3
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-24 06:06:06 +0800

    Create article_list.js

---
+++ upload/catalog/view/javascript/cms/article_list.js
@@ -0,0 +1,61 @@
+import { WebComponent } from '../component.js';
+import { loader } from '../index.js';
+
+// Config
+const config = await loader.config('default');
+
+// Language
+const language = await loader.language('catalog/article_list');
+
+customElements.define('article-list', class extends WebComponent {
+    async render() {
+        let data = {};
+
+        if (this.hasAttribute('search')) {
+            data.search = this.getAttribute('search');
+        } else {
+            data.search = '';
+        }
+
+        if (this.hasAttribute('topic_id')) {
+            data.topic_id = this.getAttribute('topic_id');
+        } else {
+            data.topic_id = 0;
+        }
+
+        if (this.hasAttribute('sort')) {
+            data.sort = this.getAttribute('sort');
+        } else {
+            data.sort = '';
+        }
+
+        if (this.hasAttribute('order')) {
+            data.order = this.getAttribute('order');
+        } else {
+            data.order = '';
+        }
+
+        if (this.hasAttribute('limit')) {
+            data.limit = this.getAttribute('limit');
+        } else {
+            data.limit = 10;
+        }
+
+        if (this.hasAttribute('page')) {
+            data.page = this.getAttribute('page');
+        } else {
+            data.page = '';
+        }
+
+        // Product Info
+        data.articles = await loader.storage('cms/topic-article-' + this.getAttribute('topic_id'));
+
+        console.log(data.articles);
+
+        return loader.template('catalog/article_list', { ...data, ...language, ...config });
+    }
+
+    onChange(e) {
+        this.setAttribute('sort');
+    }
+});

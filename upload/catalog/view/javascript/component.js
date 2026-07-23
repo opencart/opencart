6737c853ca5c0f186fe4286a8ff0abe1f02af088
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-24 06:07:14 +0800

    Update component.js

--- upload/catalog/view/javascript/component.js
+++ upload/catalog/view/javascript/component.js
@@ -1,9 +1,1 @@
 export { WebComponent, Controller } from '../../../assets/framework/component.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-    import('./component/currency.js');
-    import('./component/language.js');
-    import('./component/search.js');
-    import('./component/cart.js');
-    import('./component/menu.js');
-});

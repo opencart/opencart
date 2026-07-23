6af8a157ab84b25a860a5b34886addd3406bfbe1
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-24 06:07:09 +0800

    Create article_list.php

---
+++ upload/catalog/language/en-gb/cms/article_list.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']        = 'Article';
+
+// Text
+$_['text_by']              = 'By';
+$_['text_tags']            = 'Tags:';
+$_['text_comment']         = 'comments';
+$_['text_rating_asc']      = 'Rating (Lowest)';
+$_['text_rating_desc']     = 'Rating (Highest)';
+$_['text_date_added_asc']  = 'Date Added (Oldest)';
+$_['text_date_added_desc'] = 'Date Added (Latest)';

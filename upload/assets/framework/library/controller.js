19fff4f441ced76b0f7424c1e4f4e49e12138fb0
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-24 06:04:50 +0800

    Update controller.js

--- upload/assets/framework/library/controller.js
+++ upload/assets/framework/library/controller.js
@@ -1,3 +1,18 @@
+let event = [
+    // Window Event Attributes
+    'onafterprint',
+    'onbeforeprint',
+    'onbeforeunload',
+    'onerror',
+    'onhashchange',
+    'onload',
+    'onoffline',
+    'ononline',
+    'onpageshow',
+    'onresize',
+    'onunload'
+];
+
 export class Controller {
     element = HTMLElement;
 
@@ -6,31 +21,35 @@ export class Controller {
     }
 
     async execute() {
-        let output = await this.render();
+        let template = document.createElement('template');
 
-        if (output) {
-            this.element.innerHTML = output;
+        template.innerHTML = await this.render();
 
-            // Autoload any custom elements not already loaded
-            this.element.querySelectorAll('[data-bind], [data-on]').forEach(element => {
-                // Attach Events based on elements that have data-bind attributes
-                if (element.hasAttribute('data-bind')) {
-                    this['$' + element.getAttribute('data-bind')] = element;
+        let clone = template.content.cloneNode(true);
 
-                    element.removeAttribute('data-bind');
+        // Autoload any custom elements not already loaded
+        clone.querySelectorAll('[data-bind], [data-on]').forEach(element => {
+            console.log(element);
+
+            // Attach Events based on elements that have data-bind attributes
+            if (element.hasAttribute('data-bind')) {
+                this['$' + element.getAttribute('data-bind')] = element;
+
+                element.removeAttribute('data-bind');
+            }
+
+            // Attach events based on elements that have data-on attributes
+            if (element.getAttribute('data-on')) {
+                let [event, method] = element.getAttribute('data-on').split(':');
+
+                if (method in this) {
+                    element.addEventListener(event, this[method].bind(this));
                 }
 
-                // Attach events based on elements that have data-on attributes
-                if (element.getAttribute('data-on')) {
-                    let [event, method] = element.getAttribute('data-on').split(':');
+                element.removeAttribute('data-on');
+            }
+        });
 
-                    if (method in this) {
-                        element.addEventListener(event, this[method].bind(this));
-                    }
-
-                    element.removeAttribute('data-on');
-                }
-            });
-        }
+        return clone;
     }
 }

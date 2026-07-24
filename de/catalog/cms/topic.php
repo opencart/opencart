7a4ec01aa0e2e4f1ff7ac364f50274e48db53c48
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-25 05:49:36 +0800

    New translations topic.php (German)
    [ci skip]

--- de/catalog/cms/topic.php
+++ de/catalog/cms/topic.php
@@ -1,22 +1,13 @@
 <?php
 // Heading
-$_['heading_title']        = 'Der Blog';
+$_['heading_title']   = 'Der Blog';
 
 // Text
-$_['text_refine']          = 'Suche verfeinern';
-$_['text_all']             = 'Alle';
-$_['text_blog']            = 'Der Blog';
-$_['text_by']              = 'Von';
-$_['text_comment']         = 'Kommentare';
-$_['text_no_results']      = 'Es sind keine Artikel vorhanden.';
-$_['text_sort']            = 'Sortieren nach';
-$_['text_rating_asc']      = 'Bewertung (niedrigste)';
-$_['text_rating_desc']     = 'Bewertung (Höchste)';
-$_['text_date_added_asc']  = 'Hinzugefügt am (älteste)';
-$_['text_date_added_desc'] = 'Hinzugefügt am (aktuellstes)';
+$_['text_refine']     = 'Suche verfeinern';
+$_['text_all']        = 'Alle';
 
 // Entry
-$_['entry_search']         = 'Suchen';
+$_['entry_search']    = 'Suchen';
 
 // Button
-$_['button_continue']      = 'Weiterlesen';
+$_['button_continue'] = 'Weiterlesen';

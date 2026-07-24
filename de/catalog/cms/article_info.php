11455bd184d803353be35ca711c4dce0a14af132
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-25 05:49:38 +0800

    New translations article_info.php (German)
    [ci skip]

--- de/catalog/cms/article_info.php
+++ de/catalog/cms/article_info.php
@@ -1,13 +1,7 @@
 <?php
-// Heading
-$_['heading_title']        = 'Artikel';
-
 // Text
-$_['text_blog']            = 'Der Blog';
-$_['text_by']              = 'Von';
-$_['text_tags']            = 'Stichworte:';
-$_['text_comment']         = 'Kommentare';
-$_['text_rating_asc']      = 'Bewertung (niedrigste)';
-$_['text_rating_desc']     = 'Bewertung (Höchste)';
-$_['text_date_added_asc']  = 'Hinzugefügt am (älteste)';
-$_['text_date_added_desc'] = 'Hinzugefügt am (aktuellstes)';
+$_['text_by']         = 'Von';
+$_['text_tags']       = 'Stichworte:';
+$_['text_comment']    = 'Kommentare';
+$_['text_rating']     = 'Bewertungen';
+$_['text_date_added'] = 'Datum hinzugefügt';

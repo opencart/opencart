26e1b1d7d2c0a48e6504f77e96610ac3cda86189
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-25 05:14:28 +0800

    Update article_list.php

--- upload/catalog/language/en-gb/cms/article_list.php
+++ upload/catalog/language/en-gb/cms/article_list.php
@@ -1,12 +1,8 @@
 <?php
-// Heading
-$_['heading_title']        = 'Article';
-
 // Text
-$_['text_by']              = 'By';
-$_['text_tags']            = 'Tags:';
-$_['text_comment']         = 'comments';
-$_['text_rating_asc']      = 'Rating (Lowest)';
-$_['text_rating_desc']     = 'Rating (Highest)';
-$_['text_date_added_asc']  = 'Date Added (Oldest)';
-$_['text_date_added_desc'] = 'Date Added (Latest)';
+$_['text_sort']       = 'Sort By';
+$_['text_by']         = 'By';
+$_['text_comment']    = 'Comments';
+$_['text_popularity'] = 'Popularity';
+$_['text_date_added'] = 'Latest';
+$_['text_no_results'] = 'There are no articles.';

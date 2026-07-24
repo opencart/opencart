e444b972954252d3fcb8cc071bcd4fdcd660c48c
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-25 05:14:19 +0800

    Update topic.js

--- upload/catalog/view/javascript/cms/topic.js
+++ upload/catalog/view/javascript/cms/topic.js
@@ -8,28 +8,49 @@ const config = await loader.config('default');
 // Language
 const language = await loader.language('cms/topic');
 
+
+// Storage
+let topics = await loader.storage('topic/topic');
+
 export default class extends Controller {
     async render() {
         let data = {};
 
         let request = new URL(import.meta.url).searchParams;
 
-        let topic_id = 0;
+       data.topic_id = 0;
 
         if (request.has('topic_id')) {
-            topic_id = request.get('topic_id');
+            data.topic_id = request.get('topic_id');
         }
 
-        let topic = await loader.storage('topic/topic-' + topic_id);
+        data.heading_title = language.heading_title;
+        data.image = '';
+        data.description = '';
+
+        // If Topic ID is set
+        let topic = await loader.storage('topic/topic-' + data.topic_id);
 
         if (topic !== undefined && config.config_language in topic.description) {
-            data.topic_id = topic.topic_id;
             data.image = topic.image;
 
             let description = topic.description[config.config_language];
 
             data.name = description.name;
             data.description = description.description;
+        }
+
+        data.topics = [];
+
+        for (let topic of topics) {
+            if (config.config_language in topic.description) {
+                let description = topic.description[config.config_language];
+
+                data.topics.push({
+                    topic_id: topic.topic_id,
+                    name: description.name
+                });
+            }
         }
 
         return loader.template('cms/topic', { ...data, ...language });

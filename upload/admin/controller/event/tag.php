bf668dd657154f13e8fce4cf6cc18420f7835aa5
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-07-27 22:45:48 +0800

    Update tag.php

--- upload/admin/controller/event/tag.php
+++ upload/admin/controller/event/tag.php
@@ -1,1 +1,169 @@
 <?php
+namespace Opencart\Admin\Controller\Event;
+/**
+ * Class Tag
+ *
+ * @package Opencart\Admin\Controller\Event
+ */
+class Tag extends \Opencart\System\Engine\Controller {
+	/*
+	 * Add Tag
+	 *
+	 * Adds task to generate new tag data.
+	 *
+	 * Trigger admin/model/catalog/tag.addTag/after
+	 *
+	 * @param string                $route
+	 * @param array<string, string> $args
+	 * @param array<string, string> $output
+	 *
+	 * @return void
+	 */
+	public function addTag(string &$route, array &$args, &$output): void {
+		$this->load->model('setting/task');
+
+		$this->load->model('setting/store');
+
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
+
+		$this->load->model('catalog/tag');
+
+		$tag_ids = $this->model_catalog_tag->getTags((int)$args['tag_id']);
+
+		foreach ($store_ids as $store_id) {
+			$task_data = [
+				'code'   => 'tag.info.' . $store_id . '.' . $output,
+				'action' => 'task/catalog/tag.info',
+				'args'   => [
+					'tag_id'   => $output,
+					'store_id' => $store_id
+				]
+			];
+
+			$this->model_setting_task->addTask($task_data);
+
+			// Added filter product tasks
+			foreach ($tag_ids as $tag_id) {
+				$task_data = [
+					'code'   => 'tag.product.' . $store_id . '.' . $tag_id,
+					'action' => 'task/catalog/tag.product',
+					'args'   => [
+						'tag_id'   => $tag_id,
+						'store_id' => $store_id
+					]
+				];
+
+				$this->model_setting_task->addTask($task_data);
+			}
+		}
+	}
+
+	/*
+	 * Edit Filter
+	 *
+	 * Adds task to generate new filter data.
+	 *
+	 * Trigger admin/model/catalog/filter_group.editFilterGroup/after
+	 *
+	 * @param string                $route
+	 * @param array<string, string> $args
+	 * @param array<string, string> $output
+	 *
+	 * @return void
+	 */
+	public function editFilter(string &$route, array &$args): void {
+		$this->load->model('setting/task');
+
+		// Filters
+		$filter_ids = [];
+
+		if (isset($args[1]['filter'])) {
+			$filter_ids = array_column((array)$args[1]['filter'], 'filter_id');
+		}
+
+		$this->load->model('catalog/filter');
+
+		$remove_ids = array_diff(array_column($this->model_catalog_filter->getFilters(['filter_group_id' => $args[0]]), 'filter_id'), $filter_ids);
+
+		$this->load->model('setting/store');
+
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
+
+		foreach ($store_ids as $store_id) {
+			$task_data = [
+				'code'   => 'filter.info.' . $store_id . '.' . $args[0],
+				'action' => 'task/catalog/filter.info',
+				'args'   => [
+					'filter_group_id' => $args[0],
+					'store_id'        => $store_id
+				]
+			];
+
+			$this->model_setting_task->addTask($task_data);
+
+			foreach ($remove_ids as $remove_id) {
+				$task_data = [
+					'code'   => 'filter.deleteFilter.' . $store_id . '.' . $remove_id,
+					'action' => 'task/catalog/filter.deleteFilter',
+					'args'   => [
+						'filter_id' => $remove_id,
+						'store_id'  => $store_id
+					]
+				];
+
+				$this->model_setting_task->addTask($task_data);
+			}
+		}
+	}
+
+	/*
+	 * Delete Filter
+	 *
+	 * Adds task to generate new filter data.
+	 *
+	 * Trigger admin/model/catalog/filter_group.deleteFilterGroup/after
+	 *
+	 * @param string                $route
+	 * @param array<string, string> $args
+	 * @param array<string, string> $output
+	 *
+	 * @return void
+	 */
+	public function deleteFilter(string &$route, array &$args, &$output): void {
+		$this->load->model('setting/task');
+
+		$this->load->model('catalog/filter');
+
+		$filters = $this->model_catalog_filter->getFilters((int)$args['filter_group_id']);
+
+		$this->load->model('setting/store');
+
+		$store_ids = [0, ...array_column($this->model_setting_store->getStores(), 'store_id')];
+
+		foreach ($store_ids as $store_id) {
+			$task_data = [
+				'code'   => 'filter.delete.' . $store_id . '.' . $args[0],
+				'action' => 'task/catalog/filter.delete',
+				'args'   => [
+					'filter_group_id' => $args[0],
+					'store_id'        => $store_id
+				]
+			];
+
+			$this->model_setting_task->addTask($task_data);
+
+			foreach ($filters as $filter) {
+				$task_data = [
+					'code'   => 'filter.deleteFilter.' . $store_id . '.' . $filter['filter_id'],
+					'action' => 'task/catalog/filter.deleteFilter',
+					'args'   => [
+						'filter_id' => $filter['filter_id'],
+						'store_id'  => $store_id
+					]
+				];
+
+				$this->model_setting_task->addTask($task_data);
+			}
+		}
+	}
+}

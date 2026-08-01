285461886e204acd15eb3c9c07c74ad021431591
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-02 02:19:29 +0800

    New translations tag.php (German)
    [ci skip]

---
+++ de/admin/task/catalog/tag.php
@@ -0,0 +1,12 @@
+<?php
+// Text
+$_['text_list']       = 'Generating tag information list!';
+$_['text_info']       = 'Generating %s tag information!';
+$_['text_delete']     = 'Tag information deleted!';
+
+// Error
+$_['error_required']  = 'Error: Tag ID required!';
+$_['error_store']     = 'Error: Store could not be found!';
+$_['error_topic']     = 'Error: Tag does not exist!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';

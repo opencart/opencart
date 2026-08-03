60364b6e47f94289ff47f8083612be421ab73a5d
Anton <stalker780@users.noreply.github.com>
2026-08-03 23:28:47 +0300

    Add session check before captcha generation
    Fix PHP 8.1 warning:
    PHP Unknown:  strlen(): Passing null to parameter #1 ($string) of type string is deprecated in /public_html/catalog/controller/extension/captcha/basic.php on line 48

--- upload/catalog/controller/extension/captcha/basic.php
+++ upload/catalog/controller/extension/captcha/basic.php
@@ -25,6 +25,8 @@ class ControllerExtensionCaptchaBasic extends Controller {
 	}
 
 	public function captcha() {
+		if (!isset($this->session->data['captcha'])) exit();
+
 		$image = imagecreatetruecolor(150, 35);
 
 		$width = imagesx($image);

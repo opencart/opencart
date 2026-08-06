c7ecd9f5fa29a369ef793c79398c82a58ec62a0c
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-06 22:07:40 +0800

    Update cart.js

--- upload/catalog/view/javascript/checkout/cart.js
+++ upload/catalog/view/javascript/checkout/cart.js
@@ -24,13 +24,113 @@ export default class extends Controller {
     async render() {
         let data = {};
 
-        data.products = await cart.getProducts();
+        data.products = cart.getProducts();
 
         console.log(data.products);
+
+        let shipping = await cart.hasShipping();
+        let download = await cart.hasDownload();
+        let minimum = await cart.hasMinimum();
 
         data.currency = currency;
 
         return await loader.template('checkout/cart', { ...data,  ...language });
+    }
+
+    async add(e) {
+        e.preventDefault();
+
+        console.log('addToCart');
+
+        //this.$button_cart.state = 'loading';
+
+        let target = e.target;
+
+        let form = new FormData(target);
+
+        let response = await fetch('index.php?route=checkout/cart.add', {
+            method: 'POST',
+            body: form
+        });
+
+        if (!response.ok) {
+            console.log(response);
+
+            //throw new Error(response.thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+        }
+
+        let json = await response.json();
+
+        // Remove past error classes from inputs
+        target.querySelectorAll('.is-invalid').forEach(element => element.classList.remove('is-invalid'));
+        target.querySelectorAll('.invalid-feedback').forEach(element => element.classList.remove('d-block'));
+
+        // Display error messages
+        if (json['error'] !== undefined) {
+            for (let key in json['error']) {
+                let value = key.replaceAll('_', '-');
+
+                let input = target.querySelector('#input-' + value);
+
+                if (input) {
+                    input.classList.add('is-invalid');
+
+                    // If the element has inputs inside.
+                    input.querySelectorAll('.form-control, .form-select, .form-check-input, .form-check-label').forEach(element => element.classList.add('is-invalid'));
+                }
+
+                let error = target.querySelector('#error-' + value);
+
+                if (error) {
+                    error.classList.add('d-block');
+                }
+            }
+        }
+
+        // Display success message
+        if (json['success'] !== undefined) {
+            let alert = target.querySelector('#alert');
+
+            if (alert) {
+                alert.prepend('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json['success'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+            }
+
+            let output = [];
+
+            let regex = /^([^\[]+)\[(\w+)\]$/g;
+
+            for (let [ key, value] of form.entries()) {
+                // Get Options. JavaScript is terrible!
+                let matches = key.match(/^(.*?)\[(.*?)\]/);
+
+                console.log('form.entries');
+                console.log(key);
+                console.log(value);
+                console.log(matches);
+
+                if (matches) {
+                    let [ test, match] = matches;
+
+                    console.log('match');
+                    console.log(test);
+                    console.log(match);
+
+                    output[match] = value;
+                }
+            }
+
+            let { product_id, quantity, option, subscription_plan_id } = Object.fromEntries(form);
+
+            console.log(Object.fromEntries(form));
+
+            cart.add(1, product_id, quantity, option, subscription_plan_id);
+
+            let button = document.querySelector('#cart > button');
+
+            button.click();
+        }
+
+        //this.$button_cart.state = '';
     }
 
     onClick(e) {

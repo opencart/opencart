6d5eff3f5e7820bc05b128ada619beae148d98ca
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-14 00:53:31 +0800

    Update product_thumb.js

--- upload/catalog/view/javascript/catalog/product_thumb.js
+++ upload/catalog/view/javascript/catalog/product_thumb.js
@@ -22,29 +22,21 @@ customElements.define('product-thumb', class extends WebComponent {
         let product = await loader.storage('product/product-' + this.getAttribute('product_id'));
 
         if (product !== undefined && config.config_language in product.description) {
-            data.product_id = product.product_id;
-            data.thumb = product.thumb;
-
             let description = product.description[config.config_language];
 
-            data.name = description.name;
             data.description = description.description.substr(0, config.config_product_description_length);
 
-            data.model = product.model;
             data.price = tax.calculate(product.price);
-            data.special = tax.calculate(product.special);
+            //data.special = tax.calculate(product.special);
             data.tax = '';
 
             if (config.config_tax) {
                 data.tax = product.special ? product.special : product.price;
             }
 
-            data.rating = product.rating;
-            data.minimum = product.minimum;
-
             data.currency = currency;
 
-            return await loader.template('catalog/product_thumb', { ...data, ...language, ...config });
+            return await loader.template('catalog/product_thumb', { ...product, ...description, ...data, ...language, ...config });
         }
     }
 

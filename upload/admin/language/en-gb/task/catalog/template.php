f2cbdcc876f76d0ca9a7787ed88b06d0e90c3181
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-18 00:30:23 +0800

    Update template.php

--- upload/admin/language/en-gb/task/catalog/template.php
+++ upload/admin/language/en-gb/task/catalog/template.php
@@ -1,1 +1,12 @@
 <?php
+// Text
+$_['text_task']       = 'Generating template task list!';
+$_['text_list']       = 'Generating %s %s %s template data!';
+
+// Error
+$_['error_required']  = 'Error: %s required!';
+$_['error_store']     = 'Error: Store could not be found!';
+$_['error_language']  = 'Error: Language could not be found!';
+$_['error_directory'] = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']      = 'Error: File %s could not be written!';
+$_['error_exists']    = 'Error: File %s does not exist!';

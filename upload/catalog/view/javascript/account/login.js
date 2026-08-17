448667ed43c5afa0036208b7025c51c5b2150eb2
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-18 00:31:55 +0800

    Update login.js

--- upload/catalog/view/javascript/account/login.js
+++ upload/catalog/view/javascript/account/login.js
@@ -8,48 +8,91 @@ const language = await loader.language('account/login');
 const session = await loader.library('session');
 
 export default class extends Controller {
-    render() {
+    connect() {
+
+    }
+
+    async render() {
         let data = {};
 
         var element = this;
 
-        return loader.template('account/login', { ...data, ...language });
+        return await loader.template('account/login', { ...data, ...language });
     }
 
-    onSubmit(e) {
+    async onSubmit(e) {
         e.preventDefault();
 
-        console.log(e);
+        console.log('addToCart');
 
-        let login = api.fetch({
-            url: this.getAttribute('action'),
-            method: 'post',
-            data: new FormData(this),
-            beforeSend: () => {
-                $('#button-login').button('loading');
-            },
-            afterSend: () => {
-                $('#button-login').button('reset');
-            },
-            success: (json) => {
-                this.querySelector('.alert-dismissible').remove();
+        //this.$button_cart.state = 'loading';
 
-                let alert = document.getElementById('alert');
+        let target = e.target;
 
-                if (json.error !== undefined) {
-                    alert.append('<div class="alert alert-danger alert-dismissible"><i class="fa-solid fa-circle-exclamation"></i> ' + json.error + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
-                }
+        let form = new FormData(target);
 
-                if (json.success !== undefined) {
-                    alert.append('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json.success + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
-
-                    session.set('customer', json.customer);
-                }
-            },
-            error: (xhr, ajaxOptions, thrownError) => {
-                console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
-            }
+        let response = await fetch('index.php?route=checkout/cart.add', {
+            method: 'POST',
+            body: form
         });
+
+        if (!response.ok) {
+            console.log(response);
+
+            //throw new Error(response.thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+        }
+
+        let json = await response.json();
+
+        // Remove past error classes from inputs
+        target.querySelectorAll('.is-invalid').forEach(element => element.classList.remove('is-invalid'));
+        target.querySelectorAll('.invalid-feedback').forEach(element => element.classList.remove('d-block'));
+
+        // Display error messages
+        if (json['error'] !== undefined) {
+            for (let key in json['error']) {
+                let value = key.replaceAll('_', '-');
+
+                let input = target.querySelector('#input-' + value);
+
+                if (input) {
+                    input.classList.add('is-invalid');
+
+                    // If the element has inputs inside.
+                    input.querySelectorAll('.form-control, .form-select, .form-check-input, .form-check-label').forEach(element => element.classList.add('is-invalid'));
+                }
+
+                let error = target.querySelector('#error-' + value);
+
+                if (error) {
+                    error.classList.add('d-block');
+                }
+            }
+        }
+
+        // Display success message
+        if (json['success'] !== undefined) {
+            let alert = target.querySelector('#alert');
+
+            if (alert) {
+                alert.prepend('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json['success'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+            }
+
+            let output = [];
+
+            console.log(json['products']);
+
+            //console.log(Object.fromEntries(form));
+            for (let product of json['products']) {
+                cart.add(product);
+            }
+
+            let button = document.querySelector('#cart > button');
+
+            button.click();
+        }
+
+        // this.$button_cart.state = '';
     }
 };
 

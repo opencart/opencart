7aba5cd39ad23b22f6992046d8311720aeca213a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-18 00:31:52 +0800

    Update register.js

--- upload/catalog/view/javascript/account/register.js
+++ upload/catalog/view/javascript/account/register.js
@@ -25,36 +25,82 @@ export default class extends Controller {
             data.custom_fields = customer_group.custom_fields;
         }
 
-        data.config_telephone_status = config.config_telephone_status;
-
         return loader.template('account/register', { ...data, ...language, ...config });
     }
 
-    onSubmit(e) {
+    async onSubmit(e) {
         e.preventDefault();
 
-        console.log(e);
+        console.log('addToCart');
 
-        loader.request({
-            url: '',
+        //this.$button_cart.state = 'loading';
+
+        let target = e.target;
+
+        let form = new FormData(target);
+
+        let response = await fetch('index.php?route=account/register', {
             method: 'POST',
-            header: [],
-            data: [],
-            accept: [],
-            beforeSend: '',
-            afterSend: '',
-            onComplete: (json) => {
-                let alert = document.getElementById('alert');
+            body: form
+        });
 
-                if (json['error']) {
-                    alert.append('<div class="alert alert-danger alert-dismissible"><i class="fa-solid fa-circle-exclamation"></i> ' + json['error'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+        if (!response.ok) {
+            console.log(response);
+
+            //throw new Error(response.thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+        }
+
+        let json = await response.json();
+
+        // Remove past error classes from inputs
+        target.querySelectorAll('.is-invalid').forEach(element => element.classList.remove('is-invalid'));
+        target.querySelectorAll('.invalid-feedback').forEach(element => element.classList.remove('d-block'));
+
+        // Display error messages
+        if (json['error'] !== undefined) {
+            for (let key in json['error']) {
+                let value = key.replaceAll('_', '-');
+
+                let input = target.querySelector('#input-' + value);
+
+                if (input) {
+                    input.classList.add('is-invalid');
+
+                    // If the element has inputs inside.
+                    input.querySelectorAll('.form-control, .form-select, .form-check-input, .form-check-label').forEach(element => element.classList.add('is-invalid'));
                 }
 
-                if (json['success']) {
-                    alert.append('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json['success'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+                let error = target.querySelector('#error-' + value);
+
+                if (error) {
+                    error.classList.add('d-block');
                 }
             }
-        });
+        }
+
+        // Display success message
+        if (json['success'] !== undefined) {
+            let alert = target.querySelector('#alert');
+
+            if (alert) {
+                alert.prepend('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json['success'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+            }
+
+            let output = [];
+
+            console.log(json['products']);
+
+            //console.log(Object.fromEntries(form));
+            for (let product of json['products']) {
+                cart.add(product);
+            }
+
+            let button = document.querySelector('#cart > button');
+
+            button.click();
+        }
+
+        // this.$button_cart.state = '';
 
     }
 

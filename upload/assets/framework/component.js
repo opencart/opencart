01254c2138915763c3a86139e24b26c33b3aeb17
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-22 03:53:38 +0800

    Update component.js

--- upload/assets/framework/component.js
+++ upload/assets/framework/component.js
@@ -5,6 +5,7 @@ export { Ajax } from './library/ajax.js';
 // General
 //import('./component/alert.js');
 import('./component/button.js');
+import('./component/form.js');
 import('./component/include.js');
 import('./component/pagination.js');
 import('./component/currency.js');

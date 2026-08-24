061aa03a0cb449428008e9871d02c0e811892791
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-24 17:23:03 +0800

    Update source file stock_status.php
    [ci skip]

---
+++ upload/admin/language/en-gb/task/catalog/stock_status.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_list']           = 'Generating stock status information list!';
+
+// Error
+$_['error_required']      = 'Error: %s required!';
+$_['error_store']         = 'Error: Store could not be found!';
+$_['error_return_reason'] = 'Error: Stock Status does not exist!';
+$_['error_directory']     = 'Error: Directory %s does not exist or is not writable!';
+$_['error_file']          = 'Error: File %s could not be written!';

48be219adfb61d5f9a7a3a0a66d8bba667d45577
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-28 00:15:10 +0800

    Create action.php

---
+++ upload/action.php
@@ -0,0 +1,20 @@
+<?php
+// Version
+define('VERSION', '5.0.0.0 Alpha');
+
+// Added dirname function so the system will work from command line.
+if (is_file(dirname(__FILE__)  . '/config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit();
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+// Framework
+require_once(DIR_SYSTEM . 'framework.php');

2df1ed008e1eb1115fb4d519dc378fc1538ff5fb
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-28 00:15:16 +0800

    Update customer.js

--- upload/assets/framework/library/customer.js
+++ upload/assets/framework/library/customer.js
@@ -10,7 +10,7 @@ export default class Customer {
         if (session.has('customer')) {
             let customer = session.get('customer');
 
-            data.set
+            this.data = new Map(customer);
         }
     }
 
@@ -27,48 +27,42 @@ export default class Customer {
     }
 
     getId() {
-        return data.get('customer_id');
+        return this.data.get('customer_id');
     }
 
     getFirstName() {
-        return data.get('firstname');
+        return this.data.get('firstname');
     }
 
     getLastName() {
-        return data.get('lastname');
+        return this.data.get('lastname');
     }
 
     getGroupId() {
-        return data.get('customer_group_id');
+        return this.data.get('customer_group_id');
     }
 
     getEmail() {
-        return data.get('email');
-    }
-
-    addAddress() {
-
+        return this.data.get('email');
     }
 
     getAddressId() {
-
+        return this.data.get('address').find(address => address.default == 1);
     }
 
     getAddress(address_id) {
-        this.address.get();
+        return this.data.get('address').find(address => address.address == address_id);
     }
 
     getToken() {
-        return data.get('token');
+        return this.data.get('token');
     }
 
     getBalance() {
-
-
+        return this.data.get('balance');
     }
 
     getRewardPoints() {
-
-
+        return this.data.get('reward');
     }
 }

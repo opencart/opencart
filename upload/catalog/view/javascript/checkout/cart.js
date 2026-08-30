2c789d7cefb37fb658b800d47458cb172d87a26b
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-08-31 03:02:46 +0800

    Update cart.js

--- upload/catalog/view/javascript/checkout/cart.js
+++ upload/catalog/view/javascript/checkout/cart.js
@@ -8,6 +8,7 @@ const config = await loader.config('default');
 const language = await loader.language('checkout/cart');
 
 // library
+const ajax = await loader.library('ajax');
 const session = await loader.library('session');
 const cart = await loader.library('cart');
 const local = await loader.library('local');
@@ -48,6 +49,75 @@ export default class extends Controller {
 
     editProduct(e) {
         e.preventDefault();
+
+        let target = e.target;
+
+        let form = new FormData(target);
+
+        ajax.post('action.php?route=checkout/cart.add', form, {
+            beforeSend: (request) => {
+                this.bind('button-cart').setAttribute('loading', '');
+            },
+            onComplete: (json) => {
+                console.log(this.bind('button-cart'));
+
+                //this.bind('button-cart').loading = false;
+            },
+            onSuccess: (json) => {
+                console.log('onSuccess', json);
+
+                // Remove past error classes from inputs
+                target.querySelectorAll('.is-invalid').forEach(element => element.classList.remove('is-invalid'));
+                target.querySelectorAll('.invalid-feedback').forEach(element => element.classList.remove('d-block'));
+
+                // Display error messages
+                if (json['error'] !== undefined) {
+                    for (let key in json['error']) {
+                        let value = key.replaceAll('_', '-');
+
+                        let input = target.querySelector('#input-' + value);
+
+                        if (input) {
+                            input.classList.add('is-invalid');
+
+                            // If the element has inputs inside.
+                            input.querySelectorAll('.form-control, .form-select, .form-check-input, .form-check-label').forEach(element => element.classList.add('is-invalid'));
+                        }
+
+                        let error = target.querySelector('#error-' + value);
+
+                        if (error) {
+                            error.classList.add('d-block');
+                        }
+                    }
+                }
+
+                // Display success message
+                if (json['success'] !== undefined) {
+                    let alert = target.querySelector('#alert');
+
+                    if (alert) {
+                        alert.prepend('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json['success'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+                    }
+
+                    let output = [];
+
+                    console.log(json['products']);
+
+                    //console.log(Object.fromEntries(form));
+                    for (let product of json['products']) {
+                        cart.add(product);
+                    }
+
+                    let button = document.querySelector('#cart > button');
+
+                    button.click();
+                }
+            },
+            onError: (e) => {
+                console.log('onError', e);
+            }
+        });
 
     }
 

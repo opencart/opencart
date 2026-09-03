e038cc69e949d63e49593d70a009b5b569355252
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-09-03 17:35:42 +0800

    Update register.js

--- upload/catalog/view/javascript/checkout/register.js
+++ upload/catalog/view/javascript/checkout/register.js
@@ -1,78 +1,94 @@
-import { Controller } from '../component.js';
+import { WebComponent } from '../component.js';
 import { loader } from '../index.js';
 
 // library
+const ajax = await loader.library('ajax');
 const cart = await loader.library('cart');
 const customer = await loader.library('customer');
 
-export default class extends Controller {
-    async connected() {
+customElements.define('checkout-register', class extends WebComponent {
+    token = '';
 
+    async connected() {
+        this.token = ajax.get('action.php?route=checkout/register.token');
     }
 
     async render() {
         let data = {};
 
 
+        data.token = this.token;
 
         return loader.template('checkout/register', { ...data,  ...language });
     }
-}
 
+    onSubmit(e) {
+        e.preventDefault();
 
-// Account
-$('input[name=\'account\']').on('click', function() {
-    if ($(this).val() == 1) {
-        $('#password').removeClass('d-none');
-    } else {
-        // If guest hide password field
-        $('#password').addClass('d-none');
-    }
+        console.log('onSubmit');
 
-    if ($(this).val() == 1) {
-        $('#register-agree').removeClass('d-none');
-    } else {
-        // If guest hide register agree field
-        $('#register-agree').addClass('d-none');
-    }
-});
+        let target = e.target;
 
-$('input[name=\'account\']:checked').trigger('click');
+        let form = new FormData(target);
 
-// Customer Group
-$('#input-customer-group').on('change', function() {
-    var element = this;
+        ajax.post('action.php?route=checkout/register.save&language={{ language }}', form, {
+            beforeSend: (request) => {
+                //this.bind('button-cart').setAttribute('loading', '');
+            },
+            onComplete: (json) => {
+                //console.log(this.bind('button-cart'));
 
-    $.ajax({
-        url: 'action.php?route=account/custom_field&language={{ language }}&customer_group_id=' + $(element).val(),
-        dataType: 'json',
-        beforeSend: function() {
-            $(element).prop('disabled', true);
-        },
-        complete: function() {
-            $(element).prop('disabled', false);
-        },
-        success: function(json) {
-            $('.custom-field').addClass('d-none');
-            $('.custom-field').removeClass('required');
+                //this.bind('button-cart').loading = false;
+            },
+            onSuccess: (json) => {
+                console.log('onSuccess', json);
 
-            for (i = 0; i < json.length; i++) {
-                custom_field = json[i];
+                // Remove past error classes from inputs
+                target.querySelectorAll('.is-invalid').forEach(element => element.classList.remove('is-invalid'));
+                target.querySelectorAll('.invalid-feedback').forEach(element => element.classList.remove('d-block'));
 
-                $('.custom-field-' + custom_field['custom_field_id']).removeClass('d-none');
+                // Display error messages
+                if (json['error'] !== undefined) {
+                    for (let key in json['error']) {
+                        let value = key.replaceAll('_', '-');
 
-                if (custom_field['required']) {
-                    $('.custom-field-' + custom_field['custom_field_id']).addClass('required');
+                        let input = target.querySelector('#input-' + value);
+
+                        if (input) {
+                            input.classList.add('is-invalid');
+
+                            // If the element has inputs inside.
+                            input.querySelectorAll('.form-control, .form-select, .form-check-input, .form-check-label').forEach(element => element.classList.add('is-invalid'));
+                        }
+
+                        let error = target.querySelector('#error-' + value);
+
+                        if (error) {
+                            error.classList.add('d-block');
+                        }
+                    }
                 }
-            }
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-            console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
-        }
-    });
-});
 
-$('#input-customer-group').trigger('change');
+                // Display success message
+                if (json['success'] !== undefined) {
+                    let alert = target.querySelector('#alert');
+
+                    if (alert) {
+                        alert.prepend('<div class="alert alert-success alert-dismissible"><i class="fa-solid fa-circle-check"></i> ' + json['success'] + ' <button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>');
+                    }
+                }
+            },
+            onError: (e) => {
+                console.log('onError', e);
+            }
+        });
+
+    }
+
+    onChange() {
+
+    }
+});
 
 
 // Register
@@ -136,3 +152,58 @@ $('#form-register').on('submit', function(e) {
         }
     });
 });
+
+// Account
+$('input[name=\'account\']').on('click', function() {
+    if ($(this).val() == 1) {
+        $('#password').removeClass('d-none');
+    } else {
+        // If guest hide password field
+        $('#password').addClass('d-none');
+    }
+
+    if ($(this).val() == 1) {
+        $('#register-agree').removeClass('d-none');
+    } else {
+        // If guest hide register agree field
+        $('#register-agree').addClass('d-none');
+    }
+});
+
+$('input[name=\'account\']:checked').trigger('click');
+
+// Customer Group
+$('#input-customer-group').on('change', function() {
+    var element = this;
+
+    $.ajax({
+        url: 'action.php?route=account/custom_field&language={{ language }}&customer_group_id=' + $(element).val(),
+        dataType: 'json',
+        beforeSend: function() {
+            $(element).prop('disabled', true);
+        },
+        complete: function() {
+            $(element).prop('disabled', false);
+        },
+        success: function(json) {
+            $('.custom-field').addClass('d-none');
+            $('.custom-field').removeClass('required');
+
+            for (i = 0; i < json.length; i++) {
+                custom_field = json[i];
+
+                $('.custom-field-' + custom_field['custom_field_id']).removeClass('d-none');
+
+                if (custom_field['required']) {
+                    $('.custom-field-' + custom_field['custom_field_id']).addClass('required');
+                }
+            }
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+        }
+    });
+});
+
+$('#input-customer-group').trigger('change');
+

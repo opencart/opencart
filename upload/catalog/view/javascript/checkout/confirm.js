5b2412c21bf8f663c50b261d28c9c988f3f3ad98
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-09-14 04:07:15 +0800

    Update confirm.js

--- upload/catalog/view/javascript/checkout/confirm.js
+++ upload/catalog/view/javascript/checkout/confirm.js
@@ -1,8 +1,16 @@
-import {Controller, WebComponent} from '../component.js';
+import { WebComponent } from '../component.js';
 import { loader } from '../index.js';
 
 // Config
 const config = await loader.config('default');
+
+// library
+const ajax = await loader.library('ajax');
+const cart = await loader.library('cart');
+const customer = await loader.library('customer');
+
+// Language
+const language = await loader.language('checkout/confirm');
 
 customElements.define('checkout-confirm', class extends WebComponent {
     async connect(){

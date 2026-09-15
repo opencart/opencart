8cc9431ad4d7403bbd12171ea34b29ae99bc682a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-09-16 04:05:27 +0800

    Update index.js

--- upload/assets/framework/index.js
+++ upload/assets/framework/index.js
@@ -1,2 +1,10 @@
 export { loader } from './library/loader.js';
-export { binder, action } from './library/binder.js';
+export { action } from './library/action.js';
+export { ajax } from './library/ajax.js';
+export { binder } from './library/binder.js';
+export { config } from './library/config.js';
+export { language } from './library/language.js';
+export { local } from './library/local.js';
+export { session } from './library/session.js';
+export { storage } from './library/storage.js';
+export { template } from './library/template.js';

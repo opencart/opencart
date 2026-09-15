87b61fe05964c308483840193a7a3d8b9e8e809a
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-09-16 01:59:43 +0800

    Update index.js

--- upload/catalog/view/javascript/index.js
+++ upload/catalog/view/javascript/index.js
@@ -1,1 +1,12 @@
-export { loader, binder, action } from '../../../assets/framework/index.js';
+import { ajax, config, language, local, loader, session, storage, template } from './../../../assets/framework/index.js';
+
+const cart = await loader.library('cart');
+const currency = await loader.library('currency');
+const customer = await loader.library('customer');
+const length = await loader.library('length');
+const tax = await loader.library('tax');
+const weight = await loader.library('weight');
+
+export { ajax, cart, config, currency, customer, language, length, loader, local, session, storage, tax, template, weight };
+
+//export { binder, action } from './library/binder.js';

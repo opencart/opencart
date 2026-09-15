ce75c5296f941780521c7b8fc1c81ed79da08f7d
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-09-16 02:00:28 +0800

    Update loader.js

--- upload/assets/framework/library/loader.js
+++ upload/assets/framework/library/loader.js
@@ -1,46 +1,24 @@
-import { Config } from './config.js';
-import { Language } from './language.js';
-import { Storage } from './storage.js';
-import { Template } from './template.js';
+import { config, language, storage, template } from '../index.js';
 
 class Loader {
-    constructor() {
-        this.data = new Map();
-        this.data.set('config', new Config());
-        this.data.set('language', new Language());
-        this.data.set('storage', new Storage());
-        this.data.set('template', new Template());
-    }
-
-    async controller(path) {
-        let key = 'controller/' + path;
-
-        if (this.data.has(key)) return this.data.get(key);
-
-        let config = this.data.get('config').fetch('default');
-
-        let controller = await import(config.config_path + path + '.js');
-
-        this.data.set(key, new controller.default());
-
-        return this.data.get(key);
-    }
+    instance;
+    data = new Map();
 
     async storage(path) {
-        return await this.data.get('storage').fetch(path);
+        return await storage.fetch(path);
     }
 
     async language(path) {
-        let output = await this.data.get('language').fetch(path);
+        let output = await language.fetch(path);
 
         // Load Default Language
-        let defaults = await this.data.get('language').fetch('default');
+        let defaults = await language.fetch('default');
 
         return { ...output, ...defaults };
     }
 
     async template(path, data = {}) {
-        return await this.data.get('template').render(path, data);
+        return await template.render(path, data);
     }
 
     async library(path) {
@@ -48,13 +26,13 @@ class Loader {
 
         let object = await import('./' + path + '.js');
 
-        this.data.set(path, new object.default());
+        this.data.set(path, object.default.getInstance());
 
         return this.data.get(path);
     }
 
     async config(path) {
-        return await this.data.get('config').fetch(path);
+        return await config.fetch(path);
     }
 
     static getInstance() {

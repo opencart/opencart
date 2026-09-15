977d27e18f5b865cf9497689c582bc58175acf34
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-09-16 02:00:24 +0800

    Update binder.js

--- upload/assets/framework/library/binder.js
+++ upload/assets/framework/library/binder.js
@@ -1,8 +1,9 @@
-class Binder {
+export default class Binder {
+    instance;
     data = new Map();
 
     get(key) {
-        return this.data.get(key);
+        this.data.get(key);
     }
 
     set(key, value) {
@@ -11,104 +12,13 @@ class Binder {
 
     static getInstance() {
         if (!this.instance) {
-            this.instance = new Binder();
+            this.instance = new Map();
         }
 
         return this.instance;
     }
 }
 
-class Action {
-    data = new Map();
+const binder = new Binder();
 
-    register(key, value) {
-        this.data.set(key, value);
-    }
-
-    get(key) {
-        return this.data.get(key);
-    }
-
-    static getInstance() {
-        if (!this.instance) {
-            this.instance = new Action();
-        }
-
-        return this.instance;
-    }
-}
-
-export const binder = new Binder();
-export const action = new Action();
-
-action.register('link', class {
-    element;
-    target = '';
-
-    constructor(element) {
-        element.addEventListener('click', this.onClick.bind(this));
-
-        this.target = element.getAttribute('data-target');
-    }
-
-    onClick(e) {
-        e.preventDefault();
-
-        console.log(this.target);
-
-        let target = document.getElementById(this.target);
-
-        target.src = e.currentTarget.getAttribute('href');
-    }
-});
-
-class Button {
-    element;
-    html = '';
-    width;
-
-    constructor(element) {
-        console.log('BUTTON');
-
-        this.element = element;
-        this.html = element.innerHTML;
-        this.width = element.offsetWidth;
-
-        Object.assign(element, {
-            button: this.button.bind(this),
-        });
-    }
-
-    button(state) {
-        if (state === 'loading') {
-            this.element.innerHTML = '<i class="fa-solid fa-circle-notch fa-spin text-light"></i>';
-            this.element.style.width = this.width;
-
-            this.element.setAttribute('disabled', '');
-        }
-
-        if (state === 'reset') {
-            this.element.innerHTML = this.html;
-            this.element.style.width = '';
-
-            this.element.removeAttribute('disabled');
-        }
-    }
-}
-
-action.register('button', Button);
-
-action.register('form', class {
-    element;
-
-    constructor(element) {
-        this.element = element;
-        this.element.addEventListener('submit', this.onSubmit);
-    }
-
-    onSubmit(e) {
-        e.preventDefault();
-
-    }
-});
-
+export { binder };
